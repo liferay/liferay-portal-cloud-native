d90871b07d7596a69da13ac5db6a93189107004a
Peter Yoo <peter.yoo@liferay.com>
2023-08-25 11:23:52 -0700

    LRCI-3761 Extract GitHubSecondaryRateLimitIOException to its own file

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
@@ -5521,25 +5521,6 @@ public class JenkinsResultsParserUtil {
 
 	}
 
-	public static class GitHubSecondaryRateLimitIOException
-		extends IOException {
-
-		public GitHubSecondaryRateLimitIOException(
-			long retryAfterSeconds, IOException ioException) {
-
-			super(ioException);
-
-			_retryAfterSeconds = retryAfterSeconds;
-		}
-
-		public long getRetryAfterSeconds() {
-			return _retryAfterSeconds;
-		}
-
-		private final long _retryAfterSeconds;
-
-	}
-
 	public abstract static class HTTPAuthorization {
 
 		public Type getType() {
