183a7a2085a83115d610357c57f83e5790c404d3
Bryan Engler <bryan.engler@liferay.com>
2023-09-07 22:02:40 -0700

    LPS-194426 LPS-194423 portal-search: Remove isolation levels since they dont play well with the current framework. Things became more complex when we added the ability to reindex multiple/specific instances a while back. In the future we might need to read background task context maps to see what reindexing operations are currently running and prevent duplicate jobs from being started
    https://liferay.atlassian.net/browse/LPS-194423

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexSingleIndexerBackgroundTaskExecutor.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/background/task/ReindexSingleIndexerBackgroundTaskExecutor.java
@@ -9,9 +9,7 @@ import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
 import com.liferay.osgi.util.service.Snapshot;
 import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.backgroundtask.BackgroundTask;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskExecutor;
-import com.liferay.portal.kernel.backgroundtask.constants.BackgroundTaskConstants;
 import com.liferay.portal.kernel.change.tracking.sql.CTSQLModeThreadLocal;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.log.Log;
@@ -24,14 +22,10 @@ import com.liferay.portal.kernel.search.SearchEngine;
 import com.liferay.portal.kernel.search.SearchEngineHelper;
 import com.liferay.portal.kernel.search.background.task.ReindexBackgroundTaskConstants;
 import com.liferay.portal.kernel.search.background.task.ReindexStatusMessageSender;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.index.SyncReindexManager;
-
-import java.io.Serializable;
 
 import java.util.Collections;
 import java.util.Date;
-import java.util.Map;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
@@ -52,27 +46,9 @@ import org.osgi.service.component.annotations.Reference;
 public class ReindexSingleIndexerBackgroundTaskExecutor
 	extends BaseReindexBackgroundTaskExecutor {
 
-	public ReindexSingleIndexerBackgroundTaskExecutor() {
-		setIsolationLevel(BackgroundTaskConstants.ISOLATION_LEVEL_TASK_NAME);
-	}
-
 	@Override
 	public BackgroundTaskExecutor clone() {
 		return this;
-	}
-
-	@Override
-	public String generateLockKey(BackgroundTask backgroundTask) {
-		Map<String, Serializable> taskContextMap =
-			backgroundTask.getTaskContextMap();
-
-		String className = (String)taskContextMap.get("className");
-
-		if (Validator.isNotNull(className)) {
-			return className;
-		}
-
-		return super.generateLockKey(backgroundTask);
 	}
 
 	@Activate
