c9ae281ddd40d112bd813fa4ae0ae2e73eb9bc28
valmir junior <valmiralvesjr2000@hotmail.com>
2023-09-11 17:49:28 -0300

    LPS-195858 Transform class NodeExecutorRegistry for class Util, and apply to usages

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/node/util/NodeExecutorRegistryUtil.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/internal/node/util/NodeExecutorRegistryUtil.java
@@ -3,30 +3,34 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.workflow.kaleo.runtime.internal.node;
+package com.liferay.portal.workflow.kaleo.runtime.internal.node.util;
 
 import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.workflow.kaleo.runtime.node.NodeExecutor;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * @author Michael C. Han
  */
-@Component(service = NodeExecutorRegistry.class)
-public class NodeExecutorRegistry {
+public class NodeExecutorRegistryUtil {
 
-	public NodeExecutor getNodeExecutor(String nodeTypeString) {
+	public static NodeExecutor getNodeExecutor(String nodeTypeString) {
 		return _serviceTrackerMap.getService(nodeTypeString);
 	}
 
-	@Activate
-	protected void activate(BundleContext bundleContext) {
+	private static final ServiceTrackerMap<String, NodeExecutor>
+		_serviceTrackerMap;
+
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(NodeExecutorRegistryUtil.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
 		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
 			bundleContext, NodeExecutor.class, null,
 			ServiceReferenceMapperFactory.create(
@@ -34,12 +38,5 @@ public class NodeExecutorRegistry {
 				(nodeExecutor, emitter) -> emitter.emit(
 					String.valueOf(nodeExecutor.getNodeType()))));
 	}
-
-	@Deactivate
-	protected void deactivate() {
-		_serviceTrackerMap.close();
-	}
-
-	private ServiceTrackerMap<String, NodeExecutor> _serviceTrackerMap;
 
 }
