d19c8a00ddfd9dd019eb2925e5e2a8727ec4f87d
Julius Lee <meng-huan.lee@liferay.com>
2023-09-25 13:56:09 -0700

    LPS-197236 Move InterruptedPortletRequestWhitelistUtil to portal-impl

--- portal-impl/src/com/liferay/portal/security/auth/InterruptedPortletRequestWhitelistUtil.java
+++ portal-impl/src/com/liferay/portal/security/auth/InterruptedPortletRequestWhitelistUtil.java
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.security.auth;
+package com.liferay.portal.security.auth;
+
+import com.liferay.portal.kernel.security.auth.PortletRequestWhitelist;
 
 import java.util.Set;
 
