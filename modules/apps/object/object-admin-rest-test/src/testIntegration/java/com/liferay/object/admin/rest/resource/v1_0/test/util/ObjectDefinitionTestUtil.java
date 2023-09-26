brianchandotcom/liferay-portal

LPS-192651 Create class to simplify add object definition

modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/util/ObjectDefinitionTestUtil.java
@@ -1,0 +1,57 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.admin.rest.resource.v1_0.test.util;
+
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.service.ObjectDefinitionLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
+
+import java.util.Collections;
+
+/**
+ * @author Murilo Stodolni
+ */
+public class ObjectDefinitionTestUtil {
+
+	public static ObjectDefinition addObjectDefinition()
+		throws PortalException {
+
+		return addObjectDefinition(0);
+	}
+
+	public static ObjectDefinition addObjectDefinition(
+			boolean enableLocalization)
+		throws PortalException {
+
+		return addObjectDefinition(0, enableLocalization);
+	}
+
+	public static ObjectDefinition addObjectDefinition(long objectFolderId)
+		throws PortalException {
+
+		return addObjectDefinition(objectFolderId, false);
+	}
+
+	public static ObjectDefinition addObjectDefinition(
+			long objectFolderId, boolean enableLocalization)
+		throws PortalException {
+
+		String value = "A" + RandomTestUtil.randomString();
+
+		return ObjectDefinitionLocalServiceUtil.addCustomObjectDefinition(
+			TestPropsValues.getUserId(), objectFolderId, false,
+			enableLocalization, false, LocalizedMapUtil.getLocalizedMap(value),
+			value, null, null, LocalizedMapUtil.getLocalizedMap(value), true,
+			ObjectDefinitionConstants.SCOPE_COMPANY,
+			ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
+			Collections.emptyList());
+	}
+
+}

liferay/liferay-portal-cloud-native

modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/util/ObjectDefinitionTestUtil.java
@@ -1,0 +1,58 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.admin.rest.resource.v1_0.test.util;
+
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.service.ObjectDefinitionLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
+
+import java.util.Collections;
+
+/**
+ * @author Murilo Stodolni
+ */
+public class ObjectDefinitionTestUtil {
+
+	public static ObjectDefinition addCustomObjectDefinition()
+		throws PortalException {
+
+		return addCustomObjectDefinition(0);
+	}
+
+	public static ObjectDefinition addCustomObjectDefinition(
+			boolean enableLocalization)
+		throws PortalException {
+
+		return addCustomObjectDefinition(0, enableLocalization);
+	}
+
+	public static ObjectDefinition addCustomObjectDefinition(
+			long objectFolderId)
+		throws PortalException {
+
+		return addCustomObjectDefinition(objectFolderId, false);
+	}
+
+	public static ObjectDefinition addCustomObjectDefinition(
+			long objectFolderId, boolean enableLocalization)
+		throws PortalException {
+
+		String value = "A" + RandomTestUtil.randomString();
+
+		return ObjectDefinitionLocalServiceUtil.addCustomObjectDefinition(
+			TestPropsValues.getUserId(), objectFolderId, false,
+			enableLocalization, false, LocalizedMapUtil.getLocalizedMap(value),
+			value, null, null, LocalizedMapUtil.getLocalizedMap(value), true,
+			ObjectDefinitionConstants.SCOPE_COMPANY,
+			ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
+			Collections.emptyList());
+	}
+
+}