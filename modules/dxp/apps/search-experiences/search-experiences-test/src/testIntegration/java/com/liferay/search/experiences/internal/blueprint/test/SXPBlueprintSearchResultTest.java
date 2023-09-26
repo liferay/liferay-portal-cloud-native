liferay-search/liferay-portal

LPS-192640 LPS-193200 search-experiences-test: Add externalReferenceCode

modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/test/SXPBlueprintSearchResultTest.java
@@ -2462,6 +2462,7 @@ public class SXPBlueprintSearchResultTest {
 			_sxpBlueprint.getConfigurationJSON(),
 			_sxpBlueprint.getDescriptionMap(),
 			_sxpBlueprint.getElementInstancesJSON(),
+			_sxpBlueprint.getExternalReferenceCode(),
 			_sxpBlueprint.getSchemaVersion(), _sxpBlueprint.getTitleMap(),
 			_serviceContext);
 	}

modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/service/test/SXPElementLocalServiceTest.java
@@ -127,7 +127,8 @@ public class SXPElementLocalServiceTest {
 		sxpElement = _sxpElementLocalService.updateSXPElement(
 			sxpElement.getUserId(), sxpElement.getSXPElementId(),
 			sxpElement.getDescriptionMap(),
-			sxpElement.getElementDefinitionJSON(), sxpElement.isHidden(),
+			sxpElement.getElementDefinitionJSON(),
+			sxpElement.getExternalReferenceCode(), sxpElement.isHidden(),
 			sxpElement.getSchemaVersion(), sxpElement.getTitleMap(),
 			ServiceContextTestUtil.getServiceContext());
 

liferay/liferay-portal-cloud-native

modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/test/SXPBlueprintSearchResultTest.java
@@ -2458,7 +2458,8 @@ public class SXPBlueprintSearchResultTest {
 
 	private void _updateSXPBlueprint() throws Exception {
 		_sxpBlueprintLocalService.updateSXPBlueprint(
-			_sxpBlueprint.getUserId(), _sxpBlueprint.getSXPBlueprintId(),
+			_sxpBlueprint.getExternalReferenceCode(), _sxpBlueprint.getUserId(),
+			_sxpBlueprint.getSXPBlueprintId(),
 			_sxpBlueprint.getConfigurationJSON(),
 			_sxpBlueprint.getDescriptionMap(),
 			_sxpBlueprint.getElementInstancesJSON(),

modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/service/test/SXPElementLocalServiceTest.java
@@ -125,8 +125,8 @@ public class SXPElementLocalServiceTest {
 			externalReferenceCode, sxpElement.getExternalReferenceCode());
 
 		sxpElement = _sxpElementLocalService.updateSXPElement(
-			sxpElement.getUserId(), sxpElement.getSXPElementId(),
-			sxpElement.getDescriptionMap(),
+			sxpElement.getExternalReferenceCode(), sxpElement.getUserId(),
+			sxpElement.getSXPElementId(), sxpElement.getDescriptionMap(),
 			sxpElement.getElementDefinitionJSON(), sxpElement.isHidden(),
 			sxpElement.getSchemaVersion(), sxpElement.getTitleMap(),
 			ServiceContextTestUtil.getServiceContext());