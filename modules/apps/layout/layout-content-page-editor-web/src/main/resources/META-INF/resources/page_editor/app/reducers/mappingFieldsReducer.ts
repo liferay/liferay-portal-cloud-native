801548e7d2f3339dcfb806c2917ea5e2b1cd96c6
Pablo Molina <contact@pablomolina.me>
2023-09-27 16:18:43 +0200

    LPS-197423 Migrate mappingFieldsReducer to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/mappingFieldsReducer.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/mappingFieldsReducer.ts
@@ -3,13 +3,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import addMappingFields, {
+	MappingFieldFieldSet,
+} from '../actions/addMappingFields';
 import {ADD_MAPPING_FIELDS} from '../actions/types';
 
-export const INITIAL_STATE = {};
+export const INITIAL_STATE: Record<string, MappingFieldFieldSet[]> = {};
 
 export default function mappedInfoItemsReducer(
 	mappingFields = INITIAL_STATE,
-	action
+	action: ReturnType<typeof addMappingFields>
 ) {
 	switch (action.type) {
 		case ADD_MAPPING_FIELDS:
