03dda0d18b9ac162a58fb2f43c4cf91cc4df9255
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-09-28 11:09:29 -0300

    Revert "LPS-189313 Not need, create the job in WorkflowMetricsIndexPortalInstanceLifecycleListener"
    This reverts commit ae27f840bdd6b63f8e5cdbd72df94e22938ae7cb.

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/search/index/creation/instance/lifecycle/WorkflowMetricsReindexPortalInstanceLifecycleListener.java
@@ -0,0 +1,66 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.workflow.metrics.internal.search.index.creation.instance.lifecycle;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.messaging.DestinationNames;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.StorageType;
+import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.Trigger;
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.messaging.SchedulerResponse;
+
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(service = PortalInstanceLifecycleListener.class)
+public class WorkflowMetricsReindexPortalInstanceLifecycleListener
+	extends BasePortalInstanceLifecycleListener {
+
+	@Override
+	public void portalInstanceRegistered(Company company) throws Exception {
+		String jobName = StringBundler.concat(
+			DestinationNames.WORKFLOW_METRICS_REINDEX, StringPool.SLASH,
+			company.getCompanyId());
+
+		SchedulerResponse schedulerResponse =
+			_schedulerEngineHelper.getScheduledJob(
+				jobName, jobName, StorageType.MEMORY);
+
+		if (schedulerResponse != null) {
+			return;
+		}
+
+		Trigger trigger = _triggerFactory.createTrigger(
+			jobName, jobName, new Date(), null, 5, TimeUnit.SECOND);
+
+		Message message = new Message();
+
+		message.put("companyId", company.getCompanyId());
+
+		_schedulerEngineHelper.schedule(
+			trigger, StorageType.MEMORY, null,
+			DestinationNames.WORKFLOW_METRICS_REINDEX, message);
+	}
+
+	@Reference
+	private SchedulerEngineHelper _schedulerEngineHelper;
+
+	@Reference
+	private TriggerFactory _triggerFactory;
+
+}
