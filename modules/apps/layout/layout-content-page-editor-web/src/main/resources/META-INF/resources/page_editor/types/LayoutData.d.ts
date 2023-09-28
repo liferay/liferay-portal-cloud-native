f5837737b0e7c8ccf2cbcad183a25db1aca1d70c
Pablo Molina <contact@pablomolina.me>
2023-09-28 11:18:02 +0200

    LPS-197423 Add LayoutData configuration types

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/types/LayoutData.d.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/types/LayoutData.d.ts
@@ -3,7 +3,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {LayoutDataItemType} from '../app/config/constants/layoutDataItemTypes';
+import {CollectionItemLayoutDataItem} from './layout_data/CollectionItemLayoutDataItem';
+import {CollectionLayoutDataItem} from './layout_data/CollectionLayoutDataItem';
+import {ColumnLayoutDataItem} from './layout_data/ColumnLayoutDataItem';
+import {ContainerLayoutDataItem} from './layout_data/ContainerLayoutDataItem';
+import {DropZoneLayoutDataItem} from './layout_data/DropZoneLayoutDataItem';
+import {FormLayoutDataItem} from './layout_data/FormLayoutDataItem';
+import {FragmentDropZoneLayoutDataItem} from './layout_data/FragmentDropZoneLayoutDataItem';
+import {FragmentLayoutDataItem} from './layout_data/FragmentLayoutDataItem';
+import {RootLayoutDataItem} from './layout_data/RootLayoutDataItem';
+import {RowLayoutDataItem} from './layout_data/RowLayoutDataItem';
 
 export interface DeletedLayoutDataItem {
 	childrenItemIds: string[];
@@ -12,13 +21,17 @@ export interface DeletedLayoutDataItem {
 	position: number;
 }
 
-export interface LayoutDataItem {
-	children: string[];
-	config: Record<string, unknown>;
-	itemId: string;
-	itemType: LayoutDataItemType;
-	parentId: string;
-}
+export type LayoutDataItem =
+	| CollectionItemLayoutDataItem
+	| CollectionLayoutDataItem
+	| ColumnLayoutDataItem
+	| ContainerLayoutDataItem
+	| DropZoneLayoutDataItem
+	| FormLayoutDataItem
+	| FragmentDropZoneLayoutDataItem
+	| FragmentLayoutDataItem
+	| RootLayoutDataItem
+	| RowLayoutDataItem;
 
 export interface LayoutData {
 	deletedItems: DeletedLayoutDataItem[];
---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/types/layout_data/ContainerLayoutDataItem.d.ts
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {
+	BaseLayoutDataItem,
+	CommonStyles,
+	ResponsiveConfig,
+	TranslatedConfig,
+} from './BaseLayoutDataItem';
+
+export type ContainerGeneralConfig = ResponsiveConfig<{
+	align?: 'align-items-end';
+	contentDisplay?: 'flex-row';
+	flexWrap?: 'flex-wrap';
+	justify?: 'justify-content-end';
+	widthType?: 'fixed';
+}>;
+
+export type ContainerLayoutDataItem = BaseLayoutDataItem<
+	'container',
+	CommonStyles &
+		ContainerGeneralConfig & {
+			contentVisibility?: string;
+			htmlTag: 'header';
+			indexed?: boolean;
+			link?: {
+				href: TranslatedConfig<string>;
+				target: '_blank';
+			};
+		}
+>;
