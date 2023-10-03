a18f4c5ca0b482ac03375935e5983b9295e412ab
Carlos Montenegro <carlos.montenegro@liferay.com>
2023-10-02 21:47:59 -0300

    LPS-197615 Add filter field component to endpoint configuration form

--- modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/EditEndpointConfiguration.tsx
+++ modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/EditEndpointConfiguration.tsx
@@ -3,7 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {Text} from '@clayui/core';
 import ClayForm from '@clayui/form';
+import ClayIcon from '@clayui/icon';
+import {ClayTooltipProvider} from '@clayui/tooltip';
+import {sub} from 'frontend-js-web';
 import React, {Dispatch, SetStateAction, useEffect, useState} from 'react';
 
 import {Select} from './fieldComponents/Select';
@@ -75,6 +79,10 @@ export default function EditEndpointConfiguration({
 		);
 	};
 
+	const endpointFiltersInstruction = Liferay.Language.get(
+		'type-any-filter-using-odata-language'
+	);
+
 	return (
 		<ClayForm>
 			<ClayForm.Group>
@@ -92,6 +100,64 @@ export default function EditEndpointConfiguration({
 					placeholder={Liferay.Language.get('select-a-schema')}
 					searchable
 					selectedOption={selectedResponseBodySchema}
+				/>
+			</ClayForm.Group>
+
+			<ClayForm.Group>
+				<label htmlFor="endpointFiltersField">
+					{Liferay.Language.get('filters')}
+
+					<ClayTooltipProvider>
+						<span
+							data-tooltip-align="top"
+							title={`${Liferay.Language.get(
+								'there-is-a-limit-of-1000-characters-in-odata-language'
+							)} ${sub(
+								Liferay.Language.get(
+									'remember-not-to-include-the-x'
+								),
+								'?filter='
+							)}`}
+						>
+							&nbsp;
+							<ClayIcon symbol="question-circle-full" />
+						</span>
+					</ClayTooltipProvider>
+				</label>
+
+				<Text as="p" id="hostTextPreview" size={2} weight="lighter">
+					/?filter=
+				</Text>
+
+				<textarea
+					aria-label={endpointFiltersInstruction}
+					autoComplete="off"
+					className="form-control"
+					id="endpointFiltersField"
+					onChange={({target: {value}}) =>
+						setData((previousData) => ({
+							...previousData,
+							...(value !== ''
+								? {
+										apiEndpointToAPIFilters: [
+											{
+												...(previousData
+													.apiEndpointToAPIFilters?.[0]
+													?.id && {
+													id:
+														previousData
+															.apiEndpointToAPIFilters?.[0]
+															.id,
+												}),
+												oDataFilter: value,
+											},
+										],
+								  }
+								: {apiEndpointToAPIFilters: []}),
+						}))
+					}
+					placeholder={endpointFiltersInstruction}
+					value={data.apiEndpointToAPIFilters?.[0]?.oDataFilter}
 				/>
 			</ClayForm.Group>
 		</ClayForm>
