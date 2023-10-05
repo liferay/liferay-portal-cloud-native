6e2e5c0288f3a1620879a3d3bd8ac58187957da6
Jiefeng Wu <jeffwu0724@gmail.com>
2023-10-05 10:04:29 -0700

    LPS-198266 Move RuntimePageUtil into portal-impl

--- portal-impl/src/com/liferay/portal/layoutconfiguration/util/RuntimePageUtil.java
+++ portal-impl/src/com/liferay/portal/layoutconfiguration/util/RuntimePageUtil.java
@@ -3,9 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.layoutconfiguration.util;
+package com.liferay.portal.layoutconfiguration.util;
 
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.layoutconfiguration.util.RuntimePage;
 import com.liferay.portal.kernel.model.LayoutTemplate;
 
 import javax.servlet.http.HttpServletRequest;
