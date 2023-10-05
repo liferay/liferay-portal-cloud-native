0a0cbbc92b59b2f6035d070bfebc062dfabcb0e3
Brian Chan <brian.chan@liferay.com>
2023-10-05 15:44:19 +0200

    LPS-197639 Rename

--- workspaces/liferay-partner-workspace/client-extensions/liferay-partner-etc-cron/src/main/java/com/liferay/partner/PartnerCommandLineRunner.java
+++ workspaces/liferay-partner-workspace/client-extensions/liferay-partner-etc-cron/src/main/java/com/liferay/partner/PartnerCommandLineRunner.java
@@ -218,7 +218,7 @@ public class PartnerCommandLineRunner implements CommandLineRunner {
 		).exchangeStrategies(
 			ExchangeStrategies.builder(
 			).codecs(
-				configurer -> configurer.defaultCodecs(
+				clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs(
 				).maxInMemorySize(
 					5 * 1024 * 1024
 				)
