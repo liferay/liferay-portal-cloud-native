6328d7facb302267b2e6e338ecc6aee85bfb0c70
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-10-06 13:54:18 +0200

    LPS-196807 Add an integration test to assert that all versions, approved or not, are indexed using the right content

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleAssetEntryReindexTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticleAssetEntryReindexTest.java
@@ -12,12 +12,19 @@ import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
+import com.liferay.journal.constants.JournalArticleConstants;
 import com.liferay.journal.constants.JournalFolderConstants;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.petra.string.StringUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistry;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -25,15 +32,23 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Localization;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.search.legacy.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.test.util.IndexerFixture;
+import com.liferay.portal.search.test.util.SearchContextTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
+import java.io.Serializable;
+
 import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -131,14 +146,155 @@ public class JournalArticleAssetEntryReindexTest {
 		_assertSearch(updatedAssetTagName, journalArticle);
 	}
 
-	private void _assertSearch(String keyword, JournalArticle journalArticle) {
-		Document document = _indexerFixture.searchOnlyOne(keyword);
+	@Test
+	public void testUpdateJournalArticleTitleWithMultipleVersions()
+		throws Exception {
+
+		Locale locale = _portal.getSiteDefaultLocale(_group);
+
+		String title = RandomTestUtil.randomString();
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		JournalArticle journalArticle = JournalTestUtil.addArticle(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASS_NAME_ID_DEFAULT, StringPool.BLANK,
+			true,
+			HashMapBuilder.put(
+				locale, title
+			).build(),
+			RandomTestUtil.randomLocaleStringMap(locale),
+			RandomTestUtil.randomLocaleStringMap(locale), null, locale, null,
+			false, true, serviceContext);
+
+		_assertSearch(title, journalArticle);
+
+		Map<Double, String> versionTitleMap = HashMapBuilder.put(
+			journalArticle.getVersion(), title
+		).build();
+
+		_assertSearchJournalArticleVersions(
+			"localized_title", journalArticle, versionTitleMap);
+
+		journalArticle = _getUpdatedJournalArticle(
+			true, journalArticle, serviceContext, versionTitleMap);
+
+		_assertSearchJournalArticleVersions(
+			"localized_title", journalArticle, versionTitleMap);
+
+		journalArticle = _getUpdatedJournalArticle(
+			true, journalArticle, serviceContext, versionTitleMap);
+
+		_assertSearchJournalArticleVersions(
+			"localized_title", journalArticle, versionTitleMap);
+
+		journalArticle = _getUpdatedJournalArticle(
+			false, journalArticle, serviceContext, versionTitleMap);
+
+		_assertSearchJournalArticleVersions(
+			"localized_title", journalArticle, versionTitleMap);
+	}
+
+	private void _assertArticleId(
+		Document document, JournalArticle journalArticle) {
 
 		String[] values = document.getValues(Field.ARTICLE_ID);
 
 		Assert.assertEquals(values.toString(), 1, values.length);
 
 		Assert.assertEquals(journalArticle.getArticleId(), values[0]);
+	}
+
+	private void _assertSearch(String keyword, JournalArticle journalArticle) {
+		Document document = _indexerFixture.searchOnlyOne(keyword);
+
+		_assertArticleId(document, journalArticle);
+	}
+
+	private void _assertSearchJournalArticleVersions(
+		String fieldName, JournalArticle journalArticle,
+		Map<Double, String> versionFieldValueMap) {
+
+		Document[] documents = _searchJournalArticleVersions(
+			journalArticle.getDescriptionCurrentValue());
+
+		Assert.assertEquals(
+			documents.toString(), versionFieldValueMap.size(),
+			documents.length);
+
+		for (Document document : documents) {
+			_assertArticleId(document, journalArticle);
+
+			double version = GetterUtil.getDouble(document.get("versionCount"));
+
+			Assert.assertTrue(versionFieldValueMap.containsKey(version));
+
+			Assert.assertTrue(document.hasField(fieldName));
+
+			String fieldValue = document.get(fieldName);
+
+			Assert.assertTrue(
+				fieldValue,
+				StringUtil.equalsIgnoreCase(
+					versionFieldValueMap.get(version), fieldValue));
+		}
+	}
+
+	private JournalArticle _getUpdatedJournalArticle(
+			boolean approved, JournalArticle journalArticle,
+			ServiceContext serviceContext, Map<Double, String> versionTitleMap)
+		throws Exception {
+
+		if (approved) {
+			serviceContext.setWorkflowAction(WorkflowConstants.ACTION_PUBLISH);
+		}
+		else {
+			serviceContext.setWorkflowAction(
+				WorkflowConstants.ACTION_SAVE_DRAFT);
+		}
+
+		String title = RandomTestUtil.randomString();
+
+		JournalArticle updatedJournalArticle = JournalTestUtil.updateArticle(
+			journalArticle, title, journalArticle.getContent(), false, approved,
+			serviceContext);
+
+		if (approved) {
+			_assertSearch(title, updatedJournalArticle);
+		}
+
+		versionTitleMap.put(updatedJournalArticle.getVersion(), title);
+
+		return updatedJournalArticle;
+	}
+
+	private Document[] _searchJournalArticleVersions(String keywords) {
+		try {
+			Indexer<JournalArticle> indexer = _indexerRegistry.getIndexer(
+				JournalArticle.class.getName());
+
+			Hits hits = indexer.search(
+				SearchContextTestUtil.getSearchContext(
+					TestPropsValues.getUserId(),
+					new long[] {_group.getGroupId()}, keywords, null,
+					HashMapBuilder.<String, Serializable>put(
+						Field.STATUS, WorkflowConstants.STATUS_ANY
+					).put(
+						"head", false
+					).put(
+						"latest", false
+					).put(
+						"showNonindexable", false
+					).build()));
+
+			return hits.getDocs();
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
 	}
 
 	@Inject
@@ -154,6 +310,15 @@ public class JournalArticleAssetEntryReindexTest {
 	private Group _group;
 
 	private IndexerFixture<JournalArticle> _indexerFixture;
+
+	@Inject
+	private IndexerRegistry _indexerRegistry;
+
+	@Inject
+	private Localization _localization;
+
+	@Inject
+	private Portal _portal;
 
 	@Inject
 	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
