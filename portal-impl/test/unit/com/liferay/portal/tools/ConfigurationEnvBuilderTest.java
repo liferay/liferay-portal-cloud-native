3dd243bc6684c3d892278c9603b4b96851358452
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-10-11 12:47:27 -0300

    LPS-198272 Follow up - Fix ConfigurationEnvBuilderTest for use in removeSubstring method the com.liferay.portal.kernel.util.StringUtil class

--- portal-impl/test/unit/com/liferay/portal/tools/ConfigurationEnvBuilderTest.java
+++ portal-impl/test/unit/com/liferay/portal/tools/ConfigurationEnvBuilderTest.java
@@ -109,7 +109,7 @@ public class ConfigurationEnvBuilderTest {
 
 		for (String line : lines) {
 			if (line.contains("configuration.override")) {
-				line = StringUtil.removeSubstring(line, StringPool.POUND);
+				line = com.liferay.portal.kernel.util.StringUtil.removeSubstring(line, StringPool.POUND);
 
 				result.add(line.trim());
 			}
