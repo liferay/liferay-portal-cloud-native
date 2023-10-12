302fb20b4d8602a36f815f7b093b8641504fc9c7
Julius Lee <meng-huan.lee@liferay.com>
2023-10-11 18:16:27 -0700

    LPS-198997 Move UserBagFactoryUtil to portal-impl

--- portal-impl/src/com/liferay/portal/security/permission/UserBagFactoryUtil.java
+++ portal-impl/src/com/liferay/portal/security/permission/UserBagFactoryUtil.java
@@ -3,9 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.security.permission;
+package com.liferay.portal.security.permission;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.UserBag;
+import com.liferay.portal.kernel.security.permission.UserBagFactory;
 
 /**
  * @author Preston Crary
