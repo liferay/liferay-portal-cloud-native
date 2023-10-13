7eb31fa406572b5fe823262fdc7ff038bebf8779
Kenji Heigel <kenji.heigel@liferay.com>
2023-10-13 15:51:30 -0700

    LRCI-3800 Use liferay formatting standard for groovy

--- modules/test/jenkins-results-parser/src/main/resources/com/liferay/jenkins/results/parser/dependencies/get-build-data.groovy
+++ modules/test/jenkins-results-parser/src/main/resources/com/liferay/jenkins/results/parser/dependencies/get-build-data.groovy
@@ -1,51 +1,72 @@
-items = Jenkins.instance.allItems
+import groovy.json.JsonBuilder
 
-endDate = new Date()
+import java.util.Date
+import java.util.List
 
-startDate = endDate.minus(1)
+import jenkins.metrics.impl.TimeInQueueAction
+
+import hudson.model.Item
+import hudson.model.Items
+import hudson.model.Job
+import hudson.model.ParametersAction
+import hudson.model.ParameterValue
+import hudson.model.Result
+import hudson.model.Run
+
+Date endDate = new Date()
+
+Date startDate = endDate.minus(1)
 
 startDate = Date.parse("yyyyMMdd hh:mm:ss", startDate.format("yyyyMMdd") + " 00:00:00")
 
-buildJSONs = []
+List<Object> buildJSONs = []
 
-items.each { item ->
+List<Item> items = Jenkins.instance.allItems
+
+items.each {
+	Item item ->
+
 	if (item instanceof Job) {
-		def builds = item.getBuilds()
+		List<Run> builds = item.getBuilds()
 
-		builds.each { build ->
-			startTime = build.getTimeInMillis()
+		builds.each {
+			Run build ->
+
+			long startTime = build.getTimeInMillis()
 
 			if ((startTime < startDate.getTime()) || (startTime >= endDate.getTime())) {
 				return
 			}
 
-			def buildJSON = new groovy.json.JsonBuilder()
+			JsonBuilder buildJsonBuilder = new JsonBuilder()
 
-			buildUrl = Jenkins.instance.getRootUrl() + build.getUrl()
+			String buildURL = Jenkins.instance.getRootUrl() + build.getUrl()
 
-			parameters = []
+			List<Object> parameters = []
 
-			if (!buildUrl.contains("maintenance") && !buildUrl.contains("verification") && !buildUrl.contains("-controller") && !buildUrl.contains("-propagator")) {
-				parameterAction = build.getAction(hudson.model.ParametersAction.class)
+			if (!buildURL.contains("maintenance") && !buildURL.contains("verification") && !buildURL.contains("-controller") && !buildURL.contains("-propagator")) {
+				ParametersAction parametersAction = build.getAction(hudson.model.ParametersAction.class)
 
-				if (parameterAction != null) {
-					parameterValues = parameterAction.getParameters()
+				if (parametersAction != null) {
+					List<ParameterValue> parameterValues = parametersAction.getParameters()
 
-					parameterValues.each { parameter ->
-						parameterValue = parameter.getValue()
+					parameterValues.each {
+						ParameterValue parameterValue ->
 
-						if ((parameterValue != null) && !parameterValue.isEmpty()) {
-							parameterName = parameter.getName()
+						Object parameterValueObject = parameterValue.getValue()
 
-							if ((buildUrl.contains("-batch") || buildUrl.contains("-downstream")) && !(parameterName.equals("DIST_PATH") || parameterName.equals("JOB_VARIANT"))) {
+						if ((parameterValueObject != null) && !parameterValueObject.isEmpty()) {
+							String parameterValueName = parameterValue.getName()
+
+							if ((buildURL.contains("-batch") || buildURL.contains("-downstream")) && !(parameterValueName.equals("DIST_PATH") || parameterValueName.equals("JOB_VARIANT"))) {
 								return
 							}
 
-							def parameterJson = new groovy.json.JsonBuilder()
+							JsonBuilder parameterJsonBuilder = new JsonBuilder()
 
-							parameterJson name: parameterName, value: parameterValue
+							parameterJsonBuilder name: parameterValueName, value: parameterValueObject
 
-							parameters.add(parameterJson.getContent())
+							parameters.add(parameterJsonBuilder.getContent())
 						}
 
 					}
@@ -53,19 +74,19 @@ items.each { item ->
 				}
 			}
 
-			duration = build.getDuration()
+			TimeInQueueAction timeInQueueAction = build.getAction(jenkins.metrics.impl.TimeInQueueAction.class)
 
-			timeInQueueAction = build.getAction(jenkins.metrics.impl.TimeInQueueAction.class)
+			long queueDuration = 0
 
 			if (timeInQueueAction != null) {
 				queueDuration = timeInQueueAction.getQueuingDurationMillis()
 			}
 
-			result = build.getResult()
+			Result result = build.getResult()
 
-			buildJSON url: buildUrl, startTime: build.getTimeInMillis(), result: result.toString(), duration: duration, queueDuration: queueDuration, parameters: parameters, builtOn: build.getBuiltOnStr()
+			buildJsonBuilder url: buildURL, startTime: build.getTimeInMillis(), result: result.toString(), duration: build.getDuration(), queueDuration: queueDuration, parameters: parameters, builtOn: build.getBuiltOnStr()
 
-			buildJSONs.add(buildJSON)
+			buildJSONs.add(buildJsonBuilder)
 		}
 	}
 }
