1b8f76eccd05244cf0e8bd98689e50049f86562e
Alicia García <alicia.garcia@liferay.com>
2023-10-13 10:02:11 +0200

    LPS-197916 create methods to fetch, create and delete the saved content entries. on remote, checking permissions

--- modules/apps/saved-content/saved-content-service/src/main/java/com/liferay/saved/content/service/impl/SavedContentEntryLocalServiceImpl.java
+++ modules/apps/saved-content/saved-content-service/src/main/java/com/liferay/saved/content/service/impl/SavedContentEntryLocalServiceImpl.java
@@ -6,9 +6,18 @@
 package com.liferay.saved.content.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.saved.content.exception.DuplicateSavedContentEntryException;
+import com.liferay.saved.content.model.SavedContentEntry;
 import com.liferay.saved.content.service.base.SavedContentEntryLocalServiceBaseImpl;
 
+import java.util.Date;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -19,4 +28,63 @@ import org.osgi.service.component.annotations.Component;
 )
 public class SavedContentEntryLocalServiceImpl
 	extends SavedContentEntryLocalServiceBaseImpl {
+
+	@Override
+	public SavedContentEntry addSavedContentEntry(
+			long groupId, long userId, String className, long classPK)
+		throws PortalException {
+
+		_validate(groupId, userId, className, classPK);
+
+		long savedContentEntryId = counterLocalService.increment(
+			SavedContentEntry.class.getName());
+
+		SavedContentEntry savedContentEntry =
+			savedContentEntryPersistence.create(savedContentEntryId);
+
+		savedContentEntry.setGroupId(groupId);
+
+		User user = _userLocalService.getUserById(userId);
+
+		savedContentEntry.setUserId(user.getUserId());
+		savedContentEntry.setUserName(user.getFullName());
+
+		savedContentEntry.setCreateDate(new Date());
+		savedContentEntry.setModifiedDate(new Date());
+		savedContentEntry.setClassNameId(
+			_classNameLocalService.getClassNameId(className));
+		savedContentEntry.setClassPK(classPK);
+
+		return savedContentEntryPersistence.update(savedContentEntry);
+	}
+
+	@Override
+	public SavedContentEntry fetchSavedContentEntry(
+		long groupId, long userId, String className, long classPK) {
+
+		return savedContentEntryPersistence.fetchByG_U_C_C(
+			groupId, userId, _classNameLocalService.getClassNameId(className),
+			classPK);
+	}
+
+	private void _validate(
+			long groupId, long userId, String className, long classPK)
+		throws DuplicateSavedContentEntryException {
+
+		SavedContentEntry savedContentEntry =
+			savedContentEntryPersistence.fetchByG_U_C_C(
+				groupId, userId,
+				_classNameLocalService.getClassNameId(className), classPK);
+
+		if (savedContentEntry != null) {
+			throw new DuplicateSavedContentEntryException();
+		}
+	}
+
+	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
--- modules/apps/saved-content/saved-content-service/src/main/java/com/liferay/saved/content/service/impl/SavedContentEntryServiceImpl.java
+++ modules/apps/saved-content/saved-content-service/src/main/java/com/liferay/saved/content/service/impl/SavedContentEntryServiceImpl.java
@@ -6,9 +6,18 @@
 package com.liferay.saved.content.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.saved.content.constants.SavedContentConstants;
+import com.liferay.saved.content.model.SavedContentEntry;
 import com.liferay.saved.content.service.base.SavedContentEntryServiceBaseImpl;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Brian Wing Shun Chan
@@ -22,4 +31,58 @@ import org.osgi.service.component.annotations.Component;
 )
 public class SavedContentEntryServiceImpl
 	extends SavedContentEntryServiceBaseImpl {
+
+	@Override
+	public SavedContentEntry addSavedContentEntry(
+			long groupId, String className, long classPK)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId, ActionKeys.ADD_ENTRY);
+
+		return savedContentEntryLocalService.addSavedContentEntry(
+			groupId, getUserId(), className, classPK);
+	}
+
+	@Override
+	public void deleteSavedContentEntry(SavedContentEntry savedContentEntry)
+		throws PortalException {
+
+		_savedContentEntryModelResourcePermission.check(
+			getPermissionChecker(), savedContentEntry, ActionKeys.DELETE);
+
+		savedContentEntryLocalService.deleteSavedContentEntry(
+			savedContentEntry);
+	}
+
+	@Override
+	public SavedContentEntry fetchSavedContentEntry(
+			long groupId, String className, long classPK)
+		throws PortalException {
+
+		SavedContentEntry savedContentEntry =
+			savedContentEntryLocalService.fetchSavedContentEntry(
+				groupId, getUserId(), className, classPK);
+
+		if (savedContentEntry != null) {
+			_savedContentEntryModelResourcePermission.check(
+				getPermissionChecker(), savedContentEntry, ActionKeys.VIEW);
+		}
+
+		return savedContentEntry;
+	}
+
+	@Reference(
+		target = "(resource.name=" + SavedContentConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(model.class.name=com.liferay.saved.content.model.SavedContentEntry)"
+	)
+	private volatile ModelResourcePermission<SavedContentEntry>
+		_savedContentEntryModelResourcePermission;
+
 }
