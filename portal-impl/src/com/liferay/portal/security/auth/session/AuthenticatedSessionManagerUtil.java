664a66624042eebc773b2e5a324057a948c97b35
Julius Lee <meng-huan.lee@liferay.com>
2023-10-13 17:41:26 -0700

    LPS-199165 Move AuthenticatedSessionManagerUtil into portal-impl
    # breaking_change_report
    
    ## What portal-impl/src/com/liferay/portal/security/auth/session/AuthenticatedSessionManagerUtil.java
    
    AuthenticatedSessionManagerUtil is being moved to portal-impl
    
    ## Why
    
    We are merging portal-kernel into portal-impl as a long term goal to simplify portal structure.
    
    ## Alternatives
    
    Add portal-impl in build dependency and use the same class.

--- portal-impl/src/com/liferay/portal/security/auth/session/AuthenticatedSessionManagerUtil.java
+++ portal-impl/src/com/liferay/portal/security/auth/session/AuthenticatedSessionManagerUtil.java
@@ -3,9 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.kernel.security.auth.session;
+package com.liferay.portal.security.auth.session;
 
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.auth.session.AuthenticatedSessionManager;
 import com.liferay.portal.kernel.util.ServiceProxyFactory;
 
 import javax.servlet.http.HttpServletRequest;
