0628cfed68929cc027757c390e9932631a90ab77
Kevin Tan <kevin.tan@liferay.com>
2023-10-18 12:53:20 -0700

    LPS-191845 frontend-js-react-web: Fix react portal causing DOMException
    The exception is caused when the container is unmounted before the children. This unmounts the react portal children before the page navigates.

--- modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/js/ReactPortal.tsx
+++ modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/js/ReactPortal.tsx
@@ -5,7 +5,7 @@
 
 import classNames from 'classnames';
 import React from 'react';
-import {createPortal} from 'react-dom';
+import ReactDOM, {createPortal} from 'react-dom';
 
 interface IProps extends React.HTMLAttributes<HTMLDivElement> {
 
@@ -42,7 +42,7 @@ const ReactPortal = React.forwardRef<HTMLElement, IProps>(
 	) => {
 		const cssClass = classNames('lfr-tooltip-scope', className);
 
-		let content;
+		let content: React.ReactNode;
 
 		if (Wrapper) {
 			content = (
@@ -60,6 +60,21 @@ const ReactPortal = React.forwardRef<HTMLElement, IProps>(
 				{className: classNames(cssClass, children.props.className), id}
 			);
 		}
+
+		/**
+		 * When navigating to another page, this error is sometimes thrown:
+		 *
+		 * "Uncaught DOMException: Failed to execute 'removeChild' on 'Node':
+		 * The node to be removed is not a child of this node."
+		 *
+		 * This is caused when the container is unmounted before the children.
+		 * This ensures the portal children are unmounted first.
+		 */
+		Liferay.on('beforeNavigate', () => {
+			ReactDOM.unmountComponentAtNode(
+				content as Element | DocumentFragment
+			);
+		});
 
 		// eslint-disable-next-line @liferay/portal/no-react-dom-create-portal
 		return createPortal(content, container || document.body);
