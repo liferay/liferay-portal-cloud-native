edc0e428495dd3c3dc21f2aa08b407e505b14190
Joana Grego <joana.lima@liferay.com>
2023-10-20 17:26:28 -0300

    ISSD-1342 Improving code

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/CreateLicense/SelectSubscription/index.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/CreateLicense/SelectSubscription/index.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {useState} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 
 import RadioCardList, {
 	RadioCardContent,
@@ -14,35 +14,46 @@ interface SubscriptionSelectionProps {
 	selectedSubscriptionValue?: string;
 }
 
+const licenseKeyData = {
+	avaliableKeys: {
+		provisionedCount: 1,
+		purchasedCount: 1,
+	},
+	supportLifeEndDate: 'Sep 24, 2024',
+	supportLifeStartDate: 'Sep 24, 2023',
+};
+
 const SelectSubscription = ({
 	onSelectSubscription,
 	selectedSubscriptionValue,
 }: SubscriptionSelectionProps) => {
-	const avaliableKeys = {
-		provisionedCount: 1,
-		purchasedCount: 1,
-	};
-	const supportLifeStartDate = 'Sep 24, 2023';
-	const supportLifeEndDate = 'Sep 24, 2024';
-
-	const contentList: RadioCardContent<String>[] = [
-		{
-			description: (
-				<small className="text-success">
-					Key activations available: {avaliableKeys.purchasedCount} of{' '}
-					{avaliableKeys.provisionedCount}
-				</small>
-			),
-			label: `${supportLifeStartDate} - ${supportLifeEndDate}`,
-			selected: selectedSubscriptionValue === 'Trial',
-			title: <h3 className="mt-0">Trial</h3>,
-			value: 'Trial',
-		},
-	];
-
 	const [subscription, setSubscription] = useState<
 		RadioCardContent<String>[]
-	>(contentList);
+	>([]);
+
+	const getSubscriptionList = useCallback(async () => {
+		const contentList: RadioCardContent<String>[] = [
+			{
+				description: (
+					<small className="text-success">
+						Key activations available:{' '}
+						{licenseKeyData.avaliableKeys.purchasedCount} of{' '}
+						{licenseKeyData.avaliableKeys.provisionedCount}
+					</small>
+				),
+				label: `${licenseKeyData.supportLifeStartDate} - ${licenseKeyData.supportLifeEndDate}`,
+				selected: selectedSubscriptionValue === 'Trial',
+				title: <h3 className="mt-0">Trial</h3>,
+				value: 'Trial',
+			},
+		];
+
+		setSubscription(contentList);
+	}, [selectedSubscriptionValue]);
+
+	useEffect(() => {
+		getSubscriptionList();
+	}, [getSubscriptionList]);
 
 	const handleSelect = (radioOption: RadioOption<String>) => {
 		onSelectSubscription(String(radioOption.value));
