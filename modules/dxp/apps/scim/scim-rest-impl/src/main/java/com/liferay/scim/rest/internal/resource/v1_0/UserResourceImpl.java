89d88f53e4b7248f0f3551f2db63b600747a31c1
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2023-10-25 21:31:52 +0100

    LPS-97345 Flesh out all provisioning operations in UserResourceImpl

--- modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/resource/v1_0/UserResourceImpl.java
+++ modules/dxp/apps/scim/scim-rest-impl/src/main/java/com/liferay/scim/rest/internal/resource/v1_0/UserResourceImpl.java
@@ -5,9 +5,13 @@
 
 package com.liferay.scim.rest.internal.resource.v1_0;
 
+import com.liferay.scim.rest.dto.v1_0.User;
 import com.liferay.scim.rest.resource.v1_0.UserResource;
 
+import javax.ws.rs.core.Response;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +22,30 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = UserResource.class
 )
 public class UserResourceImpl extends BaseUserResourceImpl {
+
+	@Override
+	public Object getV2User(Integer count, Integer startIndex)
+		throws Exception {
+
+		return _userResource.listUsers(count, startIndex);
+	}
+
+	@Override
+	public Object getV2UserById(String id) throws Exception {
+		return _userResource.getUser(id);
+	}
+
+	@Override
+	public Response postV2User(User user) throws Exception {
+		return _userResource.createUser(user.toString());
+	}
+
+	@Override
+	public Response putV2User(String id, User user) throws Exception {
+		return _userResource.updateUser(id, user.toString());
+	}
+
+	@Reference
+	private com.liferay.scim.resource.UserResource _userResource;
+
 }
