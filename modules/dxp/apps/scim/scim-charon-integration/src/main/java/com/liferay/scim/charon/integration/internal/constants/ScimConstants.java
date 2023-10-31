2ae4b0f9617824ce2cf83e1ad13ad597bca52f62
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2023-10-31 15:24:07 +0000

    LPS-97345 SF ScimConstants

--- modules/dxp/apps/scim/scim-charon-integration/src/main/java/com/liferay/scim/charon/integration/internal/constants/ScimConstants.java
+++ modules/dxp/apps/scim/scim-charon-integration/src/main/java/com/liferay/scim/charon/integration/internal/constants/ScimConstants.java
@@ -5,10 +5,12 @@
 
 package com.liferay.scim.charon.integration.internal.constants;
 
+import org.wso2.charon3.core.schema.SCIMConstants;
+
 /**
  * @author Rafael Praxedes
  */
-public class ScimConstants extends org.wso2.charon3.core.schema.SCIMConstants {
+public class ScimConstants extends SCIMConstants {
 
 	public static final String LIFERAY_USER_SCHEMA_EXTENSION_URI =
 		"urn:ietf:params:scim:schemas:extension:liferay:2.0:User";
