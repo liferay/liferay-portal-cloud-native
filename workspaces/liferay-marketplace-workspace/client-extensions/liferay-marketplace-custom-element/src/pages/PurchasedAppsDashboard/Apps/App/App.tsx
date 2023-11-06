c24d8c278ebcf32f5bb1f45d6394a6096cc0bdf5
Clarice Brasil <clarice.brasil@liferay.com>
2023-11-06 08:47:51 -0300

    ISSD-1554 Fix app details page

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PurchasedAppsDashboard/Apps/App/App.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PurchasedAppsDashboard/Apps/App/App.tsx
@@ -3,21 +3,31 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayAlert from '@clayui/alert';
-import ClayButton from '@clayui/button';
-import ClayNavigationBar from '@clayui/navigation-bar';
 import {useEffect} from 'react';
-import {useOutletContext} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+import useSWR from 'swr';
 
 import {useAppContext} from '../../../../manage-app-state/AppManageState';
 import {TYPES} from '../../../../manage-app-state/actionTypes';
+import HeadlessCommerceAdminCatalogImpl from '../../../../services/rest/HeadlessCommerceAdminCatalog';
 import {ReviewAndSubmitAppPage} from '../../../ReviewAndSubmitAppPage/ReviewAndSubmitAppPage';
 
 import './App.scss';
 
 const App = () => {
 	const [, dispatch] = useAppContext();
-	const {selectedApp} = useOutletContext<any>();
+	const {appId: productId} = useParams();
+
+	const {data = []} = useSWR(`/apps/app/${productId}`, () =>
+		Promise.all([
+			HeadlessCommerceAdminCatalogImpl.getProduct(productId as string),
+			HeadlessCommerceAdminCatalogImpl.getProductSpecifications(
+				productId as string
+			),
+		])
+	);
+
+	const [selectedApp] = data ?? [];
 
 	useEffect(() => {
 		if (!selectedApp) {
@@ -44,38 +54,9 @@ const App = () => {
 		return null;
 	}
 
-	const status = selectedApp.workflowStatusInfo.label.replace(
-		/(^\w|\s\w)/g,
-		(m: string) => m.toUpperCase()
-	);
-
 	return (
-		<div className="w-100">
-			{status === 'Draft' && (
-				<ClayAlert
-					className="app-details-page-alert-container"
-					displayType="info"
-				>
-					<span className="app-details-page-alert-text">
-						This submission is currently under review by Liferay.
-						Once the process is complete, you will be able to
-						publish it to the marketplace. Meanwhile, any
-						information or data from this app submission cannot be
-						updated.
-					</span>
-				</ClayAlert>
-			)}
-
+		<div className="app-details-page-container">
 			<div>
-				<ClayNavigationBar
-					className="app-details-page-navigation-bar"
-					triggerLabel="App Detatils"
-				>
-					<ClayNavigationBar.Item active>
-						<ClayButton>App Details</ClayButton>
-					</ClayNavigationBar.Item>
-				</ClayNavigationBar>
-
 				<ReviewAndSubmitAppPage
 					onClickBack={() => {}}
 					onClickContinue={() => {}}
