liferay-core-infra/liferay-portal

LPS-196123 Fix tests

modules/apps/document-library/document-library-preview-audio-test/src/testIntegration/java/com/liferay/document/library/preview/audio/test/DLAudioFFMPEGAudioConverterTest.java
@@ -24,11 +24,10 @@ import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.test.rule.ExpectedLog;
-import com.liferay.portal.test.rule.ExpectedLogs;
-import com.liferay.portal.test.rule.ExpectedType;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portlet.documentlibrary.util.AudioProcessorImpl;
 
 import java.util.Dictionary;
 
@@ -72,35 +71,42 @@ public class DLAudioFFMPEGAudioConverterTest {
 			});
 	}
 
-	@ExpectedLogs(
-		expectedLogs = {
-			@ExpectedLog(
-				expectedLog = "ffmpeg", expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "java.io.FileNotFoundException",
-				expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "Unable to process",
-				expectedType = ExpectedType.CONTAINS
-			)
-		},
-		level = "ERROR", loggerClass = AudioProcessorImpl.class
-	)
 	@Test
 	public void testDoesNotGenerateAudioPreviewIfTheAudioIsCorrupt()
 		throws Exception {
 
-		_withDLAudioFFMPEGAudioConverterConfiguration(
-			true,
-			() -> {
-				FileEntry fileEntry = _createAudioFileEntry(
-					"audio_corrupt.wav");
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.document.library.preview.audio.internal." +
+					"processor.AudioProcessorImpl",
+				LoggerTestUtil.ERROR)) {
 
-				Assert.assertFalse(
-					AudioProcessorUtil.hasAudio(fileEntry.getFileVersion()));
-			});
+			_withDLAudioFFMPEGAudioConverterConfiguration(
+				true,
+				() -> {
+					FileEntry fileEntry = _createAudioFileEntry(
+						"audio_corrupt.wav");
+
+					Assert.assertFalse(
+						AudioProcessorUtil.hasAudio(
+							fileEntry.getFileVersion()));
+				});
+
+			for (LogEntry logEntry : logCapture.getLogEntries()) {
+				String logEntryMessage = logEntry.getMessage();
+
+				Assert.assertTrue(
+					logEntryMessage.contains("Unable to process"));
+
+				Throwable throwable = logEntry.getThrowable();
+
+				String throwableMessage = throwable.getMessage();
+
+				Assert.assertTrue(
+					throwableMessage.contains(
+						"java.io.FileNotFoundException") ||
+					throwableMessage.contains("ffmpeg"));
+			}
+		}
 	}
 
 	@Test

modules/apps/document-library/document-library-video-test/src/testIntegration/java/com/liferay/document/library/video/converter/test/DLVideoFFMPEGVideoConverterTest.java
@@ -24,11 +24,10 @@ import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.test.rule.ExpectedLog;
-import com.liferay.portal.test.rule.ExpectedLogs;
-import com.liferay.portal.test.rule.ExpectedType;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portlet.documentlibrary.util.VideoProcessorImpl;
 
 import java.util.Dictionary;
 
@@ -72,35 +71,28 @@ public class DLVideoFFMPEGVideoConverterTest {
 			});
 	}
 
-	@ExpectedLogs(
-		expectedLogs = {
-			@ExpectedLog(
-				expectedLog = "ffmpeg", expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "java.io.FileNotFoundException",
-				expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "Unable to process",
-				expectedType = ExpectedType.CONTAINS
-			)
-		},
-		level = "ERROR", loggerClass = VideoProcessorImpl.class
-	)
 	@Test
 	public void testDoesNotGenerateVideoPreviewIfTheVideoIsCorrupt()
 		throws Exception {
 
-		_withDLVideoFFMPEGVideoConverterConfiguration(
-			true,
-			() -> {
-				FileEntry fileEntry = _createVideoFileEntry(
-					"video_corrupt.mp4");
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.document.library.preview.video.internal." +
+					"processor.VideoProcessorImpl",
+				LoggerTestUtil.ERROR)) {
 
-				Assert.assertFalse(
-					VideoProcessorUtil.hasVideo(fileEntry.getFileVersion()));
-			});
+			_withDLVideoFFMPEGVideoConverterConfiguration(
+				true,
+				() -> {
+					FileEntry fileEntry = _createVideoFileEntry(
+						"video_corrupt.mp4");
+
+					Assert.assertFalse(
+						VideoProcessorUtil.hasVideo(
+							fileEntry.getFileVersion()));
+				});
+
+			_assertLogMessage(logCapture);
+		}
 	}
 
 	@Test
@@ -147,45 +139,56 @@ public class DLVideoFFMPEGVideoConverterTest {
 			});
 	}
 
-	@ExpectedLogs(
-		expectedLogs = {
-			@ExpectedLog(
-				expectedLog = "ffmpeg", expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "java.io.FileNotFoundException",
-				expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "Unable to process",
-				expectedType = ExpectedType.CONTAINS
-			)
-		},
-		level = "ERROR", loggerClass = VideoProcessorImpl.class
-	)
 	@Test
 	public void testGeneratesVideoPreviewIfTheVideoHasOnlyAudio()
 		throws Exception {
 
-		_withDLVideoFFMPEGVideoConverterConfiguration(
-			true,
-			() -> {
-				FileEntry fileEntry = _createVideoFileEntry(
-					"video_only_audio.mp4");
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.document.library.preview.video.internal." +
+					"processor.VideoProcessorImpl",
+				LoggerTestUtil.ERROR)) {
 
-				Assert.assertTrue(
-					VideoProcessorUtil.hasVideo(fileEntry.getFileVersion()));
+			_withDLVideoFFMPEGVideoConverterConfiguration(
+				true,
+				() -> {
+					FileEntry fileEntry = _createVideoFileEntry(
+						"video_only_audio.mp4");
 
-				long mp4PreviewFileSize = VideoProcessorUtil.getPreviewFileSize(
-					fileEntry.getFileVersion(), "mp4");
+					Assert.assertTrue(
+						VideoProcessorUtil.hasVideo(
+							fileEntry.getFileVersion()));
 
-				Assert.assertTrue(mp4PreviewFileSize > 0);
+					long mp4PreviewFileSize =
+						VideoProcessorUtil.getPreviewFileSize(
+							fileEntry.getFileVersion(), "mp4");
 
-				long ogvPreviewFileSize = VideoProcessorUtil.getPreviewFileSize(
-					fileEntry.getFileVersion(), "ogv");
+					Assert.assertTrue(mp4PreviewFileSize > 0);
 
-				Assert.assertTrue(ogvPreviewFileSize > 0);
-			});
+					long ogvPreviewFileSize =
+						VideoProcessorUtil.getPreviewFileSize(
+							fileEntry.getFileVersion(), "ogv");
+
+					Assert.assertTrue(ogvPreviewFileSize > 0);
+				});
+
+			_assertLogMessage(logCapture);
+		}
+	}
+
+	private void _assertLogMessage(LogCapture logCapture) {
+		for (LogEntry logEntry : logCapture.getLogEntries()) {
+			String logEntryMessage = logEntry.getMessage();
+
+			Assert.assertTrue(logEntryMessage.contains("Unable to process"));
+
+			Throwable throwable = logEntry.getThrowable();
+
+			String throwableMessage = throwable.getMessage();
+
+			Assert.assertTrue(
+				throwableMessage.contains("java.io.FileNotFoundException") ||
+				throwableMessage.contains("ffmpeg"));
+		}
 	}
 
 	private FileEntry _createVideoFileEntry(String fileName) throws Exception {

liferay/liferay-portal-cloud-native

modules/apps/document-library/document-library-preview-audio-test/src/testIntegration/java/com/liferay/document/library/preview/audio/test/DLAudioFFMPEGAudioConverterTest.java
@@ -24,11 +24,10 @@ import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.test.rule.ExpectedLog;
-import com.liferay.portal.test.rule.ExpectedLogs;
-import com.liferay.portal.test.rule.ExpectedType;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portlet.documentlibrary.util.AudioProcessorImpl;
 
 import java.util.Dictionary;
 
@@ -72,35 +71,42 @@ public class DLAudioFFMPEGAudioConverterTest {
 			});
 	}
 
-	@ExpectedLogs(
-		expectedLogs = {
-			@ExpectedLog(
-				expectedLog = "ffmpeg", expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "java.io.FileNotFoundException",
-				expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "Unable to process",
-				expectedType = ExpectedType.CONTAINS
-			)
-		},
-		level = "ERROR", loggerClass = AudioProcessorImpl.class
-	)
 	@Test
 	public void testDoesNotGenerateAudioPreviewIfTheAudioIsCorrupt()
 		throws Exception {
 
-		_withDLAudioFFMPEGAudioConverterConfiguration(
-			true,
-			() -> {
-				FileEntry fileEntry = _createAudioFileEntry(
-					"audio_corrupt.wav");
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.document.library.preview.audio.internal." +
+					"processor.AudioDLProcessorImpl",
+				LoggerTestUtil.ERROR)) {
 
-				Assert.assertFalse(
-					AudioProcessorUtil.hasAudio(fileEntry.getFileVersion()));
-			});
+			_withDLAudioFFMPEGAudioConverterConfiguration(
+				true,
+				() -> {
+					FileEntry fileEntry = _createAudioFileEntry(
+						"audio_corrupt.wav");
+
+					Assert.assertFalse(
+						AudioProcessorUtil.hasAudio(
+							fileEntry.getFileVersion()));
+				});
+
+			for (LogEntry logEntry : logCapture.getLogEntries()) {
+				String logEntryMessage = logEntry.getMessage();
+
+				Assert.assertTrue(
+					logEntryMessage.contains("Unable to process"));
+
+				Throwable throwable = logEntry.getThrowable();
+
+				String throwableMessage = throwable.getMessage();
+
+				Assert.assertTrue(
+					throwableMessage.contains(
+						"java.io.FileNotFoundException") ||
+					throwableMessage.contains("ffmpeg"));
+			}
+		}
 	}
 
 	@Test

modules/apps/document-library/document-library-video-test/src/testIntegration/java/com/liferay/document/library/video/converter/test/DLVideoFFMPEGVideoConverterTest.java
@@ -24,11 +24,10 @@ import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.test.rule.ExpectedLog;
-import com.liferay.portal.test.rule.ExpectedLogs;
-import com.liferay.portal.test.rule.ExpectedType;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portlet.documentlibrary.util.VideoProcessorImpl;
 
 import java.util.Dictionary;
 
@@ -72,35 +71,28 @@ public class DLVideoFFMPEGVideoConverterTest {
 			});
 	}
 
-	@ExpectedLogs(
-		expectedLogs = {
-			@ExpectedLog(
-				expectedLog = "ffmpeg", expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "java.io.FileNotFoundException",
-				expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "Unable to process",
-				expectedType = ExpectedType.CONTAINS
-			)
-		},
-		level = "ERROR", loggerClass = VideoProcessorImpl.class
-	)
 	@Test
 	public void testDoesNotGenerateVideoPreviewIfTheVideoIsCorrupt()
 		throws Exception {
 
-		_withDLVideoFFMPEGVideoConverterConfiguration(
-			true,
-			() -> {
-				FileEntry fileEntry = _createVideoFileEntry(
-					"video_corrupt.mp4");
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.document.library.preview.video.internal." +
+					"processor.VideoDLProcessorImpl",
+				LoggerTestUtil.ERROR)) {
 
-				Assert.assertFalse(
-					VideoProcessorUtil.hasVideo(fileEntry.getFileVersion()));
-			});
+			_withDLVideoFFMPEGVideoConverterConfiguration(
+				true,
+				() -> {
+					FileEntry fileEntry = _createVideoFileEntry(
+						"video_corrupt.mp4");
+
+					Assert.assertFalse(
+						VideoProcessorUtil.hasVideo(
+							fileEntry.getFileVersion()));
+				});
+
+			_assertLogMessage(logCapture);
+		}
 	}
 
 	@Test
@@ -147,45 +139,56 @@ public class DLVideoFFMPEGVideoConverterTest {
 			});
 	}
 
-	@ExpectedLogs(
-		expectedLogs = {
-			@ExpectedLog(
-				expectedLog = "ffmpeg", expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "java.io.FileNotFoundException",
-				expectedType = ExpectedType.CONTAINS
-			),
-			@ExpectedLog(
-				expectedLog = "Unable to process",
-				expectedType = ExpectedType.CONTAINS
-			)
-		},
-		level = "ERROR", loggerClass = VideoProcessorImpl.class
-	)
 	@Test
 	public void testGeneratesVideoPreviewIfTheVideoHasOnlyAudio()
 		throws Exception {
 
-		_withDLVideoFFMPEGVideoConverterConfiguration(
-			true,
-			() -> {
-				FileEntry fileEntry = _createVideoFileEntry(
-					"video_only_audio.mp4");
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				"com.liferay.document.library.preview.video.internal." +
+					"processor.VideoDLProcessorImpl",
+				LoggerTestUtil.ERROR)) {
 
-				Assert.assertTrue(
-					VideoProcessorUtil.hasVideo(fileEntry.getFileVersion()));
+			_withDLVideoFFMPEGVideoConverterConfiguration(
+				true,
+				() -> {
+					FileEntry fileEntry = _createVideoFileEntry(
+						"video_only_audio.mp4");
 
-				long mp4PreviewFileSize = VideoProcessorUtil.getPreviewFileSize(
-					fileEntry.getFileVersion(), "mp4");
+					Assert.assertTrue(
+						VideoProcessorUtil.hasVideo(
+							fileEntry.getFileVersion()));
 
-				Assert.assertTrue(mp4PreviewFileSize > 0);
+					long mp4PreviewFileSize =
+						VideoProcessorUtil.getPreviewFileSize(
+							fileEntry.getFileVersion(), "mp4");
 
-				long ogvPreviewFileSize = VideoProcessorUtil.getPreviewFileSize(
-					fileEntry.getFileVersion(), "ogv");
+					Assert.assertTrue(mp4PreviewFileSize > 0);
 
-				Assert.assertTrue(ogvPreviewFileSize > 0);
-			});
+					long ogvPreviewFileSize =
+						VideoProcessorUtil.getPreviewFileSize(
+							fileEntry.getFileVersion(), "ogv");
+
+					Assert.assertTrue(ogvPreviewFileSize > 0);
+				});
+
+			_assertLogMessage(logCapture);
+		}
+	}
+
+	private void _assertLogMessage(LogCapture logCapture) {
+		for (LogEntry logEntry : logCapture.getLogEntries()) {
+			String logEntryMessage = logEntry.getMessage();
+
+			Assert.assertTrue(logEntryMessage.contains("Unable to process"));
+
+			Throwable throwable = logEntry.getThrowable();
+
+			String throwableMessage = throwable.getMessage();
+
+			Assert.assertTrue(
+				throwableMessage.contains("java.io.FileNotFoundException") ||
+				throwableMessage.contains("ffmpeg"));
+		}
 	}
 
 	private FileEntry _createVideoFileEntry(String fileName) throws Exception {