df5d6a3cf4acdacc71b8ffe37d4501e68d8b0c9d
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-11-14 13:40:12 -0300

    LPS-201002 Simplify - Use AccountConstants.ACCOUNT_ENTRY_TYPES_DEFAULT_ALLOWED_TYPES

--- modules/apps/object/object-storage-salesforce/src/main/java/com/liferay/object/storage/salesforce/internal/rest/manager/v1_0/SalesforceObjectEntryManagerImpl.java
+++ modules/apps/object/object-storage-salesforce/src/main/java/com/liferay/object/storage/salesforce/internal/rest/manager/v1_0/SalesforceObjectEntryManagerImpl.java
@@ -228,10 +228,7 @@ public class SalesforceObjectEntryManagerImpl
 			_accountEntryLocalService.getUserAccountEntries(
 				dtoConverterContext.getUserId(),
 				AccountConstants.PARENT_ACCOUNT_ENTRY_ID_DEFAULT, null,
-				new String[] {
-					AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS,
-					AccountConstants.ACCOUNT_ENTRY_TYPE_PERSON
-				},
+				AccountConstants.ACCOUNT_ENTRY_TYPES_DEFAULT_ALLOWED_TYPES,
 				WorkflowConstants.STATUS_APPROVED, QueryUtil.ALL_POS,
 				QueryUtil.ALL_POS);
 
