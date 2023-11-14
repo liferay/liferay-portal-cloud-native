856af84bc4fb475afc933c88819115ce582eb6b6
Rubén Pulido <ruben.pulido@liferay.com>
2023-11-14 02:28:15 +0100

    LPS-201091 If we are searching within the current folder, exclude the current folder from the results

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1658,6 +1658,11 @@ public class JournalDisplayContext {
 				_getAssetTagNamesFilter(), BooleanClauseOccur.MUST);
 		}
 
+		if (Objects.equals(_getSearchLocation(), "current-folder")) {
+			booleanFilter.add(
+				_getCurrentFolderFilter(), BooleanClauseOccur.MUST_NOT);
+		}
+
 		if (!isSearch()) {
 			booleanFilter.addTerm(
 				Field.FOLDER_ID, String.valueOf(getFolderId()),
@@ -1712,6 +1717,19 @@ public class JournalDisplayContext {
 		searchContainer.setResultsAndTotal(() -> mbMessages, hits.getLength());
 
 		return searchContainer;
+	}
+
+	private Filter _getCurrentFolderFilter() {
+		BooleanFilter booleanFilter = new BooleanFilter();
+
+		booleanFilter.addTerm(
+			Field.ENTRY_CLASS_NAME, JournalFolder.class.getName(),
+			BooleanClauseOccur.MUST);
+		booleanFilter.addTerm(
+			Field.ENTRY_CLASS_PK, String.valueOf(getFolderId()),
+			BooleanClauseOccur.MUST);
+
+		return booleanFilter;
 	}
 
 	private EntriesChecker _getEntriesChecker() {
