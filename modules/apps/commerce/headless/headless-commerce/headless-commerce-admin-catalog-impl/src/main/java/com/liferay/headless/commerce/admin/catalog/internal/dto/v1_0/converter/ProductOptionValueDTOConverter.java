andrea-ale-sbarra/liferay-portal

COMMERCE-12504 added missing fields

modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/dto/v1_0/converter/ProductOptionValueDTOConverter.java
@@ -6,6 +6,7 @@
 package com.liferay.headless.commerce.admin.catalog.internal.dto.v1_0.converter;
 
 import com.liferay.commerce.product.model.CPDefinitionOptionValueRel;
+import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.service.CPDefinitionOptionValueRelService;
 import com.liferay.headless.commerce.admin.catalog.dto.v1_0.ProductOptionValue;
 import com.liferay.headless.commerce.core.util.LanguageUtils;
@@ -36,15 +37,31 @@ public class ProductOptionValueDTOConverter
 			CPDefinitionOptionValueRel cpDefinitionOptionValueRel)
 		throws Exception {
 
+		CPInstance cpInstance = cpDefinitionOptionValueRel.fetchCPInstance();
+
 		return new ProductOptionValue() {
 			{
+				deltaPrice = cpDefinitionOptionValueRel.getPrice();
 				id =
 					cpDefinitionOptionValueRel.
 						getCPDefinitionOptionValueRelId();
 				key = cpDefinitionOptionValueRel.getKey();
 				name = LanguageUtils.getLanguageIdMap(
 					cpDefinitionOptionValueRel.getNameMap());
+				preselected = cpDefinitionOptionValueRel.getPreselected();
 				priority = cpDefinitionOptionValueRel.getPriority();
+				quantity = cpDefinitionOptionValueRel.getQuantity();
+				unitOfMeasureKey =
+					cpDefinitionOptionValueRel.getUnitOfMeasureKey();
+
+				setSkuId(
+					() -> {
+						if (cpInstance == null) {
+							return null;
+						}
+
+						return cpInstance.getCPInstanceId();
+					});
 			}
 		};
 	}

liferay/liferay-portal-cloud-native

modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/dto/v1_0/converter/ProductOptionValueDTOConverter.java
@@ -6,6 +6,7 @@
 package com.liferay.headless.commerce.admin.catalog.internal.dto.v1_0.converter;
 
 import com.liferay.commerce.product.model.CPDefinitionOptionValueRel;
+import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.headless.commerce.admin.catalog.dto.v1_0.ProductOptionValue;
 import com.liferay.headless.commerce.core.util.LanguageUtils;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
@@ -34,15 +35,31 @@ public class ProductOptionValueDTOConverter
 			CPDefinitionOptionValueRel cpDefinitionOptionValueRel)
 		throws Exception {
 
+		CPInstance cpInstance = cpDefinitionOptionValueRel.fetchCPInstance();
+
 		return new ProductOptionValue() {
 			{
+				deltaPrice = cpDefinitionOptionValueRel.getPrice();
 				id =
 					cpDefinitionOptionValueRel.
 						getCPDefinitionOptionValueRelId();
 				key = cpDefinitionOptionValueRel.getKey();
 				name = LanguageUtils.getLanguageIdMap(
 					cpDefinitionOptionValueRel.getNameMap());
+				preselected = cpDefinitionOptionValueRel.isPreselected();
 				priority = cpDefinitionOptionValueRel.getPriority();
+				quantity = cpDefinitionOptionValueRel.getQuantity();
+				unitOfMeasureKey =
+					cpDefinitionOptionValueRel.getUnitOfMeasureKey();
+
+				setSkuId(
+					() -> {
+						if (cpInstance == null) {
+							return null;
+						}
+
+						return cpInstance.getCPInstanceId();
+					});
 			}
 		};
 	}