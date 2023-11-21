3285a7ad7cc33c6ec008a4b237335b40b83cb16d
mikellorza <mikel.lorza@liferay.com>
2023-11-21 17:22:10 +0100

    LPS-202275 Delete unused method

--- portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetTagFinderImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/persistence/impl/AssetTagFinderImpl.java
@@ -22,13 +22,10 @@ import com.liferay.portal.kernel.dao.orm.Type;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portlet.asset.model.impl.AssetTagImpl;
-import com.liferay.social.kernel.model.SocialActivityCounterTable;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -149,84 +146,6 @@ public class AssetTagFinderImpl
 
 			return (List<AssetTag>)QueryUtil.list(
 				sqlQuery, getDialect(), start, end);
-		}
-		catch (Exception exception) {
-			throw new SystemException(exception);
-		}
-		finally {
-			closeSession(session);
-		}
-	}
-
-	@Override
-	public List<AssetTag> findByG_N_S_E(
-		long groupId, String name, int startPeriod, int endPeriod,
-		int periodLength) {
-
-		Session session = null;
-
-		try {
-			session = openSession();
-
-			SQLQuery sqlQuery = session.createSynchronizedSQLQuery(
-				DSLQueryFactoryUtil.select(
-					AssetTagTable.INSTANCE.tagId, AssetTagTable.INSTANCE.name,
-					DSLFunctionFactoryUtil.sum(
-						SocialActivityCounterTable.INSTANCE.currentValue)
-				).from(
-					AssetTagTable.INSTANCE
-				).innerJoinON(
-					AssetEntries_AssetTagsTable.INSTANCE,
-					AssetEntries_AssetTagsTable.INSTANCE.tagId.eq(
-						AssetTagTable.INSTANCE.tagId)
-				).innerJoinON(
-					SocialActivityCounterTable.INSTANCE,
-					SocialActivityCounterTable.INSTANCE.classNameId.eq(
-						AssetEntryTable.INSTANCE.classNameId
-					).and(
-						SocialActivityCounterTable.INSTANCE.classPK.eq(
-							AssetEntryTable.INSTANCE.classPK)
-					)
-				).where(
-					SocialActivityCounterTable.INSTANCE.groupId.eq(
-						groupId
-					).and(
-						SocialActivityCounterTable.INSTANCE.name.eq(name)
-					).and(
-						SocialActivityCounterTable.INSTANCE.startPeriod.gte(
-							startPeriod)
-					).and(
-						SocialActivityCounterTable.INSTANCE.startPeriod.lte(
-							endPeriod)
-					).and(
-						DSLFunctionFactoryUtil.add(
-							SocialActivityCounterTable.INSTANCE.startPeriod,
-							periodLength
-						).lte(
-							endPeriod
-						)
-					)
-				).groupBy(
-					AssetTagTable.INSTANCE.tagId, AssetTagTable.INSTANCE.name
-				));
-
-			List<AssetTag> assetTags = new ArrayList<>();
-
-			Iterator<Object[]> iterator = sqlQuery.iterate();
-
-			while (iterator.hasNext()) {
-				Object[] array = iterator.next();
-
-				AssetTag assetTag = new AssetTagImpl();
-
-				assetTag.setTagId(GetterUtil.getLong(array[0]));
-				assetTag.setName(GetterUtil.getString(array[1]));
-				assetTag.setAssetCount(GetterUtil.getInteger(array[2]));
-
-				assetTags.add(assetTag);
-			}
-
-			return assetTags;
 		}
 		catch (Exception exception) {
 			throw new SystemException(exception);
