96c7939b5147a17b2fb7b061f9fdf11b01164224
Janis Zhang <janis.zhang@liferay.com>
2023-11-23 15:23:44 +0800

    LPS-201364 Move VideoProcessorMessageListener from portal-impl to module document-library-preview-video

--- modules/apps/document-library/document-library-preview-video/src/main/java/com/liferay/document/library/preview/video/internal/messaging/VideoProcessorMessageListener.java
+++ modules/apps/document-library/document-library-preview-video/src/main/java/com/liferay/document/library/preview/video/internal/messaging/VideoProcessorMessageListener.java
@@ -1,12 +1,13 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portlet.documentlibrary.messaging;
+package com.liferay.document.library.preview.video.internal.messaging;
 
 import com.liferay.document.library.kernel.util.VideoProcessorUtil;
 import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portlet.documentlibrary.messaging.BaseProcessorMessageListener;
 
 /**
  * @author Juan González
