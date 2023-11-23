edd87cca596c2478d310da0a8ac0ab6ddf312e22
Janis Zhang <janis.zhang@liferay.com>
2023-11-23 15:50:08 +0800

    LPS-201364 Move PDFProcessorMessageListener from portal-impl to module document-library-preview-video

--- modules/apps/document-library/document-library-preview-pdf/src/main/java/com/liferay/document/library/preview/pdf/internal/messaging/PDFProcessorMessageListener.java
+++ modules/apps/document-library/document-library-preview-pdf/src/main/java/com/liferay/document/library/preview/pdf/internal/messaging/PDFProcessorMessageListener.java
@@ -1,15 +1,16 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portlet.documentlibrary.messaging;
+package com.liferay.document.library.preview.pdf.internal.messaging;
 
 import com.liferay.document.library.kernel.util.PDFProcessorUtil;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.repository.model.FileVersion;
 import com.liferay.portal.repository.liferayrepository.model.LiferayFileVersion;
+import com.liferay.portlet.documentlibrary.messaging.BaseProcessorMessageListener;
 
 /**
  * @author Alexander Chow
