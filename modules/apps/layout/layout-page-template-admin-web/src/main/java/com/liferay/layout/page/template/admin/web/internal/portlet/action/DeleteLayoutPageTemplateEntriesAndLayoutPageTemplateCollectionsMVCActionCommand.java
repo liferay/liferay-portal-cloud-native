0c9b24ddccea7e56ba5c8bcb5dda85e0a17a2c8b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-11-28 16:17:28 +0000

    LPS-200477 Show custom message and take into account the errors when we are deleting folders and display pages

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/DeleteLayoutPageTemplateEntriesAndLayoutPageTemplateCollectionsMVCActionCommand.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/portlet/action/DeleteLayoutPageTemplateEntriesAndLayoutPageTemplateCollectionsMVCActionCommand.java
@@ -8,9 +8,19 @@ package com.liferay.layout.page.template.admin.web.internal.portlet.action;
 import com.liferay.layout.page.template.admin.constants.LayoutPageTemplateAdminPortletKeys;
 import com.liferay.layout.page.template.service.LayoutPageTemplateCollectionService;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseTransactionalMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.MultiSessionMessages;
+import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -40,16 +50,82 @@ public class
 		long[] deleteLayoutPageTemplateCollectionIds = ParamUtil.getLongValues(
 			actionRequest, "rowIdsLayoutPageTemplateCollection");
 
-		_layoutPageTemplateCollectionService.
-			deleteLayoutPageTemplateCollections(
-				deleteLayoutPageTemplateCollectionIds);
+		List<Long> deleteLayoutPageTemplateCollectionIdsList =
+			new ArrayList<>();
+
+		for (long deleteLayoutPageTemplateCollectionId :
+				deleteLayoutPageTemplateCollectionIds) {
+
+			try {
+				_layoutPageTemplateCollectionService.
+					deleteLayoutPageTemplateCollection(
+						deleteLayoutPageTemplateCollectionId);
+			}
+			catch (PortalException portalException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(portalException);
+				}
+
+				deleteLayoutPageTemplateCollectionIdsList.add(
+					deleteLayoutPageTemplateCollectionId);
+			}
+		}
 
 		long[] deleteLayoutPageTemplateEntryIds = ParamUtil.getLongValues(
 			actionRequest, "rowIds");
 
-		_layoutPageTemplateEntryService.deleteLayoutPageTemplateEntries(
-			deleteLayoutPageTemplateEntryIds);
+		List<Long> deleteLayoutPageTemplateEntryIdsList = new ArrayList<>();
+
+		for (long deleteLayoutPageTemplateEntryId :
+				deleteLayoutPageTemplateEntryIds) {
+
+			try {
+				_layoutPageTemplateEntryService.deleteLayoutPageTemplateEntry(
+					deleteLayoutPageTemplateEntryId);
+			}
+			catch (PortalException portalException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(portalException);
+				}
+
+				deleteLayoutPageTemplateEntryIdsList.add(
+					deleteLayoutPageTemplateEntryId);
+			}
+		}
+
+		if ((deleteLayoutPageTemplateCollectionIds.length ==
+				deleteLayoutPageTemplateCollectionIdsList.size()) &&
+			(deleteLayoutPageTemplateEntryIds.length ==
+				deleteLayoutPageTemplateEntryIdsList.size())) {
+
+			SessionErrors.add(actionRequest, PortalException.class);
+
+			sendRedirect(actionRequest, actionResponse);
+
+			return;
+		}
+
+		hideDefaultSuccessMessage(actionRequest);
+
+		MultiSessionMessages.add(
+			actionRequest, "displayPageTemplateDeleted",
+			_language.format(
+				_portal.getHttpServletRequest(actionRequest),
+				"you-successfully-deleted-x-display-page-templates-and-x-" +
+					"folders",
+				new Object[] {
+					deleteLayoutPageTemplateEntryIds.length -
+						deleteLayoutPageTemplateEntryIdsList.size(),
+					deleteLayoutPageTemplateCollectionIds.length -
+						deleteLayoutPageTemplateCollectionIdsList.size()
+				}));
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DeleteLayoutPageTemplateEntriesAndLayoutPageTemplateCollectionsMVCActionCommand.class);
+
+	@Reference
+	private Language _language;
 
 	@Reference
 	private LayoutPageTemplateCollectionService
@@ -57,5 +133,8 @@ public class
 
 	@Reference
 	private LayoutPageTemplateEntryService _layoutPageTemplateEntryService;
+
+	@Reference
+	private Portal _portal;
 
 }
