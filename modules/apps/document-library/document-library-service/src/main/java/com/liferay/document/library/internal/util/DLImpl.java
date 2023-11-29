cd637eecb7d28ed30e899b2cb88f007556e9ba5c
Roberto Díaz <roberto.diaz@liferay.com>
2023-11-29 11:35:32 +0100

    LPS-202905 Copy class to module
    # breaking
    
    ## What
    
    portal-impl/src/com/liferay/portlet/documentlibrary/util/DLImpl.java
    
    ## Why
    
    This class has to been able to get and use the friendlyURLs when the file has an AssetDisplayPage configured to improve the performance and to do so it has to have access to some OSGi components, so portal-impl/src/com/liferay/portlet/documentlibrary/util/DLImpl.java was moved to modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLImpl.java

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLImpl.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portlet.documentlibrary.util;
+package com.liferay.document.library.internal.util;
 
 import com.liferay.document.library.kernel.exception.InvalidFolderException;
 import com.liferay.document.library.kernel.model.DLFileEntry;
