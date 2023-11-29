afb7a013d9317411943f5f2fdf0aa6c76fc9e249
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-11-29 16:49:54 +0000

    LRAC-15111 We need to update the logic to match with AC, in that way it is going to be easier track clicks

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/renderer/LayoutStructureRenderer.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/renderer/LayoutStructureRenderer.java
@@ -52,7 +52,12 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.model.LayoutTemplate;
@@ -220,9 +225,23 @@ public class LayoutStructureRenderer {
 					getListObjectReference();
 
 			if (listObjectReference != null) {
-				jspWriter.write(" data-analytics-targetable-collection=\"");
-				jspWriter.write(HtmlUtil.escape(listObjectReference.toJSON()));
-				jspWriter.write("\"");
+				try {
+					JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+						listObjectReference.toJSON());
+
+					if (jsonObject.has("key")) {
+						jspWriter.write(
+							" id=\"analytics-targetable-collection-");
+						jspWriter.write(jsonObject.getString("key"));
+						jspWriter.write("\"");
+					}
+				}
+				catch (JSONException jsonException) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(
+							"Unable to parse JSON object", jsonException);
+					}
+				}
 			}
 		}
 
@@ -1320,6 +1339,9 @@ public class LayoutStructureRenderer {
 				fragmentStyledLayoutStructureItem));
 		jspWriter.write("\">");
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutStructureRenderer.class);
 
 	private final HttpServletRequest _httpServletRequest;
 	private final LayoutStructure _layoutStructure;
