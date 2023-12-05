afea344e09137acb2d67d497c283e22ac035da32
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-12-05 08:02:54 +0100

    LPS-203260 Moves the implementation to suitable module

--- modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/manager/SitemapManagerImpl.java
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/manager/SitemapManagerImpl.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.layout.internal.manager;
+package com.liferay.site.internal.manager;
 
 import com.liferay.layout.admin.kernel.model.LayoutTypePortletConstants;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
