d7d043e8e16cebe1fcc0f9c736ac81eedf90ca45
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-12 18:26:44 -0300

    LPS-203432 Add default filter properties

--- modules/apps/portal-security/portal-security-auth-verifier/src/main/java/com/liferay/portal/security/auth/verifier/internal/tracker/AuthVerifierFilterTracker.java
+++ modules/apps/portal-security/portal-security-auth-verifier/src/main/java/com/liferay/portal/security/auth/verifier/internal/tracker/AuthVerifierFilterTracker.java
@@ -313,6 +313,22 @@ public class AuthVerifierFilterTracker {
 			remoteAccessFilterServiceRegistration.unregister();
 		}
 
+		private Dictionary<String, Object> _buildDefaultFilterProperties(
+			String filterName) {
+
+			HashMapDictionaryBuilder.HashMapDictionaryWrapper<String, Object>
+				properties =
+					new HashMapDictionaryBuilder.HashMapDictionaryWrapper<>();
+
+			properties.put("servlet-context-name", "");
+			properties.put("servlet-filter-name", filterName);
+			properties.put("url-pattern", "/o/headless-delivery/*");
+			properties.put(
+				"dispatcher", new String[] {"FORWARD", "INCLUDE", "REQUEST"});
+
+			return properties.build();
+		}
+
 		private Dictionary<String, Object> _buildPropertiesForAuditFilter(
 			ServiceReference<ServletContextHelper> serviceReference) {
 
@@ -368,6 +384,9 @@ public class AuthVerifierFilterTracker {
 					"service.ranking",
 					_defaultRemoteAccessFilterServiceRanking);
 			}
+
+			properties.putAll(
+				_buildDefaultFilterProperties("Remote Access Filter"));
 
 			return properties.build();
 		}
