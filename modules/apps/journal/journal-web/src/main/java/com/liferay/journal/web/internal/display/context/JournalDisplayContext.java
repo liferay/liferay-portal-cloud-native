da0c00a4fc0f63199b24813403182154e8bfef3d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-01-03 19:11:44 +0000

    LPS-200094 Show versions only if indexAllArticleVersionsEnabled is enabled

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -1368,7 +1368,8 @@ public class JournalDisplayContext {
 		}
 
 		if (FeatureFlagManagerUtil.isEnabled("LPS-196768") &&
-			isTypeWebContent() && !hasResults() && hasVersionsResults()) {
+			isIndexAllArticleVersions() && isTypeWebContent() &&
+			!hasResults() && hasVersionsResults()) {
 
 			_type = "versions";
 
