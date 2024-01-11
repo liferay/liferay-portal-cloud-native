19aace41faf7a5ac2a69f77c142eaa1d05c24507
Brian Chan <brian.chan@liferay.com>
2024-01-11 06:29:36 -0800

    LPS-205471 SF

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/v7_4_x/test/UpgradeListTypeTypeTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/v7_4_x/test/UpgradeListTypeTypeTest.java
@@ -6,7 +6,6 @@
 package com.liferay.portal.upgrade.v7_4_x.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.ListTypeLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
@@ -15,7 +14,6 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.upgrade.v7_4_x.UpgradeListTypeType;
 import com.liferay.portal.util.PortalInstances;
 
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -32,25 +30,16 @@ public class UpgradeListTypeTypeTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
-	@Before
-	public void setUp() throws PortalException {
-		long companyId = PortalInstances.getDefaultCompanyId();
-
-		_listTypeLocalService.addListType(
-			companyId, _LIST_TYPE_NAME, _LIST_TYPE_TYPE);
-	}
-
 	@Test
 	public void testUpgrade() throws Exception {
+		_listTypeLocalService.addListType(
+			PortalInstances.getDefaultCompanyId(), "intranet",
+			"com.liferay.account.model.AccountEntry.address");
+
 		UpgradeProcess upgradeProcess = new UpgradeListTypeType();
 
 		upgradeProcess.upgrade();
 	}
-
-	private static final String _LIST_TYPE_NAME = "intranet";
-
-	private static final String _LIST_TYPE_TYPE =
-		"com.liferay.account.model.AccountEntry.address";
 
 	@Inject
 	private ListTypeLocalService _listTypeLocalService;
