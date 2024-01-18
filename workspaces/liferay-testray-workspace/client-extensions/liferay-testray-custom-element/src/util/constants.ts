494cffcce8a9054f10c883b4d9ce5942f432f1e0
José Abelenda <jose.abelenda@liferay.com>
2024-01-18 14:25:21 -0300

    LPS-206458 Sort

--- workspaces/liferay-testray-workspace/client-extensions/liferay-testray-custom-element/src/util/constants.ts
+++ workspaces/liferay-testray-workspace/client-extensions/liferay-testray-custom-element/src/util/constants.ts
@@ -2,6 +2,15 @@
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+export enum CaseResultStatuses {
+	BLOCKED = 'caseResultBlocked',
+	FAILED = 'caseResultFailed',
+	INCOMPLETE = 'caseResultIncomplete',
+	INPROGRESS = 'caseResultInProgress',
+	PASSED = 'caseResultPassed',
+	TEST_FIX = 'caseResultTestFix',
+	UNTESTED = 'caseResultUntested',
+}
 
 export enum RoleTypes {
 	REGULAR = 1,
@@ -9,14 +18,6 @@ export enum RoleTypes {
 	ORGANIZATION = 3,
 	ASSET_LIBRARY = 5,
 }
-
-export const DATA_COLORS = {
-	'metrics.blocked': '#F8D72E',
-	'metrics.failed': '#E73A45',
-	'metrics.incomplete': '#E3E9EE',
-	'metrics.passed': '#3CD587',
-	'metrics.testfix': '#59BBFC',
-};
 
 export enum Statuses {
 	PASSED = 'PASSED',
@@ -28,29 +29,11 @@ export enum Statuses {
 	OTHER = 'OTHERS COMPLETED',
 }
 
-export enum CaseResultStatuses {
-	BLOCKED = 'caseResultBlocked',
-	FAILED = 'caseResultFailed',
-	INCOMPLETE = 'caseResultIncomplete',
-	INPROGRESS = 'caseResultInProgress',
-	PASSED = 'caseResultPassed',
-	TEST_FIX = 'caseResultTestFix',
-	UNTESTED = 'caseResultUntested',
-}
-
 export enum StatusesProgressScore {
 	SELF = 'SELF COMPLETED',
 	OTHER = 'OTHERS COMPLETED',
 	INCOMPLETE = 'INCOMPLETE',
 }
-
-export const chartColors = {
-	[Statuses.BLOCKED]: DATA_COLORS['metrics.blocked'],
-	[Statuses.FAILED]: DATA_COLORS['metrics.failed'],
-	[Statuses.INCOMPLETE]: DATA_COLORS['metrics.incomplete'],
-	[Statuses.PASSED]: DATA_COLORS['metrics.passed'],
-	[Statuses.TEST_FIX]: DATA_COLORS['metrics.testfix'],
-};
 
 export const chartClassNames = {
 	[Statuses.BLOCKED]: 'blocked',
@@ -60,6 +43,22 @@ export const chartClassNames = {
 	[Statuses.SELF]: 'self-completed',
 	[Statuses.TEST_FIX]: 'testfix',
 	[Statuses.OTHER]: 'others-completed',
+};
+
+export const DATA_COLORS = {
+	'metrics.blocked': '#F8D72E',
+	'metrics.failed': '#E73A45',
+	'metrics.incomplete': '#E3E9EE',
+	'metrics.passed': '#3CD587',
+	'metrics.testfix': '#59BBFC',
+};
+
+export const chartColors = {
+	[Statuses.BLOCKED]: DATA_COLORS['metrics.blocked'],
+	[Statuses.FAILED]: DATA_COLORS['metrics.failed'],
+	[Statuses.INCOMPLETE]: DATA_COLORS['metrics.incomplete'],
+	[Statuses.PASSED]: DATA_COLORS['metrics.passed'],
+	[Statuses.TEST_FIX]: DATA_COLORS['metrics.testfix'],
 };
 
 export const LABEL_GREATER_THAN_99 = '> 99';
