ac27e200d12c34be056de011b616bd0557f8a693
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-01-24 07:41:58 +0100

    LPD-4587 Boosts the test by asserting that the HTML title is constructed with the expected layout, group and company

--- modules/apps/layout/layout-seo-web/src/test/java/com/liferay/layout/seo/web/internal/util/TitleProviderTest.java
+++ modules/apps/layout/layout-seo-web/src/test/java/com/liferay/layout/seo/web/internal/util/TitleProviderTest.java
@@ -9,11 +9,15 @@ import com.liferay.layout.seo.kernel.LayoutSEOLink;
 import com.liferay.layout.seo.kernel.LayoutSEOLinkManager;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ListMergeable;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
@@ -65,6 +69,19 @@ public class TitleProviderTest {
 					ListMergeable<String> subtitleListMergeable,
 					String companyName, Locale locale) {
 
+					Group group = layout.getGroup();
+
+					try {
+						return StringUtil.merge(
+							new String[] {
+								layout.getHTMLTitle(locale),
+								group.getDescriptiveName(), companyName
+							},
+							_SEPARATOR);
+					}
+					catch (PortalException portalException) {
+					}
+
 					return "htmlTitle";
 				}
 
@@ -82,18 +99,67 @@ public class TitleProviderTest {
 
 	@Test
 	public void testGetTitle() throws PortalException {
+		String companyName = RandomTestUtil.randomString();
+		String groupDescriptiveName = RandomTestUtil.randomString();
+		String htmlTitle = RandomTestUtil.randomString();
+
 		MockHttpServletRequest mockHttpServletRequest =
 			new MockHttpServletRequest();
 
-		ThemeDisplay themeDisplay = new ThemeDisplay();
-
-		themeDisplay.setCompany(Mockito.mock(Company.class));
-
 		mockHttpServletRequest.setAttribute(
-			WebKeys.THEME_DISPLAY, themeDisplay);
+			WebKeys.THEME_DISPLAY,
+			_getThemeDisplay(companyName, groupDescriptiveName, htmlTitle));
 
 		Assert.assertEquals(
-			"htmlTitle", _titleProvider.getTitle(mockHttpServletRequest));
+			StringUtil.merge(
+				new String[] {htmlTitle, groupDescriptiveName, companyName},
+				_SEPARATOR),
+			_titleProvider.getTitle(mockHttpServletRequest));
+	}
+
+	private ThemeDisplay _getThemeDisplay(
+			String companyName, String groupDescriptiveName, String htmlTitle)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		Company company = Mockito.mock(Company.class);
+
+		Mockito.when(
+			company.getName()
+		).thenReturn(
+			companyName
+		);
+
+		themeDisplay.setCompany(company);
+
+		Layout layout = Mockito.mock(Layout.class);
+
+		Group group = Mockito.mock(Group.class);
+
+		Mockito.when(
+			group.getDescriptiveName()
+		).thenReturn(
+			groupDescriptiveName
+		);
+
+		Mockito.when(
+			layout.getGroup()
+		).thenReturn(
+			group
+		);
+
+		Mockito.when(
+			layout.getHTMLTitle(Mockito.any(Locale.class))
+		).thenReturn(
+			htmlTitle
+		);
+
+		themeDisplay.setLayout(layout);
+
+		themeDisplay.setLocale(LocaleUtil.US);
+
+		return themeDisplay;
 	}
 
 	private void _setUpPortalUtil() {
@@ -109,6 +175,8 @@ public class TitleProviderTest {
 
 		portalUtil.setPortal(portal);
 	}
+
+	private static final String _SEPARATOR = " - ";
 
 	private TitleProvider _titleProvider;
 
