fd1f30222227c483068ba1f5b5d7c7ffcd2f32b7
mikellorza <mikel.lorza@liferay.com>
2024-01-24 13:59:09 +0100

    LPS-205243 buildService

--- portal-kernel/src/com/liferay/portal/kernel/service/LayoutFriendlyURLLocalService.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/LayoutFriendlyURLLocalService.java
@@ -365,6 +365,9 @@ public interface LayoutFriendlyURLLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getLayoutFriendlyURLsCount();
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getLayoutFriendlyURLsCount(long companyId, String friendlyURL);
+
 	/**
 	 * Returns the OSGi service identifier.
 	 *
