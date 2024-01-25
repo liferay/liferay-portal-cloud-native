fa454ed8e95caa49c8961242e53557f27b457eb8
Petteri Karttunen <petteri.karttunen@liferay.com>
2024-01-25 17:05:50 +0200

    LPD-15223 portal-search-opensearch2-impl: The mappings are in the IndexMappingRecord

--- modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/main/java/com/liferay/portal/search/opensearch2/internal/index/OpenSearchIndexInformation.java
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/main/java/com/liferay/portal/search/opensearch2/internal/index/OpenSearchIndexInformation.java
@@ -21,8 +21,10 @@ import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch.indices.GetIndexRequest;
 import org.opensearch.client.opensearch.indices.GetIndexResponse;
 import org.opensearch.client.opensearch.indices.GetMappingRequest;
+import org.opensearch.client.opensearch.indices.GetMappingResponse;
 import org.opensearch.client.opensearch.indices.IndexState;
 import org.opensearch.client.opensearch.indices.OpenSearchIndicesClient;
+import org.opensearch.client.opensearch.indices.get_mapping.IndexMappingRecord;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -45,12 +47,20 @@ public class OpenSearchIndexInformation implements IndexInformation {
 			_getOpenSearchIndicesClient();
 
 		try {
+			GetMappingResponse getMappingResponse =
+				openSearchIndicesClient.getMapping(
+					GetMappingRequest.of(
+						getMappingRequest -> getMappingRequest.index(
+							indexName)));
+
+			Map<String, IndexMappingRecord> indexMappingRecords =
+				getMappingResponse.result();
+
+			IndexMappingRecord indexMappingRecord = indexMappingRecords.get(
+				indexName);
+
 			JSONObject jsonObject = _jsonFactory.createJSONObject(
-				JsonpUtil.toString(
-					openSearchIndicesClient.getMapping(
-						GetMappingRequest.of(
-							getMappingRequest -> getMappingRequest.index(
-								indexName)))));
+				JsonpUtil.toString(indexMappingRecord.mappings()));
 
 			return jsonObject.toString(3);
 		}
