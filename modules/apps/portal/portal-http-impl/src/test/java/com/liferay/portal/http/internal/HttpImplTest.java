20470ba79b9b54d625003a68ea1364d593cbabdd
Rafael Praxedes <rafael.praxedes@liferay.com>
2024-01-31 14:52:28 -0300

    LPD-16061 Fix com.liferay.portal.http.internal.HttpImplTest

--- modules/apps/portal/portal-http-impl/src/test/java/com/liferay/portal/http/internal/HttpImplTest.java
+++ modules/apps/portal/portal-http-impl/src/test/java/com/liferay/portal/http/internal/HttpImplTest.java
@@ -11,8 +11,6 @@ import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 import com.liferay.portal.util.PortalImpl;
 
-import java.lang.reflect.Field;
-
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -50,24 +48,18 @@ public class HttpImplTest {
 		String ipAddress = "192.168.0.250";
 		String ipAddressWithStarWildcard = "182.*.0.250";
 
-		Field field = ReflectionTestUtil.getField(
-			HttpImpl.class, "_NON_PROXY_HOSTS");
-
-		Object value = field.get(null);
-
-		try {
-			field.set(
-				null,
-				new String[] {domain, ipAddress, ipAddressWithStarWildcard});
+		try (AutoCloseable autoCloseable =
+				ReflectionTestUtil.setFieldValueWithAutoCloseable(
+					HttpImpl.class, "_NON_PROXY_HOSTS",
+					new String[] {
+						domain, ipAddress, ipAddressWithStarWildcard
+					})) {
 
 			Assert.assertTrue(_httpImpl.isNonProxyHost(domain));
 			Assert.assertTrue(_httpImpl.isNonProxyHost(ipAddress));
 			Assert.assertTrue(_httpImpl.isNonProxyHost("182.123.0.250"));
 			Assert.assertFalse(_httpImpl.isNonProxyHost("182.100.1.250"));
 			Assert.assertFalse(_httpImpl.isNonProxyHost("google.com"));
-		}
-		finally {
-			field.set(null, value);
 		}
 	}
 
