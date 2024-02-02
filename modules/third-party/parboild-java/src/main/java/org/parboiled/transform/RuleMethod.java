a21888d70880d0290c2ada5bc92563f52a75a645
shuyangzhou <shuyang.zhou@liferay.com>
2024-02-02 08:15:56 -0800

    LPD-16384 Patch ParserClassNode to make it forward compatible to higher version ASM

--- modules/third-party/parboild-java/src/main/java/org/parboiled/transform/RuleMethod.java
+++ modules/third-party/parboild-java/src/main/java/org/parboiled/transform/RuleMethod.java
@@ -23,6 +23,7 @@
 package org.parboiled.transform;
 
 import static org.parboiled.common.Preconditions.*;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.Type;
@@ -71,7 +72,7 @@ class RuleMethod extends MethodNode {
 
     public RuleMethod(Class<?> ownerClass, int access, String name, String desc, String signature, String[] exceptions,
                       boolean hasExplicitActionOnlyAnno, boolean hasDontLabelAnno, boolean hasSkipActionsInPredicates) {
-        super(access, name, desc, signature, exceptions);
+        super(Opcodes.ASM4, access, name, desc, signature, exceptions);
         this.ownerClass = ownerClass;
         parameterCount = Type.getArgumentTypes(desc).length;
         hasCachedAnnotation = parameterCount == 0;
@@ -345,3 +346,4 @@ class RuleMethod extends MethodNode {
     }
     
 }
+/* @generated */
