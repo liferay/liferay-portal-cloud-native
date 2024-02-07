8fc5551bfd2cd1b0ad5231a7456013521d944a02
Pedro Leite <pedro.leite@liferay.com>
2024-02-07 15:36:16 -0300

    LPD-17297 Add resource to get roles

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/timers/select-reassignment/RoleType.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/timers/select-reassignment/RoleType.js
@@ -3,12 +3,33 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import React from 'react';
+import {useResource} from '@clayui/data-provider';
+import React, {useState} from 'react';
 
+import {contextUrl} from '../../../../../../constants';
+import {headers, userBaseURL} from '../../../../../../util/fetchUtil';
 import SidebarPanel from '../../../SidebarPanel';
 import BaseRoleType from '../../shared-components/BaseRoleType';
 
 const RoleType = (props) => {
+	const [networkStatus, setNetworkStatus] = useState(4);
+
+	const {resource} = useResource({
+		fetchOptions: {
+			headers: {
+				...headers,
+				'accept': `application/json`,
+				'x-csrf-token': Liferay.authToken,
+			},
+		},
+		fetchPolicy: 'cache-first',
+		link: `${window.location.origin}${contextUrl}${userBaseURL}/roles`,
+		onNetworkStatusChange: setNetworkStatus,
+		variables: {
+			pageSize: -1,
+		},
+	});
+
 	const updateRoleType = (values) => {
 		props.updateSelectedItem({
 			reassignments: {
@@ -26,7 +47,9 @@ const RoleType = (props) => {
 			<BaseRoleType
 				buttonName={Liferay.Language.get('new-section')}
 				inputLabel={Liferay.Language.get('role-type')}
+				networkStatus={networkStatus}
 				{...props}
+				resource={resource}
 				updateSelectedItem={updateRoleType}
 			/>
 		</SidebarPanel>
