656b35cb1f3f21856decec40bdd3b251ed37278b
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-02-12 10:40:07 +0100

    LPD-17442 SF move the test the suitable class

--- modules/apps/site-navigation/site-navigation-menu-item-display-page-test/src/testIntegration/java/com/liferay/site/navigation/menu/item/display/page/test/DisplayPageTypeSiteNavigationMenuItemTypeTest.java
+++ modules/apps/site-navigation/site-navigation-menu-item-display-page-test/src/testIntegration/java/com/liferay/site/navigation/menu/item/display/page/test/DisplayPageTypeSiteNavigationMenuItemTypeTest.java
@@ -11,16 +11,11 @@ import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
-import com.liferay.info.field.InfoField;
-import com.liferay.info.field.InfoFieldValue;
 import com.liferay.info.item.ClassPKInfoItemIdentifier;
 import com.liferay.info.item.InfoItemClassDetails;
-import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.info.item.InfoItemReference;
 import com.liferay.info.item.InfoItemServiceRegistry;
 import com.liferay.journal.model.JournalArticle;
-import com.liferay.layout.display.page.LayoutDisplayPageInfoItemFieldValuesProvider;
-import com.liferay.layout.display.page.LayoutDisplayPageInfoItemFieldValuesProviderRegistry;
 import com.liferay.layout.display.page.LayoutDisplayPageMultiSelectionProvider;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.info.item.capability.DisplayPageInfoItemCapability;
@@ -59,7 +54,6 @@ import com.liferay.site.navigation.service.SiteNavigationMenuLocalService;
 import com.liferay.site.navigation.type.SiteNavigationMenuItemType;
 import com.liferay.site.navigation.type.SiteNavigationMenuItemTypeRegistry;
 
-import java.util.Collection;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -158,51 +152,6 @@ public class DisplayPageTypeSiteNavigationMenuItemTypeTest {
 				AssetCategory.class.getName());
 
 		Assert.assertTrue(siteNavigationMenuItemType.isMultiSelection());
-	}
-
-	@Test
-	public void testLayoutDisplayPageInfoItemFieldValuesProvider()
-		throws Exception {
-
-		LayoutDisplayPageInfoItemFieldValuesProvider
-			assetCategoryLayoutDisplayPageInfoItemFieldValuesProvider =
-				_layoutDisplayPageInfoItemFieldValuesProviderRegistry.
-					getLayoutDisplayPageInfoItemFieldValuesProvider(
-						AssetCategory.class.getName());
-
-		Assert.assertNotNull(
-			assetCategoryLayoutDisplayPageInfoItemFieldValuesProvider);
-
-		InfoItemFieldValues infoItemFieldValues =
-			assetCategoryLayoutDisplayPageInfoItemFieldValuesProvider.
-				getInfoItemFieldValues(_assetCategory);
-
-		Collection<InfoFieldValue<Object>> infoFieldValues =
-			infoItemFieldValues.getInfoFieldValues();
-
-		Assert.assertEquals(
-			infoFieldValues.toString(), 2, infoFieldValues.size());
-
-		Locale locale = _portal.getSiteDefaultLocale(_group.getGroupId());
-
-		for (InfoFieldValue<Object> infoFieldValue : infoFieldValues) {
-			InfoField infoField = infoFieldValue.getInfoField();
-
-			Assert.assertTrue(
-				Objects.equals(infoField.getName(), "group") ||
-				Objects.equals(infoField.getName(), "vocabulary"));
-
-			if (Objects.equals(infoField.getName(), "group")) {
-				Assert.assertEquals(
-					_group.getDescriptiveName(locale),
-					infoFieldValue.getValue(locale));
-			}
-			else {
-				Assert.assertEquals(
-					_assetVocabulary.getTitle(locale),
-					infoFieldValue.getValue(locale));
-			}
-		}
 	}
 
 	@Test
@@ -540,10 +489,6 @@ public class DisplayPageTypeSiteNavigationMenuItemTypeTest {
 
 	@Inject
 	private InfoItemServiceRegistry _infoItemServiceRegistry;
-
-	@Inject
-	private LayoutDisplayPageInfoItemFieldValuesProviderRegistry
-		_layoutDisplayPageInfoItemFieldValuesProviderRegistry;
 
 	@Inject
 	private LayoutPageTemplateEntryLocalService
