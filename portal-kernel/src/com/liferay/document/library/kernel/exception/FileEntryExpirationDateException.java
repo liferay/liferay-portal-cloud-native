0a7c0477e0faaf65a90dae6d501970e8cb1716bf
Alicia García <alicia.garcia@liferay.com>
2024-02-13 15:19:43 +0100

    LPD-17743 move exception to let them be accessible for dlFileService classes

--- portal-kernel/src/com/liferay/document/library/kernel/exception/FileEntryExpirationDateException.java
+++ portal-kernel/src/com/liferay/document/library/kernel/exception/FileEntryExpirationDateException.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.document.library.web.internal.exception;
+package com.liferay.document.library.kernel.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
 
