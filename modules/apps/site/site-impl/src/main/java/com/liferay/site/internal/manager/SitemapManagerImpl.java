63ef50e1b4d7c85035283bc2e5930d3650f21db4
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-02-15 11:08:30 +0100

    LPD-17836 Prevents from adding to the company sitemap those configured sites that have set a Virtual Host

--- modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/manager/SitemapManagerImpl.java
+++ modules/apps/site/site-impl/src/main/java/com/liferay/site/internal/manager/SitemapManagerImpl.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.util.DateUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.TextFormatter;
@@ -249,8 +250,14 @@ public class SitemapManagerImpl implements SitemapManager {
 						return null;
 					}
 
-					return _layoutSetLocalService.getLayoutSet(
+					LayoutSet layoutSet = _layoutSetLocalService.getLayoutSet(
 						curGroup.getGroupId(), privateLayout);
+
+					if (MapUtil.isNotEmpty(layoutSet.getVirtualHostnames())) {
+						return null;
+					}
+
+					return layoutSet;
 				});
 		}
 		catch (Exception exception) {
