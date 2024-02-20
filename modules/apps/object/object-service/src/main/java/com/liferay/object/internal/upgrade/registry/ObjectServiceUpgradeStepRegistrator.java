p-albuquerque/liferay-portal

LPD-16492 [Upgrade Process] Set indexed = false for each existing ObjectField with Aggregation and Formula businessType

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -420,6 +420,11 @@ public class ObjectServiceUpgradeStepRegistrator
 			"9.0.1", "9.0.2",
 			new com.liferay.object.internal.upgrade.v9_0_2.
 				ObjectFolderUpgradeProcess());
+
+		registry.register(
+			"9.0.2", "9.0.3",
+			new com.liferay.object.internal.upgrade.v9_0_3.
+				ObjectFieldUpgradeProcess());
 	}
 
 	@Reference

liferay/liferay-portal-cloud-native

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -424,6 +424,11 @@ public class ObjectServiceUpgradeStepRegistrator
 			"9.0.2", "9.1.0",
 			UpgradeProcessFactory.addColumns(
 				"ObjectEntry", "rootObjectEntryId LONG"));
+
+		registry.register(
+			"9.1.0", "9.1.1",
+			new com.liferay.object.internal.upgrade.v9_1_1.
+				ObjectFieldUpgradeProcess());
 	}
 
 	@Reference