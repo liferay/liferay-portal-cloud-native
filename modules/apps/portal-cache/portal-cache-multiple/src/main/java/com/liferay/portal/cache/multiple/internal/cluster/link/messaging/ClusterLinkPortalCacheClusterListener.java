33bc2b1f74738caa60debdaad25b2a93b0b11e8a
Tito Kenzo <titokenzo@gmail.com>
2024-02-20 17:13:23 -0300

    LPD-8462 move destination registration logic to listener activation. Property configurationPid has no used and was removed

--- modules/apps/portal-cache/portal-cache-multiple/src/main/java/com/liferay/portal/cache/multiple/internal/cluster/link/messaging/ClusterLinkPortalCacheClusterListener.java
+++ modules/apps/portal-cache/portal-cache-multiple/src/main/java/com/liferay/portal/cache/multiple/internal/cluster/link/messaging/ClusterLinkPortalCacheClusterListener.java
@@ -17,17 +17,24 @@ import com.liferay.portal.kernel.cache.PortalCacheManager;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.util.SerializableUtil;
 
 import java.io.Serializable;
+import java.util.Dictionary;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Shuyang Zhou
@@ -41,6 +48,22 @@ public class ClusterLinkPortalCacheClusterListener extends BaseMessageListener {
 
 	@Activate
 	protected void activate(BundleContext bundleContext) {
+		DestinationConfiguration destinationConfiguration =
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_PARALLEL,
+				PortalCacheDestinationNames.CACHE_REPLICATION);
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
+
+		Dictionary<String, Object> dictionary =
+			HashMapDictionaryBuilder.<String, Object>put(
+				"destination.name", destination.getName()
+			).build();
+
+		_serviceRegistration = bundleContext.registerService(
+			Destination.class, destination, dictionary);
+
 		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
 			bundleContext,
 			(Class<PortalCacheManager<? extends Serializable, ?>>)
@@ -58,6 +81,8 @@ public class ClusterLinkPortalCacheClusterListener extends BaseMessageListener {
 	@Deactivate
 	protected void deactivate() {
 		_serviceTrackerMap.close();
+
+		_serviceRegistration.unregister();
 	}
 
 	@Override
@@ -148,8 +173,13 @@ public class ClusterLinkPortalCacheClusterListener extends BaseMessageListener {
 		}
 	}
 
+	@Reference
+	private DestinationFactory _destinationFactory;
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		ClusterLinkPortalCacheClusterListener.class);
+
+	private ServiceRegistration<Destination> _serviceRegistration;
 
 	private ServiceTrackerMap
 		<String, PortalCacheManager<? extends Serializable, ?>>
