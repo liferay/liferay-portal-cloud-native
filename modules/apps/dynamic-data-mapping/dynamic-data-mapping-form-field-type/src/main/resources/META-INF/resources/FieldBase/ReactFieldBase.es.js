74f06e1e14dc39683ea7d93cc6ad09da5def3955
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2024-02-23 19:33:23 +0100

    LPD-15605 Fix language key

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/FieldBase/ReactFieldBase.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/FieldBase/ReactFieldBase.es.js
@@ -507,7 +507,7 @@ export function FieldBase({
 							{showDisabledFieldIcon && (
 								<TooltipProperty
 									tooltip={Liferay.Language.get(
-										'this-field-can-not-be-localized'
+										'this-field-cannot-be-localized'
 									)}
 								/>
 							)}
