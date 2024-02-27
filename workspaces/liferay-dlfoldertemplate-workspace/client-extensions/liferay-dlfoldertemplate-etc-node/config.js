a9d4ad7e1d4121f21e36508397cf2b2df5ae60a1
Mahmoud Hussein Tayem <tayemmahmoud87@gmail.com>
2024-02-27 20:23:45 +0400

    LPD-5166 Removing manual configuration keys from config.js and LCP.json

--- workspaces/liferay-dlfoldertemplate-workspace/client-extensions/liferay-dlfoldertemplate-etc-node/config.js
+++ workspaces/liferay-dlfoldertemplate-workspace/client-extensions/liferay-dlfoldertemplate-etc-node/config.js
@@ -4,24 +4,11 @@
  */
 
 export default {
-	'com.liferay.lxc.dxp.domains': '127.0.0.1:8080',
-	'com.liferay.lxc.dxp.main.domain': '127.0.0.1:8080',
-	'com.liferay.lxc.dxp.server.protocol': 'http',
 	'configTreePaths': [
 		'process.env.LIFERAY_ROUTES_CLIENT_EXTENSION',
 		'process.env.LIFERAY_ROUTES_DXP',
 	],
 	'folder.template.nodes.end.point': '/o/c/t4t14foldertemplatenodes/',
-	'liferay.dlfoldertemplate.etc.node.oauth.application.server.oauth2.headless.server.client.id':
-		'id-6455f1f2-f15c-07b1-cbc8-3e267524546',
-	'liferay.dlfoldertemplate.etc.node.oauth.application.server.oauth2.headless.server.client.secret':
-		'secret-e585633a-07c8-13dd-b654-86adead8e6',
-	'liferay.dlfoldertemplate.etc.node.oauth.application.server.oauth2.token.uri':
-		'/o/oauth2/token',
-	'liferay.dlfoldertemplate.etc.node.oauth.application.user.agent.oauth2.jwks.uri':
-		'/o/oauth2/jwks',
-	'liferay.dlfoldertemplate.etc.node.oauth.application.user.agent.oauth2.user.agent.client.id':
-		'id-f39269e7-1412-ea1c-30b2-8da323478c',
 	'liferay.oauth.application.external.reference.codes':
 		'liferay-dlfoldertemplate-etc-node-oauth-application-server,liferay-dlfoldertemplate-etc-node-oauth-application-user-agent',
 	'ready.path': '/ready',
