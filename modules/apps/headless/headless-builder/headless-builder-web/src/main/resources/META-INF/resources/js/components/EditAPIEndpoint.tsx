d670c40779e1f4b3a57bc317c025e0aadd9ad0fd
sergiojimcos <sergio.jdelcoso@liferay.com>
2024-02-29 15:48:31 +0100

    LPD-1988 Set response api property when patching an existent API Endpoint

--- modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/EditAPIEndpoint.tsx
+++ modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/EditAPIEndpoint.tsx
@@ -246,10 +246,8 @@ export default function EditAPIEndpoint({
 							r_requestAPISchemaToAPIEndpoints_c_apiSchemaId:
 								localUIData.r_requestAPISchemaToAPIEndpoints_c_apiSchemaId,
 						}),
-						...(localUIData.r_responseAPISchemaToAPIEndpoints_c_apiSchemaId && {
-							r_responseAPISchemaToAPIEndpoints_c_apiSchemaId:
-								localUIData.r_responseAPISchemaToAPIEndpoints_c_apiSchemaId,
-						}),
+						r_responseAPISchemaToAPIEndpoints_c_apiSchemaId:
+							localUIData.r_responseAPISchemaToAPIEndpoints_c_apiSchemaId,
 						retrieveType: localUIData.retrieveType,
 						scope: localUIData.scope,
 					},
