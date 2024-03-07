601431067bbf553e9410a9519291edff50d30fb5
Ivica Čardić <ivica.cardic@gmail.com>
2024-03-07 19:53:59 +0100

    LPD-19884 - Copy jar and war files to osgi/modules directory

--- modules/dxp/apps/osb/osb-faro/osb-faro-docker/cloud/100_liferay_image_setup.sh
+++ modules/dxp/apps/osb/osb-faro/osb-faro-docker/cloud/100_liferay_image_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp -R /home/liferay/configs/* ${LIFERAY_HOME}
+cp -R /home/liferay/modules/* ${LIFERAY_HOME}/osgi/modules
 cp -R /home/liferay/resources/context.xml $LIFERAY_HOME/tomcat/conf
 cp -R /home/liferay/resources/log4j ${LIFERAY_HOME}/osgi/log4j
 cp -R /home/liferay/resources/portal-log4j-ext.xml ${LIFERAY_HOME}/tomcat/webapps/ROOT/WEB-INF/classes/META-INF
