071d8f2d19b3a792b68eadc75954d0f0df93894a
Nilton Vieira <nilton.vieira@liferay.com>
2024-03-13 15:44:29 -0300

    ISSD-3125 regen buildREST

---
+++ workspaces/liferay-testray-workspace/modules/testray-rest-impl/src/main/java/com/liferay/testray/rest/internal/resource/v1_0/TestrayRunComparisonResourceImpl.java
@@ -0,0 +1,17 @@
+package com.liferay.testray.rest.internal.resource.v1_0;
+
+import com.liferay.testray.rest.resource.v1_0.TestrayRunComparisonResource;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Nilton Vieira
+ */
+@Component(
+	properties = "OSGI-INF/liferay/rest/v1_0/testray-run-comparison.properties",
+	scope = ServiceScope.PROTOTYPE, service = TestrayRunComparisonResource.class
+)
+public class TestrayRunComparisonResourceImpl
+	extends BaseTestrayRunComparisonResourceImpl {
+}
