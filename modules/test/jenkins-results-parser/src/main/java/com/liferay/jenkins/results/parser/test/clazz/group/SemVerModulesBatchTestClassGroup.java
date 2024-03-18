34b15da16346802fddaf5d1498fd9f24f1438044
Charlotte Wong <chiharutaru@gmail.com>
2024-03-18 12:51:17 -0700

    LRCI-4090 add missing import statements

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/SemVerModulesBatchTestClassGroup.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/SemVerModulesBatchTestClassGroup.java
@@ -19,6 +19,9 @@ import java.nio.file.PathMatcher;
 import java.util.Collections;
 import java.util.List;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.json.JSONObject;
 
 /**
