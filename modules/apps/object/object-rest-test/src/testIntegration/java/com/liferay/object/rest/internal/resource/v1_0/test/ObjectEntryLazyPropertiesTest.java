9450745f129fcf82002a4e5a49d643a9da22a89f
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-04-05 10:56:34 +0200

    LPD-17027 Simplifying test

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryLazyPropertiesTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryLazyPropertiesTest.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -8,7 +8,6 @@ package com.liferay.object.rest.internal.resource.v1_0.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.object.rest.dto.v1_0.ObjectEntry;
 import com.liferay.petra.function.UnsafeSupplier;
-import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.HTTPTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
@@ -16,9 +15,7 @@ import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 
 import javax.ws.rs.GET;
@@ -39,9 +36,6 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceRegistration;
 
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
-
 /**
  * @author Alberto Javier Moreno Lage
  */
@@ -60,54 +54,43 @@ public class ObjectEntryLazyPropertiesTest {
 
 		BundleContext bundleContext = bundle.getBundleContext();
 
-		_serviceRegistrations.add(
-			bundleContext.registerService(
-				Application.class, new TestApplication(),
-				HashMapDictionaryBuilder.<String, Object>put(
-					"liferay.auth.verifier", false
-				).put(
-					"liferay.oauth2", false
-				).put(
-					"osgi.jaxrs.application.base", "/object-rest-test/"
-				).put(
-					"osgi.jaxrs.extension.select",
-					"(osgi.jaxrs.name=Liferay.Vulcan)"
-				).build()));
+		_serviceRegistration = bundleContext.registerService(
+			Application.class, new TestApplication(),
+			HashMapDictionaryBuilder.<String, Object>put(
+				"liferay.auth.verifier", false
+			).put(
+				"liferay.oauth2", false
+			).put(
+				"osgi.jaxrs.application.base", "/object-rest-test/"
+			).put(
+				"osgi.jaxrs.extension.select",
+				"(osgi.jaxrs.name=Liferay.Vulcan)"
+			).build());
 	}
 
 	@AfterClass
 	public static void tearDownClass() {
-		for (ServiceRegistration<?> serviceRegistration :
-				_serviceRegistrations) {
-
-			serviceRegistration.unregister();
-		}
+		_serviceRegistration.unregister();
 	}
 
 	@Test
 	public void testingLazyProperties() throws Exception {
-		JSONAssert.assertEquals(
-			JSONUtil.put(
-				"property1", "true"
-			).toString(),
-			HTTPTestUtil.invokeToJSONObject(
-				null, "/object-rest-test/testSupplier?restrictFields=property1",
-				Http.Method.GET
-			).toString(),
-			JSONCompareMode.STRICT);
+		HTTPTestUtil.invokeToJSONObject(
+			null, "/object-rest-test/testSupplier?fields=property1",
+			Http.Method.GET);
 
-		Assert.assertFalse(_value2);
+		Assert.assertTrue(
+			"property1 should have been computed", _property1Computed);
+		Assert.assertFalse(
+			"property2 should not have been computed", _property2Computed);
 
-		JSONAssert.assertEquals(
-			JSONUtil.put(
-				"property1", "true"
-			).put(
-				"property2", "true"
-			).toString(),
-			HTTPTestUtil.invokeToJSONObject(
-				null, "/object-rest-test/testSupplier", Http.Method.GET
-			).toString(),
-			JSONCompareMode.STRICT);
+		HTTPTestUtil.invokeToJSONObject(
+			null, "/object-rest-test/testSupplier", Http.Method.GET);
+
+		Assert.assertTrue(
+			"property1 should have been computed", _property1Computed);
+		Assert.assertTrue(
+			"property2 should have been computed", _property2Computed);
 	}
 
 	public static class TestApplication extends Application {
@@ -128,16 +111,16 @@ public class ObjectEntryLazyPropertiesTest {
 							<String, UnsafeSupplier<Object, Exception>>put(
 								"property1",
 								() -> {
-									_value1 = true;
+									_property1Computed = true;
 
-									return _value1;
+									return _property1Computed;
 								}
 							).put(
 								"property2",
 								() -> {
-									_value2 = true;
+									_property2Computed = true;
 
-									return _value2;
+									return _property2Computed;
 								}
 							).build());
 				}
@@ -146,9 +129,8 @@ public class ObjectEntryLazyPropertiesTest {
 
 	}
 
-	private static final List<ServiceRegistration<?>> _serviceRegistrations =
-		new ArrayList<>();
-	private static boolean _value1;
-	private static boolean _value2;
+	private static boolean _property1Computed;
+	private static boolean _property2Computed;
+	private static ServiceRegistration<?> _serviceRegistration;
 
 }
