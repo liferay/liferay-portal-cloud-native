892a4ee68ae8a57fea37a13d3cf4e246803f0254
Alan Huang <alan.huang@liferay.com>
2024-04-06 23:33:52 +0800

    LPD-21824 SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaServiceObjectCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JavaServiceObjectCheck.java
@@ -175,7 +175,7 @@ public class JavaServiceObjectCheck extends BaseJavaTermCheck {
 
 				String trimmedSetterObjectName =
 					previousSetterObjectName.replaceFirst(
-						"(.+?)(List|Map)", "$1");
+						"(.+?)(List|Map)$", "$1");
 
 				String[] parts = StringUtil.split(
 					_getTableAndColumName(
@@ -191,7 +191,7 @@ public class JavaServiceObjectCheck extends BaseJavaTermCheck {
 				}
 
 				trimmedSetterObjectName = setterObjectName.replaceFirst(
-					"(.+?)(List|Map)", "$1");
+					"(.+?)(List|Map)$", "$1");
 
 				parts = StringUtil.split(
 					_getTableAndColumName(
@@ -282,7 +282,8 @@ public class JavaServiceObjectCheck extends BaseJavaTermCheck {
 			for (Element columnElement :
 					(List<Element>)entityElement.elements("column")) {
 
-				if (!setterObjectName.equals(
+				if (!StringUtil.equalsIgnoreCase(
+						setterObjectName,
 						columnElement.attributeValue("name"))) {
 
 					continue;
