8470ea12085afb6b312ed5c8dc030b1210044e3a
Jeff Wu <jeffwu0724@gmail.com>
2024-04-10 09:09:23 -0700

    Revert "LPD-1558 SF, remove unused method"
    This reverts commit 447da72d33a4c730f9da4f6169ebc509a9e9829a.

--- modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/simplecaptcha/SimpleCaptchaImpl.java
+++ modules/apps/captcha/captcha-api/src/main/java/com/liferay/captcha/simplecaptcha/SimpleCaptchaImpl.java
@@ -381,6 +381,11 @@ public class SimpleCaptchaImpl implements Captcha {
 		return false;
 	}
 
+	protected boolean isExceededMaxChallenges(PortletRequest portletRequest) {
+		return isExceededMaxChallenges(
+			portal.getHttpServletRequest(portletRequest));
+	}
+
 	protected void setCaptchaConfiguration(
 		CaptchaConfiguration captchaConfiguration) {
 
