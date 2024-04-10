cf3154c1b6b6e1f0e500ea054a48a870b3ef466f
Joana Grego <joana.lima@liferay.com>
2024-04-10 17:24:32 -0300

    LPD-22888 Add Solution Details step structure

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublisherDashboard/pages/Solutions/NewSolutionFlow/pages/SolutionDetails.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublisherDashboard/pages/Solutions/NewSolutionFlow/pages/SolutionDetails.tsx
@@ -3,8 +3,98 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
+import ClayIcon from '@clayui/icon';
+
+import './index.scss';
+
+import {ClaySelect} from '@clayui/form';
+import ClayModal, {useModal} from '@clayui/modal';
+
+import Form from '../../../../../../components/MarketplaceForm';
+
 const SolutionDetails = () => {
-	return <div>Details</div>;
+	const {observer, onOpenChange, open} = useModal();
+	const items = [
+		{label: 'Choose an option'},
+		{label: 'Text & Images Block'},
+		{label: 'Text & Video Block'},
+		{label: 'Text Block'},
+		{label: 'Single Image Block'},
+		{label: 'Icons Block'},
+		{label: 'Images Grid Block'},
+	];
+
+	return (
+		<div className="solutions-details-container">
+			<Form.Label className="mt-3" htmlFor="minimum-blocks" required>
+				Add a minimum of 2 blocks
+			</Form.Label>
+
+			<ClayButton
+				className="align-items-center content-block d-flex flex-row justify-content-center mt-4 w-100"
+				displayType="secondary"
+				onClick={() => onOpenChange(true)}
+			>
+				<span className="d-flex flex-row inline-item inline-item-before">
+					<ClayIcon symbol="plus" />
+				</span>
+				Add Content Block
+			</ClayButton>
+
+			{open && (
+				<ClayModal center observer={observer}>
+					<ClayModal.Body className="mb-1">
+						<h1 className="d-flex justify-content-between">
+							Select Content Block
+							<ClayButtonWithIcon
+								aria-label="Close"
+								className="inline-item"
+								displayType="unstyled"
+								onClick={() => onOpenChange(false)}
+								size="sm"
+								symbol="times"
+								title="Close"
+							/>
+						</h1>
+
+						<p className="text-black-50">
+							Choose one of the following content blocks
+						</p>
+						<Form.Label
+							className="mt-5"
+							htmlFor="choose-block"
+							required
+						>
+							Choose Block
+						</Form.Label>
+
+						<ClaySelect aria-label="Select Label" id="mySelectId">
+							{items.map((item, index) => (
+								<ClaySelect.Option
+									key={index}
+									label={item.label}
+									value={item.label}
+								/>
+							))}
+						</ClaySelect>
+
+						<div className="align-items-end d-flex justify-content-end mt-8">
+							<ClayButton
+								className="mr-2"
+								displayType="secondary"
+								onClick={() => onOpenChange(false)}
+							>
+								Cancel
+							</ClayButton>
+
+							<ClayButton displayType="primary">Save</ClayButton>
+						</div>
+					</ClayModal.Body>
+				</ClayModal>
+			)}
+		</div>
+	);
 };
 
 export default SolutionDetails;
