bc22bb7434bf9cf559b09ddfeb2a43d495331bcb
Joana Grego <joana.lima@liferay.com>
2024-04-11 11:40:32 -0300

    LPD-22995 Add Company Profile and Contact Us base step

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublisherDashboard/pages/Solutions/NewSolutionFlow/pages/SolutionContactUs.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublisherDashboard/pages/Solutions/NewSolutionFlow/pages/SolutionContactUs.tsx
@@ -3,8 +3,24 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import Form from '../../../../../../components/MarketplaceForm';
+import i18n from '../../../../../../i18n';
+
 const SolutionContactUs = () => {
-	return <div>Contact</div>;
+	return (
+		<div>
+			<h3>{i18n.translate('company-profile')}</h3>
+			<hr />
+			<Form.Label className="mt-3" htmlFor="email" required>
+				Email
+			</Form.Label>
+			<Form.Input
+				name="email"
+				placeholder="name@yourdomain.com"
+				type="name@yourdomain.com"
+			/>
+		</div>
+	);
 };
 
 export default SolutionContactUs;
