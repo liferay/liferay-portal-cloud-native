a563a008152d480bf5856cad64ecacf6413c537e
Richard Jeremias <richard.jeremias@liferay.com>
2024-04-12 13:01:27 -0300

    LPD-23164 Informing BaseNotification that Regular Roles can now be a Recipient

--- modules/apps/notification/notification-api/src/main/java/com/liferay/notification/type/BaseNotificationType.java
+++ modules/apps/notification/notification-api/src/main/java/com/liferay/notification/type/BaseNotificationType.java
@@ -141,7 +141,8 @@ public abstract class BaseNotificationType implements NotificationType {
 
 					if ((role == null) ||
 						((role.getType() != RoleConstants.TYPE_ACCOUNT) &&
-						 (role.getType() != RoleConstants.TYPE_ORGANIZATION))) {
+						 (role.getType() != RoleConstants.TYPE_ORGANIZATION) &&
+						 (role.getType() != RoleConstants.TYPE_REGULAR))) {
 
 						continue;
 					}
