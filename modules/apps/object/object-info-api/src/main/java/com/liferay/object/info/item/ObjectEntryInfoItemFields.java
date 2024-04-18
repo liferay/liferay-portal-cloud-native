1622fa9326a22c2dccd31165b94f9c029b3f9461
Carolina Barbosa <carolina.barbosa@liferay.com>
2024-04-18 13:50:23 -0300

    LPD-23201 Move ObjectEntryInfoItemFields to Object Info API module

--- modules/apps/object/object-info-api/src/main/java/com/liferay/object/info/item/ObjectEntryInfoItemFields.java
+++ modules/apps/object/object-info-api/src/main/java/com/liferay/object/info/item/ObjectEntryInfoItemFields.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.object.web.internal.info.item;
+package com.liferay.object.info.item;
 
 import com.liferay.info.field.InfoField;
 import com.liferay.info.field.type.DateInfoFieldType;
