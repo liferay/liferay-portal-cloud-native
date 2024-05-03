19159e7bb6659b61958f72c1063d0158d6d8c207
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2024-05-03 16:26:48 -0300

    LPD-16413 Use randomString

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectActionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectActionLocalServiceTest.java
@@ -1847,7 +1847,7 @@ public class ObjectActionLocalServiceTest {
 					ObjectFieldUtil.createObjectField(
 						ObjectFieldConstants.BUSINESS_TYPE_ATTACHMENT,
 						ObjectFieldConstants.DB_TYPE_LONG, true, false, null,
-						"attachment", "attachment",
+						RandomTestUtil.randomString(), "attachment",
 						Arrays.asList(
 							new ObjectFieldSettingBuilder(
 							).name(
@@ -1872,7 +1872,7 @@ public class ObjectActionLocalServiceTest {
 					ObjectFieldUtil.createObjectField(
 						ObjectFieldConstants.BUSINESS_TYPE_INTEGER,
 						ObjectFieldConstants.DB_TYPE_INTEGER, true, false, null,
-						"integer", "integer", false)));
+						RandomTestUtil.randomString(), "integer", false)));
 
 		_objectDefinitionLocalService.publishCustomObjectDefinition(
 			TestPropsValues.getUserId(),
