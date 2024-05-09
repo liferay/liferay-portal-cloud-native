a10cd6cf2ba71dab61fb5d88ef0121a836c89acc
IstvanD <istvan.dezsi@liferay.com>
2024-05-09 10:25:05 +0200

    LPD-25337 Get credentials from environment in poshi and integration tests

--- modules/apps/headless/headless-portal-instances/headless-portal-instances-test/src/testIntegration/java/com/liferay/headless/portal/instances/resource/v1_0/test/BasePortalInstanceResourceTestCase.java
+++ modules/apps/headless/headless-portal-instances/headless-portal-instances-test/src/testIntegration/java/com/liferay/headless/portal/instances/resource/v1_0/test/BasePortalInstanceResourceTestCase.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
@@ -97,7 +98,7 @@ public abstract class BasePortalInstanceResourceTestCase {
 			PortalInstanceResource.builder();
 
 		portalInstanceResource = builder.authentication(
-			"test@liferay.com", "test"
+			"test@liferay.com", PropsUtil.get("default.admin.password")
 		).locale(
 			LocaleUtil.getDefault()
 		).build();
