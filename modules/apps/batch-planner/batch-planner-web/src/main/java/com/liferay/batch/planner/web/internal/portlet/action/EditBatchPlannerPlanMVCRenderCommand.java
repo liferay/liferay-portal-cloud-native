187e085aa40df9272d4ab0926fce72cbfbab1e09
Jaime Leon <jaime.leon@liferay.com>
2024-05-10 11:34:34 +0200

    LPD-25627 Add resource name to options label

--- modules/apps/batch-planner/batch-planner-web/src/main/java/com/liferay/batch/planner/web/internal/portlet/action/EditBatchPlannerPlanMVCRenderCommand.java
+++ modules/apps/batch-planner/batch-planner-web/src/main/java/com/liferay/batch/planner/web/internal/portlet/action/EditBatchPlannerPlanMVCRenderCommand.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.vulcan.batch.engine.VulcanBatchEngineTaskItemDelegate;
 import com.liferay.portal.vulcan.batch.engine.VulcanBatchEngineTaskItemDelegateRegistry;
@@ -90,11 +91,19 @@ public class EditBatchPlannerPlanMVCRenderCommand implements MVCRenderCommand {
 						getVulcanBatchEngineTaskItemDelegate(
 							companyId, entityClassName);
 
+			String[] internalClassNameKeyParts = StringUtil.split(
+				entityClassName, StringPool.PERIOD);
+
 			internalClassNameKeyCategories.put(
 				entityClassName,
-				_getInternalClassNameKeyCategory(
-					FrameworkUtil.getBundle(
-						vulcanBatchEngineTaskItemDelegate.getClass())));
+				String.format(
+					"%s (%s - %s)",
+					vulcanBatchEngineTaskItemDelegate.getResourceName(),
+					internalClassNameKeyParts
+						[internalClassNameKeyParts.length - 2],
+					_getInternalClassNameKeyCategory(
+						FrameworkUtil.getBundle(
+							vulcanBatchEngineTaskItemDelegate.getClass()))));
 		}
 
 		return internalClassNameKeyCategories;
