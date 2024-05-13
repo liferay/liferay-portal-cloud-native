83a8fe9294a0d4bc583451d4d8f3684d8ac125bd
Iván Zaera Avellón <ivan.zaera@liferay.com>
2024-05-13 10:49:55 +0200

    LPD-23882 Make sure we are passed the argument build
    In the future we will add more commands as we keep migrating stuff from
    npm-scripts to this new build.
    
    See https://github.com/liferay-frontend/liferay-portal/pull/4125#discussion_r1596275297

--- modules/_node-scripts/bin.js
+++ modules/_node-scripts/bin.js
@@ -4,6 +4,16 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import('./index.mjs')
-	.then(({default: main}) => main())
-	.catch(console.error);
+const args = process.argv.slice(2);
+
+switch (args[0]) {
+	case 'build':
+		import('./index.mjs')
+			.then(({default: main}) => main())
+			.catch(console.error);
+		break;
+
+	default:
+		console.error('Usage: node-scripts build');
+		break;
+}
