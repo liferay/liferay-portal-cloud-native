liferay-database-infra/liferay-portal

LPD-22964 Create unit tests to ensure correct behaviour of lazy methods and discard that they execute anything before requested.

portal-impl/test/unit/com/liferay/portal/service/impl/ClassNameLocalServiceImplTest.java
@@ -1,0 +1,216 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.service.impl;
+
+import com.liferay.counter.kernel.service.CounterLocalService;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.model.ModelHintsUtil;
+import com.liferay.portal.kernel.service.persistence.ClassNamePersistence;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Supplier;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import org.mockito.Answers;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class ClassNameLocalServiceImplTest {
+
+	@ClassRule
+	public static LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_classNameLocalServiceImpl = new ClassNameLocalServiceImpl();
+
+		ReflectionTestUtil.setFieldValue(
+			_classNameLocalServiceImpl, "classNameLocalService",
+			_classNameLocalServiceImpl);
+
+		ReflectionTestUtil.setFieldValue(
+			_classNameLocalServiceImpl, "counterLocalService",
+			Mockito.mock(CounterLocalService.class));
+
+		_mockClassNamePersistence();
+		_mockModelHintsUtil();
+
+		_classNameLocalServiceImpl.checkClassNames();
+	}
+
+	@Test
+	public void testGetLazyClassNameId() {
+		Assert.assertEquals(
+			_CLASS_NAME_ID1,
+			_classNameLocalServiceImpl.getLazyClassNameId(
+				_CLASS_NAME_VALUE1
+			).get());
+	}
+
+	@Test
+	public void testGetLazyClassNameIdLaziness() {
+		_testLaziness(
+			() -> {
+				Supplier<Long> lazyClassNameId =
+					_classNameLocalServiceImpl.getLazyClassNameId(
+						"wrong.value");
+
+				Mockito.verify(
+					_classNamePersistence, Mockito.never()
+				).create(
+					Mockito.anyLong()
+				);
+
+				lazyClassNameId.get();
+			});
+	}
+
+	@Test
+	public void testGetLazyClassNameIds() {
+		Assert.assertArrayEquals(
+			new Long[] {_CLASS_NAME_ID1, _CLASS_NAME_ID2},
+			_classNameLocalServiceImpl.getLazyClassNameIds(
+				new String[] {_CLASS_NAME_VALUE1, _CLASS_NAME_VALUE2}
+			).get());
+	}
+
+	@Test
+	public void testGetLazyClassNameIdsLaziness() {
+		_testLaziness(
+			() -> {
+				Supplier<Long[]> lazyClassNameIds =
+					_classNameLocalServiceImpl.getLazyClassNameIds(
+						new String[] {"wrong.value"});
+
+				Mockito.verify(
+					_classNamePersistence, Mockito.never()
+				).create(
+					Mockito.anyLong()
+				);
+
+				lazyClassNameIds.get();
+			});
+	}
+
+	@Test
+	public void testGetLazyClassNameIdsLongArray() {
+		Assert.assertArrayEquals(
+			new long[] {_CLASS_NAME_ID1, _CLASS_NAME_ID2},
+			_classNameLocalServiceImpl.getLazyClassNameIdsLongArray(
+				new String[] {_CLASS_NAME_VALUE1, _CLASS_NAME_VALUE2}
+			).get());
+	}
+
+	@Test
+	public void testGetLazyClassNameIdsLongArrayLaziness() {
+		_testLaziness(
+			() -> {
+				Supplier<long[]> lazyClassNameIds =
+					_classNameLocalServiceImpl.getLazyClassNameIdsLongArray(
+						new String[] {"wrong.value"});
+
+				Mockito.verify(
+					_classNamePersistence, Mockito.never()
+				).create(
+					Mockito.anyLong()
+				);
+
+				lazyClassNameIds.get();
+			});
+	}
+
+	private static ClassName _getMockedClassName(
+		long classNameId, String value) {
+
+		ClassName className = Mockito.mock(ClassName.class);
+
+		Mockito.when(
+			className.getClassNameId()
+		).thenReturn(
+			classNameId
+		);
+		Mockito.when(
+			className.getValue()
+		).thenReturn(
+			value
+		);
+
+		return className;
+	}
+
+	private static void _mockClassNamePersistence() {
+		_classNamePersistence = Mockito.mock(
+			ClassNamePersistence.class, Answers.RETURNS_MOCKS);
+
+		Mockito.when(
+			_classNamePersistence.findAll()
+		).thenReturn(
+			Arrays.asList(_CLASS_NAME_1, _CLASS_NAME_2)
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			_classNameLocalServiceImpl, "classNamePersistence",
+			_classNamePersistence);
+	}
+
+	private static void _mockModelHintsUtil() {
+		MockedStatic<ModelHintsUtil> modelHintsUtilMockedStatic =
+			Mockito.mockStatic(ModelHintsUtil.class);
+
+		modelHintsUtilMockedStatic.when(
+			ModelHintsUtil::getModels
+		).thenReturn(
+			Collections.emptyList()
+		);
+	}
+
+	private void _testLaziness(Runnable runnable) {
+		Mockito.reset(_classNamePersistence);
+
+		Mockito.when(
+			_classNamePersistence.fetchByValue(Mockito.anyString())
+		).thenReturn(
+			null
+		);
+
+		runnable.run();
+
+		Mockito.verify(
+			_classNamePersistence
+		).create(
+			Mockito.anyLong()
+		);
+	}
+
+	private static final ClassName _CLASS_NAME_1 = _getMockedClassName(
+		1L, "com.liferay.test1");
+
+	private static final ClassName _CLASS_NAME_2 = _getMockedClassName(
+		2L, "com.liferay.test2");
+
+	private static final Long _CLASS_NAME_ID1 = 1L;
+
+	private static final Long _CLASS_NAME_ID2 = 2L;
+
+	private static final String _CLASS_NAME_VALUE1 = "com.liferay.test1";
+
+	private static final String _CLASS_NAME_VALUE2 = "com.liferay.test2";
+
+	private static ClassNameLocalServiceImpl _classNameLocalServiceImpl;
+	private static ClassNamePersistence _classNamePersistence;
+
+}

liferay/liferay-portal-cloud-native

portal-impl/test/unit/com/liferay/portal/service/impl/ClassNameLocalServiceImplTest.java
@@ -1,0 +1,189 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.service.impl;
+
+import com.liferay.counter.kernel.service.CounterLocalService;
+import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.model.ModelHintsUtil;
+import com.liferay.portal.kernel.service.persistence.ClassNamePersistence;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Supplier;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import org.mockito.Answers;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class ClassNameLocalServiceImplTest {
+
+	@ClassRule
+	public static LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_classNameLocalServiceImpl = new ClassNameLocalServiceImpl();
+
+		ReflectionTestUtil.setFieldValue(
+			_classNameLocalServiceImpl, "classNameLocalService",
+			_classNameLocalServiceImpl);
+
+		ReflectionTestUtil.setFieldValue(
+			_classNameLocalServiceImpl, "counterLocalService",
+			Mockito.mock(CounterLocalService.class));
+
+		_mockClassNamePersistence();
+		_mockModelHintsUtil();
+
+		_classNameLocalServiceImpl.checkClassNames();
+	}
+
+	@Test
+	public void testGetClassNameIdsSupplier() {
+		Assert.assertArrayEquals(
+			new long[] {_CLASS_NAME_ID1, _CLASS_NAME_ID2},
+			_classNameLocalServiceImpl.getClassNameIdsSupplier(
+				new String[] {_CLASS_NAME_VALUE1, _CLASS_NAME_VALUE2}
+			).get());
+	}
+
+	@Test
+	public void testGetClassNameIdsSupplierLaziness() {
+		_testLaziness(
+			() -> {
+				Supplier<long[]> lazyClassNameIds =
+					_classNameLocalServiceImpl.getClassNameIdsSupplier(
+						new String[] {"wrong.value"});
+
+				Mockito.verify(
+					_classNamePersistence, Mockito.never()
+				).create(
+					Mockito.anyLong()
+				);
+
+				lazyClassNameIds.get();
+			});
+	}
+
+	@Test
+	public void testGetClassNameIdSupplier() {
+		Assert.assertEquals(
+			_CLASS_NAME_ID1,
+			_classNameLocalServiceImpl.getClassNameIdSupplier(
+				_CLASS_NAME_VALUE1
+			).get());
+	}
+
+	@Test
+	public void testGetClassNameIdSupplierLaziness() {
+		_testLaziness(
+			() -> {
+				Supplier<Long> lazyClassNameId =
+					_classNameLocalServiceImpl.getClassNameIdSupplier(
+						"wrong.value");
+
+				Mockito.verify(
+					_classNamePersistence, Mockito.never()
+				).create(
+					Mockito.anyLong()
+				);
+
+				lazyClassNameId.get();
+			});
+	}
+
+	private static ClassName _getMockedClassName(
+		long classNameId, String value) {
+
+		ClassName className = Mockito.mock(ClassName.class);
+
+		Mockito.when(
+			className.getClassNameId()
+		).thenReturn(
+			classNameId
+		);
+		Mockito.when(
+			className.getValue()
+		).thenReturn(
+			value
+		);
+
+		return className;
+	}
+
+	private static void _mockClassNamePersistence() {
+		_classNamePersistence = Mockito.mock(
+			ClassNamePersistence.class, Answers.RETURNS_MOCKS);
+
+		Mockito.when(
+			_classNamePersistence.findAll()
+		).thenReturn(
+			Arrays.asList(_CLASS_NAME_1, _CLASS_NAME_2)
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			_classNameLocalServiceImpl, "classNamePersistence",
+			_classNamePersistence);
+	}
+
+	private static void _mockModelHintsUtil() {
+		MockedStatic<ModelHintsUtil> modelHintsUtilMockedStatic =
+			Mockito.mockStatic(ModelHintsUtil.class);
+
+		modelHintsUtilMockedStatic.when(
+			ModelHintsUtil::getModels
+		).thenReturn(
+			Collections.emptyList()
+		);
+	}
+
+	private void _testLaziness(Runnable runnable) {
+		Mockito.reset(_classNamePersistence);
+
+		Mockito.when(
+			_classNamePersistence.fetchByValue(Mockito.anyString())
+		).thenReturn(
+			null
+		);
+
+		runnable.run();
+
+		Mockito.verify(
+			_classNamePersistence
+		).create(
+			Mockito.anyLong()
+		);
+	}
+
+	private static final ClassName _CLASS_NAME_1 = _getMockedClassName(
+		1L, "com.liferay.test1");
+
+	private static final ClassName _CLASS_NAME_2 = _getMockedClassName(
+		2L, "com.liferay.test2");
+
+	private static final Long _CLASS_NAME_ID1 = 1L;
+
+	private static final Long _CLASS_NAME_ID2 = 2L;
+
+	private static final String _CLASS_NAME_VALUE1 = "com.liferay.test1";
+
+	private static final String _CLASS_NAME_VALUE2 = "com.liferay.test2";
+
+	private static ClassNameLocalServiceImpl _classNameLocalServiceImpl;
+	private static ClassNamePersistence _classNamePersistence;
+
+}