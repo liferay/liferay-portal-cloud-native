c7a0b7be986616d667576e4bf1353265fce3676b
Marcos Martins <marcos.martins@liferay.com>
2024-05-28 11:07:34 -0300

    LPD-2612 Create integration test

--- modules/dxp/apps/analytics/analytics-reports-test/src/testIntegration/java/com/liferay/analytics/reports/web/internal/product/navigation/control/menu/test/AnalyticsReportsProductNavigationControlMenuEntryTest.java
+++ modules/dxp/apps/analytics/analytics-reports-test/src/testIntegration/java/com/liferay/analytics/reports/web/internal/product/navigation/control/menu/test/AnalyticsReportsProductNavigationControlMenuEntryTest.java
@@ -17,7 +17,12 @@ import com.liferay.portal.configuration.test.util.CompanyConfigurationTemporaryS
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.PortletPreferences;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
@@ -26,7 +31,10 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.impl.PortletPreferencesImpl;
+import com.liferay.portal.security.permission.SimplePermissionChecker;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -200,6 +208,31 @@ public class AnalyticsReportsProductNavigationControlMenuEntryTest {
 		}
 	}
 
+	@Test
+	public void testIsShowWithResourcePermission() throws Exception {
+		PortletPreferences portletPreferences = new PortletPreferencesImpl();
+
+		portletPreferences.setPortletPreferencesId(RandomTestUtil.nextLong());
+		portletPreferences.setCompanyId(TestPropsValues.getCompanyId());
+		portletPreferences.setOwnerId(TestPropsValues.getUserId());
+		portletPreferences.setPlid(123L);
+		portletPreferences.setPortletId(
+			"com_liferay_blogs_web_portlet_BlogsPortlet_INSTANCE_rqst");
+		portletPreferences.setNew(true);
+
+		_portletPreferences =
+			_portletPreferencesLocalService.addPortletPreferences(
+				portletPreferences);
+
+		PermissionThreadLocal.setPermissionChecker(
+			_mockPermissionChecker(
+				ActionKeys.UPDATE, true, "com.liferay.blogs.model.BlogsEntry"));
+
+		Assert.assertTrue(
+			_productNavigationControlMenuEntry.isShow(
+				_getHttpServletRequest()));
+	}
+
 	private HttpServletRequest _getHttpServletRequest() throws PortalException {
 		MockHttpServletRequest mockHttpServletRequest =
 			new MockHttpServletRequest();
@@ -209,6 +242,8 @@ public class AnalyticsReportsProductNavigationControlMenuEntryTest {
 			new InfoItemReference(MockObject.class.getName(), 0));
 		mockHttpServletRequest.setAttribute(
 			WebKeys.THEME_DISPLAY, _getThemeDisplay());
+
+		mockHttpServletRequest.setParameter("p_l_id", "123");
 
 		return mockHttpServletRequest;
 	}
@@ -224,6 +259,28 @@ public class AnalyticsReportsProductNavigationControlMenuEntryTest {
 		return themeDisplay;
 	}
 
+	private PermissionChecker _mockPermissionChecker(
+		String actionKey, boolean hasPermission, String resourceName) {
+
+		return new SimplePermissionChecker() {
+
+			@Override
+			public boolean hasPermission(
+				long groupId, String name, String primKey, String actionId) {
+
+				if (StringUtil.equals(name, resourceName) &&
+					StringUtil.equals(primKey, "0") &&
+					StringUtil.equals(actionId, actionKey)) {
+
+					return hasPermission;
+				}
+
+				return false;
+			}
+
+		};
+	}
+
 	@Inject
 	private CompanyLocalService _companyLocalService;
 
@@ -231,6 +288,12 @@ public class AnalyticsReportsProductNavigationControlMenuEntryTest {
 	private Group _group;
 
 	private Layout _layout;
+
+	@DeleteAfterTestRun
+	private PortletPreferences _portletPreferences;
+
+	@Inject
+	private PortletPreferencesLocalService _portletPreferencesLocalService;
 
 	@Inject(
 		filter = "component.name=com.liferay.analytics.reports.web.internal.product.navigation.control.menu.AnalyticsReportsProductNavigationControlMenuEntry"
