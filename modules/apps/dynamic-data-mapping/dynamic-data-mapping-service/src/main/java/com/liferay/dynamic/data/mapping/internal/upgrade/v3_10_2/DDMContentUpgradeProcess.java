liferay-site-management/liferay-portal

LPD-1652 the update process should include ctCollectionId as part of the primary key

modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v3_10_2/DDMContentUpgradeProcess.java
@@ -55,7 +55,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 		try (PreparedStatement selectPreparedStatement =
 				connection.prepareStatement(
 					StringBundler.concat(
-						"select DDMContent.contentId, DDMContent.data_, ",
+						"select DDMContent.contentId, ",
+						"DDMContent.ctCollectionId, DDMContent.data_, ",
 						"DDMStructureVersion.definition from DDMContent inner ",
 						"join DDMFormInstanceRecordVersion on ",
 						"DDMContent.contentId = ",
@@ -71,7 +72,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 			PreparedStatement updatePreparedStatement =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
-					"update DDMContent set data_ = ? where contentId = ?")) {
+					"update DDMContent set data_ = ? where contentId = ? and " +
+						"ctCollectionId = ?")) {
 
 			try (ResultSet resultSet = selectPreparedStatement.executeQuery()) {
 				while (resultSet.next()) {
@@ -87,6 +89,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 					updatePreparedStatement.setString(1, newData);
 					updatePreparedStatement.setLong(
 						2, resultSet.getLong("contentId"));
+					updatePreparedStatement.setLong(
+						3, resultSet.getLong("ctCollectionId"));
 
 					updatePreparedStatement.addBatch();
 				}

modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v3_8_0/DDMContentUpgradeProcess.java
@@ -44,7 +44,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 	protected void doUpgrade() throws Exception {
 		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
 				StringBundler.concat(
-					"select DDMContent.contentId, DDMContent.data_, ",
+					"select DDMContent.contentId, DDMContent.ctCollectionId, ",
+					"DDMContent.data_, ",
 					"DDMStructureVersion.structureVersionId, ",
 					"DDMStructureVersion.definition from DDMContent inner ",
 					"join DDMFormInstanceRecordVersion on ",
@@ -61,7 +62,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 			PreparedStatement preparedStatement2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
-					"update DDMContent set data_ = ? where contentId = ?")) {
+					"update DDMContent set data_ = ? where contentId = ? and " +
+						"ctCollectionId = ?")) {
 
 			try (ResultSet resultSet = preparedStatement1.executeQuery()) {
 				while (resultSet.next()) {
@@ -90,6 +92,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 					preparedStatement2.setString(1, newData);
 					preparedStatement2.setLong(
 						2, resultSet.getLong("contentId"));
+					preparedStatement2.setLong(
+						3, resultSet.getLong("ctCollectionId"));
 
 					preparedStatement2.addBatch();
 				}

modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v4_3_2/DDMTemplateUpgradeProcess.java
@@ -27,12 +27,13 @@ public class DDMTemplateUpgradeProcess extends UpgradeProcess {
 
 	private void _upgradeDDMTemplate() throws Exception {
 		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
-				"select templateId, script FROM DDMTemplate where " +
-					"classNameId = ?");
+				"select ctCollectionId, templateId, script FROM DDMTemplate " +
+					"where classNameId = ?");
 			PreparedStatement preparedStatement2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
-					"update DDMTemplate set script = ? where templateId = ?")) {
+					"update DDMTemplate set script = ? where ctCollectionId " +
+						"= ? and templateId = ?")) {
 
 			preparedStatement1.setLong(
 				1, PortalUtil.getClassNameId(DDMStructure.class));
@@ -45,7 +46,9 @@ public class DDMTemplateUpgradeProcess extends UpgradeProcess {
 							resultSet.getString("script"), "randomizer.",
 							"random."));
 					preparedStatement2.setLong(
-						2, resultSet.getLong("templateId"));
+						2, resultSet.getLong("ctCollectionId"));
+					preparedStatement2.setLong(
+						3, resultSet.getLong("templateId"));
 
 					preparedStatement2.addBatch();
 				}
@@ -57,13 +60,13 @@ public class DDMTemplateUpgradeProcess extends UpgradeProcess {
 
 	private void _upgradeDDMTemplateVersion() throws Exception {
 		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
-				"select templateVersionId, script FROM DDMTemplateVersion " +
-					"where classNameId = ?");
+				"select ctCollectionId, templateVersionId, script FROM " +
+					"DDMTemplateVersion where classNameId = ?");
 			PreparedStatement preparedStatement2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
 					"update DDMTemplateVersion set script = ? where " +
-						"templateVersionId = ?")) {
+						"templateVersionId = ? and ctCollectionId = ?")) {
 
 			preparedStatement1.setLong(
 				1, PortalUtil.getClassNameId(DDMStructure.class));
@@ -77,6 +80,8 @@ public class DDMTemplateUpgradeProcess extends UpgradeProcess {
 							"random."));
 					preparedStatement2.setLong(
 						2, resultSet.getLong("templateVersionId"));
+					preparedStatement2.setLong(
+						3, resultSet.getLong("ctCollectionId"));
 
 					preparedStatement2.addBatch();
 				}

liferay/liferay-portal-cloud-native

modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v3_10_2/DDMContentUpgradeProcess.java
@@ -55,7 +55,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 		try (PreparedStatement selectPreparedStatement =
 				connection.prepareStatement(
 					StringBundler.concat(
-						"select DDMContent.contentId, DDMContent.data_, ",
+						"select DDMContent.ctCollectionId, ",
+						"DDMContent.contentId, DDMContent.data_, ",
 						"DDMStructureVersion.definition from DDMContent inner ",
 						"join DDMFormInstanceRecordVersion on ",
 						"DDMContent.contentId = ",
@@ -71,7 +72,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 			PreparedStatement updatePreparedStatement =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
-					"update DDMContent set data_ = ? where contentId = ?")) {
+					"update DDMContent set data_ = ? where contentId = ? and " +
+						"ctCollectionId = ?")) {
 
 			try (ResultSet resultSet = selectPreparedStatement.executeQuery()) {
 				while (resultSet.next()) {
@@ -87,6 +89,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 					updatePreparedStatement.setString(1, newData);
 					updatePreparedStatement.setLong(
 						2, resultSet.getLong("contentId"));
+					updatePreparedStatement.setLong(
+						3, resultSet.getLong("ctCollectionId"));
 
 					updatePreparedStatement.addBatch();
 				}

modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v3_8_0/DDMContentUpgradeProcess.java
@@ -44,7 +44,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 	protected void doUpgrade() throws Exception {
 		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
 				StringBundler.concat(
-					"select DDMContent.contentId, DDMContent.data_, ",
+					"select DDMContent.ctCollectionId, DDMContent.contentId, ",
+					"DDMContent.data_, ",
 					"DDMStructureVersion.structureVersionId, ",
 					"DDMStructureVersion.definition from DDMContent inner ",
 					"join DDMFormInstanceRecordVersion on ",
@@ -61,7 +62,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 			PreparedStatement preparedStatement2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
-					"update DDMContent set data_ = ? where contentId = ?")) {
+					"update DDMContent set data_ = ? where contentId = ? and " +
+						"ctCollectionId = ?")) {
 
 			try (ResultSet resultSet = preparedStatement1.executeQuery()) {
 				while (resultSet.next()) {
@@ -90,6 +92,8 @@ public class DDMContentUpgradeProcess extends UpgradeProcess {
 					preparedStatement2.setString(1, newData);
 					preparedStatement2.setLong(
 						2, resultSet.getLong("contentId"));
+					preparedStatement2.setLong(
+						3, resultSet.getLong("ctCollectionId"));
 
 					preparedStatement2.addBatch();
 				}

modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v4_3_2/DDMTemplateUpgradeProcess.java
@@ -27,12 +27,13 @@ public class DDMTemplateUpgradeProcess extends UpgradeProcess {
 
 	private void _upgradeDDMTemplate() throws Exception {
 		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
-				"select templateId, script FROM DDMTemplate where " +
-					"classNameId = ?");
+				"select ctCollectionId, templateId, script FROM DDMTemplate " +
+					"where classNameId = ?");
 			PreparedStatement preparedStatement2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
-					"update DDMTemplate set script = ? where templateId = ?")) {
+					"update DDMTemplate set script = ? where ctCollectionId " +
+						"= ? and templateId = ?")) {
 
 			preparedStatement1.setLong(
 				1, PortalUtil.getClassNameId(DDMStructure.class));
@@ -45,7 +46,9 @@ public class DDMTemplateUpgradeProcess extends UpgradeProcess {
 							resultSet.getString("script"), "randomizer.",
 							"random."));
 					preparedStatement2.setLong(
-						2, resultSet.getLong("templateId"));
+						2, resultSet.getLong("ctCollectionId"));
+					preparedStatement2.setLong(
+						3, resultSet.getLong("templateId"));
 
 					preparedStatement2.addBatch();
 				}
@@ -57,13 +60,13 @@ public class DDMTemplateUpgradeProcess extends UpgradeProcess {
 
 	private void _upgradeDDMTemplateVersion() throws Exception {
 		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
-				"select templateVersionId, script FROM DDMTemplateVersion " +
-					"where classNameId = ?");
+				"select ctCollectionId, templateVersionId, script FROM " +
+					"DDMTemplateVersion where classNameId = ?");
 			PreparedStatement preparedStatement2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
 					"update DDMTemplateVersion set script = ? where " +
-						"templateVersionId = ?")) {
+						"ctCollectionId = ? and templateVersionId = ?")) {
 
 			preparedStatement1.setLong(
 				1, PortalUtil.getClassNameId(DDMStructure.class));
@@ -76,7 +79,9 @@ public class DDMTemplateUpgradeProcess extends UpgradeProcess {
 							resultSet.getString("script"), "randomizer.",
 							"random."));
 					preparedStatement2.setLong(
-						2, resultSet.getLong("templateVersionId"));
+						2, resultSet.getLong("ctCollectionId"));
+					preparedStatement2.setLong(
+						3, resultSet.getLong("templateVersionId"));
 
 					preparedStatement2.addBatch();
 				}