972ab2ad9e549a1b5d5bdf46f1687ad8fb3ffb81
Crescenzo Rega <crescenzo.rega@liferay.com>
2024-06-12 11:32:34 +0200

    LPD-22281 Add Adyen client extension

---
+++ workspaces/liferay-adyen-workspace/client-extensions/liferay-adyen-commerce-payment-integration/src/main/java/com/liferay/adyen/BaseRestController.java
@@ -0,0 +1,179 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.adyen;
+
+import java.util.Objects;
+
+import org.apache.commons.logging.Log;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Raymond Augé
+ * @author Gregory Amerson
+ * @author Brian Wing Shun Chan
+ * @author Crescenzo Rega
+ */
+public abstract class BaseRestController {
+
+	protected void delete(String authorization, Log log, String path) {
+		if (log.isDebugEnabled()) {
+			log.debug("Call DELETE: " + path);
+		}
+
+		getWebClient(
+		).delete(
+		).uri(
+			uriBuilder -> uriBuilder.path(
+				path
+			).build()
+		).header(
+			HttpHeaders.AUTHORIZATION, authorization
+		).retrieve(
+		).bodyToMono(
+			String.class
+		).subscribe(
+			successResponse -> {
+				if (log.isDebugEnabled()) {
+					log.debug("Successful Response: " + successResponse);
+				}
+			},
+			error -> {
+				throw new RuntimeException(error);
+			}
+		);
+	}
+
+	protected JSONObject get(String authorization, Log log, String path) {
+		Mono<String> response = getWebClient(
+		).get(
+		).uri(
+			uriBuilder -> uriBuilder.path(
+				path
+			).build()
+		).header(
+			HttpHeaders.AUTHORIZATION, authorization
+		).retrieve(
+		).bodyToMono(
+			String.class
+		);
+
+		response.subscribe(
+			successResponse -> {
+				if (log.isDebugEnabled()) {
+					log.debug("Successful Response: " + successResponse);
+				}
+			},
+			error -> {
+				throw new RuntimeException(error);
+			});
+
+		return new JSONObject(Objects.requireNonNull(response.block()));
+	}
+
+	protected WebClient getWebClient() {
+		return WebClient.builder(
+		).baseUrl(
+			lxcDXPServerProtocol + "://" + lxcDXPMainDomain
+		).defaultHeader(
+			HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE
+		).defaultHeader(
+			HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE
+		).build();
+	}
+
+	protected void log(Jwt jwt, Log log, String json) {
+		if (log.isInfoEnabled()) {
+			try {
+				JSONObject jsonObject = new JSONObject(json);
+
+				log.info("JSON: " + jsonObject.toString(4));
+			}
+			catch (Exception exception) {
+				log.error("JSON: " + json, exception);
+			}
+
+			log.info("JWT Claims: " + jwt.getClaims());
+			log.info("JWT ID: " + jwt.getId());
+			log.info("JWT Subject: " + jwt.getSubject());
+		}
+	}
+
+	protected void patch(
+		String authorization, String body, Log log, String path) {
+
+		getWebClient(
+		).patch(
+		).uri(
+			uriBuilder -> uriBuilder.path(
+				path
+			).build()
+		).bodyValue(
+			body
+		).header(
+			HttpHeaders.AUTHORIZATION, authorization
+		).retrieve(
+		).bodyToMono(
+			String.class
+		).subscribe(
+			successResponse -> {
+				if (log.isDebugEnabled()) {
+					log.debug("Successful Response: " + successResponse);
+				}
+			},
+			error -> {
+				throw new RuntimeException(error);
+			}
+		);
+	}
+
+	protected void post(
+		String authorization, String body, Log log, String path) {
+
+		if (log.isDebugEnabled()) {
+			log.debug("Call POST: " + path);
+		}
+
+		getWebClient(
+		).post(
+		).uri(
+			uriBuilder -> uriBuilder.path(
+				path
+			).build()
+		).bodyValue(
+			body
+		).header(
+			HttpHeaders.AUTHORIZATION, authorization
+		).retrieve(
+		).bodyToMono(
+			String.class
+		).subscribe(
+			successResponse -> {
+				if (log.isDebugEnabled()) {
+					log.debug("Successful Response: " + successResponse);
+				}
+			},
+			error -> {
+				throw new RuntimeException(error);
+			}
+		);
+	}
+
+	@Value("${com.liferay.lxc.dxp.mainDomain}")
+	protected String lxcDXPMainDomain;
+
+	@Value("${com.liferay.lxc.dxp.server.protocol}")
+	protected String lxcDXPServerProtocol;
+
+}
