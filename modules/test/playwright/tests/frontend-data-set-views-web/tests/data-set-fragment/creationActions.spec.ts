5d0af1fed3008d102ebf7ea72927daf14b7a5a13
Daniel Sanz <daniel.sanz@liferay.com>
2024-06-14 14:10:09 +0200

    LPD-28527 Split creationActions.spec.ts

--- modules/test/playwright/tests/frontend-data-set-views-web/tests/data-set-fragment/creationActions.spec.ts
+++ modules/test/playwright/tests/frontend-data-set-views-web/tests/data-set-fragment/creationActions.spec.ts
@@ -9,188 +9,12 @@ import {apiHelpersTest} from '../../../../fixtures/apiHelpersTest';
 import {featureFlagsTest} from '../../../../fixtures/featureFlagsTest';
 import {isolatedLayoutTest} from '../../../../fixtures/isolatedLayoutTest';
 import {loginTest} from '../../../../fixtures/loginTest';
-import {liferayConfig} from '../../../../liferay.config';
 import getRandomString from '../../../../utils/getRandomString';
-import {actionsPageTest} from './fixtures/actionsPageTest';
 import {dataSetManagerApiHelpersTest} from '../../fixtures/dataSetManagerApiHelpersTest';
-import {dataSetManagerSetupTest} from './fixtures/dataSetManagerSetupTest';
 import {fdsFragmentPageTest} from '../../tests/data-set-fragment/fixtures/fdsFragmentPageTest';
-
-const LINK_CREATION_ACTION_NAME = 'Link creation action';
-const MODAL_CREATION_ACTION_NAME = 'Modal creation action';
-const MODAL_CREATION_ACTION_TITLE = 'Modal creation title';
-const SIDE_PANEL_CREATION_ACTION_NAME = 'Side Panel creation action';
-const SIDE_PANEL_CREATION_ACTION_TITLE = 'Side Panel creation title';
-
-export const test = mergeTests(
-	actionsPageTest,
-	dataSetManagerApiHelpersTest,
-	featureFlagsTest({
-		'LPS-164563': true,
-		'LPS-178052': true,
-	}),
-	loginTest(),
-	dataSetManagerSetupTest
-);
 
 let dataSetERC: string;
 let dataSetLabel: string;
-
-test.beforeEach(async ({dataSetManagerApiHelpers}) => {
-	dataSetERC = getRandomString();
-	dataSetLabel = getRandomString();
-
-	await dataSetManagerApiHelpers.createDataSet({
-		erc: dataSetERC,
-		label: dataSetLabel,
-	});
-});
-
-test.afterEach(async ({dataSetManagerApiHelpers}) => {
-	await dataSetManagerApiHelpers.deleteDataSet({erc: dataSetERC});
-});
-
-test.describe('Creation Actions in Data Set Manager', () => {
-	test('There is a message if no Creation Action has been created', async ({
-		actionsPage,
-	}) => {
-		await test.step('Navigate to the Actions tab', async () => {
-			await actionsPage.goto({
-				dataSetLabel,
-			});
-
-			await expect(actionsPage.creationActionsTab).toBeInViewport();
-		});
-
-		await test.step('Navigate to the Creation Actions tab', async () => {
-			await actionsPage.creationActionsTab.click();
-			await actionsPage.newCreationActionButton.waitFor();
-		});
-
-		await test.step('Assert no Creation Actions are created', async () => {
-			await expect(actionsPage.noActionsWereCreatedMessage).toContainText(
-				'No actions were created.'
-			);
-		});
-	});
-
-	test('Can create a Creation Action of type Link', async ({
-		actionsPage,
-		page,
-	}) => {
-		await test.step('Navigate to the Actions tab', async () => {
-			await actionsPage.goto({
-				dataSetLabel,
-			});
-
-			await expect(actionsPage.creationActionsTab).toBeInViewport();
-		});
-
-		await test.step('Navigate to the Creation Actions tab', async () => {
-			await actionsPage.creationActionsTab.click();
-			await actionsPage.newCreationActionButton.waitFor();
-		});
-
-		await test.step('Create a creation action', async () => {
-			await actionsPage.createCreationAction({
-				icon: 'arrow-right-full',
-				name: LINK_CREATION_ACTION_NAME,
-				type: 'link',
-				url: liferayConfig.environment.baseUrl,
-			});
-		});
-
-		await test.step('Check that the creation action is in the list', async () => {
-			await expect(actionsPage.creationActionsTab).toBeInViewport();
-
-			await expect(
-				page.getByRole('cell', {
-					exact: true,
-					name: LINK_CREATION_ACTION_NAME,
-				})
-			).toBeVisible();
-		});
-	});
-
-	test('Can create a Creation Action of type Modal', async ({
-		actionsPage,
-		page,
-	}) => {
-		await test.step('Navigate to the Actions tab', async () => {
-			await actionsPage.goto({
-				dataSetLabel,
-			});
-
-			await expect(actionsPage.creationActionsTab).toBeInViewport();
-		});
-
-		await test.step('Navigate to the Creation Actions tab', async () => {
-			await actionsPage.creationActionsTab.click();
-			await actionsPage.newCreationActionButton.waitFor();
-		});
-
-		await test.step('Create a creation action', async () => {
-			await actionsPage.createCreationAction({
-				icon: 'arrow-right-full',
-				name: MODAL_CREATION_ACTION_NAME,
-				title: MODAL_CREATION_ACTION_TITLE,
-				type: 'modal',
-				url: liferayConfig.environment.baseUrl,
-				variant: 'sm',
-			});
-		});
-
-		await test.step('Check that the creation action is in the list', async () => {
-			await expect(actionsPage.creationActionsTab).toBeInViewport();
-
-			await expect(
-				page.getByRole('cell', {
-					exact: true,
-					name: MODAL_CREATION_ACTION_NAME,
-				})
-			).toBeVisible();
-		});
-	});
-
-	test('Can create a Creation Action of type Side Panel', async ({
-		actionsPage,
-		page,
-	}) => {
-		await test.step('Navigate to the Actions tab', async () => {
-			await actionsPage.goto({
-				dataSetLabel,
-			});
-
-			await expect(actionsPage.creationActionsTab).toBeInViewport();
-		});
-
-		await test.step('Navigate to the Creation Actions tab', async () => {
-			await actionsPage.creationActionsTab.click();
-			await actionsPage.newCreationActionButton.waitFor();
-		});
-
-		await test.step('Create a creation action', async () => {
-			await actionsPage.createCreationAction({
-				icon: 'arrow-right-full',
-				name: SIDE_PANEL_CREATION_ACTION_NAME,
-				title: SIDE_PANEL_CREATION_ACTION_TITLE,
-				type: 'sidePanel',
-				url: liferayConfig.environment.baseUrl,
-			});
-		});
-
-		await test.step('Check that the creation action is in the list', async () => {
-			await expect(actionsPage.creationActionsTab).toBeInViewport();
-
-			await expect(
-				page.getByRole('cell', {
-					exact: true,
-					name: SIDE_PANEL_CREATION_ACTION_NAME,
-				})
-			).toBeVisible();
-		});
-	});
-});
 
 export const fragmentTest = mergeTests(
 	apiHelpersTest,
@@ -203,6 +27,20 @@ export const fragmentTest = mergeTests(
 	isolatedLayoutTest({publish: false}),
 	loginTest()
 );
+
+fragmentTest.beforeEach(async ({dataSetManagerApiHelpers}) => {
+	dataSetERC = getRandomString();
+	dataSetLabel = getRandomString();
+
+	await dataSetManagerApiHelpers.createDataSet({
+		erc: dataSetERC,
+		label: dataSetLabel,
+	});
+});
+
+fragmentTest.afterEach(async ({dataSetManagerApiHelpers}) => {
+	await dataSetManagerApiHelpers.deleteDataSet({erc: dataSetERC});
+});
 
 fragmentTest.describe('Creation Actions in Data Set fragment', () => {
 	fragmentTest(
