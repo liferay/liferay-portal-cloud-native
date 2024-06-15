1a5c2aec3e9737460a26d66b6826ef2cd8a69e83
Lianne Louie <lianne.louie@liferay.com>
2024-06-14 19:43:02 -0700

    LPD-28116 - SF and add additional functional tests

--- modules/test/playwright/tests/account-admin-web/accountPermissions.spec.ts
+++ modules/test/playwright/tests/account-admin-web/accountPermissions.spec.ts
@@ -6,7 +6,6 @@
 import {expect, mergeTests} from '@playwright/test';
 
 import {accountsPagesTest} from '../../fixtures/accountsPagesTest';
-import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {dataApiHelpersTest} from '../../fixtures/dataApiHelpersTest';
 import {loginTest} from '../../fixtures/loginTest';
 import {usersAndOrganizationsPagesTest} from '../../fixtures/usersAndOrganizationsPagesTest';
@@ -15,92 +14,288 @@ import getRandomString from '../../utils/getRandomString';
 
 export const test = mergeTests(
 	accountsPagesTest,
-	apiHelpersTest,
 	dataApiHelpersTest,
 	loginTest(),
 	usersAndOrganizationsPagesTest
 );
 
-test('LPD-28116 Edit Organizations permission', async ({
-	accountsPage,
-	apiHelpers,
-	context,
-	page,
-	usersAndOrganizationsPage,
-}) => {
-	const organization1 = await apiHelpers.headlessAdminUser.postOrganization();
-	const organization2 = await apiHelpers.headlessAdminUser.postOrganization();
-	const user = await apiHelpers.headlessAdminUser.postUserAccount();
-	await apiHelpers.headlessAdminUser.assignUserToOrganizationByEmailAddress(
-		organization1.id,
-		user.emailAddress
-	);
+test.describe('Test for Organization Account visibility depending on Permissions', () => {
+	test('LPD-28116 Edit Organizations permission visibility', async ({
+		accountsPage,
+		apiHelpers,
+		context,
+		page,
+		usersAndOrganizationsPage,
+	}) => {
+		const organization1 =
+			await apiHelpers.headlessAdminUser.postOrganization();
+		const organization2 =
+			await apiHelpers.headlessAdminUser.postOrganization();
 
-	const companyId = await page.evaluate(() => {
-		return Liferay.ThemeDisplay.getCompanyId();
+		const user = await apiHelpers.headlessAdminUser.postUserAccount();
+
+		await apiHelpers.headlessAdminUser.assignUserToOrganizationByEmailAddress(
+			organization1.id,
+			user.emailAddress
+		);
+
+		const companyId = await page.evaluate(() => {
+			return Liferay.ThemeDisplay.getCompanyId();
+		});
+
+		const role = await apiHelpers.headlessAdminUser.postRole({
+			name: getRandomString(),
+			rolePermissions: [
+				{
+					actionIds: [
+						'EDIT_ORGANIZATIONS',
+						'MANAGE_USERS',
+						'UPDATE',
+						'VIEW',
+						'VIEW_ORGANIZATIONS',
+					],
+					primaryKey: companyId,
+					resourceName: 'com.liferay.account.model.AccountEntry',
+					scope: 1,
+				},
+				{
+					actionIds: ['MANAGE_AVAILABLE_ACCOUNTS'],
+					primaryKey: companyId,
+					resourceName:
+						'com.liferay.portal.kernel.model.Organization',
+					scope: 1,
+				},
+				{
+					actionIds: ['ACCESS_IN_CONTROL_PANEL'],
+					primaryKey: companyId,
+					resourceName:
+						'com_liferay_account_admin_web_internal_portlet_AccountEntriesAdminPortlet',
+					scope: 1,
+				},
+			],
+		});
+
+		await apiHelpers.headlessAdminUser.assignUserToOrganizationRole(
+			role.id,
+			user.id,
+			organization1.id
+		);
+
+		const account = await apiHelpers.headlessAdminUser.postAccount();
+		apiHelpers.data.push({id: account.id, type: 'account'});
+
+		await apiHelpers.headlessAdminUser.postAccountOrganization(
+			account.id,
+			organization1.id
+		);
+
+		await usersAndOrganizationsPage.goToUsers();
+
+		await (
+			await usersAndOrganizationsPage.usersTableRowActions(
+				`${user.alternateName}`
+			)
+		).click();
+
+		const pagePromise = context.waitForEvent('page');
+
+		await usersAndOrganizationsPage.impersonateUserMenuItem.click();
+
+		const newPage = await pagePromise;
+		accountsPage = new AccountsPage(newPage);
+
+		await accountsPage.goto();
+		await (await accountsPage.accountsTableRowLink(account.name)).click();
+		await accountsPage.organizationsTab.click();
+		await accountsPage.newButton.click();
+
+		await expect(
+			page.getByText(organization2.name, {exact: true})
+		).toHaveCount(0);
 	});
 
-	const role = await apiHelpers.headlessAdminUser.postRole({
-		name: getRandomString(),
-		rolePermissions: [
-			{
-				actionIds: [
-					'EDIT_ORGANIZATIONS',
-					'MANAGE_USERS',
-					'UPDATE',
-					'VIEW',
-				],
-				primaryKey: companyId,
-				resourceName: 'com.liferay.account.model.AccountEntry',
-				scope: 1,
-			},
-			{
-				actionIds: ['MANAGE_AVAILABLE_ACCOUNTS'],
-				primaryKey: companyId,
-				resourceName: 'com.liferay.portal.kernel.model.Organization',
-				scope: 1,
-			},
-			{
-				actionIds: ['ACCESS_IN_CONTROL_PANEL'],
-				primaryKey: companyId,
-				resourceName:
-					'com_liferay_account_admin_web_internal_portlet_AccountEntriesAdminPortlet',
-				scope: 1,
-			},
-		],
+	test('LPD-28116 Manage Organizations Permission visibility', async ({
+		accountsPage,
+		apiHelpers,
+		context,
+		page,
+		usersAndOrganizationsPage,
+	}) => {
+		const organization1 =
+			await apiHelpers.headlessAdminUser.postOrganization();
+		const organization2 =
+			await apiHelpers.headlessAdminUser.postOrganization();
+
+		const user = await apiHelpers.headlessAdminUser.postUserAccount();
+
+		await apiHelpers.headlessAdminUser.assignUserToOrganizationByEmailAddress(
+			organization1.id,
+			user.emailAddress
+		);
+
+		const companyId = await page.evaluate(() => {
+			return Liferay.ThemeDisplay.getCompanyId();
+		});
+
+		const role = await apiHelpers.headlessAdminUser.postRole({
+			name: getRandomString(),
+			rolePermissions: [
+				{
+					actionIds: [
+						'MANAGE_ORGANIZATIONS',
+						'MANAGE_USERS',
+						'UPDATE',
+						'VIEW',
+						'VIEW_ORGANIZATIONS',
+					],
+					primaryKey: companyId,
+					resourceName: 'com.liferay.account.model.AccountEntry',
+					scope: 1,
+				},
+				{
+					actionIds: ['MANAGE_AVAILABLE_ACCOUNTS'],
+					primaryKey: companyId,
+					resourceName:
+						'com.liferay.portal.kernel.model.Organization',
+					scope: 1,
+				},
+				{
+					actionIds: ['ACCESS_IN_CONTROL_PANEL'],
+					primaryKey: companyId,
+					resourceName:
+						'com_liferay_account_admin_web_internal_portlet_AccountEntriesAdminPortlet',
+					scope: 1,
+				},
+			],
+		});
+
+		await apiHelpers.headlessAdminUser.assignUserToOrganizationRole(
+			role.id,
+			user.id,
+			organization1.id
+		);
+
+		const account = await apiHelpers.headlessAdminUser.postAccount();
+		apiHelpers.data.push({id: account.id, type: 'account'});
+
+		await apiHelpers.headlessAdminUser.postAccountOrganization(
+			account.id,
+			organization1.id
+		);
+
+		await usersAndOrganizationsPage.goToUsers();
+
+		await (
+			await usersAndOrganizationsPage.usersTableRowActions(
+				`${user.alternateName}`
+			)
+		).click();
+
+		const pagePromise = context.waitForEvent('page');
+
+		await usersAndOrganizationsPage.impersonateUserMenuItem.click();
+
+		const newPage = await pagePromise;
+		accountsPage = new AccountsPage(newPage);
+
+		await accountsPage.goto();
+		await (await accountsPage.accountsTableRowLink(account.name)).click();
+		await accountsPage.organizationsTab.click();
+		await accountsPage.newButton.click();
+
+		await expect(
+			page.getByText(organization1.name, {exact: true})
+		).toBeVisible();
+		await expect(
+			page.getByText(organization2.name, {exact: true})
+		).toBeVisible();
 	});
 
-	await apiHelpers.headlessAdminUser.assignUserToOrganizationRole(
-		role.id,
-		user.id,
-		organization1.id
-	);
+	test('LPD-28116 No Edit or Manage Organizations permission', async ({
+		accountsPage,
+		apiHelpers,
+		context,
+		page,
+		usersAndOrganizationsPage,
+	}) => {
+		const organization1 =
+			await apiHelpers.headlessAdminUser.postOrganization();
 
-	const account = await apiHelpers.headlessAdminUser.postAccount();
-	await apiHelpers.headlessAdminUser.postAccountOrganization(
-		account.id,
-		organization1.id
-	);
+		const user = await apiHelpers.headlessAdminUser.postUserAccount();
 
-	await usersAndOrganizationsPage.goToUsers();
-	await (
-		await usersAndOrganizationsPage.usersTableRowActions(
-			`${user.alternateName}`
-		)
-	).click();
+		await apiHelpers.headlessAdminUser.assignUserToOrganizationByEmailAddress(
+			organization1.id,
+			user.emailAddress
+		);
 
-	const pagePromise = context.waitForEvent('page');
-	await usersAndOrganizationsPage.impersonateUserMenuItem.click();
+		const companyId = await page.evaluate(() => {
+			return Liferay.ThemeDisplay.getCompanyId();
+		});
 
-	const newPage = await pagePromise;
-	accountsPage = new AccountsPage(newPage);
+		const role = await apiHelpers.headlessAdminUser.postRole({
+			name: getRandomString(),
+			rolePermissions: [
+				{
+					actionIds: [
+						'MANAGE_USERS',
+						'UPDATE',
+						'VIEW',
+						'VIEW_ORGANIZATIONS',
+					],
+					primaryKey: companyId,
+					resourceName: 'com.liferay.account.model.AccountEntry',
+					scope: 1,
+				},
+				{
+					actionIds: ['MANAGE_AVAILABLE_ACCOUNTS'],
+					primaryKey: companyId,
+					resourceName:
+						'com.liferay.portal.kernel.model.Organization',
+					scope: 1,
+				},
+				{
+					actionIds: ['ACCESS_IN_CONTROL_PANEL'],
+					primaryKey: companyId,
+					resourceName:
+						'com_liferay_account_admin_web_internal_portlet_AccountEntriesAdminPortlet',
+					scope: 1,
+				},
+			],
+		});
 
-	await accountsPage.goto();
-	await (await accountsPage.accountsTableRowLink(account.name)).click();
-	await accountsPage.organizationsTab.click();
-	await accountsPage.newButton.click();
+		await apiHelpers.headlessAdminUser.assignUserToOrganizationRole(
+			role.id,
+			user.id,
+			organization1.id
+		);
 
-	await expect(page.getByText(organization2.name, {exact: true})).toHaveCount(
-		0
-	);
+		const account = await apiHelpers.headlessAdminUser.postAccount();
+		apiHelpers.data.push({id: account.id, type: 'account'});
+
+		await apiHelpers.headlessAdminUser.postAccountOrganization(
+			account.id,
+			organization1.id
+		);
+
+		await usersAndOrganizationsPage.goToUsers();
+
+		await (
+			await usersAndOrganizationsPage.usersTableRowActions(
+				`${user.alternateName}`
+			)
+		).click();
+
+		const pagePromise = context.waitForEvent('page');
+
+		await usersAndOrganizationsPage.impersonateUserMenuItem.click();
+
+		const newPage = await pagePromise;
+		accountsPage = new AccountsPage(newPage);
+
+		await accountsPage.goto();
+		await (await accountsPage.accountsTableRowLink(account.name)).click();
+		await accountsPage.organizationsTab.click();
+
+		await expect(accountsPage.newButton).toHaveCount(0);
+	});
 });
