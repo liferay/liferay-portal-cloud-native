2d6cec3f6476621974ee9d1a8307990400dd3db3
Vinicius Lopes <vinicius.lopes@liferay.com>
2024-06-17 11:43:07 -0300

    LPD-28681 Add the createDynamicSegment function to create a dynamic segment

--- modules/test/playwright/tests/osb-faro-web/utils/segments.ts
+++ modules/test/playwright/tests/osb-faro-web/utils/segments.ts
@@ -3,7 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Page} from '@playwright/test';
+import { Page } from "@playwright/test";
+
+export async function createDynamicSegment(page: Page) {
+	await page.getByLabel('Menu').click();
+	await page.getByRole('menuitem', {name: 'Dynamic Segment'}).click();
+}
 
 export async function dragAndDropCriteriaItem(page: Page, itemName: string) {
 	const source = page.getByTestId(`criteria-item-${itemName}`);
