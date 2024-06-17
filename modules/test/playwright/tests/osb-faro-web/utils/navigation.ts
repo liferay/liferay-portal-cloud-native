6c4fb21d822e9fa230e53fefe7eebb72ed788790
Vinicius Lopes <vinicius.lopes@liferay.com>
2024-06-17 09:51:52 -0300

    LPD-28681 Add the navigateToACPage function to be able to open the AC page within the demo workspace

---
+++ modules/test/playwright/tests/osb-faro-web/utils/navigation.ts
@@ -0,0 +1,18 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import { Page } from '@playwright/test';
+import {faroConfig} from '../faro.config';
+
+export async function navigateToACPage(page: Page) {
+	await page.goto(faroConfig.environment.baseUrl);
+
+	await page
+		.getByRole('link', {
+			name: 'FARO-DEV-liferay Liferay Demo Enterprise Plan',
+		})
+		.click();
+}
+
