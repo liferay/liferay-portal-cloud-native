e44135e678e232ba7af1b5f6b1c29e464f080a6d
João Victor Cordeiro <joao.cordeiro@liferay.com>
2024-06-20 18:30:42 -0300

    LPD-29697 update method calls

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/ProductResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/ProductResourceImpl.java
@@ -1091,7 +1091,7 @@ public class ProductResourceImpl extends BaseProductResourceImpl {
 						sku.getPrice(), cpInstance.getPrice()),
 					(BigDecimal)GetterUtil.get(
 						sku.getPromoPrice(), cpInstance.getPromoPrice()),
-					serviceContext);
+					StringPool.BLANK, serviceContext);
 			}
 		}
 
