a8e21b73423675bcb394d8d56de7faf4ff5070a3
gustavolimav <gustavo.lima@liferay.com>
2024-07-03 15:19:38 -0300

    LPD-26165 Move BaseExportImportPortletPreferencesProcessorTestCase to portal-search-test-util so I can reuse in other module
    https://liferay.atlassian.net/browse/LPD-26165

--- modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/exportimport/BaseExportImportPortletPreferencesProcessorTestCase.java
+++ modules/apps/portal-search/portal-search-test-util/src/main/java/com/liferay/portal/search/test/util/exportimport/BaseExportImportPortletPreferencesProcessorTestCase.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.search.web.internal.exportimport.portlet.preferences.processor.test;
+package com.liferay.portal.search.test.util.exportimport;
 
 import com.liferay.exportimport.portlet.preferences.processor.Capability;
 import com.liferay.exportimport.portlet.preferences.processor.ExportImportPortletPreferencesProcessor;
