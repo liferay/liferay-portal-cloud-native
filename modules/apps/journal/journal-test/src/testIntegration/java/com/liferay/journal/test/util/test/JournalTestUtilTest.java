4fd38d2013fc08e0469999197c1b5e5be35b27ff
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2024-07-05 11:06:28 +0200

    LPD-30519 Same for other test classes in package

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/test/util/test/JournalTestUtilTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/test/util/test/JournalTestUtilTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.journal.util.test;
+package com.liferay.journal.test.util.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
