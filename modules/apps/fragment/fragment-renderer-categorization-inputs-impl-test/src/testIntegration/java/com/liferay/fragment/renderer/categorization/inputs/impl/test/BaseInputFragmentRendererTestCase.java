63473bd812e321b34261b9763f32aaaf7bddde3e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-07-16 13:38:54 +0200

    LPD-31556 Simplify by using the test util to add the form

--- modules/apps/fragment/fragment-renderer-categorization-inputs-impl-test/src/testIntegration/java/com/liferay/fragment/renderer/categorization/inputs/impl/test/BaseInputFragmentRendererTestCase.java
+++ modules/apps/fragment/fragment-renderer-categorization-inputs-impl-test/src/testIntegration/java/com/liferay/fragment/renderer/categorization/inputs/impl/test/BaseInputFragmentRendererTestCase.java
@@ -8,16 +8,12 @@ package com.liferay.fragment.renderer.categorization.inputs.impl.test;
 import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.renderer.DefaultFragmentRendererContext;
 import com.liferay.fragment.renderer.FragmentRenderer;
-import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.info.constants.InfoDisplayWebKeys;
 import com.liferay.info.item.InfoItemServiceRegistry;
 import com.liferay.info.item.provider.InfoItemDetailsProvider;
-import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
-import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.provider.LayoutStructureProvider;
 import com.liferay.layout.test.util.ContentLayoutTestUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
-import com.liferay.layout.util.structure.FormStyledLayoutStructureItem;
-import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.field.util.ObjectFieldUtil;
@@ -25,12 +21,12 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
 import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.test.rule.Inject;
@@ -121,60 +117,31 @@ public abstract class BaseInputFragmentRendererTestCase {
 
 	protected FragmentEntryLink addFragmentEntryLinkToForm() throws Exception {
 		Layout draftLayout = layout.fetchDraftLayout();
-
-		LayoutPageTemplateStructure layoutPageTemplateStructure =
-			layoutPageTemplateStructureLocalService.
-				fetchLayoutPageTemplateStructure(
-					draftLayout.getGroupId(), draftLayout.getPlid());
-
-		long defaultSegmentsExperienceId =
+		long segmentsExperienceId =
 			segmentsExperienceLocalService.fetchDefaultSegmentsExperienceId(
 				layout.getPlid());
 
-		LayoutStructure layoutStructure = LayoutStructure.of(
-			layoutPageTemplateStructure.getData(defaultSegmentsExperienceId));
-
-		FormStyledLayoutStructureItem formStyledLayoutStructureItem =
-			(FormStyledLayoutStructureItem)
-				layoutStructure.addFormStyledLayoutStructureItem(
-					layoutStructure.getMainItemId(), 0);
-
-		formStyledLayoutStructureItem.setClassNameId(
-			portal.getClassNameId(objectDefinition.getClassName()));
+		JSONObject jsonObject = ContentLayoutTestUtil.addFormToLayout(
+			false,
+			String.valueOf(
+				portal.getClassNameId(objectDefinition.getClassName())),
+			"0", draftLayout, layoutStructureProvider, segmentsExperienceId);
 
 		FragmentRenderer fragmentRenderer = getFragmentRenderer();
 
-		FragmentEntryLink fragmentEntryLink =
-			fragmentEntryLinkLocalService.addFragmentEntryLink(
-				null, TestPropsValues.getUserId(), draftLayout.getGroupId(), 0,
-				0, defaultSegmentsExperienceId, draftLayout.getPlid(),
-				StringPool.BLANK, StringPool.BLANK, StringPool.BLANK,
-				fragmentRenderer.getConfiguration(
-					new DefaultFragmentRendererContext(null)),
-				StringPool.BLANK, StringPool.BLANK, 0, getRenderKey(),
-				fragmentRenderer.getType(),
-				ServiceContextTestUtil.getServiceContext(
-					draftLayout.getGroupId(), TestPropsValues.getUserId()));
-
-		layoutStructure.addFragmentStyledLayoutStructureItem(
-			fragmentEntryLink.getFragmentEntryLinkId(),
-			formStyledLayoutStructureItem.getItemId(), 0);
-
-		layoutPageTemplateStructureLocalService.
-			updateLayoutPageTemplateStructureData(
-				draftLayout.getGroupId(), draftLayout.getPlid(),
-				defaultSegmentsExperienceId, layoutStructure.toString());
-
-		return fragmentEntryLink;
+		return ContentLayoutTestUtil.addFragmentEntryLinkToLayout(
+			StringPool.BLANK, StringPool.BLANK,
+			fragmentRenderer.getConfiguration(
+				new DefaultFragmentRendererContext(null)),
+			0, StringPool.BLANK, StringPool.BLANK, draftLayout, getRenderKey(),
+			fragmentRenderer.getType(), jsonObject.getString("addedItemId"), 0,
+			segmentsExperienceId);
 	}
 
 	@Inject
 	protected CompanyLocalService companyLocalService;
 
 	protected FragmentEntryLink fragmentEntryLink;
-
-	@Inject
-	protected FragmentEntryLinkLocalService fragmentEntryLinkLocalService;
 
 	@DeleteAfterTestRun
 	protected Group group;
@@ -185,8 +152,7 @@ public abstract class BaseInputFragmentRendererTestCase {
 	protected Layout layout;
 
 	@Inject
-	protected LayoutPageTemplateStructureLocalService
-		layoutPageTemplateStructureLocalService;
+	protected LayoutStructureProvider layoutStructureProvider;
 
 	@DeleteAfterTestRun
 	protected ObjectDefinition objectDefinition;
