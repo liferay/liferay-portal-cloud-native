c32a921a9c686cc19c85956a284fd4d7528fcd46
Brian Chan <brian.chan@liferay.com>
2024-07-18 08:58:17 +0530

    LPD-30653 I think this is what you're trying to say.

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectAction/tabs/ActionContainer/ThenContainer.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectAction/tabs/ActionContainer/ThenContainer.tsx
@@ -156,7 +156,7 @@ export function ThenContainer({
 						'send-email-notifications-in-the-guest-users-preferred-language'
 					),
 					title: Liferay.Language.get(
-						'send-email-notifications-to-guest-users-in-the-form-submission-language'
+						'send-email-notifications-to-guest-users-in-the-form\'s-submission-language'
 					),
 				}
 			: values.parameters?.relatedObjectEntries !== undefined
