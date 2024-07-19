d5b7b59268bc654d0df8322dde2794ddd72ab9e8
Tina Tian <tina.tian@liferay.com>
2024-07-19 13:57:08 -0700

    LPD-31842 Add two methods, one use @Indexable, one do manually reindex, prepare for adding test

--- modules/util/portal-tools-service-builder-test-service/src/main/java/com/liferay/portal/tools/service/builder/test/service/impl/IndexEntryLocalServiceImpl.java
+++ modules/util/portal-tools-service-builder-test-service/src/main/java/com/liferay/portal/tools/service/builder/test/service/impl/IndexEntryLocalServiceImpl.java
@@ -5,10 +5,50 @@
 
 package com.liferay.portal.tools.service.builder.test.service.impl;
 
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.tools.service.builder.test.model.IndexEntry;
 import com.liferay.portal.tools.service.builder.test.service.base.IndexEntryLocalServiceBaseImpl;
 
 /**
  * @author Brian Wing Shun Chan
  */
 public class IndexEntryLocalServiceImpl extends IndexEntryLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	public IndexEntry addIndexEntry(long companyId, String name) {
+		long indexEntryId = counterLocalService.increment();
+
+		IndexEntry indexEntry = indexEntryPersistence.create(indexEntryId);
+
+		indexEntry.setCompanyId(companyId);
+		indexEntry.setName(name);
+
+		return indexEntryPersistence.updateImpl(indexEntry);
+	}
+
+	public boolean addKeywordsEntry(
+		long keywordsEntryId, IndexEntry indexEntry) {
+
+		if (!super.addKeywordsEntryIndexEntry(keywordsEntryId, indexEntry)) {
+			return false;
+		}
+
+		try {
+			Indexer<IndexEntry> indexer =
+				IndexerRegistryUtil.nullSafeGetIndexer(IndexEntry.class);
+
+			indexer.reindex(indexEntry);
+		}
+		catch (SearchException searchException) {
+			throw new SystemException(searchException);
+		}
+
+		return true;
+	}
+
 }
