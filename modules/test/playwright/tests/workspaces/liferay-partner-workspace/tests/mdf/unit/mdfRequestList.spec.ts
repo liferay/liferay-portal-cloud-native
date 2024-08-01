liferay-customer-solutions/liferay-portal

LRSD-5100 More cleanup

modules/test/playwright/tests/workspaces/liferay-partner-workspace/tests/mdf/unit/mdfRequestList.spec.ts
@@ -6,42 +6,68 @@
 import {expect, mergeTests} from '@playwright/test';
 
 import {partnerPagesTest} from '../../../fixtures/partnerPagesTest';
-import {companyName, mdfRequestMock} from '../../../mocks/mdfMock';
+import {accountPlatinumMock} from '../../../mocks/accountMock';
+import {userCOMMock} from '../../../mocks/userMock';
+import {TAccount} from '../../../types/account';
+import {TMDFRequest} from '../../../types/mdf';
+import {TUserAccount} from '../../../types/user';
+import {EAccountRoles} from '../../../utils/constants';
 import {customFormatDate, getDateCustomFormat} from '../../../utils/date';
+import {generateMDFRequestData} from '../../../utils/mdf';
 
 export const test = mergeTests(partnerPagesTest);
 
 test.describe('MDF Request List', () => {
-	let partnerAccount;
-	let mdfRequest;
+	let accountPlatinum: TAccount;
+	let userCOM: TUserAccount;
+	let mdfRequest: TMDFRequest;
 
 	test.beforeEach(
 		async ({mdfRequestListPage, partnerHelper, partnerSite}) => {
-			partnerAccount = await partnerHelper.createAccountUser({
-				name: companyName,
-				type: 'business',
-			});
+			accountPlatinum =
+				await partnerHelper.apiHelpers.headlessAdminUser.postAccount(
+					accountPlatinumMock
+				);
+			userCOM =
+				await partnerHelper.apiHelpers.headlessAdminUser.postUserAccount(
+					userCOMMock
+				);
 
-			await partnerHelper.assignUserToAccountRole({
-				accountId: partnerAccount.id,
-				accountRole: '[Account] Partner Manager (PM)',
-			});
+			await partnerHelper.assignUserToAccountRole(
+				accountPlatinum.id,
+				EAccountRoles.PARTNER_MANAGER,
+				Number(userCOM.id)
+			);
 
-			mdfRequest = await partnerHelper.createMDFRequest({
-				data: mdfRequestMock,
-			});
+			const mdfRequestData = generateMDFRequestData(
+				accountPlatinum,
+				userCOM
+			);
+
+			mdfRequest = await partnerHelper.createMDFRequest(mdfRequestData);
 
 			await mdfRequestListPage.goto(partnerSite.friendlyUrlPath);
 		}
 	);
 
 	test.afterEach(async ({partnerHelper}) => {
-		await partnerHelper.apiHelpers.delete(
-			`/o/c/mdfrequests/${mdfRequest.id}`
-		);
-		await partnerHelper.apiHelpers.headlessAdminUser.deleteAccount(
-			partnerAccount.id
-		);
+		if (accountPlatinum) {
+			await partnerHelper.apiHelpers.headlessAdminUser.deleteAccount(
+				accountPlatinum.id
+			);
+		}
+
+		if (userCOM) {
+			await partnerHelper.apiHelpers.headlessAdminUser.deleteUserAccount(
+				Number(userCOM.id)
+			);
+		}
+
+		if (mdfRequest) {
+			await partnerHelper.apiHelpers.delete(
+				`/o/c/mdfrequests/${mdfRequest.id}`
+			);
+		}
 	});
 
 	test('Open MDF Request List', async ({page}) => {
@@ -53,27 +79,27 @@ test.describe('MDF Request List', () => {
 	});
 
 	test('Display MDF Resquest List', async ({mdfRequestListPage}) => {
-		const autoGenerateData =
+		const generatedDataFromRequest =
 			await mdfRequestListPage.getGeneratedDataFromRequest(
-				mdfRequest.overallCampaignName
+				mdfRequest.goals.overallCampaignName
 			);
 		const campaignName = await mdfRequestListPage.getCampaignName();
 		const formatEndDate = getDateCustomFormat(
-			mdfRequest.maxDateActivity,
+			mdfRequest.activities[0].endDate,
 			customFormatDate.SHORT_MONTH
 		);
 		const formatStartDate = getDateCustomFormat(
-			mdfRequest.minDateActivity,
+			mdfRequest.activities[0].startDate,
 			customFormatDate.SHORT_MONTH
 		);
 
 		const endActPeriod =
 			await mdfRequestListPage.getEndActPeriod(formatEndDate);
 		const partnerName = await mdfRequestListPage.getPartnerName(
-			mdfRequest.companyName
+			mdfRequest.goals.companyName
 		);
 		const requested = await mdfRequestListPage.getRequested(
-			mdfRequest.totalMDFRequestAmount
+			String(mdfRequest.totalMDFRequestAmount)
 		);
 		const startActPeriod =
 			await mdfRequestListPage.getStartActPeriod(formatStartDate);
@@ -83,10 +109,10 @@ test.describe('MDF Request List', () => {
 		await expect(endActPeriod).toBeVisible();
 		await expect(partnerName).toBeVisible();
 		await expect(requested).toBeTruthy();
-		await expect(autoGenerateData.requestId).toBeTruthy();
+		await expect(generatedDataFromRequest.requestId).toBeTruthy();
 		await expect(startActPeriod).toBeVisible();
-		await expect(autoGenerateData.status).toBeTruthy();
-		await expect(autoGenerateData.submitDate).toBeTruthy();
+		await expect(generatedDataFromRequest.status).toBeTruthy();
+		await expect(generatedDataFromRequest.submitDate).toBeTruthy();
 	});
 
 	test('Filter data by Activity Period', async ({
@@ -94,11 +120,11 @@ test.describe('MDF Request List', () => {
 		page,
 	}) => {
 		const formatEndDate = getDateCustomFormat(
-			mdfRequest.maxDateActivity,
+			mdfRequest.activities[0].endDate,
 			customFormatDate.SHORT_MONTH
 		);
 		const formatStartDate = getDateCustomFormat(
-			mdfRequest.minDateActivity,
+			mdfRequest.activities[0].startDate,
 			customFormatDate.SHORT_MONTH
 		);
 		const endActPeriod =
@@ -132,11 +158,11 @@ test.describe('MDF Request List', () => {
 	});
 
 	test('Filter data by Status', async ({mdfRequestListPage, page}) => {
-		const autoGenerateData =
+		const generatedDataFromRequest =
 			await mdfRequestListPage.getGeneratedDataFromRequest(
-				mdfRequest.overallCampaignName
+				mdfRequest.goals.overallCampaignName
 			);
-		const status = autoGenerateData.status;
+		const status = generatedDataFromRequest.status;
 
 		await mdfRequestListPage.filterMDFRequestByStatus(status);
 
@@ -157,11 +183,11 @@ test.describe('MDF Request List', () => {
 
 	test('Filter data by Partner', async ({mdfRequestListPage, page}) => {
 		const partnerName = await mdfRequestListPage.getPartnerName(
-			mdfRequest.companyName
+			mdfRequest.goals.companyName
 		);
 
 		await mdfRequestListPage.filterMDFRequestByPartner(
-			mdfRequest.companyName
+			mdfRequest.goals.companyName
 		);
 
 		await page.getByText('MDF Requests').click();
@@ -201,12 +227,12 @@ test.describe('MDF Request List', () => {
 		mdfRequestFormPage,
 		mdfRequestListPage,
 	}) => {
-		const autoGenerateData =
+		const generatedDataFromRequest =
 			await mdfRequestListPage.getGeneratedDataFromRequest(
-				mdfRequest.overallCampaignName
+				mdfRequest.goals.overallCampaignName
 			);
 		const requestId = await mdfRequestListPage.getRequestId(
-			autoGenerateData.requestId
+			generatedDataFromRequest.requestId
 		);
 
 		await requestId.click();
@@ -240,14 +266,14 @@ test.describe('MDF Request List', () => {
 		page,
 	}) => {
 		const actionButton = await mdfRequestListPage.actionButton;
-		const autoGenerateData =
+		const generatedDataFromRequest =
 			await mdfRequestListPage.getGeneratedDataFromRequest(
-				mdfRequest.overallCampaignName
+				mdfRequest.goals.overallCampaignName
 			);
 		const completedTab = await mdfRequestListPage.completedTab;
 		const completeMenuItem = await mdfRequestListPage.completeMenuItem;
 		const requestId = await mdfRequestListPage.getRequestId(
-			autoGenerateData.requestId
+			generatedDataFromRequest.requestId
 		);
 		const successTooltip = await page.getByText(
 			'Success:MDF Request successfully completed!'

liferay/liferay-portal-cloud-native

modules/test/playwright/tests/workspaces/liferay-partner-workspace/tests/mdf/unit/mdfRequestList.spec.ts
@@ -6,42 +6,59 @@
 import {expect, mergeTests} from '@playwright/test';
 
 import {partnerPagesTest} from '../../../fixtures/partnerPagesTest';
-import {companyName, mdfRequestMock} from '../../../mocks/mdfMock';
 import {customFormatDate, getDateCustomFormat} from '../../../utils/date';
+import { accountPlatinumMock } from '../../../mocks/accountMock';
+import { userCOMMock } from '../../../mocks/userMock';
+import { EAccountRoles } from '../../../utils/constants';
+import { generateMDFRequestData } from '../../../utils/mdf';
+import { TAccount } from '../../../types/account';
+import { TUserAccount } from '../../../types/user';
+import { TMDFRequest } from '../../../types/mdf';
 
 export const test = mergeTests(partnerPagesTest);
 
 test.describe('MDF Request List', () => {
-	let partnerAccount;
-	let mdfRequest;
+	let accountPlatinum: TAccount;
+	let userCOM: TUserAccount;
+	let mdfRequest: TMDFRequest;
 
 	test.beforeEach(
 		async ({mdfRequestListPage, partnerHelper, partnerSite}) => {
-			partnerAccount = await partnerHelper.createAccountUser({
-				name: companyName,
-				type: 'business',
-			});
+			accountPlatinum = await partnerHelper.apiHelpers.headlessAdminUser.postAccount(
+				accountPlatinumMock
+			);
+			userCOM = await partnerHelper.apiHelpers.headlessAdminUser.postUserAccount(
+				userCOMMock
+			);
 
-			await partnerHelper.assignUserToAccountRole({
-				accountId: partnerAccount.id,
-				accountRole: '[Account] Partner Manager (PM)',
-			});
+			await partnerHelper.assignUserToAccountRole(accountPlatinum.id, EAccountRoles.PARTNER_MANAGER, Number(userCOM.id));
 
-			mdfRequest = await partnerHelper.createMDFRequest({
-				data: mdfRequestMock,
-			});
+			const mdfRequestData = generateMDFRequestData(accountPlatinum, userCOM);
+
+			mdfRequest = await partnerHelper.createMDFRequest(mdfRequestData);
 
 			await mdfRequestListPage.goto(partnerSite.friendlyUrlPath);
 		}
 	);
 
 	test.afterEach(async ({partnerHelper}) => {
-		await partnerHelper.apiHelpers.delete(
-			`/o/c/mdfrequests/${mdfRequest.id}`
-		);
-		await partnerHelper.apiHelpers.headlessAdminUser.deleteAccount(
-			partnerAccount.id
-		);
+		if (accountPlatinum) {
+			await partnerHelper.apiHelpers.headlessAdminUser.deleteAccount(
+				accountPlatinum.id
+			);
+		}
+
+		if (userCOM) {
+			await partnerHelper.apiHelpers.headlessAdminUser.deleteUserAccount(
+				Number(userCOM.id)
+			);
+		}
+
+		if (mdfRequest) {
+			await partnerHelper.apiHelpers.delete(
+				`/o/c/mdfrequests/${mdfRequest.id}`
+			);
+		}
 	});
 
 	test('Open MDF Request List', async ({page}) => {
@@ -53,27 +70,27 @@ test.describe('MDF Request List', () => {
 	});
 
 	test('Display MDF Resquest List', async ({mdfRequestListPage}) => {
-		const autoGenerateData =
+		const generatedDataFromRequest =
 			await mdfRequestListPage.getGeneratedDataFromRequest(
-				mdfRequest.overallCampaignName
+				mdfRequest.goals.overallCampaignName
 			);
 		const campaignName = await mdfRequestListPage.getCampaignName();
 		const formatEndDate = getDateCustomFormat(
-			mdfRequest.maxDateActivity,
+			mdfRequest.activities[0].endDate,
 			customFormatDate.SHORT_MONTH
 		);
 		const formatStartDate = getDateCustomFormat(
-			mdfRequest.minDateActivity,
+			mdfRequest.activities[0].startDate,
 			customFormatDate.SHORT_MONTH
 		);
 
 		const endActPeriod =
 			await mdfRequestListPage.getEndActPeriod(formatEndDate);
 		const partnerName = await mdfRequestListPage.getPartnerName(
-			mdfRequest.companyName
+			mdfRequest.goals.companyName
 		);
 		const requested = await mdfRequestListPage.getRequested(
-			mdfRequest.totalMDFRequestAmount
+			String(mdfRequest.totalMDFRequestAmount)
 		);
 		const startActPeriod =
 			await mdfRequestListPage.getStartActPeriod(formatStartDate);
@@ -83,10 +100,10 @@ test.describe('MDF Request List', () => {
 		await expect(endActPeriod).toBeVisible();
 		await expect(partnerName).toBeVisible();
 		await expect(requested).toBeTruthy();
-		await expect(autoGenerateData.requestId).toBeTruthy();
+		await expect(generatedDataFromRequest.requestId).toBeTruthy();
 		await expect(startActPeriod).toBeVisible();
-		await expect(autoGenerateData.status).toBeTruthy();
-		await expect(autoGenerateData.submitDate).toBeTruthy();
+		await expect(generatedDataFromRequest.status).toBeTruthy();
+		await expect(generatedDataFromRequest.submitDate).toBeTruthy();
 	});
 
 	test('Filter data by Activity Period', async ({
@@ -94,11 +111,11 @@ test.describe('MDF Request List', () => {
 		page,
 	}) => {
 		const formatEndDate = getDateCustomFormat(
-			mdfRequest.maxDateActivity,
+			mdfRequest.activities[0].endDate,
 			customFormatDate.SHORT_MONTH
 		);
 		const formatStartDate = getDateCustomFormat(
-			mdfRequest.minDateActivity,
+			mdfRequest.activities[0].startDate,
 			customFormatDate.SHORT_MONTH
 		);
 		const endActPeriod =
@@ -132,11 +149,11 @@ test.describe('MDF Request List', () => {
 	});
 
 	test('Filter data by Status', async ({mdfRequestListPage, page}) => {
-		const autoGenerateData =
+		const generatedDataFromRequest =
 			await mdfRequestListPage.getGeneratedDataFromRequest(
-				mdfRequest.overallCampaignName
+				mdfRequest.goals.overallCampaignName
 			);
-		const status = autoGenerateData.status;
+		const status = generatedDataFromRequest.status;
 
 		await mdfRequestListPage.filterMDFRequestByStatus(status);
 
@@ -157,11 +174,11 @@ test.describe('MDF Request List', () => {
 
 	test('Filter data by Partner', async ({mdfRequestListPage, page}) => {
 		const partnerName = await mdfRequestListPage.getPartnerName(
-			mdfRequest.companyName
+			mdfRequest.goals.companyName
 		);
 
 		await mdfRequestListPage.filterMDFRequestByPartner(
-			mdfRequest.companyName
+			mdfRequest.goals.companyName
 		);
 
 		await page.getByText('MDF Requests').click();
@@ -201,12 +218,12 @@ test.describe('MDF Request List', () => {
 		mdfRequestFormPage,
 		mdfRequestListPage,
 	}) => {
-		const autoGenerateData =
+		const generatedDataFromRequest =
 			await mdfRequestListPage.getGeneratedDataFromRequest(
-				mdfRequest.overallCampaignName
+				mdfRequest.goals.overallCampaignName
 			);
 		const requestId = await mdfRequestListPage.getRequestId(
-			autoGenerateData.requestId
+			generatedDataFromRequest.requestId
 		);
 
 		await requestId.click();
@@ -240,14 +257,14 @@ test.describe('MDF Request List', () => {
 		page,
 	}) => {
 		const actionButton = await mdfRequestListPage.actionButton;
-		const autoGenerateData =
+		const generatedDataFromRequest =
 			await mdfRequestListPage.getGeneratedDataFromRequest(
-				mdfRequest.overallCampaignName
+				mdfRequest.goals.overallCampaignName
 			);
 		const completedTab = await mdfRequestListPage.completedTab;
 		const completeMenuItem = await mdfRequestListPage.completeMenuItem;
 		const requestId = await mdfRequestListPage.getRequestId(
-			autoGenerateData.requestId
+			generatedDataFromRequest.requestId
 		);
 		const successTooltip = await page.getByText(
 			'Success:MDF Request successfully completed!'