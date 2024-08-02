igor-franca/liferay-portal

LPD-30650 Consider the user's time zone and use Intl instead of the moment

modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/display/context/KaleoDesignerDisplayContext.java
@@ -515,6 +515,18 @@ public class KaleoDesignerDisplayContext {
 		return userName;
 	}
 
+	public String getUserTimeZone() {
+		User user = _themeDisplay.getUser();
+
+		if ((user == null) || user.isGuestUser() ||
+			Validator.isNull(user.getFullName())) {
+
+			return null;
+		}
+
+		return user.getTimeZoneId();
+	}
+
 	public boolean isAllowScriptContentToBeExecutedOrIncluded() {
 		return _scriptManagementConfigurationHelper.
 			isAllowScriptContentToBeExecutedOrIncluded();

modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/util/toLocalDateTimeFormatted.ts
@@ -1,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export default function toLocalDateTimeFormatted(
+	dateUTC: string,
+	language: string,
+	userTimezone: string
+) {
+	const date = new Date(dateUTC);
+
+	const options = {
+		day: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+		month: 'short',
+		timeZone: userTimezone,
+		year: 'numeric',
+	} as const;
+
+	const timeZone = new Intl.DateTimeFormat(language, options);
+
+	return timeZone.format(date);
+}

liferay/liferay-portal-cloud-native

modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/display/context/KaleoDesignerDisplayContext.java
@@ -483,6 +483,18 @@ public class KaleoDesignerDisplayContext {
 			));
 	}
 
+	public String getTimeZoneId() {
+		User user = _themeDisplay.getUser();
+
+		if ((user == null) || user.isGuestUser() ||
+			Validator.isNull(user.getFullName())) {
+
+			return null;
+		}
+
+		return user.getTimeZoneId();
+	}
+
 	public String getTitle(KaleoDefinitionVersion kaleoDefinitionVersion) {
 		if (kaleoDefinitionVersion == null) {
 			return _getLanguage("new-workflow");

modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/util/toLocalDateTimeFormatted.ts
@@ -1,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export default function toLocalDateTimeFormatted(
+	dateUTC: string,
+	language: string,
+	timeZoneId: string
+) {
+	const date = new Date(dateUTC);
+
+	const options = {
+		day: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+		month: 'short',
+		timeZone: timeZoneId,
+		year: 'numeric',
+	} as const;
+
+	const timeZone = new Intl.DateTimeFormat(language, options);
+
+	return timeZone.format(date);
+}