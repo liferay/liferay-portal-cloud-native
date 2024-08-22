2db7c0540f537e2aeb1f1edb1a058061bafe1261
Marcos Martins <marcos.martins@liferay.com>
2024-08-22 18:13:45 -0300

    LPD-33307 implement endpoint

--- modules/dxp/apps/analytics/analytics-reports-rest-impl/src/main/java/com/liferay/analytics/reports/rest/internal/resource/v1_0/AssetMetricResourceImpl.java
+++ modules/dxp/apps/analytics/analytics-reports-rest-impl/src/main/java/com/liferay/analytics/reports/rest/internal/resource/v1_0/AssetMetricResourceImpl.java
@@ -5,9 +5,16 @@
 
 package com.liferay.analytics.reports.rest.internal.resource.v1_0;
 
+import com.liferay.analytics.reports.rest.dto.v1_0.AssetMetric;
+import com.liferay.analytics.reports.rest.internal.client.AnalyticsCloudClient;
 import com.liferay.analytics.reports.rest.resource.v1_0.AssetMetricResource;
+import com.liferay.analytics.settings.rest.manager.AnalyticsSettingsManager;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupService;
+import com.liferay.portal.kernel.util.Http;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +25,33 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = AssetMetricResource.class
 )
 public class AssetMetricResourceImpl extends BaseAssetMetricResourceImpl {
+
+	@Override
+	public AssetMetric getGroupAssetMetric(
+			Integer groupId, String assetType, String assetId, Integer rangeKey,
+			String[] selectedMetrics)
+		throws Exception {
+
+		AnalyticsCloudClient analyticsCloudClient = new AnalyticsCloudClient(
+			_http);
+
+		Group group = _groupService.getGroup(groupId);
+
+		return analyticsCloudClient.getAssetMetric(
+			_analyticsSettingsManager.getAnalyticsConfiguration(
+				group.getCompanyId()),
+			assetId, assetType,
+			Long.valueOf(group.getTypeSettingsProperty("analyticsChannelId")),
+			rangeKey, selectedMetrics);
+	}
+
+	@Reference
+	private AnalyticsSettingsManager _analyticsSettingsManager;
+
+	@Reference
+	private GroupService _groupService;
+
+	@Reference
+	private Http _http;
+
 }
