b686e87d93356724a894c2348f7ab78d59c77015
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-08-27 16:40:11 +0200

    LPD-34450 Override the export import display style since from now is based on displayStyleGroupExternalReferenceCode instead of displayStyleGroupKey

--- modules/apps/site-navigation/site-navigation-test/src/testIntegration/java/com/liferay/site/navigation/menu/web/internal/exportimport/portlet/preferences/processor/test/SiteNavigationMenuExportImportPortletPreferencesProcessorTest.java
+++ modules/apps/site-navigation/site-navigation-test/src/testIntegration/java/com/liferay/site/navigation/menu/web/internal/exportimport/portlet/preferences/processor/test/SiteNavigationMenuExportImportPortletPreferencesProcessorTest.java
@@ -6,6 +6,9 @@
 package com.liferay.site.navigation.menu.web.internal.exportimport.portlet.preferences.processor.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.model.DDMTemplate;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.dynamic.data.mapping.test.util.DDMTemplateTestUtil;
 import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationParameterMapFactoryUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataHandlerKeys;
 import com.liferay.exportimport.kernel.staging.MergeLayoutPrototypesThreadLocal;
@@ -18,8 +21,11 @@ import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
+import com.liferay.portal.kernel.servlet.taglib.ui.LanguageEntry;
+import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
@@ -27,12 +33,14 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portlet.display.template.PortletDisplayTemplate;
 import com.liferay.site.navigation.constants.SiteNavigationMenuPortletKeys;
 import com.liferay.site.navigation.model.SiteNavigationMenu;
 import com.liferay.site.navigation.model.SiteNavigationMenuItem;
@@ -230,6 +238,69 @@ public class SiteNavigationMenuExportImportPortletPreferencesProcessorTest
 					Sites.MERGE_FAIL_COUNT)));
 	}
 
+	@Override
+	protected void testExportImportDisplayStyle(
+			long displayStyleGroupId, String scopeType)
+		throws Exception {
+
+		Group displayStyleGroup = _groupLocalService.getGroup(
+			displayStyleGroupId);
+
+		long classNameId = _portal.getClassNameId(
+			LanguageEntry.class.getName());
+
+		DDMTemplate ddmTemplate = DDMTemplateTestUtil.addTemplate(
+			displayStyleGroup.getGroupId(), classNameId, 0,
+			_portal.getClassNameId(PortletDisplayTemplate.class.getName()),
+			TemplateConstants.LANG_TYPE_FTL, RandomTestUtil.randomString(),
+			_portal.getSiteDefaultLocale(displayStyleGroup));
+
+		PortletPreferences portletPreferences = getImportedPortletPreferences(
+			HashMapBuilder.put(
+				"displayStyle",
+				new String[] {
+					PortletDisplayTemplate.DISPLAY_STYLE_PREFIX +
+						ddmTemplate.getTemplateKey()
+				}
+			).put(
+				"displayStyleGroupExternalReferenceCode",
+				() -> {
+					if (displayStyleGroup.getGroupId() == layout.getGroupId()) {
+						return null;
+					}
+
+					return new String[] {
+						displayStyleGroup.getExternalReferenceCode()
+					};
+				}
+			).build());
+
+		Assert.assertEquals(
+			PortletDisplayTemplate.DISPLAY_STYLE_PREFIX +
+				ddmTemplate.getTemplateKey(),
+			portletPreferences.getValue("displayStyle", null));
+
+		DDMTemplate importedDDMTemplate =
+			_ddmTemplateLocalService.fetchTemplate(
+				layout.getGroupId(), classNameId, ddmTemplate.getTemplateKey());
+
+		String importedDisplayStyleGroupExternalReferenceCode =
+			portletPreferences.getValue(
+				"displayStyleGroupExternalReferenceCode", null);
+
+		if (displayStyleGroup.getGroupId() != layout.getGroupId()) {
+			Assert.assertNull(importedDDMTemplate);
+
+			Assert.assertEquals(
+				displayStyleGroup.getExternalReferenceCode(),
+				importedDisplayStyleGroupExternalReferenceCode);
+		}
+		else {
+			Assert.assertNotNull(importedDDMTemplate);
+			Assert.assertNull(importedDisplayStyleGroupExternalReferenceCode);
+		}
+	}
+
 	private void _publishLayouts() throws Exception {
 		Map<String, String[]> parameterMap =
 			ExportImportConfigurationParameterMapFactoryUtil.
@@ -248,6 +319,12 @@ public class SiteNavigationMenuExportImportPortletPreferencesProcessorTest
 			_liveGroup.getGroupId(), false, parameterMap);
 	}
 
+	@Inject
+	private DDMTemplateLocalService _ddmTemplateLocalService;
+
+	@Inject
+	private GroupLocalService _groupLocalService;
+
 	private Layout _layout;
 
 	@Inject
@@ -255,6 +332,9 @@ public class SiteNavigationMenuExportImportPortletPreferencesProcessorTest
 
 	@DeleteAfterTestRun
 	private Group _liveGroup;
+
+	@Inject
+	private Portal _portal;
 
 	@Inject
 	private PortletPreferencesLocalService _portletPreferencesLocalService;
