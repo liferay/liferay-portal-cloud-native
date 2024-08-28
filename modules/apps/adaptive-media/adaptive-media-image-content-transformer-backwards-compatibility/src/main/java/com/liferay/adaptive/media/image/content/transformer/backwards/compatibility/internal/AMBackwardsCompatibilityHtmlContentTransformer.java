a1ad9f32d12dc7f8d0d7289ec04a12a078e53f6d
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2024-08-28 14:11:37 +0200

    LPD-34907 Match any query param, not only 't'
    Valid URLs may have other query parameters at the first position.
    Accept any, there's no reason why we need to expect `t` to be the
    first.

--- modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/main/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformer.java
+++ modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/main/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformer.java
@@ -282,7 +282,7 @@ public class AMBackwardsCompatibilityHtmlContentTransformer
 
 	private static final Pattern _pattern = Pattern.compile(
 		"((?:/?[^\\s]*)/documents/(\\d+)/(\\d+)/([^/?]+)(?:/([-0-9a-fA-F]+))?" +
-			"(?:\\?t=\\d+)?)|((?:/?[^\\s]*)/documents/(d)/(.*)/" +
+			"(?:\\?.*$)?)|((?:/?[^\\s]*)/documents/(d)/(.*)/" +
 				"([_A-Za-z0-9-]+)?(?:\\?.*$)?)");
 
 	@Reference
