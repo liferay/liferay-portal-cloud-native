486b032137bd83c0bf32b02fa89dbf9b8ff29fcd
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2024-08-28 14:14:34 +0200

    LPD-34907 Add test

--- modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/test/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformerTest.java
+++ modules/apps/adaptive-media/adaptive-media-image-content-transformer-backwards-compatibility/src/test/java/com/liferay/adaptive/media/image/content/transformer/backwards/compatibility/internal/AMBackwardsCompatibilityHtmlContentTransformerTest.java
@@ -154,6 +154,14 @@ public class AMBackwardsCompatibilityHtmlContentTransformerTest {
 	}
 
 	@Test
+	public void testReplacesImageTagsWithQueryParameters() throws Exception {
+		Assert.assertEquals(
+			_CONTENT_PREFIX + "[REPLACED]" + _CONTENT_SUFFIX,
+			_contentTransformer.transform(
+				_CONTENT_WITH_IMAGE_AND_QUERY_PARAMETERS));
+	}
+
+	@Test
 	public void testReplacesImageTagsWithSingleQuotes() throws Exception {
 		Assert.assertEquals(
 			_CONTENT_PREFIX + "[REPLACED]" + _CONTENT_SUFFIX,
@@ -214,6 +222,12 @@ public class AMBackwardsCompatibilityHtmlContentTransformerTest {
 			_CONTENT_PREFIX, "<img src='/documents/d/site_name/sample' />",
 			AMBackwardsCompatibilityHtmlContentTransformerTest._PICTURE_TAG,
 			_CONTENT_SUFFIX);
+
+	private static final String _CONTENT_WITH_IMAGE_AND_QUERY_PARAMETERS =
+		StringBundler.concat(
+			_CONTENT_PREFIX, "<img src=\"/documents/20117/32920/sample.jpg",
+			"/f095aa50-7c0c-ae36-05b6-94a5270085c8?version=1.0&t=",
+			"1724834658363&imageThumbnail=1\" />", _CONTENT_SUFFIX);
 
 	private static final String _CONTENT_WITH_IMAGE_AND_SINGLE_QUOTES =
 		StringBundler.concat(
