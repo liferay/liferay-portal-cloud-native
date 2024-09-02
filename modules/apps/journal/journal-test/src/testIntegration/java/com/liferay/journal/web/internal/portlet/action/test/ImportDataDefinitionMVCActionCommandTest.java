52cc764924f55e4fdf062839f2a2cc9d7381d740
Attila Bakay <attila.bakay@liferay.com>
2024-09-02 15:59:27 +0200

    LPD-33587 Revert "LPS-137937 Add tests"
    This reverts commit d7dcc558d839814bad8f183b9ddc23861102c27a.

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/web/internal/portlet/action/test/ImportDataDefinitionMVCActionCommandTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/web/internal/portlet/action/test/ImportDataDefinitionMVCActionCommandTest.java
@@ -8,8 +8,6 @@ package com.liferay.journal.web.internal.portlet.action.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.data.engine.rest.dto.v2_0.DataDefinition;
 import com.liferay.data.engine.rest.dto.v2_0.DataDefinitionField;
-import com.liferay.data.engine.rest.dto.v2_0.DataLayout;
-import com.liferay.data.engine.rest.dto.v2_0.DataLayoutRow;
 import com.liferay.data.engine.rest.resource.v2_0.DataDefinitionResource;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
@@ -60,7 +58,6 @@ import org.springframework.mock.web.MockMultipartHttpServletRequest;
 
 /**
  * @author Rodrigo Paulino
- * @author Rebeca Silva
  */
 @RunWith(Arquillian.class)
 public class ImportDataDefinitionMVCActionCommandTest {
@@ -73,89 +70,6 @@ public class ImportDataDefinitionMVCActionCommandTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
-	}
-
-	@Test
-	public void testDataLayoutUpdatesWithTheNewFieldNames() throws Exception {
-		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
-			_createMockLiferayPortletActionRequest(
-				"valid_data_definition.json", "Imported Structure");
-
-		_setUpUploadPortletRequest(mockLiferayPortletActionRequest);
-
-		_mvcActionCommand.processAction(
-			mockLiferayPortletActionRequest,
-			new MockLiferayPortletActionResponse());
-
-		DataDefinition dataDefinition = _getImportedDataDefinition();
-
-		DataDefinitionField[] dataDefinitionFields =
-			dataDefinition.getDataDefinitionFields();
-
-		DataLayout dataLayout = dataDefinition.getDefaultDataLayout();
-
-		DataLayoutRow[] dataLayoutRows =
-			dataLayout.getDataLayoutPages()[0].getDataLayoutRows();
-
-		String dataDefinitionlTextFieldName = dataDefinitionFields[0].getName();
-
-		String dataDefinitionFieldsGroupFieldName =
-			dataDefinitionFields[1].getName();
-
-		String dataLayoutTextFieldName =
-			dataLayoutRows[0].getDataLayoutColumns()[0].getFieldNames()[0];
-
-		String dataLayoutFieldsGroupFieldName =
-			dataLayoutRows[1].getDataLayoutColumns()[0].getFieldNames()[0];
-
-		Assert.assertEquals(
-			dataDefinitionlTextFieldName, dataLayoutTextFieldName);
-
-		Assert.assertEquals(
-			dataDefinitionFieldsGroupFieldName, dataLayoutFieldsGroupFieldName);
-	}
-
-	@Test
-	public void testExpectImportedStructureWithUniqueFieldNames()
-		throws Exception {
-
-		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
-			_createMockLiferayPortletActionRequest(
-				"valid_data_definition.json", "Imported Structure");
-
-		_setUpUploadPortletRequest(mockLiferayPortletActionRequest);
-
-		_mvcActionCommand.processAction(
-			mockLiferayPortletActionRequest,
-			new MockLiferayPortletActionResponse());
-
-		DataDefinition dataDefinition = _getImportedDataDefinition();
-
-		DataDefinitionField[] dataDefinitionFields =
-			dataDefinition.getDataDefinitionFields();
-
-		String previousTextFieldName = "Text12293201";
-
-		String previousFieldsGroupFieldName = "FieldsGroup19507604";
-
-		Assert.assertNotEquals(
-			previousTextFieldName, dataDefinitionFields[0].getName());
-
-		Assert.assertNotEquals(
-			previousFieldsGroupFieldName, dataDefinitionFields[1].getName());
-
-		DataDefinitionField[] nestedDataDefinitionFields =
-			dataDefinitionFields[1].getNestedDataDefinitionFields();
-
-		String previousNumericFieldName = "Numeric34461674";
-
-		String previousDateFieldName = "Date46675757";
-
-		Assert.assertNotEquals(
-			previousNumericFieldName, nestedDataDefinitionFields[0].getName());
-
-		Assert.assertNotEquals(
-			previousDateFieldName, nestedDataDefinitionFields[1].getName());
 	}
 
 	@Test
