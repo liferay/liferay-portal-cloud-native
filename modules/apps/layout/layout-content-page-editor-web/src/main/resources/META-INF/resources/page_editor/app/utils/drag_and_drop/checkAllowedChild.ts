9eb6e07c632dc9bed6e3281c2ebcdc352a8d11da
Sandro Chinea <sandrochinea@gmail.com>
2024-09-05 13:23:17 +0100

    LPD-34318 Migrate checkAllowedChild to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/drag_and_drop/checkAllowedChild.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/drag_and_drop/checkAllowedChild.ts
@@ -3,6 +3,15 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {
+	FragmentEntryLink,
+	FragmentEntryLinkMap,
+} from '../../../app/actions/addFragmentEntryLinks';
+import {FormLayoutDataItem} from '../../../types/layout_data/FormLayoutDataItem';
+import {
+	LayoutData,
+	LayoutDataItem,
+} from '../../../types/layout_data/LayoutData';
 import {FRAGMENT_ENTRY_TYPES} from '../../config/constants/fragmentEntryTypes';
 import {LAYOUT_DATA_ITEM_TYPES} from '../../config/constants/layoutDataItemTypes';
 import {getStepperChild} from '../../utils/getStepperChild';
@@ -11,8 +20,14 @@ import {getFormParent} from '../getFormParent';
 import {isMultistepForm} from '../isMultistepForm';
 import {isUnmappedCollection} from '../isUnmappedCollection';
 
+type DragAndDropItem = LayoutDataItem & {
+	fieldTypes: FragmentEntryLink['fieldTypes'];
+	fragmentEntryType: FragmentEntryLink['fragmentEntryType'];
+	isWidget: boolean;
+};
+
 const LAYOUT_DATA_CHECK_ALLOWED_CHILDREN = {
-	[LAYOUT_DATA_ITEM_TYPES.root]: (child) =>
+	[LAYOUT_DATA_ITEM_TYPES.root]: (child: LayoutDataItem) =>
 		[
 			LAYOUT_DATA_ITEM_TYPES.collection,
 			LAYOUT_DATA_ITEM_TYPES.dropZone,
@@ -20,17 +35,17 @@ const LAYOUT_DATA_CHECK_ALLOWED_CHILDREN = {
 			LAYOUT_DATA_ITEM_TYPES.row,
 			LAYOUT_DATA_ITEM_TYPES.fragment,
 			LAYOUT_DATA_ITEM_TYPES.form,
-		].includes(child.type),
+		].some((type) => type === child.type),
 	[LAYOUT_DATA_ITEM_TYPES.collection]: () => false,
-	[LAYOUT_DATA_ITEM_TYPES.collectionItem]: (child) =>
+	[LAYOUT_DATA_ITEM_TYPES.collectionItem]: (child: LayoutDataItem) =>
 		[
 			LAYOUT_DATA_ITEM_TYPES.collection,
 			LAYOUT_DATA_ITEM_TYPES.container,
 			LAYOUT_DATA_ITEM_TYPES.row,
 			LAYOUT_DATA_ITEM_TYPES.fragment,
-		].includes(child.type),
+		].some((type) => type === child.type),
 	[LAYOUT_DATA_ITEM_TYPES.dropZone]: () => false,
-	[LAYOUT_DATA_ITEM_TYPES.container]: (child) =>
+	[LAYOUT_DATA_ITEM_TYPES.container]: (child: LayoutDataItem) =>
 		[
 			LAYOUT_DATA_ITEM_TYPES.collection,
 			LAYOUT_DATA_ITEM_TYPES.container,
@@ -38,18 +53,21 @@ const LAYOUT_DATA_CHECK_ALLOWED_CHILDREN = {
 			LAYOUT_DATA_ITEM_TYPES.row,
 			LAYOUT_DATA_ITEM_TYPES.fragment,
 			LAYOUT_DATA_ITEM_TYPES.form,
-		].includes(child.type),
-	[LAYOUT_DATA_ITEM_TYPES.form]: (child, parent) =>
-		formIsMapped(parent)
+		].some((type) => type === child.type),
+	[LAYOUT_DATA_ITEM_TYPES.form]: (
+		child: LayoutDataItem,
+		parent: LayoutDataItem
+	) =>
+		formIsMapped(parent as FormLayoutDataItem)
 			? [
 					LAYOUT_DATA_ITEM_TYPES.collection,
 					LAYOUT_DATA_ITEM_TYPES.container,
 					LAYOUT_DATA_ITEM_TYPES.dropZone,
 					LAYOUT_DATA_ITEM_TYPES.row,
 					LAYOUT_DATA_ITEM_TYPES.fragment,
-				].includes(child.type)
+				].some((type) => type === child.type)
 			: false,
-	[LAYOUT_DATA_ITEM_TYPES.formStep]: (child) =>
+	[LAYOUT_DATA_ITEM_TYPES.formStep]: (child: LayoutDataItem) =>
 		[
 			LAYOUT_DATA_ITEM_TYPES.collection,
 			LAYOUT_DATA_ITEM_TYPES.container,
@@ -57,12 +75,12 @@ const LAYOUT_DATA_CHECK_ALLOWED_CHILDREN = {
 			LAYOUT_DATA_ITEM_TYPES.row,
 			LAYOUT_DATA_ITEM_TYPES.fragment,
 			LAYOUT_DATA_ITEM_TYPES.form,
-		].includes(child.type),
-	[LAYOUT_DATA_ITEM_TYPES.formStepContainer]: (child) =>
-		[LAYOUT_DATA_ITEM_TYPES.formStep].includes(child.type),
-	[LAYOUT_DATA_ITEM_TYPES.row]: (child) =>
-		[LAYOUT_DATA_ITEM_TYPES.column].includes(child.type),
-	[LAYOUT_DATA_ITEM_TYPES.column]: (child) =>
+		].some((type) => type === child.type),
+	[LAYOUT_DATA_ITEM_TYPES.formStepContainer]: (child: LayoutDataItem) =>
+		[LAYOUT_DATA_ITEM_TYPES.formStep].some((type) => type === child.type),
+	[LAYOUT_DATA_ITEM_TYPES.row]: (child: LayoutDataItem) =>
+		[LAYOUT_DATA_ITEM_TYPES.column].some((type) => type === child.type),
+	[LAYOUT_DATA_ITEM_TYPES.column]: (child: LayoutDataItem) =>
 		[
 			LAYOUT_DATA_ITEM_TYPES.collection,
 			LAYOUT_DATA_ITEM_TYPES.container,
@@ -70,9 +88,9 @@ const LAYOUT_DATA_CHECK_ALLOWED_CHILDREN = {
 			LAYOUT_DATA_ITEM_TYPES.row,
 			LAYOUT_DATA_ITEM_TYPES.fragment,
 			LAYOUT_DATA_ITEM_TYPES.form,
-		].includes(child.type),
+		].some((type) => type === child.type),
 	[LAYOUT_DATA_ITEM_TYPES.fragment]: () => false,
-	[LAYOUT_DATA_ITEM_TYPES.fragmentDropZone]: (child) =>
+	[LAYOUT_DATA_ITEM_TYPES.fragmentDropZone]: (child: LayoutDataItem) =>
 		[
 			LAYOUT_DATA_ITEM_TYPES.collection,
 			LAYOUT_DATA_ITEM_TYPES.dropZone,
@@ -80,21 +98,17 @@ const LAYOUT_DATA_CHECK_ALLOWED_CHILDREN = {
 			LAYOUT_DATA_ITEM_TYPES.row,
 			LAYOUT_DATA_ITEM_TYPES.fragment,
 			LAYOUT_DATA_ITEM_TYPES.form,
-		].includes(child.type),
+		].some((type) => type === child.type),
 };
 
 /**
  * Checks if the given child can be nested inside given parent
- * @param {object} child
- * @param {object} parent
- * @param {{current: object}} layoutDataRef
- * @return {boolean}
  */
 export default function checkAllowedChild(
-	child,
-	parent,
-	layoutDataRef,
-	fragmentEntryLinksRef
+	child: DragAndDropItem,
+	parent: DragAndDropItem,
+	layoutDataRef: React.RefObject<LayoutData>,
+	fragmentEntryLinksRef: React.RefObject<FragmentEntryLinkMap>
 ) {
 	if (isUnmappedCollection(parent) || isUnmappedForm(parent)) {
 		return false;
@@ -144,6 +158,6 @@ export default function checkAllowedChild(
 	return LAYOUT_DATA_CHECK_ALLOWED_CHILDREN[parent.type](child, parent);
 }
 
-function isUnmappedForm(item) {
+function isUnmappedForm(item: LayoutDataItem) {
 	return item.type === LAYOUT_DATA_ITEM_TYPES.form && !formIsMapped(item);
 }
