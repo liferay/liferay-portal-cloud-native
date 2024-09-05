5db8fab41688ea526c61b2ac19e9c42fdda5c99b
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2024-09-05 17:15:43 +0200

    LPD-35645 Implement header ID extension and make test pass

--- modules/apps/knowledge-base/knowledge-base-markdown-converter-impl/src/main/java/com/liferay/knowledge/base/markdown/converter/internal/FlexmarkMarkdownConverter.java
+++ modules/apps/knowledge-base/knowledge-base-markdown-converter-impl/src/main/java/com/liferay/knowledge/base/markdown/converter/internal/FlexmarkMarkdownConverter.java
@@ -7,10 +7,17 @@ package com.liferay.knowledge.base.markdown.converter.internal;
 
 import com.liferay.knowledge.base.markdown.converter.MarkdownConverter;
 
+import com.vladsch.flexmark.ext.attributes.AttributesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 
 import java.io.IOException;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Adolfo Pérez
@@ -18,16 +25,39 @@ import java.io.IOException;
 public class FlexmarkMarkdownConverter implements MarkdownConverter {
 
 	public FlexmarkMarkdownConverter() {
+		MutableDataSet htmlRendererMutableDataSet = new MutableDataSet();
+
+		htmlRendererMutableDataSet.set(HtmlRenderer.RENDER_HEADER_ID, true);
+
 		_htmlRenderer = HtmlRenderer.builder(
+			htmlRendererMutableDataSet
 		).build();
+
+		MutableDataSet parserMutableDataSet = new MutableDataSet();
+
+		parserMutableDataSet.set(
+			Parser.EXTENSIONS,
+			(Collection)Arrays.asList(AttributesExtension.create()));
+
 		_parser = Parser.builder(
+			parserMutableDataSet
 		).build();
 	}
 
 	@Override
 	public String convert(String markdown) throws IOException {
-		return _htmlRenderer.render(_parser.parse(markdown));
+		return _htmlRenderer.render(
+			_parser.parse(_replaceHeaderIdWithAttribute(markdown)));
 	}
+
+	private String _replaceHeaderIdWithAttribute(String markdown) {
+		Matcher matcher = _pattern.matcher(markdown);
+
+		return matcher.replaceAll("$1 {#$2}");
+	}
+
+	private static final Pattern _pattern = Pattern.compile(
+		"^(#+.*?)\\[\\]\\(id=(([^\\s]+?))\\)\\s*$");
 
 	private final HtmlRenderer _htmlRenderer;
 	private final Parser _parser;
