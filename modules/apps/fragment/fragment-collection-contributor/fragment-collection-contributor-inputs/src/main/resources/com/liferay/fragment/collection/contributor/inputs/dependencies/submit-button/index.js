834c50ea17e9696a44f507b40faec436539940cc
Sandro Chinea <sandrochinea@gmail.com>
2024-09-10 12:39:57 +0100

    LPD-35634 Don't emit event in edit mode if clicking the editable

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/submit-button/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/submit-button/index.js
@@ -3,7 +3,15 @@ const button = fragmentElement.querySelector(
 );
 
 if (button) {
-	button.addEventListener('click', () => {
+	button.addEventListener('click', ({target}) => {
+		const isEditable =
+			target.hasAttribute('data-lfr-editable-id') ||
+			target.hasAttribute('contenteditable');
+
+		if (isEditable && layoutMode === 'edit') {
+			return;
+		}
+
 		Liferay.fire('formFragment:changeStep', {
 			emitter: fragmentElement,
 			step: configuration.type,
