b72d3024e2f7fc4ee8287660a0e5ddf94c1a7164
Iván Zaera Avellón <ivan.zaera@liferay.com>
2024-09-10 12:41:33 +0200

    LPD-5294 Rework SF rule to account for different file types
    Discriminate among the list of possible file types that can be found at
    https://github.com/liferay/liferay-portal/blob/839e11f2525c8067989ea15f099940d118ea2d2d/modules/util/source-formatter/src/main/java/com/liferay/source/formatter/processor/JSPSourceProcessor.java#L244

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSPIllegalTagsCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSPIllegalTagsCheck.java
@@ -6,6 +6,8 @@
 package com.liferay.source.formatter.check;
 
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.poshi.core.util.StringUtil;
 
 import java.util.List;
 
@@ -23,8 +25,64 @@ public class JSPIllegalTagsCheck extends BaseFileCheck {
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
 
+		String lowerCaseFileName = StringUtil.lowerCase(fileName);
+
 		List<String> illegalTagNames = getAttributeValues(
 			_ILLEGAL_TAG_NAMES_KEY, absolutePath);
+
+		if (lowerCaseFileName.endsWith(".jsp") ||
+			lowerCaseFileName.endsWith(".jspf") ||
+			lowerCaseFileName.endsWith(".jspx")) {
+
+			for (String illegalTagName : illegalTagNames) {
+				int x = -1;
+
+				while (true) {
+					x = content.indexOf(
+						CharPool.LESS_THAN + illegalTagName, x + 1);
+
+					if (x == -1) {
+						break;
+					}
+
+					int nextCharPosition = x + illegalTagName.length() + 1;
+
+					if (nextCharPosition >= content.length()) {
+						break;
+					}
+
+					char nextChar = content.charAt(nextCharPosition);
+
+					if ((nextChar == CharPool.GREATER_THAN) ||
+						(nextChar == CharPool.NEW_LINE) ||
+						(nextChar == CharPool.SPACE)) {
+
+						addMessage(
+							fileName,
+							StringBundler.concat(
+								"Do not use <", illegalTagName, "> tag (use ",
+								"<aui:", illegalTagName, "> instead), see ",
+								"LPD-18227"),
+							getLineNumber(content, x));
+					}
+				}
+			}
+		}
+		else if (lowerCaseFileName.endsWith(".ftl")) {
+			_checkAttribute(
+				"${nonceAttribute}", content, fileName, illegalTagNames);
+		}
+		else if (lowerCaseFileName.endsWith(".vm")) {
+			_checkAttribute(
+				"$nonceAttribute", content, fileName, illegalTagNames);
+		}
+
+		return content;
+	}
+
+	private void _checkAttribute(
+		String attribute, String content, String fileName,
+		List<String> illegalTagNames) {
 
 		for (String illegalTagName : illegalTagNames) {
 			int x = -1;
@@ -36,28 +94,24 @@ public class JSPIllegalTagsCheck extends BaseFileCheck {
 					break;
 				}
 
-				int nextCharPosition = x + illegalTagName.length() + 1;
+				int y = content.indexOf(CharPool.GREATER_THAN, x + 1);
 
-				if (nextCharPosition >= content.length()) {
-					break;
+				if (y == -1) {
+					y = content.length() - 1;
 				}
 
-				char nextChar = content.charAt(nextCharPosition);
+				String tagSubstring = content.substring(x, y);
 
-				if ((nextChar == CharPool.GREATER_THAN) ||
-					(nextChar == CharPool.NEW_LINE) ||
-					(nextChar == CharPool.SPACE)) {
-
+				if (!tagSubstring.contains(attribute)) {
 					addMessage(
 						fileName,
-						"Do not use <" + illegalTagName +
-							"> tag, see LPD-18227",
+						StringBundler.concat(
+							"Tag <", illegalTagName, "> is missing attribute '",
+							attribute, "', see LPD-18227"),
 						getLineNumber(content, x));
 				}
 			}
 		}
-
-		return content;
 	}
 
 	private static final String _ILLEGAL_TAG_NAMES_KEY = "illegalTagNames";
