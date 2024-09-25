7b53e1cc8a33a33306a3ac852e0b136c26b93a6c
Manuele Castro <manuele.castro@liferay.com>
2024-09-25 13:32:28 -0300

    LPD-1634 Remove oldest entry when it reaches the limit

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
@@ -76,7 +76,7 @@ import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -1045,10 +1045,17 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 				return;
 			}
 
-			samlSsoRequestContexts = new HashMap<>();
+			samlSsoRequestContexts = new LinkedHashMap<>();
 
 			httpSession.setAttribute(
 				SamlWebKeys.SAML_SSO_REQUEST_CONTEXT, samlSsoRequestContexts);
+		}
+
+		if (samlSsoRequestContexts.size() > 9) {
+			String oldestKey = (String)samlSsoRequestContexts.keySet(
+			).toArray()[0];
+
+			samlSsoRequestContexts.remove(oldestKey);
 		}
 
 		if (samlSsoRequestContext != null) {
