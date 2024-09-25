877cada6da808dbe637dabe1022f7059c41164ff
Victor Galan <victor.galan@liferay.com>
2024-09-25 14:02:44 +0200

    LPD-36107 Add topper label to the topper empty

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/topper/TopperEmpty.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/topper/TopperEmpty.js
@@ -7,30 +7,56 @@ import classNames from 'classnames';
 import React, {useRef} from 'react';
 
 import {getLayoutDataItemPropTypes} from '../../../prop_types/index';
+import {ITEM_ACTIVATION_ORIGINS} from '../../config/constants/itemActivationOrigins';
+import {
+	useHoverItem,
+	useIsActive,
+	useIsHovered,
+	useSelectItem,
+} from '../../contexts/ControlsContext';
 import {
 	useMovementTarget,
 	useMovementTargetPosition,
 } from '../../contexts/KeyboardMovementContext';
-import {useSelector} from '../../contexts/StoreContext';
+import {useSelector, useSelectorCallback} from '../../contexts/StoreContext';
 import selectCanUpdatePageStructure from '../../selectors/selectCanUpdatePageStructure';
+import selectLayoutDataItemLabel from '../../selectors/selectLayoutDataItemLabel';
 import {TARGET_POSITIONS} from '../../utils/drag_and_drop/constants/targetPositions';
 import {
 	useDropTarget,
 	useIsDroppable,
 } from '../../utils/drag_and_drop/useDragAndDrop';
 import useDropContainerId from '../../utils/useDropContainerId';
+import {TopperLabel} from './TopperLabel';
 
-export default function ({children, ...props}) {
+export default function ({children, item, ...props}) {
 	const canUpdatePageStructure = useSelector(selectCanUpdatePageStructure);
 
+	const isHovered = useIsHovered();
+	const isActive = useIsActive();
+
 	return canUpdatePageStructure ? (
-		<TopperEmpty {...props}>{children}</TopperEmpty>
+		<MemoizedTopperEmpty
+			{...props}
+			isActive={isActive(item.itemId)}
+			isHovered={isHovered(item.itemId)}
+			item={item}
+		>
+			{children}
+		</MemoizedTopperEmpty>
 	) : (
 		children
 	);
 }
 
-function TopperEmpty({children, className, item}) {
+const TopperEmpty = ({
+	children,
+	className,
+	isActive,
+	isHovered,
+	item,
+	itemElement,
+}) => {
 	const containerRef = useRef(null);
 
 	const {isOverTarget, targetPosition, targetRef} = useDropTarget(item);
@@ -48,6 +74,14 @@ function TopperEmpty({children, className, item}) {
 	const isValidDrop =
 		(isDroppable && isOverTarget) || movementTargetItemId === item.itemId;
 
+	const name = useSelectorCallback(
+		(state) => selectLayoutDataItemLabel(state, item),
+		[item]
+	);
+
+	const hoverItem = useHoverItem();
+	const selectItem = useSelectItem();
+
 	return React.Children.map(realChildren, (child) => {
 		if (!child) {
 			return child;
@@ -62,6 +96,7 @@ function TopperEmpty({children, className, item}) {
 						className,
 						'page-editor__topper',
 						{
+							'active': isActive,
 							'drag-over-bottom':
 								isValidDrop &&
 								dropTargetPosition === TARGET_POSITIONS.BOTTOM,
@@ -72,8 +107,32 @@ function TopperEmpty({children, className, item}) {
 								isValidDrop &&
 								dropTargetPosition === TARGET_POSITIONS.TOP,
 							'drop-container': dropContainerId === item.itemId,
+							'hovered': isHovered,
 						}
 					),
+					onClick: (event) => {
+						event.stopPropagation();
+
+						selectItem(item.itemId, {
+							origin: ITEM_ACTIVATION_ORIGINS.layout,
+						});
+					},
+					onMouseLeave: (event) => {
+						event.stopPropagation();
+
+						if (isHovered) {
+							hoverItem(null, {
+								origin: ITEM_ACTIVATION_ORIGINS.layout,
+							});
+						}
+					},
+					onMouseOver: (event) => {
+						event.stopPropagation();
+
+						hoverItem(item.itemId, {
+							origin: ITEM_ACTIVATION_ORIGINS.layout,
+						});
+					},
 					ref: (node) => {
 						containerRef.current = node;
 						targetRef(node);
@@ -88,10 +147,22 @@ function TopperEmpty({children, className, item}) {
 						}
 					},
 				})}
+
+				{isActive ? (
+					<TopperLabel itemElement={itemElement}>
+						<ul className="tbar-nav">
+							<li className="d-inline-block page-editor__topper__item page-editor__topper__title tbar-item tbar-item-expand">
+								{name}
+							</li>
+						</ul>
+					</TopperLabel>
+				) : null}
 			</>
 		);
 	});
-}
+};
+
+const MemoizedTopperEmpty = React.memo(TopperEmpty);
 
 TopperEmpty.propTypes = {
 	item: getLayoutDataItemPropTypes().isRequired,
