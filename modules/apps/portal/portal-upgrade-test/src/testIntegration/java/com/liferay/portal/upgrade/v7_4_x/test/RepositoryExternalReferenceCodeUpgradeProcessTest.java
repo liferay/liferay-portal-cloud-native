342654ddca7a2e47190634d54ddfea0e813a9c81
Alicia García <alicia.garcia@liferay.com>
2024-09-25 14:21:35 +0200

    LPD-36744 test for upgrade process

---
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/v7_4_x/test/RepositoryExternalReferenceCodeUpgradeProcessTest.java
@@ -0,0 +1,137 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.v7_4_x.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ExternalReferenceCodeModel;
+import com.liferay.portal.kernel.model.Repository;
+import com.liferay.portal.kernel.service.RepositoryLocalService;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.BaseExternalReferenceCodeUpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.version.Version;
+import com.liferay.portal.repository.liferayrepository.LiferayRepository;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.BaseExternalReferenceCodeUpgradeProcessTestCase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Alicia García
+ */
+@RunWith(Arquillian.class)
+public class RepositoryExternalReferenceCodeUpgradeProcessTest
+	extends BaseExternalReferenceCodeUpgradeProcessTestCase {
+
+	@Override
+	protected ExternalReferenceCodeModel[] addExternalReferenceCodeModels(
+			String tableName)
+		throws PortalException {
+
+		Repository repository = _repositoryLocalService.addRepository(
+			TestPropsValues.getUserId(), group.getGroupId(),
+			_portal.getClassNameId(LiferayRepository.class.getName()),
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), new UnicodeProperties(), true,
+			ServiceContextTestUtil.getServiceContext(group.getGroupId()));
+
+		return new ExternalReferenceCodeModel[] {repository};
+	}
+
+	@Override
+	protected void assertExternalReferenceCode(
+			String[] externalReferenceCodes, String tableName)
+		throws Exception {
+
+		try (Connection connection = dataSource.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(
+				StringBundler.concat(
+					"select 1 from ", tableName,
+					" where externalReferenceCode in ('",
+					StringUtil.merge(externalReferenceCodes, "', '"), "')"))) {
+
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+				Assert.assertFalse(resultSet.next());
+			}
+		}
+	}
+
+	@Override
+	protected ExternalReferenceCodeModel fetchExternalReferenceCodeModel(
+		ExternalReferenceCodeModel externalReferenceCodeModel,
+		String tableName) {
+
+		Repository repository = (Repository)externalReferenceCodeModel;
+
+		return _repositoryLocalService.fetchRepository(
+			repository.getRepositoryId());
+	}
+
+	@Override
+	protected String[] getTableNames() {
+		return new String[] {"Repository"};
+	}
+
+	@Override
+	protected UpgradeProcess getUpgradeProcess() {
+		return new BaseExternalReferenceCodeUpgradeProcess() {
+
+			@Override
+			protected String[][] getTableAndPrimaryKeyColumnNames() {
+				return new String[][] {{"Repository", "repositoryId"}};
+			}
+
+		};
+	}
+
+	@Override
+	protected UpgradeStepRegistrator getUpgradeStepRegistrator() {
+		return null;
+	}
+
+	@Override
+	protected Version getVersion() {
+		return null;
+	}
+
+	@Override
+	protected void updateExternalReferenceCode(
+			String[] externalReferenceCodes, String tableName)
+		throws Exception {
+
+		db.runSQL(
+			StringBundler.concat(
+				"update ", tableName,
+				" set externalReferenceCode = null where ",
+				"externalReferenceCode in ('",
+				StringUtil.merge(externalReferenceCodes, "', '"), "')"));
+
+		entityCache.clearCache();
+		multiVMPool.clear();
+	}
+
+	@Inject
+	private Portal _portal;
+
+	@Inject
+	private RepositoryLocalService _repositoryLocalService;
+
+}
