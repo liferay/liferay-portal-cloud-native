liferay-search/liferay-portal

LPD-28674 LPD-31225 search-experiences-test: add upgrade test
https://liferay.atlassian.net/browse/LPD-28674

modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/upgrade/v3_1_3/test/SXPElementUpgradeProcessTest.java
@@ -1,0 +1,99 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.search.experiences.internal.upgrade.v3_1_3.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+import com.liferay.search.experiences.model.SXPElement;
+import com.liferay.search.experiences.service.SXPElementLocalService;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Joshua Cords
+ */
+@RunWith(Arquillian.class)
+public class SXPElementUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Test
+	public void testSXPElementUpgradeProcess() throws Exception {
+		SXPElement sxpElement =
+			_sxpElementLocalService.fetchSXPElementByExternalReferenceCode(
+				"LIMIT_SEARCH_TO_THESE_SITES", TestPropsValues.getCompanyId());
+
+		sxpElement.setElementDefinitionJSON("{old value");
+
+		_sxpElementLocalService.updateSXPElement(sxpElement);
+
+		_runUpgrade("v3_1_3.SXPElementUpgradeProcess");
+
+		sxpElement =
+			_sxpElementLocalService.fetchSXPElementByExternalReferenceCode(
+				"LIMIT_SEARCH_TO_THESE_SITES", TestPropsValues.getCompanyId());
+
+		Assert.assertEquals(
+			sxpElement.getElementDefinitionJSON(),
+			_getExpectedElementDefinitionJSON());
+	}
+
+	@Rule
+	public TestName testName = new TestName();
+
+	private String _getExpectedElementDefinitionJSON() {
+		Class<?> clazz = getClass();
+
+		return StringUtil.read(
+			clazz,
+			StringBundler.concat(
+				"dependencies/", clazz.getSimpleName(), StringPool.PERIOD,
+				testName.getMethodName(), ".txt"));
+	}
+
+	private void _runUpgrade(String name) throws Exception {
+		UpgradeProcess upgradeProcess = UpgradeTestUtil.getUpgradeStep(
+			_upgradeStepRegistrator,
+			"com.liferay.search.experiences.internal.upgrade." + name);
+
+		upgradeProcess.upgrade();
+
+		_multiVMPool.clear();
+	}
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.search.experiences.internal.upgrade.registry.SXPServiceUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
+
+	@Inject
+	private SXPElementLocalService _sxpElementLocalService;
+
+}

liferay/liferay-portal-cloud-native

modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/upgrade/v3_1_3/test/SXPElementUpgradeProcessTest.java
@@ -1,0 +1,99 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.search.experiences.internal.upgrade.v3_1_3.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+import com.liferay.search.experiences.model.SXPElement;
+import com.liferay.search.experiences.service.SXPElementLocalService;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Joshua Cords
+ */
+@RunWith(Arquillian.class)
+public class SXPElementUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Test
+	public void testSXPElementUpgradeProcess() throws Exception {
+		SXPElement sxpElement =
+			_sxpElementLocalService.fetchSXPElementByExternalReferenceCode(
+				"LIMIT_SEARCH_TO_THESE_SITES", TestPropsValues.getCompanyId());
+
+		sxpElement.setElementDefinitionJSON("{old value");
+
+		_sxpElementLocalService.updateSXPElement(sxpElement);
+
+		_runUpgrade("v3_1_3.SXPElementUpgradeProcess");
+
+		sxpElement =
+			_sxpElementLocalService.fetchSXPElementByExternalReferenceCode(
+				"LIMIT_SEARCH_TO_THESE_SITES", TestPropsValues.getCompanyId());
+
+		Assert.assertEquals(
+			sxpElement.getElementDefinitionJSON(),
+			_getExpectedElementDefinitionJSON());
+	}
+
+	@Rule
+	public TestName testName = new TestName();
+
+	private String _getExpectedElementDefinitionJSON() {
+		Class<?> clazz = getClass();
+
+		return StringUtil.read(
+			clazz,
+			StringBundler.concat(
+				"dependencies/", clazz.getSimpleName(), StringPool.PERIOD,
+				testName.getMethodName(), ".json"));
+	}
+
+	private void _runUpgrade(String name) throws Exception {
+		UpgradeProcess upgradeProcess = UpgradeTestUtil.getUpgradeStep(
+			_upgradeStepRegistrator,
+			"com.liferay.search.experiences.internal.upgrade." + name);
+
+		upgradeProcess.upgrade();
+
+		_multiVMPool.clear();
+	}
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.search.experiences.internal.upgrade.registry.SXPServiceUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
+
+	@Inject
+	private SXPElementLocalService _sxpElementLocalService;
+
+}