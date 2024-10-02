aa8b105f269e99bb3ff17249c88f0fa0dfe95da4
Iván Zaera Avellón <ivan.zaera@liferay.com>
2024-10-02 11:34:56 +0200

    LPD-36212 SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/CSPComplianceCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/CSPComplianceCheck.java
@@ -74,11 +74,8 @@ public class CSPComplianceCheck extends BaseTagAttributesCheck {
 					break;
 				}
 
-				if (x == 0) {
-					continue;
-				}
-
-				if (!Character.isWhitespace(content.charAt(x - 1)) ||
+				if ((x == 0) ||
+					!Character.isWhitespace(content.charAt(x - 1)) ||
 					isJavaSource(content, x)) {
 
 					continue;
