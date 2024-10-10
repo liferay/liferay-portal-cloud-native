8261c2246ead77c917c895c57b3f4edacb2ecee7
Alessio Rendina <asilcomando@gmail.com>
2024-10-10 16:41:59 +0200

    LPD-34399 Added quick checkout transition

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/resource/v1_0/CartTransitionResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/resource/v1_0/CartTransitionResourceImpl.java
@@ -6,17 +6,33 @@
 package com.liferay.headless.commerce.delivery.cart.internal.resource.v1_0;
 
 import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.commerce.context.CommerceContextFactory;
 import com.liferay.commerce.exception.CommerceOrderStatusException;
 import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderItem;
+import com.liferay.commerce.model.CommerceShippingEngine;
+import com.liferay.commerce.model.CommerceShippingMethod;
+import com.liferay.commerce.model.CommerceShippingOption;
 import com.liferay.commerce.order.engine.CommerceOrderEngine;
 import com.liferay.commerce.order.status.CommerceOrderStatus;
 import com.liferay.commerce.order.status.CommerceOrderStatusRegistry;
+import com.liferay.commerce.payment.model.CommercePaymentMethodGroupRel;
+import com.liferay.commerce.payment.service.CommercePaymentMethodGroupRelLocalService;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
 import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.commerce.service.CommerceShippingMethodLocalService;
+import com.liferay.commerce.shipping.engine.fixed.model.CommerceShippingFixedOption;
+import com.liferay.commerce.shipping.engine.fixed.service.CommerceShippingFixedOptionLocalService;
+import com.liferay.commerce.term.service.CommerceTermEntryLocalService;
+import com.liferay.commerce.util.CommerceShippingEngineRegistry;
 import com.liferay.commerce.util.CommerceWorkflowedModelHelper;
 import com.liferay.headless.commerce.delivery.cart.dto.v1_0.CartTransition;
 import com.liferay.headless.commerce.delivery.cart.resource.v1_0.CartTransitionResource;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ObjectValuePair;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
@@ -82,6 +98,40 @@ public class CartTransitionResourceImpl extends BaseCartTransitionResourceImpl {
 			else if (commerceOrder.isDraft()) {
 				transitionOVPs.add(new ObjectValuePair<>(0L, "submit"));
 			}
+
+			List<CommerceOrderItem> commerceOrderItems =
+				commerceOrder.getCommerceOrderItems();
+
+			if ((commerceOrder.getBillingAddressId() > 0) &&
+				Validator.isNotNull(
+					commerceOrder.getCommercePaymentMethodKey()) &&
+				!commerceOrderItems.isEmpty() &&
+				_checkShippability(commerceOrder, commerceOrderItems)) {
+
+				int deliveryCommerceTermEntriesCount =
+					_commerceTermEntryLocalService.
+						getDeliveryCommerceTermEntriesCount(
+							contextCompany.getCompanyId(),
+							commerceOrder.getCommerceOrderTypeId(),
+							_getCommerceShippingFixedOptionId(commerceOrder));
+				int paymentCommerceTermEntriesCount =
+					_commerceTermEntryLocalService.
+						getPaymentCommerceTermEntriesCount(
+							contextCompany.getCompanyId(),
+							commerceOrder.getCommerceOrderTypeId(),
+							_getCommercePaymentMethodGroupRelId(commerceOrder));
+
+				if (((deliveryCommerceTermEntriesCount == 0) ||
+					 ((deliveryCommerceTermEntriesCount > 0) &&
+					  (commerceOrder.getDeliveryCommerceTermEntryId() > 0))) &&
+					((paymentCommerceTermEntriesCount == 0) ||
+					 ((paymentCommerceTermEntriesCount > 0) &&
+					  (commerceOrder.getPaymentCommerceTermEntryId() > 0)))) {
+
+					transitionOVPs.add(
+						new ObjectValuePair<>(0L, "quick-checkout"));
+				}
+			}
 		}
 
 		return Page.of(
@@ -133,6 +183,101 @@ public class CartTransitionResourceImpl extends BaseCartTransitionResourceImpl {
 			new ObjectValuePair<>(workflowTaskId, name));
 	}
 
+	private boolean _checkShippability(
+		CommerceOrder commerceOrder,
+		List<CommerceOrderItem> commerceOrderItems) {
+
+		for (CommerceOrderItem commerceOrderItem : commerceOrderItems) {
+			if (commerceOrderItem.isShippable()) {
+				if ((commerceOrder.getShippingAddressId() > 0) &&
+					(commerceOrder.getCommerceShippingMethodId() > 0) &&
+					Validator.isNotNull(
+						commerceOrder.getShippingOptionName())) {
+
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private long _getCommercePaymentMethodGroupRelId(
+		CommerceOrder commerceOrder) {
+
+		CommercePaymentMethodGroupRel commercePaymentMethodGroupRel =
+			_commercePaymentMethodGroupRelLocalService.
+				fetchCommercePaymentMethodGroupRel(
+					commerceOrder.getGroupId(),
+					commerceOrder.getCommercePaymentMethodKey());
+
+		if (commercePaymentMethodGroupRel == null) {
+			return 0;
+		}
+
+		return commercePaymentMethodGroupRel.
+			getCommercePaymentMethodGroupRelId();
+	}
+
+	private long _getCommerceShippingFixedOptionId(CommerceOrder commerceOrder)
+		throws Exception {
+
+		CommerceShippingMethod commerceShippingMethod =
+			_commerceShippingMethodLocalService.fetchCommerceShippingMethod(
+				commerceOrder.getCommerceShippingMethodId());
+
+		if (commerceShippingMethod == null) {
+			return 0;
+		}
+
+		CommerceShippingEngine commerceShippingEngine =
+			_commerceShippingEngineRegistry.getCommerceShippingEngine(
+				commerceShippingMethod.getEngineKey());
+
+		if (commerceShippingEngine == null) {
+			return 0;
+		}
+
+		CommerceChannel commerceChannel =
+			_commerceChannelLocalService.fetchCommerceChannelByGroupClassPK(
+				commerceOrder.getGroupId());
+
+		if (commerceChannel == null) {
+			return 0;
+		}
+
+		for (CommerceShippingOption commerceShippingOption :
+				commerceShippingEngine.getCommerceShippingOptions(
+					_commerceContextFactory.create(
+						contextCompany.getCompanyId(),
+						commerceChannel.getGroupId(), contextUser.getUserId(),
+						commerceOrder.getCommerceOrderId(),
+						commerceOrder.getCommerceAccountId()),
+					commerceOrder,
+					contextAcceptLanguage.getPreferredLocale())) {
+
+			if (StringUtil.equals(
+					commerceOrder.getShippingOptionName(),
+					commerceShippingOption.getKey())) {
+
+				CommerceShippingFixedOption commerceShippingFixedOption =
+					_commerceShippingFixedOptionLocalService.
+						fetchCommerceShippingFixedOption(
+							commerceOrder.getCompanyId(),
+							commerceShippingOption.getKey());
+
+				if (commerceShippingFixedOption != null) {
+					return commerceShippingFixedOption.
+						getCommerceShippingFixedOptionId();
+				}
+			}
+		}
+
+		return 0;
+	}
+
 	private CartTransition _toCartTransition(
 			long commerceOrderId, String comment,
 			ObjectValuePair<Long, String> transitionOVP)
@@ -158,6 +303,12 @@ public class CartTransitionResourceImpl extends BaseCartTransitionResourceImpl {
 		_cartTransitionDTOConverter;
 
 	@Reference
+	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@Reference
+	private CommerceContextFactory _commerceContextFactory;
+
+	@Reference
 	private CommerceOrderEngine _commerceOrderEngine;
 
 	@Reference
@@ -165,6 +316,24 @@ public class CartTransitionResourceImpl extends BaseCartTransitionResourceImpl {
 
 	@Reference
 	private CommerceOrderStatusRegistry _commerceOrderStatusRegistry;
+
+	@Reference
+	private CommercePaymentMethodGroupRelLocalService
+		_commercePaymentMethodGroupRelLocalService;
+
+	@Reference
+	private CommerceShippingEngineRegistry _commerceShippingEngineRegistry;
+
+	@Reference
+	private CommerceShippingFixedOptionLocalService
+		_commerceShippingFixedOptionLocalService;
+
+	@Reference
+	private CommerceShippingMethodLocalService
+		_commerceShippingMethodLocalService;
+
+	@Reference
+	private CommerceTermEntryLocalService _commerceTermEntryLocalService;
 
 	@Reference
 	private CommerceWorkflowedModelHelper _commerceWorkflowedModelHelper;
