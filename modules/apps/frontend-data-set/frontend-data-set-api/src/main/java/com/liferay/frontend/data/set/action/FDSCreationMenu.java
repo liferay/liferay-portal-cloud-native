1c0f64e87c1ec7645c342f1c1e3280c4f81fa942
Daniel Sanz <daniel.sanz@liferay.com>
2024-10-14 14:20:22 +0200

    LPD-42456 Create FDS action API to expose java-based FDS action providers to the Data Set Manager ecosystem. These provide item actions and creation menu in the front end

---
+++ modules/apps/frontend-data-set/frontend-data-set-api/src/main/java/com/liferay/frontend/data/set/action/FDSCreationMenu.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set.action;
+
+import com.liferay.frontend.data.set.FDSEntryItemImportPolicy;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Daniel Sanz
+ */
+public interface FDSCreationMenu {
+
+	public CreationMenu getCreationMenu(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws PortalException;
+
+	public default FDSEntryItemImportPolicy getImportPolicy() {
+		return FDSEntryItemImportPolicy.ITEM_PROXY;
+	}
+
+}
