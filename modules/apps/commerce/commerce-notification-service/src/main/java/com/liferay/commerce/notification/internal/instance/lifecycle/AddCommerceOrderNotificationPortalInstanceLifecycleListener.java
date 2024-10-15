43f725d243b7c3f2608a18a21d3724ec89ef5147
Stefano Motta <117469730+smottal@users.noreply.github.com>
2024-10-15 14:11:25 +0200

    LPD-37064 Update usage

--- modules/apps/commerce/commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/instance/lifecycle/AddCommerceOrderNotificationPortalInstanceLifecycleListener.java
+++ modules/apps/commerce/commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/instance/lifecycle/AddCommerceOrderNotificationPortalInstanceLifecycleListener.java
@@ -24,10 +24,8 @@ import com.liferay.portal.kernel.exception.NoSuchUserException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.role.RoleConstants;
-import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -67,16 +65,14 @@ public class AddCommerceOrderNotificationPortalInstanceLifecycleListener
 	}
 
 	private User _getAdminUser(long companyId) throws Exception {
-		Role role = _roleLocalService.getRole(
-			companyId, RoleConstants.ADMINISTRATOR);
-
-		List<User> users = _userLocalService.getRoleUsers(role.getRoleId());
+		List<User> users = _userLocalService.getUsersByRoleName(
+			companyId, RoleConstants.ADMINISTRATOR, 0, 1);
 
 		if (users.isEmpty()) {
 			throw new NoSuchUserException(
 				StringBundler.concat(
 					"No user exists in company ", companyId, " with role ",
-					role.getName()));
+					RoleConstants.ADMINISTRATOR));
 		}
 
 		return users.get(0);
@@ -197,9 +193,6 @@ public class AddCommerceOrderNotificationPortalInstanceLifecycleListener
 
 	@Reference
 	private ObjectFieldLocalService _objectFieldLocalService;
-
-	@Reference
-	private RoleLocalService _roleLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
