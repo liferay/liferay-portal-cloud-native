fba0397b9a08da73db4ed0203961265ff3548587
Jorge González <jorge.gonzalez@liferay.com>
2024-10-17 16:19:49 +0200

    LPD-39586 SF - Move LPDs to tags

--- modules/test/playwright/tests/message-boards-web/message-boards-web.spec.ts
+++ modules/test/playwright/tests/message-boards-web/message-boards-web.spec.ts
@@ -21,197 +21,206 @@ export const test = mergeTests(
 	workflowPagesTest
 );
 
-test('LPD-25630 Show the status to guest user', async ({
-	messageBoardsEditThreadPage,
-	messageBoardsPage,
-	messageBoardsWidgetPage,
-	page,
-	site,
-	workflowPage,
-}) => {
-	await messageBoardsPage.setGuestCategoryPermissions(site.friendlyUrlPath);
-
-	await messageBoardsEditThreadPage.publishNewBasicThread(
-		'Thread Subject',
-		'Thread Body',
-		site.friendlyUrlPath
-	);
-
-	await workflowPage.goto(site.friendlyUrlPath);
-
-	await workflowPage.changeWorkflow(
-		'Message Boards Message',
-		'Single Approver'
-	);
-
-	const layout = await messageBoardsWidgetPage.addMessageBoardsPortlet(site);
-
-	await messageBoardsWidgetPage.addGuestReply(
+test(
+	'Show the status to guest user',
+	{tag: '@LPD-25630'},
+	async ({
+		messageBoardsEditThreadPage,
+		messageBoardsPage,
+		messageBoardsWidgetPage,
+		page,
 		site,
-		layout,
-		'Submit for Workflow'
-	);
+		workflowPage,
+	}) => {
+		await messageBoardsPage.setGuestCategoryPermissions(
+			site.friendlyUrlPath
+		);
 
-	await expect(page.getByText('Pending')).toBeVisible();
-});
+		await messageBoardsEditThreadPage.publishNewBasicThread(
+			'Thread Subject',
+			'Thread Body',
+			site.friendlyUrlPath
+		);
 
-test('LPD-29524 Search for message board thread by keywords', async ({
-	apiHelpers,
-	messageBoardsWidgetPage,
-	page,
-	site,
-}) => {
-	const messageBoardThread1 =
-		await apiHelpers.headlessDelivery.postMessageBoardThread({
-			articleBody: getRandomString(),
-			headline: getRandomString(),
-			siteId: site.id,
-		});
-	const messageBoardThread2 =
-		await apiHelpers.headlessDelivery.postMessageBoardThread({
-			articleBody: getRandomString(),
-			headline: getRandomString(),
-			siteId: site.id,
-		});
+		await workflowPage.goto(site.friendlyUrlPath);
 
-	await messageBoardsWidgetPage.addMessageBoardsPortlet(site);
+		await workflowPage.changeWorkflow(
+			'Message Boards Message',
+			'Single Approver'
+		);
 
-	await expect(
-		page.getByRole('link', {name: messageBoardThread1.headline})
-	).toBeVisible();
+		const layout =
+			await messageBoardsWidgetPage.addMessageBoardsPortlet(site);
 
-	await expect(
-		page.getByRole('link', {name: messageBoardThread2.headline})
-	).toBeVisible();
+		await messageBoardsWidgetPage.addGuestReply(
+			site,
+			layout,
+			'Submit for Workflow'
+		);
 
-	await page.getByTestId('searchInput').fill(messageBoardThread1.headline);
-	await page.getByTestId('searchButton').click();
-
-	await expect(
-		page.getByRole('link', {name: messageBoardThread1.headline})
-	).toBeVisible();
-
-	await expect(
-		page.getByRole('link', {name: messageBoardThread2.headline})
-	).toBeHidden();
-});
-
-test('LPD-27633 Do not show site in breadcrumb', async ({
-	messageBoardsWidgetPage,
-	page,
-	site,
-}) => {
-	const layout = await messageBoardsWidgetPage.addMessageBoardsPortlet(site);
-
-	const categoryName = getRandomString();
-
-	await messageBoardsWidgetPage.addCategory(site, layout, categoryName);
-
-	const searchMenu = page.locator(
-		'[id="_com_liferay_message_boards_web_portlet_MBPortlet_mbCategoriesSearchContainer_1_menu"]'
-	);
-
-	await searchMenu.waitFor();
-	await searchMenu.click();
-
-	await page.getByRole('menuitem', {name: 'Move'}).click();
-
-	await page.getByRole('button', {name: 'Select'}).click();
-
-	await expect(
-		page
-			.frameLocator('iframe[title="Select Category"]')
-			.getByText(site.name)
-	).toBeHidden();
-});
-
-test('LPD-33132 Posting a Document to Forums', async ({
-	messageBoardsEditThreadPage,
-	page,
-	site,
-}) => {
-	const fileName = 'attachment , file.txt';
-
-	await messageBoardsEditThreadPage.publishNewBasicThread(
-		'Thread Subject',
-		'Thread Body',
-		site.friendlyUrlPath,
-		path.join(__dirname, '/dependencies/' + fileName)
-	);
-
-	await expect(page.locator('li').filter({hasText: fileName})).toBeVisible();
-});
-
-test('LPD-37727 Change delta to a higher value when on last page', async ({
-	apiHelpers,
-	messageBoardsPage,
-	page,
-	site,
-}) => {
-	for (let i = 0; i < 5; i++) {
-		await apiHelpers.headlessDelivery.postMessageBoardThread({
-			articleBody: getRandomString(),
-			headline: 'Thread with headline #' + i,
-			siteId: site.id,
-		});
+		await expect(page.getByText('Pending')).toBeVisible();
 	}
+);
 
-	await messageBoardsPage.goto(site.friendlyUrlPath);
+test(
+	'Search for message board thread by keywords',
+	{tag: '@LPD-29524'},
+	async ({apiHelpers, messageBoardsWidgetPage, page, site}) => {
+		const messageBoardThread1 =
+			await apiHelpers.headlessDelivery.postMessageBoardThread({
+				articleBody: getRandomString(),
+				headline: getRandomString(),
+				siteId: site.id,
+			});
+		const messageBoardThread2 =
+			await apiHelpers.headlessDelivery.postMessageBoardThread({
+				articleBody: getRandomString(),
+				headline: getRandomString(),
+				siteId: site.id,
+			});
 
-	await page.getByLabel('Items per Page').click();
-	await page.getByRole('option', {name: /4\s+Entries per Page/}).click();
+		await messageBoardsWidgetPage.addMessageBoardsPortlet(site);
 
-	await expect(
-		page.getByRole('link', {name: /Thread with headline #\d+/})
-	).toHaveCount(4);
+		await expect(
+			page.getByRole('link', {name: messageBoardThread1.headline})
+		).toBeVisible();
 
-	await page.getByRole('link', {name: /Page\s+2/}).click();
+		await expect(
+			page.getByRole('link', {name: messageBoardThread2.headline})
+		).toBeVisible();
 
-	await expect(
-		page.getByRole('link', {name: /Thread with headline #\d+/})
-	).toHaveCount(1);
+		await page
+			.getByTestId('searchInput')
+			.fill(messageBoardThread1.headline);
+		await page.getByTestId('searchButton').click();
 
-	await page.getByLabel('Items per Page').click();
-	await page.getByRole('option', {name: /8\s+Entries per Page/}).click();
+		await expect(
+			page.getByRole('link', {name: messageBoardThread1.headline})
+		).toBeVisible();
 
-	await expect(
-		page.getByRole('link', {name: /Thread with headline #\d+/})
-	).toHaveCount(5);
-});
-
-test('LPD-39570 Change delta to a higher value when on last page', async ({
-	apiHelpers,
-	messageBoardsWidgetPage,
-	page,
-	site,
-}) => {
-	for (let i = 0; i < 5; i++) {
-		await apiHelpers.headlessDelivery.postMessageBoardThread({
-			articleBody: getRandomString(),
-			headline: 'Thread with headline #' + i,
-			siteId: site.id,
-		});
+		await expect(
+			page.getByRole('link', {name: messageBoardThread2.headline})
+		).toBeHidden();
 	}
+);
 
-	await messageBoardsWidgetPage.addMessageBoardsPortlet(site);
+test(
+	'Do not show site in breadcrumb',
+	{tag: '@LPD-27633'},
+	async ({messageBoardsWidgetPage, page, site}) => {
+		const layout =
+			await messageBoardsWidgetPage.addMessageBoardsPortlet(site);
 
-	await page.getByLabel('Items per Page').click();
-	await page.getByRole('option', {name: /4\s+Entries per Page/}).click();
+		const categoryName = getRandomString();
 
-	await expect(
-		page.getByRole('link', {name: /Thread with headline #\d+/})
-	).toHaveCount(4);
+		await messageBoardsWidgetPage.addCategory(site, layout, categoryName);
 
-	await page.getByRole('link', {name: /Page\s+2/}).click();
+		const searchMenu = page.locator(
+			'[id="_com_liferay_message_boards_web_portlet_MBPortlet_mbCategoriesSearchContainer_1_menu"]'
+		);
 
-	await expect(
-		page.getByRole('link', {name: /Thread with headline #\d+/})
-	).toHaveCount(1);
+		await searchMenu.waitFor();
+		await searchMenu.click();
 
-	await page.getByLabel('Items per Page').click();
-	await page.getByRole('option', {name: /8\s+Entries per Page/}).click();
+		await page.getByRole('menuitem', {name: 'Move'}).click();
 
-	await expect(
-		page.getByRole('link', {name: /Thread with headline #\d+/})
-	).toHaveCount(5);
-});
+		await page.getByRole('button', {name: 'Select'}).click();
+
+		await expect(
+			page
+				.frameLocator('iframe[title="Select Category"]')
+				.getByText(site.name)
+		).toBeHidden();
+	}
+);
+
+test(
+	'Posting a Document to Forums',
+	{tag: '@LPD-33132'},
+	async ({messageBoardsEditThreadPage, page, site}) => {
+		const fileName = 'attachment , file.txt';
+
+		await messageBoardsEditThreadPage.publishNewBasicThread(
+			'Thread Subject',
+			'Thread Body',
+			site.friendlyUrlPath,
+			path.join(__dirname, '/dependencies/' + fileName)
+		);
+
+		await expect(
+			page.locator('li').filter({hasText: fileName})
+		).toBeVisible();
+	}
+);
+
+test(
+	'Change delta to a higher value when on last page',
+	{tag: '@LPD-37727'},
+	async ({apiHelpers, messageBoardsPage, page, site}) => {
+		for (let i = 0; i < 5; i++) {
+			await apiHelpers.headlessDelivery.postMessageBoardThread({
+				articleBody: getRandomString(),
+				headline: 'Thread with headline #' + i,
+				siteId: site.id,
+			});
+		}
+
+		await messageBoardsPage.goto(site.friendlyUrlPath);
+
+		await page.getByLabel('Items per Page').click();
+		await page.getByRole('option', {name: /4\s+Entries per Page/}).click();
+
+		await expect(
+			page.getByRole('link', {name: /Thread with headline #\d+/})
+		).toHaveCount(4);
+
+		await page.getByRole('link', {name: /Page\s+2/}).click();
+
+		await expect(
+			page.getByRole('link', {name: /Thread with headline #\d+/})
+		).toHaveCount(1);
+
+		await page.getByLabel('Items per Page').click();
+		await page.getByRole('option', {name: /8\s+Entries per Page/}).click();
+
+		await expect(
+			page.getByRole('link', {name: /Thread with headline #\d+/})
+		).toHaveCount(5);
+	}
+);
+
+test(
+	'Change delta to a higher value when on last page',
+	{tag: '@LPD-39570'},
+	async ({apiHelpers, messageBoardsWidgetPage, page, site}) => {
+		for (let i = 0; i < 5; i++) {
+			await apiHelpers.headlessDelivery.postMessageBoardThread({
+				articleBody: getRandomString(),
+				headline: 'Thread with headline #' + i,
+				siteId: site.id,
+			});
+		}
+
+		await messageBoardsWidgetPage.addMessageBoardsPortlet(site);
+
+		await page.getByLabel('Items per Page').click();
+		await page.getByRole('option', {name: /4\s+Entries per Page/}).click();
+
+		await expect(
+			page.getByRole('link', {name: /Thread with headline #\d+/})
+		).toHaveCount(4);
+
+		await page.getByRole('link', {name: /Page\s+2/}).click();
+
+		await expect(
+			page.getByRole('link', {name: /Thread with headline #\d+/})
+		).toHaveCount(1);
+
+		await page.getByLabel('Items per Page').click();
+		await page.getByRole('option', {name: /8\s+Entries per Page/}).click();
+
+		await expect(
+			page.getByRole('link', {name: /Thread with headline #\d+/})
+		).toHaveCount(5);
+	}
+);
