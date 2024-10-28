liferay-commerce/liferay-portal

LPD-37906 Upgrade process

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v11_5_3/CommercePermissionUpgradeProcess.java
@@ -1,0 +1,137 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.internal.upgrade.v11_5_3;
+
+import com.liferay.account.constants.AccountRoleConstants;
+import com.liferay.commerce.constants.CommerceOrderActionKeys;
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Crescenzo Rega
+ */
+public class CommercePermissionUpgradeProcess extends UpgradeProcess {
+
+	public CommercePermissionUpgradeProcess(
+		CompanyLocalService companyLocalService,
+		ResourceActionLocalService resourceActionLocalService,
+		ResourcePermissionLocalService resourcePermissionLocalService,
+		RoleLocalService roleLocalService) {
+
+		_companyLocalService = companyLocalService;
+		_resourceActionLocalService = resourceActionLocalService;
+		_resourcePermissionLocalService = resourcePermissionLocalService;
+		_roleLocalService = roleLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		List<String> actionIds = Arrays.asList(
+			CommerceOrderActionKeys.MANAGE_COMMERCE_ORDER_MULTI_SHIPPING);
+
+		_resourceActionLocalService.checkResourceActions(
+			CommerceOrderConstants.RESOURCE_NAME, actionIds);
+
+		_companyLocalService.forEachCompanyId(
+			companyId -> {
+				try {
+					_updateCommerceAccountRoles(
+						companyId,
+						AccountRoleConstants.
+							REQUIRED_ROLE_NAME_ACCOUNT_ADMINISTRATOR,
+						CommerceOrderConstants.RESOURCE_NAME, actionIds);
+					_updateCommerceAccountRoles(
+						companyId, AccountRoleConstants.ROLE_NAME_ACCOUNT_BUYER,
+						CommerceOrderConstants.RESOURCE_NAME, actionIds);
+					_updateCommerceAccountRoles(
+						companyId,
+						AccountRoleConstants.ROLE_NAME_ACCOUNT_ORDER_MANAGER,
+						CommerceOrderConstants.RESOURCE_NAME, actionIds);
+
+					_updateRegularRoles(
+						companyId, "Sales Agent",
+						CommerceOrderConstants.RESOURCE_NAME, actionIds);
+				}
+				catch (Exception exception) {
+					_log.error(exception);
+				}
+			});
+	}
+
+	private void _addResourcePermission(
+			String actionId, long companyId, String primKey,
+			String resourceName, long roleId, int scope)
+		throws PortalException {
+
+		if (!_resourcePermissionLocalService.hasResourcePermission(
+				companyId, resourceName, scope, primKey, roleId, actionId)) {
+
+			_resourcePermissionLocalService.addResourcePermission(
+				companyId, resourceName, scope, primKey, roleId, actionId);
+		}
+	}
+
+	private void _updateCommerceAccountRoles(
+			long companyId, String name, String resourceName,
+			List<String> actionIds)
+		throws PortalException {
+
+		Role role = _roleLocalService.fetchRole(companyId, name);
+
+		if (role == null) {
+			return;
+		}
+
+		for (String actionId : actionIds) {
+			_addResourcePermission(
+				actionId, companyId,
+				String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+				resourceName, role.getRoleId(),
+				ResourceConstants.SCOPE_GROUP_TEMPLATE);
+		}
+	}
+
+	private void _updateRegularRoles(
+			long companyId, String name, String resourceName,
+			List<String> actionIds)
+		throws PortalException {
+
+		Role role = _roleLocalService.fetchRole(companyId, name);
+
+		if (role == null) {
+			return;
+		}
+
+		for (String actionId : actionIds) {
+			_addResourcePermission(
+				actionId, companyId, String.valueOf(companyId), resourceName,
+				role.getRoleId(), ResourceConstants.SCOPE_COMPANY);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommercePermissionUpgradeProcess.class);
+
+	private final CompanyLocalService _companyLocalService;
+	private final ResourceActionLocalService _resourceActionLocalService;
+	private final ResourcePermissionLocalService
+		_resourcePermissionLocalService;
+	private final RoleLocalService _roleLocalService;
+
+}

modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/internal/upgrade/v11_5_3/test/CommercePermissionUpgradeProcessTest.java
@@ -1,0 +1,170 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.internal.upgrade.v11_5_3.test;
+
+import com.liferay.account.constants.AccountRoleConstants;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.constants.CommerceOrderActionKeys;
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Crescenzo Rega
+ */
+@RunWith(Arquillian.class)
+public class CommercePermissionUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testUpgrade() throws Exception {
+		_removeResourcePermissions();
+
+		_runUpgrade();
+
+		Assert.assertTrue(
+			ListUtil.exists(
+				_resourcePermissionLocalService.getRoles(
+					_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE,
+					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+					CommerceOrderActionKeys.
+						MANAGE_COMMERCE_ORDER_MULTI_SHIPPING),
+				role -> StringUtil.equals(
+					role.getName(),
+					AccountRoleConstants.
+						REQUIRED_ROLE_NAME_ACCOUNT_ADMINISTRATOR)));
+		Assert.assertTrue(
+			ListUtil.exists(
+				_resourcePermissionLocalService.getRoles(
+					_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE,
+					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+					CommerceOrderActionKeys.
+						MANAGE_COMMERCE_ORDER_MULTI_SHIPPING),
+				role -> StringUtil.equals(
+					role.getName(),
+					AccountRoleConstants.ROLE_NAME_ACCOUNT_BUYER)));
+		Assert.assertTrue(
+			ListUtil.exists(
+				_resourcePermissionLocalService.getRoles(
+					_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE,
+					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+					CommerceOrderActionKeys.
+						MANAGE_COMMERCE_ORDER_MULTI_SHIPPING),
+				role -> StringUtil.equals(
+					role.getName(),
+					AccountRoleConstants.ROLE_NAME_ACCOUNT_ORDER_MANAGER)));
+	}
+
+	private void _removeResourcePermissions() throws Exception {
+		Role role = _roleLocalService.fetchRole(
+			_group.getCompanyId(),
+			AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_ADMINISTRATOR);
+
+		_resourcePermissionLocalService.removeResourcePermission(
+			_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+			ResourceConstants.SCOPE_GROUP_TEMPLATE,
+			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+			role.getRoleId(),
+			CommerceOrderActionKeys.MANAGE_COMMERCE_ORDER_MULTI_SHIPPING);
+
+		role = _roleLocalService.fetchRole(
+			_group.getCompanyId(),
+			AccountRoleConstants.ROLE_NAME_ACCOUNT_BUYER);
+
+		_resourcePermissionLocalService.removeResourcePermission(
+			_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+			ResourceConstants.SCOPE_GROUP_TEMPLATE,
+			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+			role.getRoleId(),
+			CommerceOrderActionKeys.MANAGE_COMMERCE_ORDER_MULTI_SHIPPING);
+
+		role = _roleLocalService.fetchRole(
+			_group.getCompanyId(),
+			AccountRoleConstants.ROLE_NAME_ACCOUNT_ORDER_MANAGER);
+
+		_resourcePermissionLocalService.removeResourcePermission(
+			_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+			ResourceConstants.SCOPE_GROUP_TEMPLATE,
+			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+			role.getRoleId(),
+			CommerceOrderActionKeys.MANAGE_COMMERCE_ORDER_MULTI_SHIPPING);
+	}
+
+	private void _runUpgrade() throws Exception {
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				_CLASS_NAME, LoggerTestUtil.OFF)) {
+
+			UpgradeProcess upgradeProcess = UpgradeTestUtil.getUpgradeStep(
+				_upgradeStepRegistrator, _CLASS_NAME);
+
+			upgradeProcess.upgrade();
+
+			_multiVMPool.clear();
+		}
+	}
+
+	private static final String _CLASS_NAME =
+		"com.liferay.commerce.internal.upgrade.v11_5_3." +
+			"CommercePermissionUpgradeProcess";
+
+	@Inject
+	private static ResourcePermissionLocalService
+		_resourcePermissionLocalService;
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.commerce.internal.upgrade.registry.CommerceServiceUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
+
+	@Inject
+	private RoleLocalService _roleLocalService;
+
+}

liferay/liferay-portal-cloud-native

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v11_5_3/CommercePermissionUpgradeProcess.java
@@ -1,0 +1,137 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.internal.upgrade.v11_5_3;
+
+import com.liferay.account.constants.AccountRoleConstants;
+import com.liferay.commerce.constants.CommerceOrderActionKeys;
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Crescenzo Rega
+ */
+public class CommercePermissionUpgradeProcess extends UpgradeProcess {
+
+	public CommercePermissionUpgradeProcess(
+		CompanyLocalService companyLocalService,
+		ResourceActionLocalService resourceActionLocalService,
+		ResourcePermissionLocalService resourcePermissionLocalService,
+		RoleLocalService roleLocalService) {
+
+		_companyLocalService = companyLocalService;
+		_resourceActionLocalService = resourceActionLocalService;
+		_resourcePermissionLocalService = resourcePermissionLocalService;
+		_roleLocalService = roleLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		List<String> actionIds = Arrays.asList(
+			CommerceOrderActionKeys.MANAGE_COMMERCE_ORDER_MULTISHIPPING);
+
+		_resourceActionLocalService.checkResourceActions(
+			CommerceOrderConstants.RESOURCE_NAME, actionIds);
+
+		_companyLocalService.forEachCompanyId(
+			companyId -> {
+				try {
+					_updateCommerceAccountRoles(
+						companyId,
+						AccountRoleConstants.
+							REQUIRED_ROLE_NAME_ACCOUNT_ADMINISTRATOR,
+						CommerceOrderConstants.RESOURCE_NAME, actionIds);
+					_updateCommerceAccountRoles(
+						companyId, AccountRoleConstants.ROLE_NAME_ACCOUNT_BUYER,
+						CommerceOrderConstants.RESOURCE_NAME, actionIds);
+					_updateCommerceAccountRoles(
+						companyId,
+						AccountRoleConstants.ROLE_NAME_ACCOUNT_ORDER_MANAGER,
+						CommerceOrderConstants.RESOURCE_NAME, actionIds);
+
+					_updateRegularRoles(
+						companyId, "Sales Agent",
+						CommerceOrderConstants.RESOURCE_NAME, actionIds);
+				}
+				catch (Exception exception) {
+					_log.error(exception);
+				}
+			});
+	}
+
+	private void _addResourcePermission(
+			String actionId, long companyId, String primKey,
+			String resourceName, long roleId, int scope)
+		throws PortalException {
+
+		if (!_resourcePermissionLocalService.hasResourcePermission(
+				companyId, resourceName, scope, primKey, roleId, actionId)) {
+
+			_resourcePermissionLocalService.addResourcePermission(
+				companyId, resourceName, scope, primKey, roleId, actionId);
+		}
+	}
+
+	private void _updateCommerceAccountRoles(
+			long companyId, String name, String resourceName,
+			List<String> actionIds)
+		throws PortalException {
+
+		Role role = _roleLocalService.fetchRole(companyId, name);
+
+		if (role == null) {
+			return;
+		}
+
+		for (String actionId : actionIds) {
+			_addResourcePermission(
+				actionId, companyId,
+				String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+				resourceName, role.getRoleId(),
+				ResourceConstants.SCOPE_GROUP_TEMPLATE);
+		}
+	}
+
+	private void _updateRegularRoles(
+			long companyId, String name, String resourceName,
+			List<String> actionIds)
+		throws PortalException {
+
+		Role role = _roleLocalService.fetchRole(companyId, name);
+
+		if (role == null) {
+			return;
+		}
+
+		for (String actionId : actionIds) {
+			_addResourcePermission(
+				actionId, companyId, String.valueOf(companyId), resourceName,
+				role.getRoleId(), ResourceConstants.SCOPE_COMPANY);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommercePermissionUpgradeProcess.class);
+
+	private final CompanyLocalService _companyLocalService;
+	private final ResourceActionLocalService _resourceActionLocalService;
+	private final ResourcePermissionLocalService
+		_resourcePermissionLocalService;
+	private final RoleLocalService _roleLocalService;
+
+}

modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/internal/upgrade/v11_5_3/test/CommercePermissionUpgradeProcessTest.java
@@ -1,0 +1,170 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.internal.upgrade.v11_5_3.test;
+
+import com.liferay.account.constants.AccountRoleConstants;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.constants.CommerceOrderActionKeys;
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LoggerTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Crescenzo Rega
+ */
+@RunWith(Arquillian.class)
+public class CommercePermissionUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	public void testUpgrade() throws Exception {
+		_removeResourcePermissions();
+
+		_runUpgrade();
+
+		Assert.assertTrue(
+			ListUtil.exists(
+				_resourcePermissionLocalService.getRoles(
+					_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE,
+					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+					CommerceOrderActionKeys.
+						MANAGE_COMMERCE_ORDER_MULTISHIPPING),
+				role -> StringUtil.equals(
+					role.getName(),
+					AccountRoleConstants.
+						REQUIRED_ROLE_NAME_ACCOUNT_ADMINISTRATOR)));
+		Assert.assertTrue(
+			ListUtil.exists(
+				_resourcePermissionLocalService.getRoles(
+					_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE,
+					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+					CommerceOrderActionKeys.
+						MANAGE_COMMERCE_ORDER_MULTISHIPPING),
+				role -> StringUtil.equals(
+					role.getName(),
+					AccountRoleConstants.ROLE_NAME_ACCOUNT_BUYER)));
+		Assert.assertTrue(
+			ListUtil.exists(
+				_resourcePermissionLocalService.getRoles(
+					_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+					ResourceConstants.SCOPE_GROUP_TEMPLATE,
+					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+					CommerceOrderActionKeys.
+						MANAGE_COMMERCE_ORDER_MULTISHIPPING),
+				role -> StringUtil.equals(
+					role.getName(),
+					AccountRoleConstants.ROLE_NAME_ACCOUNT_ORDER_MANAGER)));
+	}
+
+	private void _removeResourcePermissions() throws Exception {
+		Role role = _roleLocalService.fetchRole(
+			_group.getCompanyId(),
+			AccountRoleConstants.REQUIRED_ROLE_NAME_ACCOUNT_ADMINISTRATOR);
+
+		_resourcePermissionLocalService.removeResourcePermission(
+			_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+			ResourceConstants.SCOPE_GROUP_TEMPLATE,
+			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+			role.getRoleId(),
+			CommerceOrderActionKeys.MANAGE_COMMERCE_ORDER_MULTISHIPPING);
+
+		role = _roleLocalService.fetchRole(
+			_group.getCompanyId(),
+			AccountRoleConstants.ROLE_NAME_ACCOUNT_BUYER);
+
+		_resourcePermissionLocalService.removeResourcePermission(
+			_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+			ResourceConstants.SCOPE_GROUP_TEMPLATE,
+			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+			role.getRoleId(),
+			CommerceOrderActionKeys.MANAGE_COMMERCE_ORDER_MULTISHIPPING);
+
+		role = _roleLocalService.fetchRole(
+			_group.getCompanyId(),
+			AccountRoleConstants.ROLE_NAME_ACCOUNT_ORDER_MANAGER);
+
+		_resourcePermissionLocalService.removeResourcePermission(
+			_group.getCompanyId(), CommerceOrderConstants.RESOURCE_NAME,
+			ResourceConstants.SCOPE_GROUP_TEMPLATE,
+			String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
+			role.getRoleId(),
+			CommerceOrderActionKeys.MANAGE_COMMERCE_ORDER_MULTISHIPPING);
+	}
+
+	private void _runUpgrade() throws Exception {
+		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+				_CLASS_NAME, LoggerTestUtil.OFF)) {
+
+			UpgradeProcess upgradeProcess = UpgradeTestUtil.getUpgradeStep(
+				_upgradeStepRegistrator, _CLASS_NAME);
+
+			upgradeProcess.upgrade();
+
+			_multiVMPool.clear();
+		}
+	}
+
+	private static final String _CLASS_NAME =
+		"com.liferay.commerce.internal.upgrade.v11_5_3." +
+			"CommercePermissionUpgradeProcess";
+
+	@Inject
+	private static ResourcePermissionLocalService
+		_resourcePermissionLocalService;
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.commerce.internal.upgrade.registry.CommerceServiceUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
+
+	@Inject
+	private RoleLocalService _roleLocalService;
+
+}