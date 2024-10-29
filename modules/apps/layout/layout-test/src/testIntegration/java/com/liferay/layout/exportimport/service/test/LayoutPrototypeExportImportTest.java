cff4e79012494ec16ac5f77eb11422e9cfbef02b
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-10-29 13:52:12 +0000

    LPD-39356 Creates a new company

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/exportimport/service/test/LayoutPrototypeExportImportTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/exportimport/service/test/LayoutPrototypeExportImportTest.java
@@ -6,21 +6,39 @@
 package com.liferay.layout.exportimport.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.exportimport.test.util.lar.BaseExportImportTestCase;
+import com.liferay.exportimport.kernel.configuration.ExportImportConfigurationSettingsMapFactoryUtil;
+import com.liferay.exportimport.kernel.configuration.constants.ExportImportConfigurationConstants;
+import com.liferay.exportimport.kernel.lar.PortletDataHandlerKeys;
+import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
+import com.liferay.exportimport.kernel.service.ExportImportConfigurationLocalServiceUtil;
+import com.liferay.exportimport.kernel.service.ExportImportServiceUtil;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.LayoutPrototype;
-import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.LayoutPrototypeLocalService;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.io.File;
+import java.io.Serializable;
+
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -31,7 +49,7 @@ import org.junit.runner.RunWith;
  * @author Eudaldo Alonso
  */
 @RunWith(Arquillian.class)
-public class LayoutPrototypeExportImportTest extends BaseExportImportTestCase {
+public class LayoutPrototypeExportImportTest {
 
 	@ClassRule
 	@Rule
@@ -39,47 +57,122 @@ public class LayoutPrototypeExportImportTest extends BaseExportImportTestCase {
 		new LiferayIntegrationTestRule();
 
 	@Before
-	@Override
 	public void setUp() throws Exception {
-		super.setUp();
-
 		UserTestUtil.setUser(TestPropsValues.getUser());
 
-		_company = _companyLocalService.getCompany(
-			TestPropsValues.getCompanyId());
+		_company = CompanyTestUtil.addCompany();
+
+		ServiceContextThreadLocal.pushServiceContext(
+			ServiceContextTestUtil.getServiceContext(_company.getGroupId()));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		ServiceContextThreadLocal.popServiceContext();
 	}
 
 	@Test
 	public void testExportImport() throws Exception {
-		group = _company.getGroup();
-
-		importedGroup = _company.getGroup();
+		User guestUser = _company.getGuestUser();
 
 		LayoutPrototype layoutPrototype =
 			_layoutPrototypeLocalService.addLayoutPrototype(
-				TestPropsValues.getUserId(), _company.getCompanyId(),
+				guestUser.getUserId(), _company.getCompanyId(),
 				HashMapBuilder.put(
 					LocaleUtil.getDefault(), RandomTestUtil.randomString()
 				).build(),
-				null, true, ServiceContextTestUtil.getServiceContext());
+				null, true,
+				ServiceContextTestUtil.getServiceContext(
+					_company.getCompanyId(), _company.getGroupId(),
+					guestUser.getUserId()));
 
-		exportLayouts(new long[0], getExportParameterMap(), false);
+		Map<String, Serializable> exportLayoutSettingsMap =
+			ExportImportConfigurationSettingsMapFactoryUtil.
+				buildExportLayoutSettingsMap(
+					guestUser, _company.getGroupId(), false, new long[0],
+					_getExportParameterMap());
+
+		ExportImportConfiguration exportImportConfiguration =
+			ExportImportConfigurationLocalServiceUtil.
+				addDraftExportImportConfiguration(
+					guestUser.getUserId(),
+					ExportImportConfigurationConstants.TYPE_EXPORT_LAYOUT,
+					exportLayoutSettingsMap);
+
+		File larFile = ExportImportServiceUtil.exportLayoutsAsFile(
+			exportImportConfiguration);
 
 		_layoutPrototypeLocalService.deleteLayoutPrototype(layoutPrototype);
 
-		importLayouts(getImportParameterMap(), false);
+		Map<String, Serializable> importLayoutSettingsMap =
+			ExportImportConfigurationSettingsMapFactoryUtil.
+				buildImportLayoutSettingsMap(
+					guestUser, _company.getGroupId(), false, null,
+					_getImportParameterMap());
 
-		layoutPrototype = _layoutPrototypeLocalService.getLayoutPrototype(
-			_company.getCompanyId(),
-			layoutPrototype.getName(LocaleUtil.getDefault()));
+		exportImportConfiguration =
+			ExportImportConfigurationLocalServiceUtil.
+				addExportImportConfiguration(
+					guestUser.getUserId(), _company.getGroupId(),
+					StringPool.BLANK, StringPool.BLANK,
+					ExportImportConfigurationConstants.TYPE_IMPORT_LAYOUT,
+					importLayoutSettingsMap, WorkflowConstants.STATUS_DRAFT,
+					ServiceContextTestUtil.getServiceContext(
+						_company.getCompanyId(), _company.getGroupId(),
+						guestUser.getUserId()));
 
-		_layoutPrototypeLocalService.deleteLayoutPrototype(layoutPrototype);
+		ExportImportServiceUtil.importLayouts(
+			exportImportConfiguration, larFile);
+
+		Assert.assertNotNull(
+			_layoutPrototypeLocalService.getLayoutPrototype(
+				_company.getCompanyId(),
+				layoutPrototype.getName(LocaleUtil.getDefault())));
 	}
 
-	private Company _company;
+	private Map<String, String[]> _getExportParameterMap() {
+		return LinkedHashMapBuilder.put(
+			PortletDataHandlerKeys.PORTLET_CONFIGURATION,
+			new String[] {Boolean.TRUE.toString()}
+		).put(
+			PortletDataHandlerKeys.PORTLET_CONFIGURATION_ALL,
+			new String[] {Boolean.TRUE.toString()}
+		).put(
+			PortletDataHandlerKeys.PORTLET_DATA,
+			new String[] {Boolean.TRUE.toString()}
+		).put(
+			PortletDataHandlerKeys.PORTLET_DATA_ALL,
+			new String[] {Boolean.TRUE.toString()}
+		).put(
+			PortletDataHandlerKeys.PORTLET_SETUP_ALL,
+			new String[] {Boolean.TRUE.toString()}
+		).build();
+	}
 
-	@Inject
-	private CompanyLocalService _companyLocalService;
+	private Map<String, String[]> _getImportParameterMap() {
+		return LinkedHashMapBuilder.put(
+			PortletDataHandlerKeys.DATA_STRATEGY,
+			new String[] {PortletDataHandlerKeys.DATA_STRATEGY_MIRROR_OVERWRITE}
+		).put(
+			PortletDataHandlerKeys.PORTLET_CONFIGURATION,
+			new String[] {Boolean.TRUE.toString()}
+		).put(
+			PortletDataHandlerKeys.PORTLET_CONFIGURATION_ALL,
+			new String[] {Boolean.TRUE.toString()}
+		).put(
+			PortletDataHandlerKeys.PORTLET_DATA,
+			new String[] {Boolean.TRUE.toString()}
+		).put(
+			PortletDataHandlerKeys.PORTLET_DATA_ALL,
+			new String[] {Boolean.TRUE.toString()}
+		).put(
+			PortletDataHandlerKeys.PORTLET_SETUP_ALL,
+			new String[] {Boolean.TRUE.toString()}
+		).build();
+	}
+
+	@DeleteAfterTestRun
+	private Company _company;
 
 	@Inject
 	private LayoutPrototypeLocalService _layoutPrototypeLocalService;
