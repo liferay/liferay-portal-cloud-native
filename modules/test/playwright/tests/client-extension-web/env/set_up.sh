8b483f83cdecaf53ddaafeb44a846b924574170e
Thiago Buarque <thiago.buarque@liferay.com>
2024-10-29 15:30:12 -0300

    LPD-36494 import sample workspace properties

---
+++ modules/test/playwright/tests/client-extension-web/env/set_up.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CURRENT_DIR_NAME=$(dirname ${BASH_SOURCE[0]})
+
+echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
+
+source ${CURRENT_DIR_NAME}/../../../env/common.sh
+
+main "${@}"
+
+function main {
+  combine_properties_files \
+    $(get_tomcat_portal_ext_properties_file) \
+    \
+    ${_PORTAL_PROJECT_DIR}/workspaces/liferay-sample-workspace/configs/local/portal-ext.properties
+
+	default_set_up
+}
+
+main "${@}"
