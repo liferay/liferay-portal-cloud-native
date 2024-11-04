98a822445db5eb448c704bc86dd1e5650a21354e
Kyle Miho <kyle.miho@liferay.com>
2024-11-04 08:43:21 -0800

    LPD-37354 Add parameterized tests that can either run a test based on the property issue.key, or every test if issue.key is not set

---
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeCatchAllCheckTest.java
@@ -0,0 +1,174 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.source.formatter.processor;
+
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.json.JSONFactoryImpl;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.SourceFormatterArgs;
+import com.liferay.source.formatter.check.UpgradeCatchAllCheck;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ * @author Kyle Miho
+ */
+@RunWith(Parameterized.class)
+public class UpgradeCatchAllCheckTest extends BaseSourceProcessorTestCase {
+
+	@Parameterized.Parameters(name = "Testcase-{index}: Testing {0} .{1}")
+	public static Iterable<Object[]> data() throws Exception {
+		List<Object[]> tickets = new ArrayList<>();
+
+		String[] issueKeys = StringUtil.split(
+			System.getProperty("issue.key", null), StringPool.COMMA);
+
+		ClassLoader classLoader = UpgradeCatchAllCheck.class.getClassLoader();
+
+		JSONFactoryUtil jsonFactoryUtil = new JSONFactoryUtil();
+
+		jsonFactoryUtil.setJSONFactory(new JSONFactoryImpl());
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray(
+			StringUtil.read(
+				classLoader.getResourceAsStream(
+					"dependencies/replacements.json")));
+
+		for (Object object : jsonArray) {
+			JSONObject jsonObject = (JSONObject)object;
+
+			String issueKey = jsonObject.getString("issueKey");
+
+			if (ArrayUtil.isNotEmpty(issueKeys) &&
+				!ArrayUtil.contains(issueKeys, issueKey)) {
+
+				continue;
+			}
+
+			String[] fileTypes = _getValidExtensions(jsonObject);
+
+			for (String fileType : fileTypes) {
+				if (_hasValidUpgradeFiles(issueKey, fileType)) {
+					tickets.add(new Object[] {issueKey, fileType});
+				}
+			}
+		}
+
+		return tickets;
+	}
+
+	public UpgradeCatchAllCheckTest(String issueKey, String fileType) {
+		_issueKey = issueKey;
+		_fileType = fileType;
+	}
+
+	@Before
+	public void setUp() {
+		UpgradeCatchAllCheck.setTestMode(true);
+		UpgradeCatchAllCheck.setIssueKey(_issueKey);
+	}
+
+	@After
+	public void tearDown() {
+		UpgradeCatchAllCheck.setTestMode(false);
+		UpgradeCatchAllCheck.setIssueKey(null);
+	}
+
+	@Test
+	public void testUpgradeCatchAllCheck() throws Exception {
+		String fileName =
+			StringUtil.replace(_issueKey, CharPool.DASH, CharPool.UNDERLINE) +
+				".test" + _fileType;
+
+		if (_hasValidUpgradeFiles(_issueKey, _fileType)) {
+			_testUpgradeCatchAllCheck(
+				"upgrade/upgrade-catch-all-check/" + fileName);
+		}
+	}
+
+	@Override
+	protected SourceFormatterArgs getSourceFormatterArgs() {
+		List<String> checkCategoryNames = new ArrayList<>();
+
+		checkCategoryNames.add("Upgrade");
+
+		List<String> sourceFormatterProperties = new ArrayList<>();
+
+		sourceFormatterProperties.add(
+			"upgrade.to.liferay.version=" + _UPGRADE_TO_LIFERAY_VERSION);
+		sourceFormatterProperties.add(
+			"upgrade.to.release.version=" + _UPGRADE_TO_RELEASE_VERSION);
+
+		SourceFormatterArgs sourceFormatterArgs =
+			super.getSourceFormatterArgs();
+
+		sourceFormatterArgs.setCheckCategoryNames(checkCategoryNames);
+		sourceFormatterArgs.setJavaParserEnabled(false);
+		sourceFormatterArgs.setSourceFormatterProperties(
+			sourceFormatterProperties);
+
+		return sourceFormatterArgs;
+	}
+
+	private static String[] _getValidExtensions(JSONObject jsonObject) {
+		String[] validExtensions = JSONUtil.toStringArray(
+			jsonObject.getJSONArray("validExtensions"));
+
+		if (ArrayUtil.isEmpty(validExtensions)) {
+			return new String[] {"java"};
+		}
+
+		return validExtensions;
+	}
+
+	private static boolean _hasValidUpgradeFiles(
+		String issueKey, String fileType) {
+
+		String fileName =
+			StringUtil.replace(issueKey, CharPool.DASH, CharPool.UNDERLINE) +
+				".test" + fileType;
+
+		Path filePath = Paths.get(
+			"src/test/resources/com/liferay/source/formatter/dependencies" +
+				"/upgrade/upgrade-catch-all-check/" + fileName);
+
+		return Files.exists(filePath);
+	}
+
+	private void _testUpgradeCatchAllCheck(String fileName) throws Exception {
+		if (fileName.endsWith(".testjava")) {
+			test(fileName, UpgradeCatchAllCheck.getExpectedMessages());
+		}
+		else {
+			test(fileName);
+		}
+	}
+
+	private static final String _UPGRADE_TO_LIFERAY_VERSION = "7.4.13.u27";
+
+	private static final String _UPGRADE_TO_RELEASE_VERSION = "2024.q1.1";
+
+	private final String _fileType;
+	private final String _issueKey;
+
+}
