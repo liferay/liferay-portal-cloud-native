34ebd8b5b7482a65872e2d10765c1121f2f23f59
Javier Moral <javier.moral@liferay.com>
2024-11-04 15:23:06 +0100

    LPD-40360 PageTemplateSet: integration tests

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageTemplateSetResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageTemplateSetResourceTest.java
@@ -6,15 +6,208 @@
 package com.liferay.headless.admin.site.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.admin.site.client.dto.v1_0.PageTemplateSet;
+import com.liferay.layout.page.template.service.LayoutPageTemplateCollectionLocalService;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.FeatureFlags;
+import com.liferay.portal.test.rule.Inject;
 
+import org.junit.Assert;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Rubén Pulido
  */
-@Ignore
+@FeatureFlags("LPD-35443")
 @RunWith(Arquillian.class)
 public class PageTemplateSetResourceTest
 	extends BasePageTemplateSetResourceTestCase {
+
+	@Override
+	@Test
+	public void testDeleteSiteSiteByExternalReferenceCodePageTemplateSet()
+		throws Exception {
+
+		PageTemplateSet pageTemplateSet =
+			testGetSiteSiteByExternalReferenceCodePageTemplateSetsPage_addPageTemplateSet(
+				testGroup.getExternalReferenceCode(), randomPageTemplateSet());
+
+		pageTemplateSetResource.
+			deleteSiteSiteByExternalReferenceCodePageTemplateSet(
+				testGroup.getExternalReferenceCode(),
+				pageTemplateSet.getExternalReferenceCode());
+
+		Assert.assertNull(
+			_layoutPageTemplateCollectionLocalService.
+				fetchLayoutPageTemplateCollectionByExternalReferenceCode(
+					pageTemplateSet.getExternalReferenceCode(),
+					testGroup.getGroupId()));
+	}
+
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodePageTemplateSet()
+		throws Exception {
+
+		PageTemplateSet pageTemplateSet =
+			testGetSiteSiteByExternalReferenceCodePageTemplateSetsPage_addPageTemplateSet(
+				testGroup.getExternalReferenceCode(), randomPageTemplateSet());
+
+		PageTemplateSet getPageTemplateSet =
+			pageTemplateSetResource.
+				getSiteSiteByExternalReferenceCodePageTemplateSet(
+					testGroup.getExternalReferenceCode(),
+					pageTemplateSet.getExternalReferenceCode());
+
+		assertEquals(pageTemplateSet, getPageTemplateSet);
+		assertValid(getPageTemplateSet);
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodePageTemplateSetPermissionsPage()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodePageTemplateSetPermissionsPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodePageTemplateSetsPage()
+		throws Exception {
+
+		super.testGetSiteSiteByExternalReferenceCodePageTemplateSetsPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteByExternalReferenceCodePageTemplateSetsPageWithPagination()
+		throws Exception {
+
+		super.
+			testGetSiteSiteByExternalReferenceCodePageTemplateSetsPageWithPagination();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetSiteSiteExternalReferenceCodePageTemplateSetPermissionsPage()
+		throws Exception {
+
+		super.
+			testGetSiteSiteExternalReferenceCodePageTemplateSetPermissionsPage();
+	}
+
+	@Override
+	@Test
+	public void testPatchSiteSiteByExternalReferenceCodePageTemplateSet()
+		throws Exception {
+
+		PageTemplateSet pageTemplateSet = randomPageTemplateSet();
+
+		pageTemplateSetResource.
+			putSiteSiteByExternalReferenceCodePageTemplateSet(
+				testGroup.getExternalReferenceCode(),
+				pageTemplateSet.getExternalReferenceCode(), pageTemplateSet);
+
+		pageTemplateSet.setName(RandomTestUtil.randomString());
+
+		PageTemplateSet patchPageTemplateSet =
+			pageTemplateSetResource.
+				patchSiteSiteByExternalReferenceCodePageTemplateSet(
+					testGroup.getExternalReferenceCode(),
+					pageTemplateSet.getExternalReferenceCode(),
+					pageTemplateSet);
+
+		assertEquals(pageTemplateSet, patchPageTemplateSet);
+		assertValid(patchPageTemplateSet);
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPostSiteSiteByExternalReferenceCodePageTemplateSet()
+		throws Exception {
+
+		super.testPostSiteSiteByExternalReferenceCodePageTemplateSet();
+	}
+
+	@Override
+	@Test
+	public void testPutSiteSiteByExternalReferenceCodePageTemplateSet()
+		throws Exception {
+
+		PageTemplateSet pageTemplateSet = randomPageTemplateSet();
+
+		PageTemplateSet putPageTemplateSet =
+			pageTemplateSetResource.
+				putSiteSiteByExternalReferenceCodePageTemplateSet(
+					testGroup.getExternalReferenceCode(),
+					pageTemplateSet.getExternalReferenceCode(),
+					pageTemplateSet);
+
+		assertEquals(pageTemplateSet, putPageTemplateSet);
+		assertValid(putPageTemplateSet);
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPutSiteSiteByExternalReferenceCodePageTemplateSetPermissionsPage()
+		throws Exception {
+
+		super.
+			testPutSiteSiteByExternalReferenceCodePageTemplateSetPermissionsPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testPutSiteSiteExternalReferenceCodePageTemplateSetPermissionsPage()
+		throws Exception {
+
+		super.
+			testPutSiteSiteExternalReferenceCodePageTemplateSetPermissionsPage();
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"externalReferenceCode", "description", "name"};
+	}
+
+	@Override
+	protected PageTemplateSet
+			testGetSiteSiteByExternalReferenceCodePageTemplateSetsPage_addPageTemplateSet(
+				String siteExternalReferenceCode,
+				PageTemplateSet pageTemplateSet)
+		throws Exception {
+
+		return pageTemplateSetResource.
+			putSiteSiteByExternalReferenceCodePageTemplateSet(
+				siteExternalReferenceCode,
+				pageTemplateSet.getExternalReferenceCode(), pageTemplateSet);
+	}
+
+	@Override
+	protected PageTemplateSet
+			testPostSiteSiteByExternalReferenceCodePageTemplateSet_addPageTemplateSet(
+				PageTemplateSet pageTemplateSet)
+		throws Exception {
+
+		return pageTemplateSetResource.
+			putSiteSiteByExternalReferenceCodePageTemplateSet(
+				testGroup.getExternalReferenceCode(),
+				pageTemplateSet.getExternalReferenceCode(), pageTemplateSet);
+	}
+
+	@Inject
+	private LayoutPageTemplateCollectionLocalService
+		_layoutPageTemplateCollectionLocalService;
+
 }
