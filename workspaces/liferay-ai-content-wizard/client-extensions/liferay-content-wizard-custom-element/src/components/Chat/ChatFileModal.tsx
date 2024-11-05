262a0bdb5da20b827358dbb964d3e2c8256a81a7
Keven Leone <keven.leone@liferay.com>
2024-11-05 12:03:50 -0300

    LPD-41296 SF

--- workspaces/liferay-ai-content-wizard/client-extensions/liferay-content-wizard-custom-element/src/components/Chat/ChatFileModal.tsx
+++ workspaces/liferay-ai-content-wizard/client-extensions/liferay-content-wizard-custom-element/src/components/Chat/ChatFileModal.tsx
@@ -1,52 +1,58 @@
-import ClayModal, { useModal } from '@clayui/modal';
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 import ClayButton from '@clayui/button';
+import ClayModal, {useModal} from '@clayui/modal';
+
 import ChatFileModalExplorer from './ChatFileModalExplorer';
 
 type Props = {
-    items: any[];
-    modal: ReturnType<typeof useModal>;
-    onChoose: (data: any) => void;
-    selectedTree: any;
-    setSelectedTree: React.Dispatch<any>;
+	items: any[];
+	modal: ReturnType<typeof useModal>;
+	onChoose: (data: any) => void;
+	selectedTree: any;
+	setSelectedTree: React.Dispatch<any>;
 };
 
 const ChatFileModal: React.FC<Props> = ({
-    modal,
-    selectedTree,
-    onChoose,
-    setSelectedTree,
-    items,
+	items,
+	modal,
+	onChoose,
+	selectedTree,
+	setSelectedTree,
 }) => (
-    <ClayModal disableAutoClose observer={modal.observer}>
-        <ClayModal.Header>
-            <ClayModal.Title>
-                Choose the Documents and Media Files
-            </ClayModal.Title>
-        </ClayModal.Header>
-        <ClayModal.Body>
-            <ChatFileModalExplorer
-                items={items}
-                setSelectedTree={setSelectedTree}
-            />
-        </ClayModal.Body>
-        <ClayModal.Footer
-            last={
-                <>
-                    <ClayButton
-                        className="mr-2"
-                        onClick={modal.onClose}
-                        displayType="secondary"
-                    >
-                        Cancel
-                    </ClayButton>
+	<ClayModal disableAutoClose observer={modal.observer}>
+		<ClayModal.Header>
+			<ClayModal.Title>
+				Choose the Documents and Media Files
+			</ClayModal.Title>
+		</ClayModal.Header>
+		<ClayModal.Body>
+			<ChatFileModalExplorer
+				items={items}
+				setSelectedTree={setSelectedTree}
+			/>
+		</ClayModal.Body>
+		<ClayModal.Footer
+			last={
+				<>
+					<ClayButton
+						className="mr-2"
+						displayType="secondary"
+						onClick={modal.onClose}
+					>
+						Cancel
+					</ClayButton>
 
-                    <ClayButton disabled={!selectedTree} onClick={onChoose}>
-                        Choose
-                    </ClayButton>
-                </>
-            }
-        />
-    </ClayModal>
+					<ClayButton disabled={!selectedTree} onClick={onChoose}>
+						Choose
+					</ClayButton>
+				</>
+			}
+		/>
+	</ClayModal>
 );
 
 export default ChatFileModal;
--- workspaces/liferay-ai-content-wizard/client-extensions/liferay-content-wizard-custom-element/src/components/Chat/ChatFileModalExplorer.tsx
+++ workspaces/liferay-ai-content-wizard/client-extensions/liferay-content-wizard-custom-element/src/components/Chat/ChatFileModalExplorer.tsx
@@ -1,78 +1,84 @@
-import { TreeView } from '@clayui/core';
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {TreeView} from '@clayui/core';
 import ClayIcon from '@clayui/icon';
-import { useMemo } from 'react';
+import {useMemo} from 'react';
 
-const ChatFileModalExplorer = ({ items, setSelectedTree }: any) => {
-    const documentAndFolders = useMemo(() => {
-        const folderStructure = {} as any;
+const ChatFileModalExplorer = ({items, setSelectedTree}: any) => {
+	const documentAndFolders = useMemo(() => {
+		const folderStructure = {} as any;
 
-        for (const item of items) {
-            const folderId = item.folder.name;
-            const folder = folderStructure[folderId];
+		for (const item of items) {
+			const folderId = item.folder.name;
+			const folder = folderStructure[folderId];
 
-            if (folder) {
-                folderStructure[folderId].push(item);
-            } else {
-                folderStructure[folderId] = [item];
-            }
-        }
+			if (folder) {
+				folderStructure[folderId].push(item);
+			}
+			else {
+				folderStructure[folderId] = [item];
+			}
+		}
 
-        const newItems = [];
+		const newItems = [];
 
-        for (const folder in folderStructure) {
-            const documents = folderStructure[folder] as any[];
+		for (const folder in folderStructure) {
+			const documents = folderStructure[folder] as any[];
 
-            newItems.push({
-                children: documents.map((document) => ({
-                    image: document.contentUrl,
-                    name: document.fileName,
-                    id: document.id,
-                })),
-                name: folder,
-                type: 'folder',
-            });
-        }
+			newItems.push({
+				children: documents.map((document) => ({
+					id: document.id,
+					image: document.contentUrl,
+					name: document.fileName,
+				})),
+				name: folder,
+				type: 'folder',
+			});
+		}
 
-        return newItems;
-    }, [items]);
+		return newItems;
+	}, [items]);
 
-    return (
-        <TreeView
-            defaultItems={documentAndFolders}
-            nestedKey="children"
-            selectionMode="single"
-        >
-            {(item) => (
-                <TreeView.Item>
-                    <TreeView.ItemStack>
-                        <ClayIcon
-                            aria-label="Stack icon"
-                            symbol={item.type ? item.type : 'folder'}
-                        />
-                        {item.name}
-                    </TreeView.ItemStack>
-                    <TreeView.Group items={item.children}>
-                        {(child) => (
-                            <TreeView.Item
-                                onClick={() => setSelectedTree(child)}
-                            >
-                                {child.image && (
-                                    <img
-                                        width={18}
-                                        height={18}
-                                        className="rounded-circle"
-                                        src={child.image}
-                                    />
-                                )}
+	return (
+		<TreeView
+			defaultItems={documentAndFolders}
+			nestedKey="children"
+			selectionMode="single"
+		>
+			{(item) => (
+				<TreeView.Item>
+					<TreeView.ItemStack>
+						<ClayIcon
+							aria-label="Stack icon"
+							symbol={item.type ? item.type : 'folder'}
+						/>
+						{item.name}
+					</TreeView.ItemStack>
+					<TreeView.Group items={item.children}>
+						{(child) => (
+							<TreeView.Item
+								onClick={() => setSelectedTree(child)}
+							>
+								{child.image && (
+									<img
+										className="rounded-circle"
+										height={18}
+										src={child.image}
+										width={18}
+									/>
+								)}
 
-                                {child.name}
-                            </TreeView.Item>
-                        )}
-                    </TreeView.Group>
-                </TreeView.Item>
-            )}
-        </TreeView>
-    );
+								{child.name}
+							</TreeView.Item>
+						)}
+					</TreeView.Group>
+				</TreeView.Item>
+			)}
+		</TreeView>
+	);
 };
 
 export default ChatFileModalExplorer;
