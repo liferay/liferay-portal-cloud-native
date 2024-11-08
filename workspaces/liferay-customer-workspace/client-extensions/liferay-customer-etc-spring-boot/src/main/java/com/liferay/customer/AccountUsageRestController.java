0ebe3a23d437e5b6815a4affa461d9244676b571
amosfong <amos.fong@liferay.com>
2024-11-08 12:10:07 -0800

    LRSD-6325 consume GCP function for usage metrics

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/AccountUsageRestController.java
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/AccountUsageRestController.java
@@ -6,6 +6,7 @@
 package com.liferay.customer;
 
 import com.liferay.customer.model.AccountUsage;
+import com.liferay.customer.service.GoogleCloudFunctionWebService;
 import com.liferay.headless.admin.user.client.dto.v1_0.Account;
 import com.liferay.headless.admin.user.client.resource.v1_0.AccountResource;
 
@@ -18,6 +19,7 @@ import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONObject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,44 +33,51 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author Amos Fong
  */
-@RequestMapping("/accounts/{id}/usage")
+@RequestMapping("/accounts/{externalReferenceCode}/usage")
 @RestController
 public class AccountUsageRestController extends BaseRestController {
 
 	@GetMapping
 	public ResponseEntity<String> get(
-			@AuthenticationPrincipal Jwt jwt, @PathVariable("id") long id)
+			@AuthenticationPrincipal Jwt jwt,
+			@PathVariable("externalReferenceCode") String externalReferenceCode)
 		throws Exception {
 
 		try {
 			AccountResource accountResource = _getAccountResource(jwt);
 
-			Account account = accountResource.getAccount(id);
+			Account account = accountResource.getAccountByExternalReferenceCode(
+				externalReferenceCode);
 
 			AccountUsage accountUsage = new AccountUsage();
 
 			accountUsage.setAccountId(account.getId());
 
+			JSONObject jsonObject =
+				_googleCloudFunctionWebService.getCustomerAccountUsage(
+					externalReferenceCode);
+
 			Random random = new Random();
 
-			int apvsMax = (random.nextInt(4) + 1) * 1000000;
+			int apvsMax = jsonObject.getInt("totalAnonymousPageViewsCount");
 
 			accountUsage.setAPVsMax(apvsMax);
 			accountUsage.setAPVsUsed(random.nextInt(apvsMax));
 
-			int malusMax = (random.nextInt(10) + 1) * 100000;
+			int malusMax = jsonObject.getInt("totalActiveLoggedInUsersCount");
 
 			accountUsage.setMALUsMax(malusMax);
 			accountUsage.setMALUsUsed(random.nextInt(malusMax));
 
-			int sitesMax = random.nextInt(19) + 1;
+			int sitesMax = jsonObject.getInt("totalSitesCount");
 
 			accountUsage.setSitesMax(sitesMax);
 			accountUsage.setSitesUsed(random.nextInt(sitesMax));
 
-			JSONObject jsonObject = accountUsage.toJSONObject();
+			JSONObject accountUsageJSONObject = accountUsage.toJSONObject();
 
-			return new ResponseEntity<>(jsonObject.toString(), HttpStatus.OK);
+			return new ResponseEntity<>(
+				accountUsageJSONObject.toString(), HttpStatus.OK);
 		}
 		catch (Exception exception) {
 			_log.error(exception, exception);
@@ -89,5 +98,8 @@ public class AccountUsageRestController extends BaseRestController {
 
 	private static final Log _log = LogFactory.getLog(
 		AccountUsageRestController.class);
+
+	@Autowired
+	private GoogleCloudFunctionWebService _googleCloudFunctionWebService;
 
 }
---
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/service/GoogleCloudFunctionWebService.java
@@ -0,0 +1,86 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.customer.service;
+
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.IdTokenCredentials;
+import com.google.auth.oauth2.IdTokenProvider;
+import com.google.common.io.CharStreams;
+
+import com.liferay.petra.string.StringBundler;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.nio.charset.StandardCharsets;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Felipe Franca
+ * @author Amos Fong
+ */
+@Component
+public class GoogleCloudFunctionWebService {
+
+	public JSONObject getCustomerAccountUsage(String accountKey)
+		throws Exception {
+
+		try (InputStream inputStream = new ByteArrayInputStream(
+				_gcfServiceAccountKey.getBytes())) {
+
+			IdTokenCredentials idTokenCredential =
+				IdTokenCredentials.newBuilder(
+				).setIdTokenProvider(
+					(IdTokenProvider)GoogleCredentials.fromStream(inputStream)
+				).setTargetAudience(
+					_gcfBaseUrl + _FUNCTION_CUSTOMER_USAGE_API_PATH
+				).build();
+
+			HttpRequest httpRequest = new NetHttpTransport(
+			).createRequestFactory(
+				new HttpCredentialsAdapter(idTokenCredential)
+			).buildGetRequest(
+				new GenericUrl(
+					StringBundler.concat(
+						_gcfBaseUrl, _FUNCTION_CUSTOMER_USAGE_API_PATH,
+						"/api/v1/customer/usage/accounts/", accountKey))
+			);
+
+			HttpResponse httpResponse = httpRequest.execute();
+
+			try {
+				String result = CharStreams.toString(
+					new InputStreamReader(
+						httpResponse.getContent(), StandardCharsets.UTF_8));
+
+				return new JSONObject(result);
+			}
+			finally {
+				httpResponse.disconnect();
+			}
+		}
+	}
+
+	private static final String _FUNCTION_CUSTOMER_USAGE_API_PATH =
+		"/customer_usage_api";
+
+	@Value("${liferay.customer.gcf.base.url}")
+	private String _gcfBaseUrl;
+
+	@Value("${liferay.customer.gcf.service.account.key}")
+	private String _gcfServiceAccountKey;
+
+}
