fc4c5e1877a2de93047833ce2b3049501cafff75
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-10 08:00:57 +0000

    LPD-41760 Migrates ViewEmptyMessage and ViewInfoMessageWhenPreviewContentPage to playwright

--- modules/test/playwright/tests/layout-admin-web/simulationMenu.spec.ts
+++ modules/test/playwright/tests/layout-admin-web/simulationMenu.spec.ts
@@ -6,19 +6,78 @@
 import {expect, mergeTests} from '@playwright/test';
 
 import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
+import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../fixtures/loginTest';
 import {pageViewModePagesTest} from '../../fixtures/pageViewModePagesTest';
+import {clickAndExpectToBeVisible} from '../../utils/clickAndExpectToBeVisible';
 import getRandomString from '../../utils/getRandomString';
+import getPageDefinition from '../layout-content-page-editor-web/utils/getPageDefinition';
 import {pagesPagesTest} from './fixtures/pagesPagesTest';
 
 const test = mergeTests(
 	apiHelpersTest,
+	featureFlagsTest({
+		'LPS-178052': true,
+	}),
 	isolatedSiteTest,
 	loginTest(),
 	pagesPagesTest,
 	pageViewModePagesTest
 );
+
+test.describe('Page content', () => {
+	test(
+		'View info messages',
+		{
+			tag: ['@LPS-186155', '@LPS-187159'],
+		},
+		async ({apiHelpers, page, simulationMenuPage, site}) => {
+
+			// Create page and go to view mode
+
+			const layout = await apiHelpers.headlessDelivery.createSitePage({
+				pageDefinition: getPageDefinition(),
+				siteId: site.id,
+				title: getRandomString(),
+			});
+
+			await page.goto(
+				`/web${site.friendlyUrlPath}${layout.friendlyUrlPath}`
+			);
+
+			// Open simulation panel
+
+			await simulationMenuPage.openSimulationPanel();
+
+			// Assert empty messages
+
+			await expect(
+				page.getByText('Showing content for the segment "Anyone".')
+			).toBeVisible();
+
+			await expect(
+				page.getByText(
+					'No segments have been added yet. To add a new segment go to Product Menu > People > Segments.'
+				)
+			).toBeVisible();
+
+			await clickAndExpectToBeVisible({
+				autoClick: true,
+				target: page.locator('#experiences'),
+				trigger: page.getByRole('combobox', {name: 'Preview By'}),
+			});
+
+			await expect(
+				page.getByText('Showing content for the experience "Default".')
+			).toBeVisible();
+
+			await expect(
+				page.getByText('No experiences have been added yet.')
+			).toBeVisible();
+		}
+	);
+});
 
 test.describe('Screen Size', () => {
 	test('View web content is shown in Web Content Display after be added via content panel', async ({
