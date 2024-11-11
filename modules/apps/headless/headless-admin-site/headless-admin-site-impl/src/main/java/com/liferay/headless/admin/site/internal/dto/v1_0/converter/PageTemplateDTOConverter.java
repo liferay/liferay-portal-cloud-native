1b1d43dfed9f4eba5490e383067e315d7eedd41c
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-11-11 15:50:32 +0100

    LPD-41541 Add a DTO Converter for PageTemplate

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/converter/PageTemplateDTOConverter.java
@@ -0,0 +1,115 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.dto.v1_0.converter;
+
+import com.liferay.headless.admin.site.dto.v1_0.ContentPageTemplate;
+import com.liferay.headless.admin.site.dto.v1_0.PageSpecification;
+import com.liferay.headless.admin.site.dto.v1_0.PageTemplate;
+import com.liferay.headless.admin.site.dto.v1_0.WidgetPageTemplate;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	property = "dto.class.name=com.liferay.layout.page.template.model.LayoutPageTemplateEntry",
+	service = DTOConverter.class
+)
+public class PageTemplateDTOConverter
+	implements DTOConverter<LayoutPageTemplateEntry, PageTemplate> {
+
+	@Override
+	public String getContentType() {
+		return PageTemplate.class.getSimpleName();
+	}
+
+	@Override
+	public PageTemplate toDTO(
+			DTOConverterContext dtoConverterContext,
+			LayoutPageTemplateEntry layoutPageTemplateEntry)
+		throws Exception {
+
+		if (layoutPageTemplateEntry.getType() ==
+				LayoutPageTemplateEntryTypeConstants.BASIC) {
+
+			return _getContentPageTemplate(layoutPageTemplateEntry);
+		}
+
+		return _getWidgetPageTemplate(layoutPageTemplateEntry);
+	}
+
+	private PageTemplate _getContentPageTemplate(
+			LayoutPageTemplateEntry layoutPageTemplateEntry)
+		throws Exception {
+
+		Layout layout = _layoutLocalService.getLayout(
+			layoutPageTemplateEntry.getPlid());
+
+		return new ContentPageTemplate() {
+			{
+				setDateCreated(layoutPageTemplateEntry::getCreateDate);
+				setDateModified(layoutPageTemplateEntry::getModifiedDate);
+				setDatePublished(layout::getPublishDate);
+				setExternalReferenceCode(
+					layoutPageTemplateEntry::getExternalReferenceCode);
+				setKey(layoutPageTemplateEntry::getLayoutPageTemplateEntryKey);
+				setName(layoutPageTemplateEntry::getName);
+				setPageSpecifications(
+					() -> new PageSpecification[] {
+						_pageSpecificationDTOConverter.toDTO(layout),
+						_pageSpecificationDTOConverter.toDTO(
+							layout.fetchDraftLayout())
+					});
+				setType(() -> Type.CONTENT_PAGE_TEMPLATE);
+				setUuid(layoutPageTemplateEntry::getUuid);
+			}
+		};
+	}
+
+	private PageTemplate _getWidgetPageTemplate(
+			LayoutPageTemplateEntry layoutPageTemplateEntry)
+		throws Exception {
+
+		Layout layout = _layoutLocalService.getLayout(
+			layoutPageTemplateEntry.getPlid());
+
+		return new WidgetPageTemplate() {
+			{
+				setDateCreated(layoutPageTemplateEntry::getCreateDate);
+				setDateModified(layoutPageTemplateEntry::getModifiedDate);
+				setDatePublished(layout::getPublishDate);
+				setExternalReferenceCode(
+					layoutPageTemplateEntry::getExternalReferenceCode);
+				setKey(layoutPageTemplateEntry::getLayoutPageTemplateEntryKey);
+				setName(layoutPageTemplateEntry::getName);
+				setPageSpecifications(
+					() -> new PageSpecification[] {
+						_pageSpecificationDTOConverter.toDTO(layout)
+					});
+				setType(() -> Type.WIDGET_PAGE_TEMPLATE);
+				setUuid(layoutPageTemplateEntry::getUuid);
+			}
+		};
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference(
+		target = "(component.name=com.liferay.headless.admin.site.internal.dto.v1_0.converter.PageSpecificationDTOConverter)"
+	)
+	private DTOConverter<Layout, PageSpecification>
+		_pageSpecificationDTOConverter;
+
+}
