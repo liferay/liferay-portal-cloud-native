95fa3728b016ec343ffedf3b19f900ac09b641cd
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2024-11-12 17:48:30 +0000

    LPD-41546 Adds Get PageExperience endpoint

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/PageExperienceResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/PageExperienceResourceImpl.java
@@ -5,9 +5,22 @@
 
 package com.liferay.headless.admin.site.internal.resource.v1_0;
 
+import com.liferay.headless.admin.site.dto.v1_0.PageExperience;
 import com.liferay.headless.admin.site.resource.v1_0.PageExperienceResource;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.segments.model.SegmentsExperience;
+import com.liferay.segments.service.SegmentsExperienceService;
+
+import java.util.Collections;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +31,47 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = PageExperienceResource.class
 )
 public class PageExperienceResourceImpl extends BasePageExperienceResourceImpl {
+
+	@Override
+	public Page<PageExperience>
+			getSiteSiteByExternalReferenceCodePageSpecificationPageExperiencesPage(
+				String siteExternalReferenceCode,
+				String pageSpecificationExternalReferenceCode)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
+			throw new UnsupportedOperationException();
+		}
+
+		Group group = groupLocalService.getGroupByExternalReferenceCode(
+			siteExternalReferenceCode, contextCompany.getCompanyId());
+
+		Layout layout = _layoutLocalService.fetchLayoutByExternalReferenceCode(
+			pageSpecificationExternalReferenceCode, group.getGroupId());
+
+		if (layout == null) {
+			return Page.of(Collections.emptyList());
+		}
+
+		return Page.of(
+			transform(
+				_segmentsExperienceService.getSegmentsExperiences(
+					group.getGroupId(), layout.getPlid(), true,
+					QueryUtil.ALL_POS, QueryUtil.ALL_POS, null),
+				segmentsExperience -> _pageExperienceDTOConverter.toDTO(
+					segmentsExperience)));
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference(
+		target = "(component.name=com.liferay.headless.admin.site.internal.dto.v1_0.converter.PageExperienceDTOConverter)"
+	)
+	private DTOConverter<SegmentsExperience, PageExperience>
+		_pageExperienceDTOConverter;
+
+	@Reference
+	private SegmentsExperienceService _segmentsExperienceService;
+
 }
