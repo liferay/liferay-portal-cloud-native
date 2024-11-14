42e5dca90cbbd755bcbf5c24f2826b22c116ea2b
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-11-14 09:06:04 +0100

    LPD-41546 Validate layout type

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/PageExperienceResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/PageExperienceResourceImpl.java
@@ -14,6 +14,7 @@ import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
@@ -27,6 +28,7 @@ import com.liferay.segments.service.SegmentsEntryLocalService;
 import com.liferay.segments.service.SegmentsExperienceService;
 
 import java.util.Collections;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -178,7 +180,11 @@ public class PageExperienceResourceImpl extends BasePageExperienceResourceImpl {
 		Layout layout = _layoutLocalService.fetchLayoutByExternalReferenceCode(
 			pageExperience.getSitePageExternalReferenceCode(), groupId);
 
-		if (layout == null) {
+		if ((layout == null) ||
+			(!Objects.equals(
+				layout.getType(), LayoutConstants.TYPE_COLLECTION) &&
+			 !Objects.equals(layout.getType(), LayoutConstants.TYPE_CONTENT))) {
+
 			throw new UnsupportedOperationException();
 		}
 
