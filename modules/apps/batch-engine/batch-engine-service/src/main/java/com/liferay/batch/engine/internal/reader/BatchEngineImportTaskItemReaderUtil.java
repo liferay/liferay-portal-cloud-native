394ce505ca4e2866d4b79f086260040bd3341e3c
Carlos Correa <carlos.correa@liferay.com>
2024-11-18 10:41:31 +0100

    LPD-38548 We don't need to loop over an empty map, as no parameters will be there

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/reader/BatchEngineImportTaskItemReaderUtil.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/reader/BatchEngineImportTaskItemReaderUtil.java
@@ -276,7 +276,7 @@ public class BatchEngineImportTaskItemReaderUtil {
 			batchEngineImportTask.getParameters();
 
 		if (parameters == null) {
-			parameters = Collections.emptyMap();
+			return new HashSet<>();
 		}
 
 		Serializable restrictedFieldNamesParam = parameters.get(
