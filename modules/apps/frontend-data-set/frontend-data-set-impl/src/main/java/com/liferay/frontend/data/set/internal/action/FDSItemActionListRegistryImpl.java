990a268ef87ac85fee769e0b206360ed09a7700e
Brian Chan <brian.chan@liferay.com>
2024-11-19 15:12:24 -0300

    LPD-42456 SF

--- modules/apps/frontend-data-set/frontend-data-set-impl/src/main/java/com/liferay/frontend/data/set/internal/action/FDSItemActionListRegistryImpl.java
+++ modules/apps/frontend-data-set/frontend-data-set-impl/src/main/java/com/liferay/frontend/data/set/internal/action/FDSItemActionListRegistryImpl.java
@@ -33,8 +33,8 @@ public class FDSItemActionListRegistryImpl
 		if (serviceWrapper == null) {
 			if (_log.isDebugEnabled()) {
 				_log.debug(
-					"No frontend data set item action list is associated with " +
-						fdsName);
+					"No frontend data set item action list is associated " +
+						"with " + fdsName);
 			}
 
 			return null;
