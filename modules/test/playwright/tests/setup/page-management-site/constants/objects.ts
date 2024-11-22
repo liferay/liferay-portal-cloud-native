30199702117accb840a33c5a7fe4a8fe95dbda6b
Sandro Chinea <sandrochinea@gmail.com>
2024-11-22 11:29:25 +0000

    LPD-42633 Add default entries constant

--- modules/test/playwright/tests/setup/page-management-site/constants/objects.ts
+++ modules/test/playwright/tests/setup/page-management-site/constants/objects.ts
@@ -3,6 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+export const DEFAULT_ENTRIES_ERCS = [
+	'lemon-basket-entry-1',
+	'lemon-basket-entry-2',
+];
+
 export const OBJECT_ENTITIES = {
 	'All Fields': {
 		ERC: 'all-fields-object-erc',
