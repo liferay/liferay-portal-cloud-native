939b78cd1231029112f24c874d16e6a3866d3bf7
Victor Galan <victor.galan@liferay.com>
2024-11-25 11:44:14 +0100

    LPD-42302 Save localization in text inputs
    add to inputs

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/rich-text-input/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/rich-text-input/index.js
@@ -2,6 +2,12 @@ const inputElement = document.getElementById(
 	`${fragmentNamespace}-rich-text-input`
 );
 
+const inputLabelElement = document.getElementById(
+	`${fragmentEntryLinkNamespace}-rich-text-input-label`
+);
+
+let currentLanguageId = themeDisplay.getDefaultLanguageId();
+
 if (input.attributes?.readOnly) {
 	if (inputElement) {
 		inputElement.innerHTML = input.value;
@@ -11,4 +17,46 @@ else if (layoutMode === 'edit') {
 	if (inputElement) {
 		inputElement.setAttribute('disabled', true);
 	}
+}
+else if (layoutMode !== 'edit' && input.attributes.localizable) {
+	CKEDITOR.on('instanceReady', (editorEvent) => {
+		if (editorEvent.editor.name === input.name) {
+			editorEvent.editor.on('change', () => {
+				const value = editorEvent.editor.getData();
+
+				const translationInput =
+					getOrCreateTranslationInput(currentLanguageId);
+
+				translationInput.value = value;
+			});
+		}
+
+		Liferay.on('localizationSelect:localeChanged', (event) => {
+			currentLanguageId = event.languageId;
+
+			const translationInput =
+				getOrCreateTranslationInput(currentLanguageId);
+
+			if (translationInput.value) {
+				editorEvent.editor.setData(translationInput.value);
+			}
+		});
+	});
+}
+
+function getOrCreateTranslationInput(languageId) {
+	const inputId = `${fragmentNamespace}${input.name}_${languageId}`;
+
+	let translationInput = document.getElementById(inputId);
+
+	if (!translationInput) {
+		translationInput = document.createElement('input');
+		translationInput.type = 'hidden';
+		translationInput.id = inputId;
+		translationInput.name = `${input.name}_${currentLanguageId}`;
+
+		inputLabelElement.parentElement.appendChild(translationInput);
+	}
+
+	return translationInput;
 }
--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/textarea/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/textarea/index.js
@@ -50,6 +50,8 @@ function onInputKeyup(event) {
 	}
 }
 
+let currentLanguageId = themeDisplay.getDefaultLanguageId();
+
 function main() {
 	if (layoutMode === 'edit' && textarea) {
 		textarea.setAttribute('disabled', true);
@@ -62,7 +64,45 @@ function main() {
 		}
 
 		textarea.addEventListener('keyup', onInputKeyup);
+
+		if (input.attributes.localizable) {
+			Liferay.on('localizationSelect:localeChanged', (event) => {
+				currentLanguageId = event.languageId;
+
+				const translationInput =
+					getOrCreateTranslationInput(currentLanguageId);
+
+				if (translationInput.value) {
+					textarea.value = translationInput.value;
+				}
+			});
+
+			textarea.addEventListener('input', (event) => {
+				const value = event.target.value;
+
+				const translationInput =
+					getOrCreateTranslationInput(currentLanguageId);
+
+				translationInput.value = value;
+			});
+		}
 	}
+}
+
+function getOrCreateTranslationInput(languageId) {
+	const inputId = `${fragmentNamespace}${input.name}_${languageId}`;
+
+	let translationInput = document.getElementById(inputId);
+
+	if (!translationInput) {
+		translationInput = document.createElement('input');
+		translationInput.type = 'hidden';
+		translationInput.id = inputId;
+		translationInput.name = `${input.name}_${currentLanguageId}`;
+		textarea.parentNode.appendChild(translationInput);
+	}
+
+	return translationInput;
 }
 
 main();
