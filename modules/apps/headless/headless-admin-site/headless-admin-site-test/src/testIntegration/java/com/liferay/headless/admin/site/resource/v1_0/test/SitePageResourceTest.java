30120a692520ff5b6a066e7bcf8c29c923db569b
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-11-25 23:11:25 +0100

    LPD-42762 Add and integration test to assert the nested field friendlyUrlHistory on the GET bay ERC endpoint of SitePageResource

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/SitePageResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/SitePageResourceTest.java
@@ -6,16 +6,29 @@
 package com.liferay.headless.admin.site.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.headless.admin.site.client.dto.v1_0.FriendlyUrlHistory;
 import com.liferay.headless.admin.site.client.dto.v1_0.SitePage;
 import com.liferay.headless.admin.site.client.dto.v1_0.WidgetPageSettings;
+import com.liferay.headless.admin.site.client.resource.v1_0.SitePageResource;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
+
+import java.util.Locale;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Ignore;
@@ -64,6 +77,10 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 
 		assertEquals(postSitePage, getSitePage);
 		assertValid(getSitePage);
+
+		_testGetSiteSiteByExternalReferenceCodeSitePageWithNestedFields(
+			testGetSiteSiteByExternalReferenceCodeSitePagesPage_addSitePage(
+				testGroup.getExternalReferenceCode(), randomSitePage()));
 	}
 
 	@Ignore
@@ -202,7 +219,70 @@ public class SitePageResourceTest extends BaseSitePageResourceTestCase {
 			sitePage.getSiteExternalReferenceCode(), sitePage);
 	}
 
+	private void _assertNestedFields(SitePage sitePage) throws Exception {
+		FriendlyUrlHistory friendlyUrlHistory =
+			sitePage.getFriendlyUrlHistory();
+
+		JSONObject jsonObject = _jsonFactory.createJSONObject(
+			GetterUtil.getString(friendlyUrlHistory.getFriendlyUrlPath_i18n()));
+
+		Layout layout = _layoutLocalService.getLayoutByExternalReferenceCode(
+			sitePage.getExternalReferenceCode(), testGroup.getGroupId());
+
+		Map<Locale, String> friendlyURLMap = layout.getFriendlyURLMap();
+
+		Assert.assertEquals(
+			jsonObject.toString(), friendlyURLMap.size(), jsonObject.length());
+
+		for (Map.Entry<Locale, String> entry : friendlyURLMap.entrySet()) {
+			String key = LocaleUtil.toBCP47LanguageId(entry.getKey());
+
+			JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+			Assert.assertEquals(jsonArray.toString(), 1, jsonArray.length());
+			Assert.assertEquals(
+				jsonArray.toString(), entry.getValue(), jsonArray.getString(0));
+		}
+	}
+
+	private SitePageResource _getSitePageResource() throws Exception {
+		User omniadminUser = UserTestUtil.addOmniadminUser();
+
+		String password = RandomTestUtil.randomString();
+
+		_userLocalService.updatePassword(
+			omniadminUser.getUserId(), password, password, false, true);
+
+		return SitePageResource.builder(
+		).authentication(
+			omniadminUser.getEmailAddress(), password
+		).locale(
+			LocaleUtil.getDefault()
+		).parameters(
+			"nestedFields", "friendlyUrlHistory"
+		).build();
+	}
+
+	private void
+			_testGetSiteSiteByExternalReferenceCodeSitePageWithNestedFields(
+				SitePage sitePage)
+		throws Exception {
+
+		SitePageResource curSitePageResource = _getSitePageResource();
+
+		_assertNestedFields(
+			curSitePageResource.getSiteSiteByExternalReferenceCodeSitePage(
+				testGroup.getExternalReferenceCode(),
+				sitePage.getExternalReferenceCode()));
+	}
+
+	@Inject
+	private JSONFactory _jsonFactory;
+
 	@Inject
 	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 }
