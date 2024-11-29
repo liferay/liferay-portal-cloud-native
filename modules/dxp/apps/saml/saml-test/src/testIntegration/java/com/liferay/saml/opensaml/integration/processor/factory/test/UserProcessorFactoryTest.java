83508d0f9f68091b93d3bddbf9e389bf59535aa0
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2024-11-29 17:40:22 +0000

    LPD-40838 SF simplify (avoid using HashMap)

--- modules/dxp/apps/saml/saml-test/src/testIntegration/java/com/liferay/saml/opensaml/integration/processor/factory/test/UserProcessorFactoryTest.java
+++ modules/dxp/apps/saml/saml-test/src/testIntegration/java/com/liferay/saml/opensaml/integration/processor/factory/test/UserProcessorFactoryTest.java
@@ -14,15 +14,11 @@ import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
-import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.saml.opensaml.integration.field.expression.handler.registry.UserFieldExpressionHandlerRegistry;
 import com.liferay.saml.opensaml.integration.processor.UserProcessor;
 import com.liferay.saml.opensaml.integration.processor.factory.UserProcessorFactory;
-
-import java.util.Collections;
-import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
@@ -46,34 +42,26 @@ public class UserProcessorFactoryTest {
 		_user = UserTestUtil.addUser();
 		_userGroup = UserGroupTestUtil.addUserGroup();
 
-		_user = _processUser(
-			HashMapBuilder.put(
-				"membership:userGroups", new String[] {_userGroup.getName()}
-			).build());
+		_user = _processUser(_userGroup.getName());
 
 		Assert.assertEquals(
 			1,
 			_userGroupLocalService.getUserUserGroupsCount(_user.getUserId()));
 
-		_user = _processUser(Collections.emptyMap());
+		_user = _processUser(null);
 
 		Assert.assertEquals(
 			1,
 			_userGroupLocalService.getUserUserGroupsCount(_user.getUserId()));
 
-		_user = _processUser(
-			HashMapBuilder.put(
-				"membership:userGroups", new String[0]
-			).build());
+		_user = _processUser();
 
 		Assert.assertEquals(
 			0,
 			_userGroupLocalService.getUserUserGroupsCount(_user.getUserId()));
 	}
 
-	private User _processUser(Map<String, String[]> attributesMap)
-		throws Exception {
-
+	private User _processUser(String... userGroupName) throws Exception {
 		ServiceContext serviceContext =
 			ServiceContextTestUtil.getServiceContext(
 				_user.getCompanyId(), _user.getGroupId(), _user.getUserId());
@@ -81,9 +69,9 @@ public class UserProcessorFactoryTest {
 		UserProcessor userProcessor = _userProcessorFactory.create(
 			_user, _userFieldExpressionHandlerRegistry);
 
-		for (Map.Entry<String, String[]> attribute : attributesMap.entrySet()) {
+		if (userGroupName != null) {
 			userProcessor.setValueArray(
-				attribute.getKey(), attribute.getValue());
+				String.class, "membership:userGroups", userGroupName);
 		}
 
 		return userProcessor.process(serviceContext);
