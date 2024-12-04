9964a98526dc9416ac842d3dba241559f23c1977
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2024-12-04 13:17:57 +0100

    LPD-42898 Modify the test to also verify that updating Open Graph settings does not overwrite existing persisted preferences for other actions, such as canonicals and custom tags

--- modules/apps/layout/layout-seo-test/src/testIntegration/java/com/liferay/layout/seo/internal/portlet/action/test/EditOpenGraphMVCActionCommandTest.java
+++ modules/apps/layout/layout-seo-test/src/testIntegration/java/com/liferay/layout/seo/internal/portlet/action/test/EditOpenGraphMVCActionCommandTest.java
@@ -6,6 +6,19 @@
 package com.liferay.layout.seo.internal.portlet.action.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializerSerializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializerSerializeResponse;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
+import com.liferay.dynamic.data.mapping.model.UnlocalizedValue;
+import com.liferay.dynamic.data.mapping.model.Value;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageEngineManager;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
 import com.liferay.layout.seo.model.LayoutSEOEntry;
 import com.liferay.layout.seo.service.LayoutSEOEntryLocalService;
 import com.liferay.layout.test.util.ContentLayoutTestUtil;
@@ -15,21 +28,30 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.portlet.ActionRequest;
@@ -64,9 +86,34 @@ public class EditOpenGraphMVCActionCommandTest {
 	}
 
 	@Test
-	public void testEditOpenGraph() throws Exception {
-		String languageId = _language.getLanguageId(
-			LocaleUtil.getSiteDefault());
+	@TestInfo("LPD-42898")
+	public void testEditOpenGraphWithExistingLayoutSEOEntry() throws Exception {
+		Locale locale = LocaleUtil.getSiteDefault();
+
+		DDMStructure ddmStructure = _ddmStructureLocalService.getStructure(
+			_company.getGroupId(),
+			_classNameLocalService.getClassNameId(
+				LayoutSEOEntry.class.getName()),
+			"custom-meta-tags");
+
+		DDMFormValues ddmFormValues = _createDDMFormValues(
+			ddmStructure.getDDMForm(), locale);
+
+		LayoutSEOEntry layoutSEOEntry =
+			_layoutSEOEntryLocalService.updateLayoutSEOEntry(
+				TestPropsValues.getUserId(), _group.getGroupId(), false,
+				_layout.getLayoutId(), true,
+				HashMapBuilder.put(
+					locale, RandomTestUtil.randomString()
+				).build(),
+				false, Collections.emptyMap(), Collections.emptyMap(), 0, false,
+				Collections.emptyMap(),
+				_getServiceContext(
+					ddmFormValues, ddmStructure.getStructureId()));
+
+		_assertDDMFormValues(ddmFormValues, layoutSEOEntry.getDDMStorageId());
+
+		String languageId = _language.getLanguageId(locale);
 		String description = RandomTestUtil.randomString();
 		String imageAlt = RandomTestUtil.randomString();
 		long fileEntryId = RandomTestUtil.randomLong();
@@ -91,21 +138,109 @@ public class EditOpenGraphMVCActionCommandTest {
 				).build()),
 			new MockLiferayPortletActionResponse());
 
-		LayoutSEOEntry layoutSEOEntry =
+		LayoutSEOEntry curLayoutSEOEntry =
 			_layoutSEOEntryLocalService.fetchLayoutSEOEntry(
 				_layout.getGroupId(), _layout.isPrivateLayout(),
 				_layout.getLayoutId());
 
-		Assert.assertTrue(layoutSEOEntry.isOpenGraphDescriptionEnabled());
 		Assert.assertEquals(
-			description, layoutSEOEntry.getOpenGraphDescription(languageId));
-		Assert.assertTrue(layoutSEOEntry.isOpenGraphTitleEnabled());
+			layoutSEOEntry.getCanonicalURL(),
+			curLayoutSEOEntry.getCanonicalURL());
+
+		_assertDDMFormValues(
+			ddmFormValues, curLayoutSEOEntry.getDDMStorageId());
+
+		Assert.assertTrue(curLayoutSEOEntry.isOpenGraphDescriptionEnabled());
 		Assert.assertEquals(
-			title, layoutSEOEntry.getOpenGraphTitle(languageId));
+			description, curLayoutSEOEntry.getOpenGraphDescription(languageId));
+		Assert.assertTrue(curLayoutSEOEntry.isOpenGraphTitleEnabled());
 		Assert.assertEquals(
-			imageAlt, layoutSEOEntry.getOpenGraphImageAlt(languageId));
+			title, curLayoutSEOEntry.getOpenGraphTitle(languageId));
 		Assert.assertEquals(
-			fileEntryId, layoutSEOEntry.getOpenGraphImageFileEntryId());
+			imageAlt, curLayoutSEOEntry.getOpenGraphImageAlt(languageId));
+		Assert.assertEquals(
+			fileEntryId, curLayoutSEOEntry.getOpenGraphImageFileEntryId());
+	}
+
+	private void _assertDDMFormFieldValue(
+		DDMFormFieldValue ddmFormFieldValue,
+		DDMFormFieldValue curDDMFormFieldValue) {
+
+		Assert.assertEquals(
+			ddmFormFieldValue.getInstanceId(),
+			curDDMFormFieldValue.getInstanceId());
+		Assert.assertEquals(
+			ddmFormFieldValue.getName(), curDDMFormFieldValue.getName());
+		Assert.assertEquals(
+			ddmFormFieldValue.getValue(), curDDMFormFieldValue.getValue());
+	}
+
+	private void _assertDDMFormValues(
+			DDMFormValues ddmFormValues, long ddmStorageId)
+		throws Exception {
+
+		Assert.assertNotEquals(0, ddmStorageId);
+
+		List<DDMFormFieldValue> ddmFormFieldValues =
+			ddmFormValues.getDDMFormFieldValues();
+
+		DDMFormFieldValue propertyDDMFormFieldValue = ddmFormFieldValues.get(0);
+
+		DDMFormValues curDDMFormValues =
+			_ddmStorageEngineManager.getDDMFormValues(ddmStorageId);
+
+		List<DDMFormFieldValue> curDDMFormFieldValues =
+			curDDMFormValues.getDDMFormFieldValues();
+
+		Assert.assertEquals(
+			curDDMFormFieldValues.toString(), 1, curDDMFormFieldValues.size());
+
+		DDMFormFieldValue curPropertyDDMFormFieldValue =
+			curDDMFormFieldValues.get(0);
+
+		_assertDDMFormFieldValue(
+			propertyDDMFormFieldValue, curPropertyDDMFormFieldValue);
+
+		List<DDMFormFieldValue> nestedDDMFormFieldValues =
+			propertyDDMFormFieldValue.getNestedDDMFormFieldValues();
+
+		DDMFormFieldValue contentDDMFormFieldValue =
+			nestedDDMFormFieldValues.get(0);
+
+		List<DDMFormFieldValue> curNestedDDMFormFieldValues =
+			curPropertyDDMFormFieldValue.getNestedDDMFormFieldValues();
+
+		Assert.assertEquals(
+			curNestedDDMFormFieldValues.toString(), 1,
+			curNestedDDMFormFieldValues.size());
+
+		_assertDDMFormFieldValue(
+			contentDDMFormFieldValue, curNestedDDMFormFieldValues.get(0));
+	}
+
+	private DDMFormValues _createDDMFormValues(DDMForm ddmForm, Locale locale)
+		throws Exception {
+
+		DDMFormValues ddmFormValues = DDMFormValuesTestUtil.createDDMFormValues(
+			ddmForm);
+
+		DDMFormFieldValue propertyDDMFormFieldValue =
+			DDMFormValuesTestUtil.createDDMFormFieldValue(
+				"property",
+				new UnlocalizedValue(RandomTestUtil.randomString()));
+
+		Value value = new LocalizedValue(locale);
+
+		value.addString(locale, RandomTestUtil.randomString());
+
+		propertyDDMFormFieldValue.setNestedDDMFormFields(
+			ListUtil.fromArray(
+				DDMFormValuesTestUtil.createDDMFormFieldValue(
+					"content", value)));
+
+		ddmFormValues.addDDMFormFieldValue(propertyDDMFormFieldValue);
+
+		return ddmFormValues;
 	}
 
 	private MockLiferayPortletActionRequest _getMockLiferayPortletActionRequest(
@@ -134,13 +269,50 @@ public class EditOpenGraphMVCActionCommandTest {
 		return mockLiferayPortletActionRequest;
 	}
 
+	private ServiceContext _getServiceContext(
+			DDMFormValues ddmFormValues, long ddmStructureId)
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group, TestPropsValues.getUserId());
+
+		DDMFormValuesSerializerSerializeRequest.Builder builder =
+			DDMFormValuesSerializerSerializeRequest.Builder.newBuilder(
+				ddmFormValues);
+
+		DDMFormValuesSerializerSerializeResponse
+			ddmFormValuesSerializerSerializeResponse =
+				_jsonDDMFormValuesSerializer.serialize(builder.build());
+
+		String serializedDDMFormValues =
+			ddmFormValuesSerializerSerializeResponse.getContent();
+
+		serviceContext.setAttribute(
+			ddmStructureId + "ddmFormValues", serializedDDMFormValues);
+
+		return serviceContext;
+	}
+
+	@Inject
+	private ClassNameLocalService _classNameLocalService;
+
 	private Company _company;
 
 	@Inject
 	private CompanyLocalService _companyLocalService;
 
+	@Inject
+	private DDMStorageEngineManager _ddmStorageEngineManager;
+
+	@Inject
+	private DDMStructureLocalService _ddmStructureLocalService;
+
 	@DeleteAfterTestRun
 	private Group _group;
+
+	@Inject(filter = "ddm.form.values.serializer.type=json")
+	private DDMFormValuesSerializer _jsonDDMFormValuesSerializer;
 
 	@Inject
 	private Language _language;
