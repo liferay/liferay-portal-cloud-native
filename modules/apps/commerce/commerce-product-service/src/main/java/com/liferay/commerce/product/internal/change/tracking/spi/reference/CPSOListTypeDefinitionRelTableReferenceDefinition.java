110be0d6767d9b3881598f0309340a99e2a2c295
Cheryl Tang <cheryl.tang@liferay.com>
2024-12-11 15:53:47 -0800

    LPD-44578 Add missing TableReferenceDefinitions

---
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/change/tracking/spi/reference/CPSOListTypeDefinitionRelTableReferenceDefinition.java
@@ -0,0 +1,54 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.product.internal.change.tracking.spi.reference;
+
+import com.liferay.change.tracking.spi.reference.TableReferenceDefinition;
+import com.liferay.change.tracking.spi.reference.builder.ChildTableReferenceInfoBuilder;
+import com.liferay.change.tracking.spi.reference.builder.ParentTableReferenceInfoBuilder;
+import com.liferay.commerce.product.model.CPSpecificationOptionListTypeDefinitionRelTable;
+import com.liferay.commerce.product.service.persistence.CPSpecificationOptionListTypeDefinitionRelPersistence;
+import com.liferay.portal.kernel.service.persistence.BasePersistence;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cheryl Tang
+ */
+@Component(service = TableReferenceDefinition.class)
+public class CPSOListTypeDefinitionRelTableReferenceDefinition
+	implements TableReferenceDefinition
+		<CPSpecificationOptionListTypeDefinitionRelTable> {
+
+	@Override
+	public void defineChildTableReferences(
+		ChildTableReferenceInfoBuilder
+			<CPSpecificationOptionListTypeDefinitionRelTable>
+				childTableReferenceInfoBuilder) {
+	}
+
+	@Override
+	public void defineParentTableReferences(
+		ParentTableReferenceInfoBuilder
+			<CPSpecificationOptionListTypeDefinitionRelTable>
+				parentTableReferenceInfoBuilder) {
+	}
+
+	@Override
+	public BasePersistence<?> getBasePersistence() {
+		return _cpsoListTypeDefinitionRelPersistence;
+	}
+
+	@Override
+	public CPSpecificationOptionListTypeDefinitionRelTable getTable() {
+		return CPSpecificationOptionListTypeDefinitionRelTable.INSTANCE;
+	}
+
+	@Reference
+	private CPSpecificationOptionListTypeDefinitionRelPersistence
+		_cpsoListTypeDefinitionRelPersistence;
+
+}
