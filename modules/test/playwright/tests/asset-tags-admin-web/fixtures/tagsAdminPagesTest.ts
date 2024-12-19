05c4495231e594573c82ed46a1e07c1bd50096be
NemethNorbert <norbert.nemeth@liferay.com>
2024-12-19 17:10:24 +0100

    LPD-41946 Add TagsUseCases tests

--- modules/test/playwright/tests/asset-tags-admin-web/fixtures/tagsAdminPagesTest.ts
+++ modules/test/playwright/tests/asset-tags-admin-web/fixtures/tagsAdminPagesTest.ts
@@ -7,15 +7,20 @@ import {test} from '@playwright/test';
 
 import {TagsAdminPage} from '../pages/TagsAdminPage';
 import {TagsEditPage} from '../pages/TagsEditPage';
+import {TagsFilterPage} from '../pages/TagsFilterPage';
 
 export const tagsPagesTest = test.extend<{
 	tagsAdminPage: TagsAdminPage;
 	tagsEditPage: TagsEditPage;
+	tagsFilterPage: TagsFilterPage;
 }>({
 	tagsAdminPage: async ({page}, use) => {
 		await use(new TagsAdminPage(page));
 	},
 	tagsEditPage: async ({page}, use) => {
 		await use(new TagsEditPage(page));
+	},
+	tagsFilterPage: async ({page}, use) => {
+		await use(new TagsFilterPage(page));
 	},
 });
