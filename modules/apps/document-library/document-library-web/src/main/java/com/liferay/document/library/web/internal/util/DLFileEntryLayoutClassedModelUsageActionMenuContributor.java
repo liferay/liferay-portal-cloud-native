f9ab9d2c1fc3fc2cbe996f9ff86d1c3094fc76c2
Gergely Szalay <95410113+gergelyszalay@users.noreply.github.com>
2024-12-23 10:28:42 +0100

    LPD-43391 moving Contributor to util

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/util/DLFileEntryLayoutClassedModelUsageActionMenuContributor.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/util/DLFileEntryLayoutClassedModelUsageActionMenuContributor.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.document.library.web.internal.asset.util;
+package com.liferay.document.library.web.internal.util;
 
 import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.document.library.kernel.model.DLFileEntry;
