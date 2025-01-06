bbefc1c6ed48335d8f7d81c3c390b7c6b70bae2c
Brian Chan <brian.chan@liferay.com>
2025-01-07 07:44:53 +0800

    LPD-45168 Order matters, so add a line break

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v5_6_1/DDMFieldAttributeUpgradeProcess.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v5_6_1/DDMFieldAttributeUpgradeProcess.java
@@ -104,6 +104,7 @@ public class DDMFieldAttributeUpgradeProcess extends UpgradeProcess {
 					(smallAttributeValue.length() > 255)) {
 
 					largeAttributeValue = smallAttributeValue;
+
 					smallAttributeValue = null;
 				}
 
