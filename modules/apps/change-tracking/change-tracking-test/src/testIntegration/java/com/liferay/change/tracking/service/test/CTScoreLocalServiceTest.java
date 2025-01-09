ef26fbe83e13f97d9bc0e99efe246c61478905c9
David Truong <david.truong@liferay.com>
2025-01-08 20:15:48 -0600

    LPD-45587 add test to replicate issue

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTScoreLocalServiceTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTScoreLocalServiceTest.java
@@ -9,7 +9,9 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.blogs.service.BlogsEntryLocalService;
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTScoreLocalService;
 import com.liferay.document.library.kernel.model.DLFolder;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
 import com.liferay.document.library.test.util.DLTestUtil;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
@@ -20,6 +22,7 @@ import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -27,11 +30,20 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionConfig;
+import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.test.log.LogCapture;
+import com.liferay.portal.test.log.LogEntry;
+import com.liferay.portal.test.log.LoggerTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
+import java.util.List;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -64,6 +76,11 @@ public class CTScoreLocalServiceTest {
 			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
 			0, CTCollectionLocalServiceTest.class.getSimpleName(), null);
 		_group = GroupTestUtil.addGroup();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		GroupTestUtil.deleteGroup(_group);
 	}
 
 	@Test
@@ -128,6 +145,59 @@ public class CTScoreLocalServiceTest {
 		Assert.assertTrue(
 			_ctCollection3.getScore() > _ctCollection2.getScore());
 	}
+
+	@Test
+	public void testUpdateCTScore() throws Throwable {
+		_ctCollection1 = _ctCollectionLocalService.fetchCTCollection(
+			_ctCollection1.getCtCollectionId());
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection1.getCtCollectionId())) {
+
+			try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
+					"com.liferay.portal.internal.increment." +
+						"BufferedIncrementRunnable",
+					LoggerTestUtil.ERROR)) {
+
+				DLFolder dlFolder = DLTestUtil.addDLFolder(_group.getGroupId());
+
+				TransactionConfig transactionConfig =
+					TransactionConfig.Factory.create(
+						Propagation.REQUIRED, new Class<?>[] {Exception.class});
+
+				TransactionInvokerUtil.invoke(
+					transactionConfig,
+					() -> {
+						_ctScoreLocalService.incrementScore(
+							dlFolder.getCtCollectionId(),
+							_classNameLocalService.getClassNameId(
+								DLFolder.class));
+
+						_ctScoreLocalService.incrementScore(
+							dlFolder.getCtCollectionId(),
+							_classNameLocalService.getClassNameId(
+								DLFolder.class));
+
+						return null;
+					});
+
+				List<LogEntry> logEntries = logCapture.getLogEntries();
+
+				Assert.assertEquals(
+					logEntries.toString(), 0, logEntries.size());
+			}
+		}
+	}
+
+	@Inject
+	private static ClassNameLocalService _classNameLocalService;
+
+	@Inject
+	private static CTScoreLocalService _ctScoreLocalService;
+
+	@Inject
+	private static DLFileEntryLocalService _dlFileEntryLocalService;
 
 	@Inject
 	private BlogsEntryLocalService _blogsEntryLocalService;
