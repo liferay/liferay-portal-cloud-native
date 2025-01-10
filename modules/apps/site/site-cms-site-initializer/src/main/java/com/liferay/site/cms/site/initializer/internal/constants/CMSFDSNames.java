brianchandotcom/liferay-portal

LPD-42596 Rename FDS
Now that we prefix it with the bundle symbolic name, there's no need
for the "CMS" prefix.

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/constants/CMSFDSNames.java
@@ -10,7 +10,7 @@ package com.liferay.site.cms.site.initializer.internal.constants;
  */
 public class CMSFDSNames {
 
-	public static final String CMS_ALL_SECTIONS =
+	public static final String ALL_SECTIONS =
 		CMSSiteInitializerConstants.BUNDLE_SYMBOLIC_NAME + "-all-sections";
 
 }

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/AllSectionFragmentRenderer.java
@@ -84,7 +84,7 @@ public class AllSectionFragmentRenderer implements FragmentRenderer {
 			headlessDisplayTag.setBulkActionDropdownItems(new ArrayList<>());
 			headlessDisplayTag.setFdsActionDropdownItems(new ArrayList<>());
 			headlessDisplayTag.setFormName("fm");
-			headlessDisplayTag.setId(CMSFDSNames.CMS_ALL_SECTIONS);
+			headlessDisplayTag.setId(CMSFDSNames.ALL_SECTIONS);
 			headlessDisplayTag.setItemsPerPage(10);
 			headlessDisplayTag.setSelectedItemsKey("id");
 			headlessDisplayTag.setSelectionType("multiple");

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/view/table/AllSectionTableFDSView.java
@@ -21,7 +21,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Jürgen Kappler
  */
 @Component(
-	property = "frontend.data.set.name=" + CMSFDSNames.CMS_ALL_SECTIONS,
+	property = "frontend.data.set.name=" + CMSFDSNames.ALL_SECTIONS,
 	service = FDSView.class
 )
 public class AllSectionTableFDSView extends BaseTableFDSView {

liferay/liferay-portal-cloud-native

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/constants/CMSFDSNames.java
@@ -10,7 +10,7 @@ package com.liferay.site.cms.site.initializer.internal.constants;
  */
 public class CMSFDSNames {
 
-	public static final String CMS_ALL_SECTION =
+	public static final String ALL_SECTION =
 		CMSSiteInitializerConstants.BUNDLE_SYMBOLIC_NAME + "-all-section";
 
 }

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/fragment/renderer/AllSectionFragmentRenderer.java
@@ -84,7 +84,7 @@ public class AllSectionFragmentRenderer implements FragmentRenderer {
 			headlessDisplayTag.setBulkActionDropdownItems(new ArrayList<>());
 			headlessDisplayTag.setFdsActionDropdownItems(new ArrayList<>());
 			headlessDisplayTag.setFormName("fm");
-			headlessDisplayTag.setId(CMSFDSNames.CMS_ALL_SECTION);
+			headlessDisplayTag.setId(CMSFDSNames.ALL_SECTION);
 			headlessDisplayTag.setItemsPerPage(10);
 			headlessDisplayTag.setSelectedItemsKey("id");
 			headlessDisplayTag.setSelectionType("multiple");

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/view/table/AllSectionTableFDSView.java
@@ -21,7 +21,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Jürgen Kappler
  */
 @Component(
-	property = "frontend.data.set.name=" + CMSFDSNames.CMS_ALL_SECTION,
+	property = "frontend.data.set.name=" + CMSFDSNames.ALL_SECTION,
 	service = FDSView.class
 )
 public class AllSectionTableFDSView extends BaseTableFDSView {