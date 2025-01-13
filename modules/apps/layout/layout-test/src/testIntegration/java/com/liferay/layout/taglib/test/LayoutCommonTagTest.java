d0de4fb971bca158576455846d7f799762310423
Georgel Pop <georgel.pop@liferay.com>
2025-01-13 15:07:10 +0100

    LPD-45658 Add test case

---
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/taglib/test/LayoutCommonTagTest.java
@@ -0,0 +1,106 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.taglib.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.layout.taglib.servlet.taglib.LayoutCommonTag;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.test.TestInfo;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockPageContext;
+
+/**
+ * @author Georgel Pop
+ */
+@RunWith(Arquillian.class)
+public class LayoutCommonTagTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+	}
+
+	@Test
+	@TestInfo("LPD-45658")
+	public void testHtmlRenderingInSessionMessages() throws Exception {
+		LayoutCommonTag layoutCommonTag = new LayoutCommonTag();
+
+		layoutCommonTag.setDisplaySessionMessages(true);
+
+		String message =
+			"User's input with single quotes and <strong>tags</strong>";
+
+		MockHttpServletRequest mockHttpServletRequest =
+			_getMockHttpServletRequest(message);
+
+		MockHttpServletResponse mockHttpServletResponse =
+			new MockHttpServletResponse();
+
+		layoutCommonTag.setPageContext(
+			new MockPageContext(
+				null, mockHttpServletRequest, mockHttpServletResponse));
+
+		layoutCommonTag.doTag(mockHttpServletRequest, mockHttpServletResponse);
+
+		String content = mockHttpServletResponse.getContentAsString();
+
+		Assert.assertTrue(
+			content, content.contains(HtmlUtil.escapeJS(message)));
+	}
+
+	private MockHttpServletRequest _getMockHttpServletRequest(String message)
+		throws Exception {
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setLanguageId(_group.getDefaultLanguageId());
+		themeDisplay.setPermissionChecker(
+			PermissionThreadLocal.getPermissionChecker());
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		themeDisplay.setRequest(mockHttpServletRequest);
+
+		SessionMessages.add(
+			mockHttpServletRequest, "test_requestProcessedWarning", message);
+
+		return mockHttpServletRequest;
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
