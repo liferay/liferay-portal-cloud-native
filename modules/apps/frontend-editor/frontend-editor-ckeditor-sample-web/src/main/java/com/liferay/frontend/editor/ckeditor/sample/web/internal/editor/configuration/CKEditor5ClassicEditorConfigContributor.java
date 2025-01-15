10f51d79b9d8979d324e1689d4a24315de035815
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2025-01-15 09:29:43 +0100

    LPD-42913 - Refactor. Revert changes and leave default editor configuration

--- modules/apps/frontend-editor/frontend-editor-ckeditor-sample-web/src/main/java/com/liferay/frontend/editor/ckeditor/sample/web/internal/editor/configuration/CKEditor5ClassicEditorConfigContributor.java
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-sample-web/src/main/java/com/liferay/frontend/editor/ckeditor/sample/web/internal/editor/configuration/CKEditor5ClassicEditorConfigContributor.java
@@ -9,7 +9,6 @@ import com.liferay.frontend.editor.ckeditor.sample.web.internal.constants.CKEdit
 import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
 import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 
@@ -38,13 +37,7 @@ public class CKEditor5ClassicEditorConfigContributor
 
 		jsonObject.put(
 			"placeholder",
-			"This placeholder is set from EditorConfigContributor."
-		).put(
-			"toolbar",
-			JSONUtil.putAll(
-				"bold", "italic", "underline", "|", "undo", "redo", "|",
-				"alignment:left", "alignment:center", "alignment:right")
-		);
+			"This placeholder is set from EditorConfigContributor.");
 	}
 
 }
