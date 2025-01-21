1d5237a914a3b0242beaa69b8099fa8ca49e8354
alvarosaugarlr <alvaro.saugar@liferay.com>
2025-01-21 16:15:50 +0100

    LPS-206534 / LPD-4254 SF

--- modules/test/playwright/tests/set-admin-password/setAdminPassword.spec.ts
+++ modules/test/playwright/tests/set-admin-password/setAdminPassword.spec.ts
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -7,14 +7,14 @@ import {expect, test} from '@playwright/test';
 
 import {liferayConfig} from '../../liferay.config';
 
-
 test('LPD-4254 Checking what is the first page load if the property is blank', async ({
 	page,
 }) => {
 	await page.goto(liferayConfig.environment.baseUrl);
-	await expect(page.getByRole('heading', { name: 'Set Password' }))
-		.toBeVisible({
-			timeout: 10 * 1000,
-		});
 
+	await expect(page.getByRole('heading', {name: 'Set Password'})).toBeVisible(
+		{
+			timeout: 10 * 1000,
+		}
+	);
 });
