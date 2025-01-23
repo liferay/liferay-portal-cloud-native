04e6808dcc435215b91ec68aa7dce44aababf243
Brian Chan <brian.chan@liferay.com>
2025-01-23 23:19:11 +0900

    LPD-42809 I didn't see the reference to _bundleContext in _serviceTrackerCustomizer#removedService

--- modules/apps/frontend-js/frontend-js-importmaps-extender/src/main/java/com/liferay/frontend/js/importmaps/extender/internal/servlet/taglib/JSImportMapsExtenderTopHeadDynamicInclude.java
+++ modules/apps/frontend-js/frontend-js-importmaps-extender/src/main/java/com/liferay/frontend/js/importmaps/extender/internal/servlet/taglib/JSImportMapsExtenderTopHeadDynamicInclude.java
@@ -122,12 +122,13 @@ public class JSImportMapsExtenderTopHeadDynamicInclude
 
 	@Deactivate
 	protected void deactivate() {
-		_bundleContext = null;
 		_jsImportMapsCache = null;
 
 		_serviceTracker.close();
 
 		_serviceTracker = null;
+
+		_bundleContext = null;
 	}
 
 	@Modified
