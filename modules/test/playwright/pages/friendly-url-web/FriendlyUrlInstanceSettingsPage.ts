99fed2b3cc90ea1c5e0f49c1932f7e307b0e2d03
Jorge González <jorge.gonzalez@liferay.com>
2025-01-31 14:37:04 +0100

    LPD-47849 Add missing dependency

--- modules/test/playwright/pages/friendly-url-web/FriendlyUrlInstanceSettingsPage.ts
+++ modules/test/playwright/pages/friendly-url-web/FriendlyUrlInstanceSettingsPage.ts
@@ -5,6 +5,7 @@
 
 import {Locator, Page} from '@playwright/test';
 
+import {clickAndExpectToBeHidden} from '../../utils/clickAndExpectToBeHidden';
 import {waitForAlert} from '../../utils/waitForAlert';
 import {InstanceSettingsPage} from '../configuration-admin-web/InstanceSettingsPage';
 
