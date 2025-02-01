adolfopa/liferay-portal

LPD-42474 Upgrade process

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v10_1_0/util/ObjectEntryFolderTable.java
@@ -1,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.upgrade.v10_1_0.util;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ * @see com.liferay.portal.tools.upgrade.table.builder.UpgradeTableBuilder
+ */
+public class ObjectEntryFolderTable {
+
+	public static UpgradeProcess create() {
+		return new UpgradeProcess() {
+
+			@Override
+			protected void doUpgrade() throws Exception {
+				if (!hasTable(_TABLE_NAME)) {
+					runSQL(_TABLE_SQL_CREATE);
+				}
+			}
+
+		};
+	}
+
+	private static final String _TABLE_NAME = "ObjectEntryFolder";
+
+	private static final String _TABLE_SQL_CREATE =
+		"create table ObjectEntryFolder (mvccVersion LONG default 0 not null,uuid_ VARCHAR(75) null,objectEntryFolderId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,externalReferenceCode VARCHAR(75) null,label STRING null,parentObjectEntryFolderId LONG)";
+
+}

liferay/liferay-portal-cloud-native

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/v10_4_0/util/ObjectEntryFolderTable.java
@@ -1,0 +1,35 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.internal.upgrade.v10_4_0.util;
+
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ * @see com.liferay.portal.tools.upgrade.table.builder.UpgradeTableBuilder
+ */
+public class ObjectEntryFolderTable {
+
+	public static UpgradeProcess create() {
+		return new UpgradeProcess() {
+
+			@Override
+			protected void doUpgrade() throws Exception {
+				if (!hasTable(_TABLE_NAME)) {
+					runSQL(_TABLE_SQL_CREATE);
+				}
+			}
+
+		};
+	}
+
+	private static final String _TABLE_NAME = "ObjectEntryFolder";
+
+	private static final String _TABLE_SQL_CREATE =
+		"create table ObjectEntryFolder (mvccVersion LONG default 0 not null,uuid_ VARCHAR(75) null,externalReferenceCode VARCHAR(75) null,objectEntryFolderId LONG not null primary key,groupId LONG,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,label STRING null,name VARCHAR(75) null,parentObjectEntryFolderId LONG,treePath STRING null)";
+
+}