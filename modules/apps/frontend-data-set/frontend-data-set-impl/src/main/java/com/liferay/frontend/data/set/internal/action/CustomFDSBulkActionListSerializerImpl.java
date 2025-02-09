dsanz/liferay-portal

LPD-44159 Provide FDS bulk action list serializers for system data sets, with unit tests. Custom data sets don't yet support definition of bulk actions so we provide a placeholder implementation to facilitate reference handling in the renderer

modules/apps/frontend-data-set/frontend-data-set-impl/src/main/java/com/liferay/frontend/data/set/internal/action/CustomFDSBulkActionListSerializerImpl.java
@@ -1,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set.internal.action;
+
+import com.liferay.frontend.data.set.action.FDSBulkActionListSerializer;
+import com.liferay.frontend.data.set.internal.serializer.BaseCustomFDSSerializer;
+import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
+import com.liferay.frontend.data.set.serializer.FDSSerializer;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Daniel Sanz
+ */
+@Component(
+	property = "frontend.data.set.serializer.type=" + FDSSerializer.CUSTOM,
+	service = FDSBulkActionListSerializer.class
+)
+public class CustomFDSBulkActionListSerializerImpl
+	extends BaseCustomFDSSerializer implements FDSBulkActionListSerializer {
+
+	@Override
+	public List<FDSActionDropdownItem> serialize(
+		String fdsName, HttpServletRequest httpServletRequest) {
+
+		// TODO: add support for bulk actions in the DSM
+
+		return Collections.emptyList();
+	}
+
+}

modules/apps/frontend-data-set/frontend-data-set-impl/src/main/java/com/liferay/frontend/data/set/internal/action/SystemFDSBulkActionListSerializerImpl.java
@@ -1,0 +1,49 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set.internal.action;
+
+import com.liferay.frontend.data.set.action.FDSBulkActionList;
+import com.liferay.frontend.data.set.action.FDSBulkActionListRegistry;
+import com.liferay.frontend.data.set.action.FDSBulkActionListSerializer;
+import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
+import com.liferay.frontend.data.set.serializer.FDSSerializer;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Daniel Sanz
+ */
+@Component(
+	property = "frontend.data.set.serializer.type=" + FDSSerializer.SYSTEM,
+	service = FDSBulkActionListSerializer.class
+)
+public class SystemFDSBulkActionListSerializerImpl
+	implements FDSBulkActionListSerializer {
+
+	@Override
+	public List<FDSActionDropdownItem> serialize(
+		String fdsName, HttpServletRequest httpServletRequest) {
+
+		FDSBulkActionList fdsBulkActionList =
+			_fdsBulkActionListRegistry.getFDSBulkActionList(fdsName);
+
+		if (fdsBulkActionList == null) {
+			return Collections.emptyList();
+		}
+
+		return fdsBulkActionList.getFDSActionDropdownItems(httpServletRequest);
+	}
+
+	@Reference
+	private FDSBulkActionListRegistry _fdsBulkActionListRegistry;
+
+}

liferay/liferay-portal-cloud-native

modules/apps/frontend-data-set/frontend-data-set-impl/src/main/java/com/liferay/frontend/data/set/internal/action/CustomFDSBulkActionListSerializerImpl.java
@@ -1,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set.internal.action;
+
+import com.liferay.frontend.data.set.action.FDSBulkActionListSerializer;
+import com.liferay.frontend.data.set.internal.serializer.BaseCustomFDSSerializer;
+import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
+import com.liferay.frontend.data.set.serializer.FDSSerializer;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Daniel Sanz
+ */
+@Component(
+	property = "frontend.data.set.serializer.type=" + FDSSerializer.TYPE_CUSTOM,
+	service = FDSBulkActionListSerializer.class
+)
+public class CustomFDSBulkActionListSerializerImpl
+	extends BaseCustomFDSSerializer implements FDSBulkActionListSerializer {
+
+	@Override
+	public List<FDSActionDropdownItem> serialize(
+		String fdsName, HttpServletRequest httpServletRequest) {
+
+		// TODO: add support for bulk actions in the DSM
+
+		return Collections.emptyList();
+	}
+
+}

modules/apps/frontend-data-set/frontend-data-set-impl/src/main/java/com/liferay/frontend/data/set/internal/action/SystemFDSBulkActionListSerializerImpl.java
@@ -1,0 +1,49 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.data.set.internal.action;
+
+import com.liferay.frontend.data.set.action.FDSBulkActionList;
+import com.liferay.frontend.data.set.action.FDSBulkActionListRegistry;
+import com.liferay.frontend.data.set.action.FDSBulkActionListSerializer;
+import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
+import com.liferay.frontend.data.set.serializer.FDSSerializer;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Daniel Sanz
+ */
+@Component(
+	property = "frontend.data.set.serializer.type=" + FDSSerializer.TYPE_SYSTEM,
+	service = FDSBulkActionListSerializer.class
+)
+public class SystemFDSBulkActionListSerializerImpl
+	implements FDSBulkActionListSerializer {
+
+	@Override
+	public List<FDSActionDropdownItem> serialize(
+		String fdsName, HttpServletRequest httpServletRequest) {
+
+		FDSBulkActionList fdsBulkActionList =
+			_fdsBulkActionListRegistry.getFDSBulkActionList(fdsName);
+
+		if (fdsBulkActionList == null) {
+			return Collections.emptyList();
+		}
+
+		return fdsBulkActionList.getFDSActionDropdownItems(httpServletRequest);
+	}
+
+	@Reference
+	private FDSBulkActionListRegistry _fdsBulkActionListRegistry;
+
+}