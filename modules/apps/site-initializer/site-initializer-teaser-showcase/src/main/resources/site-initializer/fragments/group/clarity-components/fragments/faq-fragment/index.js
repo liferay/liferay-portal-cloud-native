a224990570a932f0d28a38df18c39bf3c29ccd22
Mumen Tayyem <mumen_tayyem@outlook.com>
2025-02-12 18:29:40 +0400

    LPD-45276 SF

--- modules/apps/site-initializer/site-initializer-teaser-showcase/src/main/resources/site-initializer/fragments/group/clarity-components/fragments/faq-fragment/index.js
+++ modules/apps/site-initializer/site-initializer-teaser-showcase/src/main/resources/site-initializer/fragments/group/clarity-components/fragments/faq-fragment/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+
 /**
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
@@ -19,20 +20,18 @@ for (let i = 0; i < customIcons.length; i++) {
 }
 
 function toggleVisability() {
-	console.log("toggleVisability");
-		const flag = faqList.classList.contains('collapse');
-					
-		if (flag) {
-			faqList.classList.remove('collapse');
-			plusIcon.classList.add('d-none');
-			minusIcon.classList.remove('d-none');
-			question.classList.add('text-primary');
-			
-		}
-		else {
-			faqList.classList.add('collapse');
-			plusIcon.classList.remove('d-none');
-			minusIcon.classList.add('d-none');
-			question.classList.remove('text-primary');
-		}
-	};
+	const flag = faqList.classList.contains('collapse');
+
+	if (flag) {
+		faqList.classList.remove('collapse');
+		plusIcon.classList.add('d-none');
+		minusIcon.classList.remove('d-none');
+		question.classList.add('text-primary');
+	}
+	else {
+		faqList.classList.add('collapse');
+		plusIcon.classList.remove('d-none');
+		minusIcon.classList.add('d-none');
+		question.classList.remove('text-primary');
+	}
+}
