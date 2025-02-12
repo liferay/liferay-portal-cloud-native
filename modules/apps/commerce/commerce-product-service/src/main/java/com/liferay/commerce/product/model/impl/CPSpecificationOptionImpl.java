8b8e354ab5c575e744e6edae233df7336fbb3e06
monaco-fabio <fabio.monaco@liferay.com>
2025-02-12 14:42:25 +0100

    LPD-46276 Fix recursive product window reopens when saving specification value
    # breaking
    
    ## What modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPSpecificationOptionImpl.java
    
    modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPSpecificationOptionImpl.java not longer includes the getListTypeEntries() method
    
    ## Why
    
    because this method was called only in the EditCPDefinitionSpecificationOptionValueMVCActionCommand class and to avoid using the Util classes
    
    ----

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPSpecificationOptionImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPSpecificationOptionImpl.java
@@ -11,10 +11,7 @@ import com.liferay.commerce.product.model.CPSpecificationOptionListTypeDefinitio
 import com.liferay.commerce.product.service.CPOptionCategoryLocalServiceUtil;
 import com.liferay.commerce.product.service.CPSpecificationOptionListTypeDefinitionRelLocalServiceUtil;
 import com.liferay.list.type.model.ListTypeDefinition;
-import com.liferay.list.type.model.ListTypeEntry;
 import com.liferay.list.type.service.ListTypeDefinitionServiceUtil;
-import com.liferay.list.type.service.ListTypeEntryLocalServiceUtil;
-import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 
 import java.util.ArrayList;
@@ -71,17 +68,6 @@ public class CPSpecificationOptionImpl extends CPSpecificationOptionBaseImpl {
 		List<ListTypeDefinition> listTypeDefinitions = getListTypeDefinitions();
 
 		return listTypeDefinitions.size();
-	}
-
-	@Override
-	public List<ListTypeEntry> getListTypeEntries() {
-		return ListTypeEntryLocalServiceUtil.getListTypeEntries(
-			TransformUtil.transformToLongArray(
-				CPSpecificationOptionListTypeDefinitionRelLocalServiceUtil.
-					getCPSpecificationOptionListTypeDefinitionRels(
-						getCPSpecificationOptionId()),
-				CPSpecificationOptionListTypeDefinitionRel::
-					getListTypeDefinitionId));
 	}
 
 }
