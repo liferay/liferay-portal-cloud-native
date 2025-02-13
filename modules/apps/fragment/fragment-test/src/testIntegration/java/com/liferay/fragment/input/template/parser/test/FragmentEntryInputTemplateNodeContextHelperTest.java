ced1be80dbc767f1948769b6328406de61f9ed1c
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-02-13 10:22:14 +0100

    LPD-48832 Fix parameter order

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/input/template/parser/test/FragmentEntryInputTemplateNodeContextHelperTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/input/template/parser/test/FragmentEntryInputTemplateNodeContextHelperTest.java
@@ -445,12 +445,12 @@ public class FragmentEntryInputTemplateNodeContextHelperTest {
 
 		_testToInputTemplateNodeLocalizedInputValue(
 			objectDefinition.getClassName(), "ObjectField_myDate", objectEntry,
+			Collections.emptyMap(),
 			HashMapBuilder.put(
 				LocaleUtil.SPAIN, enValue
 			).put(
 				LocaleUtil.US, enValue
-			).build(),
-			Collections.emptyMap());
+			).build());
 
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(
 			"yyyy-MM-dd'T'HH:mm");
@@ -459,13 +459,12 @@ public class FragmentEntryInputTemplateNodeContextHelperTest {
 
 		_testToInputTemplateNodeLocalizedInputValue(
 			objectDefinition.getClassName(), "ObjectField_myDateTime",
-			objectEntry,
+			objectEntry, Collections.emptyMap(),
 			HashMapBuilder.put(
 				LocaleUtil.SPAIN, enValue
 			).put(
 				LocaleUtil.US, enValue
-			).build(),
-			Collections.emptyMap());
+			).build());
 
 		enValue = enDateFormat.format(enDate);
 
