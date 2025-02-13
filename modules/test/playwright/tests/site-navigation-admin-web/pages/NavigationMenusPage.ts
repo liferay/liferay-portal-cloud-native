d92e365edb2f620b52046f48b732b1b4763b76f0
Joao Victor <joao.victor@liferay.com>
2025-02-13 17:10:38 -0300

    LPD-48696 Add playwright tests to cover the NavigationMenuAccessibility.testcase test file

--- modules/test/playwright/tests/site-navigation-admin-web/pages/NavigationMenusPage.ts
+++ modules/test/playwright/tests/site-navigation-admin-web/pages/NavigationMenusPage.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Locator, Page} from '@playwright/test';
+import {FrameLocator, Locator, Page} from '@playwright/test';
 
 import {clickAndExpectToBeVisible} from '../../../utils/clickAndExpectToBeVisible';
 import {PORTLET_URLS} from '../../../utils/portletUrls';
@@ -12,14 +12,28 @@ import {waitForAlert} from '../../../utils/waitForAlert';
 export class NavigationMenusPage {
 	readonly page: Page;
 
+	readonly addChildButton: Locator;
 	readonly addItemButton: Locator;
+	readonly blogsModal: FrameLocator;
+	readonly categoriesModal: FrameLocator;
 	readonly newButton: Locator;
+	readonly pagesModal: FrameLocator;
+	readonly selectButton: Locator;
 
 	constructor(page: Page) {
 		this.page = page;
 
+		this.addChildButton = page.getByRole('menuitem', {name: 'Add Child'});
 		this.addItemButton = page.getByLabel('Add Menu Item');
+		this.blogsModal = page.frameLocator(
+			'iframe[title="Select Blogs Entry"]'
+		);
+		this.categoriesModal = page.frameLocator(
+			'iframe[title="Select Categories"]'
+		);
 		this.newButton = page.getByRole('button', {name: 'Add'});
+		this.pagesModal = page.frameLocator('iframe[title="Select Pages"]');
+		this.selectButton = page.getByRole('button', {name: 'Select'});
 	}
 
 	async goto(siteUrl?: Site['friendlyUrlPath']) {
@@ -52,8 +66,35 @@ export class NavigationMenusPage {
 			trigger: this.addItemButton,
 		});
 
-		const modal = this.page.frameLocator('iframe[title="Select Pages"]');
+		await this.pagesModal.getByPlaceholder('Search').waitFor();
+	}
 
-		await modal.getByPlaceholder('Search').waitFor();
+	async openAddChildCategoryModal(parentName: string) {
+		await this.page.getByText(parentName, {exact: true}).hover();
+
+		await this.page.getByLabel(`View ${parentName} Options`).click();
+
+		await this.addChildButton.hover();
+
+		await this.page
+			.getByRole('menuitem', {
+				name: 'Category',
+			})
+			.click();
+
+		await this.categoriesModal.getByPlaceholder('Search').waitFor();
+	}
+
+	async openAddCategoryModal() {
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: this.page.getByRole('menuitem', {
+				exact: true,
+				name: 'Category',
+			}),
+			trigger: this.addItemButton,
+		});
+
+		await this.categoriesModal.getByPlaceholder('Search').waitFor();
 	}
 }
