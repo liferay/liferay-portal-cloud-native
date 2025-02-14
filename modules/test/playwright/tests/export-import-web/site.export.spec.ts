3b505f32b1a21b0c73f9f0759180d3fafe24716c
Alberto Moreno <alberto.moreno@liferay.com>
2025-02-14 09:38:48 +0100

    LPD-47216 Create test to make sure the user can establish a custom export task name at site level

--- modules/test/playwright/tests/export-import-web/site.export.spec.ts
+++ modules/test/playwright/tests/export-import-web/site.export.spec.ts
@@ -10,6 +10,7 @@ import {dataApiHelpersTest} from '../../fixtures/dataApiHelpersTest';
 import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {loginTest} from '../../fixtures/loginTest';
 import {productMenuPageTest} from '../../fixtures/productMenuPageTest';
+import getRandomString from '../../utils/getRandomString';
 import {getTempDir} from '../../utils/temp';
 import {exportImportPagesTest} from './fixtures/exportImportPagesTest';
 
@@ -50,6 +51,28 @@ async function expectExportName(exportImportPage, taskName: string) {
 
 	expect(exportFilePath).toMatch(new RegExp(`^${getTempDir()}${taskName}-`));
 }
+
+test('can export at site level with custom export task name', async ({
+	exportImportPage,
+}) => {
+	await exportImportPage.goToExport();
+
+	const taskName = 'MyExport-' + getRandomString();
+
+	await exportImportPage.createNewExportProcess(taskName);
+
+	await expect(
+		exportImportPage.page
+			.getByText(taskName)
+			.locator('../..')
+			.getByText('Successful')
+	).toBeVisible();
+
+	const exportFilePath =
+		await exportImportPage.downloadExportProcess(taskName);
+
+	expect(exportFilePath).toMatch(new RegExp(`^${getTempDir()}MyExport-`));
+});
 
 test('can export at site level with old file name', async ({
 	exportImportPage,
