58ab2c48727c58c51f87e1e323202ceca1453348
rafaellajordao <rafaella.jordao@liferay.com>
2025-02-17 15:32:23 -0300

    LPD-46610 add 'Search using special characters shown in search overview' test

--- modules/test/playwright/tests/osb-faro-web/search-term.spec.ts
+++ modules/test/playwright/tests/osb-faro-web/search-term.spec.ts
@@ -164,12 +164,100 @@ test(
 				page.getByRole('cell', {exact: true, name: '1'})
 			).toBeVisible();
 		});
+	}
+);
 
-		await test.step('delete channel', async () => {
-			await apiHelpers.jsonWebServicesOSBFaro.deleteChannel(
-				`[${channel.id}]`,
-				project.groupId
+test(
+	'Search using special characters shown in search overview',
+
+	{
+		tag: '@LRAC-11056',
+	},
+
+	async ({apiHelpers, page}) => {
+		const generateIndividual = (name) => {
+			const id = getRandomString();
+
+			return {
+				id,
+				name,
+			};
+		};
+
+		const individuals = [generateIndividual('ac')];
+
+		await test.step('Create Individuals', async () => {
+			await createIndividuals({
+				apiHelpers,
+				individuals,
+			});
+		});
+
+		await test.step('Create Individuals Events', async () => {
+			const date = new Date();
+
+			await apiHelpers.jsonWebServicesOSBAsah.createEvents(
+				individuals.map((individual) => ({
+					applicationId: 'Page',
+					canonicalUrl: `https://${liferayConfig.environment.baseUrl}/search`,
+					channelId: channel.id,
+					eventDate: date.toISOString(),
+					eventId: 'pageViewed',
+					title: 'Liferay',
+					url: `https://${liferayConfig.environment.baseUrl}/search?q=atenção com a criança,子供との注意,atención al niño`,
+					userId: individual.id,
+				}))
 			);
+		});
+
+		await test.step('Go to Analytics Cloud and Switch the property', async () => {
+			await navigateToACPageViaURL({
+				acPage: ACPage.sitePage,
+				channelID: channel.id,
+				page,
+				projectID: project.groupId,
+			});
+		});
+
+		await test.step('Change time filter in Search Terms to Last 24 Hours and click on "All Search Terms" link', async () => {
+			await changeTimeFilter({
+				cardSelector: CardSelectors.SearchTerms,
+				page,
+				timeFilterPeriod: 'Last 24 hours',
+			});
+
+			await navigateTo({
+				page,
+				pageName: 'All Search Terms',
+			});
+		});
+
+		await test.step('Check if user is in the Search Terms tab with the same time filter previously set (Last 24h)', async () => {
+			await expect(
+				page.getByRole('link', {exact: true, name: 'Search Terms'})
+			).toBeVisible();
+
+			await expect(
+				page.getByText(`Search Terms on ${channelName}`)
+			).toBeVisible();
+
+			await expect(
+				page.getByRole('button', {name: 'Last 24 hours'})
+			).toBeVisible();
+		});
+
+		await test.step('Check if count and percentage are correct in Search Terms tab', async () => {
+			await viewNameOnTableList({
+				itemNames: [
+					'atenção com a criança,子供との注意,atención al niño',
+					'100.00%',
+				],
+				page,
+			});
+
+			await expect(
+				page.getByRole('cell', {exact: true, name: '1'})
+			).toBeVisible();
 		});
 	}
 );
