18b7962ecc3c7a24c676622943b1370634abf248
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-02-18 09:24:39 -0300

    LPD-48341 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/util/FileEntryMVCActionCommandUtil.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/util/FileEntryMVCActionCommandUtil.java
@@ -6,47 +6,37 @@
 package com.liferay.dynamic.data.mapping.form.web.internal.portlet.action.util;
 
 import com.liferay.document.library.kernel.model.DLFileEntry;
-import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
 import com.liferay.dynamic.data.mapping.util.DDMFormUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import javax.portlet.PortletRequest;
 
 /**
  * @author Igor Costa
  */
-public class DLFileEntryUtil {
+public class FileEntryMVCActionCommandUtil {
 
-	public static void deleteDLFileEntry(
-			PortletRequest portletRequest,
-			DLFileEntryLocalService dlFileEntryLocalService)
+	public static void deleteFileEntry(
+			long fileEntryId, ThemeDisplay themeDisplay)
 		throws PortalException {
-
-		long fileEntryId = ParamUtil.getLong(portletRequest, "oldFileEntryId");
 
 		if (fileEntryId == 0) {
 			return;
 		}
 
-		DLFileEntry dlFileEntry = dlFileEntryLocalService.fetchDLFileEntry(
+		DLFileEntry dlFileEntry = DLFileEntryLocalServiceUtil.fetchDLFileEntry(
 			fileEntryId);
 
 		if (dlFileEntry == null) {
 			return;
 		}
 
-		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
 		User user = DDMFormUtil.getDDMFormDefaultUser(
 			themeDisplay.getCompanyId());
 
 		if (dlFileEntry.getUserId() == user.getUserId()) {
-			dlFileEntryLocalService.deleteFileEntry(fileEntryId);
+			DLFileEntryLocalServiceUtil.deleteFileEntry(fileEntryId);
 		}
 	}
 
