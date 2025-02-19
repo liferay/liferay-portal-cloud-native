liferay-content-management/liferay-portal

LPD-48254 Build service

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryFolderLocalService.java
@@ -124,6 +124,10 @@ public interface ObjectEntryFolderLocalService
 			ObjectEntryFolder objectEntryFolder)
 		throws PortalException;
 
+	public ObjectEntryFolder deleteObjectEntryFolder(
+			String externalReferenceCode, long companyId, long groupId)
+		throws PortalException;
+
 	/**
 	 * @throws PortalException
 	 */
@@ -265,6 +269,11 @@ public interface ObjectEntryFolderLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<ObjectEntryFolder> getObjectEntryFolders(int start, int end);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<ObjectEntryFolder> getObjectEntryFolders(
+		long groupId, long companyId, long parentObjectEntryFolderId, int start,
+		int end);
+
 	/**
 	 * Returns all the object entry folders matching the UUID and company.
 	 *
@@ -298,6 +307,10 @@ public interface ObjectEntryFolderLocalService
 	 */
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getObjectEntryFoldersCount();
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getObjectEntryFoldersCount(
+		long groupId, long companyId, long parentObjectEntryFolderId);
 
 	/**
 	 * Returns the OSGi service identifier.

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryFolderLocalServiceWrapper.java
@@ -126,6 +126,15 @@ public class ObjectEntryFolderLocalServiceWrapper
 			objectEntryFolder);
 	}
 
+	@Override
+	public com.liferay.object.model.ObjectEntryFolder deleteObjectEntryFolder(
+			String externalReferenceCode, long companyId, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _objectEntryFolderLocalService.deleteObjectEntryFolder(
+			externalReferenceCode, companyId, groupId);
+	}
+
 	/**
 	 * @throws PortalException
 	 */
@@ -340,6 +349,16 @@ public class ObjectEntryFolderLocalServiceWrapper
 		return _objectEntryFolderLocalService.getObjectEntryFolders(start, end);
 	}
 
+	@Override
+	public java.util.List<com.liferay.object.model.ObjectEntryFolder>
+		getObjectEntryFolders(
+			long groupId, long companyId, long parentObjectEntryFolderId,
+			int start, int end) {
+
+		return _objectEntryFolderLocalService.getObjectEntryFolders(
+			groupId, companyId, parentObjectEntryFolderId, start, end);
+	}
+
 	/**
 	 * Returns all the object entry folders matching the UUID and company.
 	 *
@@ -386,6 +405,14 @@ public class ObjectEntryFolderLocalServiceWrapper
 	@Override
 	public int getObjectEntryFoldersCount() {
 		return _objectEntryFolderLocalService.getObjectEntryFoldersCount();
+	}
+
+	@Override
+	public int getObjectEntryFoldersCount(
+		long groupId, long companyId, long parentObjectEntryFolderId) {
+
+		return _objectEntryFolderLocalService.getObjectEntryFoldersCount(
+			groupId, companyId, parentObjectEntryFolderId);
 	}
 
 	/**

liferay/liferay-portal-cloud-native

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryFolderLocalService.java
@@ -125,6 +125,10 @@ public interface ObjectEntryFolderLocalService
 			ObjectEntryFolder objectEntryFolder)
 		throws PortalException;
 
+	public ObjectEntryFolder deleteObjectEntryFolder(
+			String externalReferenceCode, long companyId, long groupId)
+		throws PortalException;
+
 	/**
 	 * @throws PortalException
 	 */
@@ -266,6 +270,11 @@ public interface ObjectEntryFolderLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<ObjectEntryFolder> getObjectEntryFolders(int start, int end);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<ObjectEntryFolder> getObjectEntryFolders(
+		long companyId, long groupId, long parentObjectEntryFolderId, int start,
+		int end);
+
 	/**
 	 * Returns all the object entry folders matching the UUID and company.
 	 *
@@ -299,6 +308,10 @@ public interface ObjectEntryFolderLocalService
 	 */
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getObjectEntryFoldersCount();
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getObjectEntryFoldersCount(
+		long companyId, long groupId, long parentObjectEntryFolderId);
 
 	/**
 	 * Returns the OSGi service identifier.

modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryFolderLocalServiceWrapper.java
@@ -126,6 +126,15 @@ public class ObjectEntryFolderLocalServiceWrapper
 			objectEntryFolder);
 	}
 
+	@Override
+	public com.liferay.object.model.ObjectEntryFolder deleteObjectEntryFolder(
+			String externalReferenceCode, long companyId, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _objectEntryFolderLocalService.deleteObjectEntryFolder(
+			externalReferenceCode, companyId, groupId);
+	}
+
 	/**
 	 * @throws PortalException
 	 */
@@ -340,6 +349,16 @@ public class ObjectEntryFolderLocalServiceWrapper
 		return _objectEntryFolderLocalService.getObjectEntryFolders(start, end);
 	}
 
+	@Override
+	public java.util.List<com.liferay.object.model.ObjectEntryFolder>
+		getObjectEntryFolders(
+			long companyId, long groupId, long parentObjectEntryFolderId,
+			int start, int end) {
+
+		return _objectEntryFolderLocalService.getObjectEntryFolders(
+			companyId, groupId, parentObjectEntryFolderId, start, end);
+	}
+
 	/**
 	 * Returns all the object entry folders matching the UUID and company.
 	 *
@@ -386,6 +405,14 @@ public class ObjectEntryFolderLocalServiceWrapper
 	@Override
 	public int getObjectEntryFoldersCount() {
 		return _objectEntryFolderLocalService.getObjectEntryFoldersCount();
+	}
+
+	@Override
+	public int getObjectEntryFoldersCount(
+		long companyId, long groupId, long parentObjectEntryFolderId) {
+
+		return _objectEntryFolderLocalService.getObjectEntryFoldersCount(
+			companyId, groupId, parentObjectEntryFolderId);
 	}
 
 	/**