liferay-site-management/liferay-portal

LPD-48112 Build service

portal-impl/src/com/liferay/portlet/asset/service/http/AssetVocabularyGroupRelServiceHttp.java
@@ -5,13 +5,21 @@
 
 package com.liferay.portlet.asset.service.http;
 
+import com.liferay.asset.kernel.service.AssetVocabularyGroupRelServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.HttpPrincipal;
+import com.liferay.portal.kernel.service.http.TunnelUtil;
+import com.liferay.portal.kernel.util.MethodHandler;
+import com.liferay.portal.kernel.util.MethodKey;
+
 /**
  * Provides the HTTP utility for the
- * <code>com.liferay.asset.kernel.service.AssetVocabularyGroupRelServiceUtil</code> service
+ * <code>AssetVocabularyGroupRelServiceUtil</code> service
  * utility. The
  * static methods of this class calls the same methods of the service utility.
  * However, the signatures are different because it requires an additional
- * <code>com.liferay.portal.kernel.security.auth.HttpPrincipal</code> parameter.
+ * <code>HttpPrincipal</code> parameter.
  *
  * <p>
  * The benefits of using the HTTP utility is that it is fast and allows for
@@ -32,4 +40,143 @@ package com.liferay.portlet.asset.service.http;
  * @generated
  */
 public class AssetVocabularyGroupRelServiceHttp {
+
+	public static com.liferay.asset.kernel.model.AssetVocabularyGroupRel
+			addAssetVocabularyGroupRel(
+				HttpPrincipal httpPrincipal, long vocabularyId, long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				AssetVocabularyGroupRelServiceUtil.class,
+				"addAssetVocabularyGroupRel",
+				_addAssetVocabularyGroupRelParameterTypes0);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, vocabularyId, groupId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (com.liferay.asset.kernel.model.AssetVocabularyGroupRel)
+				returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static java.util.List
+		<com.liferay.asset.kernel.model.AssetVocabularyGroupRel>
+				getAssetVocabularyGroupRelsByVocabularyId(
+					HttpPrincipal httpPrincipal, long vocabularyId)
+			throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				AssetVocabularyGroupRelServiceUtil.class,
+				"getAssetVocabularyGroupRelsByVocabularyId",
+				_getAssetVocabularyGroupRelsByVocabularyIdParameterTypes1);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, vocabularyId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (java.util.List
+				<com.liferay.asset.kernel.model.AssetVocabularyGroupRel>)
+					returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static void setAssetVocabularyGroupRels(
+			HttpPrincipal httpPrincipal, long vocabularyId, long[] groupIds)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				AssetVocabularyGroupRelServiceUtil.class,
+				"setAssetVocabularyGroupRels",
+				_setAssetVocabularyGroupRelsParameterTypes2);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, vocabularyId, groupIds);
+
+			try {
+				TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		AssetVocabularyGroupRelServiceHttp.class);
+
+	private static final Class<?>[] _addAssetVocabularyGroupRelParameterTypes0 =
+		new Class[] {long.class, long.class};
+	private static final Class<?>[]
+		_getAssetVocabularyGroupRelsByVocabularyIdParameterTypes1 =
+			new Class[] {long.class};
+	private static final Class<?>[]
+		_setAssetVocabularyGroupRelsParameterTypes2 = new Class[] {
+			long.class, long[].class
+		};
+
 }

portal-kernel/src/com/liferay/asset/kernel/service/AssetVocabularyGroupRelLocalServiceUtil.java
@@ -52,6 +52,13 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 		return getService().addAssetVocabularyGroupRel(assetVocabularyGroupRel);
 	}
 
+	public static AssetVocabularyGroupRel addAssetVocabularyGroupRel(
+			long vocabularyId, long groupId)
+		throws PortalException {
+
+		return getService().addAssetVocabularyGroupRel(vocabularyId, groupId);
+	}
+
 	/**
 	 * Creates a new asset vocabulary group rel with the primary key. Does not add the asset vocabulary group rel to the database.
 	 *
@@ -109,6 +116,16 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 
 		return getService().deleteAssetVocabularyGroupRel(
 			assetVocabularyGroupRelId);
+	}
+
+	public static void deleteAssetVocabularyGroupRelsByGroupId(long groupId) {
+		getService().deleteAssetVocabularyGroupRelsByGroupId(groupId);
+	}
+
+	public static void deleteAssetVocabularyGroupRelsByVocabularyId(
+		long vocabularyId) {
+
+		getService().deleteAssetVocabularyGroupRelsByVocabularyId(vocabularyId);
 	}
 
 	/**
@@ -283,6 +300,12 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 		return getService().getAssetVocabularyGroupRels(start, end);
 	}
 
+	public static List<AssetVocabularyGroupRel>
+		getAssetVocabularyGroupRelsByGroupId(long groupId) {
+
+		return getService().getAssetVocabularyGroupRelsByGroupId(groupId);
+	}
+
 	/**
 	 * Returns all the asset vocabulary group rels matching the UUID and company.
 	 *
@@ -317,6 +340,13 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 			uuid, companyId, start, end, orderByComparator);
 	}
 
+	public static List<AssetVocabularyGroupRel>
+		getAssetVocabularyGroupRelsByVocabularyId(long vocabularyId) {
+
+		return getService().getAssetVocabularyGroupRelsByVocabularyId(
+			vocabularyId);
+	}
+
 	/**
 	 * Returns the number of asset vocabulary group rels.
 	 *
@@ -349,6 +379,13 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 		throws PortalException {
 
 		return getService().getPersistedModel(primaryKeyObj);
+	}
+
+	public static void setAssetVocabularyGroupRels(
+			long vocabularyId, long[] groupIds)
+		throws PortalException {
+
+		getService().setAssetVocabularyGroupRels(vocabularyId, groupIds);
 	}
 
 	/**

portal-kernel/src/com/liferay/asset/kernel/service/AssetVocabularyGroupRelServiceUtil.java
@@ -5,6 +5,11 @@
 
 package com.liferay.asset.kernel.service;
 
+import com.liferay.asset.kernel.model.AssetVocabularyGroupRel;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.List;
+
 /**
  * Provides the remote service utility for AssetVocabularyGroupRel. This utility wraps
  * <code>com.liferay.portlet.asset.service.impl.AssetVocabularyGroupRelServiceImpl</code> and is an
@@ -24,14 +29,35 @@ public class AssetVocabularyGroupRelServiceUtil {
 	 *
 	 * Never modify this class directly. Add custom service methods to <code>com.liferay.portlet.asset.service.impl.AssetVocabularyGroupRelServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
 	 */
+	public static AssetVocabularyGroupRel addAssetVocabularyGroupRel(
+			long vocabularyId, long groupId)
+		throws PortalException {
+
+		return getService().addAssetVocabularyGroupRel(vocabularyId, groupId);
+	}
+
+	public static List<AssetVocabularyGroupRel>
+			getAssetVocabularyGroupRelsByVocabularyId(long vocabularyId)
+		throws PortalException {
+
+		return getService().getAssetVocabularyGroupRelsByVocabularyId(
+			vocabularyId);
+	}
 
 	/**
 	 * Returns the OSGi service identifier.
 	 *
 	 * @return the OSGi service identifier
 	 */
-	public static java.lang.String getOSGiServiceIdentifier() {
+	public static String getOSGiServiceIdentifier() {
 		return getService().getOSGiServiceIdentifier();
+	}
+
+	public static void setAssetVocabularyGroupRels(
+			long vocabularyId, long[] groupIds)
+		throws PortalException {
+
+		getService().setAssetVocabularyGroupRels(vocabularyId, groupIds);
 	}
 
 	public static AssetVocabularyGroupRelService getService() {

liferay/liferay-portal-cloud-native

portal-impl/src/com/liferay/portlet/asset/service/http/AssetVocabularyGroupRelServiceHttp.java
@@ -5,13 +5,21 @@
 
 package com.liferay.portlet.asset.service.http;
 
+import com.liferay.asset.kernel.service.AssetVocabularyGroupRelServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.HttpPrincipal;
+import com.liferay.portal.kernel.service.http.TunnelUtil;
+import com.liferay.portal.kernel.util.MethodHandler;
+import com.liferay.portal.kernel.util.MethodKey;
+
 /**
  * Provides the HTTP utility for the
- * <code>com.liferay.asset.kernel.service.AssetVocabularyGroupRelServiceUtil</code> service
+ * <code>AssetVocabularyGroupRelServiceUtil</code> service
  * utility. The
  * static methods of this class calls the same methods of the service utility.
  * However, the signatures are different because it requires an additional
- * <code>com.liferay.portal.kernel.security.auth.HttpPrincipal</code> parameter.
+ * <code>HttpPrincipal</code> parameter.
  *
  * <p>
  * The benefits of using the HTTP utility is that it is fast and allows for
@@ -32,4 +40,143 @@ package com.liferay.portlet.asset.service.http;
  * @generated
  */
 public class AssetVocabularyGroupRelServiceHttp {
+
+	public static com.liferay.asset.kernel.model.AssetVocabularyGroupRel
+			addAssetVocabularyGroupRel(
+				HttpPrincipal httpPrincipal, long groupId, long vocabularyId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				AssetVocabularyGroupRelServiceUtil.class,
+				"addAssetVocabularyGroupRel",
+				_addAssetVocabularyGroupRelParameterTypes0);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, groupId, vocabularyId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (com.liferay.asset.kernel.model.AssetVocabularyGroupRel)
+				returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static java.util.List
+		<com.liferay.asset.kernel.model.AssetVocabularyGroupRel>
+				getAssetVocabularyGroupRelsByVocabularyId(
+					HttpPrincipal httpPrincipal, long vocabularyId)
+			throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				AssetVocabularyGroupRelServiceUtil.class,
+				"getAssetVocabularyGroupRelsByVocabularyId",
+				_getAssetVocabularyGroupRelsByVocabularyIdParameterTypes1);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, vocabularyId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (java.util.List
+				<com.liferay.asset.kernel.model.AssetVocabularyGroupRel>)
+					returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	public static void setAssetVocabularyGroupRels(
+			HttpPrincipal httpPrincipal, long vocabularyId, long[] groupIds)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				AssetVocabularyGroupRelServiceUtil.class,
+				"setAssetVocabularyGroupRels",
+				_setAssetVocabularyGroupRelsParameterTypes2);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, vocabularyId, groupIds);
+
+			try {
+				TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		AssetVocabularyGroupRelServiceHttp.class);
+
+	private static final Class<?>[] _addAssetVocabularyGroupRelParameterTypes0 =
+		new Class[] {long.class, long.class};
+	private static final Class<?>[]
+		_getAssetVocabularyGroupRelsByVocabularyIdParameterTypes1 =
+			new Class[] {long.class};
+	private static final Class<?>[]
+		_setAssetVocabularyGroupRelsParameterTypes2 = new Class[] {
+			long.class, long[].class
+		};
+
 }

portal-kernel/src/com/liferay/asset/kernel/service/AssetVocabularyGroupRelLocalServiceUtil.java
@@ -52,6 +52,13 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 		return getService().addAssetVocabularyGroupRel(assetVocabularyGroupRel);
 	}
 
+	public static AssetVocabularyGroupRel addAssetVocabularyGroupRel(
+			long groupId, long vocabularyId)
+		throws PortalException {
+
+		return getService().addAssetVocabularyGroupRel(groupId, vocabularyId);
+	}
+
 	/**
 	 * Creates a new asset vocabulary group rel with the primary key. Does not add the asset vocabulary group rel to the database.
 	 *
@@ -109,6 +116,16 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 
 		return getService().deleteAssetVocabularyGroupRel(
 			assetVocabularyGroupRelId);
+	}
+
+	public static void deleteAssetVocabularyGroupRelsByGroupId(long groupId) {
+		getService().deleteAssetVocabularyGroupRelsByGroupId(groupId);
+	}
+
+	public static void deleteAssetVocabularyGroupRelsByVocabularyId(
+		long vocabularyId) {
+
+		getService().deleteAssetVocabularyGroupRelsByVocabularyId(vocabularyId);
 	}
 
 	/**
@@ -283,6 +300,12 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 		return getService().getAssetVocabularyGroupRels(start, end);
 	}
 
+	public static List<AssetVocabularyGroupRel>
+		getAssetVocabularyGroupRelsByGroupId(long groupId) {
+
+		return getService().getAssetVocabularyGroupRelsByGroupId(groupId);
+	}
+
 	/**
 	 * Returns all the asset vocabulary group rels matching the UUID and company.
 	 *
@@ -317,6 +340,13 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 			uuid, companyId, start, end, orderByComparator);
 	}
 
+	public static List<AssetVocabularyGroupRel>
+		getAssetVocabularyGroupRelsByVocabularyId(long vocabularyId) {
+
+		return getService().getAssetVocabularyGroupRelsByVocabularyId(
+			vocabularyId);
+	}
+
 	/**
 	 * Returns the number of asset vocabulary group rels.
 	 *
@@ -349,6 +379,13 @@ public class AssetVocabularyGroupRelLocalServiceUtil {
 		throws PortalException {
 
 		return getService().getPersistedModel(primaryKeyObj);
+	}
+
+	public static void setAssetVocabularyGroupRels(
+			long vocabularyId, long[] groupIds)
+		throws PortalException {
+
+		getService().setAssetVocabularyGroupRels(vocabularyId, groupIds);
 	}
 
 	/**

portal-kernel/src/com/liferay/asset/kernel/service/AssetVocabularyGroupRelServiceUtil.java
@@ -5,6 +5,11 @@
 
 package com.liferay.asset.kernel.service;
 
+import com.liferay.asset.kernel.model.AssetVocabularyGroupRel;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.List;
+
 /**
  * Provides the remote service utility for AssetVocabularyGroupRel. This utility wraps
  * <code>com.liferay.portlet.asset.service.impl.AssetVocabularyGroupRelServiceImpl</code> and is an
@@ -24,14 +29,35 @@ public class AssetVocabularyGroupRelServiceUtil {
 	 *
 	 * Never modify this class directly. Add custom service methods to <code>com.liferay.portlet.asset.service.impl.AssetVocabularyGroupRelServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
 	 */
+	public static AssetVocabularyGroupRel addAssetVocabularyGroupRel(
+			long groupId, long vocabularyId)
+		throws PortalException {
+
+		return getService().addAssetVocabularyGroupRel(groupId, vocabularyId);
+	}
+
+	public static List<AssetVocabularyGroupRel>
+			getAssetVocabularyGroupRelsByVocabularyId(long vocabularyId)
+		throws PortalException {
+
+		return getService().getAssetVocabularyGroupRelsByVocabularyId(
+			vocabularyId);
+	}
 
 	/**
 	 * Returns the OSGi service identifier.
 	 *
 	 * @return the OSGi service identifier
 	 */
-	public static java.lang.String getOSGiServiceIdentifier() {
+	public static String getOSGiServiceIdentifier() {
 		return getService().getOSGiServiceIdentifier();
+	}
+
+	public static void setAssetVocabularyGroupRels(
+			long vocabularyId, long[] groupIds)
+		throws PortalException {
+
+		getService().setAssetVocabularyGroupRels(vocabularyId, groupIds);
 	}
 
 	public static AssetVocabularyGroupRelService getService() {