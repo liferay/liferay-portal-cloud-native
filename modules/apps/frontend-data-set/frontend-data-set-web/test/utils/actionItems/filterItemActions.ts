d28d19724098c693e7a7efe753f8949c16443143
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2025-03-14 14:22:36 +0100

    LPD-50120 - Refactor. Replace mocks by a function that generates actions

--- modules/apps/frontend-data-set/frontend-data-set-web/test/utils/actionItems/filterItemActions.ts
+++ modules/apps/frontend-data-set/frontend-data-set-web/test/utils/actionItems/filterItemActions.ts
@@ -3,64 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {IItemsActions} from '../../../src/main/resources/META-INF/resources';
+import {
+	IItemActionsData,
+	IItemsActions,
+} from '../../../src/main/resources/META-INF/resources';
 import filterItemActions from '../../../src/main/resources/META-INF/resources/utils/actionItems/filterItemActions';
 
-const testActionsWithPermissionKey: IItemsActions[] = [
-	{
-		data: {
-			permissionKey: 'DELETE',
-			title: 'Link action sample',
-		},
-		href: '/o/data-test-endpoint/{id}',
-		label: 'Link action sample',
-		target: 'link',
-	},
-	{
-		data: {
-			permissionKey: 'POST',
-			title: 'Another one',
-		},
-		href: '/home',
-		label: 'Another one',
-		target: 'link',
-	},
-];
-
-const testActionsWithRandomCasePermissionKey: IItemsActions[] = [
-	{
-		data: {
-			permissionKey: 'STANDALONEACTION',
-			title: 'Stand Alone Action',
-		},
-		href: '/o/data-test-endpoint/{id}',
-		label: 'Stand Alone Action',
-		target: 'headless',
-	},
-];
-
-const testActionsWithPermissionKeyAndFilters: IItemsActions[] = [
-	{
-		data: {
-			filters: [{key: 'color', value: 'green'}],
-			permissionKey: 'UPDATE',
-		},
-		href: '/o/data-test-endpoint/{id}',
-		label: 'Action with color filter & permissionKey',
-		target: 'link',
-	},
-	{
-		data: {
-			filters: [{key: 'color', value: 'blue'}],
-			permissionKey: 'UPDATE',
-		},
-		href: '/o/data-test-endpoint/{id}',
-		label: 'Action with color filter & permissionKey',
-		target: 'link',
-	},
-];
-
-const testActionsWithoutPermissionKey: IItemsActions[] = [
+const baseItemActions: IItemsActions[] = [
 	{
 		href: '/o/data-test-endpoint/{id}',
 		label: 'Link action sample',
@@ -72,6 +21,29 @@ const testActionsWithoutPermissionKey: IItemsActions[] = [
 		target: 'link',
 	},
 ];
+
+const generateCustomItemActions = function (
+	data?: Array<IItemActionsData>
+): IItemsActions[] {
+	if (data && data.length !== baseItemActions.length) {
+		return baseItemActions.map((action) => {
+			return {
+				...action,
+				data: data[0],
+			};
+		});
+	}
+	else if (data && data.length === baseItemActions.length) {
+		return baseItemActions.map((action, index) => {
+			return {
+				...action,
+				data: data[index],
+			};
+		});
+	}
+
+	return baseItemActions;
+};
 
 const availableItemData = {
 	actions: {
@@ -125,96 +97,109 @@ const availableItemData = {
 describe('filterItemActions', () => {
 	describe('when permissionKey is defined for an action', () => {
 		it('returns the actions where the permissionKey matches the itemData.actions key', () => {
+			const customActionsWithPermissionKey = generateCustomItemActions([
+				{permissionKey: 'DELETE'},
+				{permissionKey: 'POST'},
+			]);
 			const filteredActions = filterItemActions(
-				testActionsWithPermissionKey,
+				customActionsWithPermissionKey,
 				availableItemData
 			);
 
 			expect(filteredActions.length).toBeLessThan(
-				testActionsWithPermissionKey.length
+				customActionsWithPermissionKey.length
 			);
 		});
 
 		it('returns the actions where the permissionKey matches the itemData.actions key regardless of letter case', () => {
+			const customItemActions = generateCustomItemActions([
+				{
+					permissionKey: 'STANDALONEACTION',
+				},
+			]);
 			const filteredActions = filterItemActions(
-				testActionsWithRandomCasePermissionKey,
+				customItemActions,
 				availableItemData
 			);
 
 			expect(filteredActions[0].data).toMatchObject(
-				testActionsWithRandomCasePermissionKey[0].data!
+				customItemActions[0].data!
 			);
 		});
 	});
 
 	describe('when permissionKey is not defined for an item', () => {
 		it('returns all the actions', () => {
+			const customItemActions = generateCustomItemActions();
+
 			const filteredActions = filterItemActions(
-				testActionsWithoutPermissionKey,
+				customItemActions,
 				availableItemData
 			);
 
-			expect(filteredActions).toMatchObject(
-				testActionsWithoutPermissionKey
-			);
+			expect(filteredActions).toMatchObject(customItemActions);
 		});
 	});
 
 	describe('when permissionKey and action filters are defined for an action item', () => {
 		it('returns only the action that matches the permissionKey and the action filter criteria', () => {
+			const customItemActions = generateCustomItemActions([
+				{
+					filters: [{key: 'color', value: 'green'}],
+					permissionKey: 'UPDATE',
+				},
+				{
+					filters: [{key: 'color', value: 'blue'}],
+					permissionKey: 'UPDATE',
+				},
+			]);
+
 			const filteredActions = filterItemActions(
-				testActionsWithPermissionKeyAndFilters,
+				customItemActions,
 				availableItemData
 			);
 
 			expect(filteredActions.length).toEqual(1);
-			expect(filteredActions[0]).toMatchObject(
-				testActionsWithPermissionKeyAndFilters[1]
-			);
+			expect(filteredActions[0]).toMatchObject(customItemActions[1]);
 		});
 	});
 
 	describe('when only action filters are defined for an action item', () => {
 		it('returns only the action that matches the action filter criteria', () => {
-			const testActionsWithOnlyFilters: IItemsActions[] =
-				testActionsWithPermissionKeyAndFilters.map((action) => {
-					delete action.data?.permissionKey;
-
-					return {
-						...action,
-					};
-				});
+			const customItemActions = generateCustomItemActions([
+				{
+					filters: [{key: 'color', value: 'green'}],
+				},
+				{
+					filters: [{key: 'color', value: 'blue'}],
+				},
+			]);
 
 			const filteredActions = filterItemActions(
-				testActionsWithOnlyFilters,
+				customItemActions,
 				availableItemData
 			);
 
 			expect(filteredActions.length).toEqual(1);
 
-			expect(filteredActions[0]).toMatchObject(
-				testActionsWithOnlyFilters[1]
-			);
+			expect(filteredActions[0]).toMatchObject(customItemActions[1]);
 		});
 
 		it('returns only the actions that matches all action filters criteria', () => {
-			const testActionsWithOnlyFilters: IItemsActions[] =
-				testActionsWithPermissionKeyAndFilters.map((action) => {
-					delete action.data?.permissionKey;
-
-					const newActionFilter = {key: 'type', value: 'boolean'};
-
-					return {
-						...action,
-						data: {
-							filters:
-								action.data?.filters?.concat(newActionFilter),
-						},
-					};
-				});
+			const customItemActions = generateCustomItemActions([
+				{
+					filters: [{key: 'color', value: 'green'}],
+				},
+				{
+					filters: [
+						{key: 'color', value: 'blue'},
+						{key: 'type', value: 'boolean'},
+					],
+				},
+			]);
 
 			const filteredActions = filterItemActions(
-				testActionsWithOnlyFilters,
+				customItemActions,
 				availableItemData
 			);
 
@@ -222,57 +207,47 @@ describe('filterItemActions', () => {
 		});
 
 		it('returns actions if the filter criteria key includes a composed field name', () => {
-			const testActionsWithOnlyFilters: IItemsActions[] =
-				testActionsWithPermissionKeyAndFilters.map((action) => {
-					delete action.data?.permissionKey;
-
-					const newActionFilter = {
-						key: 'creator.name',
-						value: 'Test Test',
-					};
-
-					return {
-						...action,
-						data: {
-							filters:
-								action.data?.filters?.concat(newActionFilter),
-						},
-					};
-				});
+			const customItemActions = generateCustomItemActions([
+				{
+					filters: [{key: 'color', value: 'green'}],
+				},
+				{
+					filters: [
+						{key: 'color', value: 'blue'},
+						{key: 'creator.name', value: 'Test Test'},
+					],
+				},
+			]);
 
 			const filteredActions = filterItemActions(
-				testActionsWithOnlyFilters,
+				customItemActions,
 				availableItemData
 			);
 
 			expect(filteredActions.length).toEqual(1);
-			expect(filteredActions[0]).toMatchObject(
-				testActionsWithOnlyFilters[1]
-			);
+			expect(filteredActions[0]).toMatchObject(customItemActions[1]);
 		});
 
 		it('returns actions if the filter criteria is based on a boolean value', () => {
-			const testActionsWithOnlyFilters: IItemsActions[] =
-				testActionsWithPermissionKeyAndFilters.map((action) => {
-					delete action.data?.permissionKey;
-
-					const newActionFilter = {key: 'sortable', value: true};
-
-					return {
-						...action,
-						data: {
-							filters:
-								action.data?.filters?.concat(newActionFilter),
-						},
-					};
-				});
+			const customItemActions = generateCustomItemActions([
+				{
+					filters: [{key: 'color', value: 'green'}],
+				},
+				{
+					filters: [
+						{key: 'color', value: 'blue'},
+						{key: 'sortable', value: true},
+					],
+				},
+			]);
 
 			const filteredActions = filterItemActions(
-				testActionsWithOnlyFilters,
+				customItemActions,
 				availableItemData
 			);
 
 			expect(filteredActions.length).toEqual(1);
+			expect(filteredActions[0]).toMatchObject(customItemActions[1]);
 		});
 	});
 
@@ -287,7 +262,7 @@ describe('filterItemActions', () => {
 			const spyCallback = jest.spyOn(isVisibleFn, 'isPopular');
 
 			const testActionsWithIsVisibleCallback: IItemsActions[] =
-				testActionsWithoutPermissionKey.map((action) => {
+				baseItemActions.map((action) => {
 					return {
 						...action,
 						isVisible: isVisibleFn.isPopular,
