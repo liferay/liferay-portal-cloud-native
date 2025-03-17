fbc256008245ac4d961a9457fd7bb22029ede421
Brian Chan <brian.chan@liferay.com>
2025-03-17 10:33:37 -0300

    LPD-46407 Wordsmith

--- portal-impl/src/com/liferay/portal/db/partition/util/DBPartitionUtil.java
+++ portal-impl/src/com/liferay/portal/db/partition/util/DBPartitionUtil.java
@@ -920,7 +920,7 @@ public class DBPartitionUtil {
 			}
 			catch (SQLException sqlException) {
 				throw new PortalException(
-					"Unable to roll back schema extraction", sqlException);
+					"Unable to roll back company extraction", sqlException);
 			}
 
 			throw new PortalException(
