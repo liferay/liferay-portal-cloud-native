5f13cfaecf9a44263577eb6f7c56a8aa0474e0ae
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-03-18 18:10:26 +0100

    LPD-50780 show warning message

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/components/modal/ImportStructureModalContent.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/components/modal/ImportStructureModalContent.tsx
@@ -3,20 +3,38 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayAlert from '@clayui/alert';
 import ClayButton from '@clayui/button';
 import ClayModal from '@clayui/modal';
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function ImportStructureModalContent({
 	closeModal,
 }: {
 	closeModal: () => void;
 }) {
+	const [warning, setWarning] = useState(true);
+
 	return (
 		<>
 			<ClayModal.Header>
 				{Liferay.Language.get('import-and-override-structure')}
 			</ClayModal.Header>
+
+			{warning && (
+				<ClayAlert
+					displayType="warning"
+					onClose={() => {
+						setWarning(false);
+					}}
+					title={`${Liferay.Language.get('warning')}:`}
+					variant="stripe"
+				>
+					{Liferay.Language.get(
+						'import-and-override-structure-warning-message'
+					)}
+				</ClayAlert>
+			)}
 
 			<ClayModal.Body>
 				<h1>modal content for the import</h1>
