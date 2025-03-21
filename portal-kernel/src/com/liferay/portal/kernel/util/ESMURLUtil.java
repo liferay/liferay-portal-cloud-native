8c9214046573013717a179776d0f4a721177f17b
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2025-03-21 13:36:29 +0400

    LPD-51570 - Add util for generating ESM import URLs

---
+++ portal-kernel/src/com/liferay/portal/kernel/util/ESMURLUtil.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.kernel.util;
+
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+
+/**
+ * @author Bryce Osterhaus
+ */
+public class ESMURLUtil {
+
+	public static String buildURL(
+		ThemeDisplay themeDisplay, String contextPath) {
+
+		return buildURL(themeDisplay, contextPath, "index");
+	}
+
+	public static String buildURL(
+		ThemeDisplay themeDisplay, String contextPath, String submodule) {
+
+		return StringBundler.concat(
+			themeDisplay.getPathContext(), "/o/", contextPath, "/__liferay__/",
+			submodule, ".js");
+	}
+
+}
