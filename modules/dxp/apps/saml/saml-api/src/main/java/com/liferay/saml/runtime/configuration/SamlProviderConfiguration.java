59b7e877d9ad6083baa1d36593e4145ebbca2378
Alan Huang <alan.huang@liferay.com>
2025-03-23 02:20:54 +0800

    LPD-43790 Change language keys

--- modules/dxp/apps/saml/saml-api/src/main/java/com/liferay/saml/runtime/configuration/SamlProviderConfiguration.java
+++ modules/dxp/apps/saml/saml-api/src/main/java/com/liferay/saml/runtime/configuration/SamlProviderConfiguration.java
@@ -99,7 +99,7 @@ public interface SamlProviderConfiguration {
 
 	@Meta.AD(
 		deflt = SamlProviderConfigurationKeys.SAML_ROLE_SP, id = "saml.role",
-		name = "saml-role", optionLabels = {"saml-role-idp", "saml-role-sp"},
+		name = "saml-role", optionLabels = {"identity-provider", "service-provider"},
 		optionValues = {
 			SamlProviderConfigurationKeys.SAML_ROLE_IDP,
 			SamlProviderConfigurationKeys.SAML_ROLE_SP
