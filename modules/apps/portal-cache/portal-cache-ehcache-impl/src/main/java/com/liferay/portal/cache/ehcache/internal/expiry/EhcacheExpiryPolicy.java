9fec293e5153acfd44cc6d780cfc2270d4f885f2
Dante Wang <dante.wang@liferay.com>
2025-03-24 09:47:11 +0800

    LPD-41245 Move EhcacheExpiryPolicy to a new package expiry to match its interface

--- modules/apps/portal-cache/portal-cache-ehcache-impl/src/main/java/com/liferay/portal/cache/ehcache/internal/expiry/EhcacheExpiryPolicy.java
+++ modules/apps/portal-cache/portal-cache-ehcache-impl/src/main/java/com/liferay/portal/cache/ehcache/internal/expiry/EhcacheExpiryPolicy.java
@@ -3,7 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.cache.ehcache.internal;
+package com.liferay.portal.cache.ehcache.internal.expiry;
+
+import com.liferay.portal.cache.ehcache.internal.EhcacheValue;
 
 import java.time.Duration;
 
