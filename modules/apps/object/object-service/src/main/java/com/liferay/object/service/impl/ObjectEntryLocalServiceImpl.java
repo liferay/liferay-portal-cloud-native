3deb34051b6585b1e9bb5ffd1a5416718dec2ec6
Guilherme Camacho <guilherme.camacho@liferay.com>
2025-03-26 14:02:19 -0300

    LPD-40689 Rename

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -1969,7 +1969,7 @@ public class ObjectEntryLocalServiceImpl
 			ServiceContext serviceContext, long userId)
 		throws PortalException {
 
-		List<ValidationError> validationErrorList = new ArrayList<>();
+		List<ValidationError> validationErrors = new ArrayList<>();
 
 		try {
 			_objectValidationRuleLocalService.validate(
@@ -1979,7 +1979,7 @@ public class ObjectEntryLocalServiceImpl
 		catch (ObjectValidationRuleEngineException
 					objectValidationRuleEngineException) {
 
-			validationErrorList = ListUtil.toList(
+			validationErrors = ListUtil.toList(
 				objectValidationRuleEngineException.
 					getObjectValidationRuleResults(),
 				objectValidationRuleResult -> new ValidationError(
@@ -1992,7 +1992,7 @@ public class ObjectEntryLocalServiceImpl
 			_objectDefinitionPersistence.findByPrimaryKey(
 				objectEntry.getObjectDefinitionId());
 
-		validationErrorList.addAll(
+		validationErrors.addAll(
 			ListUtil.toList(
 				_validateValues(
 					Collections.emptyMap(), Collections.emptySet(), objectEntry,
@@ -2001,7 +2001,7 @@ public class ObjectEntryLocalServiceImpl
 				objectEntryValuesException -> new ValidationError(
 					objectEntryValuesException.getMessage())));
 
-		return validationErrorList;
+		return validationErrors;
 	}
 
 	@Activate
