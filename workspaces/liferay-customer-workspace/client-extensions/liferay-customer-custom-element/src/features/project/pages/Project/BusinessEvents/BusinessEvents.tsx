liferay-customer-solutions/liferay-portal

LRSD-7741 Refactor business event modal and add record go live flow

workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/features/project/pages/Project/BusinessEvents/BusinessEvents.tsx
@@ -9,7 +9,7 @@ import './BusinessEvents.css';
 
 import Button from '@clayui/button';
 import ClayIcon from '@clayui/icon';
-import ClayModal, {useModal} from '@clayui/modal';
+import {useModal} from '@clayui/modal';
 import {useCallback, useEffect, useMemo, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {ButtonDropDown} from '~/components';
@@ -25,7 +25,7 @@ import {getFormattedDate} from '~/utils/getFormattedDate';
 import {getFormattedTime} from '~/utils/getFormattedTime';
 import {IBusinessEvent} from '~/utils/types';
 
-import CancelEventForm from './components/CancelEventForm';
+import ManageEventModal from './components/ManageEventModal';
 import useHasAllEventsPermissions from './hooks/useHasAllEventsPermissions';
 import {INITIAL_FILTER} from './utils/constants/initialFilter';
 
@@ -73,6 +73,7 @@ const BusinessEvents = () => {
 	});
 
 	const [businessEvents, setBusinessEvents] = useState<IBusinessEvent[]>([]);
+	const [modalType, setModalType] = useState('');
 	const [loading, setLoading] = useState(true);
 
 	const {client} = useAppPropertiesContext();
@@ -235,12 +236,17 @@ const BusinessEvents = () => {
 						{
 							customOptionStyle: 'pr-5',
 							label: i18n.translate('record-actual-go-live'),
-							onClick: () => {},
+							onClick: () => {
+								setModalType('goLiveEvent');
+								onOpenChange(true);
+								setSelectedBusinessEvent(businessEvent);
+							},
 						},
 						{
 							customOptionStyle: 'be-cancel-event-option pr-5',
 							label: i18n.translate('cancel-event'),
 							onClick: () => {
+								setModalType('cancelEvent');
 								onOpenChange(true);
 								setSelectedBusinessEvent(businessEvent);
 							},
@@ -416,18 +422,14 @@ const BusinessEvents = () => {
 						/>
 
 						{selectedBusinessEvent && open && (
-							<ClayModal
-								center
-								disableAutoClose
+							<ManageEventModal
+								businessEvent={selectedBusinessEvent}
+								client={client}
+								closeFunction={onOpenChange}
+								modalType={modalType}
 								observer={observer}
-							>
-								<CancelEventForm
-									businessEvent={selectedBusinessEvent}
-									client={client}
-									closeFunction={onOpenChange}
-									onCancel={handleEventCanceled}
-								/>
-							</ClayModal>
+								onCancel={handleEventCanceled}
+							/>
 						)}
 					</>
 				) : (

workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/features/project/pages/Project/BusinessEvents/pages/BusinessEventsItem/BusinessEventsItemDetails/BusinessEventsItemDetails.tsx
@@ -6,7 +6,7 @@
 import {Nav} from '@clayui/core';
 import ClayIcon from '@clayui/icon';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
-import ClayModal, {useModal} from '@clayui/modal';
+import {useModal} from '@clayui/modal';
 import NavigationBar from '@clayui/navigation-bar';
 import {useCallback, useEffect, useState} from 'react';
 import {Link, useNavigate, useParams} from 'react-router-dom';
@@ -19,7 +19,7 @@ import {getFormattedDate} from '~/utils/getFormattedDate';
 import {getFormattedTime} from '~/utils/getFormattedTime';
 import {IBusinessEvent} from '~/utils/types';
 
-import CancelEventForm from '../../../components/CancelEventForm';
+import CancelEventForm from '../../../components/ManageEventModal';
 
 import './BusinessEventsItemDetails.css';
 import useHasAllEventsPermissions from '../../../hooks/useHasAllEventsPermissions';
@@ -33,6 +33,7 @@ const BusinessEventsItemDetails = () => {
 
 	const {client} = useAppPropertiesContext();
 
+	const [modalType, setModalType] = useState('');
 	const [loading, setLoading] = useState(true);
 	const {hasAllEventsPermissions} = useHasAllEventsPermissions();
 
@@ -98,13 +99,17 @@ const BusinessEventsItemDetails = () => {
 			customOptionStyle: 'pr-5',
 			icon: <ClayIcon symbol="check-circle" />,
 			label: i18n.translate('record-actual-go-live'),
-			onClick: () => {},
+			onClick: () => {
+				setModalType('goLiveEvent');
+				onOpenChange(true);
+			},
 		},
 		{
 			customOptionStyle: 'cancel-event-option pr-5',
 			icon: <ClayIcon symbol="trash" />,
 			label: i18n.translate('cancel-event'),
 			onClick: () => {
+				setModalType('cancelEvent');
 				onOpenChange(true);
 			},
 		},
@@ -291,14 +296,14 @@ const BusinessEventsItemDetails = () => {
 			</div>
 
 			{businessEvent && open && (
-				<ClayModal center disableAutoClose observer={observer}>
-					<CancelEventForm
-						businessEvent={businessEvent}
-						client={client}
-						closeFunction={onOpenChange}
-						onCancel={handleEventCanceled}
-					/>
-				</ClayModal>
+				<CancelEventForm
+					businessEvent={businessEvent}
+					client={client}
+					closeFunction={onOpenChange}
+					modalType={modalType}
+					observer={observer}
+					onCancel={handleEventCanceled}
+				/>
 			)}
 		</div>
 	);

liferay/liferay-portal-cloud-native

workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/features/project/pages/Project/BusinessEvents/BusinessEvents.tsx
@@ -9,8 +9,8 @@ import './BusinessEvents.css';
 
 import Button from '@clayui/button';
 import ClayIcon from '@clayui/icon';
-import ClayModal, {useModal} from '@clayui/modal';
-import {useCallback, useMemo, useState} from 'react';
+import {useModal} from '@clayui/modal';
+import {useCallback, useEffect, useMemo, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {ButtonDropDown} from '~/components';
 import Table, {IRow} from '~/components/Table';
@@ -22,11 +22,11 @@ import {getFormattedDate} from '~/utils/getFormattedDate';
 import {getFormattedTime} from '~/utils/getFormattedTime';
 import {IBusinessEvent} from '~/utils/types';
 
-import CancelEventForm from './components/CancelEventForm';
-import useFilters from './hooks/useFilters';
-import useGetBusinessEvents from './hooks/useGetBusinessEvents';
+import ManageEventModal from './components/ManageEventModal';
 import useHasAllEventsPermissions from './hooks/useHasAllEventsPermissions';
 import useIsSaasOnly from './utils/useIsSaasOnly';
+import useFilters from './hooks/useFilters';
+import useGetBusinessEvents from './hooks/useGetBusinessEvents';
 
 const columns = [
 	{
@@ -64,6 +64,7 @@ const BusinessEvents = () => {
 
 	const {businessEvents, fetchBusinessEvents, loading} =
 		useGetBusinessEvents(filterQuery);
+	const [modalType, setModalType] = useState('');
 
 	const {client} = useAppPropertiesContext();
 
@@ -184,12 +185,17 @@ const BusinessEvents = () => {
 						{
 							customOptionStyle: 'pr-5',
 							label: i18n.translate('record-actual-go-live'),
-							onClick: () => {},
+							onClick: () => {
+								setModalType('goLiveEvent');
+								onOpenChange(true);
+								setSelectedBusinessEvent(businessEvent);
+							},
 						},
 						{
 							customOptionStyle: 'be-cancel-event-option pr-5',
 							label: i18n.translate('cancel-event'),
 							onClick: () => {
+								setModalType('cancelEvent');
 								onOpenChange(true);
 								setSelectedBusinessEvent(businessEvent);
 							},
@@ -314,21 +320,17 @@ const BusinessEvents = () => {
 						/>
 
 						{selectedBusinessEvent && open && (
-							<ClayModal
-								center
-								disableAutoClose
+							<ManageEventModal
+								accountExternalReferenceCode={
+									project?.accountKey || ''
+								}
+								businessEvent={selectedBusinessEvent}
+								client={client}
+								closeFunction={onOpenChange}
+								modalType={modalType}
 								observer={observer}
-							>
-								<CancelEventForm
-									accountExternalReferenceCode={
-										project?.accountKey || ''
-									}
-									businessEvent={selectedBusinessEvent}
-									client={client}
-									closeFunction={onOpenChange}
-									onCancel={handleOnCancel}
-								/>
-							</ClayModal>
+								onCancel={handleOnCancel}
+							/>
 						)}
 					</>
 				) : (

workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/features/project/pages/Project/BusinessEvents/pages/BusinessEventsItem/BusinessEventsItemDetails/BusinessEventsItemDetails.tsx
@@ -6,7 +6,7 @@
 import {Nav} from '@clayui/core';
 import ClayIcon from '@clayui/icon';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
-import ClayModal, {useModal} from '@clayui/modal';
+import {useModal} from '@clayui/modal';
 import NavigationBar from '@clayui/navigation-bar';
 import {useCallback, useEffect, useState} from 'react';
 import {Link, useNavigate, useParams} from 'react-router-dom';
@@ -17,14 +17,12 @@ import i18n from '~/utils/I18n';
 import {getFormattedDate} from '~/utils/getFormattedDate';
 import {getFormattedTime} from '~/utils/getFormattedTime';
 import {ITicket} from '~/utils/types';
-
-import CancelEventForm from '../../../components/CancelEventForm';
-
 import './BusinessEventsItemDetails.css';
 import TicketList from '../../../components/AssociatedTicketsContainer/TicketList';
 import useAccountTickets from '../../../hooks/useAccountTickets';
 import useGetBusinessEvent from '../../../hooks/useGetBusinessEvent';
 import useHasAllEventsPermissions from '../../../hooks/useHasAllEventsPermissions';
+import ManageEventModal from '../../../components/ManageEventModal';
 
 const BusinessEventsItemDetails = () => {
 	const {accountKey, id} = useParams<{accountKey: string; id: string}>();
@@ -35,6 +33,7 @@ const BusinessEventsItemDetails = () => {
 
 	const {client} = useAppPropertiesContext();
 
+	const [modalType, setModalType] = useState('');
 	const {hasAllEventsPermissions} = useHasAllEventsPermissions();
 
 	const {loading: loadingTickets, tickets} = useAccountTickets(
@@ -58,13 +57,17 @@ const BusinessEventsItemDetails = () => {
 			customOptionStyle: 'pr-5',
 			icon: <ClayIcon symbol="check-circle" />,
 			label: i18n.translate('record-actual-go-live'),
-			onClick: () => {},
+			onClick: () => {
+				setModalType('goLiveEvent');
+				onOpenChange(true);
+			},
 		},
 		{
 			customOptionStyle: 'cancel-event-option pr-5',
 			icon: <ClayIcon symbol="trash" />,
 			label: i18n.translate('cancel-event'),
 			onClick: () => {
+				setModalType('cancelEvent');
 				onOpenChange(true);
 			},
 		},
@@ -309,15 +312,15 @@ const BusinessEventsItemDetails = () => {
 			</div>
 
 			{businessEvent && open && (
-				<ClayModal center disableAutoClose observer={observer}>
-					<CancelEventForm
-						accountExternalReferenceCode={accountKey || ''}
-						businessEvent={businessEvent}
-						client={client}
-						closeFunction={onOpenChange}
-						onCancel={handleOnCancel}
-					/>
-				</ClayModal>
+				<ManageEventModal
+					accountExternalReferenceCode={accountKey || ''}
+					businessEvent={businessEvent}
+					client={client}
+					closeFunction={onOpenChange}
+					modalType={modalType}
+					observer={observer}
+					onCancel={handleOnCancel}
+				/>
 			)}
 		</div>
 	);