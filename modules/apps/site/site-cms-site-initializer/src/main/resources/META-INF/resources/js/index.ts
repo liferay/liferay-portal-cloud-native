ctang3/liferay-portal

LPD-51251 Refactor vocabularies view to render in jsp

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -7,9 +7,10 @@ export {default as AllFDSPropsTransformer} from './main/FDSPropsTransformer/AllF
 export {default as ContentsFDSPropsTransformer} from './main/FDSPropsTransformer/ContentsFDSPropsTransformer';
 export {default as FilesFDSPropsTransformer} from './main/FDSPropsTransformer/FilesFDSPropsTransformer';
 export {default as StructuresFDSPropsTransformer} from './main/FDSPropsTransformer/StructuresFDSPropsTransformer';
+export {default as VocabularyAssetTypesFDSPropsTransformer} from './main/FDSPropsTransformer/VocabularyAssetTypesFDSPropsTransformer';
+export {default as CategorizationToolbar} from './main/categorization/CategorizationToolbar';
 export {default as ViewTags} from './main/categorization/tags/ViewTags';
 export {default as EditVocabulary} from './main/categorization/vocabularies/EditVocabulary';
-export {default as ViewVocabularies} from './main/categorization/vocabularies/ViewVocabularies';
 export {default as SpacesSticker} from './main/components/SpaceSticker';
 export {default as SpacesNavigation} from './main/spaces_navigation/SpacesNavigation';
 export {default as StructureBuilder} from './structure_builder/components/StructureBuilder';

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/view_vocabularies.jsp
@@ -12,10 +12,20 @@ ViewVocabulariesDisplayContext viewVocabulariesDisplayContext = (ViewVocabularie
 %>
 
 <div class="cms-section">
-	<div id="<%= CMSSiteInitializerFDSNames.CATEGORIZATION_SECTION %>">
-		<react:component
-			module="{ViewVocabularies} from site-cms-site-initializer"
-			props="<%= viewVocabulariesDisplayContext.getReactData() %>"
+	<div class="categorization-section">
+		<div>
+			<react:component
+				module="{CategorizationToolbar} from site-cms-site-initializer"
+				props="<%= viewVocabulariesDisplayContext.getReactData() %>"
+			/>
+		</div>
+
+		<frontend-data-set:headless-display
+			apiURL="<%= viewVocabulariesDisplayContext.getAPIURL() %>"
+			creationMenu="<%= viewVocabulariesDisplayContext.getCreationMenu() %>"
+			fdsFilters="<%= viewVocabulariesDisplayContext.getFDSFilters() %>"
+			id="test"
+			propsTransformer="{VocabularyAssetTypesFDSPropsTransformer} from site-cms-site-initializer"
 		/>
 	</div>
 </div>

liferay/liferay-portal-cloud-native

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -8,9 +8,10 @@ export {default as ContentsFDSPropsTransformer} from './main/FDSPropsTransformer
 export {default as FilesFDSPropsTransformer} from './main/FDSPropsTransformer/FilesFDSPropsTransformer';
 export {default as StructureUsagesFDSPropsTransformer} from './main/FDSPropsTransformer/StructureUsagesFDSPropsTransformer';
 export {default as StructuresFDSPropsTransformer} from './main/FDSPropsTransformer/StructuresFDSPropsTransformer';
+export {default as VocabularyFDSPropsTransformer} from './main/FDSPropsTransformer/VocabularyFDSPropsTransformer';
+export {default as CategorizationToolbar} from './main/categorization/CategorizationToolbar';
 export {default as ViewTags} from './main/categorization/tags/ViewTags';
 export {default as EditVocabulary} from './main/categorization/vocabularies/EditVocabulary';
-export {default as ViewVocabularies} from './main/categorization/vocabularies/ViewVocabularies';
 export {default as SpacesSticker} from './main/components/SpaceSticker';
 export {default as SpacesNavigation} from './main/spaces_navigation/SpacesNavigation';
 export {default as StructureBuilder} from './structure_builder/components/StructureBuilder';

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/view_vocabularies.jsp
@@ -12,10 +12,20 @@ ViewVocabulariesDisplayContext viewVocabulariesDisplayContext = (ViewVocabularie
 %>
 
 <div class="cms-section">
-	<div id="<%= CMSSiteInitializerFDSNames.CATEGORIZATION_SECTION %>">
-		<react:component
-			module="{ViewVocabularies} from site-cms-site-initializer"
-			props="<%= viewVocabulariesDisplayContext.getReactData() %>"
+	<div class="categorization-section">
+		<div>
+			<react:component
+				module="{CategorizationToolbar} from site-cms-site-initializer"
+				props="<%= viewVocabulariesDisplayContext.getReactData() %>"
+			/>
+		</div>
+
+		<frontend-data-set:headless-display
+			apiURL="<%= viewVocabulariesDisplayContext.getAPIURL() %>"
+			creationMenu="<%= viewVocabulariesDisplayContext.getCreationMenu() %>"
+			fdsFilters="<%= viewVocabulariesDisplayContext.getFDSFilters() %>"
+			id="test"
+			propsTransformer="{VocabularyFDSPropsTransformer} from site-cms-site-initializer"
 		/>
 	</div>
 </div>