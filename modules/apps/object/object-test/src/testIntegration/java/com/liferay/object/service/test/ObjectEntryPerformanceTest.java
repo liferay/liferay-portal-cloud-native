183095123d1e3eb9c2466093049d549cce4a23a7
Lily.Chi <lily.chi@liferay.com>
2025-04-10 18:59:50 +0800

    LPD-37359 Add the integration test for importing and deleting object entries by Rest API with non default company

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryPerformanceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectEntryPerformanceTest.java
@@ -6,6 +6,9 @@
 package com.liferay.object.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.admin.rest.client.http.HttpInvoker;
+import com.liferay.object.admin.rest.dto.v1_0.ObjectFolder;
+import com.liferay.object.admin.rest.resource.v1_0.ObjectFolderResource;
 import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.field.util.ObjectFieldUtil;
@@ -16,12 +19,25 @@ import com.liferay.object.rest.manager.v1_0.ObjectEntryManagerRegistry;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
 import com.liferay.object.test.util.ObjectDefinitionTestUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.test.performance.PerformanceTimer;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.transaction.TransactionConfig;
+import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
@@ -29,14 +45,20 @@ import com.liferay.portal.kernel.util.PropertiesUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.util.PortalInstances;
+import com.liferay.portal.util.PropsValues;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 
 import java.io.Closeable;
 
+import java.net.ConnectException;
+
 import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
+import java.util.function.Predicate;
 
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -67,6 +89,9 @@ public class ObjectEntryPerformanceTest {
 			clazz.getResourceAsStream(
 				"dependencies/object-entry-performance.properties"),
 			"UTF-8");
+
+		_objectEntriesCount = GetterUtil.getInteger(
+			_properties.getProperty("object.entries.count"));
 	}
 
 	@Test
@@ -94,11 +119,7 @@ public class ObjectEntryPerformanceTest {
 				false, Collections.emptyMap(), _dtoConverterRegistry, null,
 				LocaleUtil.getDefault(), null, TestPropsValues.getUser());
 
-		for (int counter = 0;
-			 counter < GetterUtil.getInteger(
-				 _properties.getProperty("object.entries.count"));
-			 counter++) {
-
+		for (int i = 0; i < _objectEntriesCount; i++) {
 			objectEntryManager.addObjectEntry(
 				dtoConverterContext, _customObjectDefinition,
 				new ObjectEntry() {
@@ -123,13 +144,179 @@ public class ObjectEntryPerformanceTest {
 		}
 	}
 
+	@Test
+	public void testHttpPostAndDeleteObjectEntries() throws Throwable {
+		TransactionConfig.Builder transactionConfigBuilder =
+			new TransactionConfig.Builder();
+
+		_company = TransactionInvokerUtil.invoke(
+			transactionConfigBuilder.setPropagation(
+				Propagation.REQUIRED
+			).setRollbackForClasses(
+				Exception.class
+			).build(),
+			() -> {
+				Company company = CompanyLocalServiceUtil.addCompany(
+					null, _VIRTUAL_HOST_NAME, _VIRTUAL_HOST_NAME,
+					_VIRTUAL_HOST_NAME, 0, true, true, null, null, null, null,
+					null, null);
+
+				PortalInstances.initCompany(company);
+
+				return company;
+			});
+
+		try {
+			_invokeHttp(null, HttpInvoker.HttpMethod.GET, _getPath(""));
+		}
+		catch (ConnectException connectException) {
+			throw new Exception(
+				StringBundler.concat(
+					"\"", _VIRTUAL_HOST_NAME, "\" can not be resolved. Please ",
+					"add a mapping to /etc/hosts and rerun the test."),
+				connectException);
+		}
+
+		ObjectFolderResource.Builder objectFolderResourceBuilder =
+			_objectFolderResourceFactory.create();
+
+		ObjectFolderResource objectFolderResource =
+			objectFolderResourceBuilder.user(
+				UserTestUtil.getAdminUser(_company.getCompanyId())
+			).build();
+
+		objectFolderResource.setContextCompany(_company);
+
+		JSONObject objectFolderJSONObject = _jsonFactory.createJSONObject(
+			StringUtil.read(
+				ObjectEntryPerformanceTest.class.getClassLoader(),
+				"/com/liferay/object/web/internal/object/definitions/portlet" +
+					"/action/test/dependencies/test-object-folder-4.json"));
+
+		ObjectFolder objectFolder = ObjectFolder.toDTO(
+			objectFolderJSONObject.toString());
+
+		objectFolder.setName("SampleObjectFolder");
+
+		objectFolderResource.putObjectFolderByExternalReferenceCode(
+			objectFolder.getExternalReferenceCode(), objectFolder);
+
+		try (PerformanceTimer performanceTimer1 = new PerformanceTimer(
+				36000,
+				StringBundler.concat(
+					"Import all the ", _objectEntriesCount, " Object Entries ",
+					"to SampleObjectFolder by HTTP POST"))) {
+
+			_invokeHttp(
+				_createObjectEntryJSON(), HttpInvoker.HttpMethod.POST,
+				_getPath(_PATH_SUFFIX));
+
+			List<ObjectDefinition> objectDefinitions =
+				_objectDefinitionLocalService.getCustomObjectDefinitions(
+					_CUSTOM_OBJECT_DEFINITION_STATUS);
+
+			_customObjectDefinition = objectDefinitions.get(
+				_OBJECT_DEFINITION_LIST_INDEX);
+
+			_waitForObjectEntriesCountChangedTo(
+				0,
+				currentObjectEntriesCount ->
+					currentObjectEntriesCount < _objectEntriesCount);
+		}
+
+		try (PerformanceTimer performanceTimer = new PerformanceTimer(
+				30000,
+				StringBundler.concat(
+					"Delete all the ", _objectEntriesCount, " Object Entries ",
+					"from SampleObjectFolder by HTTP POST"))) {
+
+			HttpInvoker.HttpResponse httpResponse = _invokeHttp(
+				null, HttpInvoker.HttpMethod.GET,
+				_getPath(
+					"/o/c/foos/?fields=id&pageSize=" + _objectEntriesCount));
+
+			JSONObject jsonObject = _jsonFactory.createJSONObject(
+				httpResponse.getContent());
+
+			JSONArray jsonArray = (JSONArray)jsonObject.get("items");
+
+			_invokeHttp(
+				jsonArray.toString(), HttpInvoker.HttpMethod.DELETE,
+				_getPath(_PATH_SUFFIX));
+
+			_waitForObjectEntriesCountChangedTo(
+				_objectEntriesCount,
+				currentObjectEntriesCount -> currentObjectEntriesCount != 0);
+		}
+	}
+
+	private String _createObjectEntryJSON() {
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		for (int i = 0; i < _objectEntriesCount; i++) {
+			jsonArray.put(JSONUtil.put("alpha", "foo"));
+		}
+
+		return jsonArray.toString();
+	}
+
+	private String _getPath(String pathSuffix) {
+		return StringBundler.concat(
+			"http://", _VIRTUAL_HOST_NAME, ":8080", pathSuffix);
+	}
+
+	private HttpInvoker.HttpResponse _invokeHttp(
+			String bodyJSON, HttpInvoker.HttpMethod httpMethod, String path)
+		throws Exception {
+
+		HttpInvoker httpInvoker = HttpInvoker.newHttpInvoker();
+
+		if (bodyJSON != null) {
+			httpInvoker.body(bodyJSON, "application/json");
+		}
+
+		httpInvoker.httpMethod(httpMethod);
+		httpInvoker.path(path);
+		httpInvoker.userNameAndPassword(
+			StringBundler.concat(
+				"test@", _VIRTUAL_HOST_NAME, ":",
+				PropsValues.DEFAULT_ADMIN_PASSWORD));
+
+		return httpInvoker.invoke();
+	}
+
+	private void _waitForObjectEntriesCountChangedTo(
+		long currentObjectEntriesCount, Predicate<Long> condition) {
+
+		while (condition.test(currentObjectEntriesCount)) {
+			currentObjectEntriesCount =
+				_objectEntryLocalService.getObjectEntriesCount(
+					_customObjectDefinition.getObjectDefinitionId());
+		}
+	}
+
+	private static final int _CUSTOM_OBJECT_DEFINITION_STATUS = 0;
+
+	private static final int _OBJECT_DEFINITION_LIST_INDEX = 0;
+
+	private static final String _PATH_SUFFIX = "/o/c/foos/batch";
+
+	private static final String _VIRTUAL_HOST_NAME = "www.able.com";
+
+	private static int _objectEntriesCount;
 	private static Properties _properties;
+
+	@DeleteAfterTestRun
+	private Company _company;
 
 	@DeleteAfterTestRun
 	private ObjectDefinition _customObjectDefinition;
 
 	@Inject
 	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Inject
+	private JSONFactory _jsonFactory;
 
 	@Inject
 	private ObjectDefinitionLocalService _objectDefinitionLocalService;
@@ -139,5 +326,8 @@ public class ObjectEntryPerformanceTest {
 
 	@Inject
 	private ObjectEntryManagerRegistry _objectEntryManagerRegistry;
+
+	@Inject
+	private ObjectFolderResource.Factory _objectFolderResourceFactory;
 
 }
