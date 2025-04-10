liferay/liferay-portal

LPD-41025 adjust jaxrs route to avoid conflicting with PortalToolsRESTBuilderTestApplication

modules/apps/portal-remote/portal-remote-cors-test/src/testIntegration/java/com/liferay/portal/remote/cors/client/test/AnnotationCORSClientTest.java
@@ -32,7 +32,7 @@ public class AnnotationCORSClientTest extends BaseCORSClientTestCase {
 	@Before
 	public void setUp() {
 		registerJaxRsApplication(
-			new CORSTestApplication(), "test",
+			new CORSTestApplication(), "test-annotation",
 			HashMapDictionaryBuilder.<String, Object>put(
 				"liferay.cors.annotation", true
 			).build());
@@ -40,14 +40,18 @@ public class AnnotationCORSClientTest extends BaseCORSClientTestCase {
 
 	@Test
 	public void testApplicationAnnotationCORSForGuestUser() throws Exception {
-		assertJaxRSUrl("/test/cors-app", HttpMethod.OPTIONS, true, true);
-		assertJaxRSUrl("/test/cors-app", HttpMethod.GET, true, false);
+		assertJaxRSUrl(
+			"/test-annotation/cors-app", HttpMethod.OPTIONS, true, true);
+		assertJaxRSUrl(
+			"/test-annotation/cors-app", HttpMethod.GET, true, false);
 	}
 
 	@Test
 	public void testApplicationAnnotationCORSWithoutOAuth2() throws Exception {
-		assertJaxRSUrl("/test/cors-app", HttpMethod.OPTIONS, true, true);
-		assertJaxRSUrl("/test/cors-app", HttpMethod.GET, true, false);
+		assertJaxRSUrl(
+			"/test-annotation/cors-app", HttpMethod.OPTIONS, true, true);
+		assertJaxRSUrl(
+			"/test-annotation/cors-app", HttpMethod.GET, true, false);
 	}
 
 }

liferay/liferay-portal-cloud-native

modules/apps/portal-remote/portal-remote-cors-test/src/testIntegration/java/com/liferay/portal/remote/cors/client/test/AnnotationCORSClientTest.java
@@ -32,7 +32,7 @@ public class AnnotationCORSClientTest extends BaseCORSClientTestCase {
 	@Before
 	public void setUp() {
 		registerJaxRsApplication(
-			new CORSTestApplication(), "test",
+			new CORSTestApplication(), "cors-annotation",
 			HashMapDictionaryBuilder.<String, Object>put(
 				"liferay.cors.annotation", true
 			).build());
@@ -40,14 +40,18 @@ public class AnnotationCORSClientTest extends BaseCORSClientTestCase {
 
 	@Test
 	public void testApplicationAnnotationCORSForGuestUser() throws Exception {
-		assertJaxRSUrl("/test/cors-app", HttpMethod.OPTIONS, true, true);
-		assertJaxRSUrl("/test/cors-app", HttpMethod.GET, true, false);
+		assertJaxRSUrl(
+			"/cors-annotation/cors-app", HttpMethod.OPTIONS, true, true);
+		assertJaxRSUrl(
+			"/cors-annotation/cors-app", HttpMethod.GET, true, false);
 	}
 
 	@Test
 	public void testApplicationAnnotationCORSWithoutOAuth2() throws Exception {
-		assertJaxRSUrl("/test/cors-app", HttpMethod.OPTIONS, true, true);
-		assertJaxRSUrl("/test/cors-app", HttpMethod.GET, true, false);
+		assertJaxRSUrl(
+			"/cors-annotation/cors-app", HttpMethod.OPTIONS, true, true);
+		assertJaxRSUrl(
+			"/cors-annotation/cors-app", HttpMethod.GET, true, false);
 	}
 
 }