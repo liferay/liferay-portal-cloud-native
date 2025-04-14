64b0c328b8133fa9f658469ad53ecda132e08515
Brian Chan <brian.chan@liferay.com>
2025-04-14 21:57:47 +0530

    LPD-51750 Rename

--- modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/servlet/taglib/LiferayGlobalObjectPreAUIDynamicInclude.java
+++ modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/servlet/taglib/LiferayGlobalObjectPreAUIDynamicInclude.java
@@ -129,7 +129,7 @@ public class LiferayGlobalObjectPreAUIDynamicInclude
 
 		printWriter.println(
 			StringUtil.replace(
-				_LIFERAY_TPL, new String[] {"[$DEFINITION$]", "[$DEV_MODE$]"},
+				_TPL_LIFERAY_JS, new String[] {"[$DEFINITION$]", "[$DEV_MODE$]"},
 				new Object[] {sb, requestURL.startsWith("http://localhost")}));
 
 		printWriter.println("</script>");
@@ -441,7 +441,7 @@ public class LiferayGlobalObjectPreAUIDynamicInclude
 
 		sb.append(
 			StringUtil.replace(
-				_TPL_LANGUAGE, new String[] {"[$AVAILABLE$]", "[$DIRECTION$]"},
+				_TPL_LANGUAGE_JS, new String[] {"[$AVAILABLE$]", "[$DIRECTION$]"},
 				new Object[] {availableSB, directionSB}));
 		sb.append(StringPool.NEW_LINE);
 	}
@@ -727,9 +727,9 @@ public class LiferayGlobalObjectPreAUIDynamicInclude
 		CharPool.DASH, CharPool.FORWARD_SLASH, CharPool.PERIOD
 	};
 
-	private static final String _LIFERAY_TPL;
+	private static final String _TPL_LIFERAY_JS;
 
-	private static final String _TPL_LANGUAGE;
+	private static final String _TPL_LANGUAGE_JS;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		LiferayGlobalObjectPreAUIDynamicInclude.class);
@@ -738,8 +738,8 @@ public class LiferayGlobalObjectPreAUIDynamicInclude
 		new ConcurrentHashMap<>();
 
 	static {
-		_TPL_LANGUAGE = _loadTemplate("language.js.tpl");
-		_LIFERAY_TPL = _loadTemplate("liferay.js.tpl");
+		_TPL_LANGUAGE_JS = _loadTemplate("language.js.tpl");
+		_TPL_LIFERAY_JS = _loadTemplate("liferay.js.tpl");
 	}
 
 	@Reference
