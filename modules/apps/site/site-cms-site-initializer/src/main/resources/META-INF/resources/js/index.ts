1e09c745425991052834bf0ff3958dbb82ff19cd
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-04-22 15:11:49 +0200

    LPD-47341 Create dummy react component for the Edit Folder page

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -17,6 +17,7 @@ export {default as CategorizationBreadcrumb} from './main/categorization/compone
 export {default as ViewTags} from './main/categorization/tags/ViewTags';
 export {default as EditVocabulary} from './main/categorization/vocabularies/EditVocabulary';
 export {default as SpaceSticker} from './main/components/SpaceSticker';
+export {default as EditFolder} from './main/components/folders/EditFolder';
 export {default as ViewDashboard} from './main/dashboard/ViewDashboard';
 export {default as SpacesNavigation} from './main/spaces_navigation/SpacesNavigation';
 export {default as StructureBuilder} from './structure_builder/components/StructureBuilder';
