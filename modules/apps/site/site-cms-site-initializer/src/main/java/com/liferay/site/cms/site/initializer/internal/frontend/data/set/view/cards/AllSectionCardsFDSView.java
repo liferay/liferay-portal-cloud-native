800e66b1672242d7adb15246d5ef66b99bc0666a
Mikel Lorza <mikel.lorza@liferay.com>
2025-04-28 14:37:09 +0200

    LPD-54244 Adapt usages for the new CMS

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/view/cards/AllSectionCardsFDSView.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/view/cards/AllSectionCardsFDSView.java
@@ -7,10 +7,17 @@ package com.liferay.site.cms.site.initializer.internal.frontend.data.set.view.ca
 
 import com.liferay.frontend.data.set.view.FDSView;
 import com.liferay.frontend.data.set.view.cards.BaseCardsFDSView;
+import com.liferay.frontend.data.set.view.cards.FDSCardSchema;
+import com.liferay.frontend.data.set.view.cards.FDSCardSchemaBuilder;
+import com.liferay.frontend.data.set.view.cards.FDSCardSchemaBuilderFactory;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
 
+import java.util.Locale;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Balázs Sáfrány-Kovalik
@@ -32,8 +39,41 @@ public class AllSectionCardsFDSView extends BaseCardsFDSView {
 	}
 
 	@Override
+	public FDSCardSchema getFDSCardSchema(Locale locale) {
+		FDSCardSchemaBuilder fdsCardSchemaBuilder =
+			_fdsCardSchemaBuilderFactory.create();
+
+		return fdsCardSchemaBuilder.add(
+			"embedded.status.label",
+			HashMapBuilder.put(
+				"approved", "success"
+			).put(
+				"denied", "danger"
+			).put(
+				"draft", "secondary"
+			).put(
+				"expired", "danger"
+			).put(
+				"in-trash", "info"
+			).put(
+				"inactive", "secondary"
+			).put(
+				"incomplete", "warning"
+			).put(
+				"pending", "info"
+			).put(
+				"scheduled", "info"
+			).build(),
+			"embedded.status.label_i18n"
+		).build();
+	}
+
+	@Override
 	public String getTitle() {
 		return "embedded.title";
 	}
+
+	@Reference
+	private FDSCardSchemaBuilderFactory _fdsCardSchemaBuilderFactory;
 
 }
