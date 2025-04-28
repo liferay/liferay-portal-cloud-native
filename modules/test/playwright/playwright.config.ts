02cb11c9b37373d612a00eb4f8af8f12b8f2379a
Jonathan McCann <jonathan.mccann@liferay.com>
2025-04-28 10:59:24 -0700

    LPD-54034 Add Playwright test

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -126,6 +126,7 @@ import {config as siteNavigationBreadcrumbWebConfig} from './tests/site-navigati
 import {config as siteNavigationDirectoryWebConfig} from './tests/site-navigation-directory-web/config';
 import {config as siteNavigationLanguageWebConfig} from './tests/site-navigation-language-web/config';
 import {config as siteNavigationMenuWebConfig} from './tests/site-navigation-menu-web/config';
+import {config as siteSitemapWebConfig} from './tests/site-sitemap-web/config';
 import {config as smokeConfig} from './tests/smoke/config';
 import {config as stagingConfig} from './tests/staging-configuration-web/config';
 import {config as stylebookWebConfig} from './tests/style-book-web/config';
@@ -267,6 +268,7 @@ export default defineConfig({
 		siteNavigationDirectoryWebConfig,
 		siteNavigationLanguageWebConfig,
 		siteNavigationMenuWebConfig,
+		siteSitemapWebConfig,
 		smokeConfig,
 		stagingConfig,
 		stylebookWebConfig,
