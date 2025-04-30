4ce5c809d3798c22689d4070c9059730b425125a
Sandro Chinea <sandrochinea@gmail.com>
2025-04-30 09:54:17 +0100

    LPD-54411 Add main folder for portal-search-admin-web

--- modules/test/playwright/tests/portal-search-admin-web/main/indexActions.spec.ts
+++ modules/test/playwright/tests/portal-search-admin-web/main/indexActions.spec.ts
@@ -5,8 +5,8 @@
 
 import {expect, mergeTests} from '@playwright/test';
 
-import {loginTest} from '../../fixtures/loginTest';
-import {searchAdminPageTest} from '../../fixtures/searchAdminPageTest';
+import {loginTest} from '../../../fixtures/loginTest';
+import {searchAdminPageTest} from '../../../fixtures/searchAdminPageTest';
 
 export const test = mergeTests(loginTest(), searchAdminPageTest);
 
