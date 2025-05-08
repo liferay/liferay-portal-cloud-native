cgmonte/liferay-portal

LPD-53714 Remove inline styles from simple_color_picker.js

modules/apps/calendar/calendar-web/src/main/resources/META-INF/resources/js/legacy/simple_color_picker.js
@@ -34,9 +34,6 @@ AUI.add(
 			'<tpl for="pallete">',
 			'<button aria-label="{.}" class="',
 			CSS_SIMPLE_COLOR_PICKER_ITEM,
-			'" style="background-color: {.}',
-			'; border-color:',
-			'{.};',
 			'" role="radio"></button>',
 			'</tpl>'
 		);
@@ -190,11 +187,18 @@ AUI.add(
 				_renderPallete() {
 					const instance = this;
 
+					const corlorPallet = instance.get('pallete');
+
 					instance.items = A.NodeList.create(
 						TPL_SIMPLE_COLOR_PICKER_ITEM.parse({
-							pallete: instance.get('pallete'),
+							pallete: corlorPallet,
 						})
 					);
+
+					instance.items._nodes.forEach((node, index) => {
+						node.style.backgroundColor = corlorPallet[index];
+						node.style.borderColor = corlorPallet[index];
+					});
 
 					const contentBox = instance.get('contentBox');
 

liferay/liferay-portal-cloud-native

modules/apps/calendar/calendar-web/src/main/resources/META-INF/resources/js/legacy/simple_color_picker.js
@@ -34,9 +34,6 @@ AUI.add(
 			'<tpl for="pallete">',
 			'<button aria-label="{.}" class="',
 			CSS_SIMPLE_COLOR_PICKER_ITEM,
-			'" style="background-color: {.}',
-			'; border-color:',
-			'{.};',
 			'" role="radio"></button>',
 			'</tpl>'
 		);
@@ -190,11 +187,18 @@ AUI.add(
 				_renderPallete() {
 					const instance = this;
 
+					const colorPallet = instance.get('pallete');
+
 					instance.items = A.NodeList.create(
 						TPL_SIMPLE_COLOR_PICKER_ITEM.parse({
-							pallete: instance.get('pallete'),
+							pallete: colorPallet,
 						})
 					);
+
+					instance.items._nodes.forEach((node, index) => {
+						node.style.backgroundColor = colorPallet[index];
+						node.style.borderColor = colorPallet[index];
+					});
 
 					const contentBox = instance.get('contentBox');
 