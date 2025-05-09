liferay-site-management/liferay-portal

LPD-53551 Add test

modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-test/src/testIntegration/java/com/liferay/headless/admin/taxonomy/resource/v1_0/test/KeywordResourceTest.java
@@ -450,6 +450,29 @@ public class KeywordResourceTest extends BaseKeywordResourceTestCase {
 
 	@Override
 	@Test
+	public void testPutKeywordMergeKeyword() throws Exception {
+		Keyword keyword1 = _addKeywordWithAssetLibraries(_randomAssetLibrary());
+		Keyword keyword2 = _addKeywordWithAssetLibraries(_randomAssetLibrary());
+		Keyword keyword3 = _addKeywordWithAssetLibraries(_randomAssetLibrary());
+
+		keywordResource.putKeywordMergeKeyword(
+			keyword1.getId(), new Long[] {keyword2.getId(), keyword3.getId()});
+
+		List<AssetTagGroupRel> assetTagGroupRels =
+			_assetTagGroupRelLocalService.getAssetTagGroupRelsByTagId(
+				keyword1.getId());
+
+		Assert.assertEquals(
+			assetTagGroupRels.toString(), 1, assetTagGroupRels.size());
+
+		AssetTagGroupRel assetTagGroupRel = assetTagGroupRels.get(0);
+
+		Assert.assertEquals(
+			assetTagGroupRels.toString(), -1, assetTagGroupRel.getGroupId());
+	}
+
+	@Override
+	@Test
 	public void testPutSiteKeywordByExternalReferenceCode() throws Exception {
 		super.testPutSiteKeywordByExternalReferenceCode();
 

liferay/liferay-portal-cloud-native

modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-test/src/testIntegration/java/com/liferay/headless/admin/taxonomy/resource/v1_0/test/KeywordResourceTest.java
@@ -450,6 +450,29 @@ public class KeywordResourceTest extends BaseKeywordResourceTestCase {
 
 	@Override
 	@Test
+	public void testPutKeywordMerge() throws Exception {
+		Keyword keyword1 = _addKeywordWithAssetLibraries(_randomAssetLibrary());
+		Keyword keyword2 = _addKeywordWithAssetLibraries(_randomAssetLibrary());
+		Keyword keyword3 = _addKeywordWithAssetLibraries(_randomAssetLibrary());
+
+		keywordResource.putKeywordMerge(
+			keyword1.getId(), new Long[] {keyword2.getId(), keyword3.getId()});
+
+		List<AssetTagGroupRel> assetTagGroupRels =
+			_assetTagGroupRelLocalService.getAssetTagGroupRelsByTagId(
+				keyword1.getId());
+
+		Assert.assertEquals(
+			assetTagGroupRels.toString(), 1, assetTagGroupRels.size());
+
+		AssetTagGroupRel assetTagGroupRel = assetTagGroupRels.get(0);
+
+		Assert.assertEquals(
+			assetTagGroupRels.toString(), -1, assetTagGroupRel.getGroupId());
+	}
+
+	@Override
+	@Test
 	public void testPutSiteKeywordByExternalReferenceCode() throws Exception {
 		super.testPutSiteKeywordByExternalReferenceCode();
 