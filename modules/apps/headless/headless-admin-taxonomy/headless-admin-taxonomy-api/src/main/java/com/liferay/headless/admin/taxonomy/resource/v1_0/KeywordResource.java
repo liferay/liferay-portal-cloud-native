liferay-site-management/liferay-portal

LPD-53551 buildREST

modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-api/src/main/java/com/liferay/headless/admin/taxonomy/resource/v1_0/KeywordResource.java
@@ -161,6 +161,9 @@ public interface KeywordResource {
 	public Response putKeywordBatch(String callbackURL, Object object)
 		throws Exception;
 
+	public void putKeywordMergeKeyword(Long targetKeywordId, Long[] longs)
+		throws Exception;
+
 	public void putKeywordSubscribe(Long keywordId) throws Exception;
 
 	public void putKeywordUnsubscribe(Long keywordId) throws Exception;

liferay/liferay-portal-cloud-native

modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-api/src/main/java/com/liferay/headless/admin/taxonomy/resource/v1_0/KeywordResource.java
@@ -161,6 +161,9 @@ public interface KeywordResource {
 	public Response putKeywordBatch(String callbackURL, Object object)
 		throws Exception;
 
+	public void putKeywordMerge(Long toKeywordId, Long[] fromKeywordIds)
+		throws Exception;
+
 	public void putKeywordSubscribe(Long keywordId) throws Exception;
 
 	public void putKeywordUnsubscribe(Long keywordId) throws Exception;