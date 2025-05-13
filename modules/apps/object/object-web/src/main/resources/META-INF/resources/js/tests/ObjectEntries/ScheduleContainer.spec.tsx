2c1ab91b73859d4772bb106743e5a0aac62b3415
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2025-05-13 18:16:40 -0300

    LPD-54813 Add unit tests

---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/tests/ObjectEntries/ScheduleContainer.spec.tsx
@@ -0,0 +1,109 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import ScheduleContainer from '../../../object_entries/object_entry/ScheduleContainer';
+
+function renderScheduleContainer(
+	scheduledProperties = {
+		reviewDate: {
+			checked: false,
+			value: '',
+		},
+	},
+	portletNamespace = ''
+) {
+	return render(
+		<>
+			<ScheduleContainer
+				portletNamespace={portletNamespace}
+				scheduledProperties={scheduledProperties}
+			/>
+			<button data-testid="outside-click-target">
+				Outside Click Target
+			</button>
+		</>
+	);
+}
+
+describe('ScheduleContainer component', () => {
+	beforeAll(() => {
+		global.Liferay = {
+			...global.Liferay,
+			FeatureFlags: {
+				...global.Liferay?.FeatureFlags,
+				'LPD-17564': true,
+			},
+		};
+	});
+
+	it('reviewDate is disabled when checked is true', async () => {
+		const {container} = renderScheduleContainer({
+			reviewDate: {
+				checked: true,
+				value: '',
+			},
+		});
+
+		const checkbox = screen.getByRole('checkbox');
+		const reviewDateInput = container.querySelector(
+			'input[id$="reviewDate"]'
+		);
+
+		expect(reviewDateInput).toBeDisabled();
+		expect(checkbox).toBeChecked();
+	});
+
+	it('shows required error on blur when no value is provided', async () => {
+		const {container} = renderScheduleContainer({
+			reviewDate: {
+				checked: false,
+				value: '',
+			},
+		});
+
+		const reviewDateInput = container.querySelector(
+			'input[id$="reviewDate"]'
+		) as HTMLElement;
+
+		await userEvent.click(reviewDateInput);
+
+		await userEvent.click(screen.getByTestId('outside-click-target'));
+
+		const errorMessage = screen.getByText('this-field-is-required');
+
+		expect(errorMessage).toBeInTheDocument();
+	});
+
+	it('shows required error on change when no value is provided', async () => {
+		const {container} = renderScheduleContainer({
+			reviewDate: {
+				checked: false,
+				value: '05/13/2025 02:38 PM',
+			},
+		});
+
+		const reviewDateInput = container.querySelector(
+			'input[id$="reviewDate"]'
+		) as HTMLElement;
+
+		await userEvent.click(reviewDateInput);
+
+		await userEvent.type(reviewDateInput, '{selectall}{backspace}');
+
+		const errorMessage = await screen.findByText('this-field-is-required');
+		expect(errorMessage).toBeInTheDocument();
+
+		await userEvent.type(reviewDateInput, '05/13/2025 02:38 PM');
+
+		expect(
+			screen.queryByText('this-field-is-required')
+		).not.toBeInTheDocument();
+	});
+});
