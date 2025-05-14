f111cfd8ebb226f3764b1e8dbed7d6f7312af7c5
Mauricio Valdivia <mauricio.valdivia@liferay.com>
2025-05-14 12:01:21 +0200

    LPD-51654 test that the new created company's group should have L_GLOBAL external reference code and checkCompanyGroup should create a new one if already exist.

--- modules/apps/site/site-test/src/testIntegration/java/com/liferay/site/service/test/GroupLocalServiceTest.java
+++ modules/apps/site/site-test/src/testIntegration/java/com/liferay/site/service/test/GroupLocalServiceTest.java
@@ -6,11 +6,20 @@
 package com.liferay.site.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.persistence.GroupPersistence;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.TextFormatter;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
@@ -22,6 +31,8 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static com.liferay.portal.kernel.test.util.CompanyTestUtil.addCompany;
 
 /**
  * @author Miguel Pastor
@@ -62,6 +73,82 @@ public class GroupLocalServiceTest {
 	}
 
 	@Test
+	public void testCheckCompanyGroup() throws Exception {
+		Company company = addCompany();
+
+		try(SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(
+					company.getCompanyId())) {
+
+			long companyId = company.getCompanyId();
+
+			//Verify initial state after company creation
+
+			Group globalGroup = company.getGroup();
+			Assert.assertNotNull(globalGroup);
+
+			long companyClassNameId = _classNameLocalService.getClassNameId(
+				Company.class);
+
+			Assert.assertEquals(companyClassNameId, globalGroup.getClassNameId());
+			Assert.assertEquals(companyId, globalGroup.getClassPK());
+			Assert.assertEquals(GroupConstants.GLOBAL_FRIENDLY_URL,
+				globalGroup.getFriendlyURL());
+			Assert.assertEquals(
+				GroupConstants.GLOBAL,
+				globalGroup.getName(LocaleUtil.getDefault(), true));
+			Assert.assertEquals(
+				"L_" + TextFormatter.format(
+					GroupConstants.GLOBAL, TextFormatter.A),
+				globalGroup.getExternalReferenceCode());
+
+			Assert.assertEquals(
+				1,
+				_groupPersistence.countByC_C_C(
+					companyId, _classNameLocalService.getClassNameId(Company.class),
+					companyId));
+
+			long initialGlobalGroupId = globalGroup.getGroupId();
+
+			// Verify idempotency
+
+			_groupLocalService.checkCompanyGroup(companyId);
+
+			Assert.assertEquals(
+				1, _groupPersistence.countByC_C_C(
+					companyId, _classNameLocalService.getClassNameId(Company.class),
+					companyId));
+
+			Group globalGroupAfterCheck = company.getGroup();
+			Assert.assertNotNull(
+				globalGroupAfterCheck);
+			Assert.assertEquals(
+				initialGlobalGroupId, globalGroupAfterCheck.getGroupId());
+
+			Assert.assertEquals(
+				companyClassNameId, globalGroupAfterCheck.getClassNameId());
+			Assert.assertEquals(
+				companyId,
+				globalGroupAfterCheck.getClassPK());
+			Assert.assertEquals(
+				GroupConstants.GLOBAL_FRIENDLY_URL,
+				globalGroupAfterCheck.getFriendlyURL());
+			Assert.assertEquals(
+				GroupConstants.GLOBAL,
+				globalGroupAfterCheck.getName(LocaleUtil.getDefault(), true));
+			Assert.assertEquals(
+				"L_" + TextFormatter.format(
+					GroupConstants.GLOBAL, TextFormatter.A),
+				globalGroupAfterCheck.getExternalReferenceCode());
+		}
+		finally {
+			if (company != null) {
+				_companyLocalService.deleteCompany(company);
+			}
+		}
+	}
+
+	@Test
 	public void testGetStagedSites() {
 		List<Group> groups = _groupLocalService.getStagedSites();
 
@@ -86,6 +173,15 @@ public class GroupLocalServiceTest {
 
 	@Inject
 	private GroupLocalService _groupLocalService;
+
+	@Inject
+	private ClassNameLocalService _classNameLocalService;
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@Inject
+	private GroupPersistence _groupPersistence;
 
 	@DeleteAfterTestRun
 	private final LinkedList<Group> _groups = new LinkedList<>();
