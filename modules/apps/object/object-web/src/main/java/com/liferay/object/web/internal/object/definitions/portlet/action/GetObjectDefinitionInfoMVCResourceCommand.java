2db202e2b3eb32f472ade59a50cfc02f158c36a3
Nathaly Gomes <nathaly.gomes@liferay.com>
2025-05-16 10:35:20 -0300

    Revert "LPD-53922 Return the workflowDefinitionLabel to show in the ObjectDefinition info"
    This reverts commit 9dad88a003d58670820a1bec5538b65e707f120b.

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/action/GetObjectDefinitionInfoMVCResourceCommand.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/action/GetObjectDefinitionInfoMVCResourceCommand.java
@@ -9,18 +9,10 @@ import com.liferay.object.constants.ObjectPortletKeys;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.model.WorkflowDefinitionLink;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
-import com.liferay.portal.kernel.service.ServiceContextFactory;
-import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalService;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.workflow.kaleo.model.KaleoDefinition;
-import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
 
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
@@ -56,61 +48,10 @@ public class GetObjectDefinitionInfoMVCResourceCommand
 
 		JSONPortletResponseUtil.writeJSON(
 			resourceRequest, resourceResponse,
-			JSONUtil.put(
-				"tableName", objectDefinition.getDBTableName()
-			).put(
-				"workflowDefinitionTitle",
-				() -> {
-					if (!objectDefinition.isRootDescendantNode()) {
-						return _getWorkflowDefinitionTitle(
-							objectDefinition, resourceRequest);
-					}
-
-					ObjectDefinition rootObjectDefinition =
-						_objectDefinitionLocalService.fetchObjectDefinition(
-							objectDefinition.getRootObjectDefinitionId());
-
-					return _getWorkflowDefinitionTitle(
-						rootObjectDefinition, resourceRequest);
-				}
-			));
+			JSONUtil.put("tableName", objectDefinition.getDBTableName()));
 	}
-
-	private String _getWorkflowDefinitionTitle(
-			ObjectDefinition objectDefinition, ResourceRequest resourceRequest)
-		throws Exception {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		WorkflowDefinitionLink workflowDefinitionLink =
-			_workflowDefinitionLinkLocalService.fetchWorkflowDefinitionLink(
-				themeDisplay.getCompanyId(), themeDisplay.getSiteGroupId(),
-				objectDefinition.getClassName(), 0, 0);
-
-		if (workflowDefinitionLink == null) {
-			return _language.get(themeDisplay.getLocale(), "no-workflow");
-		}
-
-		KaleoDefinition kaleoDefinition =
-			_kaleoDefinitionLocalService.fetchKaleoDefinition(
-				workflowDefinitionLink.getWorkflowDefinitionName(),
-				ServiceContextFactory.getInstance(resourceRequest));
-
-		return kaleoDefinition.getTitle(themeDisplay.getLocale());
-	}
-
-	@Reference
-	private KaleoDefinitionLocalService _kaleoDefinitionLocalService;
-
-	@Reference
-	private Language _language;
 
 	@Reference
 	private ObjectDefinitionLocalService _objectDefinitionLocalService;
-
-	@Reference
-	private WorkflowDefinitionLinkLocalService
-		_workflowDefinitionLinkLocalService;
 
 }
