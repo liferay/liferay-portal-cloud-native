is-solutions-delivery/liferay-portal

LPD-54752 Updated the AdministratorSumary page with KPI charts

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/enums/Product.ts
@@ -19,6 +19,11 @@ export enum ProductCategories {
 	MARKETPLACE_SOLUTION_TAGS = 'marketplace-solution-tags',
 }
 
+export enum ProductCategorieTypes {
+	FRAGMENTS = 'Fragments',
+	PAYMENT_METHODS = 'Payment methods',
+}
+
 export enum ProductEditionOption {
 	EE = 'EE',
 }
@@ -172,3 +177,7 @@ export const ProductWorkflowDisplayType = {
 	[ProductWorkflowStatusCode.DRAFT]: 'secondary',
 	[ProductWorkflowStatusCode.PENDING]: 'warn',
 };
+
+export enum PRODUCT_TECHNOLOGY_INTEGRATION {
+	TECHNOLOGY_PARTNERSHIP = 'Technology Partnership',
+}

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AdministratorDashboard/components/charts/DonutKPIChart.tsx
@@ -12,35 +12,27 @@ import {Cell, Pie, PieChart, ResponsiveContainer} from 'recharts';
 
 import Loading from '../../../../components/Loading';
 import i18n from '../../../../i18n';
+import useKPI from '../../hooks/useKPI';
 
-import './donoutChart.scss';
+import './donutKPIChart.scss';
 
-interface DonutCardProps {
-	chartData: {
-		annualTargetCurrent: number;
-		annualTargetTotal: string;
-		colors: string[];
-		monthlyIncreasePct: number;
-		monthlyIncreaseValue: number;
-		percentage: number;
-		title: string;
-		viewDetailsPath?: string;
-	};
-	isloading: boolean;
+interface DonutKPIChartProps {
+	chartData: NonNullable<ReturnType<typeof useKPI>['data']>[number];
+	isLoading: boolean;
 }
 
-const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
+const DonutKPIChart: React.FC<DonutKPIChartProps> = ({
+	chartData,
+	isLoading,
+}) => {
 	const navigate = useNavigate();
 
-	const hdetailsPath = chartData.viewDetailsPath;
+	const hdetailsPath = chartData?.viewDetailsPath;
 
 	const data = [
 		{name: 'filed', value: chartData.percentage},
 		{name: 'remainder', value: 100 - chartData.percentage},
 	];
-
-	const monthlyIncreaseValue = chartData.monthlyIncreaseValue;
-	const monthlyIncreaseValueIsGrowing = monthlyIncreaseValue > 0;
 
 	return (
 		<div className="border d-inline-flex flex-column justify-content-between marketplace-donut-chart p-5">
@@ -63,7 +55,7 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 				)}
 			</div>
 
-			{isloading ? (
+			{isLoading ? (
 				<div className="align-items-center d-flex flex-column h-100 justify-content-center">
 					<Loading
 						className="loading"
@@ -86,7 +78,7 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 									paddingAngle={0}
 									startAngle={90}
 								>
-									{data.map((_, index) => (
+									{data.map((_: any, index: number) => (
 										<Cell
 											fill={chartData.colors[index]}
 											key={index}
@@ -134,7 +126,7 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 							</div>
 						</div>
 
-						{!!monthlyIncreaseValue && (
+						{!!chartData.monthlyIncreaseValue && (
 							<div className="d-flex flex-column">
 								<span className="font-weight-bold text-small">
 									{i18n.translate('monthly-increase')}
@@ -142,12 +134,12 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 
 								<div className="align-items-center d-flex flex-row">
 									<div className="font-weight-bold mr-3 text-title">
-										{`${monthlyIncreaseValueIsGrowing ? '+' : '-'}${monthlyIncreaseValue}`}
+										{`${chartData.monthlyIncreaseValueIsGrowing ? '+' : '-'}${chartData.monthlyIncreaseValue}`}
 									</div>
 
 									<Label
 										displayType={
-											monthlyIncreaseValueIsGrowing
+											chartData.monthlyIncreaseValueIsGrowing
 												? 'success'
 												: 'danger'
 										}
@@ -155,7 +147,7 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 										<span className="d-flex lign-items-center">
 											<ClayIcon
 												symbol={
-													monthlyIncreaseValueIsGrowing
+													chartData.monthlyIncreaseValueIsGrowing
 														? 'order-arrow-up'
 														: 'order-arrow-donw'
 												}
@@ -173,4 +165,4 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 	);
 };
 
-export default DonutCard;
+export default DonutKPIChart;

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AdministratorDashboard/hooks/useKPI.ts
@@ -1,0 +1,192 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import useSWR from 'swr';
+
+import {useMarketplaceContext} from '../../../context/MarketplaceContext';
+import SearchBuilder from '../../../core/SearchBuilder';
+import {
+	PRODUCT_TECHNOLOGY_INTEGRATION,
+	ProductCategorieTypes,
+} from '../../../enums/Product';
+import HeadlessCommerceAdminCatalog from '../../../services/rest/HeadlessCommerceAdminCatalog';
+
+const baseSearchBuilder = new SearchBuilder()
+	.group('OPEN')
+	.lambdaContains('specificationValues', '2025 Q')
+	.or()
+	.lambdaContains('specificationValues', '2024 Q')
+	.or()
+	.lambdaContains('specificationValues', '2023 Q')
+	.group('CLOSE');
+
+const projectUsingMarketplaceAppsFilter = new URLSearchParams({
+	fields: 'totalCount',
+	pageSize: '-1',
+});
+
+const partnerShipIntegrationFilter = new URLSearchParams({
+	fields: 'totalCount,productSpecifications',
+	filter: new SearchBuilder()
+		.lambdaContains(
+			'specificationValues',
+			PRODUCT_TECHNOLOGY_INTEGRATION.TECHNOLOGY_PARTNERSHIP
+		)
+		.build(),
+	nestedFields: 'productSpecifications',
+	pageSize: '-1',
+});
+
+const supportingQuartelyReleaseFilter = new URLSearchParams({
+	fields: 'totalCount,productSpecifications',
+	filter: baseSearchBuilder.clone().build(),
+	nestedFields: 'productSpecifications',
+	pageSize: '-1',
+});
+
+const connectorQuartelyReleaseFilter = new URLSearchParams({
+	fields: 'totalCount,productSpecifications',
+	filter: baseSearchBuilder
+		.clone()
+		.and()
+		.lambda('categoryNames', ProductCategorieTypes.PAYMENT_METHODS)
+		.build(),
+	nestedFields: 'productSpecifications',
+	pageSize: '-1',
+});
+
+const lowCodeConfigurationsPublishedFilter = new URLSearchParams({
+	fields: 'totalCount,productSpecifications,categories',
+	filter: new SearchBuilder()
+		.lambda('categoryNames', ProductCategorieTypes.FRAGMENTS)
+		.build(),
+	nestedFields: 'productSpecifications',
+	pageSize: '-1',
+});
+
+const percentage = (total: number, partial: number): number => {
+	if (!total) {
+		return 0;
+	}
+
+	return Math.round((partial / total) * 100);
+};
+
+const useKPI = () => {
+	const {
+		properties: {kpi: anualTargetKPIs},
+	} = useMarketplaceContext();
+
+	const {
+		kpiConnectorQuartelyRelease,
+		kpiLowCodePublishedApps,
+		kpiPartnershipIntegration,
+		kpiProjectUsingMarketplaceApps,
+		kpiQuartelyReleaseApps,
+	} = anualTargetKPIs;
+
+	const {data, ...swr} = useSWR(
+		'metrics/kpi',
+		async () => {
+			const [
+				connectorQuartelyRelease,
+				lowcodeConfigurationsPublished,
+				partnerShipIntegration,
+				projectUsingMarketplaceApps,
+				supportingQuartelyRelease,
+			] = await Promise.all(
+				[
+					connectorQuartelyReleaseFilter,
+					lowCodeConfigurationsPublishedFilter,
+					partnerShipIntegrationFilter,
+					projectUsingMarketplaceAppsFilter,
+					supportingQuartelyReleaseFilter,
+				].map((searchParams) =>
+					HeadlessCommerceAdminCatalog.getProducts(searchParams)
+				)
+			);
+
+			return [
+				{
+					annualTargetCurrent: projectUsingMarketplaceApps.totalCount,
+					annualTargetTotal: kpiProjectUsingMarketplaceApps,
+					colors: ['#9CE269', '#D4F3BE'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiProjectUsingMarketplaceApps),
+						projectUsingMarketplaceApps.totalCount
+					),
+					title: 'New Projects Using Marketplace Apps	',
+				},
+				{
+					annualTargetCurrent: partnerShipIntegration.totalCount,
+					annualTargetTotal: kpiPartnershipIntegration,
+					colors: ['#FFB46E', '#FFE9D4'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiPartnershipIntegration),
+						partnerShipIntegration.totalCount
+					),
+					title: 'Technology Partnership With Integrations',
+					viewDetailsPath: `/apps?filter=${partnerShipIntegrationFilter.get('filter')}`,
+				},
+				{
+					annualTargetCurrent: supportingQuartelyRelease.totalCount,
+					annualTargetTotal: kpiQuartelyReleaseApps,
+					colors: ['#4B9BFF', '#B1D4FF'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiQuartelyReleaseApps),
+						supportingQuartelyRelease.totalCount
+					),
+					title: 'Publisher With Apps Supporting Quarterly Release',
+					viewDetailsPath: `/apps?filter=${supportingQuartelyReleaseFilter.get('filter')}`,
+				},
+				{
+					annualTargetCurrent: connectorQuartelyRelease.totalCount,
+					annualTargetTotal: kpiConnectorQuartelyRelease,
+					colors: ['#FF73C3', '#FFE1F0'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiConnectorQuartelyRelease),
+						connectorQuartelyRelease.totalCount
+					),
+					title: 'Apps & Connectors Supporting Quarterly Release',
+					viewDetailsPath: `/apps?filter=${connectorQuartelyReleaseFilter.get('filter')}`,
+				},
+				{
+					annualTargetCurrent:
+						lowcodeConfigurationsPublished.totalCount,
+					annualTargetTotal: kpiLowCodePublishedApps,
+					colors: ['#FFD76E', '#FFF3D4'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiLowCodePublishedApps),
+						lowcodeConfigurationsPublished.totalCount
+					),
+					title: 'Low Code Configurations Published',
+					viewDetailsPath: `/apps?filter=${lowCodeConfigurationsPublishedFilter.get('filter')}`,
+				},
+			];
+		},
+		{
+			refreshInterval: 60000,
+		}
+	);
+
+	return {data, ...swr};
+};
+
+export default useKPI;

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AdministratorDashboard/pages/Apps.tsx
@@ -5,6 +5,7 @@
 
 import Label from '@clayui/label';
 import {ComponentProps} from 'react';
+import {useSearchParams} from 'react-router-dom';
 
 import ListView, {ListViewProps} from '../../../components/ListView';
 import Page from '../../../components/Page';
@@ -113,17 +114,18 @@ export function AdministratorAppsListView({
 }
 
 export default function Apps() {
+	const [filters] = useSearchParams();
+
+	const pageFilter =
+		new URLSearchParams(filters).get('filter') ||
+		SearchBuilder.lambda('categoryNames', ProductTypeVocabulary.APP);
+
 	return (
 		<Page
 			description={i18n.translate('list-with-latest-published-apps')}
 			title="Marketplace Apps"
 		>
-			<AdministratorAppsListView
-				filter={SearchBuilder.lambda(
-					'categoryNames',
-					ProductTypeVocabulary.APP
-				)}
-			/>
+			<AdministratorAppsListView filter={pageFilter} />
 		</Page>
 	);
 }

liferay/liferay-portal-cloud-native

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/enums/Product.ts
@@ -19,6 +19,11 @@ export enum ProductCategories {
 	MARKETPLACE_SOLUTION_TAGS = 'marketplace-solution-tags',
 }
 
+export enum ProductCategories {
+	FRAGMENTS = 'Fragments',
+	PAYMENT_METHODS = 'Payment methods',
+}
+
 export enum ProductEditionOption {
 	EE = 'EE',
 }
@@ -212,4 +217,8 @@ export const ProductWorkflowStatusLabel = {
 
 export function getOfferingTypes(type: ProductType) {
 	return offeringTypes[type as keyof typeof offeringTypes];
+}
+
+export enum PARTNERSHIP_TYPE {
+	TECHNOLOGY_PARTNERSHIP = 'Technology Partnership',
 }

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AdministratorDashboard/components/charts/DonutKPIChart.tsx
@@ -12,35 +12,27 @@ import {Cell, Pie, PieChart, ResponsiveContainer} from 'recharts';
 
 import Loading from '../../../../components/Loading';
 import i18n from '../../../../i18n';
+import useKPI from '../../hooks/useKPI';
 
-import './donoutChart.scss';
+import './DonutKPIChart.scss';
 
-interface DonutCardProps {
-	chartData: {
-		annualTargetCurrent: number;
-		annualTargetTotal: string;
-		colors: string[];
-		monthlyIncreasePct: number;
-		monthlyIncreaseValue: number;
-		percentage: number;
-		title: string;
-		viewDetailsPath?: string;
-	};
-	isloading: boolean;
+interface DonutKPIChartProps {
+	chartData: NonNullable<ReturnType<typeof useKPI>['data']>[number];
+	isLoading: boolean;
 }
 
-const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
+const DonutKPIChart: React.FC<DonutKPIChartProps> = ({
+	chartData,
+	isLoading,
+}) => {
 	const navigate = useNavigate();
 
-	const hdetailsPath = chartData.viewDetailsPath;
+	const viewDetailsPath = chartData?.viewDetailsPath;
 
 	const data = [
 		{name: 'filed', value: chartData.percentage},
 		{name: 'remainder', value: 100 - chartData.percentage},
 	];
-
-	const monthlyIncreaseValue = chartData.monthlyIncreaseValue;
-	const monthlyIncreaseValueIsGrowing = monthlyIncreaseValue > 0;
 
 	return (
 		<div className="border d-inline-flex flex-column justify-content-between marketplace-donut-chart p-5">
@@ -49,12 +41,12 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 					{chartData.title}
 				</span>
 
-				{hdetailsPath && (
+				{viewDetailsPath && (
 					<span className="align-items-center d-flex justify-content-center">
 						<Button
 							className="details-button rounded-lg text-nowrap"
 							displayType="secondary"
-							onClick={() => navigate(hdetailsPath)}
+							onClick={() => navigate(viewDetailsPath)}
 							size="sm"
 						>
 							{i18n.translate('view-details')}
@@ -63,7 +55,7 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 				)}
 			</div>
 
-			{isloading ? (
+			{isLoading ? (
 				<div className="align-items-center d-flex flex-column h-100 justify-content-center">
 					<Loading
 						className="loading"
@@ -86,7 +78,7 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 									paddingAngle={0}
 									startAngle={90}
 								>
-									{data.map((_, index) => (
+									{data.map((_: any, index: number) => (
 										<Cell
 											fill={chartData.colors[index]}
 											key={index}
@@ -134,7 +126,7 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 							</div>
 						</div>
 
-						{!!monthlyIncreaseValue && (
+						{!!chartData.monthlyIncreaseValue && (
 							<div className="d-flex flex-column">
 								<span className="font-weight-bold text-small">
 									{i18n.translate('monthly-increase')}
@@ -142,12 +134,12 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 
 								<div className="align-items-center d-flex flex-row">
 									<div className="font-weight-bold mr-3 text-title">
-										{`${monthlyIncreaseValueIsGrowing ? '+' : '-'}${monthlyIncreaseValue}`}
+										{`${chartData.monthlyIncreaseValueIsGrowing ? '+' : '-'}${chartData.monthlyIncreaseValue}`}
 									</div>
 
 									<Label
 										displayType={
-											monthlyIncreaseValueIsGrowing
+											chartData.monthlyIncreaseValueIsGrowing
 												? 'success'
 												: 'danger'
 										}
@@ -155,7 +147,7 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 										<span className="d-flex lign-items-center">
 											<ClayIcon
 												symbol={
-													monthlyIncreaseValueIsGrowing
+													chartData.monthlyIncreaseValueIsGrowing
 														? 'order-arrow-up'
 														: 'order-arrow-donw'
 												}
@@ -173,4 +165,4 @@ const DonutCard: React.FC<DonutCardProps> = ({chartData, isloading}) => {
 	);
 };
 
-export default DonutCard;
+export default DonutKPIChart;

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AdministratorDashboard/hooks/useKPI.ts
@@ -1,0 +1,190 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import useSWR from 'swr';
+
+import {useMarketplaceContext} from '../../../context/MarketplaceContext';
+import SearchBuilder from '../../../core/SearchBuilder';
+import {
+	PARTNERSHIP_TYPE,
+	ProductCategories,
+	ProductType,
+} from '../../../enums/Product';
+import HeadlessCommerceAdminCatalog from '../../../services/rest/HeadlessCommerceAdminCatalog';
+
+const baseSearchBuilder = new SearchBuilder()
+	.group('OPEN')
+	.lambdaContains('specificationValues', '2025 Q')
+	.or()
+	.lambdaContains('specificationValues', '2024 Q')
+	.or()
+	.lambdaContains('specificationValues', '2023 Q')
+	.group('CLOSE');
+
+const projectUsingMarketplaceAppsFilter = new URLSearchParams({
+	fields: 'totalCount',
+	pageSize: '-1',
+});
+
+const partnershipIntegrationFilter = new URLSearchParams({
+	fields: 'totalCount,productSpecifications',
+	filter: new SearchBuilder()
+		.lambda('specificationValues', PARTNERSHIP_TYPE.TECHNOLOGY_PARTNERSHIP)
+		.build(),
+	nestedFields: 'productSpecifications',
+	pageSize: '-1',
+});
+
+const supportingQuartelyReleaseFilter = new URLSearchParams({
+	fields: 'totalCount,productSpecifications',
+	filter: baseSearchBuilder.clone().build(),
+	nestedFields: 'productSpecifications',
+	pageSize: '-1',
+});
+
+const connectorQuartelyReleaseFilter = new URLSearchParams({
+	fields: 'totalCount,productSpecifications',
+	filter: baseSearchBuilder
+		.clone()
+		.and()
+		.lambda('categoryNames', ProductCategories.PAYMENT_METHODS)
+		.build(),
+	nestedFields: 'productSpecifications',
+	pageSize: '-1',
+});
+
+const lowCodeConfigurationsPublishedFilter = new URLSearchParams({
+	fields: 'totalCount,productSpecifications,categories',
+	filter: new SearchBuilder()
+		.lambda('specificationValues', ProductType.LOW_CODE_CONFIGURATION)
+		.build(),
+	nestedFields: 'productSpecifications',
+	pageSize: '-1',
+});
+
+const percentage = (total: number, partial: number): number => {
+	if (!total) {
+		return 0;
+	}
+
+	return Math.round((partial / total) * 100);
+};
+
+const useKPI = () => {
+	const {
+		properties: {kpi: anualTargetKPIs},
+	} = useMarketplaceContext();
+
+	const {
+		kpiConnectorQuartelyRelease,
+		kpiLowCodePublishedApps,
+		kpiPartnershipIntegration,
+		kpiProjectUsingMarketplaceApps,
+		kpiQuartelyReleaseApps,
+	} = anualTargetKPIs;
+
+	const {data, ...swr} = useSWR(
+		'metrics/kpi',
+		async () => {
+			const [
+				connectorQuartelyRelease,
+				lowcodeConfigurationsPublished,
+				partnerShipIntegration,
+				projectUsingMarketplaceApps,
+				supportingQuartelyRelease,
+			] = await Promise.all(
+				[
+					connectorQuartelyReleaseFilter,
+					lowCodeConfigurationsPublishedFilter,
+					partnershipIntegrationFilter,
+					projectUsingMarketplaceAppsFilter,
+					supportingQuartelyReleaseFilter,
+				].map((searchParams) =>
+					HeadlessCommerceAdminCatalog.getProducts(searchParams)
+				)
+			);
+
+			return [
+				{
+					annualTargetCurrent: projectUsingMarketplaceApps.totalCount,
+					annualTargetTotal: kpiProjectUsingMarketplaceApps,
+					colors: ['#9CE269', '#D4F3BE'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiProjectUsingMarketplaceApps),
+						projectUsingMarketplaceApps.totalCount
+					),
+					title: 'New Projects Using Marketplace Apps	',
+				},
+				{
+					annualTargetCurrent: partnerShipIntegration.totalCount,
+					annualTargetTotal: kpiPartnershipIntegration,
+					colors: ['#FFB46E', '#FFE9D4'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiPartnershipIntegration),
+						partnerShipIntegration.totalCount
+					),
+					title: 'Technology Partnership With Integrations',
+					viewDetailsPath: `/apps?filter=${partnershipIntegrationFilter.get('filter')}`,
+				},
+				{
+					annualTargetCurrent: supportingQuartelyRelease.totalCount,
+					annualTargetTotal: kpiQuartelyReleaseApps,
+					colors: ['#4B9BFF', '#B1D4FF'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiQuartelyReleaseApps),
+						supportingQuartelyRelease.totalCount
+					),
+					title: 'Publisher With Apps Supporting Quarterly Release',
+					viewDetailsPath: `/apps?filter=${supportingQuartelyReleaseFilter.get('filter')}`,
+				},
+				{
+					annualTargetCurrent: connectorQuartelyRelease.totalCount,
+					annualTargetTotal: kpiConnectorQuartelyRelease,
+					colors: ['#FF73C3', '#FFE1F0'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiConnectorQuartelyRelease),
+						connectorQuartelyRelease.totalCount
+					),
+					title: 'Apps & Connectors Supporting Quarterly Release',
+					viewDetailsPath: `/apps?filter=${connectorQuartelyReleaseFilter.get('filter')}`,
+				},
+				{
+					annualTargetCurrent:
+						lowcodeConfigurationsPublished.totalCount,
+					annualTargetTotal: kpiLowCodePublishedApps,
+					colors: ['#FFD76E', '#FFF3D4'],
+					monthlyIncreasePct: 0,
+					monthlyIncreaseValue: 0,
+					monthlyIncreaseValueIsGrowing: 0 > 0,
+					percentage: percentage(
+						Number(kpiLowCodePublishedApps),
+						lowcodeConfigurationsPublished.totalCount
+					),
+					title: 'Low Code Configurations Published',
+					viewDetailsPath: `/apps?filter=${lowCodeConfigurationsPublishedFilter.get('filter')}`,
+				},
+			];
+		},
+		{
+			refreshInterval: 60000,
+		}
+	);
+
+	return {data, ...swr};
+};
+
+export default useKPI;

workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/AdministratorDashboard/pages/Apps.tsx
@@ -5,6 +5,7 @@
 
 import Label from '@clayui/label';
 import {ComponentProps} from 'react';
+import {useSearchParams} from 'react-router-dom';
 
 import ListView, {ListViewProps} from '../../../components/ListView';
 import Page from '../../../components/Page';
@@ -113,17 +114,18 @@ export function AdministratorAppsListView({
 }
 
 export default function Apps() {
+	const [searchParams] = useSearchParams();
+
+	const pageFilter =
+		new URLSearchParams(searchParams).get('filter') ||
+		SearchBuilder.lambda('categoryNames', ProductTypeVocabulary.APP);
+
 	return (
 		<Page
 			description={i18n.translate('list-with-latest-published-apps')}
 			title="Marketplace Apps"
 		>
-			<AdministratorAppsListView
-				filter={SearchBuilder.lambda(
-					'categoryNames',
-					ProductTypeVocabulary.APP
-				)}
-			/>
+			<AdministratorAppsListView filter={pageFilter} />
 		</Page>
 	);
 }