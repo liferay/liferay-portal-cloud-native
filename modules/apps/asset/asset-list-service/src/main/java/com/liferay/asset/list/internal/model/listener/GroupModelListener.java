b1deaa9b1d579688d470f11580f2182b1ba7a365
Joshua Cords <joshua.cords@liferay.com>
2025-05-22 15:34:26 -0700

    LPD-36771 asset-list-service: remove groupIds from Collections' typeSettings when the group is deleted
    https://liferay.atlassian.net/browse/LPD-36771

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/model/listener/GroupModelListener.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/model/listener/GroupModelListener.java
@@ -5,19 +5,32 @@
 
 package com.liferay.asset.list.internal.model.listener;
 
+import com.liferay.asset.list.constants.AssetListEntryTypeConstants;
 import com.liferay.asset.list.model.AssetListEntry;
 import com.liferay.asset.list.service.AssetListEntryLocalService;
+import com.liferay.asset.list.service.AssetListEntrySegmentsEntryRelLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
+ * @author Joshua Cords
  * @author Jürgen Kappler
  */
 @Component(service = ModelListener.class)
@@ -25,10 +38,14 @@ public class GroupModelListener extends BaseModelListener<Group> {
 
 	@Override
 	public void onBeforeRemove(Group group) throws ModelListenerException {
+		_removeAssetEntriesFromAssetLists(group.getGroupId());
+		_removeGroupFromAssetLists(group.getGroupId());
+	}
+
+	private void _removeAssetEntriesFromAssetLists(long groupId) {
 		try {
 			List<AssetListEntry> assetListEntries =
-				_assetListEntryLocalService.getAssetListEntries(
-					group.getGroupId());
+				_assetListEntryLocalService.getAssetListEntries(groupId);
 
 			for (AssetListEntry assetListEntry : assetListEntries) {
 				_assetListEntryLocalService.deleteAssetListEntry(
@@ -40,7 +57,76 @@ public class GroupModelListener extends BaseModelListener<Group> {
 		}
 	}
 
+	private void _removeFromTypeSettings(
+		long groupId, AssetListEntry assetListEntry, long segmentsEntryId) {
+
+		UnicodeProperties unicodeProperties = UnicodePropertiesBuilder.create(
+			true
+		).fastLoad(
+			assetListEntry.getTypeSettings(segmentsEntryId)
+		).build();
+
+		long[] storedGroupIds = GetterUtil.getLongValues(
+			StringUtil.split(
+				unicodeProperties.getProperty("groupIds", StringPool.BLANK)));
+
+		if (ArrayUtil.isEmpty(storedGroupIds)) {
+			return;
+		}
+
+		List<Long> storedGroupIdsList = ListUtil.fromArray(storedGroupIds);
+
+		if (storedGroupIdsList.remove(groupId)) {
+			unicodeProperties.setProperty(
+				"groupIds", StringUtil.merge(storedGroupIdsList));
+
+			_assetListEntrySegmentsEntryRelLocalService.
+				updateAssetListEntrySegmentsEntryRelTypeSettings(
+					assetListEntry.getAssetListEntryId(), segmentsEntryId,
+					unicodeProperties.toString());
+		}
+	}
+
+	private void _removeGroupFromAssetLists(long groupId) {
+		try {
+			List<AssetListEntry> assetListEntries =
+				_assetListEntryLocalService.getAssetListEntries(
+					QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+			for (AssetListEntry assetListEntry : assetListEntries) {
+				if (Objects.equals(
+						assetListEntry.getType(),
+						AssetListEntryTypeConstants.TYPE_MANUAL)) {
+
+					continue;
+				}
+
+				long[] segmentsEntryIds = ArrayUtil.toLongArray(
+					TransformUtil.transform(
+						_assetListEntrySegmentsEntryRelLocalService.
+							getAssetListEntrySegmentsEntryRels(
+								assetListEntry.getAssetListEntryId(),
+								QueryUtil.ALL_POS, QueryUtil.ALL_POS),
+						assetListEntrySegmentsEntryRel ->
+							assetListEntrySegmentsEntryRel.
+								getSegmentsEntryId()));
+
+				for (long segmentsEntryId : segmentsEntryIds) {
+					_removeFromTypeSettings(
+						groupId, assetListEntry, segmentsEntryId);
+				}
+			}
+		}
+		catch (Exception exception) {
+			throw new ModelListenerException(exception);
+		}
+	}
+
 	@Reference
 	private AssetListEntryLocalService _assetListEntryLocalService;
+
+	@Reference
+	private AssetListEntrySegmentsEntryRelLocalService
+		_assetListEntrySegmentsEntryRelLocalService;
 
 }
