2ff83884c136494de528e84602ca2762aa012ca3
Marko Čikoš <marko.cikos@liferay.com>
2025-05-22 17:08:32 +0200

    LPD-55650 Add tests for new interactions

--- modules/test/playwright/tests/frontend-js-bootstrap-support-web/main/dropdown.spec.ts
+++ modules/test/playwright/tests/frontend-js-bootstrap-support-web/main/dropdown.spec.ts
@@ -25,90 +25,151 @@ export const test = mergeTests(
 );
 
 test(
-	'Check dropdown disappears after tabbing through',
+	'Keyboard interactions',
 	{tag: '@LPD-55650'},
 	async ({apiHelpers, page, pageEditorPage, site}) => {
 		const dropdownHTML =
-			'<div class="row">\n' +
-			'    <div class="col">\n' +
-			'        <button aria-expanded="false" aria-haspopup="true" class="mr-5 dropdown-toggle nav-link"\n' +
-			'            data-toggle="liferay-dropdown" id="t_dropdown_menu" type="button">\n' +
-			'            <span class="font-weight-bold ml-1">Menu 1</span>\n' +
-			'        </button>\n' +
-			'\n' +
-			'        <ul aria-labelledby="t_dropdown_menu" class="dropdown-menu right">\n' +
-			'            <li>\n' +
-			'                <a class="dropdown-item" href="javascript:void(0)">\n' +
-			'                    My Learning\n' +
-			'                </a>\n' +
-			'            </li>\n' +
-			'        </ul>\n' +
-			'    </div>\n' +
-			'    <div class="col">\n' +
-			'        <button aria-expanded="false" aria-haspopup="true" class="dropdown-toggle nav-link"\n' +
-			'            data-toggle="liferay-dropdown" id="t_dropdown_menu" type="button">\n' +
-			'            <span class="font-weight-bold ml-1">Menu 2</span>\n' +
-			'        </button>\n' +
-			'        <ul aria-labelledby="t_dropdown_menu" class="dropdown-menu right">\n' +
-			'            <li>\n' +
-			'                <a class="dropdown-item" href="javascript:void(0)">\n' +
-			'                    My Learning\n' +
-			'                </a>\n' +
-			'            </li>\n' +
-			'        </ul>\n' +
-			'    </div>\n' +
+			'<div id="sampleDropdownContainer">\n' +
+			'    <button class="dropdown-toggle" data-toggle="liferay-dropdown">\n' +
+			'        Sample Menu\n' +
+			'    </button>\n' +
+			'    <ul class="dropdown-menu">\n' +
+			'        <li>\n' +
+			'           <a class="dropdown-item" href="#">\n' +
+			'                Sample Item 1\n' +
+			'           </a>\n' +
+			'        </li>\n' +
+			'        <li>\n' +
+			'           <div>\n' +
+			'               <a class="dropdown-item" href="#">\n' +
+			'                    Sample Item 2\n' +
+			'               </a>\n' +
+			'           </div>\n' +
+			'        </li>\n' +
+			'        <li>\n' +
+			'           <a class="dropdown-item" href="#">\n' +
+			'                Sample Item 3\n' +
+			'           </a>\n' +
+			'        </li>\n' +
+			'    </ul>\n' +
+			'    <a href="#">\n' +
+			'        Next Interactable Element \n' +
+			'    </a>' +
 			'</div>';
 
-		const layoutTitle = getRandomString();
+		await test.step('Prepare HTML example of dropdown markup', async () => {
+			const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
+				groupId: site.id,
+				options: {type: 'content'},
+				title: getRandomString(),
+			});
 
-		const layout = await apiHelpers.jsonWebServicesLayout.addLayout({
-			groupId: site.id,
-			options: {type: 'content'},
-			title: layoutTitle,
+			await pageEditorPage.goto(layout, site.friendlyUrlPath);
+
+			await pageEditorPage.addFragment('Basic Components', 'HTML');
+
+			const htmlFragmentId = await pageEditorPage.getFragmentId('HTML');
+
+			await pageEditorPage.selectEditable(htmlFragmentId, 'element-html');
+
+			await page.getByText('HTML Example').click();
+
+			const htmlEditor = page.getByText('<h1>HTML Example</h1>');
+
+			await htmlEditor.click();
+
+			await page.keyboard.press('Control+A');
+
+			await page.keyboard.press('Backspace');
+
+			await page.keyboard.insertText(dropdownHTML);
+
+			await page.getByRole('button', {name: 'Save'}).click();
+
+			await pageEditorPage.waitForChangesSaved();
+
+			await pageEditorPage.publishPage();
+
+			await page.goto(`/web${site.friendlyUrlPath}${layout.friendlyURL}`);
 		});
 
-		await pageEditorPage.goto(layout, site.friendlyUrlPath);
+		const container = page.locator('#sampleDropdownContainer');
 
-		await pageEditorPage.addFragment('Basic Components', 'HTML');
+		const trigger = container.getByRole('button', {name: 'Sample Menu'});
+		const menu = container.locator('.dropdown-menu');
+		const firstMenuItem = container.getByRole('link', {
+			name: 'Sample Item 1',
+		});
+		const secondMenuItem = container.getByRole('link', {
+			name: 'Sample Item 2',
+		});
+		const thirdMenuItem = container.getByRole('link', {
+			name: 'Sample Item 3',
+		});
+		const nextInteractableElement = container.getByText(
+			'Next Interactable Element'
+		);
 
-		const htmlFragmentId = await pageEditorPage.getFragmentId('HTML');
+		await test.step('Open menu with ArrowDown key', async () => {
+			await trigger.focus();
 
-		await pageEditorPage.selectEditable(htmlFragmentId, 'element-html');
+			await page.keyboard.press('ArrowDown');
 
-		await page.getByText('HTML Example').click();
+			await expect(menu).toBeVisible();
 
-		const htmlEditor = page.getByText('<h1>HTML Example</h1>');
-
-		await htmlEditor.click();
-
-		await page.keyboard.press('Control+A');
-
-		await page.keyboard.press('Backspace');
-
-		await page.keyboard.insertText(dropdownHTML);
-
-		await page.getByRole('button', {name: 'Save'}).click();
-
-		await pageEditorPage.waitForChangesSaved();
-
-		await pageEditorPage.publishPage();
-
-		await page.goto(`/web${site.friendlyUrlPath}${layout.friendlyURL}`);
-
-		const menu1Button = page.getByRole('button', {name: 'Menu 1'});
-
-		await menu1Button.click();
-
-		await page.keyboard.press('Tab');
-
-		const myLearningDropdown = page.getByRole('link', {
-			name: 'My Learning',
+			await expect(firstMenuItem).toBeFocused();
 		});
 
-		await expect(myLearningDropdown).toBeVisible();
+		await test.step('Close menu with Escape key', async () => {
+			await page.keyboard.press('Escape');
 
-		await page.keyboard.press('Tab');
+			await expect(menu).toBeHidden();
 
-		await expect(myLearningDropdown).not.toBeVisible();
+			await expect(trigger).toBeFocused();
+		});
+
+		await test.step('Open menu with Space key', async () => {
+			await trigger.focus();
+
+			await page.keyboard.press('Space');
+
+			await expect(menu).toBeVisible();
+
+			await expect(firstMenuItem).toBeFocused();
+		});
+
+		await test.step('Navigate menu items with Arrow keys', async () => {
+			await page.keyboard.press('ArrowDown');
+
+			await expect(secondMenuItem).toBeFocused();
+
+			await page.keyboard.press('ArrowDown');
+
+			await expect(thirdMenuItem).toBeFocused();
+
+			await page.keyboard.press('ArrowDown');
+
+			await expect(firstMenuItem).toBeFocused();
+
+			await page.keyboard.press('ArrowUp');
+
+			await expect(thirdMenuItem).toBeFocused();
+
+			await page.keyboard.press('ArrowUp');
+
+			await expect(secondMenuItem).toBeFocused();
+
+			await page.keyboard.press('ArrowUp');
+
+			await expect(firstMenuItem).toBeFocused();
+		});
+
+		await test.step('Close menu and navigate away with Tab key', async () => {
+			await page.keyboard.press('Tab');
+
+			await expect(nextInteractableElement).toBeFocused();
+
+			await expect(menu).toBeHidden();
+		});
 	}
 );
