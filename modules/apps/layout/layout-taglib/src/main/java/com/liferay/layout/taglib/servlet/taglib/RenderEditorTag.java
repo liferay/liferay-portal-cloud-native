68878a4f9b72a33908e98f066a0607cf7c44b068
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-05-28 11:42:54 +0200

    LPD-55891 Adapt to jakarta

--- modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/RenderEditorTag.java
+++ modules/apps/layout/layout-taglib/src/main/java/com/liferay/layout/taglib/servlet/taglib/RenderEditorTag.java
@@ -10,8 +10,8 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.taglib.util.IncludeTag;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.jsp.PageContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.jsp.PageContext;
 
 /**
  * @author Verónica González
