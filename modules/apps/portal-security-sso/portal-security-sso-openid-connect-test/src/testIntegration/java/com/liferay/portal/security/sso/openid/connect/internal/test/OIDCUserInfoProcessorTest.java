67b119f45d6d31854a58f1993e7a90e7c95ea1b5
Jorge García Jiménez <jorge.garcia@liferay.com>
2025-05-30 11:34:52 +0200

    LPD-46626 use Inject in test, SF

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-test/src/testIntegration/java/com/liferay/portal/security/sso/openid/connect/internal/test/OIDCUserInfoProcessorTest.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-test/src/testIntegration/java/com/liferay/portal/security/sso/openid/connect/internal/test/OIDCUserInfoProcessorTest.java
@@ -7,20 +7,10 @@ package com.liferay.portal.security.sso.openid.connect.internal.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.oauth.client.persistence.constants.OAuthClientEntryConstants;
-import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
-import com.liferay.portal.kernel.module.util.BundleUtil;
-import com.liferay.portal.kernel.module.util.SystemBundleUtil;
-import com.liferay.portal.kernel.service.AddressLocalService;
-import com.liferay.portal.kernel.service.CompanyLocalService;
-import com.liferay.portal.kernel.service.CountryLocalService;
-import com.liferay.portal.kernel.service.ListTypeLocalService;
-import com.liferay.portal.kernel.service.PhoneLocalService;
-import com.liferay.portal.kernel.service.RegionLocalService;
-import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserGroupLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
@@ -30,21 +20,16 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.util.Props;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-
-import java.lang.reflect.Constructor;
 
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import org.osgi.framework.Bundle;
 
 /**
  * @author Jorge García Jiménez
@@ -59,44 +44,6 @@ public class OIDCUserInfoProcessorTest {
 
 	@Test
 	public void testProcessUserInfo() throws Exception {
-		Bundle bundle = BundleUtil.getBundle(
-			SystemBundleUtil.getBundleContext(),
-			"com.liferay.portal.security.sso.openid.connect.impl");
-
-		Class<?> clazz = bundle.loadClass(_CLASS_NAME_OIDC_USER_INFO_PROCESSOR);
-
-		Constructor<?> constructor = clazz.getConstructor();
-
-		_oidcUserInfoProcessor = constructor.newInstance();
-
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_jsonFactory", _jsonFactory);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_companyLocalService",
-			_companyLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_userGroupLocalService",
-			_userGroupLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_userLocalService", _userLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_addressLocalService",
-			_addressLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_countryLocalService",
-			_countryLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_listTypeLocalService",
-			_listTypeLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_phoneLocalService", _phoneLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_props", _props);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_regionLocalService", _regionLocalService);
-		ReflectionTestUtil.setFieldValue(
-			_oidcUserInfoProcessor, "_roleLocalService", _roleLocalService);
-
 		String email = RandomTestUtil.randomString() + "@liferay.com";
 		String uuid = PortalUUIDUtil.generate();
 
@@ -128,6 +75,9 @@ public class OIDCUserInfoProcessorTest {
 
 		_serviceContext = ServiceContextTestUtil.getServiceContext(
 			_group.getGroupId(), TestPropsValues.getUserId());
+
+		_serviceContext.setAttribute(
+			"oAuthClientEntryId", RandomTestUtil.randomLong());
 
 		_userGroup = _userGroupLocalService.fetchUserGroup(
 			_group.getCompanyId(), "group1");
@@ -199,41 +149,14 @@ public class OIDCUserInfoProcessorTest {
 			_userGroupLocalService.getUserUserGroupsCount(_user.getUserId()));
 	}
 
-	private static final String _CLASS_NAME_OIDC_USER_INFO_PROCESSOR =
-		"com.liferay.portal.security.sso.openid.connect.internal." +
-			"OIDCUserInfoProcessor";
-
-	@Inject
-	private AddressLocalService _addressLocalService;
-
-	@Inject
-	private CompanyLocalService _companyLocalService;
-
-	@Inject
-	private CountryLocalService _countryLocalService;
-
 	@DeleteAfterTestRun
 	private Group _group;
 
-	@Inject
-	private JSONFactory _jsonFactory;
-
-	@Inject
-	private ListTypeLocalService _listTypeLocalService;
-
+	@Inject(
+		filter = "component.name=com.liferay.portal.security.sso.openid.connect.internal.OIDCUserInfoProcessor",
+		type = Inject.NoType.class
+	)
 	private Object _oidcUserInfoProcessor;
-
-	@Inject
-	private PhoneLocalService _phoneLocalService;
-
-	@Inject
-	private Props _props;
-
-	@Inject
-	private RegionLocalService _regionLocalService;
-
-	@Inject
-	private RoleLocalService _roleLocalService;
 
 	private ServiceContext _serviceContext;
 
