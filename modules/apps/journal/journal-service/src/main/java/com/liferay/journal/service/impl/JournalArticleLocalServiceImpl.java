9652616797f743be96707992089f862083175293
Brian Chan <brian.chan@liferay.com>
2025-06-02 12:27:07 -0300

    LPD-52357 Please move the long description there or reword this because the wording sounds off

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -6081,13 +6081,7 @@ public class JournalArticleLocalServiceImpl
 						folder.getGroupId(), folder.getFolderId(),
 						tempFileEntry.getFileName(), false);
 
-					// The UUID of the temporary file is stored on the field
-					// once saved as temp. However, when the system auto saves,
-					// this temporary file will be deleted and it's published
-					// with a different UUID, so it doesn't match. To be able to
-					// use the file, we'll use the temporary file's UUID as
-					// published file entry ERC so we could fetch it using this
-					// field. See LPD-52357.
+					// See LPD-52357
 
 					fileEntry = _portletFileRepository.addPortletFileEntry(
 						tempFileEntry.getUuid(), folder.getGroupId(),
