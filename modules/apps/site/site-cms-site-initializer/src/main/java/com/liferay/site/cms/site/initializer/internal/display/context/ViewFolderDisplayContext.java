liferay-content-management/liferay-portal

LPD-55752 Using the breadcrumb when showing the folder items

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewFolderDisplayContext.java
@@ -13,14 +13,20 @@ import com.liferay.object.service.ObjectDefinitionService;
 import com.liferay.object.service.ObjectDefinitionSettingLocalService;
 import com.liferay.object.service.ObjectEntryFolderLocalService;
 import com.liferay.petra.string.CharPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.site.cms.site.initializer.internal.util.ActionUtil;
 
+import java.util.Collections;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -45,6 +51,46 @@ public class ViewFolderDisplayContext extends BaseSectionDisplayContext {
 			objectDefinitionSettingLocalService, portal);
 
 		_objectEntryFolderLocalService = objectEntryFolderLocalService;
+	}
+
+	public Map<String, Object> getBreadcrumbReactData() {
+		if (objectEntryFolder == null) {
+			return Collections.emptyMap();
+		}
+
+		Group group = groupLocalService.fetchGroup(
+			objectEntryFolder.getGroupId());
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		_addBreadcrumbItem(
+			jsonArray, false,
+			ActionUtil.getSpaceFullURL(group.getClassPK(), themeDisplay),
+			group.getName(themeDisplay.getLocale()));
+
+		String[] parts = StringUtil.split(
+			objectEntryFolder.getTreePath(), CharPool.SLASH);
+
+		if (parts.length > 2) {
+			for (int i = 1; i < (parts.length - 1); i++) {
+				ObjectEntryFolder objectEntryFolder =
+					_objectEntryFolderLocalService.fetchObjectEntryFolder(
+						GetterUtil.getLong(parts[i]));
+
+				_addBreadcrumbItem(
+					jsonArray, false,
+					ActionUtil.geViewFolderFullURL(
+						objectEntryFolder.getObjectEntryFolderId(),
+						themeDisplay),
+					objectEntryFolder.getName());
+			}
+		}
+
+		_addBreadcrumbItem(jsonArray, true, null, objectEntryFolder.getName());
+
+		return HashMapBuilder.<String, Object>put(
+			"breadcrumbItems", jsonArray
+		).build();
 	}
 
 	@Override
@@ -143,6 +189,19 @@ public class ViewFolderDisplayContext extends BaseSectionDisplayContext {
 	@Override
 	protected String getCMSSectionFilterString() {
 		return null;
+	}
+
+	private void _addBreadcrumbItem(
+		JSONArray jsonArray, boolean active, String friendlyURL, String label) {
+
+		jsonArray.put(
+			JSONUtil.put(
+				"active", active
+			).put(
+				"href", () -> friendlyURL
+			).put(
+				"label", label
+			));
 	}
 
 	private final ObjectEntryFolderLocalService _objectEntryFolderLocalService;

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -17,6 +17,7 @@ export {default as EditCategoryPage} from './main/categorization/categories/Edit
 export {default as CategorizationBreadcrumb} from './main/categorization/components/CategorizationBreadcrumb';
 export {default as ViewTags} from './main/categorization/tags/ViewTags';
 export {default as EditVocabulary} from './main/categorization/vocabularies/EditVocabulary';
+export {default as FolderBreadcrumb} from './main/components/FolderBreadcrumb';
 export {default as SpaceSticker} from './main/components/SpaceSticker';
 export {default as EditFolder} from './main/components/folders/EditFolder';
 export {default as ViewDashboard} from './main/dashboard/ViewDashboard';

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/view_folder.jsp
@@ -12,6 +12,13 @@ ViewFolderDisplayContext viewFolderDisplayContext = (ViewFolderDisplayContext)re
 %>
 
 <div class="cms-section">
+	<div>
+		<react:component
+			module="{FolderBreadcrumb} from site-cms-site-initializer"
+			props="<%= viewFolderDisplayContext.getBreadcrumbReactData() %>"
+		/>
+	</div>
+
 	<frontend-data-set:headless-display
 		apiURL="<%= viewFolderDisplayContext.getAPIURL() %>"
 		bulkActionDropdownItems="<%= viewFolderDisplayContext.getBulkActionDropdownItems() %>"

liferay/liferay-portal-cloud-native

modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/ViewFolderDisplayContext.java
@@ -15,17 +15,23 @@ import com.liferay.object.service.ObjectDefinitionSettingLocalService;
 import com.liferay.object.service.ObjectEntryFolderLocalService;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.site.cms.site.initializer.internal.util.ActionUtil;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -50,6 +56,46 @@ public class ViewFolderDisplayContext extends BaseSectionDisplayContext {
 			objectDefinitionSettingLocalService, portal);
 
 		_objectEntryFolderLocalService = objectEntryFolderLocalService;
+	}
+
+	public Map<String, Object> getBreadcrumbProps() {
+		if (objectEntryFolder == null) {
+			return Collections.emptyMap();
+		}
+
+		Group group = groupLocalService.fetchGroup(
+			objectEntryFolder.getGroupId());
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		_addBreadcrumbItem(
+			jsonArray, false,
+			ActionUtil.getSpaceURL(group.getClassPK(), themeDisplay),
+			group.getName(themeDisplay.getLocale()));
+
+		String[] parts = StringUtil.split(
+			objectEntryFolder.getTreePath(), CharPool.SLASH);
+
+		if (parts.length > 2) {
+			for (int i = 1; i < (parts.length - 1); i++) {
+				ObjectEntryFolder objectEntryFolder =
+					_objectEntryFolderLocalService.fetchObjectEntryFolder(
+						GetterUtil.getLong(parts[i]));
+
+				_addBreadcrumbItem(
+					jsonArray, false,
+					ActionUtil.geViewFolderURL(
+						objectEntryFolder.getObjectEntryFolderId(),
+						themeDisplay),
+					objectEntryFolder.getName());
+			}
+		}
+
+		_addBreadcrumbItem(jsonArray, true, null, objectEntryFolder.getName());
+
+		return HashMapBuilder.<String, Object>put(
+			"breadcrumbItems", jsonArray
+		).build();
 	}
 
 	@Override
@@ -190,6 +236,19 @@ public class ViewFolderDisplayContext extends BaseSectionDisplayContext {
 	@Override
 	protected String getCMSSectionFilterString() {
 		return null;
+	}
+
+	private void _addBreadcrumbItem(
+		JSONArray jsonArray, boolean active, String friendlyURL, String label) {
+
+		jsonArray.put(
+			JSONUtil.put(
+				"active", active
+			).put(
+				"href", friendlyURL
+			).put(
+				"label", label
+			));
 	}
 
 	private final ObjectEntryFolderLocalService _objectEntryFolderLocalService;

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -27,6 +27,7 @@ export {default as EditCategoryPage} from './main/categorization/categories/Edit
 export {default as CategorizationBreadcrumb} from './main/categorization/components/CategorizationBreadcrumb';
 export {default as ViewTags} from './main/categorization/tags/ViewTags';
 export {default as EditVocabulary} from './main/categorization/vocabularies/EditVocabulary';
+export {default as Breadcrumb} from './main/components/Breadcrumb';
 export {default as SpaceSticker} from './main/components/SpaceSticker';
 export {default as EditFolder} from './main/components/folders/EditFolder';
 export {default as ViewDashboard} from './main/dashboard/ViewDashboard';

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/view_folder.jsp
@@ -12,6 +12,13 @@ ViewFolderDisplayContext viewFolderDisplayContext = (ViewFolderDisplayContext)re
 %>
 
 <div class="cms-section">
+	<div>
+		<react:component
+			module="{Breadcrumb} from site-cms-site-initializer"
+			props="<%= viewFolderDisplayContext.getBreadcrumbProps() %>"
+		/>
+	</div>
+
 	<frontend-data-set:headless-display
 		apiURL="<%= viewFolderDisplayContext.getAPIURL() %>"
 		bulkActionDropdownItems="<%= viewFolderDisplayContext.getBulkActionDropdownItems() %>"