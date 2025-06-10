9d24e65bacd4ea1406fa554d2023838727d87bce
Joshua Cords <joshua.cords@liferay.com>
2025-06-09 18:31:07 -0700

    LPD-36771 asset-list-service: use new finder
    https://liferay.atlassian.net/browse/LPD-36771

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/model/listener/GroupModelListener.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/model/listener/GroupModelListener.java
@@ -5,13 +5,11 @@
 
 package com.liferay.asset.list.internal.model.listener;
 
-import com.liferay.asset.list.constants.AssetListEntryTypeConstants;
 import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.asset.list.model.AssetListEntrySegmentsEntryRel;
 import com.liferay.asset.list.service.AssetListEntryLocalService;
 import com.liferay.asset.list.service.AssetListEntrySegmentsEntryRelLocalService;
-import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.Group;
@@ -22,7 +20,6 @@ import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 
 import java.util.List;
-import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -37,7 +34,7 @@ public class GroupModelListener extends BaseModelListener<Group> {
 	@Override
 	public void onBeforeRemove(Group group) throws ModelListenerException {
 		_deleteAssetListEntries(group.getGroupId());
-		_updateTypeSettings(group.getGroupId());
+		_updateTypeSettings(group);
 	}
 
 	private void _deleteAssetListEntries(long groupId) {
@@ -55,63 +52,38 @@ public class GroupModelListener extends BaseModelListener<Group> {
 		}
 	}
 
-	private void _updateTypeSettings(
-		AssetListEntry assetListEntry, String groupId, long segmentsEntryId) {
-
-		UnicodeProperties unicodeProperties = UnicodePropertiesBuilder.create(
-			true
-		).fastLoad(
-			assetListEntry.getTypeSettings(segmentsEntryId)
-		).build();
-
-		String[] groupIds = StringUtil.split(
-			unicodeProperties.getProperty("groupIds", StringPool.BLANK));
-
-		if (ArrayUtil.contains(groupIds, groupId)) {
-			unicodeProperties.setProperty(
-				"groupIds",
-				StringUtil.merge(ArrayUtil.remove(groupIds, groupId)));
-
+	private void _updateTypeSettings(Group group) {
+		List<AssetListEntrySegmentsEntryRel> assetListEntrySegmentsEntryRels =
 			_assetListEntrySegmentsEntryRelLocalService.
-				updateAssetListEntrySegmentsEntryRelTypeSettings(
-					assetListEntry.getAssetListEntryId(), segmentsEntryId,
-					unicodeProperties.toString());
-		}
-	}
+				fetchDynamicAssetListEntrySegmentsEntryRels(
+					group.getCompanyId());
 
-	private void _updateTypeSettings(long groupId) {
-		try {
-			List<AssetListEntry> assetListEntries =
-				_assetListEntryLocalService.getAssetListEntries(
-					QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		String groupId = String.valueOf(group.getGroupId());
 
-			for (AssetListEntry assetListEntry : assetListEntries) {
-				if (Objects.equals(
-						assetListEntry.getType(),
-						AssetListEntryTypeConstants.TYPE_MANUAL)) {
+		for (AssetListEntrySegmentsEntryRel assetListEntrySegmentsEntryRel :
+				assetListEntrySegmentsEntryRels) {
 
-					continue;
-				}
+			UnicodeProperties unicodeProperties =
+				UnicodePropertiesBuilder.create(
+					true
+				).fastLoad(
+					assetListEntrySegmentsEntryRel.getTypeSettings()
+				).build();
 
-				long[] segmentsEntryIds = ArrayUtil.toLongArray(
-					TransformUtil.transform(
-						_assetListEntrySegmentsEntryRelLocalService.
-							getAssetListEntrySegmentsEntryRels(
-								assetListEntry.getAssetListEntryId(),
-								QueryUtil.ALL_POS, QueryUtil.ALL_POS),
-						assetListEntrySegmentsEntryRel ->
-							assetListEntrySegmentsEntryRel.
-								getSegmentsEntryId()));
+			String[] groupIds = StringUtil.split(
+				unicodeProperties.getProperty("groupIds", StringPool.BLANK));
 
-				for (long segmentsEntryId : segmentsEntryIds) {
-					_updateTypeSettings(
-						assetListEntry, String.valueOf(groupId),
-						segmentsEntryId);
-				}
+			if (ArrayUtil.contains(groupIds, groupId)) {
+				unicodeProperties.setProperty(
+					"groupIds",
+					StringUtil.merge(ArrayUtil.remove(groupIds, groupId)));
+
+				_assetListEntrySegmentsEntryRelLocalService.
+					updateAssetListEntrySegmentsEntryRelTypeSettings(
+						assetListEntrySegmentsEntryRel.getAssetListEntryId(),
+						assetListEntrySegmentsEntryRel.getSegmentsEntryId(),
+						unicodeProperties.toString());
 			}
-		}
-		catch (Exception exception) {
-			throw new ModelListenerException(exception);
 		}
 	}
 
