7c06a0f0e2921990ffe95a9edff37656207b3370
ZakiMiklos <miklos.zakanyi@liferay.com>
2025-06-12 15:24:08 +0200

    LPD-57344 add manual assetList creation and update with api

--- modules/test/playwright/helpers/json-web-services/JSONWebServicesAssetListEntryApiHelper.ts
+++ modules/test/playwright/helpers/json-web-services/JSONWebServicesAssetListEntryApiHelper.ts
@@ -42,34 +42,49 @@ export class JSONWebServicesAssetListEntryApiHelper {
 		);
 	}
 
+	async addManualAssetListEntry({
+		groupId,
+		title,
+		type = '1',
+	}: {
+		groupId: string;
+		title: string;
+		type?: string;
+	}): Promise<AssetListEntry> {
+		const urlSearchParams = new URLSearchParams();
+
+		urlSearchParams.append('externalReferenceCode', '');
+		urlSearchParams.append('groupId', groupId);
+		urlSearchParams.append('title', title);
+		urlSearchParams.append('type', type);
+
+		return await this.apiHelpers.post(
+			`${liferayConfig.environment.baseUrl}${this.basePath}/add-asset-list-entry`,
+			{
+				data: urlSearchParams.toString(),
+				failOnStatusCode: true,
+				headers: await this.apiHelpers.getJSONWebServicesHeaders(),
+			}
+		);
+	}
+
 	async updateAssetListEntry({
 		assetListEntryId,
-		groupId,
-		segmentsEntryId,
-		typeSettings = '',
+		segmentsEntryId = '0',
+		typeSettings,
 	}: {
 		assetListEntryId: string;
-		groupId: string;
-		segmentsEntryId: string;
-		typeSettings?: string;
+		typeSettings: string;
+		segmentsEntryId?: string;
 	}): Promise<AssetListEntry> {
-		const user =
-			await this.apiHelpers.headlessAdminUser.getUserAccountByEmailAddress(
-				'test@liferay.com'
-			);
-
 		const urlSearchParams = new URLSearchParams();
 
 		urlSearchParams.append('assetListEntryId', assetListEntryId);
 		urlSearchParams.append('segmentsEntryId', segmentsEntryId);
 		urlSearchParams.append('typeSettings', typeSettings);
-		urlSearchParams.append(
-			'serviceContext',
-			JSON.stringify({scopeGroupId: groupId, userId: user.userId})
-		);
 
 		return await this.apiHelpers.post(
-			`${liferayConfig.environment.baseUrl}${this.basePath}/update-asset-list-entry`,
+			`${liferayConfig.environment.baseUrl}${this.basePath}/update-asset-list-entry-type-settings`,
 			{
 				data: urlSearchParams.toString(),
 				failOnStatusCode: true,
