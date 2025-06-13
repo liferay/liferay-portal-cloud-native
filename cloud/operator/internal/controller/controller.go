f779464e8967579662ba758ffac472ad16e48daf
jaredgorski <jared.gorski@liferay.com>
2025-06-13 09:59:35 -0400

    LCD-46861 Ordering

--- cloud/operator/internal/controller/controller.go
+++ cloud/operator/internal/controller/controller.go
@@ -9,21 +9,34 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-type Reconciler struct {
-	client.Client
-}
-
-func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).For(&corev1.ConfigMap{}).Named("Controller").Complete(r)
-}
-
-func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+func (r *Reconciler) Reconcile(
+	ctx context.Context,
+	req ctrl.Request,
+) (ctrl.Result, error) {
 	cm := &corev1.ConfigMap{}
+
 	if err := r.Get(ctx, req.NamespacedName, cm); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	logf.FromContext(ctx).Info("Hello, world! ConfigMap reconciled.", "name", cm.Name)
+	log := logf.FromContext(ctx)
+	log.Info("Hello, world! ConfigMap reconciled.", "name", cm.Name)
 
 	return ctrl.Result{}, nil
+}
+
+func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(
+		mgr,
+	).For(
+		&corev1.ConfigMap{},
+	).Named(
+		"Controller",
+	).Complete(
+		r,
+	)
+}
+
+type Reconciler struct {
+	client.Client
 }
