92c8c5b541436e0b8fe5b8b460f2b27cd4e091c9
Mikel Lorza <mikel.lorza@liferay.com>
2025-06-20 09:45:28 +0200

    LPD-57841 Rename test method

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/service/test/DDMFieldLocalServiceTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/service/test/DDMFieldLocalServiceTest.java
@@ -325,7 +325,7 @@ public class DDMFieldLocalServiceTest {
 	}
 
 	@Test
-	public void testUpdateDDMFormValuesWithLegacyField() throws Exception {
+	public void testUpdateDDMFormValuesWithLegacyDDMFormField() throws Exception {
 		DDMForm ddmForm = DDMFormTestUtil.createDDMForm("field");
 
 		DDMStructure ddmStructure = _ddmStructureTestHelper.addStructure(
