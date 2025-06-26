e341e0ffd87aa8df16ad0c036840b8764d00f5ae
shuyangzhou <shuyang.zhou@liferay.com>
2025-06-25 18:46:48 -0700

    LPD-59218 Move portal-impl com.liferay.portal.configuration to portal-kernel com.liferay.portal.kernel.internal.configuration

--- portal-kernel/src/com/liferay/portal/kernel/internal/configuration/ClassLoaderAggregateProperties.java
+++ portal-kernel/src/com/liferay/portal/kernel/internal/configuration/ClassLoaderAggregateProperties.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.configuration;
+package com.liferay.portal.kernel.internal.configuration;
 
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.CharPool;
@@ -60,10 +60,6 @@ public class ClassLoaderAggregateProperties extends CompositeConfiguration {
 		setThrowExceptionOnMissing(false);
 
 		_addBaseFileName(componentName.concat(".properties"));
-	}
-
-	public CompositeConfiguration getBaseConfiguration() {
-		return _baseCompositeConfiguration;
 	}
 
 	public Properties getProperties() {
