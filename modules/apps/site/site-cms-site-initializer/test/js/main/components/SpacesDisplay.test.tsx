3df63bc1b8a8d79ead348117ca6e6bb7887d15a4
Bruno Carvalho <bruno.carvalho@liferay.com>
2025-07-01 10:14:15 -0300

    LPD-57575 SF

--- modules/apps/site/site-cms-site-initializer/test/js/main/components/SpacesDisplay.test.tsx
+++ modules/apps/site/site-cms-site-initializer/test/js/main/components/SpacesDisplay.test.tsx
@@ -7,8 +7,8 @@ import '@testing-library/jest-dom/extend-expect';
 import {render, screen} from '@testing-library/react';
 import React from 'react';
 
+import {Space} from '../../../../src/main/resources/META-INF/resources/js/common/types/Space';
 import SpacesDisplay from '../../../../src/main/resources/META-INF/resources/js/main/components/SpacesDisplay';
-import {Space} from '../../../../src/main/resources/META-INF/resources/js/types/Space';
 
 const mockLiferayLanguageGet = jest.fn((key: string) => {
 	if (key === 'available-in-spaces-x') {
