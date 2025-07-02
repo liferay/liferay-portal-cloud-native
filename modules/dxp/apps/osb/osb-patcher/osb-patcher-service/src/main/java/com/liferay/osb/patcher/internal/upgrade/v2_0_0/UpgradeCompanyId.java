liferay/liferay-portal

LPD-57803 Upgrade - Adds new upgrade process:
- Adds companyId to OSB_PatcherAccounts_PatcherBuilds, OSB_PatcherBuilds_PatcherFixes, OSB_PatcherBuildRel, OSB_PatcherFixRel and OSB_PatcherFixes_PatcherFixPacks tables
- Rename portletId from 1_WAR_osbpatcherportlet to com_liferay_osb_patcher_web_internal_portlet_PatcherPortlet
- Rename tables removing OSB_ prefix
- Replace osbpatcher_WAR_osbpatchertheme theme with classic_WAR_classictheme
- Removes PatcherFixPack with patcherBuildId 0, to avoid WARN message with PatcherFixPack.IX_F3A916A7

modules/dxp/apps/osb/osb-patcher/osb-patcher-service/src/main/java/com/liferay/osb/patcher/internal/upgrade/v2_0_0/UpgradeCompanyId.java
@@ -1,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.patcher.internal.upgrade.v2_0_0;
+
+import com.liferay.portal.kernel.upgrade.BaseCompanyIdUpgradeProcess;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class UpgradeCompanyId extends BaseCompanyIdUpgradeProcess {
+
+	@Override
+	protected TableUpdater[] getTableUpdaters() {
+		return new TableUpdater[] {
+			new TableUpdater(
+				"PatcherAccounts_PatcherBuilds", "PatcherAccount",
+				"patcherAccountId"),
+			new TableUpdater(
+				"PatcherBuilds_PatcherFixes", "PatcherBuild", "patcherBuildId"),
+			new TableUpdater("PatcherBuildRel", "User_", "userId"),
+			new TableUpdater("PatcherFixRel", "User_", "userId"),
+			new TableUpdater(
+				"PatcherFixes_PatcherFixPacks", "PatcherFix", "patcherFixId")
+		};
+	}
+
+}

liferay/liferay-portal-cloud-native

modules/dxp/apps/osb/osb-patcher/osb-patcher-service/src/main/java/com/liferay/osb/patcher/internal/upgrade/v2_0_0/UpgradeCompanyId.java
@@ -1,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.patcher.internal.upgrade.v2_0_0;
+
+import com.liferay.portal.kernel.upgrade.BaseCompanyIdUpgradeProcess;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class UpgradeCompanyId extends BaseCompanyIdUpgradeProcess {
+
+	@Override
+	protected TableUpdater[] getTableUpdaters() {
+		return new TableUpdater[] {
+			new TableUpdater(
+				"OSBPatcher_PAccounts_PBuilds", "PatcherAccount",
+				"patcherAccountId"),
+			new TableUpdater(
+				"OSBPatcher_PBuilds_PFixes", "PatcherBuild", "patcherBuildId"),
+			new TableUpdater("OSBPatcher_PatcherBuildRel", "User_", "userId"),
+			new TableUpdater("OSBPatcher_PatcherFixRel", "User_", "userId"),
+			new TableUpdater(
+				"OSBPatcher_PFixes_PFixPacks", "PatcherFix", "patcherFixId")
+		};
+	}
+
+}