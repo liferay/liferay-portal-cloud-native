ae59553e46d34e0a0a53ecfbdc968d18d2ad2793
Brian Chan <brian.chan@liferay.com>
2025-07-03 12:20:55 -0300

    LPD-56540 Usual pattern

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPConfigurationEntryLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPConfigurationEntryLocalServiceImpl.java
@@ -70,6 +70,8 @@ public class CPConfigurationEntryLocalServiceImpl
 			double weight, double width)
 		throws PortalException {
 
+		_validateAllowedOrderQuantities(allowedOrderQuantities);
+
 		User user = _userLocalService.getUser(userId);
 
 		CPConfigurationEntry cpConfigurationEntry =
@@ -85,9 +87,6 @@ public class CPConfigurationEntryLocalServiceImpl
 		cpConfigurationEntry.setClassPK(classPK);
 		cpConfigurationEntry.setCPConfigurationListId(cpConfigurationListId);
 		cpConfigurationEntry.setCPTaxCategoryId(cpTaxCategoryId);
-
-		_validateAllowedOrderQuantities(allowedOrderQuantities);
-
 		cpConfigurationEntry.setAllowedOrderQuantities(allowedOrderQuantities);
 		cpConfigurationEntry.setBackOrders(backOrders);
 		cpConfigurationEntry.setCommerceAvailabilityEstimateId(
@@ -354,15 +353,14 @@ public class CPConfigurationEntryLocalServiceImpl
 			double weight, double width)
 		throws PortalException {
 
+		_validateAllowedOrderQuantities(allowedOrderQuantities);
+
 		CPConfigurationEntry cpConfigurationEntry =
 			cpConfigurationEntryPersistence.findByPrimaryKey(
 				cpConfigurationEntryId);
 
 		cpConfigurationEntry.setExternalReferenceCode(externalReferenceCode);
 		cpConfigurationEntry.setCPTaxCategoryId(cpTaxCategoryId);
-
-		_validateAllowedOrderQuantities(allowedOrderQuantities);
-
 		cpConfigurationEntry.setAllowedOrderQuantities(allowedOrderQuantities);
 		cpConfigurationEntry.setBackOrders(backOrders);
 		cpConfigurationEntry.setCommerceAvailabilityEstimateId(
