32475732d89e8253b1d9748e50bc0039e99a817b
Joshua Cords <joshua.cords@liferay.com>
2025-07-21 15:42:34 -0700

    LPD-60525 search-experiences-api: bump schemaVersion
    https://liferay.atlassian.net/browse/LPD-60525

--- modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/constants/SXPConstants.java
+++ modules/dxp/apps/search-experiences/search-experiences-api/src/main/java/com/liferay/search/experiences/constants/SXPConstants.java
@@ -12,6 +12,6 @@ public class SXPConstants {
 
 	public static final String RESOURCE_NAME = "com.liferay.search.experiences";
 
-	public static final String SXP_BLUEPRINT_SCHEMA_VERSION = "1.1";
+	public static final String SXP_BLUEPRINT_SCHEMA_VERSION = "1.2";
 
 }
