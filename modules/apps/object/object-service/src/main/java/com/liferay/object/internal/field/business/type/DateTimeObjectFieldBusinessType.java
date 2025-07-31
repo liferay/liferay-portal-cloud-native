liferay-bpm/liferay-portal

LPD-61446 Normalize Date metadata display to support DateTime

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/DateTimeObjectFieldBusinessType.java
@@ -68,6 +68,11 @@ public class DateTimeObjectFieldBusinessType
 	}
 
 	@Override
+	public String getDDMFormFieldTypeName(ObjectField objectField) {
+		return DDMFormFieldTypeConstants.DATE_TIME;
+	}
+
+	@Override
 	public String getDescription(Locale locale) {
 		return _language.get(locale, "add-date-and-time-values");
 	}

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImpl.java
@@ -1084,13 +1084,11 @@ public class ObjectEntryDisplayContextImpl
 		// TODO Store the type and the object field type in the database
 
 		ObjectFieldBusinessType objectFieldBusinessType =
-			_objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
-				objectField.getBusinessType());
+			_getEffectiveObjectFieldBusinessType(objectField);
 
 		DDMFormField ddmFormField = new DDMFormField(
 			objectField.getName(),
-			objectFieldBusinessType.getDDMFormFieldTypeName(
-				objectField.isLocalized()));
+			objectFieldBusinessType.getDDMFormFieldTypeName(objectField));
 
 		if (!readOnly) {
 			readOnly = ObjectFieldUtil.isReadOnly(
@@ -1285,8 +1283,7 @@ public class ObjectEntryDisplayContextImpl
 		throws PortalException {
 
 		ObjectFieldBusinessType objectFieldBusinessType =
-			_objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
-				objectField.getBusinessType());
+			_getEffectiveObjectFieldBusinessType(objectField);
 
 		return objectFieldBusinessType.getDisplayContextValue(
 			objectField, _objectRequestHelper.getUserId(), values);
@@ -1297,6 +1294,22 @@ public class ObjectEntryDisplayContextImpl
 			false, null, null, _objectRequestHelper.getRequest(), null,
 			_themeDisplay.getSiteDefaultLocale(), null,
 			_themeDisplay.getUser());
+	}
+
+	private ObjectFieldBusinessType _getEffectiveObjectFieldBusinessType(
+		ObjectField objectField) {
+
+		if (Objects.equals(
+				objectField.getBusinessType(),
+				ObjectFieldConstants.BUSINESS_TYPE_DATE) &&
+			objectField.isMetadata()) {
+
+			return _objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
+				ObjectFieldConstants.BUSINESS_TYPE_DATE_TIME);
+		}
+
+		return _objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
+			objectField.getBusinessType());
 	}
 
 	private long _getGroupId() {

liferay/liferay-portal-cloud-native

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/DateTimeObjectFieldBusinessType.java
@@ -68,6 +68,11 @@ public class DateTimeObjectFieldBusinessType
 	}
 
 	@Override
+	public String getDDMFormFieldTypeName(boolean localized) {
+		return DDMFormFieldTypeConstants.DATE_TIME;
+	}
+
+	@Override
 	public String getDescription(Locale locale) {
 		return _language.get(locale, "add-date-and-time-values");
 	}

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContextImpl.java
@@ -1084,8 +1084,7 @@ public class ObjectEntryDisplayContextImpl
 		// TODO Store the type and the object field type in the database
 
 		ObjectFieldBusinessType objectFieldBusinessType =
-			_objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
-				objectField.getBusinessType());
+			_getObjectFieldBusinessType(objectField);
 
 		DDMFormField ddmFormField = new DDMFormField(
 			objectField.getName(),
@@ -1285,8 +1284,7 @@ public class ObjectEntryDisplayContextImpl
 		throws PortalException {
 
 		ObjectFieldBusinessType objectFieldBusinessType =
-			_objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
-				objectField.getBusinessType());
+			_getObjectFieldBusinessType(objectField);
 
 		return objectFieldBusinessType.getDisplayContextValue(
 			objectField, _objectRequestHelper.getUserId(), values);
@@ -1371,6 +1369,22 @@ public class ObjectEntryDisplayContextImpl
 		}
 
 		return _objectEntry;
+	}
+
+	private ObjectFieldBusinessType _getObjectFieldBusinessType(
+		ObjectField objectField) {
+
+		if (Objects.equals(
+				objectField.getBusinessType(),
+				ObjectFieldConstants.BUSINESS_TYPE_DATE) &&
+			objectField.isMetadata()) {
+
+			return _objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
+				ObjectFieldConstants.BUSINESS_TYPE_DATE_TIME);
+		}
+
+		return _objectFieldBusinessTypeRegistry.getObjectFieldBusinessType(
+			objectField.getBusinessType());
 	}
 
 	private Object _getValue(