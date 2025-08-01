43e35aae5b9b42e5f2c628ddbe4188c2326c6313
Bryan Engler <bryan.engler@liferay.com>
2025-08-01 15:40:54 -0700

    LPD-50745 portal-search-elasticsearch7-impl: Move translator to legacy package and refactor test
    https://liferay.atlassian.net/browse/LPD-50745

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/legacy/sort/SortTranslator.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/legacy/sort/SortTranslator.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.search.elasticsearch7.internal.sort;
+package com.liferay.portal.search.elasticsearch7.internal.legacy.sort;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.search.Field;
