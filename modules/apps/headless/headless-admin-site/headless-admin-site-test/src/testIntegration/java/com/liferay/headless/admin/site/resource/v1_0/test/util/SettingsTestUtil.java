52f938787e6b31af80cda3102d64e9e895d1ca80
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-08-04 23:23:11 +0200

    LPD-55438 Create a ClientExtensionEntry that will be deleted by the data guard instead of register a service

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/SettingsTestUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/SettingsTestUtil.java
@@ -7,15 +7,15 @@ package com.liferay.headless.admin.site.resource.v1_0.test.util;
 
 import com.liferay.client.extension.constants.ClientExtensionEntryConstants;
 import com.liferay.client.extension.model.ClientExtensionEntryRel;
+import com.liferay.client.extension.service.ClientExtensionEntryLocalServiceUtil;
 import com.liferay.client.extension.service.ClientExtensionEntryRelLocalServiceUtil;
-import com.liferay.client.extension.type.configuration.CETConfiguration;
 import com.liferay.client.extension.type.manager.CETManager;
 import com.liferay.headless.admin.site.client.dto.v1_0.ClientExtension;
 import com.liferay.headless.admin.site.client.dto.v1_0.ItemExternalReference;
 import com.liferay.headless.admin.site.client.dto.v1_0.Settings;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalServiceUtil;
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.TreeMapBuilder;
@@ -33,6 +34,7 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.style.book.model.StyleBookEntry;
 import com.liferay.style.book.service.StyleBookEntryLocalServiceUtil;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -470,18 +472,16 @@ public class SettingsTestUtil {
 			}
 		};
 
-		cetManager.addCET(
-			ConfigurableUtil.createConfigurable(
-				CETConfiguration.class,
-				HashMapBuilder.<String, Object>put(
-					"baseURL", RandomTestUtil.randomString()
-				).put(
-					"name", RandomTestUtil.randomString()
-				).put(
-					"type", "customElement"
-				).build()),
-			serviceContext.getCompanyId(),
-			clientExtension.getExternalReferenceCode());
+		ClientExtensionEntryLocalServiceUtil.addClientExtensionEntry(
+			clientExtension.getExternalReferenceCode(),
+			TestPropsValues.getUserId(), StringPool.BLANK,
+			Collections.singletonMap(
+				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
+			StringPool.BLANK, StringPool.BLANK,
+			ClientExtensionEntryConstants.TYPE_THEME_CSS,
+			UnicodePropertiesBuilder.create(
+				clientExtension.getClientExtensionConfig(), true
+			).buildString());
 
 		return clientExtension;
 	}
