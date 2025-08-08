50e556eaeb34d6a7780bad6202990f2eaa31b5a4
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2025-08-07 17:08:37 -0700

    LPD-56437 | LPD-62035 buildREST

--- modules/apps/analytics/analytics-cms-rest-impl/src/main/java/com/liferay/analytics/cms/rest/internal/resource/v1_0/OpenAPIResourceImpl.java
+++ modules/apps/analytics/analytics-cms-rest-impl/src/main/java/com/liferay/analytics/cms/rest/internal/resource/v1_0/OpenAPIResourceImpl.java
@@ -85,6 +85,8 @@ public class OpenAPIResourceImpl {
 
 	private final Set<Class<?>> _resourceClasses = new HashSet<Class<?>>() {
 		{
+			add(ChannelResourceImpl.class);
+
 			add(ConnectionInfoResourceImpl.class);
 
 			add(InventoryAnalysisResourceImpl.class);
