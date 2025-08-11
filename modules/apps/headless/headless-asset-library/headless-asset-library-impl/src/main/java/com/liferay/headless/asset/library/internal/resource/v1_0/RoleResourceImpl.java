92ea6f5034779c222f0ec3df1a65631429eea88d
Jürgen Kappler <jurgen.kappler@liferay.com>
2025-08-11 09:46:57 +0200

    LPD-62030 Implements get methods

--- modules/apps/headless/headless-asset-library/headless-asset-library-impl/src/main/java/com/liferay/headless/asset/library/internal/resource/v1_0/RoleResourceImpl.java
+++ modules/apps/headless/headless-asset-library/headless-asset-library-impl/src/main/java/com/liferay/headless/asset/library/internal/resource/v1_0/RoleResourceImpl.java
@@ -5,9 +5,21 @@
 
 package com.liferay.headless.asset.library.internal.resource.v1_0;
 
+import com.liferay.headless.asset.library.dto.v1_0.Role;
 import com.liferay.headless.asset.library.resource.v1_0.RoleResource;
+import com.liferay.portal.kernel.exception.NoSuchGroupException;
+import com.liferay.portal.kernel.exception.NoSuchUserException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.GroupService;
+import com.liferay.portal.kernel.service.RoleService;
+import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.vulcan.pagination.Page;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +30,75 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = RoleResource.class
 )
 public class RoleResourceImpl extends BaseRoleResourceImpl {
+
+	@Override
+	public Page<Role>
+			getAssetLibraryByExternalReferenceCodeAssetLibraryExternalReferenceCodeUserAccountByExternalReferenceCodeUserExternalReferenceCodeRolesPage(
+				String assetLibraryExternalReferenceCode,
+				String userExternalReferenceCode)
+		throws Exception {
+
+		Group group = _getGroup(assetLibraryExternalReferenceCode);
+		User user = _userService.getUserByExternalReferenceCode(
+			userExternalReferenceCode, contextCompany.getCompanyId());
+
+		return getAssetLibraryUserAccountUserRolesPage(
+			group.getGroupId(), user.getUserId());
+	}
+
+	@Override
+	public Page<Role> getAssetLibraryUserAccountUserRolesPage(
+			Long assetLibraryId, Long userId)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-17564")) {
+			throw new UnsupportedOperationException();
+		}
+
+		if (!_groupService.hasUserGroup(userId, assetLibraryId)) {
+			throw new NoSuchUserException(
+				"No user exists with user group ID " + userId);
+		}
+
+		return Page.of(
+			transform(
+				_roleService.getUserGroupRoles(userId, assetLibraryId),
+				this::_toRole));
+	}
+
+	private Group _getGroup(String externalReferenceCode) throws Exception {
+		Group group = _groupService.fetchGroupByExternalReferenceCode(
+			externalReferenceCode, contextCompany.getCompanyId());
+
+		if (group == null) {
+			throw new NoSuchGroupException(
+				"No group exists with external reference code " +
+					externalReferenceCode);
+		}
+
+		return group;
+	}
+
+	private Role _toRole(com.liferay.portal.kernel.model.Role role)
+		throws PortalException {
+
+		return new Role() {
+			{
+				setExternalReferenceCode(role::getExternalReferenceCode);
+				setId(role::getRoleId);
+				setName(role::getName);
+				setRoleType(role::getType);
+			}
+		};
+	}
+
+	@Reference
+	private GroupService _groupService;
+
+	@Reference
+	private RoleService _roleService;
+
+	@Reference
+	private UserService _userService;
+
 }
