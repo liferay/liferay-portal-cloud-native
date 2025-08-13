74e6f6d42f5e774df2525050a756a894cfa88e57
Carolina Barbosa <carolina.barbosa@liferay.com>
2025-08-13 15:48:01 -0300

    LPD-62587 [Service Layer] If the object entry folder is deleted or is in trash, the entry is restored to the parent object entry folder

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -1785,6 +1785,36 @@ public class ObjectEntryLocalServiceImpl
 				RestoreEntryException.INVALID_STATUS);
 		}
 
+		if (objectEntry.getObjectEntryFolderId() !=
+				ObjectEntryFolderConstants.
+					PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT) {
+
+			ObjectEntryFolder objectEntryFolder =
+				_objectEntryFolderPersistence.fetchByPrimaryKey(
+					objectEntry.getObjectEntryFolderId());
+
+			while ((objectEntryFolder != null) &&
+				   (objectEntryFolder.getStatus() ==
+					   WorkflowConstants.STATUS_IN_TRASH)) {
+
+				objectEntryFolder =
+					_objectEntryFolderPersistence.fetchByPrimaryKey(
+						objectEntryFolder.getParentObjectEntryFolderId());
+			}
+
+			if (objectEntryFolder == null) {
+				objectEntry.setObjectEntryFolderId(
+					ObjectEntryFolderConstants.
+						PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT);
+			}
+			else {
+				objectEntry.setObjectEntryFolderId(
+					objectEntryFolder.getObjectEntryFolderId());
+			}
+
+			objectEntry = objectEntryPersistence.update(objectEntry);
+		}
+
 		ObjectDefinition objectDefinition =
 			_objectDefinitionPersistence.findByPrimaryKey(
 				objectEntry.getObjectDefinitionId());
