e145371171c2d0ee03d8745e69de877a9d2856ad
Attila Bakay <attila.bakay@liferay.com>
2025-08-26 13:01:07 +0200

    LPD-63819 Only display Item selector with group permissions

--- modules/apps/item-selector/item-selector-web/src/main/java/com/liferay/item/selector/web/internal/portlet/ItemSelectorPortlet.java
+++ modules/apps/item-selector/item-selector-web/src/main/java/com/liferay/item/selector/web/internal/portlet/ItemSelectorPortlet.java
@@ -8,9 +8,13 @@ package com.liferay.item.selector.web.internal.portlet;
 import com.liferay.item.selector.ItemSelector;
 import com.liferay.item.selector.ItemSelectorRendering;
 import com.liferay.item.selector.constants.ItemSelectorPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Release;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -69,7 +73,25 @@ public class ItemSelectorPortlet extends MVCPortlet {
 
 		localizedItemSelectorRendering.store(renderRequest);
 
-		super.render(renderRequest, renderResponse);
+		try {
+			PermissionChecker permissionChecker =
+				themeDisplay.getPermissionChecker();
+
+			if (permissionChecker.isGroupAdmin(
+					themeDisplay.getScopeGroupId()) ||
+				GroupPermissionUtil.contains(
+					permissionChecker, themeDisplay.getScopeGroup(),
+					ActionKeys.VIEW)) {
+
+				super.render(renderRequest, renderResponse);
+			}
+			else {
+				include("/error.jsp", renderRequest, renderResponse);
+			}
+		}
+		catch (PortalException portalException) {
+			throw new PortletException(portalException);
+		}
 	}
 
 	@Reference
