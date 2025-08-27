liferay-site-management/liferay-portal

LPD-62032 Update the external reference code of product navigation menu items when the CPDefinition external reference code related to the menu item changes

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/model/listener/CProductModelListener.java
@@ -5,10 +5,18 @@
 
 package com.liferay.commerce.internal.model.listener;
 
+import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CProduct;
 import com.liferay.commerce.service.CPDAvailabilityEstimateLocalService;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalService;
+
+import java.util.List;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -20,13 +28,63 @@ import org.osgi.service.component.annotations.Reference;
 public class CProductModelListener extends BaseModelListener<CProduct> {
 
 	@Override
+	public void onAfterUpdate(CProduct originalCProduct, CProduct cProduct) {
+		if (!Objects.equals(
+				originalCProduct.getExternalReferenceCode(),
+				cProduct.getExternalReferenceCode())) {
+
+			_updateSiteNavigationMenuItem(
+				originalCProduct.getExternalReferenceCode(),
+				cProduct.getExternalReferenceCode());
+		}
+	}
+
+	@Override
 	public void onBeforeRemove(CProduct cProduct) {
 		_cpdAvailabilityEstimateLocalService.
 			deleteCPDAvailabilityEstimateByCProductId(cProduct.getCProductId());
 	}
 
+	private void _updateSiteNavigationMenuItem(
+		String originalExternalReferenceCode, String newExternalReferenceCode) {
+
+		List<SiteNavigationMenuItem> siteNavigationMenuItems =
+			_siteNavigationMenuItemLocalService.getSiteNavigationMenuItems(
+				CPDefinition.class.getName());
+
+		for (SiteNavigationMenuItem siteNavigationMenuItem :
+				siteNavigationMenuItems) {
+
+			UnicodeProperties unicodeProperties =
+				UnicodePropertiesBuilder.fastLoad(
+					siteNavigationMenuItem.getTypeSettings()
+				).build();
+
+			String productNavigationMenuItemExternalReferenceCode =
+				unicodeProperties.getProperty("externalReferenceCode");
+
+			if (Objects.equals(
+					originalExternalReferenceCode,
+					productNavigationMenuItemExternalReferenceCode)) {
+
+				unicodeProperties.setProperty(
+					"externalReferenceCode", newExternalReferenceCode);
+
+				siteNavigationMenuItem.setTypeSettings(
+					unicodeProperties.toString());
+
+				_siteNavigationMenuItemLocalService.
+					updateSiteNavigationMenuItem(siteNavigationMenuItem);
+			}
+		}
+	}
+
 	@Reference
 	private CPDAvailabilityEstimateLocalService
 		_cpdAvailabilityEstimateLocalService;
+
+	@Reference
+	private SiteNavigationMenuItemLocalService
+		_siteNavigationMenuItemLocalService;
 
 }

liferay/liferay-portal-cloud-native

modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/model/listener/CProductModelListener.java
@@ -5,10 +5,18 @@
 
 package com.liferay.commerce.internal.model.listener;
 
+import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CProduct;
 import com.liferay.commerce.service.CPDAvailabilityEstimateLocalService;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalService;
+
+import java.util.List;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -20,13 +28,65 @@ import org.osgi.service.component.annotations.Reference;
 public class CProductModelListener extends BaseModelListener<CProduct> {
 
 	@Override
+	public void onAfterUpdate(CProduct originalCProduct, CProduct cProduct) {
+		if (Objects.equals(
+				originalCProduct.getExternalReferenceCode(),
+				cProduct.getExternalReferenceCode())) {
+
+			return;
+		}
+
+		_updateSiteNavigationMenuItem(
+			cProduct.getExternalReferenceCode(),
+			originalCProduct.getExternalReferenceCode());
+	}
+
+	@Override
 	public void onBeforeRemove(CProduct cProduct) {
 		_cpdAvailabilityEstimateLocalService.
 			deleteCPDAvailabilityEstimateByCProductId(cProduct.getCProductId());
 	}
 
+	private void _updateSiteNavigationMenuItem(
+		String newExternalReferenceCode, String originalExternalReferenceCode) {
+
+		List<SiteNavigationMenuItem> siteNavigationMenuItems =
+			_siteNavigationMenuItemLocalService.getSiteNavigationMenuItems(
+				CPDefinition.class.getName());
+
+		for (SiteNavigationMenuItem siteNavigationMenuItem :
+				siteNavigationMenuItems) {
+
+			UnicodeProperties unicodeProperties =
+				UnicodePropertiesBuilder.fastLoad(
+					siteNavigationMenuItem.getTypeSettings()
+				).build();
+
+			String productNavigationMenuItemExternalReferenceCode =
+				unicodeProperties.getProperty("externalReferenceCode");
+
+			if (Objects.equals(
+					originalExternalReferenceCode,
+					productNavigationMenuItemExternalReferenceCode)) {
+
+				unicodeProperties.setProperty(
+					"externalReferenceCode", newExternalReferenceCode);
+
+				siteNavigationMenuItem.setTypeSettings(
+					unicodeProperties.toString());
+
+				_siteNavigationMenuItemLocalService.
+					updateSiteNavigationMenuItem(siteNavigationMenuItem);
+			}
+		}
+	}
+
 	@Reference
 	private CPDAvailabilityEstimateLocalService
 		_cpdAvailabilityEstimateLocalService;
+
+	@Reference
+	private SiteNavigationMenuItemLocalService
+		_siteNavigationMenuItemLocalService;
 
 }