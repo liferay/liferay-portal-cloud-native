liferay-commerce/liferay-portal

LPD-60079 Adds to whitelist

modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
@@ -126,6 +126,10 @@ public class ObjectDefinitionUtil {
 		).put(
 			"Bookmark", "/bookmarks"
 		).put(
+			"BulkActionTask", "/cms/bulk-action-task"
+		).put(
+			"BulkActionTaskItem", "/cms/bulk-action-task-item"
+		).put(
 			"CommerceReturn", "/commerce/returns"
 		).put(
 			"CommerceReturnItem", "/commerce/return-items"

liferay/liferay-portal-cloud-native

modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
@@ -126,6 +126,10 @@ public class ObjectDefinitionUtil {
 		).put(
 			"Bookmark", "/bookmarks"
 		).put(
+			"BulkActionTask", "/cms/bulk-action-tasks"
+		).put(
+			"BulkActionTaskItem", "/cms/bulk-action-task-items"
+		).put(
 			"CommerceReturn", "/commerce/returns"
 		).put(
 			"CommerceReturnItem", "/commerce/return-items"