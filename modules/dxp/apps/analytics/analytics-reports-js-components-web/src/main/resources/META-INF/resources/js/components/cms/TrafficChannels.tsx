419db2169802fd2b8579df1890eb949813c76e9a
Tiago Campêlo <tiago.campelo@liferay.com>
2025-09-02 15:46:12 -0300

    LPD-62039 Fix name

--- modules/dxp/apps/analytics/analytics-reports-js-components-web/src/main/resources/META-INF/resources/js/components/cms/TrafficChannels.tsx
+++ modules/dxp/apps/analytics/analytics-reports-js-components-web/src/main/resources/META-INF/resources/js/components/cms/TrafficChannels.tsx
@@ -29,7 +29,7 @@ type TrafficChannelsApiResponse = {
 	totalCount: number;
 };
 
-const TrafficChannelsNames: Record<string, string> = {
+const trafficChannelsNames: Record<string, string> = {
 	'affiliates': Liferay.Language.get('affiliates'),
 	'direct': Liferay.Language.get('direct'),
 	'display': Liferay.Language.get('display'),
@@ -65,19 +65,19 @@ const TrafficChannelsEntry = ({
 }) => {
 	return (
 		<div
-			aria-label={`${Liferay.Language.get('traffic-channel')}: ${Liferay.Language.get(TrafficChannelsNames['name'])}`}
+			aria-label={`${Liferay.Language.get('traffic-channel')}: ${Liferay.Language.get(trafficChannelsNames[name])}`}
 			className="d-flex flex-row py-3 tab-focus traffic-channel-item"
 			role="row"
 			tabIndex={0}
 		>
 			<div
-				aria-label={Liferay.Language.get(TrafficChannelsNames[name])}
+				aria-label={Liferay.Language.get(trafficChannelsNames[name])}
 				className="tab-focus traffic-channel-item__name"
 				role="cell"
 				style={{width: '35%'}}
 			>
 				<Text size={3} weight="semi-bold">
-					{Liferay.Language.get(TrafficChannelsNames[name])}
+					{Liferay.Language.get(trafficChannelsNames[name])}
 				</Text>
 			</div>
 
