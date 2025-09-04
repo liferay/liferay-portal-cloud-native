123ab8c0deb093e5867fe48fa23864751b1938d5
Rubén Pulido <ruben.pulido@liferay.com>
2025-09-04 14:13:06 +0200

    LPD-59859 Set layoutTemplateId in typeSettings only if it has been specified

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SitePageResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SitePageResourceImpl.java
@@ -396,10 +396,13 @@ public class SitePageResourceImpl extends BaseSitePageResourceImpl {
 				LayoutConstants.CUSTOMIZABLE_LAYOUT,
 				String.valueOf(
 					GetterUtil.getBoolean(widgetPageSettings.getCustomizable()))
-			).setProperty(
-				LayoutTypePortletConstants.LAYOUT_TEMPLATE_ID,
-				widgetPageSettings.getLayoutTemplateId()
 			);
+
+		if (widgetPageSettings.getLayoutTemplateId() != null) {
+			unicodePropertiesWrapper.setProperty(
+				LayoutTypePortletConstants.LAYOUT_TEMPLATE_ID,
+				widgetPageSettings.getLayoutTemplateId());
+		}
 
 		String[] customizableSectionIds =
 			widgetPageSettings.getCustomizableSectionIds();
