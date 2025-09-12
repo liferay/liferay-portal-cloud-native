liferay-customer-solutions/liferay-portal

LRSD-10053 LRSD-8308 LRSD-8311 LRSD-8312 LRSD-8309  Refactor Zendesk business events to JSM with feature flag, new Jira pagination using nextPageToken, update/add heat tag logic and customer account structure

workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/AccountsTicketsRestController.java
@@ -7,15 +7,21 @@ package com.liferay.customer;
 
 import com.liferay.client.extension.util.spring.boot3.BaseRestController;
 import com.liferay.customer.constants.ExternalLinkConstants;
+import com.liferay.customer.constants.JiraIssueConstants;
+import com.liferay.customer.model.JiraIssue;
 import com.liferay.customer.permission.BusinessEventPermission;
+import com.liferay.customer.service.JiraService;
 import com.liferay.customer.service.KoroneikiService;
 import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.ExternalLink;
 import com.liferay.osb.spring.boot.client.zendesk.model.ZendeskTicket;
 import com.liferay.osb.spring.boot.client.zendesk.search.SearchHits;
 import com.liferay.osb.spring.boot.client.zendesk.search.ZendeskTicketQuery;
 import com.liferay.osb.spring.boot.client.zendesk.service.ZendeskService;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.List;
 
@@ -23,8 +29,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -32,6 +40,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -44,9 +53,88 @@ public class AccountsTicketsRestController extends BaseRestController {
 		method = RequestMethod.GET,
 		path = "/accounts/{externalReferenceCode}/tickets"
 	)
-	public ResponseEntity<String> getZendeskTickets(
+	public ResponseEntity<String> getTickets(
 			@AuthenticationPrincipal Jwt jwt,
-			@PathVariable("externalReferenceCode") String externalReferenceCode)
+			@PathVariable("externalReferenceCode") String externalReferenceCode,
+			@RequestParam(defaultValue = "", required = false) String[] tickets)
+		throws Exception {
+
+		if (_featureFlagLRSD_8280) {
+			return _getJSMTickets(jwt, externalReferenceCode, tickets);
+		}
+
+		return _getZendeskTickets(jwt, externalReferenceCode);
+	}
+
+	private long _fetchZendeskOrganizationId(String externalReferenceCode)
+		throws Exception {
+
+		List<ExternalLink> externalLinks = _koroneikiService.fetchExternalLinks(
+			externalReferenceCode, 1, 1000);
+
+		for (ExternalLink externalLink : externalLinks) {
+			String domain = externalLink.getDomain();
+			String entityName = externalLink.getEntityName();
+
+			if (domain.equals(ExternalLinkConstants.DOMAIN_ZENDESK) &&
+				entityName.equals(
+					ExternalLinkConstants.ENTITY_NAME_ZENDESK_ORGANIZATION)) {
+
+				return GetterUtil.getLong(externalLink.getEntityId());
+			}
+		}
+
+		return 0;
+	}
+
+	private ResponseEntity<String> _getJSMTickets(
+			Jwt jwt, String externalReferenceCode, String[] tickets)
+		throws Exception {
+
+		try {
+			_businessEventPermission.check(
+				jwt, externalReferenceCode, ActionKeys.VIEW);
+
+			StringBundler sb = new StringBundler(9);
+
+			sb.append("Organization in aqlFunction('\"External Key\" = ");
+			sb.append(externalReferenceCode);
+			sb.append("') and (status not in ('");
+			sb.append(
+				StringUtil.merge(
+					JiraIssueConstants.STATUSES_SOLVED_AND_CLOSED, "','"));
+			sb.append("')");
+
+			if (ArrayUtil.isNotEmpty(tickets)) {
+				sb.append(" or key in ('");
+				sb.append(StringUtil.merge(tickets, "','"));
+				sb.append("')");
+			}
+
+			sb.append(")");
+
+			List<JiraIssue> jiraIssues = _jiraService.search(
+				sb.toString(),
+				new String[] {"key", "labels", "status", "summary"});
+
+			JSONArray jsonArray = new JSONArray();
+
+			for (JiraIssue jiraIssue : jiraIssues) {
+				jsonArray.put(_toJSONObject(jiraIssue));
+			}
+
+			return new ResponseEntity<>(jsonArray.toString(), HttpStatus.OK);
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+
+			return new ResponseEntity(
+				exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private ResponseEntity<String> _getZendeskTickets(
+			Jwt jwt, String externalReferenceCode)
 		throws Exception {
 
 		try {
@@ -86,25 +174,17 @@ public class AccountsTicketsRestController extends BaseRestController {
 		}
 	}
 
-	private long _fetchZendeskOrganizationId(String externalReferenceCode)
-		throws Exception {
-
-		List<ExternalLink> externalLinks = _koroneikiService.fetchExternalLinks(
-			externalReferenceCode, 1, 1000);
-
-		for (ExternalLink externalLink : externalLinks) {
-			String domain = externalLink.getDomain();
-			String entityName = externalLink.getEntityName();
-
-			if (domain.equals(ExternalLinkConstants.DOMAIN_ZENDESK) &&
-				entityName.equals(
-					ExternalLinkConstants.ENTITY_NAME_ZENDESK_ORGANIZATION)) {
-
-				return GetterUtil.getLong(externalLink.getEntityId());
-			}
-		}
-
-		return 0;
+	private JSONObject _toJSONObject(JiraIssue jiraIssue) {
+		return new JSONObject(
+		).put(
+			"link", jiraIssue.getTicketURL()
+		).put(
+			"status", jiraIssue.getStatus()
+		).put(
+			"subject", jiraIssue.getSummary()
+		).put(
+			"ticketId", jiraIssue.getKey()
+		);
 	}
 
 	private static final Log _log = LogFactory.getLog(
@@ -112,6 +192,12 @@ public class AccountsTicketsRestController extends BaseRestController {
 
 	@Autowired
 	private BusinessEventPermission _businessEventPermission;
+
+	@Value("${liferay.customer.feature.flag.LRSD-8280}")
+	private boolean _featureFlagLRSD_8280;
+
+	@Autowired
+	private JiraService _jiraService;
 
 	@Autowired
 	private KoroneikiService _koroneikiService;

liferay/liferay-portal-cloud-native

workspaces/liferay-customer-workspace/client-extensions/liferay-customer-etc-spring-boot/src/main/java/com/liferay/customer/AccountsTicketsRestController.java
@@ -7,15 +7,21 @@ package com.liferay.customer;
 
 import com.liferay.client.extension.util.spring.boot3.BaseRestController;
 import com.liferay.customer.constants.ExternalLinkConstants;
+import com.liferay.customer.constants.JiraIssueConstants;
+import com.liferay.customer.model.JiraSupportIssue;
 import com.liferay.customer.permission.BusinessEventPermission;
+import com.liferay.customer.service.JiraService;
 import com.liferay.customer.service.KoroneikiService;
 import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.ExternalLink;
 import com.liferay.osb.spring.boot.client.zendesk.model.ZendeskTicket;
 import com.liferay.osb.spring.boot.client.zendesk.search.SearchHits;
 import com.liferay.osb.spring.boot.client.zendesk.search.ZendeskTicketQuery;
 import com.liferay.osb.spring.boot.client.zendesk.service.ZendeskService;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.List;
 
@@ -23,8 +29,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -32,6 +40,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -44,9 +53,89 @@ public class AccountsTicketsRestController extends BaseRestController {
 		method = RequestMethod.GET,
 		path = "/accounts/{externalReferenceCode}/tickets"
 	)
-	public ResponseEntity<String> getZendeskTickets(
+	public ResponseEntity<String> getTickets(
 			@AuthenticationPrincipal Jwt jwt,
-			@PathVariable("externalReferenceCode") String externalReferenceCode)
+			@PathVariable("externalReferenceCode") String externalReferenceCode,
+			@RequestParam(defaultValue = "", required = false) String[]
+				ticketIds)
+		throws Exception {
+
+		if (_featureFlagLRSD_8280) {
+			return _getJSMTickets(jwt, externalReferenceCode, ticketIds);
+		}
+
+		return _getZendeskTickets(jwt, externalReferenceCode);
+	}
+
+	private long _fetchZendeskOrganizationId(String externalReferenceCode)
+		throws Exception {
+
+		List<ExternalLink> externalLinks = _koroneikiService.fetchExternalLinks(
+			externalReferenceCode, 1, 1000);
+
+		for (ExternalLink externalLink : externalLinks) {
+			String domain = externalLink.getDomain();
+			String entityName = externalLink.getEntityName();
+
+			if (domain.equals(ExternalLinkConstants.DOMAIN_ZENDESK) &&
+				entityName.equals(
+					ExternalLinkConstants.ENTITY_NAME_ZENDESK_ORGANIZATION)) {
+
+				return GetterUtil.getLong(externalLink.getEntityId());
+			}
+		}
+
+		return 0;
+	}
+
+	private ResponseEntity<String> _getJSMTickets(
+			Jwt jwt, String externalReferenceCode, String[] ticketIds)
+		throws Exception {
+
+		try {
+			_businessEventPermission.check(
+				jwt, externalReferenceCode, ActionKeys.VIEW);
+
+			StringBundler sb = new StringBundler(9);
+
+			sb.append("Organization in aqlFunction('\"External Key\" = \"");
+			sb.append(externalReferenceCode);
+			sb.append("\"') and (status not in ('");
+			sb.append(
+				StringUtil.merge(
+					JiraIssueConstants.STATUSES_SOLVED_AND_CLOSED, "','"));
+			sb.append("')");
+
+			if (ArrayUtil.isNotEmpty(ticketIds)) {
+				sb.append(" or key in ('");
+				sb.append(StringUtil.merge(ticketIds, "','"));
+				sb.append("')");
+			}
+
+			sb.append(")");
+
+			List<JiraSupportIssue> jiraSupportIssues = _jiraService.search(
+				sb.toString(),
+				new String[] {"key", "labels", "status", "summary"});
+
+			JSONArray jsonArray = new JSONArray();
+
+			for (JiraSupportIssue jiraSupportIssue : jiraSupportIssues) {
+				jsonArray.put(_toJSONObject(jiraSupportIssue));
+			}
+
+			return new ResponseEntity<>(jsonArray.toString(), HttpStatus.OK);
+		}
+		catch (Exception exception) {
+			_log.error(exception, exception);
+
+			return new ResponseEntity(
+				exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private ResponseEntity<String> _getZendeskTickets(
+			Jwt jwt, String externalReferenceCode)
 		throws Exception {
 
 		try {
@@ -86,25 +175,17 @@ public class AccountsTicketsRestController extends BaseRestController {
 		}
 	}
 
-	private long _fetchZendeskOrganizationId(String externalReferenceCode)
-		throws Exception {
-
-		List<ExternalLink> externalLinks = _koroneikiService.fetchExternalLinks(
-			externalReferenceCode, 1, 1000);
-
-		for (ExternalLink externalLink : externalLinks) {
-			String domain = externalLink.getDomain();
-			String entityName = externalLink.getEntityName();
-
-			if (domain.equals(ExternalLinkConstants.DOMAIN_ZENDESK) &&
-				entityName.equals(
-					ExternalLinkConstants.ENTITY_NAME_ZENDESK_ORGANIZATION)) {
-
-				return GetterUtil.getLong(externalLink.getEntityId());
-			}
-		}
-
-		return 0;
+	private JSONObject _toJSONObject(JiraSupportIssue jiraSupportIssue) {
+		return new JSONObject(
+		).put(
+			"link", jiraSupportIssue.getTicketURL()
+		).put(
+			"status", jiraSupportIssue.getStatus()
+		).put(
+			"subject", jiraSupportIssue.getSummary()
+		).put(
+			"ticketId", jiraSupportIssue.getKey()
+		);
 	}
 
 	private static final Log _log = LogFactory.getLog(
@@ -112,6 +193,12 @@ public class AccountsTicketsRestController extends BaseRestController {
 
 	@Autowired
 	private BusinessEventPermission _businessEventPermission;
+
+	@Value("${liferay.customer.feature.flag.LRSD-8280}")
+	private boolean _featureFlagLRSD_8280;
+
+	@Autowired
+	private JiraService _jiraService;
 
 	@Autowired
 	private KoroneikiService _koroneikiService;