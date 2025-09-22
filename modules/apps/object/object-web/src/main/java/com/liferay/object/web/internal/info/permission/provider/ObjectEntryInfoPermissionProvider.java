larissacribeiro/liferay-portal

LPD-64207 User should only see object definition when is enableObjectDefinitionMapping

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/permission/provider/ObjectEntryInfoPermissionProvider.java
@@ -48,7 +48,7 @@ public class ObjectEntryInfoPermissionProvider
 		if (FeatureFlagManagerUtil.isEnabled(
 				permissionChecker.getCompanyId(), "LPD-17564")) {
 
-			if (_objectDefinition.isUnmodifiableSystemObject()) {
+			if (!_objectDefinition.isEnableObjectDefinitionMapping()) {
 				return false;
 			}
 		}

liferay/liferay-portal-cloud-native

modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/permission/provider/ObjectEntryInfoPermissionProvider.java
@@ -48,7 +48,7 @@ public class ObjectEntryInfoPermissionProvider
 		if (FeatureFlagManagerUtil.isEnabled(
 				permissionChecker.getCompanyId(), "LPD-17564")) {
 
-			if (_objectDefinition.isUnmodifiableSystemObject()) {
+			if (!_objectDefinition.isEnableFormContainer()) {
 				return false;
 			}
 		}