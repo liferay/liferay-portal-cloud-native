larissacribeiro/liferay-portal

LPD-64207 Add new column enableObjectDefinitionMapping for ObjectDefinition

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -651,6 +651,11 @@ public class ObjectServiceUpgradeStepRegistrator
 			UpgradeProcessFactory.runSQL(
 				"update ObjectEntry set rootObjectEntryId = 0 where " +
 					"rootObjectEntryId is null"));
+
+		registry.register(
+			"10.22.1", "10.23.0",
+			new com.liferay.object.internal.upgrade.v10_23_0.
+				ObjectDefinitionUpgradeProcess());
 	}
 
 	@Reference

liferay/liferay-portal-cloud-native

modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -658,6 +658,11 @@ public class ObjectServiceUpgradeStepRegistrator
 			"10.22.1", "10.23.0",
 			new ObjectEntryFolderPermissionUpgradeProcess(
 				_resourceActionLocalService, _resourcePermissionLocalService));
+
+		registry.register(
+			"10.23.0", "10.24.0",
+			new com.liferay.object.internal.upgrade.v10_24_0.
+				ObjectDefinitionUpgradeProcess());
 	}
 
 	@Reference