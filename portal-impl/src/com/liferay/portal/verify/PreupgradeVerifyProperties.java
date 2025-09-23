jeffwu0724/liferay-portal

LPD-58844 removed unneeded properties

portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyProperties.java
@@ -1943,6 +1943,14 @@ public class PreupgradeVerifyProperties extends PreupgradeVerifyProcess {
 		"mail.hook.sendmail.delete.user", "mail.hook.sendmail.home",
 		"mail.hook.sendmail.virtusertable",
 		"mail.hook.sendmail.virtusertable.refresh", "mail.hook.shell.script",
+		"mail.session.mail.advanced.properties", "mail.session.mail.pop3.host",
+		"mail.session.mail.pop3.password", "mail.session.mail.pop3.port",
+		"mail.session.mail.pop3.user", "mail.session.mail.smtp.host",
+		"mail.session.mail.smtp.password", "mail.session.mail.smtp.port",
+		"mail.session.mail.smtp.starttls.enable", "mail.session.mail.smtp.user",
+		"mail.session.mail.store.protocol",
+		"mail.session.mail.transport.protocol",
+		"pop.server.notifications.enabled",
 		"memory.cluster.scheduler.lock.cache.enabled",
 		"message.boards.email.message.added.signature",
 		"message.boards.email.message.updated.signature",

liferay/liferay-portal-cloud-native

portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyProperties.java
@@ -1943,6 +1943,13 @@ public class PreupgradeVerifyProperties extends PreupgradeVerifyProcess {
 		"mail.hook.sendmail.delete.user", "mail.hook.sendmail.home",
 		"mail.hook.sendmail.virtusertable",
 		"mail.hook.sendmail.virtusertable.refresh", "mail.hook.shell.script",
+		"mail.session.mail", "mail.session.mail.advanced.properties",
+		"mail.session.mail.pop3.host", "mail.session.mail.pop3.password",
+		"mail.session.mail.pop3.port", "mail.session.mail.pop3.user",
+		"mail.session.mail.smtp.host", "mail.session.mail.smtp.password",
+		"mail.session.mail.smtp.port", "mail.session.mail.smtp.starttls.enable",
+		"mail.session.mail.smtp.user", "mail.session.mail.store.protocol",
+		"mail.session.mail.transport.protocol",
 		"memory.cluster.scheduler.lock.cache.enabled",
 		"message.boards.email.message.added.signature",
 		"message.boards.email.message.updated.signature",
@@ -1983,6 +1990,7 @@ public class PreupgradeVerifyProperties extends PreupgradeVerifyProcess {
 		"organizations.indexer.enabled", "organizations.rootable",
 		"organizations.types", "permissions.object.blocking.cache",
 		"poller.notifications.timeout", "poller.request.timeout",
+		"pop.server.notifications.enabled",
 		"portal.cache.manager.type.multi.vm",
 		"portal.cache.manager.type.single.vm", "portal.ctx",
 		"portal.fabric.enabled", "portal.fabric.agent.selector.class",