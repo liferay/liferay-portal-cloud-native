larissacribeiro/liferay-portal

LPD-64249 Add new configuration toggle for mapping object definition in form containers

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectDetails/ConfigurationContainer.tsx
@@ -176,32 +176,61 @@ export function ConfigurationContainer({
 			</ClayForm.Group>
 
 			{Liferay.FeatureFlags['LPD-17564'] && (
-				<ClayForm.Group>
-					<Toggle
-						disabled={
-							disabled ||
-							(isEnableObjectEntrySchedule && values.active)
-						}
-						label={Liferay.Language.get(
-							'allow-users-to-schedule-a-display-expiration-and-review-date-for-entries'
-						)}
-						name="enableObjectEntrySchedule"
-						onBlur={(event) => {
-							event.stopPropagation();
-
-							if (onSubmit) {
-								onSubmit();
+				<>
+					<ClayForm.Group>
+						<Toggle
+							disabled={
+								disabled ||
+								(isEnableObjectEntrySchedule && values.active)
 							}
-						}}
-						onToggle={() => {
-							setValues({
-								enableObjectEntrySchedule:
-									!values.enableObjectEntrySchedule,
-							});
-						}}
-						toggled={values.enableObjectEntrySchedule}
-					/>
-				</ClayForm.Group>
+							label={Liferay.Language.get(
+								'allow-users-to-schedule-a-display-expiration-and-review-date-for-entries'
+							)}
+							name="enableObjectEntrySchedule"
+							onBlur={(event) => {
+								event.stopPropagation();
+
+								if (onSubmit) {
+									onSubmit();
+								}
+							}}
+							onToggle={() => {
+								setValues({
+									enableObjectEntrySchedule:
+										!values.enableObjectEntrySchedule,
+								});
+							}}
+							toggled={values.enableObjectEntrySchedule}
+						/>
+					</ClayForm.Group>
+
+					<ClayForm.Group>
+						<Toggle
+							disabled={disabled}
+							label={sub(
+								Liferay.Language.get('enable-x'),
+								Liferay.Language.get(
+									'mapping-in-form-container'
+								)
+							)}
+							name="enableObjectDefinitionMapping"
+							onBlur={(event) => {
+								event.stopPropagation();
+
+								if (onSubmit) {
+									onSubmit();
+								}
+							}}
+							onToggle={() => {
+								setValues({
+									enableObjectDefinitionMapping:
+										!values.enableObjectDefinitionMapping,
+								});
+							}}
+							toggled={values.enableObjectDefinitionMapping}
+						/>
+					</ClayForm.Group>
+				</>
 			)}
 		</div>
 	);

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/types.d.ts
@@ -201,6 +201,7 @@ interface ObjectDefinition {
 	enableFriendlyURLCustomization: boolean;
 	enableIndexSearch: boolean;
 	enableLocalization: boolean;
+	enableObjectDefinitionMapping: boolean;
 	enableObjectEntryDraft: boolean;
 	enableObjectEntryHistory: boolean;
 	enableObjectEntrySchedule: boolean;

liferay/liferay-portal-cloud-native

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectDetails/ConfigurationContainer.tsx
@@ -176,32 +176,61 @@ export function ConfigurationContainer({
 			</ClayForm.Group>
 
 			{Liferay.FeatureFlags['LPD-17564'] && (
-				<ClayForm.Group>
-					<Toggle
-						disabled={
-							disabled ||
-							(isEnableObjectEntrySchedule && values.active)
-						}
-						label={Liferay.Language.get(
-							'allow-users-to-schedule-a-display-expiration-and-review-date-for-entries'
-						)}
-						name="enableObjectEntrySchedule"
-						onBlur={(event) => {
-							event.stopPropagation();
-
-							if (onSubmit) {
-								onSubmit();
+				<>
+					<ClayForm.Group>
+						<Toggle
+							disabled={
+								disabled ||
+								(isEnableObjectEntrySchedule && values.active)
 							}
-						}}
-						onToggle={() => {
-							setValues({
-								enableObjectEntrySchedule:
-									!values.enableObjectEntrySchedule,
-							});
-						}}
-						toggled={values.enableObjectEntrySchedule}
-					/>
-				</ClayForm.Group>
+							label={Liferay.Language.get(
+								'allow-users-to-schedule-a-display-expiration-and-review-date-for-entries'
+							)}
+							name="enableObjectEntrySchedule"
+							onBlur={(event) => {
+								event.stopPropagation();
+
+								if (onSubmit) {
+									onSubmit();
+								}
+							}}
+							onToggle={() => {
+								setValues({
+									enableObjectEntrySchedule:
+										!values.enableObjectEntrySchedule,
+								});
+							}}
+							toggled={values.enableObjectEntrySchedule}
+						/>
+					</ClayForm.Group>
+
+					<ClayForm.Group>
+						<Toggle
+							disabled={disabled}
+							label={sub(
+								Liferay.Language.get('enable-x'),
+								Liferay.Language.get(
+									'mapping-in-form-container'
+								)
+							)}
+							name="enableFormContainer"
+							onBlur={(event) => {
+								event.stopPropagation();
+
+								if (onSubmit) {
+									onSubmit();
+								}
+							}}
+							onToggle={() => {
+								setValues({
+									enableFormContainer:
+										!values.enableFormContainer,
+								});
+							}}
+							toggled={values.enableFormContainer}
+						/>
+					</ClayForm.Group>
+				</>
 			)}
 		</div>
 	);

modules/apps/object/object-web/src/main/resources/META-INF/resources/js/types.d.ts
@@ -198,6 +198,7 @@ interface ObjectDefinition {
 	defaultLanguageId: Liferay.Language.Locale;
 	enableCategorization: boolean;
 	enableComments: boolean;
+	enableFormContainer: boolean;
 	enableFriendlyURLCustomization: boolean;
 	enableIndexSearch: boolean;
 	enableLocalization: boolean;