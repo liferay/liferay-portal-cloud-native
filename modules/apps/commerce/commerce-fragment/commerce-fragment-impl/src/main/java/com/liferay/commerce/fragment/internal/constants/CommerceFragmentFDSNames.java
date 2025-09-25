3de42c0acd6ea5ccf313a984126e6a3137ce8bb5
Alessio Rendina <asilcomando@gmail.com>
2025-09-25 11:31:15 +0200

    LPD-63169 Added new constants

---
+++ modules/apps/commerce/commerce-fragment/commerce-fragment-impl/src/main/java/com/liferay/commerce/fragment/internal/constants/CommerceFragmentFDSNames.java
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.fragment.internal.constants;
+
+import com.liferay.commerce.constants.CommerceFragmentRendererKeys;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceFragmentFDSNames {
+
+	public static final String PENDING_ACCOUNT_ORDERS =
+		CommerceFragmentRendererKeys.ACCOUNT_ORDERS_DATA_SET +
+			"-pendingAccountOrders";
+
+}
