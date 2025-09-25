4a96787e4d3605ea543d60adc2be58fd9bff206a
Anthony Chu <anthony.chu@liferay.com>
2025-09-25 14:09:57 -0700

    LPD-66447 Add additional test case for Jakarta JSTL core URIs

--- modules/sdk/ant-bnd/src/test/java/com/liferay/ant/bnd/jsp/JspAnalyzerPluginTest.java
+++ modules/sdk/ant-bnd/src/test/java/com/liferay/ant/bnd/jsp/JspAnalyzerPluginTest.java
@@ -181,6 +181,32 @@ public class JspAnalyzerPluginTest {
 	}
 
 	@Test
+	public void testTaglibRequirementsWithJakartaJstlCoreUri()
+		throws Exception {
+
+		JspAnalyzerPlugin jspAnalyzerPlugin = new JspAnalyzerPlugin();
+
+		URL url = getResource("dependencies/imports_without_comments.jsp");
+
+		InputStream inputStream = url.openStream();
+
+		String content = IO.collect(inputStream);
+
+		Builder builder = new Builder();
+
+		builder.build();
+
+		Set<String> taglibURIs = new HashSet<>();
+
+		jspAnalyzerPlugin.addTaglibRequirements(builder, content, taglibURIs);
+
+		String requireCapability = builder.getProperty(
+			Constants.REQUIRE_CAPABILITY);
+
+		Assert.assertFalse(requireCapability.contains("jakarta.tags.core"));
+	}
+
+	@Test
 	public void testTaglibRequirementsWithJavaxJstlCoreUri() throws Exception {
 		JspAnalyzerPlugin jspAnalyzerPlugin = new JspAnalyzerPlugin();
 
