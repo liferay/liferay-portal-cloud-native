liferay-site-management/liferay-portal

LPD-64994 Fix styling for edit categories page

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/categorization/categories/EditCategoryPage.tsx
@@ -353,19 +353,21 @@ const EditCategoryPage = ({
 
 	return (
 		<div className="categorization-section">
-			<CategorizationManagementToolbar
-				backURL={backURL}
-				handleSave={handleSave}
-				handleSaveAndAddAnother={
-					isCreateNew ? handleSaveAndAddAnother : undefined
-				}
-				showSaveAndAddAnotherButton={isCreateNew}
-				title={getTitle()}
-			/>
+			<div className="edit-vocabulary">
+				<CategorizationManagementToolbar
+					backURL={backURL}
+					handleSave={handleSave}
+					handleSaveAndAddAnother={
+						isCreateNew ? handleSaveAndAddAnother : undefined
+					}
+					showSaveAndAddAnotherButton={isCreateNew}
+					title={getTitle()}
+				/>
 
-			<CategorizationContentContainer
-				mainContentMap={createMainContentMap()}
-			/>
+				<CategorizationContentContainer
+					mainContentMap={createMainContentMap()}
+				/>
+			</div>
 		</div>
 	);
 };

liferay/liferay-portal-cloud-native

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/categorization/categories/EditCategoryPage.tsx
@@ -353,19 +353,21 @@ const EditCategoryPage = ({
 
 	return (
 		<div className="categorization-section">
-			<CategorizationManagementToolbar
-				backURL={backURL}
-				handleSave={handleSave}
-				handleSaveAndAddAnother={
-					isCreateNew ? handleSaveAndAddAnother : undefined
-				}
-				showSaveAndAddAnotherButton={isCreateNew}
-				title={getTitle()}
-			/>
+			<div className="edit-page">
+				<CategorizationManagementToolbar
+					backURL={backURL}
+					handleSave={handleSave}
+					handleSaveAndAddAnother={
+						isCreateNew ? handleSaveAndAddAnother : undefined
+					}
+					showSaveAndAddAnotherButton={isCreateNew}
+					title={getTitle()}
+				/>
 
-			<CategorizationContentContainer
-				mainContentMap={createMainContentMap()}
-			/>
+				<CategorizationContentContainer
+					mainContentMap={createMainContentMap()}
+				/>
+			</div>
 		</div>
 	);
 };