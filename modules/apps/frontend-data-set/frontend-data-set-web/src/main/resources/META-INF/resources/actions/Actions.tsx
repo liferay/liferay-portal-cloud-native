13fa09a57856333e0ca247fed092f12d267f11e5
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2025-10-02 10:59:41 +0400

    LPD-66897 - Fix possibility of formattedActions being undefined

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/actions/Actions.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/actions/Actions.tsx
@@ -117,7 +117,7 @@ function Actions({
 	};
 
 	const quickActions =
-		formattedActions[0].type === EItemActionsType.GROUP
+		formattedActions[0]?.type === EItemActionsType.GROUP
 			? formattedActions[0].items?.slice(0, QUICK_ACTIONS_MAX_NUMBER) ||
 				[]
 			: formattedActions.slice(0, QUICK_ACTIONS_MAX_NUMBER);
