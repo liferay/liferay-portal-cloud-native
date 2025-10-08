376b3a7434c35060c7819df74613f2f33c19979a
Mumen Hussein Mahmoud Tayyem <mumen.tayyem@liferay.com>
2025-10-08 10:34:28 +0400

    LRDOCS-14839 SF

--- workspaces/clarity-solution-workspace/client-extensions/clarity-solution-custom-element-distributors-map/vite.config.js
+++ workspaces/clarity-solution-workspace/client-extensions/clarity-solution-custom-element-distributors-map/vite.config.js
@@ -7,13 +7,13 @@ import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-	plugins: [react()],
 	build: {
 		outDir: 'build/vite',
 		rollupOptions: {
 			external: ['react', 'react-dom', 'clarity-distributors-api'],
 		},
 	},
+	plugins: [react()],
 	server: {
 		origin: 'http://localhost:5173',
 	},
