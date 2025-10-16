b703fcc7b0bef9df1aa0f6f7dbaa75a8d38b17a3
Luis Ortiz <luis.ortiz@liferay.com>
2025-10-16 18:46:42 +0200

    LPD-61502 Enabling the manual execution of the ServiceComponentDataCleanupVerifyProcess from the user interface

--- modules/apps/data-cleanup/data-cleanup/src/main/java/com/liferay/data/cleanup/internal/upgrade/executor/DataRemovalExecutor.java
+++ modules/apps/data-cleanup/data-cleanup/src/main/java/com/liferay/data/cleanup/internal/upgrade/executor/DataRemovalExecutor.java
@@ -16,12 +16,14 @@ import com.liferay.data.cleanup.internal.upgrade.OutdatedPublishedCTCollectionUp
 import com.liferay.data.cleanup.internal.upgrade.PublishedCTSContentDataUpgradeProcess;
 import com.liferay.data.cleanup.internal.upgrade.WidgetLayoutTypeSettingsUpgradeProcess;
 import com.liferay.data.cleanup.internal.upgrade.util.ConfigurationUtil;
+import com.liferay.data.cleanup.internal.verify.ServiceComponentDataCleanupVerifyProcess;
 import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.layout.manager.ContentManager;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureRelLocalService;
 import com.liferay.layout.service.LayoutClassedModelUsageLocalService;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.cache.CacheRegistryUtil;
 import com.liferay.portal.kernel.model.Release;
@@ -44,12 +46,18 @@ import com.liferay.portal.upgrade.data.cleanup.JournalDataCleanupPreupgradeProce
 import com.liferay.portal.upgrade.data.cleanup.NullUnicodeContentDataCleanupPreupgradeProcess;
 import com.liferay.portal.upgrade.data.cleanup.QuartzJobDetailsDataCleanupPreupgradeProcess;
 import com.liferay.portal.upgrade.data.cleanup.UserDataCleanupPreupgradeProcess;
+import com.liferay.portal.verify.VerifyProcess;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.function.Supplier;
 
 import org.apache.felix.cm.PersistenceManager;
 
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -130,6 +138,30 @@ public class DataRemovalExecutor {
 				dataCleanupPreupgradeProcess.upgrade();
 
 				CacheRegistryUtil.clear();
+			}
+		}
+
+		if (dataRemovalConfiguration.removeServiceComponentOrphanData()) {
+			Bundle bundle = FrameworkUtil.getBundle(getClass());
+
+			BundleContext bundleContext = bundle.getBundleContext();
+
+			Collection<ServiceReference<VerifyProcess>> serviceReferences =
+				bundleContext.getServiceReferences(
+					VerifyProcess.class,
+					StringBundler.concat(
+						"(component.name=",
+						ServiceComponentDataCleanupVerifyProcess.class.
+							getName(),
+						")"));
+
+			for (ServiceReference<VerifyProcess> serviceReference :
+					serviceReferences) {
+
+				VerifyProcess verifyProcess = bundleContext.getService(
+					serviceReference);
+
+				verifyProcess.verify();
 			}
 		}
 	}
