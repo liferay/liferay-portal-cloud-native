f7a24ddbaea3eafb4d6b65f3be863d753f917534
Noor Najjar <noor.najjar@liferay.com>
2025-10-16 17:28:05 -0700

    LPD-60775 Close permissions modal after save

--- modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/components/PublicationsPermissions.js
+++ modules/apps/change-tracking/change-tracking-web/src/main/resources/META-INF/resources/publications/js/components/PublicationsPermissions.js
@@ -107,6 +107,9 @@ export default function PublicationsPermissions({
 								aria-label={Liferay.Language.get('save')}
 								onClick={() => {
 									saveRolePermissions();
+									Liferay.Portlet.refresh(
+										`#p_p_id${namespace}`
+									);
 								}}
 								type="submit"
 							>
