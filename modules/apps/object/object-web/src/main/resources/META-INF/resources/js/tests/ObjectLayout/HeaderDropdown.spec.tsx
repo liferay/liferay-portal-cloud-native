6e94043fd157f0262b485a701f529670c8783ee0
Igor Franca <igor.franca@liferay.com>
2025-10-21 13:34:35 -0300

    LPD-68721 - Update unit tests

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/tests/ObjectLayout/HeaderDropdown.spec.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/tests/ObjectLayout/HeaderDropdown.spec.tsx
@@ -5,7 +5,7 @@
 
 import {fireEvent, render, screen} from '@testing-library/react';
 
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import React from 'react';
 
 import {HeaderDropdown} from '../../components/Layout/LayoutScreen/HeaderDropdown';
