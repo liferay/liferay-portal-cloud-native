113a1fba19bfef364d459ef543bf78a5a60b750f
Raymond Augé <raymond.auge@liferay.com>
2025-10-23 14:31:48 -0400

    LPD-67987 add service linked role

--- cloud/terraform/aws/dependencies/liferay.tf
+++ cloud/terraform/aws/dependencies/liferay.tf
@@ -82,6 +82,9 @@ resource "aws_iam_role_policy_attachment" "s3" {
 	policy_arn=aws_iam_policy.s3.arn
 	role=var.liferay_sa_role_name
 }
+resource "aws_iam_service_linked_role" "opensearch" {
+	aws_service_name="opensearchservice.amazonaws.com"
+}
 resource "aws_opensearch_domain" "os" {
 	access_policies=<<POLICY
 {
