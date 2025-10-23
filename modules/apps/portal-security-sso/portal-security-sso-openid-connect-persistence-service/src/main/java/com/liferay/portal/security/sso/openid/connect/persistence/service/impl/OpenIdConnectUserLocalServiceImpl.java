f465c9fda2fd05062c54262e081dd9352fe80f63
Rafael Praxedes <rafael.praxedes@liferay.com>
2025-10-23 14:10:42 -0300

    LPD-67467 Implement service methods

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-persistence-service/src/main/java/com/liferay/portal/security/sso/openid/connect/persistence/service/impl/OpenIdConnectUserLocalServiceImpl.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-persistence-service/src/main/java/com/liferay/portal/security/sso/openid/connect/persistence/service/impl/OpenIdConnectUserLocalServiceImpl.java
@@ -6,7 +6,15 @@
 package com.liferay.portal.security.sso.openid.connect.persistence.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.security.sso.openid.connect.persistence.exception.DuplicateOpenIdConnectUserException;
+import com.liferay.portal.security.sso.openid.connect.persistence.exception.OpenIdConnectUserIssuerException;
+import com.liferay.portal.security.sso.openid.connect.persistence.exception.OpenIdConnectUserSubjectException;
+import com.liferay.portal.security.sso.openid.connect.persistence.model.OpenIdConnectUser;
 import com.liferay.portal.security.sso.openid.connect.persistence.service.base.OpenIdConnectUserLocalServiceBaseImpl;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -19,4 +27,50 @@ import org.osgi.service.component.annotations.Component;
 )
 public class OpenIdConnectUserLocalServiceImpl
 	extends OpenIdConnectUserLocalServiceBaseImpl {
+
+	@Override
+	public OpenIdConnectUser addOpenIdConnectUser(
+			long companyId, long userId, String issuer, String subject)
+		throws PortalException {
+
+		OpenIdConnectUser openIdConnectUser =
+			openIdConnectUserPersistence.fetchByC_I_S(
+				companyId, issuer, subject);
+
+		if (openIdConnectUser != null) {
+			throw new DuplicateOpenIdConnectUserException();
+		}
+
+		long openIdConnectUserId = counterLocalService.increment();
+
+		openIdConnectUser = openIdConnectUserPersistence.create(
+			openIdConnectUserId);
+
+		openIdConnectUser.setCompanyId(companyId);
+		openIdConnectUser.setUserId(userId);
+		openIdConnectUser.setCreateDate(new Date());
+
+		if (Validator.isNull(issuer)) {
+			throw new OpenIdConnectUserIssuerException("Issuer is null");
+		}
+
+		openIdConnectUser.setIssuer(issuer);
+
+		if (Validator.isNull(subject)) {
+			throw new OpenIdConnectUserSubjectException("Subject is null");
+		}
+
+		openIdConnectUser.setSubject(subject);
+
+		return openIdConnectUserPersistence.update(openIdConnectUser);
+	}
+
+	@Override
+	public OpenIdConnectUser fetchOpenIdConnectUser(
+		long companyId, String issuer, String subject) {
+
+		return openIdConnectUserPersistence.fetchByC_I_S(
+			companyId, issuer, subject);
+	}
+
 }
