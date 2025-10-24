4a4f71f76a277727d096beab071a2e22a96774e3
Roberto Díaz <roberto.diaz@liferay.com>
2025-10-24 12:55:39 +0200

    LPD-69470 Add FileEntryTypeDLAppServiceWrapper to handle file entry type metadata updates during copy and move operations

---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/service/FileEntryTypeDLAppServiceWrapper.java
@@ -0,0 +1,142 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.document.library.internal.service;
+
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.model.DLFileEntryMetadata;
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.document.library.kernel.model.DLFileEntryTypeConstants;
+import com.liferay.document.library.kernel.model.DLFileVersion;
+import com.liferay.document.library.kernel.service.DLAppServiceWrapper;
+import com.liferay.document.library.kernel.service.DLFileEntryMetadataLocalService;
+import com.liferay.document.library.kernel.service.DLFileEntryService;
+import com.liferay.document.library.kernel.service.DLFileEntryTypeLocalService;
+import com.liferay.document.library.kernel.service.DLFileVersionLocalService;
+import com.liferay.document.library.util.DLFileEntryTypeUtil;
+import com.liferay.dynamic.data.mapping.kernel.DDMFormValues;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageEngineManager;
+import com.liferay.dynamic.data.mapping.util.DDMBeanTranslator;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Roberto Díaz
+ */
+@Component(service = ServiceWrapper.class)
+public class FileEntryTypeDLAppServiceWrapper extends DLAppServiceWrapper {
+
+	@Override
+	public FileEntry copyFileEntry(
+			long fileEntryId, long destinationFolderId,
+			long destinationRepositoryId, long fileEntryTypeId, long[] groupIds,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		_populateServiceContext(serviceContext, fileEntryId);
+
+		return super.copyFileEntry(
+			fileEntryId, destinationFolderId, destinationRepositoryId,
+			fileEntryTypeId, groupIds, serviceContext);
+	}
+
+	@Override
+	public FileEntry moveFileEntry(
+			long fileEntryId, long newFolderId, ServiceContext serviceContext)
+		throws PortalException {
+
+		_populateServiceContext(serviceContext, fileEntryId);
+
+		return super.moveFileEntry(fileEntryId, newFolderId, serviceContext);
+	}
+
+	private boolean _isSystemFileEntryType(DLFileEntryType dlFileEntryType) {
+		if (dlFileEntryType.getFileEntryTypeId() ==
+				DLFileEntryTypeConstants.FILE_ENTRY_TYPE_ID_BASIC_DOCUMENT) {
+
+			return true;
+		}
+
+		return ArrayUtil.contains(
+			_SYSTEM_FILE_ENTRY_TYPE_KEYS,
+			dlFileEntryType.getFileEntryTypeKey());
+	}
+
+	private void _populateServiceContext(
+			ServiceContext serviceContext, long fileEntryId)
+		throws PortalException {
+
+		DLFileEntry dlFileEntry = _dlFileEntryService.getFileEntry(fileEntryId);
+
+		long fileEntryTypeId = dlFileEntry.getFileEntryTypeId();
+
+		DLFileEntryType dlFileEntryType =
+			_dlFileEntryTypeLocalService.getFileEntryType(fileEntryTypeId);
+
+		if (_isSystemFileEntryType(dlFileEntryType)) {
+			return;
+		}
+
+		serviceContext.setAttribute("fileEntryTypeId", fileEntryTypeId);
+
+		DLFileVersion dlFileVersion =
+			_dlFileVersionLocalService.getLatestFileVersion(
+				dlFileEntry.getFileEntryId(), !dlFileEntry.isCheckedOut());
+
+		List<DDMStructure> ddmStructures = DLFileEntryTypeUtil.getDDMStructures(
+			dlFileEntryType);
+
+		for (DDMStructure ddmStructure : ddmStructures) {
+			DLFileEntryMetadata dlFileEntryMetadata =
+				_dlFileEntryMetadataLocalService.fetchFileEntryMetadata(
+					ddmStructure.getStructureId(),
+					dlFileVersion.getFileVersionId());
+
+			if (dlFileEntryMetadata == null) {
+				continue;
+			}
+
+			serviceContext.setAttribute(
+				DDMFormValues.class.getName() + StringPool.POUND +
+					ddmStructure.getStructureId(),
+				_ddmBeanTranslator.translate(
+					_ddmStorageEngineManager.getDDMFormValues(
+						dlFileEntryMetadata.getDDMStorageId())));
+		}
+	}
+
+	private static final String[] _SYSTEM_FILE_ENTRY_TYPE_KEYS = {
+		"DL_VIDEO_EXTERNAL_SHORTCUT", "GOOGLE_DRIVE"
+	};
+
+	@Reference
+	private DDMBeanTranslator _ddmBeanTranslator;
+
+	@Reference
+	private DDMStorageEngineManager _ddmStorageEngineManager;
+
+	@Reference
+	private DLFileEntryMetadataLocalService _dlFileEntryMetadataLocalService;
+
+	@Reference
+	private DLFileEntryService _dlFileEntryService;
+
+	@Reference
+	private DLFileEntryTypeLocalService _dlFileEntryTypeLocalService;
+
+	@Reference
+	private DLFileVersionLocalService _dlFileVersionLocalService;
+
+}
