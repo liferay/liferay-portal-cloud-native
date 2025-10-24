a493f82b08b5317ee9b902ffabfb685b2d277612
Marco Galluzzi <marco.galluzzi@liferay.com>
2025-10-24 16:56:17 +0100

    LPD-68581 Fix tests

--- modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/admin/web/internal/product/navigation/control/menu/test/EditAssetVocabularyHeaderProductNavigationControlMenuEntryTest.java
+++ modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/admin/web/internal/product/navigation/control/menu/test/EditAssetVocabularyHeaderProductNavigationControlMenuEntryTest.java
@@ -6,17 +6,12 @@
 package com.liferay.asset.categories.admin.web.internal.product.navigation.control.menu.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.asset.categories.admin.web.constants.AssetCategoriesAdminPortletKeys;
 import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.asset.kernel.model.AssetVocabularyConstants;
 import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
-import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.LayoutConstants;
 import com.liferay.portal.kernel.portlet.bridges.mvc.constants.MVCRenderConstants;
-import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
-import com.liferay.portal.kernel.servlet.PortletServlet;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderRequest;
 import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderResponse;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -24,12 +19,8 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.theme.PortletDisplay;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.model.impl.LayoutImpl;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -37,8 +28,6 @@ import com.liferay.portlet.test.MockLiferayPortletContext;
 import com.liferay.product.navigation.control.menu.ProductNavigationControlMenuEntry;
 
 import jakarta.portlet.Portlet;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -65,11 +54,6 @@ public class EditAssetVocabularyHeaderProductNavigationControlMenuEntryTest {
 	@Before
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
-
-		LayoutTestUtil.addTypePortletLayout(_group);
-
-		ServiceContextThreadLocal.pushServiceContext(
-			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
 	}
 
 	@Test
@@ -77,37 +61,45 @@ public class EditAssetVocabularyHeaderProductNavigationControlMenuEntryTest {
 		Assert.assertTrue(
 			_productNavigationControlMenuEntry.isShow(
 				_getMockHttpServletRequest(
-					_getAssetVocabulary(
+					_addAssetVocabulary(
 						AssetVocabularyConstants.VISIBILITY_TYPE_EMPTY))));
+	}
+
+	@Test
+	public void testIsShowWithDifferentPath() throws Exception {
+		Assert.assertFalse(
+			_productNavigationControlMenuEntry.isShow(
+				_getMockHttpServletRequest(
+					_addAssetVocabulary(
+						AssetVocabularyConstants.VISIBILITY_TYPE_EMPTY),
+					"/view.jsp")));
 	}
 
 	@Test
 	public void testIsShowWithNotVisibilityTypeEmptyAssetVocabulary()
 		throws Exception {
 
-		AssetVocabulary assetVocabulary = _getAssetVocabulary(
-			AssetVocabularyConstants.VISIBILITY_TYPE_INTERNAL);
+		Assert.assertFalse(
+			_productNavigationControlMenuEntry.isShow(
+				_getMockHttpServletRequest(
+					_addAssetVocabulary(
+						AssetVocabularyConstants.VISIBILITY_TYPE_INTERNAL))));
 
 		Assert.assertFalse(
 			_productNavigationControlMenuEntry.isShow(
-				_getMockHttpServletRequest(assetVocabulary)));
-
-		assetVocabulary = _getAssetVocabulary(
-			AssetVocabularyConstants.VISIBILITY_TYPE_PUBLIC);
-
-		Assert.assertFalse(
-			_productNavigationControlMenuEntry.isShow(
-				_getMockHttpServletRequest(assetVocabulary)));
+				_getMockHttpServletRequest(
+					_addAssetVocabulary(
+						AssetVocabularyConstants.VISIBILITY_TYPE_PUBLIC))));
 	}
 
 	@Test
 	public void testIsShowWithoutAssetVocabulary() throws Exception {
 		Assert.assertFalse(
 			_productNavigationControlMenuEntry.isShow(
-				_getMockHttpServletRequest()));
+				_getMockHttpServletRequest(null)));
 	}
 
-	private AssetVocabulary _getAssetVocabulary(int visibilityType)
+	private AssetVocabulary _addAssetVocabulary(int visibilityType)
 		throws Exception {
 
 		return _assetVocabularyLocalService.addVocabulary(
@@ -120,79 +112,42 @@ public class EditAssetVocabularyHeaderProductNavigationControlMenuEntryTest {
 			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
 	}
 
-	private MockHttpServletRequest _getMockHttpServletRequest()
+	private MockHttpServletRequest _getMockHttpServletRequest(
+			AssetVocabulary assetVocabulary)
+		throws Exception {
+
+		return _getMockHttpServletRequest(
+			assetVocabulary, "/edit_asset_vocabulary.jsp");
+	}
+
+	private MockHttpServletRequest _getMockHttpServletRequest(
+			AssetVocabulary assetVocabulary, String path)
 		throws Exception {
 
 		MockHttpServletRequest mockHttpServletRequest =
 			new MockHttpServletRequest();
 
 		MockLiferayPortletRenderRequest mockLiferayPortletRenderRequest =
-			_getMockLiferayPortletRenderRequest(mockHttpServletRequest);
+			new MockLiferayPortletRenderRequest(mockHttpServletRequest);
 
-		mockLiferayPortletRenderRequest.setAttribute(
-			PortletServlet.PORTLET_SERVLET_REQUEST, mockHttpServletRequest);
-
-		_portlet.render(
-			mockLiferayPortletRenderRequest,
-			new MockLiferayPortletRenderResponse());
-
-		return mockHttpServletRequest;
-	}
-
-	private MockHttpServletRequest _getMockHttpServletRequest(
-			AssetVocabulary assetVocabulary)
-		throws Exception {
-
-		MockHttpServletRequest mockHttpServletRequest =
-			_getMockHttpServletRequest();
-
-		mockHttpServletRequest.addParameter(
-			"vocabularyId", String.valueOf(assetVocabulary.getVocabularyId()));
-
-		return mockHttpServletRequest;
-	}
-
-	private MockLiferayPortletRenderRequest _getMockLiferayPortletRenderRequest(
-			HttpServletRequest httpServletRequest)
-		throws Exception {
-
-		MockLiferayPortletRenderRequest mockLiferayPortletRenderRequest =
-			new MockLiferayPortletRenderRequest(httpServletRequest);
-
-		mockLiferayPortletRenderRequest.setAttribute(
-			WebKeys.COMPANY_ID, _group.getCompanyId());
-
-		String path = "/view.jsp";
+		if (assetVocabulary != null) {
+			mockLiferayPortletRenderRequest.addParameter(
+				"vocabularyId",
+				String.valueOf(assetVocabulary.getVocabularyId()));
+		}
 
 		mockLiferayPortletRenderRequest.setAttribute(
 			MVCRenderConstants.
 				PORTLET_CONTEXT_OVERRIDE_REQUEST_ATTIBUTE_NAME_PREFIX + path,
 			new MockLiferayPortletContext(path));
 
-		mockLiferayPortletRenderRequest.setAttribute(
-			WebKeys.THEME_DISPLAY, _getThemeDisplay());
+		mockLiferayPortletRenderRequest.setParameter("mvcPath", path);
 
-		return mockLiferayPortletRenderRequest;
-	}
+		_portlet.render(
+			mockLiferayPortletRenderRequest,
+			new MockLiferayPortletRenderResponse());
 
-	private ThemeDisplay _getThemeDisplay() throws Exception {
-		ThemeDisplay themeDisplay = new ThemeDisplay();
-
-		PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
-
-		portletDisplay.setId(
-			AssetCategoriesAdminPortletKeys.ASSET_CATEGORIES_ADMIN);
-
-		Layout layout = new LayoutImpl();
-
-		layout.setType(LayoutConstants.TYPE_CONTROL_PANEL);
-
-		themeDisplay.setLayout(layout);
-
-		themeDisplay.setScopeGroupId(_group.getGroupId());
-		themeDisplay.setUser(TestPropsValues.getUser());
-
-		return themeDisplay;
+		return mockHttpServletRequest;
 	}
 
 	@Inject
@@ -211,26 +166,5 @@ public class EditAssetVocabularyHeaderProductNavigationControlMenuEntryTest {
 	)
 	private ProductNavigationControlMenuEntry
 		_productNavigationControlMenuEntry;
-
-	private static class MockLiferayPortletRenderRequest
-		extends com.liferay.portal.kernel.test.portlet.
-					MockLiferayPortletRenderRequest {
-
-		public MockLiferayPortletRenderRequest(
-			HttpServletRequest httpServletRequest) {
-
-			_httpServletRequest = httpServletRequest;
-		}
-
-		@Override
-		public void setAttribute(String name, Object value) {
-			super.setAttribute(name, value);
-
-			_httpServletRequest.setAttribute(name, value);
-		}
-
-		private final HttpServletRequest _httpServletRequest;
-
-	}
 
 }
