liferay-page-management/liferay-portal

LPD-63444 Add new methods getScopeExternalReferenceCode in FragmentEntryImpl.java and getFragmentEntryGroupId in FragmentEntryLinkImpl.java

modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/model/impl/FragmentEntryImpl.java
@@ -20,8 +20,10 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -91,6 +93,29 @@ public class FragmentEntryImpl extends FragmentEntryBaseImpl {
 		}
 
 		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getScopeExternalReferenceCode() {
+		if (getFragmentEntryId() == 0) {
+			return null;
+		}
+
+		try {
+			Group companyGroup = GroupLocalServiceUtil.getCompanyGroup(
+				getCompanyId());
+
+			if ((companyGroup != null) &&
+				(getGroupId() == companyGroup.getGroupId())) {
+
+				return companyGroup.getExternalReferenceCode();
+			}
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
+
+		return null;
 	}
 
 	@JSON

liferay/liferay-portal-cloud-native

modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/model/impl/FragmentEntryImpl.java
@@ -20,8 +20,10 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -91,6 +93,29 @@ public class FragmentEntryImpl extends FragmentEntryBaseImpl {
 		}
 
 		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getScopeERC() {
+		if (getFragmentEntryId() == 0) {
+			return null;
+		}
+
+		try {
+			Group companyGroup = GroupLocalServiceUtil.getCompanyGroup(
+				getCompanyId());
+
+			if ((companyGroup != null) &&
+				(getGroupId() == companyGroup.getGroupId())) {
+
+				return companyGroup.getExternalReferenceCode();
+			}
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
+
+		return null;
 	}
 
 	@JSON