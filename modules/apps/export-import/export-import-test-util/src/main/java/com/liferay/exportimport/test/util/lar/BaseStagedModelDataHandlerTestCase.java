liferay-page-management/liferay-portal

LPD-63444 Add a test case for LPS-149718 Investigate if limitation that causes loss of reference from page to fragment in global when importing LAR can be removed

modules/apps/export-import/export-import-test-util/src/main/java/com/liferay/exportimport/test/util/lar/BaseStagedModelDataHandlerTestCase.java
@@ -612,19 +612,9 @@ public abstract class BaseStagedModelDataHandlerTestCase {
 	protected void exportImportStagedModel(StagedModel stagedModel)
 		throws Exception {
 
-		initExport();
+		StagedModel exportedModel = exportStagedModel(stagedModel);
 
-		StagedModelDataHandlerUtil.exportStagedModel(
-			portletDataContext, stagedModel);
-
-		initImport();
-
-		StagedModel exportedStagedModel = readExportedStagedModel(stagedModel);
-
-		Assert.assertNotNull(exportedStagedModel);
-
-		StagedModelDataHandlerUtil.importStagedModel(
-			portletDataContext, exportedStagedModel);
+		importStagedModel(exportedModel);
 	}
 
 	protected void exportImportStagedModelFromLiveToStaging(
@@ -644,6 +634,23 @@ public abstract class BaseStagedModelDataHandlerTestCase {
 
 		StagedModelDataHandlerUtil.importStagedModel(
 			portletDataContext, exportedStagedModel);
+	}
+
+	protected StagedModel exportStagedModel(StagedModel stagedModel)
+		throws Exception {
+
+		initExport();
+
+		StagedModelDataHandlerUtil.exportStagedModel(
+			portletDataContext, stagedModel);
+
+		initImport();
+
+		StagedModel exportedStagedModel = readExportedStagedModel(stagedModel);
+
+		Assert.assertNotNull(exportedStagedModel);
+
+		return exportedStagedModel;
 	}
 
 	protected AssetEntry fetchAssetEntry(StagedModel stagedModel, Group group)
@@ -689,6 +696,13 @@ public abstract class BaseStagedModelDataHandlerTestCase {
 
 	protected Date getStartDate() {
 		return new Date(System.currentTimeMillis() - Time.HOUR);
+	}
+
+	protected void importStagedModel(StagedModel exportedStagedModel)
+		throws Exception {
+
+		StagedModelDataHandlerUtil.importStagedModel(
+			portletDataContext, exportedStagedModel);
 	}
 
 	protected void initExport() throws Exception {

modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/exportimport/data/handler/test/FragmentEntryLinkStagedModelDataHandlerTest.java
@@ -88,7 +88,7 @@ public class FragmentEntryLinkStagedModelDataHandlerTest
 	}
 
 	@Test
-	@TestInfo("LPD-40868")
+	@TestInfo({"LPD-40868", "LPS-149718"})
 	public void testFragmentEntryLinkWithFragmentEntryInCompanyGroup()
 		throws Exception {
 
@@ -133,6 +133,8 @@ public class FragmentEntryLinkStagedModelDataHandlerTest
 		Assert.assertEquals(
 			fragmentEntry.getExternalReferenceCode(),
 			importedFragmentEntryLink.getFragmentEntryExternalReferenceCode());
+
+		_testUpdateERCAfterExportImportStagedModel(fragmentEntry, stagedModel);
 	}
 
 	@Test
@@ -569,6 +571,43 @@ public class FragmentEntryLinkStagedModelDataHandlerTest
 	private String _read(String fileName) throws Exception {
 		return new String(
 			FileUtil.getBytes(getClass(), "dependencies/" + fileName));
+	}
+
+	private void _testUpdateERCAfterExportImportStagedModel(
+			FragmentEntry fragmentEntry, StagedModel stagedModel)
+		throws Exception {
+
+		ExportImportThreadLocal.setPortletImportInProcess(true);
+
+		try {
+			StagedModel exportedModel = exportStagedModel(stagedModel);
+
+			fragmentEntry.setExternalReferenceCode(
+				RandomTestUtil.randomString());
+
+			fragmentEntry = _fragmentEntryLocalService.updateFragmentEntry(
+				fragmentEntry);
+
+			importStagedModel(exportedModel);
+		}
+		finally {
+			ExportImportThreadLocal.setPortletImportInProcess(false);
+		}
+
+		StagedModel importedStagedModel = getStagedModel(
+			stagedModel.getUuid(), liveGroup);
+
+		Assert.assertNotNull(importedStagedModel);
+
+		FragmentEntryLink importedFragmentEntryLink =
+			(FragmentEntryLink)importedStagedModel;
+
+		Assert.assertEquals(
+			fragmentEntry.getGroupId(),
+			importedFragmentEntryLink.getFragmentEntryGroupId());
+		Assert.assertEquals(
+			fragmentEntry.getExternalReferenceCode(),
+			importedFragmentEntryLink.getFragmentEntryExternalReferenceCode());
 	}
 
 	@Inject

liferay/liferay-portal-cloud-native

modules/apps/export-import/export-import-test-util/src/main/java/com/liferay/exportimport/test/util/lar/BaseStagedModelDataHandlerTestCase.java
@@ -612,19 +612,9 @@ public abstract class BaseStagedModelDataHandlerTestCase {
 	protected void exportImportStagedModel(StagedModel stagedModel)
 		throws Exception {
 
-		initExport();
+		exportStagedModel(stagedModel);
 
-		StagedModelDataHandlerUtil.exportStagedModel(
-			portletDataContext, stagedModel);
-
-		initImport();
-
-		StagedModel exportedStagedModel = readExportedStagedModel(stagedModel);
-
-		Assert.assertNotNull(exportedStagedModel);
-
-		StagedModelDataHandlerUtil.importStagedModel(
-			portletDataContext, exportedStagedModel);
+		importStagedModel(stagedModel);
 	}
 
 	protected void exportImportStagedModelFromLiveToStaging(
@@ -644,6 +634,13 @@ public abstract class BaseStagedModelDataHandlerTestCase {
 
 		StagedModelDataHandlerUtil.importStagedModel(
 			portletDataContext, exportedStagedModel);
+	}
+
+	protected void exportStagedModel(StagedModel stagedModel) throws Exception {
+		initExport();
+
+		StagedModelDataHandlerUtil.exportStagedModel(
+			portletDataContext, stagedModel);
 	}
 
 	protected AssetEntry fetchAssetEntry(StagedModel stagedModel, Group group)
@@ -689,6 +686,17 @@ public abstract class BaseStagedModelDataHandlerTestCase {
 
 	protected Date getStartDate() {
 		return new Date(System.currentTimeMillis() - Time.HOUR);
+	}
+
+	protected void importStagedModel(StagedModel stagedModel) throws Exception {
+		initImport();
+
+		StagedModel exportedStagedModel = readExportedStagedModel(stagedModel);
+
+		Assert.assertNotNull(exportedStagedModel);
+
+		StagedModelDataHandlerUtil.importStagedModel(
+			portletDataContext, exportedStagedModel);
 	}
 
 	protected void initExport() throws Exception {

modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/exportimport/data/handler/test/FragmentEntryLinkStagedModelDataHandlerTest.java
@@ -88,7 +88,7 @@ public class FragmentEntryLinkStagedModelDataHandlerTest
 	}
 
 	@Test
-	@TestInfo("LPD-40868")
+	@TestInfo({"LPD-40868", "LPS-149718"})
 	public void testFragmentEntryLinkWithFragmentEntryInCompanyGroup()
 		throws Exception {
 
@@ -133,6 +133,9 @@ public class FragmentEntryLinkStagedModelDataHandlerTest
 		Assert.assertEquals(
 			fragmentEntry.getExternalReferenceCode(),
 			importedFragmentEntryLink.getFragmentEntryERC());
+
+		_testImportRestoresDeletedFragmentEntryInCompanyGroup(
+			fragmentEntry, stagedModel);
 	}
 
 	@Test
@@ -567,6 +570,43 @@ public class FragmentEntryLinkStagedModelDataHandlerTest
 	private String _read(String fileName) throws Exception {
 		return new String(
 			FileUtil.getBytes(getClass(), "dependencies/" + fileName));
+	}
+
+	private void _testImportRestoresDeletedFragmentEntryInCompanyGroup(
+			FragmentEntry fragmentEntry, StagedModel stagedModel)
+		throws Exception {
+
+		ExportImportThreadLocal.setPortletImportInProcess(true);
+
+		try {
+			exportStagedModel(stagedModel);
+
+			fragmentEntry.setExternalReferenceCode(
+				RandomTestUtil.randomString());
+
+			fragmentEntry = _fragmentEntryLocalService.updateFragmentEntry(
+				fragmentEntry);
+
+			importStagedModel(stagedModel);
+		}
+		finally {
+			ExportImportThreadLocal.setPortletImportInProcess(false);
+		}
+
+		StagedModel importedStagedModel = getStagedModel(
+			stagedModel.getUuid(), liveGroup);
+
+		Assert.assertNotNull(importedStagedModel);
+
+		FragmentEntryLink importedFragmentEntryLink =
+			(FragmentEntryLink)importedStagedModel;
+
+		Assert.assertEquals(
+			fragmentEntry.getGroupId(),
+			importedFragmentEntryLink.getFragmentEntryGroupId());
+		Assert.assertEquals(
+			fragmentEntry.getExternalReferenceCode(),
+			importedFragmentEntryLink.getFragmentEntryERC());
 	}
 
 	@Inject