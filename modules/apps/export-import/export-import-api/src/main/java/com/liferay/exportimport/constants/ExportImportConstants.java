brianchandotcom/liferay-portal

LPD-67949 Rename

modules/apps/export-import/export-import-api/src/main/java/com/liferay/exportimport/constants/ExportImportConstants.java
@@ -10,7 +10,7 @@ package com.liferay.exportimport.constants;
  */
 public class ExportImportConstants {
 
-	public static final String EXPORT_IMPORT_MODEL_NAME =
+	public static final String CLASS_NAME_EXPORT_IMPORT =
 		"com.liferay.exportimport.kernel.model.ExportImportConfiguration";
 
 	public static final String EXPORT_IMPORT_SCHEMA_VERSION = "4.0.0";

modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/portlet/configuration/icon/ExportPortletConfigurationIcon.java
@@ -74,7 +74,7 @@ public class ExportPortletConfigurationIcon
 			return PortletURLBuilder.create(
 				PortletProviderUtil.getPortletURL(
 					portletRequest,
-					ExportImportConstants.EXPORT_IMPORT_MODEL_NAME,
+					ExportImportConstants.CLASS_NAME_EXPORT_IMPORT,
 					PortletProvider.Action.VIEW)
 			).setMVCPath(
 				"/export_portlet.jsp"

modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/portlet/configuration/icon/ImportPortletConfigurationIcon.java
@@ -69,7 +69,7 @@ public class ImportPortletConfigurationIcon
 			return PortletURLBuilder.create(
 				PortletProviderUtil.getPortletURL(
 					portletRequest,
-					ExportImportConstants.EXPORT_IMPORT_MODEL_NAME,
+					ExportImportConstants.CLASS_NAME_EXPORT_IMPORT,
 					PortletProvider.Action.VIEW)
 			).setMVCPath(
 				"/import_portlet.jsp"

liferay/liferay-portal-cloud-native

modules/apps/export-import/export-import-api/src/main/java/com/liferay/exportimport/constants/ExportImportConstants.java
@@ -10,7 +10,7 @@ package com.liferay.exportimport.constants;
  */
 public class ExportImportConstants {
 
-	public static final String EXPORT_IMPORT_MODEL_NAME =
+	public static final String EXPORT_IMPORT_CONFIGURATION =
 		"com.liferay.exportimport.kernel.model.ExportImportConfiguration";
 
 	public static final String EXPORT_IMPORT_SCHEMA_VERSION = "4.0.0";

modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/portlet/configuration/icon/ExportPortletConfigurationIcon.java
@@ -74,7 +74,7 @@ public class ExportPortletConfigurationIcon
 			return PortletURLBuilder.create(
 				PortletProviderUtil.getPortletURL(
 					portletRequest,
-					ExportImportConstants.EXPORT_IMPORT_MODEL_NAME,
+					ExportImportConstants.EXPORT_IMPORT_CONFIGURATION,
 					PortletProvider.Action.VIEW)
 			).setMVCPath(
 				"/export_portlet.jsp"

modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/portlet/configuration/icon/ImportPortletConfigurationIcon.java
@@ -69,7 +69,7 @@ public class ImportPortletConfigurationIcon
 			return PortletURLBuilder.create(
 				PortletProviderUtil.getPortletURL(
 					portletRequest,
-					ExportImportConstants.EXPORT_IMPORT_MODEL_NAME,
+					ExportImportConstants.EXPORT_IMPORT_CONFIGURATION,
 					PortletProvider.Action.VIEW)
 			).setMVCPath(
 				"/import_portlet.jsp"