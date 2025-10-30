37f05482c16655a8bad202c8d59ec86a7b0d2df9
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-10-30 10:53:43 -0300

    LPD-69638 refactor: move site/site-ai-hub-site-initializer to ai-hub/ai-hub-site-initializer

--- modules/dxp/apps/ai-hub/ai-hub-site-initializer/src/main/java/com/liferay/ai/hub/site/initializer/internal/workflow/kaleo/runtime/node/ImproveWritingTaskNodeExecutorAIDelegate.java
+++ modules/dxp/apps/ai-hub/ai-hub-site-initializer/src/main/java/com/liferay/ai/hub/site/initializer/internal/workflow/kaleo/runtime/node/ImproveWritingTaskNodeExecutorAIDelegate.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.site.ai.hub.site.initializer.internal.workflow.kaleo.runtime.node;
+package com.liferay.ai.hub.site.initializer.internal.workflow.kaleo.runtime.node;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.PortalException;
