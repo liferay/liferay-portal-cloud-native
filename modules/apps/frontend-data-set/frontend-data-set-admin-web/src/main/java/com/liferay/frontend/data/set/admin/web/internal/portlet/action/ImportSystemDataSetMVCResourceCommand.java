90f12b0e347503c907e4819a32347072ea7e05a0
Daniel Sanz <daniel.sanz@liferay.com>
2025-11-06 15:37:22 +0100

    LPD-67036 Import flag when creating a customizable copy of a system dataset

--- modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/java/com/liferay/frontend/data/set/admin/web/internal/portlet/action/ImportSystemDataSetMVCResourceCommand.java
+++ modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/java/com/liferay/frontend/data/set/admin/web/internal/portlet/action/ImportSystemDataSetMVCResourceCommand.java
@@ -113,6 +113,9 @@ public class ImportSystemDataSetMVCResourceCommand
 			).put(
 				"description", systemFDSEntry.getDescription()
 			).put(
+				"hideManagementBarInEmptyState",
+				systemFDSEntry.getHideManagementBarInEmptyState()
+			).put(
 				"label", systemFDSEntry.getTitle()
 			).put(
 				"listOfItemsPerPage",
