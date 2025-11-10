b7781ef198cf2b3fb2824b7914cb3792a65f8320
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-11-10 15:37:57 -0300

    LPD-70612 generated: gradlew buildService

---
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoNodeSettingLocalServiceImpl.java
@@ -0,0 +1,22 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.workflow.kaleo.service.impl;
+
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.workflow.kaleo.service.base.KaleoNodeSettingLocalServiceBaseImpl;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(
+	property = "model.class.name=com.liferay.portal.workflow.kaleo.model.KaleoNodeSetting",
+	service = AopService.class
+)
+public class KaleoNodeSettingLocalServiceImpl
+	extends KaleoNodeSettingLocalServiceBaseImpl {
+}
