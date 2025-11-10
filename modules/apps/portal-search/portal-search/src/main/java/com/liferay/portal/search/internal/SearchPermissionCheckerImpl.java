4d32fa16f67711555c2d37d4a23974b33fee6333
Alan Huang <alan.huang@liferay.com>
2025-11-10 23:39:01 +0800

    LPD-57042 Auto SF

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/SearchPermissionCheckerImpl.java
@@ -243,9 +243,9 @@ public class SearchPermissionCheckerImpl implements SearchPermissionChecker {
 			}
 		}
 
-		document.addKeyword(Field.ROLE_ID, roleIds.toArray(new Long[0]));
 		document.addKeyword(
 			Field.GROUP_ROLE_ID, groupRoleIds.toArray(new String[0]));
+		document.addKeyword(Field.ROLE_ID, roleIds.toArray(new Long[0]));
 	}
 
 	private SearchPermissionContext _createSearchPermissionContext(
