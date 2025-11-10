239d006b51bfb2a0082f43b375af1291522c4be6
Brian Chan <brian.chan@liferay.com>
2025-11-10 10:49:48 -0300

    LPD-71153 Missing @Override

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/MessageBoardAttachmentResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/MessageBoardAttachmentResourceTest.java
@@ -111,6 +111,7 @@ public class MessageBoardAttachmentResourceTest
 		super.testGraphQLGetMessageBoardThreadMessageBoardAttachmentsPage();
 	}
 
+	@Override
 	@Test
 	public void testPostMessageBoardMessageMessageBoardAttachment()
 		throws Exception {
