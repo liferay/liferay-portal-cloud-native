6eb1b836dc15fdbbf23bae71c3317330bb058582
Thiago Buarque <thiago.buarque@liferay.com>
2025-11-11 17:09:55 -0300

    LPD-70603 add methods to account for a specified pid

--- modules/apps/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/util/ConfigurationFilterStringUtil.java
+++ modules/apps/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/util/ConfigurationFilterStringUtil.java
@@ -9,6 +9,10 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.ConfigurationAdmin;
 
 /**
  * @author Thiago Buarque
@@ -31,6 +35,16 @@ public class ConfigurationFilterStringUtil {
 			"=*)))");
 	}
 
+	public static String getCompanyScopedFilterString(
+		String companyId, String pid, String virtualInstanceId) {
+
+		return StringBundler.concat(
+			StringPool.OPEN_PARENTHESIS, StringPool.AMPERSAND,
+			_getScopedFilterString(pid),
+			getCompanyScopedFilterString(companyId, virtualInstanceId),
+			StringPool.CLOSE_PARENTHESIS);
+	}
+
 	public static String getGroupScopedFilterString(
 		String groupId, String siteExternalReferenceCode) {
 
@@ -42,6 +56,16 @@ public class ConfigurationFilterStringUtil {
 			ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE.
 				getPropertyKey(),
 			"=*)))");
+	}
+
+	public static String getGroupScopedFilterString(
+		String groupId, String pid, String siteExternalReferenceCode) {
+
+		return StringBundler.concat(
+			StringPool.OPEN_PARENTHESIS, StringPool.AMPERSAND,
+			_getScopedFilterString(pid),
+			getGroupScopedFilterString(groupId, siteExternalReferenceCode),
+			StringPool.CLOSE_PARENTHESIS);
 	}
 
 	public static String getPortletScopedFilterString(
@@ -58,6 +82,18 @@ public class ConfigurationFilterStringUtil {
 			"=", GetterUtil.get(portletInstanceId, "*"), "))");
 	}
 
+	public static String getPortletScopedFilterString(
+		String groupId, String pid, String portletInstanceId,
+		String siteExternalReferenceCode) {
+
+		return StringBundler.concat(
+			StringPool.OPEN_PARENTHESIS, StringPool.AMPERSAND,
+			_getScopedFilterString(pid),
+			getPortletScopedFilterString(
+				groupId, portletInstanceId, siteExternalReferenceCode),
+			StringPool.CLOSE_PARENTHESIS);
+	}
+
 	public static String getSystemScopedFilterString() {
 		return StringBundler.concat(
 			"(&(|(!(",
@@ -70,6 +106,60 @@ public class ConfigurationFilterStringUtil {
 			ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE.
 				getPropertyKey(),
 			"=*)))");
+	}
+
+	public static String getSystemScopedFilterString(String pid) {
+		String filterString = StringBundler.concat(
+			StringPool.OPEN_PARENTHESIS, StringPool.PIPE,
+			_getPropertyFilterString(
+				ConfigurationAdmin.SERVICE_FACTORYPID, pid),
+			_getPropertyFilterString(Constants.SERVICE_PID, pid),
+			StringPool.CLOSE_PARENTHESIS);
+
+		if (pid.contains("~")) {
+			filterString = StringBundler.concat(
+				StringPool.OPEN_PARENTHESIS, StringPool.AMPERSAND,
+				_getPropertyFilterString(
+					ConfigurationAdmin.SERVICE_FACTORYPID,
+					ConfigurationPidUtil.getRawPid(pid)),
+				_getPropertyFilterString(Constants.SERVICE_PID, pid),
+				StringPool.CLOSE_PARENTHESIS);
+		}
+
+		return StringBundler.concat(
+			StringPool.OPEN_PARENTHESIS, StringPool.AMPERSAND, filterString,
+			getSystemScopedFilterString(), StringPool.CLOSE_PARENTHESIS);
+	}
+
+	private static String _getPropertyFilterString(String key, String value) {
+		if (Validator.isNull(key) || Validator.isNull(value)) {
+			return StringPool.BLANK;
+		}
+
+		return StringBundler.concat(
+			StringPool.OPEN_PARENTHESIS, key, StringPool.EQUAL, value,
+			StringPool.CLOSE_PARENTHESIS);
+	}
+
+	private static String _getScopedFilterString(String pid) {
+		String rawPid = ConfigurationPidUtil.getRawPid(pid);
+
+		String filterString = StringBundler.concat(
+			StringPool.OPEN_PARENTHESIS, StringPool.PIPE,
+			_getPropertyFilterString(
+				ConfigurationAdmin.SERVICE_FACTORYPID, rawPid),
+			_getPropertyFilterString(
+				ConfigurationAdmin.SERVICE_FACTORYPID, rawPid + ".scoped"),
+			StringPool.CLOSE_PARENTHESIS);
+
+		if (pid.contains("~")) {
+			filterString = StringBundler.concat(
+				StringPool.OPEN_PARENTHESIS, StringPool.AMPERSAND, filterString,
+				_getPropertyFilterString(Constants.SERVICE_PID, pid),
+				StringPool.CLOSE_PARENTHESIS);
+		}
+
+		return filterString;
 	}
 
 }
