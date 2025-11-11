9d1ab9929cbb81485cc28b38b7bab5c903ccbc2c
Daniel Raposo <daniel.raposo@liferay.com>
2025-11-11 12:11:34 +0100

    LPD-70530 buildSerice

--- modules/apps/export-import/export-import-report-service/src/main/java/com/liferay/exportimport/report/model/impl/ExportImportReportEntryModelImpl.java
+++ modules/apps/export-import/export-import-report-service/src/main/java/com/liferay/exportimport/report/model/impl/ExportImportReportEntryModelImpl.java
@@ -95,7 +95,7 @@ public class ExportImportReportEntryModelImpl
 	}
 
 	public static final String TABLE_SQL_CREATE =
-		"create table ExportImportReportEntry (mvccVersion LONG default 0 not null,exportImportReportEntryId LONG not null primary key,groupId LONG,companyId LONG,createDate DATE null,modifiedDate DATE null,classExternalReferenceCode VARCHAR(75) null,classNameId LONG,classPK LONG,exportImportConfigurationId LONG,errorMessage TEXT null,errorStacktrace TEXT null,modelNameLanguageKey VARCHAR(75) null,origin INTEGER,type_ INTEGER,status INTEGER)";
+		"create table ExportImportReportEntry (mvccVersion LONG default 0 not null,exportImportReportEntryId LONG not null primary key,groupId LONG,companyId LONG,createDate DATE null,modifiedDate DATE null,classExternalReferenceCode VARCHAR(75) null,classNameId LONG,classPK LONG,exportImportConfigurationId LONG,errorMessage TEXT null,errorStacktrace TEXT null,modelNameLanguageKey VARCHAR(255) null,origin INTEGER,type_ INTEGER,status INTEGER)";
 
 	public static final String TABLE_SQL_DROP =
 		"drop table ExportImportReportEntry";
