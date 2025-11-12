91aeffd9b7c00d92f9a3509670f1126a107406d9
pei-jung <pei-jung.lan@liferay.com>
2025-11-12 04:22:08 -0800

    LPD-67882 Add test to assert add with permission

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTCollectionTemplateServiceTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTCollectionTemplateServiceTest.java
@@ -6,15 +6,28 @@
 package com.liferay.change.tracking.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.constants.CTActionKeys;
+import com.liferay.change.tracking.constants.CTConstants;
 import com.liferay.change.tracking.model.CTCollectionTemplate;
 import com.liferay.change.tracking.service.CTCollectionTemplateLocalService;
 import com.liferay.change.tracking.service.CTCollectionTemplateService;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.test.context.ContextUserReplace;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.RoleTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -23,7 +36,6 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -42,36 +54,76 @@ public class CTCollectionTemplateServiceTest {
 			new LiferayIntegrationTestRule(),
 			PermissionCheckerMethodTestRule.INSTANCE);
 
-	@Before
-	public void setUp() throws Exception {
-		_name = RandomTestUtil.randomString();
+	@Test
+	public void testAddCTCollectionTemplate() throws Exception {
+		User user = UserTestUtil.addUser();
 
-		_ctCollectionTemplate =
-			_ctCollectionTemplateService.addCTCollectionTemplate(
-				_name, RandomTestUtil.randomString(),
-				JSONUtil.put(
-					"description", RandomTestUtil.randomString()
-				).put(
-					"name", RandomTestUtil.randomString()
-				).put(
-					"publicationsUserRoleUserIds", Collections.emptyList()
-				).put(
-					"roleValues", Collections.emptyList()
-				).put(
-					"userIds", new long[] {RandomTestUtil.randomLong()}
-				).toString());
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
+
+			_addCTCollectionTemplate(RandomTestUtil.randomString());
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			Assert.assertNotNull(principalException);
+		}
+
+		Role role = RoleTestUtil.addRole(RoleConstants.TYPE_REGULAR);
+
+		RoleTestUtil.addResourcePermission(
+			role, CTConstants.RESOURCE_NAME, ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()),
+			CTActionKeys.ADD_TEMPLATE);
+
+		UserLocalServiceUtil.addRoleUser(role.getRoleId(), user.getUserId());
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
+
+			CTCollectionTemplate ctCollectionTemplate =
+				_addCTCollectionTemplate(RandomTestUtil.randomString());
+
+			Assert.assertNotNull(ctCollectionTemplate);
+		}
 	}
 
 	@Test
-	public void testGetCTCollectionTemplates() {
+	public void testGetCTCollectionTemplates() throws Exception {
+		String name = RandomTestUtil.randomString();
+
+		_addCTCollectionTemplate(name);
+
 		_testGetCTCollectionTemplates(StringPool.BLANK);
-		_testGetCTCollectionTemplates(_name);
+		_testGetCTCollectionTemplates(name);
 	}
 
 	@Test
-	public void testGetCTCollectionTemplatesCount() {
+	public void testGetCTCollectionTemplatesCount() throws Exception {
+		String name = RandomTestUtil.randomString();
+
+		_addCTCollectionTemplate(name);
+
 		_testGetCTCollectionTemplatesCount(StringPool.BLANK);
 		_testGetCTCollectionTemplatesCount(_name);
+	}
+
+	private CTCollectionTemplate _addCTCollectionTemplate(String name)
+		throws Exception {
+
+		return _ctCollectionTemplateService.addCTCollectionTemplate(
+			name, RandomTestUtil.randomString(),
+			JSONUtil.put(
+				"description", RandomTestUtil.randomString()
+			).put(
+				"name", RandomTestUtil.randomString()
+			).put(
+				"publicationsUserRoleUserIds", Collections.emptyList()
+			).put(
+				"roleValues", Collections.emptyList()
+			).put(
+				"userIds", new long[] {RandomTestUtil.randomLong()}
+			).toString());
 	}
 
 	private void _testGetCTCollectionTemplates(String keywords) {
