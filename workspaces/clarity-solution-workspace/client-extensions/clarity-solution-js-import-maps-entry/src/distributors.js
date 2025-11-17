351f519de52be319ec4b5e52d0eb519649711fd8
Mumen Hussein Mahmoud Tayyem <mumen.tayyem@liferay.com>
2025-11-17 12:27:38 +0400

    LRDOCS-14839 Sort

--- workspaces/clarity-solution-workspace/client-extensions/clarity-solution-js-import-maps-entry/src/distributors.js
+++ workspaces/clarity-solution-workspace/client-extensions/clarity-solution-js-import-maps-entry/src/distributors.js
@@ -5,51 +5,51 @@
 
 export default [
 	{
+		city: 'Austin',
 		id: 1,
 		name: 'Warby Parker',
-		street: '11700 Domain Blvd Suite 114',
-		city: 'Austin',
-		state: 'TX',
-		zipCode: '78758',
 		position: {
 			lat: 30.402968,
 			lng: -97.720991,
 		},
+		state: 'TX',
+		street: '11700 Domain Blvd Suite 114',
+		zipCode: '78758'
 	},
 	{
+		city: 'Southlake',
 		id: 2,
 		name: 'ClearView Optics',
-		street: '1980 East State Hwy 114',
-		city: 'Southlake',
-		state: 'TX',
-		zipCode: '76092',
 		position: {
 			lat: 32.945113,
 			lng: -97.121117,
 		},
+		state: 'TX',
+		street: '1980 East State Hwy 114',
+		zipCode: '76092'
 	},
 	{
+		city: 'McKinney',
 		id: 3,
 		name: 'Stanton Optical',
-		street: '3620 W University Dr #400',
-		city: 'McKinney',
-		state: 'TX',
-		zipCode: '75071',
 		position: {
 			lat: 33.217463,
 			lng: -96.660282,
 		},
+		state: 'TX',
+		street: '3620 W University Dr #400',
+		zipCode: '75071'
 	},
 	{
+		city: 'Dallas',
 		id: 3,
 		name: 'Roka',
-		street: '5646 Milton St. Suite 540',
-		city: 'Dallas',
-		state: 'TX',
-		zipCode: '75206',
 		position: {
 			lat: 32.848092,
 			lng: -96.770822,
 		},
+		state: 'TX',
+		street: '5646 Milton St. Suite 540',
+		zipCode: '75206'
 	},
 ];
