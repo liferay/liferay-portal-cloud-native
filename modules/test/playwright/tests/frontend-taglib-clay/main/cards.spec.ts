29c648131022ba6c8da33ad706683fdd3eda76b6
Gabriel Lima <gabriel.lima@liferay.com>
2025-11-19 09:46:13 -0300

    LPD-72003 Use shared locators instead of functions

--- modules/test/playwright/tests/frontend-taglib-clay/main/cards.spec.ts
+++ modules/test/playwright/tests/frontend-taglib-clay/main/cards.spec.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Page, expect, mergeTests} from '@playwright/test';
+import {expect, mergeTests} from '@playwright/test';
 
 import {loginTest} from '../../../fixtures/loginTest';
 import {clickAndExpectToBeVisible} from '../../../utils/clickAndExpectToBeVisible';
@@ -12,21 +12,20 @@ import {TabName} from './pages/ClaySamplePage';
 
 const test = mergeTests(claySamplePageTest, loginTest());
 
-function getCardsInSection(page: Page, title: string) {
-	return getSectionByTitle(page, title).locator('.form-check-card');
-}
-
-function getSectionByTitle(page: Page, title: string) {
-	return page.locator(`div.h4:has-text("${title}") + div.row`);
-}
-
 test.beforeEach(async ({claySamplePage}) => {
 	await claySamplePage.selectTab(TabName.CARDS);
 });
 
 test('Asserts selectable image cards behavior', async ({page}) => {
+	const selectableImageCardsSection = page.locator(
+		'div.h4:has-text("Selectable Image Card") + div.row'
+	);
+
+	const selectableImageCards =
+		selectableImageCardsSection.locator('.form-check-card');
+
 	await test.step('Asserts the default state of selectable image cards', async () => {
-		const checkbox = getSectionByTitle(page, 'Selectable Image Card')
+		const checkbox = selectableImageCardsSection
 			.locator('[data-qa-id="image-card-icon-block"]')
 			.getByRole('checkbox');
 
@@ -34,9 +33,7 @@ test('Asserts selectable image cards behavior', async ({page}) => {
 	});
 
 	await test.step('Card becomes active when checkbox is checked', async () => {
-		const cards = getCardsInSection(page, 'Selectable Image Card');
-
-		for (const card of await cards.all()) {
+		for (const card of await selectableImageCards.all()) {
 			const checkbox = card.getByRole('checkbox');
 
 			await checkbox.check();
@@ -52,7 +49,7 @@ test('Asserts selectable image cards behavior', async ({page}) => {
 	await test.step('Click on card title navigates to href URL', async () => {
 		expect(page.url()).not.toContain('#image-card-href');
 
-		await getCardsInSection(page, 'Selectable Image Card')
+		await selectableImageCards
 			.first()
 			.getByRole('link', {name: 'Beetle'})
 			.click();
@@ -68,7 +65,7 @@ test('Asserts selectable image cards behavior', async ({page}) => {
 			target: page.getByRole('menuitem', {
 				name: 'Group 1 - Option 1',
 			}),
-			trigger: getCardsInSection(page, 'Selectable Image Card')
+			trigger: selectableImageCards
 				.first()
 				.getByRole('button', {name: 'More actions'}),
 		});
