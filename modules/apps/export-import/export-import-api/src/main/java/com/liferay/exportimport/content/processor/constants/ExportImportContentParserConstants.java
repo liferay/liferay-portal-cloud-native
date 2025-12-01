708364aacf885688f4af7030cbf16d59c32570da
Carlos Correa <carlos.correa@liferay.com>
2025-12-01 12:07:54 +0100

    LPD-72635 Add new interface to implement in order to handle dependencies, and also a class for the constants

---
+++ modules/apps/export-import/export-import-api/src/main/java/com/liferay/exportimport/content/processor/constants/ExportImportContentParserConstants.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.exportimport.content.processor.constants;
+
+/**
+ * @author Carlos Correa
+ */
+public class ExportImportContentParserConstants {
+
+	public static final String CONTENT_PARSER_TYPE = "content.parser.type";
+
+	public static final String DOCUMENT_LIBRARY = "DOCUMENT_LIBRARY";
+
+}
