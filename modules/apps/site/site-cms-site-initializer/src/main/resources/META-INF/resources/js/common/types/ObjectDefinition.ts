f42e8665aa606b7c95c3784a2f1046d6d9b0f5d7
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2025-12-01 16:23:37 +0000

    LPD-72877 Move types to common folder

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/types/ObjectDefinition.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/types/ObjectDefinition.ts
@@ -3,11 +3,23 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {FieldBusinessType} from '../utils/field';
-
 export type ObjectField = {
 	DBType: string;
-	businessType: FieldBusinessType;
+	businessType:
+		| 'Attachment'
+		| 'Boolean'
+		| 'Decimal'
+		| 'Date'
+		| 'DateTime'
+		| 'Integer'
+		| 'MultiselectPicklist'
+		| 'Long'
+		| 'LongText'
+		| 'Picklist'
+		| 'RichText'
+		| 'Relationship'
+		| 'String'
+		| 'Text';
 	externalReferenceCode: string;
 	indexed: boolean;
 	indexedAsKeyword?: boolean;
