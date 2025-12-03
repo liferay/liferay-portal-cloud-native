429c223fef56f959e8021cb81067cd7a71b7b0eb
Mikel Lorza <mikel.lorza@liferay.com>
2025-12-03 13:41:21 +0100

    LPD-72250 Extract FragmentViewportStyle to reuse

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/FragmentViewportTestUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/FragmentViewportTestUtil.java
@@ -6,8 +6,6 @@
 package com.liferay.headless.admin.site.resource.v1_0.test.util;
 
 import com.liferay.headless.admin.site.client.dto.v1_0.FragmentViewport;
-import com.liferay.headless.admin.site.client.dto.v1_0.FragmentViewportStyle;
-import com.liferay.layout.responsive.ViewportSize;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 
 /**
@@ -20,50 +18,20 @@ public class FragmentViewportTestUtil {
 			new FragmentViewport() {
 				{
 					setCustomCSS(RandomTestUtil.randomString());
-					setFragmentViewportStyle(_getFragmentViewportStyle());
-					setId(ViewportSize.MOBILE_LANDSCAPE::getViewportSizeId);
+					setFragmentViewportStyle(
+						FragmentViewportStyleTestUtil.
+							getFragmentViewportStyle());
+					setId(Id.LANDSCAPE_MOBILE);
 				}
 			},
 			new FragmentViewport() {
 				{
 					setCustomCSS(RandomTestUtil.randomString());
-					setFragmentViewportStyle(_getFragmentViewportStyle());
-					setId(ViewportSize.TABLET::getViewportSizeId);
+					setFragmentViewportStyle(
+						FragmentViewportStyleTestUtil.
+							getFragmentViewportStyle());
+					setId(Id.TABLET);
 				}
-			}
-		};
-	}
-
-	private static FragmentViewportStyle _getFragmentViewportStyle() {
-		return new FragmentViewportStyle() {
-			{
-				setBackgroundColor(RandomTestUtil::randomString);
-				setBorderColor(RandomTestUtil::randomString);
-				setBorderRadius(RandomTestUtil::randomString);
-				setBorderWidth(RandomTestUtil::randomString);
-				setFontFamily(RandomTestUtil::randomString);
-				setFontSize(RandomTestUtil::randomString);
-				setFontWeight(RandomTestUtil::randomString);
-				setHeight(RandomTestUtil::randomString);
-				setHidden(RandomTestUtil::randomBoolean);
-				setMarginBottom(RandomTestUtil::randomString);
-				setMarginLeft(RandomTestUtil::randomString);
-				setMarginRight(RandomTestUtil::randomString);
-				setMarginTop(RandomTestUtil::randomString);
-				setMaxHeight(RandomTestUtil::randomString);
-				setMaxWidth(RandomTestUtil::randomString);
-				setMinHeight(RandomTestUtil::randomString);
-				setMinWidth(RandomTestUtil::randomString);
-				setOpacity(RandomTestUtil::randomString);
-				setOverflow(RandomTestUtil::randomString);
-				setPaddingBottom(RandomTestUtil::randomString);
-				setPaddingLeft(RandomTestUtil::randomString);
-				setPaddingRight(RandomTestUtil::randomString);
-				setPaddingTop(RandomTestUtil::randomString);
-				setShadow(RandomTestUtil::randomString);
-				setTextAlign(RandomTestUtil::randomString);
-				setTextColor(RandomTestUtil::randomString);
-				setWidth(RandomTestUtil::randomString);
 			}
 		};
 	}
