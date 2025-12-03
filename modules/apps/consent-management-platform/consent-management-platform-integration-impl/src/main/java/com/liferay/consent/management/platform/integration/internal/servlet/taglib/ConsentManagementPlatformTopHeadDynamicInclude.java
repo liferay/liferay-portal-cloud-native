liferay-appsec/liferay-portal

LPD-71281 create Dynamic Include to insert CMP script tag into the html

modules/apps/consent-management-platform/consent-management-platform-web/src/main/java/com/liferay/consent/management/platform/web/internal/servlet/taglib/ConsentManagementPlatformTopHeadDynamicInclude.java
@@ -1,0 +1,86 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.consent.management.platform.web.internal.servlet.taglib;
+
+import com.liferay.consent.management.platform.configuration.ConsentManagementPlatformConfiguration;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProviderUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Christian Moura
+ */
+@Component(
+	property = "service.ranking:Integer=1000", service = DynamicInclude.class
+)
+public class ConsentManagementPlatformTopHeadDynamicInclude
+	extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		if (!FeatureFlagManagerUtil.isEnabled(
+				themeDisplay.getCompanyId(), "LPD-65286")) {
+
+			return;
+		}
+
+		ConsentManagementPlatformConfiguration
+			consentManagementPlatformConfiguration = null;
+
+		try {
+			consentManagementPlatformConfiguration =
+				ConfigurationProviderUtil.getGroupConfiguration(
+					ConsentManagementPlatformConfiguration.class,
+					themeDisplay.getScopeGroupId());
+		}
+		catch (ConfigurationException configurationException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(configurationException);
+			}
+		}
+
+		if ((consentManagementPlatformConfiguration == null) ||
+			!consentManagementPlatformConfiguration.enabled()) {
+
+			return;
+		}
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		printWriter.println(consentManagementPlatformConfiguration.scriptTag());
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register("/html/common/themes/top_head.jsp#pre");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ConsentManagementPlatformTopHeadDynamicInclude.class);
+
+}

liferay/liferay-portal-cloud-native

modules/apps/consent-management-platform/consent-management-platform-integration-impl/src/main/java/com/liferay/consent/management/platform/integration/internal/servlet/taglib/ConsentManagementPlatformTopHeadDynamicInclude.java
@@ -1,0 +1,86 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.consent.management.platform.integration.internal.servlet.taglib;
+
+import com.liferay.consent.management.platform.integration.configuration.ConsentManagementPlatformConfiguration;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProviderUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Christian Moura
+ */
+@Component(
+	property = "service.ranking:Integer=1000", service = DynamicInclude.class
+)
+public class ConsentManagementPlatformTopHeadDynamicInclude
+	extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		if (!FeatureFlagManagerUtil.isEnabled(
+				themeDisplay.getCompanyId(), "LPD-65286")) {
+
+			return;
+		}
+
+		ConsentManagementPlatformConfiguration
+			consentManagementPlatformConfiguration = null;
+
+		try {
+			consentManagementPlatformConfiguration =
+				ConfigurationProviderUtil.getGroupConfiguration(
+					ConsentManagementPlatformConfiguration.class,
+					themeDisplay.getScopeGroupId());
+		}
+		catch (ConfigurationException configurationException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(configurationException);
+			}
+		}
+
+		if ((consentManagementPlatformConfiguration == null) ||
+			!consentManagementPlatformConfiguration.enabled()) {
+
+			return;
+		}
+
+		PrintWriter printWriter = httpServletResponse.getWriter();
+
+		printWriter.println(consentManagementPlatformConfiguration.scriptTag());
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register("/html/common/themes/top_head.jsp#pre");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ConsentManagementPlatformTopHeadDynamicInclude.class);
+
+}