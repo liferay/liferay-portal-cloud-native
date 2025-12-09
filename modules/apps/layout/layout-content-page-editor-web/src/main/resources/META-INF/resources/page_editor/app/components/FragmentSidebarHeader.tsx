756833ba4957e0d1b63f24257c347c635cd707c6
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2025-12-09 18:08:49 +0000

    LPD-72607 Allow swapping fragments

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/FragmentSidebarHeader.tsx
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/FragmentSidebarHeader.tsx
@@ -3,17 +3,32 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayButton from '@clayui/button';
 import ClayIcon from '@clayui/icon';
+import {openSelectionModal} from 'frontend-js-components-web';
+import {createPortletURL} from 'frontend-js-web';
 import React from 'react';
 
 import SidebarPanelHeader from '../../common/components/SidebarPanelHeader';
 import {LayoutDataItem} from '../../types/layout_data/LayoutData';
-import {useSelectorCallback} from '../contexts/StoreContext';
+import {FragmentEntry} from '../actions/updateFragments';
+import {LAYOUT_DATA_ITEM_TYPES} from '../config/constants/layoutDataItemTypes';
+import {config} from '../config/index';
+import {
+	useDispatch,
+	useSelector,
+	useSelectorCallback,
+} from '../contexts/StoreContext';
 import {useGetWidgets} from '../contexts/WidgetsContext';
 import selectLayoutDataItemIcon from '../selectors/selectLayoutDataItemIcon';
 import selectLayoutDataItemLabel from '../selectors/selectLayoutDataItemLabel';
+import swapFragment from '../thunks/swapFragment';
+import isInputFragment from '../utils/isInputFragment';
 
 export function FragmentSidebarHeader({item}: {item: LayoutDataItem}) {
+	const fragmentEntryLinks = useSelector((state) => state.fragmentEntryLinks);
+
+	const dispatch = useDispatch();
 	const getWidgets = useGetWidgets();
 
 	const label = useSelectorCallback(
@@ -30,6 +45,43 @@ export function FragmentSidebarHeader({item}: {item: LayoutDataItem}) {
 		return null;
 	}
 
+	const isSwappable = isInputFragment(item, fragmentEntryLinks);
+
+	const onSwap = () => {
+		if (item.type !== LAYOUT_DATA_ITEM_TYPES.fragment) {
+			return;
+		}
+
+		const fragmentEntryLink =
+			fragmentEntryLinks[item.config.fragmentEntryLinkId];
+
+		const url = createPortletURL(config.getFragmentEntryInputURL, {
+			inputTypes: fragmentEntryLink.fieldTypes.join(','),
+		});
+
+		openSelectionModal<{
+			fragmententrykey: FragmentEntry['fragmentEntryKey'];
+			groupid: string;
+		}>({
+			onSelect: (response) => {
+				dispatch(
+					swapFragment({
+						editableValues: JSON.stringify(
+							fragmentEntryLink.editableValues
+						),
+						fragmentEntryKey: response.fragmententrykey,
+						fragmentEntryLinkId:
+							fragmentEntryLink.fragmentEntryLinkId,
+						groupId: response.groupid,
+					})
+				);
+			},
+			selectEventName: 'selectFragment',
+			title: Liferay.Language.get('swap-fragment'),
+			url: url.toString(),
+		});
+	};
+
 	return (
 		<SidebarPanelHeader
 			iconLeft={
@@ -38,6 +90,21 @@ export function FragmentSidebarHeader({item}: {item: LayoutDataItem}) {
 						className="mr-3 mt-0 text-secondary"
 						symbol={icon}
 					/>
+				) : null
+			}
+			iconRight={
+				isSwappable ? (
+					<ClayButton
+						aria-label={Liferay.Language.get('swap-fragment')}
+						borderless
+						displayType="secondary"
+						monospaced
+						onClick={() => onSwap()}
+						size="sm"
+						title={Liferay.Language.get('swap-fragment')}
+					>
+						<ClayIcon symbol="change" />
+					</ClayButton>
 				) : null
 			}
 			showCloseButton={false}
