94e8293eb4de12157c49c0960d497b22933c756c
Joao Victor <joao.victor@liferay.com>
2025-12-10 14:45:48 -0300

    LPD-73436 generated: gradlew buildRest

---
+++ modules/dxp/apps/ai-hub/ai-hub-rest-impl/src/main/java/com/liferay/ai/hub/rest/internal/resource/v1_0/TaskDefinitionResourceImpl.java
@@ -0,0 +1,21 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.hub.rest.internal.resource.v1_0;
+
+import com.liferay.ai.hub.rest.resource.v1_0.TaskDefinitionResource;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(
+	properties = "OSGI-INF/liferay/rest/v1_0/task-definition.properties",
+	scope = ServiceScope.PROTOTYPE, service = TaskDefinitionResource.class
+)
+public class TaskDefinitionResourceImpl extends BaseTaskDefinitionResourceImpl {
+}
