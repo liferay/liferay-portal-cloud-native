43b65cdf2c8b2070fd18d01a4ab9c8986fbcb485
Álvaro Saugar <alvaro.saugar@liferay.com>
2025-12-17 16:16:25 +0100

    LPD-67473 buildService

---
+++ modules/apps/oauth-client/oauth-client-persistence-api/src/main/java/com/liferay/oauth/client/persistence/exception/OAuthClientASLocalMetadataIssuerURIException.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+package com.liferay.oauth.client.persistence.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class OAuthClientASLocalMetadataIssuerURIException extends PortalException {
+
+	public OAuthClientASLocalMetadataIssuerURIException() {
+	}
+
+	public OAuthClientASLocalMetadataIssuerURIException(String msg) {
+		super(msg);
+	}
+
+	public OAuthClientASLocalMetadataIssuerURIException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public OAuthClientASLocalMetadataIssuerURIException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
