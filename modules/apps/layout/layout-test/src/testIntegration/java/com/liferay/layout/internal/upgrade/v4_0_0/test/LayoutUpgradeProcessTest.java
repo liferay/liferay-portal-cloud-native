georgel-pop-lr/liferay-portal

LPD-68134 Add test for ctCollectionId logic for masterLPTEERC LayoutUpgradeProcessTest.java

modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/internal/upgrade/v4_0_0/test/LayoutUpgradeProcessTest.java
@@ -6,30 +6,39 @@
 package com.liferay.layout.internal.upgrade.v4_0_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.test.util.BaseCTUpgradeProcessTestCase;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.test.util.LayoutPageTemplateTestUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.cache.MultiVMPool;
 import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.dao.orm.EntityCache;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.change.tracking.CTService;
 import com.liferay.portal.kernel.test.TestInfo;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.version.Version;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.portal.model.impl.LayoutImpl;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+
+import java.lang.reflect.Method;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -46,16 +55,20 @@ import org.junit.runner.RunWith;
  * @author Alberto Chaparro
  */
 @RunWith(Arquillian.class)
-public class LayoutUpgradeProcessTest {
+public class LayoutUpgradeProcessTest extends BaseCTUpgradeProcessTestCase {
 
 	@ClassRule
 	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
 		_connection = DataAccess.getConnection();
+
+		_dbInspector = new DBInspector(_connection);
 
 		_db = DBManagerUtil.getDB();
 
@@ -65,8 +78,6 @@ public class LayoutUpgradeProcessTest {
 
 	@After
 	public void tearDown() throws Exception {
-		_db.alterTableDropColumn(_connection, "Layout", "masterLayoutPlid");
-
 		DataAccess.cleanUp(_connection);
 	}
 
@@ -90,7 +101,7 @@ public class LayoutUpgradeProcessTest {
 			masterLayoutPageTemplateEntry.getPlid(), masterLayout1.getPlid());
 		_updateLayout(RandomTestUtil.nextLong(), masterLayout2.getPlid());
 
-		_runUpgrade();
+		runUpgrade();
 
 		masterLayout1 = _layoutLocalService.fetchLayout(
 			masterLayout1.getPlid());
@@ -108,17 +119,66 @@ public class LayoutUpgradeProcessTest {
 		Assert.assertTrue(
 			Validator.isNull(
 				masterLayout3.getMasterLayoutPageTemplateEntryERC()));
+
+		Assert.assertFalse(
+			_dbInspector.hasColumn("Layout", "masterLayoutPlid"));
+		Assert.assertTrue(_dbInspector.hasColumn("Layout", "masterLPTEERC"));
 	}
 
-	private void _runUpgrade() throws Exception {
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		return LayoutTestUtil.addTypeContentLayout(
+			_groupLocalService.fetchGroup(TestPropsValues.getGroupId()));
+	}
+
+	@Override
+	protected void deleteCTModel(long primaryKey) throws Exception {
+		Layout layout = _layoutLocalService.getLayout(primaryKey);
+
+		Layout layoutSetPrototypeLayout = layout.getLayoutSetPrototypeLayout();
+
+		_layoutLocalService.deleteLayout(layout);
+
+		if (layoutSetPrototypeLayout != null) {
+			_layoutLocalService.deleteLayout(layoutSetPrototypeLayout);
+		}
+	}
+
+	@Override
+	protected CTService<?> getCTService() {
+		return _layoutLocalService;
+	}
+
+	@Override
+	protected void runUpgrade() throws Exception {
 		UpgradeProcess[] upgradeProcesses = UpgradeTestUtil.getUpgradeSteps(
 			_upgradeStepRegistrator, new Version(4, 0, 0));
 
-		UpgradeProcess upgradeProcess = upgradeProcesses[0];
+		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
+			Class<?> upgradeProcessClass = upgradeProcess.getClass();
 
-		upgradeProcess.upgrade();
+			Method getPostUpgradeStepsMethod =
+				upgradeProcessClass.getDeclaredMethod("getPostUpgradeSteps");
 
-		_entityCache.clearCache(LayoutImpl.class);
+			getPostUpgradeStepsMethod.setAccessible(true);
+
+			UpgradeStep[] postUpgradeSteps =
+				(UpgradeStep[])getPostUpgradeStepsMethod.invoke(upgradeProcess);
+
+			upgradeProcess.upgrade();
+
+			for (UpgradeStep postUpgradeStep : postUpgradeSteps) {
+				postUpgradeStep.upgrade();
+			}
+		}
+
+		_entityCache.clearCache();
+		_multiVMPool.clear();
+	}
+
+	@Override
+	protected CTModel<?> updateCTModel(CTModel<?> ctModel) {
+		return _layoutLocalService.updateLayout((Layout)ctModel);
 	}
 
 	private void _updateLayout(long masterLayoutPlid, long plid)
@@ -141,6 +201,7 @@ public class LayoutUpgradeProcessTest {
 
 	private Connection _connection;
 	private DB _db;
+	private DBInspector _dbInspector;
 
 	@Inject
 	private EntityCache _entityCache;
@@ -150,5 +211,8 @@ public class LayoutUpgradeProcessTest {
 
 	@Inject
 	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
 
 }

liferay/liferay-portal-cloud-native

modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/internal/upgrade/v4_0_0/test/LayoutUpgradeProcessTest.java
@@ -6,30 +6,39 @@
 package com.liferay.layout.internal.upgrade.v4_0_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.test.util.BaseCTUpgradeProcessTestCase;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.test.util.LayoutPageTemplateTestUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.cache.MultiVMPool;
 import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBInspector;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.dao.orm.EntityCache;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.change.tracking.CTService;
 import com.liferay.portal.kernel.test.TestInfo;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.version.Version;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.portal.model.impl.LayoutImpl;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+
+import java.lang.reflect.Method;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -46,16 +55,20 @@ import org.junit.runner.RunWith;
  * @author Alberto Chaparro
  */
 @RunWith(Arquillian.class)
-public class LayoutUpgradeProcessTest {
+public class LayoutUpgradeProcessTest extends BaseCTUpgradeProcessTestCase {
 
 	@ClassRule
 	@Rule
-	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
-		new LiferayIntegrationTestRule();
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
 		_connection = DataAccess.getConnection();
+
+		_dbInspector = new DBInspector(_connection);
 
 		_db = DBManagerUtil.getDB();
 
@@ -65,8 +78,6 @@ public class LayoutUpgradeProcessTest {
 
 	@After
 	public void tearDown() throws Exception {
-		_db.alterTableDropColumn(_connection, "Layout", "masterLayoutPlid");
-
 		DataAccess.cleanUp(_connection);
 	}
 
@@ -90,7 +101,7 @@ public class LayoutUpgradeProcessTest {
 			masterLayoutPageTemplateEntry.getPlid(), masterLayout1.getPlid());
 		_updateLayout(RandomTestUtil.nextLong(), masterLayout2.getPlid());
 
-		_runUpgrade();
+		runUpgrade();
 
 		masterLayout1 = _layoutLocalService.fetchLayout(
 			masterLayout1.getPlid());
@@ -108,17 +119,62 @@ public class LayoutUpgradeProcessTest {
 		Assert.assertTrue(
 			Validator.isNull(
 				masterLayout3.getMasterLayoutPageTemplateEntryERC()));
+
+		Assert.assertFalse(
+			_dbInspector.hasColumn("Layout", "masterLayoutPlid"));
+		Assert.assertTrue(_dbInspector.hasColumn("Layout", "masterLPTEERC"));
 	}
 
-	private void _runUpgrade() throws Exception {
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		return LayoutTestUtil.addTypeContentLayout(
+			_groupLocalService.fetchGroup(TestPropsValues.getGroupId()));
+	}
+
+	@Override
+	protected void deleteCTModel(long primaryKey) throws Exception {
+		Layout layout = _layoutLocalService.fetchLayout(primaryKey);
+
+		if (layout != null) {
+			_layoutLocalService.deleteLayout(layout);
+		}
+	}
+
+	@Override
+	protected CTService<?> getCTService() {
+		return _layoutLocalService;
+	}
+
+	@Override
+	protected void runUpgrade() throws Exception {
 		UpgradeProcess[] upgradeProcesses = UpgradeTestUtil.getUpgradeSteps(
 			_upgradeStepRegistrator, new Version(4, 0, 0));
 
-		UpgradeProcess upgradeProcess = upgradeProcesses[0];
+		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
+			Class<?> upgradeProcessClass = upgradeProcess.getClass();
 
-		upgradeProcess.upgrade();
+			Method getPostUpgradeStepsMethod =
+				upgradeProcessClass.getDeclaredMethod("getPostUpgradeSteps");
 
-		_entityCache.clearCache(LayoutImpl.class);
+			getPostUpgradeStepsMethod.setAccessible(true);
+
+			UpgradeStep[] postUpgradeSteps =
+				(UpgradeStep[])getPostUpgradeStepsMethod.invoke(upgradeProcess);
+
+			upgradeProcess.upgrade();
+
+			for (UpgradeStep postUpgradeStep : postUpgradeSteps) {
+				postUpgradeStep.upgrade();
+			}
+		}
+
+		_entityCache.clearCache();
+		_multiVMPool.clear();
+	}
+
+	@Override
+	protected CTModel<?> updateCTModel(CTModel<?> ctModel) {
+		return _layoutLocalService.updateLayout((Layout)ctModel);
 	}
 
 	private void _updateLayout(long masterLayoutPlid, long plid)
@@ -141,6 +197,7 @@ public class LayoutUpgradeProcessTest {
 
 	private Connection _connection;
 	private DB _db;
+	private DBInspector _dbInspector;
 
 	@Inject
 	private EntityCache _entityCache;
@@ -150,5 +207,8 @@ public class LayoutUpgradeProcessTest {
 
 	@Inject
 	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private MultiVMPool _multiVMPool;
 
 }