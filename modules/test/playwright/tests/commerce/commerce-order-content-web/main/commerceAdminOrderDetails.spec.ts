smottal/liferay-portal

LPD-71166 Adds playwright tests

modules/test/playwright/tests/commerce/commerce-order-content-web/main/commerceAdminOrderDetails.spec.ts
@@ -1134,3 +1134,125 @@ test('LPD-30856 Can update order status by deleting unshipped items', async ({
 		commerceAdminOrdersPage.keyOrderStatus('Completed')
 	).toBeVisible();
 });
+
+test('COMMERCE-7982 Can Edit Order Measurement Unit', async ({
+	apiHelpers,
+	commerceAdminOrderDetailsPage,
+	commerceAdminOrdersPage,
+	page,
+}) => {
+	const site = await apiHelpers.headlessSite.createSite({
+		name: getRandomString(),
+	});
+
+	apiHelpers.data.push({id: site.id, type: 'site'});
+
+	const channel = await apiHelpers.headlessCommerceAdminChannel.postChannel({
+		siteGroupId: site.id,
+	});
+
+	const catalog = await apiHelpers.headlessCommerceAdminCatalog.postCatalog();
+
+	const product1 = await apiHelpers.headlessCommerceAdminCatalog.postProduct({
+		catalogId: catalog.id,
+		name: {en_US: 'Product1'},
+	});
+
+	const productSkus1 = await apiHelpers.headlessCommerceAdminCatalog
+		.getProduct(product1.productId)
+		.then((product) => {
+			return product.skus;
+		});
+
+	const sku1 = productSkus1[0];
+
+	const account = await apiHelpers.headlessAdminUser.postAccount({
+		name: getRandomString(),
+		type: 'business',
+	});
+
+	await apiHelpers.headlessAdminUser.assignUserToAccountByEmailAddress(
+		account.id,
+		['test@liferay.com']
+	);
+
+	const address = await apiHelpers.headlessCommerceAdminAccount.postAddress(
+		account.id,
+		{
+			regionISOCode: 'LA',
+		}
+	);
+
+	const warehouse =
+		await apiHelpers.headlessCommerceAdminInventoryApiHelper.postWarehouses(
+			{
+				active: true,
+				latitude: getRandomInt(),
+				longitude: getRandomInt(),
+				warehouseItems: [
+					{
+						quantity: 1,
+						sku: sku1.sku,
+					},
+				],
+			}
+		);
+
+	await apiHelpers.headlessCommerceAdminInventoryApiHelper.postWarehousesChannels(
+		warehouse.id,
+		channel.id
+	);
+
+	const order = await apiHelpers.headlessCommerceAdminOrder.postOrder({
+		accountId: account.id,
+		billingAddressId: address.id,
+		channelId: channel.id,
+		orderItems: [
+			{
+				quantity: 1,
+				skuId: sku1.id,
+			},
+		],
+		orderStatus: '1',
+		paymentMethod: 'money-order',
+		paymentStatus: '0',
+		shippingAddressId: address.id,
+		shippingMethod: 'by-weight',
+		shippingOption: 'standard-option',
+	});
+
+	await commerceAdminOrdersPage.goto();
+
+	await expect(
+		await commerceAdminOrdersPage.tableRowLink({
+			colIndex: 1,
+			rowValue: order.id,
+		})
+	).toBeVisible();
+	await (
+		await commerceAdminOrdersPage.tableRowLink({
+			colIndex: 1,
+			rowValue: order.id,
+		})
+	).click();
+
+	await page.waitForLoadState('domcontentloaded');
+
+	await commerceAdminOrderDetailsPage.orderItemActions.click();
+
+	await commerceAdminOrderDetailsPage.orderItemActionEdit.click();
+
+	await commerceAdminOrderDetailsPage.orderItemMeasurementUnits.selectOption(
+		'meters'
+	);
+
+	await commerceAdminOrderDetailsPage.orderItemSaveButton.click();
+
+	await waitForAlert(commerceAdminOrderDetailsPage.orderItemFrame);
+
+	await commerceAdminOrderDetailsPage.orderItemFrameCloseButton.click();
+
+	await expect(
+		await commerceAdminOrderDetailsPage.orderItemQuantityColumn('1 meters')
+	).toBeVisible();
+});

liferay/liferay-portal-cloud-native

modules/test/playwright/tests/commerce/commerce-order-content-web/main/commerceAdminOrderDetails.spec.ts
@@ -1134,3 +1134,125 @@ test('LPD-30856 Can update order status by deleting unshipped items', async ({
 		commerceAdminOrdersPage.keyOrderStatus('Completed')
 	).toBeVisible();
 });
+
+test('COMMERCE-7982 Can Edit Order Measurement Unit', async ({
+	apiHelpers,
+	commerceAdminOrderDetailsPage,
+	commerceAdminOrdersPage,
+	page,
+}) => {
+	const site = await apiHelpers.headlessSite.createSite({
+		name: getRandomString(),
+	});
+
+	apiHelpers.data.push({id: site.id, type: 'site'});
+
+	const channel = await apiHelpers.headlessCommerceAdminChannel.postChannel({
+		siteGroupId: site.id,
+	});
+
+	const catalog = await apiHelpers.headlessCommerceAdminCatalog.postCatalog();
+
+	const product = await apiHelpers.headlessCommerceAdminCatalog.postProduct({
+		catalogId: catalog.id,
+		name: {en_US: 'Product'},
+	});
+
+	const productSkus = await apiHelpers.headlessCommerceAdminCatalog
+		.getProduct(product.productId)
+		.then((product) => {
+			return product.skus;
+		});
+
+	const sku1 = productSkus[0];
+
+	const account = await apiHelpers.headlessAdminUser.postAccount({
+		name: getRandomString(),
+		type: 'business',
+	});
+
+	await apiHelpers.headlessAdminUser.assignUserToAccountByEmailAddress(
+		account.id,
+		['test@liferay.com']
+	);
+
+	const address = await apiHelpers.headlessCommerceAdminAccount.postAddress(
+		account.id,
+		{
+			regionISOCode: 'LA',
+		}
+	);
+
+	const warehouse =
+		await apiHelpers.headlessCommerceAdminInventoryApiHelper.postWarehouses(
+			{
+				active: true,
+				latitude: getRandomInt(),
+				longitude: getRandomInt(),
+				warehouseItems: [
+					{
+						quantity: 1,
+						sku: sku1.sku,
+					},
+				],
+			}
+		);
+
+	await apiHelpers.headlessCommerceAdminInventoryApiHelper.postWarehousesChannels(
+		warehouse.id,
+		channel.id
+	);
+
+	const order = await apiHelpers.headlessCommerceAdminOrder.postOrder({
+		accountId: account.id,
+		billingAddressId: address.id,
+		channelId: channel.id,
+		orderItems: [
+			{
+				quantity: 1,
+				skuId: sku1.id,
+			},
+		],
+		orderStatus: '1',
+		paymentMethod: 'money-order',
+		paymentStatus: '0',
+		shippingAddressId: address.id,
+		shippingMethod: 'by-weight',
+		shippingOption: 'standard-option',
+	});
+
+	await commerceAdminOrdersPage.goto();
+
+	await expect(
+		await commerceAdminOrdersPage.tableRowLink({
+			colIndex: 1,
+			rowValue: order.id,
+		})
+	).toBeVisible();
+	await (
+		await commerceAdminOrdersPage.tableRowLink({
+			colIndex: 1,
+			rowValue: order.id,
+		})
+	).click();
+
+	await page.waitForLoadState('domcontentloaded');
+
+	await commerceAdminOrderDetailsPage.orderItemActions.click();
+
+	await commerceAdminOrderDetailsPage.orderItemActionEdit.click();
+
+	await commerceAdminOrderDetailsPage.orderItemMeasurementUnits.selectOption(
+		'meters'
+	);
+
+	await commerceAdminOrderDetailsPage.orderItemSaveButton.click();
+
+	await waitForAlert(commerceAdminOrderDetailsPage.orderItemFrame);
+
+	await commerceAdminOrderDetailsPage.orderItemFrameCloseButton.click();
+
+	await expect(
+		await commerceAdminOrderDetailsPage.orderItemQuantityColumn('1 meters')
+	).toBeVisible();
+});