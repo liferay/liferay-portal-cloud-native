3e97bb40c02c4c5a349c24e58087eca9fe0488a3
Peter Yoo <peter.yoo@liferay.com>
2025-12-20 08:23:05 -0800

    Revert "LRCI-6534 use new remoteURL functions"
    This reverts commit b7d8a4d36a13b9fbd01dc9e6610a4be58ed5e3f6.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/Build.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/Build.java
@@ -278,10 +278,6 @@ public interface Build {
 			_buildURL = JenkinsResultsParserUtil.getBuildURL(
 				_build.getJobName(), getJenkinsMaster(), getQueueId());
 
-				String localBuildURL = JenkinsResultsParserUtil.getLocalURL(_buildURL);
-
-				_buildURL = JenkinsResultsParserUtil.getRemoteURL(localBuildURL);
-
 			return _buildURL;
 		}
 
