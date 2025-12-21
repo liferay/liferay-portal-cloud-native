liferay-page-management/liferay-portal

LPD-73194 Add drag and drop hook

modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/hooks/drag_and_drop/useDragAndDrop.ts
@@ -1,0 +1,46 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import usePointerDragAndDrop from './usePointerDragAndDrop';
+
+export const DROP_POSITIONS = {
+	bottom: 'bottom',
+	top: 'top',
+} as const;
+
+export type DropPosition =
+	| (typeof DROP_POSITIONS)[keyof typeof DROP_POSITIONS]
+	| '';
+
+interface Props<T> {
+	dragItemRef: React.RefObject<HTMLElement>;
+	dropItemRef: React.RefObject<HTMLElement>;
+	item: T;
+	items: T[];
+}
+
+export default function useDragAndDrop<T extends {id: string; name: string}>({
+	dragItemRef,
+	dropItemRef,
+	item,
+	items,
+}: Props<T>) {
+	const {
+		isPointerDragging,
+		isPointerDropBottomPosition,
+		isPointerDropTopPosition,
+	} = usePointerDragAndDrop<T>({
+		dragItemRef,
+		dropItemRef,
+		items,
+		targetItem: item,
+	});
+
+	return {
+		isDragging: isPointerDragging,
+		isDropBottomPosition: isPointerDropBottomPosition,
+		isDropTopPosition: isPointerDropTopPosition,
+	};
+}

modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/hooks/drag_and_drop/usePointerDragAndDrop.ts
@@ -1,0 +1,144 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {useEffect, useRef, useState} from 'react';
+import {
+	DragObjectWithType,
+	DropTargetMonitor,
+	useDrag,
+	useDrop,
+} from 'react-dnd';
+import {getEmptyImage} from 'react-dnd-html5-backend';
+
+import './useDragAndDrop.scss';
+import {DROP_POSITIONS, DropPosition} from './useDragAndDrop';
+
+const HOVER_BORDER_LIMIT = 60;
+
+const ITEM_TYPE = 'item';
+
+export default function usePointerDragAndDrop<
+	T extends {
+		id: string;
+	},
+>({
+	dragItemRef,
+	dropItemRef,
+	hoverLimit = HOVER_BORDER_LIMIT,
+	items,
+	targetItem,
+}: {
+	dragItemRef: React.RefObject<HTMLElement>;
+	dropItemRef: React.RefObject<HTMLElement>;
+	hoverLimit?: number;
+	items: T[];
+	targetItem: T;
+}) {
+	const dropIndexRef = useRef<number>(0);
+	const [dropPosition, setDropPosition] = useState<DropPosition>('');
+
+	const [{isOver}, drop] = useDrop<
+		T & DragObjectWithType,
+		void,
+		{draggingItem: T; isOver: boolean}
+	>({
+		accept: ITEM_TYPE,
+		canDrop: (draggedItem) => {
+			const draggedItemIndex = items.findIndex(
+				({id}) => id === draggedItem.id
+			);
+
+			return draggedItemIndex !== dropIndexRef.current;
+		},
+		collect: (monitor) => ({
+			draggingItem: monitor.getItem(),
+			isOver: monitor.isOver({shallow: true}),
+		}),
+		drop(droppedItem, monitor) {
+			if (!monitor.canDrop()) {
+				return;
+			}
+
+			const newItems = items.filter(({id}) => id !== droppedItem.id);
+
+			newItems.splice(dropIndexRef.current, 0, droppedItem);
+		},
+		hover(draggedItem, monitor) {
+			if (!dropItemRef.current) {
+				return;
+			}
+
+			let dropPosition: DropPosition = '';
+
+			if (targetItem.id !== draggedItem.id) {
+				dropPosition = getDropPosition(
+					dropItemRef,
+					monitor,
+					hoverLimit
+				);
+			}
+
+			setDropPosition(dropPosition);
+
+			const targetIndex = items
+				.filter(({id}) => id !== draggedItem.id)
+				.findIndex(({id}) => id === targetItem.id);
+
+			dropIndexRef.current = Math.max(
+				0,
+				targetIndex + (dropPosition === DROP_POSITIONS.bottom ? 1 : 0)
+			);
+		},
+	});
+
+	const [{isDragging}, drag, previewRef] = useDrag<
+		DragObjectWithType,
+		void,
+		{isDragging: boolean}
+	>({
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging(),
+		}),
+		item: {...targetItem, type: ITEM_TYPE},
+	});
+
+	useEffect(() => {
+		drag(dragItemRef);
+	}, [drag, dragItemRef]);
+
+	useEffect(() => {
+		drop(dropItemRef);
+	}, [drop, dropItemRef]);
+
+	useEffect(() => {
+		previewRef(getEmptyImage());
+	}, [previewRef]);
+
+	return {
+		isPointerDragging: isDragging,
+		isPointerDropBottomPosition:
+			isOver && dropPosition === DROP_POSITIONS.bottom,
+		isPointerDropTopPosition: isOver && dropPosition === DROP_POSITIONS.top,
+	};
+}
+
+function getDropPosition(
+	ref: React.RefObject<HTMLElement>,
+	monitor: DropTargetMonitor,
+	hoverLimit: number
+) {
+	if (!ref.current) {
+		return '';
+	}
+
+	const clientOffset = monitor.getClientOffset()!;
+	const dropItemBoundingRect = ref.current.getBoundingClientRect();
+	const hoverBottomLimit = dropItemBoundingRect.height - hoverLimit;
+	const hoverClientY = clientOffset.y - dropItemBoundingRect.top;
+
+	return hoverClientY > hoverBottomLimit
+		? DROP_POSITIONS.bottom
+		: DROP_POSITIONS.top;
+}

liferay/liferay-portal-cloud-native

modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/hooks/drag_and_drop/useDragAndDrop.ts
@@ -1,0 +1,46 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import usePointerDragAndDrop from './usePointerDragAndDrop';
+
+export const DROP_POSITIONS = {
+	bottom: 'bottom',
+	top: 'top',
+} as const;
+
+export type DropPosition =
+	| (typeof DROP_POSITIONS)[keyof typeof DROP_POSITIONS]
+	| '';
+
+interface Props<T> {
+	dragHandlerRef: React.RefObject<HTMLElement>;
+	dropItemRef: React.RefObject<HTMLElement>;
+	item: T;
+	items: T[];
+}
+
+export default function useDragAndDrop<T extends {id: string; name: string}>({
+	dragHandlerRef,
+	dropItemRef,
+	item,
+	items,
+}: Props<T>) {
+	const {
+		isPointerDragging,
+		isPointerDropBottomPosition,
+		isPointerDropTopPosition,
+	} = usePointerDragAndDrop<T>({
+		dragHandlerRef,
+		dropItemRef,
+		items,
+		targetItem: item,
+	});
+
+	return {
+		isDragging: isPointerDragging,
+		isDropBottomPosition: isPointerDropBottomPosition,
+		isDropTopPosition: isPointerDropTopPosition,
+	};
+}

modules/apps/layout/layout-js-components-web/src/main/resources/META-INF/resources/js/hooks/drag_and_drop/usePointerDragAndDrop.ts
@@ -1,0 +1,144 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {useEffect, useRef, useState} from 'react';
+import {
+	DragObjectWithType,
+	DropTargetMonitor,
+	useDrag,
+	useDrop,
+} from 'react-dnd';
+import {getEmptyImage} from 'react-dnd-html5-backend';
+
+import './useDragAndDrop.scss';
+import {DROP_POSITIONS, DropPosition} from './useDragAndDrop';
+
+const HOVER_BORDER_LIMIT = 60;
+
+const ITEM_TYPE = 'item';
+
+export default function usePointerDragAndDrop<
+	T extends {
+		id: string;
+	},
+>({
+	dragHandlerRef,
+	dropItemRef,
+	hoverLimit = HOVER_BORDER_LIMIT,
+	items,
+	targetItem,
+}: {
+	dragHandlerRef: React.RefObject<HTMLElement>;
+	dropItemRef: React.RefObject<HTMLElement>;
+	hoverLimit?: number;
+	items: T[];
+	targetItem: T;
+}) {
+	const dropIndexRef = useRef<number>(0);
+	const [dropPosition, setDropPosition] = useState<DropPosition>('');
+
+	const [{isOver}, drop] = useDrop<
+		T & DragObjectWithType,
+		void,
+		{draggingItem: T; isOver: boolean}
+	>({
+		accept: ITEM_TYPE,
+		canDrop: (draggedItem) => {
+			const draggedItemIndex = items.findIndex(
+				({id}) => id === draggedItem.id
+			);
+
+			return draggedItemIndex !== dropIndexRef.current;
+		},
+		collect: (monitor) => ({
+			draggingItem: monitor.getItem(),
+			isOver: monitor.isOver({shallow: true}),
+		}),
+		drop(droppedItem, monitor) {
+			if (!monitor.canDrop()) {
+				return;
+			}
+
+			const newItems = items.filter(({id}) => id !== droppedItem.id);
+
+			newItems.splice(dropIndexRef.current, 0, droppedItem);
+		},
+		hover(draggedItem, monitor) {
+			if (!dropItemRef.current) {
+				return;
+			}
+
+			let dropPosition: DropPosition = '';
+
+			if (targetItem.id !== draggedItem.id) {
+				dropPosition = getDropPosition(
+					dropItemRef,
+					monitor,
+					hoverLimit
+				);
+			}
+
+			setDropPosition(dropPosition);
+
+			const targetIndex = items
+				.filter(({id}) => id !== draggedItem.id)
+				.findIndex(({id}) => id === targetItem.id);
+
+			dropIndexRef.current = Math.max(
+				0,
+				targetIndex + (dropPosition === DROP_POSITIONS.bottom ? 1 : 0)
+			);
+		},
+	});
+
+	const [{isDragging}, drag, previewRef] = useDrag<
+		DragObjectWithType,
+		void,
+		{isDragging: boolean}
+	>({
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging(),
+		}),
+		item: {...targetItem, type: ITEM_TYPE},
+	});
+
+	useEffect(() => {
+		drag(dragHandlerRef);
+	}, [drag, dragHandlerRef]);
+
+	useEffect(() => {
+		drop(dropItemRef);
+	}, [drop, dropItemRef]);
+
+	useEffect(() => {
+		previewRef(getEmptyImage());
+	}, [previewRef]);
+
+	return {
+		isPointerDragging: isDragging,
+		isPointerDropBottomPosition:
+			isOver && dropPosition === DROP_POSITIONS.bottom,
+		isPointerDropTopPosition: isOver && dropPosition === DROP_POSITIONS.top,
+	};
+}
+
+function getDropPosition(
+	ref: React.RefObject<HTMLElement>,
+	monitor: DropTargetMonitor,
+	hoverLimit: number
+) {
+	if (!ref.current) {
+		return '';
+	}
+
+	const clientOffset = monitor.getClientOffset()!;
+	const dropItemBoundingRect = ref.current.getBoundingClientRect();
+	const hoverBottomLimit = dropItemBoundingRect.height - hoverLimit;
+	const hoverClientY = clientOffset.y - dropItemBoundingRect.top;
+
+	return hoverClientY > hoverBottomLimit
+		? DROP_POSITIONS.bottom
+		: DROP_POSITIONS.top;
+}