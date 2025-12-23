8c616f98a255335beac4fbaa3c8eafb91b5dabc2
Brian Chan <brian.chan@liferay.com>
2025-12-23 16:52:18 +0530

    LPD-74255 Avoid calling _getObjectDefinitionId N times

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/bulk/selection/ResetPermissionObjectBulkSelectionAction.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/bulk/selection/ResetPermissionObjectBulkSelectionAction.java
@@ -76,11 +76,11 @@ public class ResetPermissionObjectBulkSelectionAction
 
 			values = objectEntry.getValues();
 
-			long companyId = objectEntry.getCompanyId();
+			long objectDefinitionId = _getObjectDefinitionId(
+				objectEntry.getCompanyId());
 
 			bulkSelection.forEach(
 				object -> {
-					long objectDefinitionId = _getObjectDefinitionId(companyId);
 					String status = "completed";
 
 					try {
