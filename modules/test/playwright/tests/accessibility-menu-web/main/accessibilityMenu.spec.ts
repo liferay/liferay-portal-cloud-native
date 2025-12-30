770f2cea759b70bf0bd72ebb1940061b3405f4f1
Gabriel Lima <gabriel.lima@imogen.com.br>
2025-12-30 14:33:55 -0300

    LPD-74263 Assert accessibility menu persistence

--- modules/test/playwright/tests/accessibility-menu-web/main/accessibilityMenu.spec.ts
+++ modules/test/playwright/tests/accessibility-menu-web/main/accessibilityMenu.spec.ts
@@ -3,24 +3,103 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {expect, mergeTests} from '@playwright/test';
+import {Page, expect, mergeTests} from '@playwright/test';
 
 import {accessibilityMenuPagesTest} from '../../../fixtures/accessibilityMenuPagesTest';
 import {apiHelpersTest} from '../../../fixtures/apiHelpersTest';
 import {instanceSettingsPagesTest} from '../../../fixtures/instanceSettingsPagesTest';
 import {loginTest} from '../../../fixtures/loginTest';
-import {usersAndOrganizationsPagesTest} from '../../../fixtures/usersAndOrganizationsPagesTest';
+import {ApiHelpers} from '../../../helpers/ApiHelpers';
+import {AccessibilityMenuPage} from '../../../pages/accessibility-menu-web/AccessibilityMenuPage';
 import {doAndGoBack} from '../../../utils/doAndGoBack';
-import performLoginViaApi, {performLogout} from '../../../utils/performLogin';
-import {featureFlagPagesTest} from '../../feature-flag-web/main/fixtures/featureFlagPagesTest';
+import {
+	performLoginViaApi,
+	performLogout,
+	userData,
+} from '../../../utils/performLogin';
+
+async function assertUnderlinedLinksValue(page: Page, enabled: boolean) {
+	const body = page.locator('body');
+	const underlinedLinksClass = /c-prefers-link-underline/;
+
+	if (enabled) {
+		await expect(body).toHaveClass(underlinedLinksClass);
+	}
+	else {
+		await expect(body).not.toHaveClass(underlinedLinksClass);
+	}
+}
+
+async function setUnderlinedLinks(
+	accessibilityMenuPage: AccessibilityMenuPage,
+	enabled: boolean
+) {
+	await accessibilityMenuPage.openAccessibilityMenu();
+
+	await accessibilityMenuPage.toggleUnderlinedLinks(enabled);
+
+	await assertUnderlinedLinksValue(accessibilityMenuPage.page, enabled);
+}
+
+interface UsersContext {
+	loginAsUserA(): Promise<void>;
+	loginAsUserB(): Promise<void>;
+}
+
+async function withUsers(
+	apiHelpers: ApiHelpers,
+	handler: (users: UsersContext) => Promise<void>
+) {
+	await performLoginViaApi({
+		page: apiHelpers.page,
+		screenName: 'test',
+	});
+
+	const userA = await apiHelpers.headlessAdminUser.postUserAccount();
+	const userB = await apiHelpers.headlessAdminUser.postUserAccount();
+
+	for (const userAccount of [userA, userB]) {
+		userData[userAccount.alternateName] = {
+			name: userAccount.givenName,
+			password: 'test',
+			surname: userAccount.familyName,
+		};
+	}
+
+	try {
+		await performLogout(apiHelpers.page);
+
+		await handler({
+			loginAsUserA: async () => {
+				await performLoginViaApi({
+					page: apiHelpers.page,
+					screenName: userA.alternateName,
+				});
+			},
+			loginAsUserB: async () => {
+				await performLoginViaApi({
+					page: apiHelpers.page,
+					screenName: userB.alternateName,
+				});
+			},
+		});
+	}
+	finally {
+		await performLoginViaApi({
+			page: apiHelpers.page,
+			screenName: 'test',
+		});
+
+		await apiHelpers.headlessAdminUser.deleteUserAccount(Number(userA.id));
+		await apiHelpers.headlessAdminUser.deleteUserAccount(Number(userB.id));
+	}
+}
 
 const test = mergeTests(
-	accessibilityMenuPagesTest,
 	apiHelpersTest,
-	featureFlagPagesTest,
+	accessibilityMenuPagesTest,
 	instanceSettingsPagesTest,
-	loginTest(),
-	usersAndOrganizationsPagesTest
+	loginTest()
 );
 
 test.beforeEach(async ({accessibilityMenuPage, instanceSettingsPage, page}) => {
@@ -32,9 +111,13 @@ test.beforeEach(async ({accessibilityMenuPage, instanceSettingsPage, page}) => {
 
 		await accessibilityMenuPage.enableAccessibilityMenu();
 	});
+
+	await performLogout(page);
 });
 
-test.afterEach(async ({instanceSettingsPage}) => {
+test.afterEach(async ({instanceSettingsPage, page}) => {
+	await performLoginViaApi({page, screenName: 'test'});
+
 	await instanceSettingsPage.goToInstanceSetting(
 		'Accessibility',
 		'Accessibility Menu'
@@ -79,87 +162,151 @@ test(
 	}
 );
 
-test('Verify that the default value is displayed when the user has never changed the accessibility setting, regardless of the login status', async ({
-	accessibilityMenuPage,
-	apiHelpers,
-	page,
-}) => {
-	const userAccount = await apiHelpers.headlessAdminUser.postUserAccount();
+test(
+	'Accessibility settings for authenticated users persist in the database',
+	{tag: '@LPD-74263'},
+	async ({accessibilityMenuPage, apiHelpers, page}) => {
+		const valuesToTest = [
+			{valueAfterToggle: true, valueBeforeToggle: false},
+			{valueAfterToggle: false, valueBeforeToggle: true},
+		];
 
-	try {
-		await test.step('Verify that the "underlined links" toggle is off when logged out, then turn it on', async () => {
-			await performLogout(page);
+		await withUsers(apiHelpers, async (usersContext) => {
+			for (const {valueAfterToggle, valueBeforeToggle} of valuesToTest) {
+				await test.step(`Set underlined links to ${valueAfterToggle} for User A`, async () => {
+					await usersContext.loginAsUserA();
 
-			await accessibilityMenuPage.openAccessibilityMenu();
+					await assertUnderlinedLinksValue(page, valueBeforeToggle);
 
-			await expect(
-				accessibilityMenuPage.underlinedLinksToggle
-			).not.toBeChecked();
+					await setUnderlinedLinks(
+						accessibilityMenuPage,
+						valueAfterToggle
+					);
+				});
 
-			await accessibilityMenuPage.toggleUnderlinedLinks(true);
-		});
+				await test.step(`Toggle underlined links to ${valueBeforeToggle} as a guest`, async () => {
+					await performLogout(page);
 
-		await test.step('Login as a new user and assert that the "underlined links" guest preference is copied to the user', async () => {
-			await performLoginViaApi(page, 'test');
+					await setUnderlinedLinks(
+						accessibilityMenuPage,
+						valueBeforeToggle
+					);
+				});
 
-			await accessibilityMenuPage.openAccessibilityMenu();
+				await test.step(`Verify underlined links is ${valueAfterToggle} for User A after re-login`, async () => {
+					await page.reload();
 
-			await expect(
-				accessibilityMenuPage.underlinedLinksToggle
-			).toBeChecked();
-		});
+					await usersContext.loginAsUserA();
 
-		await test.step('Verify that the "underlined links" toggle is on when logged out, then turn it off', async () => {
-			await performLogout(page);
-
-			await accessibilityMenuPage.openAccessibilityMenu();
-
-			await expect(
-				accessibilityMenuPage.underlinedLinksToggle
-			).toBeChecked();
-
-			await accessibilityMenuPage.toggleUnderlinedLinks(false);
-		});
-
-		await test.step('Confirm that the "underlined links" toggle is off when logged in, since user did not change the preference', async () => {
-			await performLoginViaApi(page, 'test');
-
-			await accessibilityMenuPage.openAccessibilityMenu();
-
-			await expect(
-				accessibilityMenuPage.underlinedLinksToggle
-			).not.toBeChecked();
-		});
-
-		await test.step('Then, change the "underlined links" preference for that user for the first time', async () => {
-			await accessibilityMenuPage.toggleUnderlinedLinks(true);
-		});
-
-		await test.step('Ensure that the "underlined links" toggle is off after logging out', async () => {
-			await performLogout(page);
-
-			await accessibilityMenuPage.openAccessibilityMenu();
-
-			await expect(
-				accessibilityMenuPage.underlinedLinksToggle
-			).not.toBeChecked();
-		});
-
-		await test.step('Confirm that the "underlined links" toggle is on after logging in', async () => {
-			await performLoginViaApi(page, 'test');
-
-			await accessibilityMenuPage.openAccessibilityMenu();
-
-			await expect(
-				accessibilityMenuPage.underlinedLinksToggle
-			).toBeChecked();
+					await assertUnderlinedLinksValue(page, valueAfterToggle);
+				});
+			}
 		});
 	}
-	finally {
-		await test.step('Delete new user', async () => {
-			await apiHelpers.headlessAdminUser.deleteUserAccount(
-				Number(userAccount.id)
-			);
+);
+
+test(
+	'Guest accessibility settings persist in local storage',
+	{tag: '@LPD-74263'},
+	async ({accessibilityMenuPage, page}) => {
+		const valuesToTest = [
+			{valueAfterToggle: true, valueBeforeToggle: false},
+			{valueAfterToggle: false, valueBeforeToggle: true},
+		];
+
+		for (const {valueAfterToggle, valueBeforeToggle} of valuesToTest) {
+			await test.step(`Set underlined links to ${valueAfterToggle} as a guest and verify it persists after reload`, async () => {
+				await assertUnderlinedLinksValue(page, valueBeforeToggle);
+
+				await setUnderlinedLinks(
+					accessibilityMenuPage,
+					valueAfterToggle
+				);
+
+				await page.reload();
+
+				await assertUnderlinedLinksValue(page, valueAfterToggle);
+			});
+		}
+	}
+);
+
+test(
+	'Guest accessibility settings persist in local storage if unchanged during a signed‑in session',
+	{tag: '@LPD-74263'},
+	async ({accessibilityMenuPage, apiHelpers, page}) => {
+		await withUsers(apiHelpers, async (usersContext) => {
+			await test.step('Turn on underlined links as a guest', async () => {
+				await assertUnderlinedLinksValue(page, false);
+
+				await setUnderlinedLinks(accessibilityMenuPage, true);
+			});
+
+			await test.step('Verify underlined links is enabled for User A (first login)', async () => {
+				await page.reload();
+
+				await usersContext.loginAsUserA();
+
+				await assertUnderlinedLinksValue(page, true);
+			});
+
+			await test.step('Disable underlined links as a guest', async () => {
+				await performLogout(page);
+
+				await assertUnderlinedLinksValue(page, true);
+
+				await setUnderlinedLinks(accessibilityMenuPage, false);
+			});
+
+			await test.step('Verify underlined links is disabled for User B (first login)', async () => {
+				await page.reload();
+
+				await usersContext.loginAsUserB();
+
+				await assertUnderlinedLinksValue(page, false);
+			});
 		});
 	}
-});
+);
+
+test(
+	'Guest accessibility settings extend and persist to other users',
+	{tag: '@LPD-74263'},
+	async ({accessibilityMenuPage, apiHelpers, page}) => {
+		await withUsers(apiHelpers, async (usersContext) => {
+			await test.step('Turn on underlined links as a guest', async () => {
+				await assertUnderlinedLinksValue(page, false);
+
+				await setUnderlinedLinks(accessibilityMenuPage, true);
+			});
+
+			await test.step('First login as User A', async () => {
+				await page.reload();
+
+				await usersContext.loginAsUserA();
+
+				await assertUnderlinedLinksValue(page, true);
+			});
+
+			await test.step('Disable underlined links as a User A', async () => {
+				await page.reload();
+
+				await assertUnderlinedLinksValue(page, true);
+
+				await setUnderlinedLinks(accessibilityMenuPage, false);
+			});
+
+			await test.step('First login as User B', async () => {
+				await performLogout(page);
+
+				await page.reload();
+
+				await assertUnderlinedLinksValue(page, false);
+
+				await usersContext.loginAsUserB();
+
+				await assertUnderlinedLinksValue(page, true);
+			});
+		});
+	}
+);
