6871e0dcf7445d5fdc6a96ad82922d015337ab78
Rafael Praxedes <rafael.praxedes@liferay.com>
2025-12-30 18:53:00 -0300

    LPD-75173 Add mechanism to generate a signed jwtToken that will be used by the AI Hub to make requests on behalf of an user

---
+++ modules/dxp/apps/ai-hub/ai-hub-api/src/main/java/com/liferay/ai/hub/util/JWTTokenUtil.java
@@ -0,0 +1,119 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.hub.util;
+
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.SecureRandomUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+import java.util.Date;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class JWTTokenUtil {
+
+	public static String generateToken(
+		long companyId, long expirationTime, String issuer, long userId) {
+
+		Date now = new Date();
+
+		JWTClaimsSet claimsSet = new JWTClaimsSet.Builder(
+		).claim(
+			"companyId", companyId
+		).expirationTime(
+			new Date(now.getTime() + expirationTime)
+		).issuer(
+			issuer
+		).issueTime(
+			now
+		).subject(
+			String.valueOf(userId)
+		).build();
+
+		SignedJWT signedJWT = new SignedJWT(
+			new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+
+		try {
+			JWSSigner jwsSigner = new MACSigner(_SECRET);
+
+			signedJWT.sign(jwsSigner);
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			throw new SystemException("Unable to generate a signed token");
+		}
+
+		return signedJWT.serialize();
+	}
+
+	public static long getUserId(String token) {
+		JWTClaimsSet jwtClaimsSet = null;
+
+		try {
+			SignedJWT signedJWT = SignedJWT.parse(token);
+
+			JWSVerifier jwsVerifier = new MACVerifier(_SECRET);
+
+			if (!signedJWT.verify(jwsVerifier)) {
+				throw new SystemException("Invalid JWT signature");
+			}
+
+			jwtClaimsSet = signedJWT.getJWTClaimsSet();
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			if (exception instanceof SystemException) {
+				throw (SystemException)exception;
+			}
+
+			throw new SystemException(
+				"Unable to parse and verify the JWT token", exception);
+		}
+
+		Date expirationTime = jwtClaimsSet.getExpirationTime();
+
+		if ((expirationTime == null) || expirationTime.before(new Date())) {
+			throw new SystemException("The JWT token has been expired");
+		}
+
+		return GetterUtil.getLong(jwtClaimsSet.getSubject());
+	}
+
+	private static final byte[] _SECRET;
+
+	private static final Log _log = LogFactoryUtil.getLog(JWTTokenUtil.class);
+
+	static {
+		int sha256BlockSize = 64;
+
+		byte[] secret = new byte[sha256BlockSize];
+
+		for (int i = 0; i < secret.length; i++) {
+			secret[i] = SecureRandomUtil.nextByte();
+		}
+
+		_SECRET = secret;
+	}
+
+}
---
+++ modules/dxp/apps/ai-hub/ai-hub-api/src/test/java/com/liferay/ai/hub/util/JWTTokenUtilTest.java
@@ -0,0 +1,114 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.hub.util;
+
+import com.liferay.portal.kernel.security.SecureRandomUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/**
+ * @author Rafael Praxedes
+ */
+public class JWTTokenUtilTest {
+
+	@ClassRule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testGenerateToken() throws Exception {
+		String token = JWTTokenUtil.generateToken(
+			_COMPANY_ID, TimeUnit.MINUTES.toMillis(1), _ISSUER, _USER_ID);
+
+		Assert.assertNotNull(token);
+		Assert.assertFalse(token.isEmpty());
+
+		SignedJWT signedJWT = SignedJWT.parse(token);
+
+		JWTClaimsSet jwtClaimsSet = signedJWT.getJWTClaimsSet();
+
+		Assert.assertEquals(_COMPANY_ID, jwtClaimsSet.getClaim("companyId"));
+		Assert.assertEquals(
+			String.valueOf(_USER_ID), jwtClaimsSet.getSubject());
+		Assert.assertEquals(_ISSUER, jwtClaimsSet.getIssuer());
+	}
+
+	@Test
+	public void testGetUserId() {
+		String token = JWTTokenUtil.generateToken(
+			_COMPANY_ID, TimeUnit.MINUTES.toMillis(1), _ISSUER, _USER_ID);
+
+		Assert.assertEquals(_USER_ID, JWTTokenUtil.getUserId(token));
+	}
+
+	@Test
+	public void testGetUserIdWithInvalidToken() throws Exception {
+		String token = JWTTokenUtil.generateToken(
+			_COMPANY_ID, TimeUnit.MINUTES.toMillis(1), _ISSUER, _USER_ID);
+
+		try (AutoCloseable autoCloseable =
+				ReflectionTestUtil.setFieldValueWithAutoCloseable(
+					JWTTokenUtil.class, "_SECRET", _SECRET)) {
+
+			_testGetUserIdWithInvalidToken("Invalid JWT signature", token);
+		}
+
+		_testGetUserIdWithInvalidToken(
+			"Invalid JWT signature",
+			token.substring(0, token.length() - 5) + "abcde");
+		_testGetUserIdWithInvalidToken(
+			"The JWT token has been expired",
+			JWTTokenUtil.generateToken(_COMPANY_ID, 0, _ISSUER, _USER_ID));
+		_testGetUserIdWithInvalidToken(
+			"Unable to parse and verify the JWT token",
+			RandomTestUtil.randomString());
+	}
+
+	private void _testGetUserIdWithInvalidToken(
+		String expectedExceptionMessage, String token) {
+
+		try {
+			JWTTokenUtil.getUserId(token);
+
+			Assert.fail();
+		}
+		catch (Exception exception) {
+			Assert.assertEquals(
+				expectedExceptionMessage, exception.getMessage());
+		}
+	}
+
+	private static final long _COMPANY_ID = RandomTestUtil.randomLong();
+
+	private static final String _ISSUER = RandomTestUtil.randomString();
+
+	private static final byte[] _SECRET;
+
+	private static final long _USER_ID = RandomTestUtil.randomLong();
+
+	static {
+		int sha256BlockSize = 64;
+
+		byte[] secret = new byte[sha256BlockSize];
+
+		for (int i = 0; i < secret.length; i++) {
+			secret[i] = SecureRandomUtil.nextByte();
+		}
+
+		_SECRET = secret;
+	}
+
+}
