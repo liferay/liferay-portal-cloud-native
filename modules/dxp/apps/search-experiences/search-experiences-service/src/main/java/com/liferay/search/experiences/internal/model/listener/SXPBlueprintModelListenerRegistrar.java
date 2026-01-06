11737c3ff42f2eb20102c330b583376c1cc5109a
Olivia Yu <olivia.yu@liferay.com>
2026-01-06 11:38:22 -0800

    LPD-72790 LPD-72791 search-experiences-service: Removal FF LPS-129412 (refactor feature flag listener)

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/model/listener/SXPBlueprintModelListenerRegistrar.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/model/listener/SXPBlueprintModelListenerRegistrar.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.search.experiences.internal.feature.flag;
+package com.liferay.search.experiences.internal.model.listener;
 
 import com.liferay.asset.util.AssetHelper;
 import com.liferay.blogs.service.BlogsEntryLocalService;
@@ -15,7 +15,6 @@ import com.liferay.journal.service.JournalArticleService;
 import com.liferay.knowledge.base.service.KBArticleLocalService;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
-import com.liferay.portal.kernel.feature.flag.FeatureFlagListener;
 import com.liferay.portal.kernel.model.ModelListener;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.CompanyLocalService;
@@ -23,84 +22,51 @@ import com.liferay.portal.kernel.service.GroupService;
 import com.liferay.portal.search.asset.AssetSubtypeIdentifierBuilder;
 import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
 import com.liferay.portal.search.searcher.Searcher;
-import com.liferay.search.experiences.internal.model.listener.InfoCollectionProviderSXPBlueprintModelListener;
-import com.liferay.search.experiences.internal.model.listener.SXPBlueprintInfoCollectionProviderSXPBlueprintModelListener;
 import com.liferay.search.experiences.service.SXPBlueprintLocalService;
-
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Shuyang Zhou
  */
-@Component(
-	enabled = false, property = "feature.flag.key=LPS-129412",
-	service = FeatureFlagListener.class
-)
-public class SXPBlueprintFeatureFlagListener implements FeatureFlagListener {
-
-	@Override
-	public void onValue(
-		long companyId, String featureFlagKey, boolean enabled) {
-
-		Map.Entry
-			<InfoCollectionProviderSXPBlueprintModelListener,
-			 ServiceRegistration<?>> entry = null;
-
-		if (enabled && Objects.equals(featureFlagKey, "LPS-129412")) {
-			InfoCollectionProviderSXPBlueprintModelListener
-				infoCollectionProviderSXPBlueprintModelListener =
-					new SXPBlueprintInfoCollectionProviderSXPBlueprintModelListener(
-						_assetHelper, _assetSubtypeIdentifierBuilder,
-						_blogsEntryLocalService, _bundleContext,
-						_calendarBookingLocalService, _classNameLocalService,
-						_companyLocalService, _ddmStructureService,
-						_dlFileEntryTypeLocalService, _dlAppLocalService,
-						_groupService, _journalArticleService,
-						_kbArticleLocalService, _objectDefinitionLocalService,
-						_objectEntryLocalService, _searcher,
-						_searchRequestBuilderFactory,
-						_sxpBlueprintLocalService);
-
-			infoCollectionProviderSXPBlueprintModelListener.start();
-
-			entry = _serviceRegistrationEntries.put(
-				featureFlagKey,
-				new AbstractMap.SimpleImmutableEntry<>(
-					infoCollectionProviderSXPBlueprintModelListener,
-					_bundleContext.registerService(
-						ModelListener.class,
-						infoCollectionProviderSXPBlueprintModelListener,
-						null)));
-		}
-		else {
-			entry = _serviceRegistrationEntries.remove(featureFlagKey);
-		}
-
-		if (entry != null) {
-			ServiceRegistration<?> serviceRegistration = entry.getValue();
-
-			serviceRegistration.unregister();
-
-			InfoCollectionProviderSXPBlueprintModelListener
-				infoCollectionProviderSXPBlueprintModelListener =
-					entry.getKey();
-
-			infoCollectionProviderSXPBlueprintModelListener.stop();
-		}
-	}
+@Component(enabled = false, service = {})
+public class SXPBlueprintModelListenerRegistrar {
 
 	@Activate
 	protected void activate(BundleContext bundleContext) {
-		_bundleContext = bundleContext;
+		_infoCollectionProviderSXPBlueprintModelListener =
+			new SXPBlueprintInfoCollectionProviderSXPBlueprintModelListener(
+				_assetHelper, _assetSubtypeIdentifierBuilder,
+				_blogsEntryLocalService, bundleContext,
+				_calendarBookingLocalService, _classNameLocalService,
+				_companyLocalService, _ddmStructureService,
+				_dlFileEntryTypeLocalService, _dlAppLocalService, _groupService,
+				_journalArticleService, _kbArticleLocalService,
+				_objectDefinitionLocalService, _objectEntryLocalService,
+				_searcher, _searchRequestBuilderFactory,
+				_sxpBlueprintLocalService);
+
+		_infoCollectionProviderSXPBlueprintModelListener.start();
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelListener.class,
+			_infoCollectionProviderSXPBlueprintModelListener, null);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		if (_serviceRegistration != null) {
+			_serviceRegistration.unregister();
+		}
+
+		if (_infoCollectionProviderSXPBlueprintModelListener != null) {
+			_infoCollectionProviderSXPBlueprintModelListener.stop();
+		}
 	}
 
 	@Reference
@@ -111,8 +77,6 @@ public class SXPBlueprintFeatureFlagListener implements FeatureFlagListener {
 
 	@Reference
 	private BlogsEntryLocalService _blogsEntryLocalService;
-
-	private BundleContext _bundleContext;
 
 	@Reference
 	private CalendarBookingLocalService _calendarBookingLocalService;
@@ -135,6 +99,9 @@ public class SXPBlueprintFeatureFlagListener implements FeatureFlagListener {
 	@Reference
 	private GroupService _groupService;
 
+	private InfoCollectionProviderSXPBlueprintModelListener
+		_infoCollectionProviderSXPBlueprintModelListener;
+
 	@Reference
 	private JournalArticleService _journalArticleService;
 
@@ -153,12 +120,7 @@ public class SXPBlueprintFeatureFlagListener implements FeatureFlagListener {
 	@Reference
 	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
 
-	private final Map
-		<String,
-		 Map.Entry
-			 <InfoCollectionProviderSXPBlueprintModelListener,
-			  ServiceRegistration<?>>> _serviceRegistrationEntries =
-				new ConcurrentHashMap<>();
+	private ServiceRegistration<?> _serviceRegistration;
 
 	@Reference
 	private SXPBlueprintLocalService _sxpBlueprintLocalService;
