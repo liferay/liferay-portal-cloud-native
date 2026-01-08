cb923f3c74d8d7ededca75d4b85fbaf3b5053e08
Brian Chan <brian.chan@liferay.com>
2026-01-08 14:04:33 +0800

    LPD-67475 Sort

--- modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/dynamic/registration/LiferayDynamicRegistrationService.java
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/dynamic/registration/LiferayDynamicRegistrationService.java
@@ -171,6 +171,8 @@ public class LiferayDynamicRegistrationService
 			new LiferayClientRegistrationResponse();
 
 		liferayClientRegistrationResponse.setClientId(client.getClientId());
+		liferayClientRegistrationResponse.setClientIdIssuedAt(
+			client.getRegisteredAt());
 
 		if (Validator.isNotNull(client.getApplicationName())) {
 			liferayClientRegistrationResponse.setClientName(
@@ -183,8 +185,6 @@ public class LiferayDynamicRegistrationService
 			liferayClientRegistrationResponse.setClientSecretExpiresAt(0L);
 		}
 
-		liferayClientRegistrationResponse.setClientIdIssuedAt(
-			client.getRegisteredAt());
 		liferayClientRegistrationResponse.setGrantTypes(
 			client.getAllowedGrantTypes());
 		liferayClientRegistrationResponse.setLogoUri(
