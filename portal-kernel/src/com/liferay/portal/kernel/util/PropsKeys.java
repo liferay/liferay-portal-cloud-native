afdd1bf3a94725f03437bc5a98f071c151f0952f
Petteri Karttunen <petteri.karttunen@liferay.com>
2026-01-08 11:27:37 +0200

    LPD-75526 portal-kernel: Make final static

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -2964,6 +2964,6 @@ public interface PropsKeys {
 	public static final String ZIP_FILE_WRITER_EXPORT_BUFFER_SIZE =
 		"zip.file.writer.export.buffer.size";
 
-	public String ZIP_FILE_READER_NIO_ENABLED = "zip.file.reader.nio.enabled";
+	public static final String ZIP_FILE_READER_NIO_ENABLED = "zip.file.reader.nio.enabled";
 
 }
