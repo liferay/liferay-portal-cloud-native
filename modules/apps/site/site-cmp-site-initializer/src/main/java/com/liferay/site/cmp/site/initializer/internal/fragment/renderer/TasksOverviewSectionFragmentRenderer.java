11a051ff2bc28f8ab87352335e75a0979bdaceb0
Kevin Tan <kevin.tan@liferay.com>
2026-01-13 15:55:01 -0800

    LPD-75958 Fetch task state counts

--- modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/fragment/renderer/TasksOverviewSectionFragmentRenderer.java
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/fragment/renderer/TasksOverviewSectionFragmentRenderer.java
@@ -8,20 +8,26 @@ package com.liferay.site.cmp.site.initializer.internal.fragment.renderer;
 import com.liferay.fragment.renderer.FragmentRenderer;
 import com.liferay.fragment.renderer.FragmentRendererContext;
 import com.liferay.info.constants.InfoDisplayWebKeys;
+import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.rest.filter.factory.FilterFactory;
 import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.petra.sql.dsl.expression.Predicate;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+
+import java.time.Instant;
 
 import java.util.Map;
 import java.util.Objects;
@@ -88,31 +94,65 @@ public class TasksOverviewSectionFragmentRenderer
 
 		ObjectEntry objectEntry = (ObjectEntry)object;
 
-		ObjectDefinition objectDefinition =
-			_objectDefinitionLocalService.fetchObjectDefinition(
-				objectEntry.getObjectDefinitionId());
-
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)httpServletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
 
+		ObjectDefinition taskObjectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_CMP_TASK", themeDisplay.getCompanyId());
+
 		return HashMapBuilder.<String, Object>put(
-			"blockedCount", 0
+			"blockedCount",
+			() -> _getCount(objectEntry, taskObjectDefinition, "blocked")
 		).put(
-			"doneCount", 5
+			"doneCount",
+			() -> _getCount(objectEntry, taskObjectDefinition, "done")
 		).put(
-			"inProgressCount", 0
+			"inProgressCount",
+			() -> _getCount(objectEntry, taskObjectDefinition, "inProgress")
 		).put(
-			"overdueCount", 0
+			"overdueCount",
+			() -> _getCount(objectEntry, taskObjectDefinition, "overdue")
 		).put(
-			"totalCount", 50
+			"totalCount",
+			() -> _getCount(objectEntry, taskObjectDefinition, null)
 		).build();
 	}
+
+	private int _getCount(
+			ObjectEntry projectObjectEntry,
+			ObjectDefinition taskObjectDefinition, String state)
+		throws Exception {
+
+		StringBundler filterStringBundler = new StringBundler(1);
+
+		if (Objects.equals(state, "overdue")) {
+			filterStringBundler.append(
+				"dueDate lt " + Instant.now() + " and state ne 'done'");
+		}
+		else if (state != null) {
+			filterStringBundler.append("state eq '" + state + "'");
+		}
+
+		return _objectEntryLocalService.getValuesListCount(
+			new Long[] {projectObjectEntry.getGroupId()}, 0, 0,
+			taskObjectDefinition.getObjectDefinitionId(),
+			_filterFactory.create(
+				filterStringBundler.toString(), taskObjectDefinition),
+			false, null);
+	}
+
+	@Reference(
+		target = "(filter.factory.key=" + ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT + ")"
+	)
+	private FilterFactory<Predicate> _filterFactory;
 
 	@Reference
 	private ObjectDefinitionLocalService _objectDefinitionLocalService;
 
 	@Reference
-	private Portal _portal;
+	private ObjectEntryLocalService _objectEntryLocalService;
 
 }
