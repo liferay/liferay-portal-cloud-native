81e7ede4e536f133c5180d588cd9ffd3c5d3611e
Gabriel Lima <gabriel.lima@liferay.com>
2026-01-14 13:21:42 -0300

    LPD-66976 Assert a warning appears before deleting a style book

--- modules/test/playwright/tests/style-book-web/main/styleBookAdmin.spec.ts
+++ modules/test/playwright/tests/style-book-web/main/styleBookAdmin.spec.ts
@@ -8,7 +8,9 @@ import {Locator, expect, mergeTests} from '@playwright/test';
 import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../fixtures/loginTest';
 import {styleBookPageTest} from '../../../fixtures/styleBookPageTest';
+import {clickAndExpectToBeVisible} from '../../../utils/clickAndExpectToBeVisible';
 import getRandomString from '../../../utils/getRandomString';
+import {waitForAlert} from '../../../utils/waitForAlert';
 
 const test = mergeTests(isolatedSiteTest, loginTest(), styleBookPageTest);
 
@@ -90,6 +92,58 @@ test(
 			await styleBooksPage.markAsDefault(styleBookName);
 
 			await expect(sticker).toBeVisible();
+		});
+	}
+);
+
+test(
+	'A warning message appears when deleting a style book',
+	{tag: '@LPD-66976'},
+	async ({page, site, styleBooksPage}) => {
+		const styleBookName = getRandomString();
+
+		await test.step('Create a new style book', async () => {
+			await styleBooksPage.create(styleBookName);
+
+			await styleBooksPage.goto(site.friendlyUrlPath);
+		});
+
+		const card = styleBooksPage.getStyleBookCard(styleBookName);
+
+		async function expectWarningAndClickOnAction(name: string) {
+			await clickAndExpectToBeVisible({
+				autoClick: true,
+				target: page.getByRole('menuitem', {name: 'Delete'}),
+				trigger: card.getByLabel('More actions'),
+			});
+
+			const dialog = page.getByRole('dialog');
+
+			await expect(
+				dialog.getByRole('heading', {name: 'Delete Style Book'})
+			).toBeVisible();
+
+			await expect(
+				dialog.getByText(
+					"Deleting a style book is an action impossible to revert. All Style Book tokens and values will be removed and it will not be possible to recover it. Watch out for a critical impact on the site's look and feel."
+				)
+			).toBeVisible();
+
+			await dialog.getByRole('button', {name}).click();
+		}
+
+		await test.step('The warning message appears before deleting the style book and can be dismissed', async () => {
+			await expectWarningAndClickOnAction('Cancel');
+
+			await expect(card).toBeVisible();
+		});
+
+		await test.step('The style book is deleted when confirming the action', async () => {
+			await expectWarningAndClickOnAction('Delete');
+
+			await waitForAlert(page);
+
+			await expect(card).toBeHidden();
 		});
 	}
 );
