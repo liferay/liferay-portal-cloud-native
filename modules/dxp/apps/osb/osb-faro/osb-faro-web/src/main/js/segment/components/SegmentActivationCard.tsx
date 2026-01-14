03a35eb67adbb0303c46a916eb9d546f4e172b0a
Tiago Campêlo <tiago.campelo@liferay.com>
2026-01-14 14:28:02 -0300

    LPD-76089 Get data from segment property

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/components/SegmentActivationCard.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/components/SegmentActivationCard.tsx
@@ -2,14 +2,14 @@ import Card from 'shared/components/Card';
 import Label from '@clayui/label';
 import List from '@clayui/list';
 import React from 'react';
-import {ReportContainer} from 'shared/components/download-report/DownloadPDFReport';
 import {Segment} from 'shared/util/records';
 
 interface ISegmentActivationCardProps {
 	segment?: Segment;
 }
 
-type mock = {
+export type SegmentActivationDetails = {
+	segmentActivationId: string;
 	title: string;
 	description: string;
 	scheduleType: 'BETWEEN' | 'INDEFINITELY';
@@ -18,33 +18,33 @@ type mock = {
 	frequencyType: 'BATCH' | 'REAL_TIME';
 };
 
-const data: mock = {
+const data: SegmentActivationDetails = {
 	description:
 		'Syncs individual profiles to Liferay DXP to deliver personalization via pages, collections, A/B tests, and recommendations.',
 	frequencyType: 'BATCH',
 	scheduleEndDate: '',
 	scheduleStartDate: '',
 	scheduleType: 'INDEFINITELY',
+	segmentActivationId: '1',
 	title: 'Liferay DXP'
 };
 
 const SegmentActivationCard: React.FC<ISegmentActivationCardProps> = ({
-	segment = null
+	segment
 }) => {
-	const {description, frequencyType, scheduleType, title} = data;
+	// Use mocked data
+	const {description, frequencyType, scheduleType, title} =
+		segment.activationStatus || data;
 
 	return (
-		<Card
-			className='segment-membership-root'
-			reportContainer={ReportContainer.SegmentMembershipCard}
-		>
+		<Card className='segment-membership-root'>
 			<Card.Header className='align-items-center d-flex justify-content-between'>
-				<Card.Title>{'Activations'}</Card.Title>
+				<Card.Title>{Liferay.Language.get('activations')}</Card.Title>
 			</Card.Header>
 			<Card.Body>
 				<List showQuickActionsOnHover>
 					<List.Item className='px-2' flex>
-						<List.ItemField className='d-flex' expand>
+						<List.ItemField expand>
 							<List.ItemTitle className='mb-2'>
 								{title}
 							</List.ItemTitle>
@@ -62,7 +62,6 @@ const SegmentActivationCard: React.FC<ISegmentActivationCardProps> = ({
 							<List.QuickActionMenu>
 								<List.QuickActionMenu.Item
 									aria-label={Liferay.Language.get('edit')}
-									onClick={() => alert('Clicked the pencil!')}
 									symbol='pencil'
 									title={Liferay.Language.get('edit')}
 								/>
