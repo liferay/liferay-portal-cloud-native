363cd6ffde414354027c18db9d2890f9efa4013b
Daniel Raposo <daniel.raposo@liferay.com>
2026-01-15 12:55:00 +0100

    LPD-75638 This checks the portlets by default, although the main.js will still uncheck the empty ones

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandler.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/data/handler/BatchEnginePortletDataHandler.java
@@ -251,6 +251,7 @@ public class BatchEnginePortletDataHandler extends BasePortletDataHandler {
 			setDataLevel(DataLevel.SITE);
 		}
 
+		setPublishToLiveByDefault(true);
 		_updateDeletionSystemEventStagedModelTypes();
 		_updateExportPortletDataHandlerControls();
 	}
