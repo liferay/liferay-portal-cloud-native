e780a3b092c5de2704ccad44b1d40976bfa725b0
Brian Chan <brian.chan@liferay.com>
2026-01-16 15:34:03 +0900

    LPD-72292 New pattern

--- modules/apps/launch/launch-api/src/main/java/com/liferay/launch/exception/NoSuchLaunchEntryException.java
+++ modules/apps/launch/launch-api/src/main/java/com/liferay/launch/exception/NoSuchLaunchEntryException.java
@@ -2,7 +2,6 @@
  * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
-
 package com.liferay.launch.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -10,20 +9,20 @@ import com.liferay.portal.kernel.exception.NoSuchModelException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class NoSuchSetException extends NoSuchModelException {
+public class NoSuchLaunchEntryException extends NoSuchModelException {
 
-	public NoSuchSetException() {
+	public NoSuchLaunchEntryException() {
 	}
 
-	public NoSuchSetException(String msg) {
+	public NoSuchLaunchEntryException(String msg) {
 		super(msg);
 	}
 
-	public NoSuchSetException(String msg, Throwable throwable) {
+	public NoSuchLaunchEntryException(String msg, Throwable throwable) {
 		super(msg, throwable);
 	}
 
-	public NoSuchSetException(Throwable throwable) {
+	public NoSuchLaunchEntryException(Throwable throwable) {
 		super(throwable);
 	}
 
--- modules/apps/launch/launch-api/src/main/java/com/liferay/launch/exception/NoSuchLaunchSetException.java
+++ modules/apps/launch/launch-api/src/main/java/com/liferay/launch/exception/NoSuchLaunchSetException.java
@@ -2,7 +2,6 @@
  * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
-
 package com.liferay.launch.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -10,20 +9,20 @@ import com.liferay.portal.kernel.exception.NoSuchModelException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class NoSuchEntryException extends NoSuchModelException {
+public class NoSuchLaunchSetException extends NoSuchModelException {
 
-	public NoSuchEntryException() {
+	public NoSuchLaunchSetException() {
 	}
 
-	public NoSuchEntryException(String msg) {
+	public NoSuchLaunchSetException(String msg) {
 		super(msg);
 	}
 
-	public NoSuchEntryException(String msg, Throwable throwable) {
+	public NoSuchLaunchSetException(String msg, Throwable throwable) {
 		super(msg, throwable);
 	}
 
-	public NoSuchEntryException(Throwable throwable) {
+	public NoSuchLaunchSetException(Throwable throwable) {
 		super(throwable);
 	}
 
