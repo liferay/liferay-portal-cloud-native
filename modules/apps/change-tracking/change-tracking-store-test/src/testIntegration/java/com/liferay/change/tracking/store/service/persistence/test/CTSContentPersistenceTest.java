liferay-site-management/liferay-portal

LPD-76300 build service

modules/apps/change-tracking/change-tracking-store-test/src/testIntegration/java/com/liferay/change/tracking/store/service/persistence/test/CTSContentPersistenceTest.java
@@ -180,6 +180,15 @@ public class CTSContentPersistenceTest {
 	}
 
 	@Test
+	public void testCountByR_LikeP() throws Exception {
+		_persistence.countByR_LikeP(RandomTestUtil.nextLong(), "");
+
+		_persistence.countByR_LikeP(0L, "null");
+
+		_persistence.countByR_LikeP(0L, (String)null);
+	}
+
+	@Test
 	public void testCountByC_R_S() throws Exception {
 		_persistence.countByC_R_S(
 			RandomTestUtil.nextLong(), RandomTestUtil.nextLong(), "");

liferay/liferay-portal-cloud-native

modules/apps/change-tracking/change-tracking-store-test/src/testIntegration/java/com/liferay/change/tracking/store/service/persistence/test/CTSContentPersistenceTest.java
@@ -180,6 +180,15 @@ public class CTSContentPersistenceTest {
 	}
 
 	@Test
+	public void testCountByR_P() throws Exception {
+		_persistence.countByR_P(RandomTestUtil.nextLong(), "");
+
+		_persistence.countByR_P(0L, "null");
+
+		_persistence.countByR_P(0L, (String)null);
+	}
+
+	@Test
 	public void testCountByC_R_S() throws Exception {
 		_persistence.countByC_R_S(
 			RandomTestUtil.nextLong(), RandomTestUtil.nextLong(), "");