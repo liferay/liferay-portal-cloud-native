liferay-content-management/liferay-portal

LPD-73461 Add group relationship on PUT when feature flag is enabled

modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/resource/v1_0/KeywordResourceImpl.java
@@ -282,22 +282,8 @@ public class KeywordResourceImpl
 	public Keyword postSiteKeyword(Long siteId, Keyword keyword)
 		throws Exception {
 
-		AssetTag assetTag = _assetTagService.addTag(
-			keyword.getExternalReferenceCode(), siteId, keyword.getName(),
-			new ServiceContext());
-
-		Group group = _groupLocalService.getGroup(siteId);
-
-		if (FeatureFlagManagerUtil.isEnabled("LPD-17564") && group.isCMS() &&
-			ArrayUtil.isNotEmpty(keyword.getAssetLibraries())) {
-
-			_assetTagGroupRelLocalService.setAssetTagGroupRels(
-				assetTag.getTagId(),
-				TaxonomyGroupUtil.getAssetLibraryGroupIds(
-					keyword.getAssetLibraries()));
-		}
-
-		return _toKeyword(assetTag);
+		return _postSiteKeyword(
+			keyword.getExternalReferenceCode(), siteId, keyword);
 	}
 
 	@Override
@@ -316,10 +302,7 @@ public class KeywordResourceImpl
 					keyword.getName(), null));
 		}
 
-		return _toKeyword(
-			_assetTagService.addTag(
-				externalReferenceCode, assetLibraryId, keyword.getName(),
-				new ServiceContext()));
+		return _postSiteKeyword(externalReferenceCode, assetLibraryId, keyword);
 	}
 
 	@Override
@@ -390,10 +373,7 @@ public class KeywordResourceImpl
 					keyword.getName(), null));
 		}
 
-		return _toKeyword(
-			_assetTagService.addTag(
-				externalReferenceCode, siteId, keyword.getName(),
-				new ServiceContext()));
+		return _postSiteKeyword(externalReferenceCode, siteId, keyword);
 	}
 
 	@Override
@@ -496,6 +476,28 @@ public class KeywordResourceImpl
 		}
 
 		return _assetTagLocalService.dynamicQueryCount(dynamicQuery);
+	}
+
+	private Keyword _postSiteKeyword(
+			String externalReferenceCode, Long siteId, Keyword keyword)
+		throws Exception {
+
+		AssetTag assetTag = _assetTagService.addTag(
+			externalReferenceCode, siteId, keyword.getName(),
+			new ServiceContext());
+
+		Group group = _groupLocalService.getGroup(siteId);
+
+		if (FeatureFlagManagerUtil.isEnabled("LPD-17564") && group.isCMS() &&
+			ArrayUtil.isNotEmpty(keyword.getAssetLibraries())) {
+
+			_assetTagGroupRelLocalService.setAssetTagGroupRels(
+				assetTag.getTagId(),
+				TaxonomyGroupUtil.getAssetLibraryGroupIds(
+					keyword.getAssetLibraries()));
+		}
+
+		return _toKeyword(assetTag);
 	}
 
 	private AssetTag _toAssetTag(Object[] assetTags) {

liferay/liferay-portal-cloud-native

modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/resource/v1_0/KeywordResourceImpl.java
@@ -287,22 +287,8 @@ public class KeywordResourceImpl
 	public Keyword postSiteKeyword(Long siteId, Keyword keyword)
 		throws Exception {
 
-		AssetTag assetTag = _assetTagService.addTag(
-			keyword.getExternalReferenceCode(), siteId, keyword.getName(),
-			new ServiceContext());
-
-		Group group = _groupLocalService.getGroup(siteId);
-
-		if (FeatureFlagManagerUtil.isEnabled("LPD-17564") && group.isCMS() &&
-			ArrayUtil.isNotEmpty(keyword.getAssetLibraries())) {
-
-			_assetTagGroupRelLocalService.setAssetTagGroupRels(
-				assetTag.getTagId(),
-				TaxonomyGroupUtil.getAssetLibraryGroupIds(
-					keyword.getAssetLibraries()));
-		}
-
-		return _toKeyword(assetTag);
+		return _postSiteKeyword(
+			keyword.getExternalReferenceCode(), keyword, siteId);
 	}
 
 	@Override
@@ -321,10 +307,7 @@ public class KeywordResourceImpl
 					keyword.getName(), null));
 		}
 
-		return _toKeyword(
-			_assetTagService.addTag(
-				externalReferenceCode, assetLibraryId, keyword.getName(),
-				new ServiceContext()));
+		return _postSiteKeyword(externalReferenceCode, keyword, assetLibraryId);
 	}
 
 	@Override
@@ -395,10 +378,7 @@ public class KeywordResourceImpl
 					keyword.getName(), null));
 		}
 
-		return _toKeyword(
-			_assetTagService.addTag(
-				externalReferenceCode, siteId, keyword.getName(),
-				new ServiceContext()));
+		return _postSiteKeyword(externalReferenceCode, keyword, siteId);
 	}
 
 	@Override
@@ -501,6 +481,28 @@ public class KeywordResourceImpl
 		}
 
 		return _assetTagLocalService.dynamicQueryCount(dynamicQuery);
+	}
+
+	private Keyword _postSiteKeyword(
+			String externalReferenceCode, Keyword keyword, Long siteId)
+		throws Exception {
+
+		AssetTag assetTag = _assetTagService.addTag(
+			externalReferenceCode, siteId, keyword.getName(),
+			new ServiceContext());
+
+		Group group = _groupLocalService.getGroup(siteId);
+
+		if (FeatureFlagManagerUtil.isEnabled("LPD-17564") && group.isCMS() &&
+			ArrayUtil.isNotEmpty(keyword.getAssetLibraries())) {
+
+			_assetTagGroupRelLocalService.setAssetTagGroupRels(
+				assetTag.getTagId(),
+				TaxonomyGroupUtil.getAssetLibraryGroupIds(
+					keyword.getAssetLibraries()));
+		}
+
+		return _toKeyword(assetTag);
 	}
 
 	private AssetTag _toAssetTag(Object[] assetTags) {