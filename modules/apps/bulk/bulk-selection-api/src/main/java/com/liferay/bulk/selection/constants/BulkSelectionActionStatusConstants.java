2736f11ad9b380b3e26637b3f746bc1b55db3ab1
balazssk <balazs.safrany-kovalik@liferay.com>
2026-01-19 09:47:27 +0100

    LPD-76554 Move to bulk-selection-api
    # breaking
    
    ## What modules/apps/site/site-cms-site-initializer-api/src/main/java/com/liferay/site/cms/site/initializer/constants/BulkActionExecutionStatusConstants.java
    
    Moved to modules/apps/bulk/bulk-selection-api/src/main/java/com/liferay/bulk/selection/constants/BulkSelectionActionStatusConstants.java
    
    ## Why
    
    Reusability. These constants are used in other modules that should not depend on site-cms-site-initializer-api.
    
    ----

--- modules/apps/bulk/bulk-selection-api/src/main/java/com/liferay/bulk/selection/constants/BulkSelectionActionStatusConstants.java
+++ modules/apps/bulk/bulk-selection-api/src/main/java/com/liferay/bulk/selection/constants/BulkSelectionActionStatusConstants.java
@@ -3,12 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.site.cms.site.initializer.constants;
+package com.liferay.bulk.selection.constants;
 
 /**
  * @author Crescenzo Rega
  */
-public class BulkActionExecutionStatusConstants {
+public class BulkSelectionActionStatusConstants {
 
 	public static final String COMPLETED = "completed";
 
