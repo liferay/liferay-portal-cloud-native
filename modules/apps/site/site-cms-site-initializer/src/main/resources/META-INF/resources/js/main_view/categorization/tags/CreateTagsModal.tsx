liferay-content-management/liferay-portal

LPD-73461 Pass scopeKey in Response when creating asset vocabularies and tags

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/categorization/tags/CreateTagsModal.tsx
@@ -39,7 +39,7 @@ export default function CreateTagsModalContent({
 	invalidTagCharacters: string;
 }) {
 	const [nameInputError, setNameInputError] = useState<string>('');
-	const [selectedSpaces, setSelectedSpaces] = useState<number[]>([-1]);
+	const [selectedSpaces, setSelectedSpaces] = useState<string[]>([]);
 	const [spaceInputError, setSpaceInputError] = useState('');
 	const [close, setClose] = useState(false);
 
@@ -60,8 +60,8 @@ export default function CreateTagsModalContent({
 		onSubmit: (values) => {
 			const url = `/o/headless-admin-taxonomy/v1.0/sites/${cmsGroupId}/keywords`;
 			const body = {
-				assetLibraries: selectedSpaces.map((number) => ({
-					id: number,
+				assetLibraries: selectedSpaces.map((scopeKey) => ({
+					scopeKey,
 				})),
 				name: values.tagName,
 			};

liferay/liferay-portal-cloud-native

modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/categorization/tags/CreateTagsModal.tsx
@@ -39,7 +39,7 @@ export default function CreateTagsModalContent({
 	invalidTagCharacters: string;
 }) {
 	const [nameInputError, setNameInputError] = useState<string>('');
-	const [selectedSpaces, setSelectedSpaces] = useState<number[]>([-1]);
+	const [selectedSpaces, setSelectedSpaces] = useState<string[]>([]);
 	const [spaceInputError, setSpaceInputError] = useState('');
 	const [close, setClose] = useState(false);
 
@@ -60,8 +60,8 @@ export default function CreateTagsModalContent({
 		onSubmit: (values) => {
 			const url = `/o/headless-admin-taxonomy/v1.0/sites/${cmsGroupId}/keywords`;
 			const body = {
-				assetLibraries: selectedSpaces.map((number) => ({
-					id: number,
+				assetLibraries: selectedSpaces.map((string) => ({
+					scopeKey: string,
 				})),
 				name: values.tagName,
 			};