3ed7b7573e1ceea1f922d295de7ac07475dbbf2b
Jerry Adriano <ajerry550@gmail.com>
2026-01-20 15:37:49 -0300

    Revert "LPD-67476 add GET Method to endpoint and MessageBodyWriter impl"
    This reverts commit 3fb090032512673ec0fbf26a28e1b146a2877e7a.

--- modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/dynamic/registration/message/body/ClientRegistrationMessageBodyWriter.java
+++ modules/apps/oauth2-provider/oauth2-provider-rest/src/main/java/com/liferay/oauth2/provider/rest/internal/endpoint/dynamic/registration/message/body/ClientRegistrationMessageBodyWriter.java
@@ -5,10 +5,8 @@
 
 package com.liferay.oauth2.provider.rest.internal.endpoint.dynamic.registration.message.body;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import com.liferay.oauth2.provider.rest.internal.endpoint.authorize.message.body.BaseMessageBodyWriter;
@@ -116,9 +114,6 @@ public class ClientRegistrationMessageBodyWriter
 				configure(
 					DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 				configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-				setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-				setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-				setSerializationInclusion(JsonInclude.Include.NON_NULL);
 			}
 		};
 
