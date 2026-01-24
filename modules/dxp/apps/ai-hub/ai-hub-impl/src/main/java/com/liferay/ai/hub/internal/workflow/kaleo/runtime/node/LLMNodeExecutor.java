9dfdcab1751fad8ca39e969748605c6a1130599c
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-01-23 21:43:05 -0300

    LPD-76267 fix: store the output variable with the result of the ai assistant

--- modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/kaleo/runtime/node/LLMNodeExecutor.java
+++ modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/kaleo/runtime/node/LLMNodeExecutor.java
@@ -11,6 +11,7 @@ import com.liferay.ai.hub.internal.mcp.tool.provider.MCPToolProviderUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.ContentRetrieverUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.ToolsUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.VariablesUtil;
+import com.liferay.ai.hub.rest.resource.v1_0.util.SseUtil;
 import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -44,7 +45,6 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -175,13 +175,10 @@ public class LLMNodeExecutor extends BaseNodeExecutor {
 			Map<String, Serializable> workflowContext =
 				executionContext.getWorkflowContext();
 
-			BiConsumer<String, String> biConsumer =
-				(BiConsumer)workflowContext.get("sendOutBoundEvent");
+			AiMessage aiMessage = chatResponse.aiMessage();
 
 			JSONArray jsonArray = VariablesUtil.getVariablesJSONArray(
 				"outputVariables", kaleoNodeSettingValues);
-
-			AiMessage aiMessage = chatResponse.aiMessage();
 
 			if ((jsonArray != null) && (jsonArray.length() > 0)) {
 				JSONObject jsonObject = jsonArray.getJSONObject(0);
@@ -190,9 +187,12 @@ public class LLMNodeExecutor extends BaseNodeExecutor {
 					jsonObject.getString("name"), aiMessage.text());
 			}
 
-			biConsumer.accept(
+			workflowContext.put("output", aiMessage.text());
+
+			SseUtil.send(
 				aiMessage.text(),
-				GetterUtil.getString(workflowContext.get("outBoundEventName")));
+				GetterUtil.getString(workflowContext.get("outBoundEventName")),
+				GetterUtil.getString(workflowContext.get("sseEventSinkKey")));
 
 			KaleoInstanceToken kaleoInstanceToken =
 				executionContext.getKaleoInstanceToken();
