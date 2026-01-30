537a72ea8ec40112bff1562e4058d3543bf366d8
Yuri Monteiro <yuri.monteiro@liferay.com>
2026-01-30 14:08:34 -0300

    LPD-77785 Adding test

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/util/DDMFormFieldTemplateContextContributorUtilTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/test/java/com/liferay/dynamic/data/mapping/util/DDMFormFieldTemplateContextContributorUtilTest.java
@@ -7,11 +7,18 @@ package com.liferay.dynamic.data.mapping.util;
 
 import com.liferay.dynamic.data.mapping.form.field.type.internal.checkbox.CheckboxDDMFormFieldTemplateContextContributor;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.DDMFormFieldOptions;
 import com.liferay.dynamic.data.mapping.test.util.BaseDDMFormFieldTemplateContextContributorTestCase;
+import com.liferay.list.type.model.ListTypeEntry;
+import com.liferay.list.type.service.ListTypeEntryLocalService;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -19,6 +26,8 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import org.mockito.Mockito;
 
 /**
  * @author Pedro Leite
@@ -64,6 +73,63 @@ public class DDMFormFieldTemplateContextContributorUtilTest
 			LocaleUtil.US.toString(), editingLocaleJSONObject.get("localeId"));
 		Assert.assertTrue((boolean)parameters.get("editOnlyInDefaultLanguage"));
 		Assert.assertTrue((boolean)parameters.get("isLocalizationSupported"));
+	}
+
+	@Test
+	public void testGetOptions() {
+		DDMFormFieldOptions ddmFormFieldOptions = new DDMFormFieldOptions();
+
+		ddmFormFieldOptions.setDefaultLocale(LocaleUtil.BRAZIL);
+
+		String optionValue = "OptionA";
+
+		ddmFormFieldOptions.addOptionLabel(
+			optionValue, LocaleUtil.US, "Option A");
+		ddmFormFieldOptions.addOptionLabel(optionValue, LocaleUtil.BRAZIL, "");
+
+		Map<Locale, String> nameMap = LinkedHashMapBuilder.put(
+			LocaleUtil.US, "Option A"
+		).build();
+
+		long listTypeDefinitionId = RandomTestUtil.randomLong();
+
+		List<Map<String, Object>> options =
+			DDMFormFieldTemplateContextContributorUtil.getOptions(
+				ddmFormFieldOptions, listTypeDefinitionId,
+				_mockListTypeEntryLocalService(
+					listTypeDefinitionId, nameMap, optionValue));
+
+		Assert.assertEquals(options.toString(), 1, options.size());
+
+		Map<String, Object> option = options.get(0);
+
+		Assert.assertEquals("Option A", option.get("label"));
+		Assert.assertEquals(nameMap, option.get("labelMap"));
+	}
+
+	private ListTypeEntryLocalService _mockListTypeEntryLocalService(
+		long listTypeDefinitionId, Map<Locale, String> nameMap,
+		String optionValue) {
+
+		ListTypeEntry listTypeEntry = Mockito.mock(ListTypeEntry.class);
+
+		Mockito.when(
+			listTypeEntry.getNameMap()
+		).thenReturn(
+			nameMap
+		);
+
+		ListTypeEntryLocalService listTypeEntryLocalService = Mockito.mock(
+			ListTypeEntryLocalService.class);
+
+		Mockito.when(
+			listTypeEntryLocalService.fetchListTypeEntry(
+				listTypeDefinitionId, optionValue)
+		).thenReturn(
+			listTypeEntry
+		);
+
+		return listTypeEntryLocalService;
 	}
 
 }
