3d13321fe3dfcc8d14c227dba4dd92c013d7a093
Brian Chan <brian.chan@liferay.com>
2026-02-04 14:13:40 +0000

    LPD-77509 Inline

--- modules/integrations/vercel/clone_template.sh
+++ modules/integrations/vercel/clone_template.sh
@@ -12,22 +12,12 @@ function main {
 
 	TEMP_DIR=$(mktemp -d)
 
-	_clone_latest_commit
-
-	_extract_template
-
-	_make_destination_a_git_repo
-}
-
-function _clone_latest_commit {
 	git clone \
 		--branch=master \
 		--depth 1 \
 		--single-branch \
 		https://github.com/liferay/liferay-portal.git "${TEMP_DIR}"
-}
 
-function _extract_template {
 	local template_origin="${TEMP_DIR}/modules/integrations/vercel/${TEMPLATE}"
 
 	echo "Moving ${template_origin} to ${DESTINATION}"
@@ -38,9 +28,7 @@ function _extract_template {
 	fi
 
 	mv -v "${template_origin}" "${DESTINATION}"
-}
 
-function _make_destination_a_git_repo {
 	cd "${DESTINATION}/${TEMPLATE}" && git init && git add . && git commit --message "chore: clone TEMPLATE"
 }
 
