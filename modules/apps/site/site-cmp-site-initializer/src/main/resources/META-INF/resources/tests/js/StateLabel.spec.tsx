e86c1b9e0e925e644e02be235bedb4db3656be0a
Kevin Tan <kevin.tan@liferay.com>
2026-02-05 14:58:56 -0800

    LPD-77236 Add tests for overdue label

--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/tests/js/StateLabel.spec.tsx
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/tests/js/StateLabel.spec.tsx
@@ -9,12 +9,69 @@ import React from 'react';
 
 import StateLabel from '../../js/components/StateLabel';
 
+const getYesterdaysDateString = () => {
+	const date = new Date();
+
+	date.setDate(date.getDate() - 1);
+
+	return date.toISOString();
+};
+
 describe('StateLabel', () => {
 	it('renders the name text', () => {
-		const name = 'In Progress';
+		const state = {
+			key: 'inProgress',
+			name: 'In Progress',
+		};
 
-		const {getByText} = render(<StateLabel key="inProgress" name={name} />);
+		const {getByText} = render(<StateLabel state={state} />);
 
-		expect(getByText(name)).toBeInTheDocument();
+		expect(getByText(state.name)).toBeInTheDocument();
+	});
+
+	it('renders nothing if no state is provided', () => {
+		const {container} = render(<StateLabel />);
+
+		expect(container).toBeEmptyDOMElement();
+	});
+
+	it('renders the overdue label when the due date has passed', () => {
+		const state = {
+			key: 'inProgress',
+			name: 'In Progress',
+		};
+
+		const {getByText} = render(
+			<StateLabel dueDate={getYesterdaysDateString()} state={state} />
+		);
+
+		expect(getByText(state.name)).toBeInTheDocument();
+		expect(getByText('overdue')).toBeInTheDocument();
+	});
+
+	it('does not render the overdue label if the state is "done"', () => {
+		const state = {
+			key: 'done',
+			name: 'Done',
+		};
+
+		const {getByText, queryByText} = render(
+			<StateLabel dueDate={getYesterdaysDateString()} state={state} />
+		);
+
+		expect(getByText(state.name)).toBeInTheDocument();
+		expect(queryByText('overdue')).not.toBeInTheDocument();
+	});
+
+	it('does not render the overdue label if no due date is provided', () => {
+		const state = {
+			key: 'inProgress',
+			name: 'In Progress',
+		};
+
+		const {getByText, queryByText} = render(<StateLabel state={state} />);
+
+		expect(getByText(state.name)).toBeInTheDocument();
+		expect(queryByText('overdue')).not.toBeInTheDocument();
 	});
 });
