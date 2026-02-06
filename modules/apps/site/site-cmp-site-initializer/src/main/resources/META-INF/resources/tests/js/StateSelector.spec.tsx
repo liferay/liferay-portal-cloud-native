b782233624291db4e80026810387cb785a95b0ba
Carolina Barbosa <carolina.barbosa@liferay.com>
2026-02-06 11:31:30 -0300

    LPD-76950 [FE] Add unit test

--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/tests/js/StateSelector.spec.tsx
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/tests/js/StateSelector.spec.tsx
@@ -10,8 +10,14 @@ import React from 'react';
 import StateSelector from '../../js/components/StateSelector';
 
 const mockStates = [
-	{key: 'notStarted', name: 'Not Started', nextStates: ['inProgress']},
-	{key: 'inProgress', name: 'In Progress', nextStates: ['notStarted']},
+	{key: 'blocked', name: 'Blocked', nextStates: ['done', 'inProgress']},
+	{key: 'done', name: 'Done', nextStates: ['inProgress']},
+	{key: 'inProgress', name: 'In Progress', nextStates: ['blocked', 'done']},
+	{
+		key: 'notStarted',
+		name: 'Not Started',
+		nextStates: ['blocked', 'inProgress'],
+	},
 ];
 
 const mockOnChange = jest.fn();
@@ -47,5 +53,94 @@ describe('StateSelector', () => {
 		fireEvent.click(option);
 
 		expect(mockOnChange).toHaveBeenCalled();
+	});
+
+	it('does not update next states when the initialSelectedKey is provided', async () => {
+		const {getAllByRole, getByRole, getByText} = render(
+			<StateSelector
+				initialSelectedKey="notStarted"
+				onChange={mockOnChange}
+				states={mockStates}
+			/>
+		);
+
+		fireEvent.click(getByText('not-started'));
+
+		let options = getAllByRole('option').map(
+			(option) => option.textContent
+		);
+
+		expect(options.length).toBe(3);
+		expect(options[0]).toBe('Not Started');
+		expect(options[1]).toBe('In Progress');
+		expect(options[2]).toBe('Blocked');
+
+		fireEvent.click(getByRole('option', {name: 'In Progress'}));
+		fireEvent.click(getByText('in-progress'));
+
+		options = getAllByRole('option').map((option) => option.textContent);
+
+		expect(options.length).toBe(3);
+		expect(options[0]).toBe('Not Started');
+		expect(options[1]).toBe('In Progress');
+		expect(options[2]).toBe('Blocked');
+	});
+
+	it('updates next states in the specific display order when the selectedKey is provided', async () => {
+		const ControlledStateSelector = () => {
+			const [selectedKey, setSelectedKey] = React.useState('notStarted');
+
+			return (
+				<StateSelector
+					onChange={setSelectedKey}
+					selectedKey={selectedKey}
+					states={mockStates}
+				/>
+			);
+		};
+
+		const {getAllByRole, getByRole, getByText} = render(
+			<ControlledStateSelector />
+		);
+
+		fireEvent.click(getByText('not-started'));
+
+		let options = getAllByRole('option').map(
+			(option) => option.textContent
+		);
+
+		expect(options.length).toBe(3);
+		expect(options[0]).toBe('Not Started');
+		expect(options[1]).toBe('In Progress');
+		expect(options[2]).toBe('Blocked');
+
+		fireEvent.click(getByRole('option', {name: 'In Progress'}));
+		fireEvent.click(getByText('in-progress'));
+
+		options = getAllByRole('option').map((option) => option.textContent);
+
+		expect(options.length).toBe(3);
+		expect(options[0]).toBe('In Progress');
+		expect(options[1]).toBe('Blocked');
+		expect(options[2]).toBe('Done');
+
+		fireEvent.click(getByRole('option', {name: 'Blocked'}));
+		fireEvent.click(getByText('blocked'));
+
+		options = getAllByRole('option').map((option) => option.textContent);
+
+		expect(options.length).toBe(3);
+		expect(options[0]).toBe('In Progress');
+		expect(options[1]).toBe('Blocked');
+		expect(options[2]).toBe('Done');
+
+		fireEvent.click(getByRole('option', {name: 'Done'}));
+		fireEvent.click(getByText('done'));
+
+		options = getAllByRole('option').map((option) => option.textContent);
+
+		expect(options.length).toBe(2);
+		expect(options[0]).toBe('In Progress');
+		expect(options[1]).toBe('Done');
 	});
 });
