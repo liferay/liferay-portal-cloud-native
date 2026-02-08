761aabb033db1d11bc45d0353696944010f97eb9
Alberto Chaparro <alberto.chaparro@liferay.com>
2026-02-08 23:25:52 +0100

    LPD-78515 Use correct privateLayout value for LayoutSEOEntry too

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SitePageResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/SitePageResourceImpl.java
@@ -591,7 +591,7 @@ public class SitePageResourceImpl
 
 		_updateSEOEntry(
 			layout.getGroupId(), layout.getLayoutId(), pageSettings,
-			serviceContext);
+			layout.isPrivateLayout(), serviceContext);
 
 		if ((pageSettings != null) && (pageSettings.getPriority() != null)) {
 			layout = _layoutService.updatePriority(
@@ -1049,7 +1049,7 @@ public class SitePageResourceImpl
 			_getParentLayoutId(
 				layout.getParentLayoutId(), layout.getGroupId(),
 				sitePage.getParentSitePageExternalReferenceCode(),
-				sitePage.getPrivatePage(), serviceContext));
+				layout.isPrivateLayout(), serviceContext));
 
 		if (Objects.equals(sitePage.getType(), SitePage.Type.CONTENT_PAGE)) {
 			layout = LayoutUtil.updateContentLayout(
@@ -1092,7 +1092,7 @@ public class SitePageResourceImpl
 
 		_updateSEOEntry(
 			layout.getGroupId(), layout.getLayoutId(), pageSettings,
-			serviceContext);
+			layout.isPrivateLayout(), serviceContext);
 
 		int priority = Integer.MAX_VALUE;
 
@@ -1109,7 +1109,7 @@ public class SitePageResourceImpl
 
 	private void _updateSEOEntry(
 			long groupId, long layoutId, PageSettings pageSettings,
-			ServiceContext serviceContext)
+			boolean privateLayout, ServiceContext serviceContext)
 		throws Exception {
 
 		boolean canonicalURLEnabled = false;
@@ -1176,14 +1176,14 @@ public class SitePageResourceImpl
 		}
 
 		_layoutSEOEntryService.updateLayoutSEOEntry(
-			groupId, false, layoutId, canonicalURLEnabled, canonicalURLMap,
-			openGraphDescriptionEnabled, openGraphDescriptionMap,
-			openGraphImageAltMap, openGraphImageFileEntryERC,
-			openGraphImageFileEntryScopeERC, openGraphTitleEnabled,
-			openGraphTitleMap, serviceContext);
+			groupId, privateLayout, layoutId, canonicalURLEnabled,
+			canonicalURLMap, openGraphDescriptionEnabled,
+			openGraphDescriptionMap, openGraphImageAltMap,
+			openGraphImageFileEntryERC, openGraphImageFileEntryScopeERC,
+			openGraphTitleEnabled, openGraphTitleMap, serviceContext);
 
 		_layoutSEOEntryService.updateCustomMetaTags(
-			groupId, false, layoutId,
+			groupId, privateLayout, layoutId,
 			transformToList(
 				_getCustomMetaTags(pageSettings),
 				customMetaTag -> new LayoutSEOEntryCustomMetaTagProperty(
