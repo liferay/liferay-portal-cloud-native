5708bff185b8d921920bc1d22908d31b50f71709
Javier Moral <javier.moral@liferay.com>
2026-02-10 15:37:53 +0100

    LPD-77079 SF

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/util/test/FragmentEntryConfigurationParserTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/util/test/FragmentEntryConfigurationParserTest.java
@@ -31,7 +31,6 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.JavaConstants;
-import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
@@ -41,7 +40,6 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Locale;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 import org.junit.Assert;
@@ -195,71 +193,15 @@ public class FragmentEntryConfigurationParserTest {
 
 	@Test
 	@TestInfo("LPD-77079")
-	public void testGetFieldValueLocalizableFieldsWithoutLocale()
-		throws Exception {
-
-		Map<String, Object[]> fieldTypeValues =
-			LinkedHashMapBuilder.<String, Object[]>put(
-				"text",
-				new Object[] {
-					RandomTestUtil.randomString(), RandomTestUtil.randomString()
-				}
-			).put(
-				"colorPicker", new Object[] {"#0F0303", "#35CC58"}
-			).put(
-				"length", new Object[] {"300px", "320px"}
-			).put(
-				"checkbox", new Object[] {Boolean.FALSE, Boolean.TRUE}
-			).build();
-
-		for (Map.Entry<String, Object[]> entry : fieldTypeValues.entrySet()) {
-			String fieldType = entry.getKey();
-			Object englishValue = entry.getValue()[0];
-			Object spanishValue = entry.getValue()[1];
-
-			String fieldName = RandomTestUtil.randomString();
-
-			JSONObject configurationJSONObject = JSONUtil.put(
-				"fieldSets",
-				JSONUtil.put(
-					JSONUtil.put(
-						"fields",
-						JSONUtil.put(
-							JSONUtil.put(
-								"defaultValue", englishValue
-							).put(
-								"label", fieldName
-							).put(
-								"localizable", true
-							).put(
-								"name", fieldName
-							).put(
-								"type", fieldType
-							)))));
-
-			JSONObject editableValuesJSONObject = JSONUtil.put(
-				FragmentEntryProcessorConstants.
-					KEY_FREEMARKER_FRAGMENT_ENTRY_PROCESSOR,
-				JSONUtil.put(
-					fieldName,
-					JSONUtil.put(
-						LocaleUtil.toLanguageId(LocaleUtil.US), englishValue
-					).put(
-						LocaleUtil.toLanguageId(LocaleUtil.SPAIN), spanishValue
-					)));
-
-			Object value = _fragmentEntryConfigurationParser.getFieldValue(
-				configurationJSONObject, editableValuesJSONObject, fieldName);
-
-			if (englishValue instanceof Boolean) {
-				_assertLocalizableValue(
-					value, englishValue, spanishValue, true);
-			}
-			else {
-				_assertLocalizableValue(
-					value, englishValue, spanishValue, false);
-			}
-		}
+	public void testGetFieldValueWithLocalizableFields() {
+		_testGetFieldValueWithLocalizableField(
+			"checkbox", Boolean.FALSE, Boolean.TRUE);
+		_testGetFieldValueWithLocalizableField(
+			"colorPicker", "#0F0303", "#35CC58");
+		_testGetFieldValueWithLocalizableField("length", "300px", "320px");
+		_testGetFieldValueWithLocalizableField(
+			"text", RandomTestUtil.randomString(),
+			RandomTestUtil.randomString());
 	}
 
 	@Test
@@ -270,34 +212,6 @@ public class FragmentEntryConfigurationParserTest {
 	@Test
 	public void testTranslateConfigurationEs() throws Exception {
 		_testTranslateConfiguration("es");
-	}
-
-	private void _assertLocalizableValue(
-		Object value, Object englishValue, Object spanishValue,
-		boolean booleanValue) {
-
-		Assert.assertTrue(value instanceof JSONObject);
-
-		JSONObject valueJSONObject = (JSONObject)value;
-
-		Object expectedEnglishValue;
-		Object expectedSpanishValue;
-
-		if (booleanValue) {
-			expectedEnglishValue = valueJSONObject.getBoolean(
-				LocaleUtil.toLanguageId(LocaleUtil.US));
-			expectedSpanishValue = valueJSONObject.getBoolean(
-				LocaleUtil.toLanguageId(LocaleUtil.SPAIN));
-		}
-		else {
-			expectedEnglishValue = valueJSONObject.getString(
-				LocaleUtil.toLanguageId(LocaleUtil.US));
-			expectedSpanishValue = valueJSONObject.getString(
-				LocaleUtil.toLanguageId(LocaleUtil.SPAIN));
-		}
-
-		Assert.assertEquals(expectedEnglishValue, englishValue);
-		Assert.assertEquals(expectedSpanishValue, spanishValue);
 	}
 
 	private ResourceBundle _getResourceBundle(String language) {
@@ -314,6 +228,67 @@ public class FragmentEntryConfigurationParserTest {
 		return JSONFactoryUtil.createJSONObject(
 			new String(
 				FileUtil.getBytes(getClass(), "dependencies/" + fileName)));
+	}
+
+	private void _testGetFieldValueWithLocalizableField(
+		String fieldType, Object expectedEnglishValue,
+		Object expectedSpanishValue) {
+
+		String fieldName = RandomTestUtil.randomString();
+
+		JSONObject configurationJSONObject = JSONUtil.put(
+			"fieldSets",
+			JSONUtil.put(
+				JSONUtil.put(
+					"fields",
+					JSONUtil.put(
+						JSONUtil.put(
+							"defaultValue", expectedEnglishValue
+						).put(
+							"label", fieldName
+						).put(
+							"localizable", true
+						).put(
+							"name", fieldName
+						).put(
+							"type", fieldType
+						)))));
+
+		JSONObject valueJSONObject =
+			(JSONObject)_fragmentEntryConfigurationParser.getFieldValue(
+				configurationJSONObject,
+				JSONUtil.put(
+					FragmentEntryProcessorConstants.
+						KEY_FREEMARKER_FRAGMENT_ENTRY_PROCESSOR,
+					JSONUtil.put(
+						fieldName,
+						JSONUtil.put(
+							LocaleUtil.toLanguageId(LocaleUtil.US),
+							expectedEnglishValue
+						).put(
+							LocaleUtil.toLanguageId(LocaleUtil.SPAIN),
+							expectedSpanishValue
+						))),
+				fieldName);
+
+		Object englishValue = null;
+		Object spanishValue = null;
+
+		if (expectedEnglishValue instanceof Boolean) {
+			englishValue = valueJSONObject.getBoolean(
+				LocaleUtil.toLanguageId(LocaleUtil.US));
+			spanishValue = valueJSONObject.getBoolean(
+				LocaleUtil.toLanguageId(LocaleUtil.SPAIN));
+		}
+		else {
+			englishValue = valueJSONObject.getString(
+				LocaleUtil.toLanguageId(LocaleUtil.US));
+			spanishValue = valueJSONObject.getString(
+				LocaleUtil.toLanguageId(LocaleUtil.SPAIN));
+		}
+
+		Assert.assertEquals(expectedEnglishValue, englishValue);
+		Assert.assertEquals(expectedSpanishValue, spanishValue);
 	}
 
 	private void _testTranslateConfiguration(String language) throws Exception {
