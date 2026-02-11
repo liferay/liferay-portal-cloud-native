4cdb7dba194b807e45faec2f2ca9b1f358bbd0ea
Jorge González <jorge.gonzalez@liferay.com>
2026-02-11 18:35:16 +0100

    LPD-78136 Test continue button enablement in SetupStep

--- modules/apps/export-import/export-import-web/test/revamp/js/pages/export/NewExport.test.tsx
+++ modules/apps/export-import/export-import-web/test/revamp/js/pages/export/NewExport.test.tsx
@@ -5,14 +5,19 @@
 
 // eslint-disable-next-line @liferay/portal/no-cross-module-deep-import
 import {checkAccessibility} from '@liferay/layout-js-components-web/test/__lib__/index';
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import {NewExport} from '../../../../../src/main/resources/META-INF/resources/revamp/js/pages/export/NewExport';
 
+const renderComponent = () => {
+	return render(<NewExport backURL="/some/back/url" />);
+};
+
 describe('NewExport', () => {
 	it('renders the SetupStep', async () => {
-		const {container} = render(<NewExport backURL="/some/back/url" />);
+		const {container} = renderComponent();
 
 		const fileNameInput = screen.getByRole('textbox', {
 			name: /file-name/,
@@ -28,5 +33,39 @@ describe('NewExport', () => {
 		expect(continueButton).toBeDisabled();
 
 		await checkAccessibility({context: container});
+	});
+
+	it('enables the continue button when the SetupStep is valid', async () => {
+		renderComponent();
+
+		const continueButton = screen.getByRole('button', {name: /continue/i});
+		expect(continueButton).toBeDisabled();
+
+		const fileNameInput = await screen.findByRole('textbox', {
+			name: /file-name/,
+		});
+
+		await userEvent.click(fileNameInput);
+		fileNameInput.blur();
+
+		await screen.findByText('this-field-is-required');
+
+		await userEvent.type(fileNameInput, 'test-file');
+
+		await waitFor(() => {
+			expect(
+				screen.queryByText('this-field-is-required')
+			).not.toBeInTheDocument();
+		});
+
+		const designCheckbox = await screen.findByRole('checkbox', {
+			name: /Design/,
+		});
+
+		await userEvent.click(designCheckbox);
+
+		await waitFor(() => {
+			expect(continueButton).toBeEnabled();
+		});
 	});
 });
