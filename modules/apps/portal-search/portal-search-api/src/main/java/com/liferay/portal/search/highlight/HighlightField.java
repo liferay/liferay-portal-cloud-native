bd5c8fe668deab2869bf1aa0630efa4c9ae599aa
shuyangzhou <shuyang.zhou@liferay.com>
2026-02-12 10:59:49 -0800

    LPD-79324 Fix wrong builder pattern

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/highlight/HighlightField.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/highlight/HighlightField.java
@@ -14,16 +14,13 @@ import java.util.List;
  */
 public class HighlightField {
 
-	public HighlightField() {
-	}
-
-	public HighlightField(HighlightField highlightField) {
-		_fragments.addAll(highlightField._fragments);
-		_name = highlightField._name;
+	public HighlightField(List<String> fragments, String name) {
+		_fragments = Collections.unmodifiableList(fragments);
+		_name = name;
 	}
 
 	public List<String> getFragments() {
-		return Collections.unmodifiableList(_fragments);
+		return _fragments;
 	}
 
 	public String getName() {
@@ -35,35 +32,36 @@ public class HighlightField {
 
 		@Override
 		public HighlightFieldBuilder addFragment(String fragment) {
-			_highlightField._fragments.add(fragment);
+			_fragments.add(fragment);
 
 			return this;
 		}
 
 		@Override
 		public HighlightField build() {
-			return new HighlightField(_highlightField);
+			return new HighlightField(_fragments, _name);
 		}
 
 		@Override
 		public HighlightFieldBuilder fragments(List<String> fragments) {
-			_highlightField._fragments = fragments;
+			_fragments = fragments;
 
 			return this;
 		}
 
 		@Override
 		public HighlightFieldBuilder name(String name) {
-			_highlightField._name = name;
+			_name = name;
 
 			return this;
 		}
 
-		private final HighlightField _highlightField = new HighlightField();
+		private List<String> _fragments = new ArrayList<>();
+		private String _name;
 
 	}
 
-	private List<String> _fragments = new ArrayList<>();
-	private String _name;
+	private final List<String> _fragments;
+	private final String _name;
 
 }
