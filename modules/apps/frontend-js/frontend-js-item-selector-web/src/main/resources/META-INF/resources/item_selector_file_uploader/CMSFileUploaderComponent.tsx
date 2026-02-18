37a38510d52352934b53c55af88bcf28030d68ed
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2026-02-18 21:56:40 +0100

    LPD-67215 Simplilfy component to avoid circular dependency with site-cms-initializer (this needs SpaceSelector component from cms, that also uses ItemSelector). Need to re-think this once we want to integrate FileUploaderComponentExample here

--- modules/apps/frontend-js/frontend-js-item-selector-web/src/main/resources/META-INF/resources/item_selector_file_uploader/CMSFileUploaderComponent.tsx
+++ modules/apps/frontend-js/frontend-js-item-selector-web/src/main/resources/META-INF/resources/item_selector_file_uploader/CMSFileUploaderComponent.tsx
@@ -3,14 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {type Space, SpaceSelector} from '@liferay/site-cms-site-initializer';
 import {
-	FieldBase,
 	MultipleFileUploader,
 	UploadRequestCallback,
 } from 'frontend-js-components-web';
 import {getFileAsBase64} from 'frontend-js-web';
-import React, {useId, useState} from 'react';
+import React from 'react';
 
 import {FilesUploaderComponent} from '../item_selector/ItemSelectorModal';
 
@@ -19,28 +17,15 @@ const CMSFileUploaderComponent: FilesUploaderComponent = function ({
 	groupId,
 	onCloseUploadView,
 }) {
-	const [spaceId, setSpaceId] = useState<number | undefined>(groupId);
-	const [groupIdError, setGroupIdError] = useState(false);
-
-	const groupIdInputId = useId();
-
-	const formValidation = async () => {
-		const error = (spaceId || 0) <= 0;
-
-		setGroupIdError(error);
-
-		return error === false;
-	};
-
 	const uploadRequest: UploadRequestCallback = async ({fileData}) => {
-		if (!spaceId) {
+		if (!groupId) {
 			throw new Error('spaceId is not defined');
 		}
 
 		const fileBase64 = await getFileAsBase64(fileData.file);
 
 		const response = await Liferay.Util.fetch(
-			`/o/cms/basic-documents/scopes/${spaceId}`,
+			`/o/cms/basic-documents/scopes/${groupId}`,
 			{
 				body: JSON.stringify({
 					file: {
@@ -77,39 +62,8 @@ const CMSFileUploaderComponent: FilesUploaderComponent = function ({
 	return (
 		<MultipleFileUploader
 			filesToUpload={files}
-			formValidation={!groupId ? formValidation : undefined}
 			onModalClose={onCloseUploadView}
 			onUploadComplete={onUploadComplete}
-			scopeSelectorElement={
-				!groupId ? (
-					<div className="mt-4">
-						<FieldBase
-							errorMessage={
-								groupIdError
-									? Liferay.Language.get(
-											'this-field-is-required'
-										)
-									: undefined
-							}
-							helpMessage={Liferay.Language.get(
-								'select-the-space-to-upload-the-file'
-							)}
-							id={groupIdInputId}
-							label={Liferay.Language.get('space')}
-							required
-						>
-							<SpaceSelector
-								id={groupIdInputId}
-								onSpaceChange={(space: Space) => {
-									setGroupIdError(false);
-									setSpaceId(space.siteId);
-								}}
-								space={spaceId}
-							/>
-						</FieldBase>
-					</div>
-				) : undefined
-			}
 			uploadRequest={uploadRequest}
 		/>
 	);
