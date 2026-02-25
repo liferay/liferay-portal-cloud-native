616a3bdfb8b0a45e1b0dc970242fd6aebc358e35
jaredgorski <jared.gorski@liferay.com>
2026-02-25 17:24:43 -0500

    LCD-49810 Clarify security group rules, remove unnecessary EnvoyProxy annotations

--- cloud/terraform/aws/eks/security.tf
+++ cloud/terraform/aws/eks/security.tf
@@ -1,14 +1,8 @@
-resource "aws_vpc_security_group_ingress_rule" "node_ingress_envoy_tls" {
-	cidr_ipv4="0.0.0.0/0"
-	from_port=8443
-	ip_protocol="tcp"
-	security_group_id=module.eks.node_security_group_id
-	to_port=8443
-}
-resource "aws_vpc_security_group_ingress_rule" "node_ingress_liferay" {
+resource "aws_vpc_security_group_ingress_rule" "envoy_ingress_managed" {
 	cidr_ipv4=var.vpc_cidr
-	from_port=8080
+	for_each=toset(["10080", "10443"])
+	from_port=each.value
 	ip_protocol="tcp"
-	security_group_id=module.eks.node_security_group_id
-	to_port=8080
+	security_group_id=module.eks.cluster_primary_security_group_id
+	to_port=each.value
 }
