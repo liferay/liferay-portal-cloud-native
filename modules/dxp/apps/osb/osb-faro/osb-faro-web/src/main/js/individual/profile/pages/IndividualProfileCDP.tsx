5fb6eee019c5d2463f76cdfd5b5657bfae0fbe0d
Marcio Fonseca <marcio.fonseca@liferay.com>
2026-02-26 12:57:14 -0300

    LPD-80353 Create IndividualProfileCDP component

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/individual/profile/pages/IndividualProfileCDP.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// TODO: create IndividualProfileCDP component
+
+const IndividualProfileCDP = () => <div>{'Individual Profile CDP page'}</div>;
+export default IndividualProfileCDP;
