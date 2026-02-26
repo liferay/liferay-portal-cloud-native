805cec6bfef68f8adc58ed1d0ec47e05d0c16c50
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-02-26 17:50:02 +0100

    LPD-80449 Update GlobalMenuPage to go to any menuitem

--- modules/test/playwright/pages/product-navigation-applications-menu/GlobalMenuPage.ts
+++ modules/test/playwright/pages/product-navigation-applications-menu/GlobalMenuPage.ts
@@ -7,12 +7,103 @@ import {Locator, Page, expect} from '@playwright/test';
 
 import {clickAndExpectToBeVisible} from '../../utils/clickAndExpectToBeVisible';
 import {openProductMenu} from '../../utils/productMenu';
+import {waitForPageToBeLoaded} from '../../utils/waitForPageToBeLoaded';
+import {HomePage} from '../portal-web/HomePage';
 
 type Categories = 'Applications' | 'CMS' | 'Commerce' | 'Control Panel';
+
+type ApplicationsMenuItem =
+	| 'Announcements and Alerts'
+	| 'Asset Libraries'
+	| 'Blueprints'
+	| 'Client Extensions'
+	| 'Content Dashboard'
+	| 'Data Migration Center'
+	| 'Export'
+	| 'Import'
+	| 'Metrics'
+	| 'Process Builder'
+	| 'Publications'
+	| 'Result Rankings'
+	| 'Submissions'
+	| 'Synonyms';
+
+type ControlPanelMenuItem =
+	| 'App Manager'
+	| 'Accounts'
+	| 'Account Groups'
+	| 'Account Users'
+	| 'Adaptive Media'
+	| 'API Builder'
+	| 'Audit'
+	| 'Components'
+	| 'Countries Management'
+	| 'Custom Fields'
+	| 'Data Sets'
+	| 'Gogo Shell'
+	| 'Instance Settings'
+	| 'Job Scheduler'
+	| 'Language Override'
+	| 'Monitoring'
+	| 'License Manager'
+	| 'OAuth 2 Administration'
+	| 'OAuth Client Administration'
+	| 'Objects'
+	| 'On-Demand Admin'
+	| 'Password Policies'
+	| 'Picklists'
+	| 'Push Notifications'
+	| 'Purchased'
+	| 'Queue'
+	| 'Roles'
+	| 'SAML Admin'
+	| 'Search'
+	| 'Server Administration'
+	| 'Service Access Policy'
+	| 'Service Accounts'
+	| 'Site Templates'
+	| 'Sites'
+	| 'Store'
+	| 'System Settings'
+	| 'Templates'
+	| 'Users and Organizations'
+	| 'User Groups'
+	| 'Virtual Instances';
+
+type CommerceMenuItem =
+	| 'Avalara Connector'
+	| 'Availability Estimates'
+	| 'Channels'
+	| 'Catalogs'
+	| 'Currencies'
+	| 'Digital Sales Room Management'
+	| 'Discounts'
+	| 'Health Check'
+	| 'Inventory'
+	| 'Measurement Units'
+	| 'On-Demand Admin'
+	| 'Options'
+	| 'Orders'
+	| 'Order Types'
+	| 'Order Rules'
+	| 'Payments'
+	| 'Price Lists'
+	| 'Products'
+	| 'Product Configurations'
+	| 'Product Groups'
+	| 'Promotions'
+	| 'Returns'
+	| 'Shipments'
+	| 'Specifications'
+	| 'Subscriptions'
+	| 'Terms and Conditions'
+	| 'Tax Categories'
+	| 'Warehouses';
 
 export class GlobalMenuPage {
 	readonly categoriesList: Locator;
 	readonly globalMenuButton: Locator;
+	private readonly homePage: HomePage;
 	readonly page: Page;
 	readonly sitesList: Locator;
 
@@ -21,8 +112,9 @@ export class GlobalMenuPage {
 			.getByRole('menu')
 			.and(page.locator('.categories-list'));
 		this.globalMenuButton = page
-			.getByRole('button', {name: 'Applications Menu'})
+			.getByRole('button', {name: 'Open Applications Menu'})
 			.or(page.getByTestId('globalMenu'));
+		this.homePage = new HomePage(page);
 		this.page = page;
 		this.sitesList = page
 			.getByRole('menu')
@@ -30,37 +122,135 @@ export class GlobalMenuPage {
 	}
 
 	async goTo(categoryName: Categories) {
-		await clickAndExpectToBeVisible({
-			autoClick: true,
-			target: this.page.getByRole('menuitem', {name: categoryName}),
-			trigger: this.globalMenuButton,
-		});
+		await expect(async () => {
+			await this.globalMenuButton.click();
 
-		const menu = this.page.getByLabel(`${categoryName} Menu`, {
-			exact: true,
-		});
+			const menuItem = this.page.getByRole('menuitem', {
+				name: categoryName,
+			});
 
-		await expect(menu).toBeAttached();
+			await expect(menuItem).toBeVisible();
 
-		if (!(await menu.isVisible())) {
-			openProductMenu(this.page);
-		}
+			const isActive = await menuItem.evaluate((element) =>
+				element.classList.contains('active')
+			);
+
+			if (!isActive) {
+				await menuItem.click();
+
+				await expect(menuItem).toBeHidden({timeout: 2000});
+			}
+		}).toPass();
+
+		const sidebar = this.page.getByTestId('sideNavigation');
+
+		let forceToReload = false;
+
+		await expect(async () => {
+			if (forceToReload) {
+				await this.page.reload();
+			}
+
+			forceToReload = true;
+
+			await expect(sidebar).toBeAttached();
+			await expect(sidebar.locator('.sidebar-header')).toBeAttached();
+		}).toPass();
+
+		await openProductMenu(this.page);
 	}
 
-	async goToApplications() {
+	async goToHome() {
+		await this.homePage.goto();
+	}
+
+	async goToApplications(menuItem?: ApplicationsMenuItem, link?: string) {
 		await this.goTo('Applications');
+
+		if (menuItem) {
+			await this.goToMenuItem(menuItem);
+
+			await waitForPageToBeLoaded(this.page);
+
+			if (link) {
+				await this.goToLink(link);
+			}
+		}
 	}
 
 	async goToCMS() {
 		await this.goTo('CMS');
 	}
 
-	async goToCommerce() {
+	async goToCommerce(menuItem?: CommerceMenuItem, link?: string) {
 		await this.goTo('Commerce');
+
+		if (menuItem) {
+			await this.goToMenuItem(menuItem);
+
+			await waitForPageToBeLoaded(this.page);
+
+			if (link) {
+				await this.goToLink(link);
+			}
+		}
 	}
 
-	async goToControlPanel() {
+	async goToControlPanel(menuItem?: ControlPanelMenuItem, link?: string) {
 		await this.goTo('Control Panel');
+
+		if (menuItem) {
+			await this.goToMenuItem(menuItem);
+
+			await waitForPageToBeLoaded(this.page);
+
+			if (link) {
+				await this.goToLink(link);
+			}
+		}
+	}
+
+	async goToObjectDefinition(menuItem: string) {
+		await this.goToHome();
+
+		await this.goTo('Control Panel');
+
+		await this.goToMenuItem(menuItem);
+	}
+
+	async goToMenuItem(
+		menuItem:
+			| ApplicationsMenuItem
+			| ControlPanelMenuItem
+			| CommerceMenuItem
+			| string
+	) {
+		const input = this.page
+			.locator('.side-navigation-container')
+			.getByPlaceholder('Search', {exact: true});
+
+		await input.waitFor();
+
+		await input.fill(menuItem);
+
+		await input.press('Tab');
+
+		await this.page
+			.getByRole('menuitem', {
+				exact: true,
+				name: menuItem as string,
+			})
+			.and(this.page.locator('.nav-link[href]'))
+			.click();
+	}
+
+	async goToLink(link: string) {
+		await this.page
+			.getByRole('link', {
+				exact: true,
+				name: link,
+			})
+			.click();
 	}
 
 	async openGlobalMenu() {
