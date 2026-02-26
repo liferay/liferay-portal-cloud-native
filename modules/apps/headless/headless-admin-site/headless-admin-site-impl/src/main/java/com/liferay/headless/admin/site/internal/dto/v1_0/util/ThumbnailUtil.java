62fcb5f11406c45dd5c71985f799e92fb610843e
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-02-26 12:01:05 +0100

    LPD-79850 Remove unused

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/util/ThumbnailUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/dto/v1_0/util/ThumbnailUtil.java
@@ -8,35 +8,13 @@ package com.liferay.headless.admin.site.internal.dto.v1_0.util;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
 import com.liferay.exportimport.attachment.ExportImportAttachmentManagerUtil;
-import com.liferay.headless.admin.site.dto.v1_0.ItemExternalReference;
 import com.liferay.headless.admin.site.dto.v1_0.ThumbnailURLReference;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
-import com.liferay.portal.kernel.repository.model.FileEntry;
 
 /**
  * @author Lourdes Fernández Besada
  */
 public class ThumbnailUtil {
-
-	public static ItemExternalReference
-			getPortletFileEntryItemExternalReference(long fileEntryId)
-		throws PortalException {
-
-		if (fileEntryId <= 0) {
-			return null;
-		}
-
-		FileEntry fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
-			fileEntryId);
-
-		return new ItemExternalReference() {
-			{
-				setClassName(() -> FileEntry.class.getName());
-				setExternalReferenceCode(fileEntry::getExternalReferenceCode);
-			}
-		};
-	}
 
 	public static ThumbnailURLReference
 			getPortletFileEntryThumbnailURLReference(long fileEntryId)
