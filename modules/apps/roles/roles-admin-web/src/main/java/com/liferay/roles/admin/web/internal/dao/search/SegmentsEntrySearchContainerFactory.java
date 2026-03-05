5ae40c93506c98f99781303227287de33585969e
Victor Cunha <victor.cunha@liferay.com>
2026-03-05 15:12:53 -0300

    Revert "LPD-78402 Sort by searchContext.*"
    This reverts commit 5498152280d78866a2356bdfe8b590c9c37b6917.

--- modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/dao/search/SegmentsEntrySearchContainerFactory.java
+++ modules/apps/roles/roles-admin-web/src/main/java/com/liferay/roles/admin/web/internal/dao/search/SegmentsEntrySearchContainerFactory.java
@@ -116,12 +116,6 @@ public class SegmentsEntrySearchContainerFactory {
 				Field.NAME, keywords
 			).build();
 
-		params.put("keywords", keywords);
-
-		attributes.put("params", params);
-
-		searchContext.setAttributes(attributes);
-
 		if (!FeatureFlagManagerUtil.isEnabled(
 				CompanyConstants.SYSTEM, "LPD-78863")) {
 
@@ -138,6 +132,12 @@ public class SegmentsEntrySearchContainerFactory {
 						booleanQuery, BooleanClauseOccur.MUST.getName())
 				});
 		}
+
+		params.put("keywords", keywords);
+
+		attributes.put("params", params);
+
+		searchContext.setAttributes(attributes);
 
 		searchContext.setCompanyId(companyId);
 		searchContext.setEnd(end);
