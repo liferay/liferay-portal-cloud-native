6793b40241e8886038a5a6f88938fb3bc9a9959f
Keven Leone <keven.santos.sz@gmail.com>
2026-03-05 16:14:32 -0300

    LPD-79523 SF

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/ProductPurchase/pages/LiferayProduct/LDPNextSteps.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/ProductPurchase/pages/LiferayProduct/LDPNextSteps.tsx
@@ -8,8 +8,8 @@ import React from 'react';
 import Loading from '../../../../components/Loading';
 import {OrderStatus} from '../../../../enums/Order';
 import {usePlacedOrder} from '../../../../hooks/data/usePlacedOrder';
-
 import i18n from '../../../../i18n';
+
 import './LDPNextSteps.scss';
 
 const Content = ({
