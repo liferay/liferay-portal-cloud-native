61815e9bb31e676848496d3727c91bcfae82b900
Vendel Töreki <vendel.toreki@liferay.com>
2026-03-06 14:10:51 +0100

    LPD-80796 Register the upgrade step

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/upgrade/registry/ObjectRESTImplUpgradeStepRegistrator.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/upgrade/registry/ObjectRESTImplUpgradeStepRegistrator.java
@@ -6,9 +6,13 @@
 package com.liferay.object.rest.internal.upgrade.registry;
 
 import com.liferay.object.rest.internal.upgrade.v1_0_0.SAPEntryAllowedServiceSignaturesUpgradeProcess;
+import com.liferay.object.rest.internal.upgrade.v1_0_1.VulcanCompanyConfigurationUpgradeProcess;
+import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Alejandro Tardín
@@ -24,6 +28,17 @@ public class ObjectRESTImplUpgradeStepRegistrator
 		registry.register(
 			"0.0.1", "1.0.0",
 			new SAPEntryAllowedServiceSignaturesUpgradeProcess());
+
+		registry.register(
+			"1.0.0", "1.0.1",
+			new VulcanCompanyConfigurationUpgradeProcess(
+				_configurationAdmin, _objectDefinitionLocalService));
 	}
+
+	@Reference
+	private ConfigurationAdmin _configurationAdmin;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
 
 }
