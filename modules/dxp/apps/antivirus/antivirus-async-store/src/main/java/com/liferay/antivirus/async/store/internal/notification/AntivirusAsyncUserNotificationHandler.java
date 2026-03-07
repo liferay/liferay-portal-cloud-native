alvarosaugarlr/liferay-portal

LPD-78374 Changes in the logic when a virus is found and it is a new version of the file. Changes in the notification

modules/dxp/apps/antivirus/antivirus-async-store/src/main/java/com/liferay/antivirus/async/store/internal/notification/AntivirusAsyncUserNotificationHandler.java
@@ -53,10 +53,11 @@ public class AntivirusAsyncUserNotificationHandler
 
 		return _language.format(
 			serviceContext.getLocale(),
-			"the-file-x-was-deleted-due-to-the-detection-of-virus-y-during-" +
-				"an-asynchronous-scan",
+			"the-file-x-version-y-was-deleted-due-to-the-detection-of-virus-" +
+				"z-during-an-asynchronous-scan",
 			new Object[] {
 				jsonObject.getString("fileName"),
+				jsonObject.getString("version"),
 				jsonObject.getString("virusName")
 			},
 			false);

liferay/liferay-portal-cloud-native

modules/dxp/apps/antivirus/antivirus-async-store/src/main/java/com/liferay/antivirus/async/store/internal/notification/AntivirusAsyncUserNotificationHandler.java
@@ -53,10 +53,11 @@ public class AntivirusAsyncUserNotificationHandler
 
 		return _language.format(
 			serviceContext.getLocale(),
-			"the-file-x-was-deleted-due-to-the-detection-of-virus-x-during-" +
-				"an-asynchronous-scan",
+			"the-file-x-version-x-was-deleted-due-to-the-detection-of-virus-" +
+				"x-during-an-asynchronous-scan",
 			new Object[] {
 				jsonObject.getString("fileName"),
+				jsonObject.getString("version"),
 				jsonObject.getString("virusName")
 			},
 			false);