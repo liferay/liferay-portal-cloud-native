e2c5f6895330859adf4fc17cc507b59a686ce2b1
Carlos Correa <carlos.correa@liferay.com>
2026-03-10 18:17:31 +0100

    LPD-81248 Add test for the upgrade process

---
+++ modules/apps/staging/staging-test/src/testIntegration/java/com/liferay/staging/internal/upgrade/v1_0_0/test/RemovePublishDisplayedContentInStagingConfigurationUpgradeProcessTest.java
@@ -0,0 +1,132 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.staging.internal.upgrade.v1_0_0.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.version.Version;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
+import com.liferay.staging.configuration.StagingConfiguration;
+
+import java.util.Dictionary;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Carlos Correa
+ */
+@RunWith(Arquillian.class)
+public class
+	RemovePublishDisplayedContentInStagingConfigurationUpgradeProcessTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void test() throws Exception {
+		long companyId = TestPropsValues.getCompanyId();
+
+		Configuration originalConfiguration = _getConfiguration(companyId);
+
+		Dictionary<String, Object> originalProperties = null;
+
+		if (originalConfiguration != null) {
+			originalProperties = originalConfiguration.getProperties();
+		}
+
+		try {
+			_configurationProvider.saveCompanyConfiguration(
+				StagingConfiguration.class, companyId,
+				HashMapDictionaryBuilder.<String, Object>put(
+					"publishDisplayedContent", true
+				).build());
+
+			Configuration configuration = _getConfiguration(companyId);
+
+			Dictionary<String, Object> properties =
+				configuration.getProperties();
+
+			Assert.assertTrue(
+				GetterUtil.getBoolean(
+					properties.get("publishDisplayedContent")));
+
+			_runUpgrade();
+
+			configuration = _getConfiguration(companyId);
+
+			properties = configuration.getProperties();
+
+			Assert.assertNull(properties.get("publishDisplayedContent"));
+		}
+		finally {
+			if (originalConfiguration != null) {
+				originalConfiguration.update(originalProperties);
+			}
+			else {
+				_configurationProvider.deleteCompanyConfiguration(
+					StagingConfiguration.class, companyId);
+			}
+		}
+	}
+
+	private Configuration _getConfiguration(long companyId) throws Exception {
+		String filterString = StringBundler.concat(
+			"(&(service.factoryPid=", _STAGING_CONFIGURATION, ".scoped)(",
+			ExtendedObjectClassDefinition.Scope.COMPANY.getPropertyKey(), "=",
+			companyId, "))");
+
+		Configuration[] configurations = _configurationAdmin.listConfigurations(
+			filterString);
+
+		if (configurations == null) {
+			return null;
+		}
+
+		return configurations[0];
+	}
+
+	private void _runUpgrade() throws Exception {
+		UpgradeProcess[] upgradeProcesses = UpgradeTestUtil.getUpgradeSteps(
+			_upgradeStepRegistrator, new Version(1, 0, 0));
+
+		for (UpgradeProcess upgradeProcess : upgradeProcesses) {
+			upgradeProcess.upgrade();
+		}
+	}
+
+	private static final String _STAGING_CONFIGURATION =
+		"com.liferay.staging.configuration.StagingConfiguration";
+
+	@Inject(
+		filter = "(&(component.name=com.liferay.staging.internal.upgrade.registry.StagingImplUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+	@Inject
+	private ConfigurationAdmin _configurationAdmin;
+
+	@Inject
+	private ConfigurationProvider _configurationProvider;
+
+}
