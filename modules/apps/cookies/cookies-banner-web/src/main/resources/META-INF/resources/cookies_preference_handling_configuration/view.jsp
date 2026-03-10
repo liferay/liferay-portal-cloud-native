liferay-appsec/liferay-portal

LPD-79710 Show modal only when Consent Renewal Period changes

modules/apps/cookies/cookies-banner-web/src/main/resources/META-INF/resources/cookies_preference_handling_configuration/view.jsp
@@ -91,6 +91,14 @@ CookiesPreferenceHandlingConfigurationDisplayContext cookiesPreferenceHandlingCo
 <aui:script>
 	var form = document.<portlet:namespace />fm;
 
+	var consentRenewalPeriodInput = document.getElementById(
+		'<portlet:namespace />consentRenewalPeriod'
+	);
+
+	var originalConsentRenewalPeriod = consentRenewalPeriodInput
+		? consentRenewalPeriodInput.value
+		: null;
+
 	if (form) {
 		form.addEventListener('submit', (event) => {
 			var consentRenewalPeriod = document.getElementById(
@@ -106,7 +114,9 @@ CookiesPreferenceHandlingConfigurationDisplayContext cookiesPreferenceHandlingCo
 			var enabled = document.getElementById('<portlet:namespace />enabled');
 
 			if (
+				consentRenewalPeriod.value !== originalConsentRenewalPeriod &&
 				enabled.checked &&
+				originalConsentRenewalPeriod !== null &&
 				<%= cookiesPreferenceHandlingConfigurationDisplayContext.getCookiesPreferenceHandlingEnabled() %>
 			) {
 				event.preventDefault();

liferay/liferay-portal-cloud-native

modules/apps/cookies/cookies-banner-web/src/main/resources/META-INF/resources/cookies_preference_handling_configuration/view.jsp
@@ -91,12 +91,16 @@ CookiesPreferenceHandlingConfigurationDisplayContext cookiesPreferenceHandlingCo
 <aui:script>
 	var form = document.<portlet:namespace />fm;
 
+	var consentRenewalPeriod = document.getElementById(
+		'<portlet:namespace />consentRenewalPeriod'
+	);
+
+	var originalConsentRenewalPeriod = consentRenewalPeriod
+		? consentRenewalPeriod.value
+		: null;
+
 	if (form) {
 		form.addEventListener('submit', (event) => {
-			var consentRenewalPeriod = document.getElementById(
-				'<portlet:namespace />consentRenewalPeriod'
-			);
-
 			if (!consentRenewalPeriod.value || isNaN(consentRenewalPeriod.value)) {
 				event.preventDefault();
 				event.stopImmediatePropagation();
@@ -106,7 +110,9 @@ CookiesPreferenceHandlingConfigurationDisplayContext cookiesPreferenceHandlingCo
 			var enabled = document.getElementById('<portlet:namespace />enabled');
 
 			if (
+				consentRenewalPeriod.value !== originalConsentRenewalPeriod &&
 				enabled.checked &&
+				originalConsentRenewalPeriod !== null &&
 				<%= cookiesPreferenceHandlingConfigurationDisplayContext.getCookiesPreferenceHandlingEnabled() %>
 			) {
 				event.preventDefault();