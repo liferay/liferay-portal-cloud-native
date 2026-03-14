36de92e0f1f2da89d20d6da7e5e5a9a75ac8c8da
Brian Chan <brian.chan@liferay.com>
2026-03-14 08:43:03 -0300

    LPD-80809 Can you rephrase

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/MarketplaceRestController.java
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-etc-spring-boot/src/main/java/com/liferay/marketplace/MarketplaceRestController.java
@@ -266,9 +266,7 @@ public class MarketplaceRestController extends BaseRestController {
 			Objects.equals(appLicenseKey.getMacAddresses(), null)) {
 
 			throw new ResponseStatusException(
-				HttpStatus.BAD_REQUEST,
-				"Required at least one of: hostName, ipAddresses, " +
-					"macAddresses");
+				HttpStatus.BAD_REQUEST);
 		}
 
 		Order order = _marketplaceService.getOrder(
