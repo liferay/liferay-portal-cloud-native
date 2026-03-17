e95b2eb89a55722b1ca870768f624f554d359cb2
shuyangzhou <shuyang.zhou@liferay.com>
2026-03-17 10:23:29 -0700

    LPD-82927 Skip JavaParser when generated content is unchanged
    ServiceBuilder calls JavaParser.parse() on every generated file to format the code, even when the generated content hasn't changed.
    
    Embed a content hash in generated Java files as a trailing comment:
    
      // LIFERAY-SERVICE-BUILDER-HASH::<contentHash>
    
    On subsequent runs, if the content hash matches, skip the expensive JavaParser.parse() call entirely.

--- modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/ServiceBuilder.java
@@ -8152,8 +8152,10 @@ public class ServiceBuilder {
 
 		String year = simpleDateFormat.format(new Date());
 
+		String oldContent = null;
+
 		if (file.exists()) {
-			String oldContent = _read(file);
+			oldContent = _read(file);
 
 			int x = oldContent.indexOf("/**\n * SPDX-FileCopyrightText: (c) ");
 
@@ -8167,6 +8169,22 @@ public class ServiceBuilder {
 		header = header.replaceFirst(Pattern.quote("{$year}"), year);
 
 		content = header + "\n\n" + content;
+
+		if (oldContent != null) {
+			int index = oldContent.lastIndexOf(
+				_LIFERAY_SERVICE_BUILDER_HASH_PREFIX);
+
+			if (index != -1) {
+				int contentHash = content.hashCode();
+
+				String hashLine = oldContent.substring(
+					index + _LIFERAY_SERVICE_BUILDER_HASH_PREFIX.length());
+
+				if (GetterUtil.getInteger(hashLine) == contentHash) {
+					return;
+				}
+			}
+		}
 
 		String fileName = _normalize(file.toString());
 
@@ -8199,16 +8217,19 @@ public class ServiceBuilder {
 			}
 		}
 
-		ToolsUtil.writeFileRaw(
+		String parsedContent = JavaParser.parse(
 			file,
-			JavaParser.parse(
-				file,
-				StringUtil.replace(
-					fileName.substring(
-						startIndex, fileName.lastIndexOf(StringPool.SLASH)),
-					CharPool.SLASH, CharPool.PERIOD),
-				content, _MAX_LINE_LENGTH, false),
-			modifiedFileNames);
+			StringUtil.replace(
+				fileName.substring(
+					startIndex, fileName.lastIndexOf(StringPool.SLASH)),
+				CharPool.SLASH, CharPool.PERIOD),
+			content, _MAX_LINE_LENGTH, false);
+
+		parsedContent =
+			parsedContent + _LIFERAY_SERVICE_BUILDER_HASH_PREFIX +
+				content.hashCode();
+
+		ToolsUtil.writeFileRaw(file, parsedContent, modifiedFileNames);
 	}
 
 	private static final int _DEFAULT_COLUMN_MAX_LENGTH = 75;
@@ -8222,6 +8243,9 @@ public class ServiceBuilder {
 
 	private static final String _HIBERNATE_5_HBM_NAMESPACE =
 		"\"http://www.hibernate.org/dtd/hibernate-mapping-3.0.dtd\"";
+
+	private static final String _LIFERAY_SERVICE_BUILDER_HASH_PREFIX =
+		"\n// LIFERAY-SERVICE-BUILDER-HASH:";
 
 	private static final int _MAX_LINE_LENGTH = 80;
 
