b7e43dde79919222453908a8000da9b9b7f28a31
chestofwonders <miguelarroyoes@hotmail.com>
2026-03-18 12:04:03 +0100

    LPD-74587 Add ManageUserViews Data Set name filter

--- modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/resources/META-INF/resources/js/ManageUserViews.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-admin-web/src/main/resources/META-INF/resources/js/ManageUserViews.tsx
@@ -156,118 +156,23 @@ export default function ManageUserViews({
                         multiple: false,
                         type: 'selection',
                 },
-        ], []);
+                {
+                        apiURL: getUserViewsDataSetsURL,
+                        autocompleteEnabled: true,
+                        entityFieldType: 'string',
+                        id: 'fdsName',
+                        inputPlaceholder: Liferay.Language.get('search'),
+                        itemKey: 'value',
+                        itemLabel: 'label',
+                        items: [],
+                        label: Liferay.Language.get('data-set'),
+                        multiple: false,
+                        type: 'selection',
+                },
+        ], [getUserViewsDataSetsURL]);
 
         const systemDataSetNamesSet = useMemo(
                 () => new Set(systemDataSetEntries.map((entry) => entry.name)),
                 [systemDataSetEntries]
         );
-        const filters = useMemo(() => [
-                {
-                        apiURL: '/o/headless-admin-user/v1.0/user-accounts?sort=name:asc',
-                        autocompleteEnabled: true,
-                        entityFieldType: 'integer',
-                        id: 'creatorId',
-                        inputPlaceholder: Liferay.Language.get('search'),
-                        itemKey: 'id',
-                        itemLabel: 'name',
-                        items: [],
-                        label: Liferay.Language.get('created-by'),
-                        multiple: false,
-                        type: 'selection',
-                },
-        ], []);
-        const requestLabel = useCallback(const requestLabel = useCallback(
-		(fdsName: string) => {
-			if (dataSetLabels[fdsName] !== undefined) {
-				return;
-			}
-
-			if (labelRequestsRef.current.has(fdsName)) {
-				return;
-			}
-
-			labelRequestsRef.current.add(fdsName);
-
-			const url = getDataSetResourceURL({
-				dataSetERC: fdsName,
-				params: {fields: 'label'},
-			});
-
-			fetch(url, {headers: DEFAULT_FETCH_HEADERS})
-				.then((response) => (response.ok ? response.json() : null))
-				.then((responseJSON) => {
-					const label = responseJSON?.label || '';
-
-					setDataSetLabels((previous) => ({
-						...previous,
-						[fdsName]: label,
-					}));
-				})
-				.catch(() => {
-					setDataSetLabels((previous) => ({
-						...previous,
-						[fdsName]: '',
-					}));
-				});
-		},
-		[dataSetLabels]
-	);
-
-	return (
-		<DataSetLabelsContext.Provider
-			value={{
-				dataSetLabels,
-				requestLabel,
-				systemDataSetNamesSet,
-			}}
-		>
-			<div className="data-sets manage-user-views">
-				<FrontendDataSet
-					{...FDS_DEFAULT_PROPS}
-					apiURL={apiURL}
-					currentURL={currentURL}
-					customDataRenderers={{
-						dataSetNameRenderer: DataSetNameRenderer,
-						dataSetTypeRenderer: DataSetTypeRenderer,
-					}}
-					emptyState={{
-						description: Liferay.Language.get(
-							'no-user-views-were-found'
-						),
-						image: '/states/empty_state.svg',
-						title: Liferay.Language.get('no-results-found'),
-					}}
-					id={`${namespace}ManageUserViews`}
-					itemsActions={[
-						{
-							className: 'component-action',
-							data: {
-								confirmationMessage: Liferay.Language.get(
-									'deleting-a-user-view-is-an-action-that-cannot-be-reversed'
-								),
-								errorMessage: Liferay.Language.get(
-									'an-unexpected-error-occurred'
-								),
-								status: 'danger',
-								successMessage: Liferay.Language.get(
-									'view-was-deleted-successfully'
-								),
-								title: Liferay.Language.get('delete-user-view'),
-							},
-							href: '{actions.delete.href}',
-							icon: 'trash',
-							isVisible: (item) => !!item.actions?.delete?.href,
-							label: Liferay.Language.get('delete'),
-							method: 'delete',
-							target: 'headless',
-						},
-					]}
-					portletId={portletId}
-					filters={filters}
-                                        views={[views]}
-				/>
-			</div>
-		</DataSetLabelsContext.Provider>
-	);
-}
+        
