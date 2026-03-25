b741341f5a9f6a0ef99ce5ccee5b27c7483c9f2a
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2026-03-24 19:42:12 -0500

    LCD-50502 Add crossplane IAM and function configuration

--- cloud/terraform/gcp/gitops/resources/locals.tf
+++ cloud/terraform/gcp/gitops/resources/locals.tf
@@ -1,10 +1,36 @@
 locals {
+	cloudplatform_roles=[
+		"roles/iam.serviceAccountAdmin",
+		"roles/resourcemanager.projectIamAdmin",
+	]
 	common_labels={
 		"app.kubernetes.io/component"="gitops-infrastructure"
 		"app.kubernetes.io/managed-by"=local.terraform_manager_name
 		"app.kubernetes.io/part-of"="liferay-gitops"
 		"environment"="internal"
 		"liferay.com/project"="liferay-cloud-native"
+	}
+	default_crossplane_container_security_context={
+		allowPrivilegeEscalation=false
+		capabilities={
+			drop=["ALL"]
+		}
+		privileged=false
+		readOnlyRootFilesystem=true
+	}
+	default_crossplane_pod_security_context={
+		fsGroup=2000
+		runAsGroup=2000
+		runAsNonRoot=true
+		runAsUser=2000
+		seccompProfile={
+			type="RuntimeDefault"
+		}
+	}
+	direct_provider_ksas={
+		compute="roles/compute.admin"
+		sql="roles/cloudsql.admin"
+		storage="roles/storage.admin"
 	}
 	git_repo_auth_configs=merge(
 		local.git_repo_infrastructure_separate_from_liferay ? {
@@ -24,6 +50,7 @@ locals {
 	)
 	git_repo_infrastructure_separate_from_liferay=local.infrastructure_git_repo_url != var.liferay_git_repo_url
 	infrastructure_git_repo_url=coalesce(var.infrastructure_git_repo_config.url, var.liferay_git_repo_url)
+	ksa_principal_base="principal://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${var.project_id}.svc.id.goog/subject/ns/${var.crossplane_namespace}/sa"
 	secret_prefixes={
 		certificates="liferay-certificates-"
 		licenses="liferay-licenses-"
