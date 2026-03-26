cf68a4dcdf964111b4dc4d44afb88a03af1b19f7
Joao Victor <joao.victor@liferay.com>
2026-03-26 17:27:59 -0300

    LPD-73437 follow-up: implement method

--- modules/dxp/apps/ai-hub/ai-hub-rest-impl/src/main/java/com/liferay/ai/hub/rest/internal/resource/v1_0/SiteResourceImpl.java
+++ modules/dxp/apps/ai-hub/ai-hub-rest-impl/src/main/java/com/liferay/ai/hub/rest/internal/resource/v1_0/SiteResourceImpl.java
@@ -6,8 +6,12 @@
 package com.liferay.ai.hub.rest.internal.resource.v1_0;
 
 import com.liferay.ai.hub.rest.resource.v1_0.SiteResource;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.site.initializer.SiteInitializer;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +22,24 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = SiteResource.class
 )
 public class SiteResourceImpl extends BaseSiteResourceImpl {
+
+	@Override
+	public void putSiteByExternalReferenceCodeSiteInitializer(
+			String externalReferenceCode)
+		throws Exception {
+
+		Group group = _groupLocalService.getGroupByExternalReferenceCode(
+			externalReferenceCode, contextCompany.getCompanyId());
+
+		_siteInitializer.initialize(group.getGroupId());
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference(
+		target = "(site.initializer.key=com.liferay.ai.hub.site.initializer)"
+	)
+	private SiteInitializer _siteInitializer;
+
 }
