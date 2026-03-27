e8ba5bce3512f271d0afc9fec7764d357b4e02a0
Nícolas Moura <nicolas_moura02@outlook.com>
2026-03-27 15:06:33 -0300

    Revert "LPD-84056 SF"
    This reverts commit 1e7f3ff881e295ed98e25e62f4e8be88b6efee8c.

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -1349,7 +1349,7 @@ public class ObjectDefinitionResourceImpl
 	private long _getObjectFolderId(String objectFolderExternalReferenceCode)
 		throws Exception {
 
-		ObjectFolder objectFolder = null;
+		ObjectFolder objectFolder;
 
 		if (Validator.isNull(objectFolderExternalReferenceCode)) {
 			objectFolder =
