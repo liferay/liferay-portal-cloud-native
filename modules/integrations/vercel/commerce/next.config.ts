6ebab6438bf6f57173dbcfc1b3c4e136a66b996a
Keven Leone <keven.santos.sz@gmail.com>
2026-03-30 16:02:32 -0300

    LPD-83421 Update next.config to support properly the image domains path

--- modules/integrations/vercel/commerce/next.config.ts
+++ modules/integrations/vercel/commerce/next.config.ts
@@ -12,6 +12,13 @@ const imageDomain = process.env.LIFERAY_HOST
 const nextConfig: NextConfig = {
 	images: {
 		domains: [imageDomain],
+		remotePatterns: [
+			{
+				hostname: imageDomain,
+				protocol: 'https',
+			},
+		],
+		qualities: [100],
 	},
 };
 
