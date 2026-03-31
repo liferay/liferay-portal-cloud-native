0252b7dfe3b08c7414f01a5c5f98123906ad4bcb
Carlos Correa <carlos.correa@liferay.com>
2026-03-31 14:55:27 +0200

    LPD-84206 No longer expected

--- modules/test/playwright/tests/batch-planner/main/export.spec.ts
+++ modules/test/playwright/tests/batch-planner/main/export.spec.ts
@@ -463,10 +463,6 @@ test('can export as JSONL with excluded fields', async ({
 				href: expect.stringContaining('/o/c/stocks/'),
 				method: 'PATCH',
 			},
-			versions: {
-				href: expect.stringContaining('/o/c/stocks/'),
-				method: 'GET',
-			},
 		});
 	});
 });
