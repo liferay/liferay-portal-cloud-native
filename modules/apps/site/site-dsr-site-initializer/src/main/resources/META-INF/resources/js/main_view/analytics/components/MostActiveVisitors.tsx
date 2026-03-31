ec1b6345268dca072de574323567cb65f7eca1c9
monaco-fabio <fabio.monaco@liferay.com>
2026-03-31 17:55:18 +0200

    LPD-78211 Remove duplicate component

--- modules/apps/site/site-dsr-site-initializer/src/main/resources/META-INF/resources/js/main_view/analytics/components/MostActiveVisitors.tsx
+++ modules/apps/site/site-dsr-site-initializer/src/main/resources/META-INF/resources/js/main_view/analytics/components/MostActiveVisitors.tsx
@@ -6,9 +6,8 @@
 import {FrontendDataSet} from '@liferay/frontend-data-set-web';
 import React from 'react';
 
-import AccountSticker from '../../../common/components/AccountSticker';
-
 import './../../../../css/components/MostActiveVisitors.scss';
+import {visitorSticker} from './cell_renderers/VisitorSticker';
 
 type TVisitor = {
 	activitiesCount: number;
@@ -17,44 +16,6 @@ type TVisitor = {
 	lastName: string;
 	logoURL: string | undefined;
 };
-
-function visitorSticker({itemData}: {itemData: TVisitor}) {
-	return (
-		<div className="d-flex inline-item">
-			<AccountSticker
-				logoURL={itemData.logoURL}
-				name={itemData.firstName}
-				shape="user-icon"
-			/>
-
-			<div className="ml-3">
-				<div className="align-items-center font-weight-semi-bold visitors-full-name">
-					<span className="mb-0 mr-1">
-						{Liferay.Language.get(itemData.firstName)}
-					</span>
-
-					<span className="mb-0">
-						{Liferay.Language.get(itemData.lastName)}
-					</span>
-				</div>
-
-				<div className="align-items-center">
-					<span className="mb-0 mr-1">
-						{itemData.activitiesCount}
-					</span>
-
-					<span className="mb-0">
-						{Liferay.Language.get('actions')}
-					</span>
-				</div>
-
-				<p className="email-text mb-0 text-secondary">
-					{Liferay.Language.get(itemData.emailAddress)}
-				</p>
-			</div>
-		</div>
-	);
-}
 
 const MostActiveVisitors = ({
 	items = [],
