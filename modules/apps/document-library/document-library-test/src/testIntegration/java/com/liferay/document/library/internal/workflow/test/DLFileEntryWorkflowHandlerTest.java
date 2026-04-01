42768f0f9f652d70fd8ae10aeb8f7f7e0dc16486
Attila Bakay <attila.bakay@liferay.com>
2026-04-01 14:26:45 +0200

    LPD-84632 Update integration test

--- modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/internal/workflow/test/DLFileEntryWorkflowHandlerTest.java
+++ modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/internal/workflow/test/DLFileEntryWorkflowHandlerTest.java
@@ -14,6 +14,7 @@ import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
 import com.liferay.document.library.kernel.service.DLFileEntryTypeLocalService;
+import com.liferay.dynamic.data.mapping.kernel.DDMFormValues;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
@@ -105,6 +106,8 @@ public class DLFileEntryWorkflowHandlerTest {
 
 		_serviceContext.setRequest(httpServletRequest);
 
+		_serviceContext.setAttribute(DDMFormValues.class.getName(), null);
+
 		_addLayoutPageTemplateEntry();
 
 		FileEntry fileEntry = _addFileEntry(null, null);
@@ -124,6 +127,15 @@ public class DLFileEntryWorkflowHandlerTest {
 
 		Map<String, Serializable> approvedWorkflowContext =
 			workflowInstance.getWorkflowContext();
+
+		ServiceContext serviceContext =
+			(ServiceContext)approvedWorkflowContext.get(
+				WorkflowConstants.CONTEXT_SERVICE_CONTEXT);
+
+		Map<String, Serializable> attributes = serviceContext.getAttributes();
+
+		Assert.assertFalse(
+			attributes.containsKey(DDMFormValues.class.getName()));
 
 		Assert.assertNotEquals(
 			StringPool.BLANK,
