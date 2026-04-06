9f5e42800424918788555e05d1dd319e4b6636d7
Manuele Castro <manuele.castro@liferay.com>
2026-04-06 16:12:16 -0300

    LPD-84985 Add test

--- modules/apps/portal-security/portal-security-ldap-impl/src/test/java/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImplTest.java
+++ modules/apps/portal-security/portal-security-ldap-impl/src/test/java/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImplTest.java
@@ -5,7 +5,20 @@
 
 package com.liferay.portal.security.ldap.internal.exportimport;
 
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.ldap.LDAPSettings;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.security.ldap.configuration.ConfigurationProvider;
+import com.liferay.portal.security.ldap.configuration.LDAPServerConfiguration;
+import com.liferay.portal.security.ldap.exportimport.LDAPUser;
+import com.liferay.portal.security.ldap.exportimport.configuration.LDAPImportConfiguration;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Date;
 
 import javax.naming.InvalidNameException;
 
@@ -13,6 +26,9 @@ import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 /**
  * @author Jorge Díaz
@@ -34,6 +50,104 @@ public class LDAPUserImporterImplTest {
 			"cn=User\\\\2cwith\\\\2ccommas,ou=users,dc=example,dc=com",
 			escapeLDAPName(
 				"cn=User\\2cwith\\2ccommas,ou=users,dc=example,dc=com"));
+	}
+
+	@Test
+	public void testUpdateUser() throws Exception {
+		ConfigurationProvider<LDAPImportConfiguration>
+			ldapImportConfigurationProvider = Mockito.mock(
+				ConfigurationProvider.class);
+
+		Mockito.when(
+			ldapImportConfigurationProvider.getConfiguration(Mockito.anyLong())
+		).thenReturn(
+			Mockito.mock(LDAPImportConfiguration.class)
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			_ldapUserImporterImpl, "_ldapImportConfigurationProvider",
+			ldapImportConfigurationProvider);
+
+		ConfigurationProvider<LDAPServerConfiguration>
+			ldapServerConfigurationProvider = Mockito.mock(
+				ConfigurationProvider.class);
+
+		LDAPServerConfiguration ldapServerConfiguration = Mockito.mock(
+			LDAPServerConfiguration.class);
+
+		String modifiedDate = "Thu Apr 2 19:18:33 GMT 2026";
+
+		Mockito.when(
+			ldapServerConfiguration.modifiedDate()
+		).thenReturn(
+			modifiedDate
+		);
+
+		Mockito.when(
+			ldapServerConfigurationProvider.getConfiguration(
+				Mockito.anyLong(), Mockito.anyLong())
+		).thenReturn(
+			ldapServerConfiguration
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			_ldapUserImporterImpl, "_ldapServerConfigurationProvider",
+			ldapServerConfigurationProvider);
+
+		ReflectionTestUtil.setFieldValue(
+			_ldapUserImporterImpl, "_ldapSettings",
+			Mockito.mock(LDAPSettings.class));
+
+		LDAPImportContext ldapImportContext = Mockito.mock(
+			LDAPImportContext.class);
+
+		Mockito.when(
+			ldapImportContext.getCompanyId()
+		).thenReturn(
+			RandomTestUtil.randomLong()
+		);
+
+		Mockito.when(
+			ldapImportContext.getLdapServerId()
+		).thenReturn(
+			RandomTestUtil.randomLong()
+		);
+
+		LDAPUser ldapUser = new LDAPUser();
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setLanguageId(LocaleUtil.BRAZIL.toLanguageTag());
+
+		ldapUser.setServiceContext(serviceContext);
+
+		User user = Mockito.mock(User.class);
+
+		Mockito.when(
+			user.getModifiedDate()
+		).thenReturn(
+			new Date()
+		);
+
+		try (MockedStatic<DateUtil> dateUtilMockedStatic = Mockito.mockStatic(
+				DateUtil.class, Mockito.CALLS_REAL_METHODS)) {
+
+			ReflectionTestUtil.invoke(
+				_ldapUserImporterImpl, "_updateUser",
+				new Class<?>[] {
+					LDAPImportContext.class, LDAPUser.class, User.class,
+					String.class, String.class, boolean.class
+				},
+				ldapImportContext, ldapUser, user,
+				RandomTestUtil.randomString(),
+				String.valueOf(RandomTestUtil.randomLong()), false);
+
+			dateUtilMockedStatic.verify(
+				() -> DateUtil.parseDate(
+					Mockito.eq("EEE MMM d HH:mm:ss zzz yyyy"),
+					Mockito.eq(modifiedDate), Mockito.eq(LocaleUtil.US)),
+				Mockito.times(1));
+		}
 	}
 
 	protected String escapeLDAPName(String query) {
