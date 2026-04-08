b5f609d410d264f3b01e140239cae542b2f27b88
brookedalton <brooke.dalton@liferay.com>
2026-04-08 14:50:22 -0700

    LPD-84569 Add com.liferay.site.initializer.seo.studio to _ALLOWED_INVOKER_BUNDLE_SYMBOLIC_NAMES

--- modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/definition/util/ObjectDefinitionUtil.java
@@ -128,7 +128,8 @@ public class ObjectDefinitionUtil {
 		"com.liferay.headless.builder.impl", "com.liferay.list.type.service",
 		"com.liferay.mcp.server", "com.liferay.notification.service",
 		"com.liferay.object.service", "com.liferay.site.initializer.cmp",
-		"com.liferay.site.initializer.cms", "com.liferay.site.initializer.dsr"
+		"com.liferay.site.initializer.cms", "com.liferay.site.initializer.dsr",
+		"com.liferay.site.initializer.seo.studio"
 	};
 
 	private static final Map<String, String>
