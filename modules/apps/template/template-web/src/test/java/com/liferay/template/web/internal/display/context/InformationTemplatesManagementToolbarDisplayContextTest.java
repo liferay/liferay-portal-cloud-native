b06026c935701c5e848e28e7307511b8aa1c1620
ricardocousolr <ricardo.couso@liferay.com>
2026-04-08 18:18:35 +0200

    LPD-63947 Add test to check order is correct
    Generalize existing _testGetItemTypesJSONArray to allow several labels
    allowing for the current tests of single and null labels

--- modules/apps/template/template-web/src/test/java/com/liferay/template/web/internal/display/context/InformationTemplatesManagementToolbarDisplayContextTest.java
+++ modules/apps/template/template-web/src/test/java/com/liferay/template/web/internal/display/context/InformationTemplatesManagementToolbarDisplayContextTest.java
@@ -32,6 +32,8 @@ import jakarta.portlet.PortletURL;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.AfterClass;
@@ -122,6 +124,12 @@ public class InformationTemplatesManagementToolbarDisplayContextTest {
 		String label = RandomTestUtil.randomString();
 
 		_testGetItemTypesJSONArray(label, label);
+
+	@Test
+	public void testSortedGetItemTypesJSONArray() {
+		_testGetItemTypesJSONArray(
+			Arrays.asList("EarlierInTheAlphabet", "LaterInTheAlphabet"),
+			Arrays.asList("LaterInTheAlphabet", "EarlierInTheAlphabet"));
 	}
 
 	private InfoItemClassDetails _mockInfoItemClassDetails() {
@@ -170,6 +178,65 @@ public class InformationTemplatesManagementToolbarDisplayContextTest {
 		themeDisplay.setLocale(LocaleUtil.CANADA);
 
 		return themeDisplay;
+	}
+
+	private void _testGetItemTypesJSONArray(
+		List<String> expectedLabels, List<String> labels) {
+
+		List<InfoItemFormVariation> informationItemFormVariations =
+			new ArrayList<>();
+		List<String> keys = new ArrayList<>();
+
+		for (String label : labels) {
+			String key = RandomTestUtil.randomString();
+
+			keys.add(key);
+
+			informationItemFormVariations.add(
+				_mockInfoItemFormVariation(key, label));
+		}
+
+		Mockito.when(
+			_infoItemFormVariationsProvider.getInfoItemFormVariations(0)
+		).thenReturn(
+			informationItemFormVariations
+		);
+
+		JSONArray jsonArray = ReflectionTestUtil.invoke(
+			_informationTemplatesManagementToolbarDisplayContext,
+			"_getItemTypesJSONArray", new Class<?>[0]);
+
+		Assert.assertEquals(jsonArray.toString(), 1, jsonArray.length());
+
+		JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+		Assert.assertEquals(_CLASS_NAME, jsonObject.getString("value"));
+		Assert.assertEquals(_LABEL, jsonObject.getString("label"));
+
+		Assert.assertTrue(jsonObject.has("subtypes"));
+
+		JSONArray subtypesJSONArray = jsonObject.getJSONArray("subtypes");
+
+		Assert.assertEquals(
+			subtypesJSONArray.toString(), labels.size(),
+			subtypesJSONArray.length());
+
+		for (int i = 0; i < labels.size(); i++) {
+			JSONObject subtypesJSONObject = subtypesJSONArray.getJSONObject(i);
+
+			String expectedLabel = expectedLabels.get(i);
+
+			int index = labels.indexOf(expectedLabel);
+
+			if ((index < 0) && StringPool.BLANK.equals(expectedLabel)) {
+				index = labels.indexOf(null);
+			}
+
+			Assert.assertEquals(
+				keys.get(index), subtypesJSONObject.getString("value"));
+			Assert.assertEquals(
+				expectedLabel, subtypesJSONObject.getString("label"));
+		}
 	}
 
 	private void _testGetItemTypesJSONArray(
